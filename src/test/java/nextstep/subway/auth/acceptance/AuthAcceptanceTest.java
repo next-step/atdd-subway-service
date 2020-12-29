35caa170d09168d6dcc467bfba55d438b898975f
cionman <topspin1278@naver.com>
2020-12-30 04:30:48 +0900

    test : 토큰 발급 기능 (로그인) 인수 테스트 작성

--- src/test/java/nextstep/subway/auth/acceptance/AuthAcceptanceTest.java
+++ src/test/java/nextstep/subway/auth/acceptance/AuthAcceptanceTest.java
@@ -1,24 +1,84 @@
 package nextstep.subway.auth.acceptance;
 
-import nextstep.subway.AcceptanceTest;
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import nextstep.subway.AcceptanceTest;
+import nextstep.subway.auth.dto.TokenRequest;
+import nextstep.subway.auth.dto.TokenResponse;
+import nextstep.subway.auth.infrastructure.JwtTokenProvider;
+import nextstep.subway.member.MemberAcceptanceTest;
+
+@DisplayName("로그인 기능")
 public class AuthAcceptanceTest extends AcceptanceTest {
+	public static final String EMAIL = "test@test.com";
+	public static final String PASSWORD = "testpass";
+	public static final String WRONG_PASSWORD = "testpass1";
+	public static final int AGE = 20;
 
-    @DisplayName("Bearer Auth")
-    @Test
-    void myInfoWithBearerAuth() {
-    }
+	@Autowired
+	private JwtTokenProvider jwtTokenProvider;
 
-    @DisplayName("Bearer Auth 로그인 실패")
-    @Test
-    void myInfoWithBadBearerAuth() {
-    }
+	@BeforeEach
+	public void setUp() {
+		super.setUp();
+		MemberAcceptanceTest.회원_생성을_요청(EMAIL, PASSWORD, AGE);
+	}
 
-    @DisplayName("Bearer Auth 유효하지 않은 토큰")
-    @Test
-    void myInfoWithWrongBearerAuth() {
-    }
+	@DisplayName("로그인을 시도")
+	@Test
+	void myInfoWithBearerAuth() {
+		//when
+		ExtractableResponse<Response> response = 로그인_요청(EMAIL, PASSWORD);
+		//then
+		로그인_됨(response);
+	}
+
+	@DisplayName("잘못된 패스워드로 로그인 시도, 로그인 실패한다.")
+	@Test
+	void myInfoWithBadBearerAuth() {
+		//when
+		ExtractableResponse<Response> response = 로그인_요청(EMAIL, WRONG_PASSWORD);
+		//then
+		로그인_실패됨(response);
+	}
+
+	public static ExtractableResponse<Response> 로그인_요청(String email, String password) {
+		TokenRequest tokenRequest = new TokenRequest(email, password);
+
+		return RestAssured
+			.given().log().all()
+			.contentType(MediaType.APPLICATION_JSON_VALUE)
+			.body(tokenRequest)
+			.when().post("/login/token")
+			.then().log().all()
+			.extract();
+	}
+
+	private void 로그인_됨(ExtractableResponse<Response> response) {
+
+		TokenResponse tokenResponse = response.as(TokenResponse.class);
+		boolean isValid = jwtTokenProvider.validateToken(tokenResponse.getAccessToken());
+
+		assertAll(
+			() -> assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value()),
+			() -> assertThat(isValid).isTrue()
+		);
+	}
+
+	private void 로그인_실패됨(ExtractableResponse<Response> response) {
+
+		assertThat(response.statusCode()).isEqualTo(HttpStatus.UNAUTHORIZED.value());
+	}
 
 }
