e366648be1f6881a36aff5a8526d3a7876ff0c4c
poppohoo <ccf05017@gmail.com>
2020-12-29 11:11:58 +0900

    refactor(LineService): add read only transaction
    - 쿼리 안정성을 높이기 위해 조회로만 사용되는 쿼리에 read only 옵션 추가

--- src/main/java/nextstep/subway/line/application/LineService.java
+++ src/main/java/nextstep/subway/line/application/LineService.java
@@ -40,6 +40,7 @@ public class LineService {
         return LineResponse.of(persistLine, stations);
     }
 
+    @Transactional(readOnly = true)
     public List<LineResponse> findLines() {
         List<Line> persistLines = lineRepository.findAll();
         return persistLines.stream()
@@ -52,12 +53,13 @@ public class LineService {
                 .collect(Collectors.toList());
     }
 
+    @Transactional(readOnly = true)
     public Line findLineById(Long id) {
         return lineRepository.findById(id)
                 .orElseThrow(() -> new EntityNotFoundException("해당 지하철 노선이 존재하지 않습니다."));
     }
 
-
+    @Transactional(readOnly = true)
     public LineResponse findLineResponseById(Long id) {
         Line persistLine = findLineById(id);
         List<StationResponse> stations = persistLine.getStations().stream()
