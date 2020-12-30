765756a27ae9a3ff327e5359be1590114e2ea6a6
poppohoo <ccf05017@gmail.com>
2020-12-30 15:39:25 +0900

    test(JgraphTest): make two lines with connection
    - 접점이 있는 두개의 그래프를 만들고 탐색하는 학습 테스트 진행

--- src/test/java/study/jgraph/JgraphTest.java
+++ src/test/java/study/jgraph/JgraphTest.java
@@ -2,6 +2,7 @@ package study.jgraph;
 
 import nextstep.subway.line.domain.Line;
 import nextstep.subway.station.StationFixtures;
+import org.jgrapht.EdgeFactory;
 import org.jgrapht.GraphPath;
 import org.jgrapht.alg.interfaces.ShortestPathAlgorithm;
 import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
@@ -12,7 +13,10 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -88,5 +92,77 @@ public class JgraphTest {
             ShortestPathAlgorithm.SingleSourcePaths paths = dijkstraShortestPath.getPaths(station);
             assertThat(paths.getGraph()).isEqualTo(graph);
         });
+    }
+
+    @DisplayName("겹치는 구간이 있는 그래프 두개 그리고 탐색해보기")
+    @Test
+    void twoLinesWithConnectionTest() {
+        /*
+                         왕십리역
+                            |
+            강남역 - 역삼역 - 선릉역
+                            |
+                           분당역
+         */
+
+        String station1 = "강남역";
+        String station2 = "역삼역";
+        String station3 = "선릉역";
+        String station4 = "분당역";
+        String station5 = "왕십리역";
+
+        // 아예 처음부터 통째로 다 등록해버린다. (이렇게 어마어마하게 큰 Path를 만들어도 문제는 없을까?)
+        WeightedMultigraph<String, DefaultWeightedEdge> path = new WeightedMultigraph(DefaultWeightedEdge.class);
+        // 2호선 등록
+        path.addVertex(station1);
+        path.addVertex(station2);
+        path.addVertex(station3);
+        path.setEdgeWeight(path.addEdge(station1, station2), 1);
+        path.setEdgeWeight(path.addEdge(station2, station3), 2);
+        // 분당선 등록
+        path.addVertex(station3);   // vetex는 set으로 관리되기 때문에 중복되도 상관 없음
+        path.addVertex(station4);
+        path.addVertex(station5);
+        path.setEdgeWeight(path.addEdge(station5, station3), 3);
+        path.setEdgeWeight(path.addEdge(station3, station4), 4);
+
+        DijkstraShortestPath dijkstraShortestPath = new DijkstraShortestPath(path);
+        List stations = dijkstraShortestPath.getPath(station1, station5).getVertexList();
+        assertThat(stations.get(0)).isEqualTo(station1);
+        assertThat(stations.get(1)).isEqualTo(station2);
+        assertThat(stations.get(2)).isEqualTo(station3);
+        assertThat(stations.get(3)).isEqualTo(station5);
+
+        double pathWeight = dijkstraShortestPath.getPathWeight(station1, station5);
+        assertThat(pathWeight).isEqualTo(6);
+
+        // 그래프를 나누고 통합하는 방향으로 구현해보려 했으나, edge를 더하는 방식에서 문제 발생
+//        WeightedMultigraph<String, DefaultWeightedEdge> lineNumberTwo = new WeightedMultigraph(DefaultWeightedEdge.class);
+//        lineNumberTwo.addVertex(station1);
+//        lineNumberTwo.addVertex(station2);
+//        lineNumberTwo.addVertex(station3);
+//        lineNumberTwo.setEdgeWeight(lineNumberTwo.addEdge(station1, station2), 1);
+//        lineNumberTwo.setEdgeWeight(lineNumberTwo.addEdge(station2, station3), 2);
+//
+//        WeightedMultigraph<String, DefaultWeightedEdge> lineBundang = new WeightedMultigraph(DefaultWeightedEdge.class);
+//        lineBundang.addVertex(station5);
+//        lineBundang.addVertex(station3);
+//        lineBundang.addVertex(station4);
+//        lineBundang.setEdgeWeight(lineBundang.addEdge(station5, station3), 3);
+//        lineBundang.setEdgeWeight(lineBundang.addEdge(station3, station4), 4);
+//
+//        Set<String> lineNumberTwoStations = lineNumberTwo.vertexSet();
+//        Set<String> lineBundangStations = lineBundang.vertexSet();
+//        Set<String> mergeStations = new HashSet<>(lineNumberTwoStations);
+//        mergeStations.addAll(lineBundangStations);
+//
+//        Set<DefaultWeightedEdge> lineNumberTwoEdges = lineNumberTwo.getAllEdges(station1, station3);
+//        Set<DefaultWeightedEdge> lineBundangAllEdges = lineBundang.getAllEdges(station5, station4);
+//        Set<DefaultWeightedEdge> mergeEdges = new HashSet<>(lineNumberTwoEdges);
+//        mergeEdges.addAll(lineBundangAllEdges);
+//
+//        WeightedMultigraph<String, DefaultWeightedEdge> allPaths = new WeightedMultigraph(DefaultWeightedEdge.class);
+//        mergeStations.stream().forEach(allPaths::addVertex);
+        // mergeEdges.stream().forEach(edge -> allPaths.addEdge());
     }
 }
