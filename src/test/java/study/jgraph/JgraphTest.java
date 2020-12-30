c506316a79c7a06aad7b4b964c37142ad275f61f
poppohoo <ccf05017@gmail.com>
2020-12-30 14:56:49 +0900

    test(Jgrapht): learning test
    - 경로가 한 개일 때 Jgrapht 탐색 관련 학습 테스트 진행

--- src/test/java/study/jgraph/JgraphTest.java
+++ src/test/java/study/jgraph/JgraphTest.java
@@ -1,13 +1,19 @@
 package study.jgraph;
 
+import nextstep.subway.line.domain.Line;
+import nextstep.subway.station.StationFixtures;
 import org.jgrapht.GraphPath;
+import org.jgrapht.alg.interfaces.ShortestPathAlgorithm;
 import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
 import org.jgrapht.alg.shortestpath.KShortestPaths;
 import org.jgrapht.graph.DefaultWeightedEdge;
 import org.jgrapht.graph.WeightedMultigraph;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.IntStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -49,6 +55,38 @@ public class JgraphTest {
         paths.forEach(it -> {
                     assertThat(it.getVertexList()).startsWith(source);
                     assertThat(it.getVertexList()).endsWith(target);
+        });
+    }
+
+    @DisplayName("노선 한개짜리 그래프를 그리고 탐색하기")
+    @Test
+    void justOneLineTest() {
+        String station1 = "강남역";
+        String station2 = "역삼역";
+        String station3 = "선릉역";
+
+        WeightedMultigraph<String, DefaultWeightedEdge> graph = new WeightedMultigraph(DefaultWeightedEdge.class);
+        graph.addVertex(station1);
+        graph.addVertex(station2);
+        graph.addVertex(station3);
+        graph.setEdgeWeight(graph.addEdge(station1, station2), 2);
+        graph.setEdgeWeight(graph.addEdge(station2, station3), 2);
+
+        DijkstraShortestPath dijkstraShortestPath = new DijkstraShortestPath(graph);
+
+        // 최단 경로 탐색 가능
+        List<String> shortestPath = dijkstraShortestPath.getPath(station1, station3).getVertexList();
+        assertThat(shortestPath.get(0)).isEqualTo(station1);
+        assertThat(shortestPath.get(2)).isEqualTo(station3);
+
+        // 탐색까지의 총 소요되는 가중치 계산 가능
+        double pathWeight = dijkstraShortestPath.getPathWeight(station1, station3);
+        assertThat(pathWeight).isEqualTo(4);
+
+        // 경로의 그래프 확인 가능
+        Arrays.asList(station1, station2, station3).forEach(station -> {
+            ShortestPathAlgorithm.SingleSourcePaths paths = dijkstraShortestPath.getPaths(station);
+            assertThat(paths.getGraph()).isEqualTo(graph);
         });
     }
 }
