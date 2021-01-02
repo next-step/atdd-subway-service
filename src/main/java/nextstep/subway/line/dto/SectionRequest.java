81768c94f42a245571d8867ccadefca51a342480
jiho <jhhj424@naver.com>
2021-01-03 01:01:46 +0900

    refactor: `int distance` -> Distance 객체로 포장 (distance에 대한 검증은 모두 Distance 클래스에서 할 수 있음)

--- src/main/java/nextstep/subway/line/dto/SectionRequest.java
+++ src/main/java/nextstep/subway/line/dto/SectionRequest.java
@@ -1,9 +1,11 @@
 package nextstep.subway.line.dto;
 
+import nextstep.subway.line.domain.Distance;
+
 public class SectionRequest {
     private Long upStationId;
     private Long downStationId;
-    private int distance;
+    private Distance distance;
 
     public SectionRequest() {
     }
@@ -11,7 +13,7 @@ public class SectionRequest {
     public SectionRequest(Long upStationId, Long downStationId, int distance) {
         this.upStationId = upStationId;
         this.downStationId = downStationId;
-        this.distance = distance;
+        this.distance = new Distance(distance);
     }
 
     public Long getUpStationId() {
@@ -23,6 +25,6 @@ public class SectionRequest {
     }
 
     public int getDistance() {
-        return distance;
+        return distance.get();
     }
 }
