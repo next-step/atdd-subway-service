470e6c6e15722724e65953d8e48f4e3afb773053
yohan <good_1411@naver.com>
2021-01-03 14:50:03 +0900

    refactor: 불필요 로직을 제거합니다.

--- src/main/java/nextstep/subway/line/domain/Line.java
+++ src/main/java/nextstep/subway/line/domain/Line.java
@@ -5,7 +5,6 @@ import nextstep.subway.BaseEntity;
 import nextstep.subway.station.domain.Station;
 
 import javax.persistence.*;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -18,11 +17,8 @@ public class Line extends BaseEntity {
     private String name;
     private String color;
 
-    @OneToMany(mappedBy = "line", cascade = {CascadeType.PERSIST, CascadeType.MERGE}, orphanRemoval = true)
-    private List<Section> sections = new ArrayList<>();
-
     @Embedded
-    private Sections newSections;
+    private Sections sections;
 
     public Line() {
     }
@@ -37,8 +33,7 @@ public class Line extends BaseEntity {
         this.name = name;
         this.color = color;
         Section section = new Section(this, upStation, downStation, distance);
-        sections.add(section);
-        this.newSections = new Sections(Arrays.asList(section));
+        this.sections = new Sections(Arrays.asList(section));
     }
 
     public void update(Line line) {
@@ -58,19 +53,15 @@ public class Line extends BaseEntity {
         return color;
     }
 
-    public List<Section> getSections() {
-        return sections;
-    }
-
     public List<Station> getOrderedStations() {
-        return newSections.getOrderedStations();
+        return sections.getOrderedStations();
     }
 
     public void add(final Section section) {
-        newSections.add(section);
+        sections.add(section);
     }
 
     public void remove(final Station station) {
-        newSections.remove(station);
+        sections.remove(station);
     }
 }
