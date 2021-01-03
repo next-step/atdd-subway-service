0fdad860dffba72ef2c9941791840c9e20616a4c
poppohoo <ccf05017@gmail.com>
2021-01-03 20:55:35 +0900

    feat(FeeCalculatorService): can calculate transfer extra fee
    - 최단경로에서 환승한 경우 환승 추가 요금이 제대로 계산되는지 확인하는 테스트 추가

--- src/main/java/nextstep/subway/path/domain/FeeCalculatorService.java
+++ src/main/java/nextstep/subway/path/domain/FeeCalculatorService.java
@@ -1,13 +1,33 @@
 package nextstep.subway.path.domain;
 
+import nextstep.subway.path.domain.adapters.SafeLineAdapter;
 import nextstep.subway.path.domain.fee.distanceFee.*;
+import nextstep.subway.path.domain.fee.transferFee.LineOfStationInPaths;
+import nextstep.subway.path.domain.fee.transferFee.TransferLines;
 import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+import java.util.List;
 
 @Component
 public class FeeCalculatorService {
+    private final SafeLineAdapter safeLineAdapter;
+
+    public FeeCalculatorService(SafeLineAdapter safeLineAdapter) {
+        this.safeLineAdapter = safeLineAdapter;
+    }
+
     DistanceFee calculateDistanceFee(ShortestPath shortestPath) {
         int distance = (int) shortestPath.calculateTotalDistance();
 
         return DistanceFeeSelector.select(distance);
+    }
+
+    BigDecimal calculateTransferFee(ShortestPath shortestPath) {
+        List<Long> pathStations = shortestPath.getPathStations();
+        LineOfStationInPaths lineOfStationInPaths = safeLineAdapter.getLineOfStationInPaths(pathStations);
+        TransferLines transferLines = lineOfStationInPaths.findTransferLines();
+
+        return transferLines.calculateTransferFee();
     }
 }
--- src/test/java/nextstep/subway/path/domain/DistanceFeeCalculatorServiceTest.java
+++ src/test/java/nextstep/subway/path/domain/DistanceFeeCalculatorServiceTest.java
@@ -1,27 +1,39 @@
 package nextstep.subway.path.domain;
 
-import nextstep.subway.path.domain.fee.distanceFee.DefaultDistanceFee;
+import nextstep.subway.path.domain.adapters.SafeLineAdapter;
 import nextstep.subway.path.domain.fee.distanceFee.DistanceFee;
+import nextstep.subway.path.domain.fee.transferFee.LineOfStationInPath;
+import nextstep.subway.path.domain.fee.transferFee.LineOfStationInPaths;
+import nextstep.subway.path.domain.fee.transferFee.LineWithExtraFee;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.math.BigDecimal;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
 
+@ExtendWith(MockitoExtension.class)
 class DistanceFeeCalculatorServiceTest {
     private FeeCalculatorService feeCalculatorService;
 
+    @Mock
+    private SafeLineAdapter safeLineAdapter;
+
     @BeforeEach
     void setup() {
-        feeCalculatorService = new FeeCalculatorService();
+        feeCalculatorService = new FeeCalculatorService(safeLineAdapter);
     }
 
     @DisplayName("거리에 따른 요금을 계산할 수 있다.")
@@ -49,5 +61,34 @@ class DistanceFeeCalculatorServiceTest {
                 Arguments.of(1L, 3L, BigDecimal.valueOf(1350)),
                 Arguments.of(1L, 5L, BigDecimal.valueOf(2150))
         );
+    }
+
+    @DisplayName("환승 노선에 따른 추가금을 계산할 수 있다.")
+    @Test
+    void calculateTransferFeeTest() {
+        Long source = 1L;
+        Long target = 3L;
+
+        List<Long> stationIds = Arrays.asList(1L, 2L, 3L, 4L);
+        List<SafeSectionInfo> safeSectionInfos = Arrays.asList(
+                new SafeSectionInfo(1L, 2L, 5),
+                new SafeSectionInfo(2L, 3L, 6),
+                new SafeSectionInfo(2L, 4L, 5)
+        );
+
+        PathFinder pathFinder = PathFinder.of(stationIds, safeSectionInfos);
+        ShortestPath shortestPath = pathFinder.findShortestPath(source, target);
+
+        LineOfStationInPaths transferOnce = new LineOfStationInPaths(Arrays.asList(
+                new LineOfStationInPath(Collections.singletonList(new LineWithExtraFee(1L, BigDecimal.ZERO))),
+                new LineOfStationInPath(Arrays.asList(new LineWithExtraFee(1L, BigDecimal.ZERO), new LineWithExtraFee(2L, BigDecimal.TEN))),
+                new LineOfStationInPath(Collections.singletonList(new LineWithExtraFee(2L, BigDecimal.TEN)))
+        ));
+
+        given(safeLineAdapter.getLineOfStationInPaths(Arrays.asList(1L, 2L, 3L))).willReturn(transferOnce);
+
+        BigDecimal transferFee = feeCalculatorService.calculateTransferFee(shortestPath);
+
+        assertThat(transferFee).isEqualTo(BigDecimal.valueOf(10));
     }
 }
