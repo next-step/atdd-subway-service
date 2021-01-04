1bff8ac520a794483aff419f39c21b8679e6cd57
hyeji <khji1210@naver.com>
2021-01-04 19:21:36 +0900

    refactor : 기존 메서드 삭제

--- src/main/java/nextstep/subway/line/application/LineService.java
+++ src/main/java/nextstep/subway/line/application/LineService.java
@@ -6,17 +6,13 @@ import nextstep.subway.line.domain.Section;
 import nextstep.subway.line.dto.LineRequest;
 import nextstep.subway.line.dto.LineResponse;
 import nextstep.subway.line.dto.SectionRequest;
+import nextstep.subway.line.exception.LineNotFoundException;
 import nextstep.subway.station.application.StationService;
 import nextstep.subway.station.domain.Station;
-import nextstep.subway.station.dto.StationResponse;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
 
 @Service
 @Transactional
@@ -29,16 +25,6 @@ public class LineService {
         this.stationService = stationService;
     }
 
-//    public LineResponse saveLine(LineRequest request) {
-//        Station upStation = stationService.findById(request.getUpStationId());
-//        Station downStation = stationService.findById(request.getDownStationId());
-//        Line persistLine = lineRepository.save(request.toLine(upStation, downStation));
-//        List<StationResponse> stations = getStations(persistLine).stream()
-//                .map(StationResponse::of)
-//                .collect(Collectors.toList());
-//        return LineResponse.of(persistLine, stations);
-//    }
-
     public LineResponse saveLine(LineRequest request) {
         Station upStation = stationService.findById(request.getUpStationId());
         Station downStation = stationService.findById(request.getDownStationId());
@@ -47,34 +33,16 @@ public class LineService {
         return LineResponse.of(persistLine);
     }
 
-//    public List<LineResponse> findLines() {
-//        List<Line> persistLines = lineRepository.findAll();
-//        return persistLines.stream()
-//                .map(line -> {
-//                    List<StationResponse> stations = getStations(line).stream()
-//                            .map(StationResponse::of)
-//                            .collect(Collectors.toList());
-//                    return LineResponse.of(line, stations);
-//                })
-//                .collect(Collectors.toList());
-//    }
-
     public List<LineResponse> findLines() {
         return LineResponse.ofList(lineRepository.findAll());
     }
 
     public Line findLineById(Long id) {
-        return lineRepository.findById(id).orElseThrow(RuntimeException::new);
+        return lineRepository.findById(id).orElseThrow(() -> new LineNotFoundException("해당 Line을 찾을 수가 없습니다."));
     }
 
-
     public LineResponse findLineResponseById(Long id) {
-        Line persistLine = findLineById(id);
-//        List<StationResponse> stations = getStations(persistLine).stream()
-//                .map(it -> StationResponse.of(it))
-//                .collect(Collectors.toList());
-
-        return LineResponse.of(persistLine);
+        return LineResponse.of(findLineById(id));
     }
 
     public void updateLine(Long id, LineRequest lineUpdateRequest) {
@@ -100,45 +68,4 @@ public class LineService {
 
         line.removeSection(station);
     }
-
-
-//    public List<Station> getStations(Line line) {
-//        if (line.getSections().getSections().isEmpty()) {
-//            return Arrays.asList();
-//        }
-//
-//        List<Station> stations = new ArrayList<>();
-//        Station downStation = findUpStation(line);
-//        stations.add(downStation);
-//
-//        while (downStation != null) {
-//            Station finalDownStation = downStation;
-//            Optional<Section> nextLineStation = line.getSections().getSections().stream()
-//                    .filter(it -> it.getUpStation() == finalDownStation)
-//                    .findFirst();
-//            if (!nextLineStation.isPresent()) {
-//                break;
-//            }
-//            downStation = nextLineStation.get().getDownStation();
-//            stations.add(downStation);
-//        }
-//
-//        return stations;
-//    }
-
-//    private Station findUpStation(Line line) {
-//        Station downStation = line.getSections().getSections().get(0).getUpStation();
-//        while (downStation != null) {
-//            Station finalDownStation = downStation;
-//            Optional<Section> nextLineStation = line.getSections().getSections().stream()
-//                    .filter(it -> it.getDownStation() == finalDownStation)
-//                    .findFirst();
-//            if (!nextLineStation.isPresent()) {
-//                break;
-//            }
-//            downStation = nextLineStation.get().getUpStation();
-//        }
-//
-//        return downStation;
-//    }
 }
---
+++ src/main/java/nextstep/subway/line/exception/LineNotFoundException.java
@@ -0,0 +1,7 @@
+package nextstep.subway.line.exception;
+
+public class LineNotFoundException extends RuntimeException {
+    public LineNotFoundException(String message) {
+        super(message);
+    }
+}
