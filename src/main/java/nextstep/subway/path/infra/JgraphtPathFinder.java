f89767fc93efdf170926c1f19af65bd39e03503a
yohan <good_1411@naver.com>
2021-01-04 21:31:59 +0900

    feat: 경로 탐색기(PathFinder)에 대한 로직을 구현합니다.
    - [X] 노선 하나의 지하철역에서 최단거리를 구한다.
    - [X] 노선 여러개의 지하철역에서 최단거리를 구한다.
    - [X] 출발역과 도착역이 같은 경우를 예외처리 한다.
    - [X] 출발역과 도착역이 연결이 되어 있지 않은 경우 예외처리 한다.
    - [X] 존재하지 않은 출발역이나 도착역을 조회 할 경우 예외처리 한다.

---
+++ src/main/java/nextstep/subway/path/infra/JgraphtPathFinder.java
@@ -0,0 +1,58 @@
+package nextstep.subway.path.infra;
+
+import lombok.RequiredArgsConstructor;
+import nextstep.subway.path.domain.Path;
+import nextstep.subway.path.domain.PathFinder;
+import nextstep.subway.path.domain.PathSections;
+import nextstep.subway.path.domain.PathStation;
+import org.jgrapht.GraphPath;
+import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
+import org.jgrapht.graph.DefaultWeightedEdge;
+import org.jgrapht.graph.WeightedMultigraph;
+import org.springframework.stereotype.Component;
+
+import java.util.Objects;
+
+@RequiredArgsConstructor
+@Component
+public class JgraphtPathFinder implements PathFinder {
+
+    @Override
+    public Path findShortest(final PathSections pathSections, final PathStation source, final PathStation target) {
+        validate(source, target);
+
+        WeightedMultigraph<PathStation, DefaultWeightedEdge> graph = new WeightedMultigraph<>(DefaultWeightedEdge.class);
+        DijkstraShortestPath<PathStation, DefaultWeightedEdge> dijkstraShortestPath = initGraph(graph, pathSections);
+        GraphPath<PathStation, DefaultWeightedEdge> shortestPath = dijkstraShortestPath.getPath(source, target);
+        if (isDisconnectedPath(shortestPath)) {
+            throw new IllegalArgumentException("연결되지 않은 지하철역 입니다.");
+        }
+
+        return new Path(shortestPath.getVertexList(), (int) shortestPath.getWeight());
+    }
+
+    private void validate(final PathStation source, final PathStation target) {
+        if (source.equals(target)) {
+            throw new IllegalArgumentException("출발역과 도착역이 동일합니다.");
+        }
+    }
+
+    private boolean isDisconnectedPath(final GraphPath<PathStation, DefaultWeightedEdge> path) {
+        return Objects.isNull(path);
+    }
+
+    private DijkstraShortestPath<PathStation, DefaultWeightedEdge> initGraph(
+            final WeightedMultigraph<PathStation, DefaultWeightedEdge> graph,
+            final PathSections pathSections) {
+        pathSections.getPathStations()
+                .forEach(graph::addVertex);
+
+        pathSections.getPathSections()
+                .forEach(pathSection -> graph.setEdgeWeight(graph.addEdge(
+                        pathSection.getUpStation(),
+                        pathSection.getDownStation()),
+                        pathSection.getDistance())
+                );
+        return new DijkstraShortestPath<>(graph);
+    }
+}
