78c920a6356ae2087f63d1796c4b49d74ff74575
Pilhwan Kim <pilhwankim03@gmail.com>
2021-01-06 14:42:28 +0900

    test: 노석에 역 추가 & 삭제 여러 케이스별로 단위 테스트 추가

--- src/test/java/nextstep/subway/line/domain/LineTest.java
+++ src/test/java/nextstep/subway/line/domain/LineTest.java
@@ -25,7 +25,7 @@ class LineTest {
         신분당선 = new Line(1L, "신분당선", "bg-red-600", 강남역, 역삼역, 10);
     }
 
-    @DisplayName("종점역을 조회함 - Top upStation")
+    @DisplayName("상행 종점역을 조회함")
     @Test
     void findUpStation() {
         //given
@@ -47,9 +47,9 @@ class LineTest {
         assertThat(stations).containsExactly(강남역, 역삼역);
     }
 
-    @DisplayName("한 노선에 역 추가")
+    @DisplayName("노선에 역 추가 - 기존 upStation 기준 중간에 추가")
     @Test
-    void addLineStation() {
+    void addLineStation1() {
         //given
         //when
         신분당선.addLineStation(강남역, 잠실역, 4);
@@ -59,9 +59,45 @@ class LineTest {
         assertThat(stations).containsExactly(강남역, 잠실역, 역삼역);
     }
 
-    @DisplayName("한 노선에 역 삭제")
+    @DisplayName("노선에 역 추가 - 기존 downStation 기준 중간에 추가")
     @Test
-    void removeLineStation() {
+    void addLineStation2() {
+        //given
+        //when
+        신분당선.addLineStation(잠실역, 역삼역, 4);
+
+        //then
+        List<Station> stations = 신분당선.getStations();
+        assertThat(stations).containsExactly(강남역, 잠실역, 역삼역);
+    }
+
+    @DisplayName("노선에 역 추가 - 최 상행역 앞에 추가")
+    @Test
+    void addLineStation3() {
+        //given
+        //when
+        신분당선.addLineStation(잠실역, 강남역, 4);
+
+        //then
+        List<Station> stations = 신분당선.getStations();
+        assertThat(stations).containsExactly(잠실역, 강남역, 역삼역);
+    }
+
+    @DisplayName("노선에 역 추가 - 최 하행역 뒤에 추가")
+    @Test
+    void addLineStation4() {
+        //given
+        //when
+        신분당선.addLineStation(역삼역, 잠실역, 4);
+
+        //then
+        List<Station> stations = 신분당선.getStations();
+        assertThat(stations).containsExactly(강남역, 역삼역, 잠실역);
+    }
+
+    @DisplayName("노선에 역 삭제 - 최 상행역 삭제 ")
+    @Test
+    void removeLineStation1() {
         //given
         신분당선.addLineStation(강남역, 잠실역, 4);
 
@@ -70,5 +106,31 @@ class LineTest {
         //then
         List<Station> stations = 신분당선.getStations();
         assertThat(stations).containsExactly(잠실역, 역삼역);
+    }
+
+    @DisplayName("노선에 역 삭제 - 중간역 삭제")
+    @Test
+    void removeLineStation2() {
+        //given
+        신분당선.addLineStation(강남역, 잠실역, 4);
+
+        //when
+        신분당선.removeLineStation(잠실역);
+        //then
+        List<Station> stations = 신분당선.getStations();
+        assertThat(stations).containsExactly(강남역, 역삼역);
+    }
+
+    @DisplayName("노선에 역 삭제 - 최 하행역 삭제")
+    @Test
+    void removeLineStation3() {
+        //given
+        신분당선.addLineStation(강남역, 잠실역, 4);
+
+        //when
+        신분당선.removeLineStation(역삼역);
+        //then
+        List<Station> stations = 신분당선.getStations();
+        assertThat(stations).containsExactly(강남역, 잠실역);
     }
 }
