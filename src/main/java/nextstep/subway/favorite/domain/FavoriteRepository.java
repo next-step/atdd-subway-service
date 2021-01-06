b4cd86c558bcea0ab2de0e59caa565871dcd426d
yohan <good_1411@naver.com>
2021-01-06 23:07:21 +0900

    refactor: 쿼리 성능을 최적화합니다.

--- src/main/java/nextstep/subway/favorite/domain/FavoriteRepository.java
+++ src/main/java/nextstep/subway/favorite/domain/FavoriteRepository.java
@@ -1,5 +1,6 @@
 package nextstep.subway.favorite.domain;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -7,7 +8,9 @@ import java.util.Optional;
 
 public interface FavoriteRepository extends JpaRepository<Favorite, Long> {
 
+    @EntityGraph(attributePaths = {"sourceStation", "targetStation"})
     List<Favorite> findAllByMemberId(Long memberId);
 
+    @EntityGraph(attributePaths = {"sourceStation", "targetStation"})
     Optional<Favorite> findByMemberIdAndId(Long memberId, Long favoriteId);
 }
