673262e86a0608a27bde1eaf2676c3b9d41346d6
slamdunk7575 <slamdunk7575@gmail.com>
2021-01-07 19:03:37 +0900

    (test) StationFixtures 추가

---
+++ src/test/java/nextstep/subway/station/StationFixtures.java
@@ -0,0 +1,10 @@
+package nextstep.subway.station;
+
+import nextstep.subway.station.domain.Station;
+
+public class StationFixtures {
+    public static final Station 천호역 = new Station("천호역");
+    public static final Station 잠실역 = new Station("잠실역");
+    public static final Station 문정역 = new Station("문정역");
+    public static final Station 산성역 = new Station("산성역");
+}
