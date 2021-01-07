2b41911e274481cdb12b1b312e1613d75a2caa7f
slamdunk7575 <slamdunk7575@gmail.com>
2021-01-07 20:23:48 +0900

    (test) StationFixtures 사용하도록 수정

--- src/test/java/nextstep/subway/line/domain/LineTest.java
+++ src/test/java/nextstep/subway/line/domain/LineTest.java
@@ -1,6 +1,6 @@
 package nextstep.subway.line.domain;
 
-import nextstep.subway.station.domain.Station;
+import nextstep.subway.station.StationFixtures;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -11,29 +11,20 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class LineTest {
     private Line 팔호선;
-    private Station 천호역;
-    private Station 잠실역;
-    private Station 문정역;
-    private Station 산성역;
 
     @BeforeEach
     void setUp() {
-        천호역 = new Station("천호역");
-        잠실역 = new Station("잠실역");
-        문정역 = new Station("문정역");
-        산성역 = new Station("산성역");
-
         팔호선 = Line.builder()
                 .name("팔호선")
                 .color("pink")
-                .upStation(천호역)
-                .downStation(산성역)
+                .upStation(StationFixtures.천호역)
+                .downStation(StationFixtures.산성역)
                 .distance(30)
                 .build();
 
         Section 천호역_잠실역 = Section.builder().line(팔호선)
-                .upStation(천호역)
-                .downStation(잠실역)
+                .upStation(StationFixtures.천호역)
+                .downStation(StationFixtures.잠실역)
                 .distance(new Distance(10))
                 .build();
 
@@ -45,8 +36,8 @@ class LineTest {
     void addSectionInLine() {
         // given
         Section newSection = Section.builder().line(팔호선)
-                .upStation(잠실역)
-                .downStation(문정역)
+                .upStation(StationFixtures.잠실역)
+                .downStation(StationFixtures.문정역)
                 .distance(new Distance(5))
                 .build();
 
@@ -54,7 +45,8 @@ class LineTest {
         팔호선.add(newSection);
 
         // then
-        assertThat(팔호선.getStations()).containsExactlyElementsOf(Arrays.asList(천호역, 잠실역, 문정역, 산성역));
+        assertThat(팔호선.getStations()).containsExactlyElementsOf(Arrays.asList(StationFixtures.천호역, StationFixtures.잠실역,
+                StationFixtures.문정역, StationFixtures.산성역));
         assertThat(라인전체길이()).isEqualTo(30);
     }
 
@@ -62,10 +54,10 @@ class LineTest {
     @Test
     void deleteStationInLine() {
         // when
-        팔호선.remove(잠실역);
+        팔호선.remove(StationFixtures.잠실역);
 
         // then
-        assertThat(팔호선.getStations()).containsExactlyElementsOf(Arrays.asList(천호역, 산성역));
+        assertThat(팔호선.getStations()).containsExactlyElementsOf(Arrays.asList(StationFixtures.천호역, StationFixtures.산성역));
         assertThat(라인전체길이()).isEqualTo(30);
     }
 
