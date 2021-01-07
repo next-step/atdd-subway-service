d2a339a0f05ab61d832139272aa647ce5b630561
Sang Uk Ahn <suahnn@gmail.com>
2021-01-08 00:28:05 +0900

    refactor: 시나리오 기반 테스트에서 일부 단위테스트 제외

--- src/test/java/nextstep/subway/line/acceptance/LineSectionAcceptanceTest.java
+++ src/test/java/nextstep/subway/line/acceptance/LineSectionAcceptanceTest.java
@@ -68,24 +68,36 @@ public class LineSectionAcceptanceTest extends AcceptanceTest {
 
         // then
         지하철_노선에_지하철역_순서_정렬됨(findResponseAfterRemove, Arrays.asList(강남역, 광교역));
+    }
 
+    @DisplayName("지하철 노선에 이미 등록되어있는 역을 등록한다.")
+    @Test
+    void addLineSectionWithSameStation() {
         // when
-        ExtractableResponse<Response> sameStationResponse = 지하철_노선에_지하철역_등록_요청(신분당선, 강남역, 광교역, 3);
+        ExtractableResponse<Response> response = 지하철_노선에_지하철역_등록_요청(신분당선, 강남역, 광교역, 3);
 
         // then
-        지하철_노선에_지하철역_등록_실패됨(sameStationResponse);
+        지하철_노선에_지하철역_등록_실패됨(response);
+    }
 
+    @DisplayName("지하철 노선에 등록되지 않은 역을 기준으로 등록한다.")
+    @Test
+    void addLineSectionWithNoStation() {
         // when
-        ExtractableResponse<Response> noStationResponse = 지하철_노선에_지하철역_등록_요청(신분당선, 정자역, 양재역, 3);
+        ExtractableResponse<Response> response = 지하철_노선에_지하철역_등록_요청(신분당선, 정자역, 양재역, 3);
 
         // then
-        지하철_노선에_지하철역_등록_실패됨(noStationResponse);
+        지하철_노선에_지하철역_등록_실패됨(response);
+    }
 
+    @DisplayName("지하철 노선에 등록된 지하철역이 두개일 때 한 역을 제외한다.")
+    @Test
+    void removeLineSection2() {
         // when
-        ExtractableResponse<Response> singleSectionResponse = 지하철_노선에_지하철역_제외_요청(신분당선, 강남역);
+        ExtractableResponse<Response> removeResponse = 지하철_노선에_지하철역_제외_요청(신분당선, 강남역);
 
         // then
-        지하철_노선에_지하철역_제외_실패됨(singleSectionResponse);
+        지하철_노선에_지하철역_제외_실패됨(removeResponse);
     }
 
     public static ExtractableResponse<Response> 지하철_노선에_지하철역_등록_요청(LineResponse line, StationResponse upStation, StationResponse downStation, int distance) {
