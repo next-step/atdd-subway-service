bfa3c66f8ea46648f020111b0be6a3e22df5376e
slamdunk7575 <slamdunk7575@gmail.com>
2021-01-09 01:30:23 +0900

    (refactor) 지난 피드백 반영 (유효성 검사 중복 제거)

--- src/main/java/nextstep/subway/line/domain/Distance.java
+++ src/main/java/nextstep/subway/line/domain/Distance.java
@@ -24,18 +24,14 @@ public class Distance {
         if (newDistance <= MIN_DISTANCE) {
             throw new IllegalArgumentException("거리는 0 또는 음수가 될 수 없습니다.");
         }
-
-        if (this.distance <= newDistance) {
-            throw new IllegalArgumentException("구간을 등록할 경우 기존 역 사이 길이보다 크거나 같으면 등록을 할 수 없습니다.");
-        }
     }
 
     public Distance addDistance(Distance newDistance) {
-        return new Distance(this.distance += newDistance.distance);
+        return new Distance(this.distance + newDistance.distance);
     }
 
     public Distance minusDistance(Distance newDistance) {
-        return new Distance(this.distance -= newDistance.distance);
+        return new Distance(this.distance - newDistance.distance);
     }
 
     public int get() {
