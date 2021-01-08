a836fad01b3f5daf6a80b823a706ccf198ca246f
tearight <tearightss@gmail.com>
2021-01-08 17:44:48 +0900

    fix(path): optimization repository access

--- src/main/java/nextstep/subway/path/application/PathService.java
+++ src/main/java/nextstep/subway/path/application/PathService.java
@@ -10,6 +10,7 @@ import nextstep.subway.station.domain.StationRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Arrays;
 import java.util.List;
 
 @Service
@@ -25,9 +26,16 @@ public class PathService {
 	}
 
 	public PathResponse calculatePath(PathCalculateRequest pathCalculateRequest) {
-		Station source = stationRepository.findById(pathCalculateRequest.getSourceStationId())
+		List<Station> findResult = stationRepository.findAllByIdIn(
+				Arrays.asList(pathCalculateRequest.getSourceStationId(), pathCalculateRequest.getTargetStationId()));
+
+		Station source = findResult.stream()
+				.filter(station -> station.getId().equals(pathCalculateRequest.getSourceStationId()))
+				.findFirst()
 				.orElseThrow(() -> new PathCalculateException("존재하지 않는 출발역입니다."));
-		Station target = stationRepository.findById(pathCalculateRequest.getTargetStationId())
+		Station target = findResult.stream()
+				.filter(station -> station.getId().equals(pathCalculateRequest.getTargetStationId()))
+				.findFirst()
 				.orElseThrow(() -> new PathCalculateException("존재하지 않는 도착역입니다."));
 
 		List<Line> allLines = lineRepository.findAll();
