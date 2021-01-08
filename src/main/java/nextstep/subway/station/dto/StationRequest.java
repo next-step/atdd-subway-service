cfb455f3a69a2d4e1c5ea4aea4ebc40fbea673f1
charlloss.dev <charlloss.dev@gmail.com>
2021-01-08 19:31:13 +0900

    refactor(line): line 인수테스트 리펙토링

--- src/main/java/nextstep/subway/station/dto/StationRequest.java
+++ src/main/java/nextstep/subway/station/dto/StationRequest.java
@@ -1,22 +1,15 @@
 package nextstep.subway.station.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import nextstep.subway.station.domain.Station;
 
+@AllArgsConstructor
+@Getter
 public class StationRequest {
-    private String name;
-
-    public StationRequest() {
-    }
-
-    public StationRequest(String name) {
-        this.name = name;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public Station toStation() {
-        return new Station(name);
-    }
+	private String name;
+	
+	public Station toStation() {
+		return new Station(name);
+	}
 }
