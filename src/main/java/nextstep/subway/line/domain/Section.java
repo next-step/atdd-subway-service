6501f52403e2dc965891d3496297bc93dfec988a
slamdunk7575 <slamdunk7575@gmail.com>
2021-01-09 01:59:16 +0900

    (refactor) 지난 피드백 반영 (객체에 메시지 보내도록 수정)

--- src/main/java/nextstep/subway/line/domain/Section.java
+++ src/main/java/nextstep/subway/line/domain/Section.java
@@ -37,6 +37,14 @@ public class Section {
         this.distance = distance;
     }
 
+    public boolean isMatchUpStation(Station station) {
+        return this.upStation == station;
+    }
+
+    public boolean isMatchDownStation(Station station) {
+        return this.downStation == station;
+    }
+
     public Long getId() {
         return id;
     }
