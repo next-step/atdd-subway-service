e13fca781eb16b0f625d373caa24ac150fa04708
himj131@gmail.com <himj131@gmail.com>
2021-01-10 23:24:45 +0900

    refactor: 커스텀 예외 추가, 의미있는 메소드 추출

---
+++ src/main/java/nextstep/subway/line/application/CannotAddSectionException.java
@@ -0,0 +1,6 @@
+package nextstep.subway.line.application;
+
+public class CannotAddSectionException extends RuntimeException {
+    public CannotAddSectionException(String s) {
+    }
+}
--- src/main/java/nextstep/subway/line/domain/Sections.java
+++ src/main/java/nextstep/subway/line/domain/Sections.java
@@ -1,12 +1,14 @@
 package nextstep.subway.line.domain;
 
+import nextstep.subway.line.application.CannotAddSectionException;
+import nextstep.subway.line.application.CannotRemoveException;
 import nextstep.subway.station.domain.Station;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Embeddable;
 import javax.persistence.OneToMany;
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
@@ -45,13 +47,13 @@ public class Sections {
             sections.add(new Section(line, upStation, downStation, distance));
 
         } else {
-            throw new RuntimeException();
+            throw new CannotAddSectionException("구간 등록 에러!! 상행역과 하행역을 잘 입력했는지 확인해 주세요.");
         }
     }
 
     public List<Station> getStations() {
         if (sections.isEmpty()) {
-            return Arrays.asList();
+            return Collections.emptyList();
         }
 
         List<Station> stations = new ArrayList<>();
@@ -87,21 +89,25 @@ public class Sections {
 
     public void removeLineStation(Line line, Station station) {
         if (sections.size() <= 1) {
-            throw new RuntimeException();
+            throw new CannotRemoveException("구간이 하나일 경우 역을 삭제할 수 없습니다.");
         }
 
         Optional<Section> upLineStation = getSameUpstationSection(station);
         Optional<Section> downLineStation = getSameDownStationSection(station);
 
         if (upLineStation.isPresent() && downLineStation.isPresent()) {
-            Station newUpStation = downLineStation.get().getUpStation();
-            Station newDownStation = upLineStation.get().getDownStation();
-            int newDistance = upLineStation.get().getDistance() + downLineStation.get().getDistance();
-            sections.add(new Section(line, newUpStation, newDownStation, newDistance));
+            connectPreStationAndNextStation(line, upLineStation, downLineStation);
         }
 
         upLineStation.ifPresent(it -> sections.remove(it));
         downLineStation.ifPresent(it -> sections.remove(it));
+    }
+
+    private void connectPreStationAndNextStation(Line line, Optional<Section> upLineStation, Optional<Section> downLineStation) {
+        Station newUpStation = downLineStation.get().getUpStation();
+        Station newDownStation = upLineStation.get().getDownStation();
+        int newDistance = upLineStation.get().getDistance() + downLineStation.get().getDistance();
+        sections.add(new Section(line, newUpStation, newDownStation, newDistance));
     }
 
     private Optional<Section> getSameUpstationSection(Station station) {
@@ -132,11 +138,11 @@ public class Sections {
 
     private void validate(List<Station> stations, boolean isUpStationExisted, boolean isDownStationExisted) {
         if (isUpStationExisted && isDownStationExisted) {
-            throw new RuntimeException("이미 등록된 구간 입니다.");
+            throw new CannotAddSectionException("이미 등록된 구간 입니다.");
         }
 
         if (!stations.isEmpty() && !isUpStationExisted && !isDownStationExisted) {
-            throw new RuntimeException("등록할 수 없는 구간 입니다.");
+            throw new CannotAddSectionException("등록할 수 없는 구간 입니다.");
         }
     }
 
