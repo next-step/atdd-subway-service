967a2180f268af661c3ef5735f4b45213d8b57b1
lazy-moon <magyeon@naver.com>
2021-01-11 02:53:09 +0900

    test: 로그인 테스트 작성

--- src/test/java/nextstep/subway/auth/acceptance/AuthAcceptanceTest.java
+++ src/test/java/nextstep/subway/auth/acceptance/AuthAcceptanceTest.java
@@ -1,14 +1,34 @@
 package nextstep.subway.auth.acceptance;
 
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
 import nextstep.subway.AcceptanceTest;
+import nextstep.subway.auth.dto.TokenRequest;
+import nextstep.subway.member.MemberAcceptanceTest;
+import nextstep.subway.member.dto.MemberResponse;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+import java.awt.image.RescaleOp;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class AuthAcceptanceTest extends AcceptanceTest {
 
     @DisplayName("Bearer Auth")
     @Test
     void myInfoWithBearerAuth() {
+        //given
+        String email = "example@naver.com";
+        String password = "example";
+        MemberAcceptanceTest.회원_생성을_요청(email, password, 18);
+        //when
+        ExtractableResponse<Response> loginResponse = 로그인_요청(email, password);
+        //then
+        로그인_성공(loginResponse);
     }
 
     @DisplayName("Bearer Auth 로그인 실패")
@@ -21,4 +41,18 @@ public class AuthAcceptanceTest extends AcceptanceTest {
     void myInfoWithWrongBearerAuth() {
     }
 
+    private void 로그인_성공(ExtractableResponse<Response> response) {
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+    }
+
+    private ExtractableResponse<Response> 로그인_요청(String email, String password) {
+        TokenRequest request = new TokenRequest(email, password);
+        return RestAssured.given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when().log().all()
+                .body(request)
+                .post("/login/token")
+                .then().log().all()
+                .extract();
+    }
 }
