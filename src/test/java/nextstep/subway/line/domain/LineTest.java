445ea95247e5b3443b749f7e5bb30e17d8acd6de
himj131@gmail.com <himj131@gmail.com>
2021-01-10 18:39:21 +0900

    refactor: Sections 일급 컬렉션 생성, 공통로직 메서드 추출

--- src/test/java/nextstep/subway/line/domain/LineTest.java
+++ src/test/java/nextstep/subway/line/domain/LineTest.java
@@ -4,7 +4,6 @@ import nextstep.subway.station.domain.Station;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -23,7 +22,7 @@ class LineTest {
         line.addSection(양재, 광교, 10);
 
         List<Station> stations = line.getStations();
-        assertThat(stations).containsExactlyElementsOf(Arrays.asList(양재, 광교));
+        assertThat(stations).containsExactly(양재, 광교);
     }
 
     @DisplayName("지하철 노선에 여러개의 역을 순서 상관 없이 등록한다.")
@@ -35,7 +34,7 @@ class LineTest {
 
         //then
         List<Station> stations = line.getStations();
-        assertThat(stations).containsExactlyElementsOf(Arrays.asList(정자, 강남, 양재));
+        assertThat(stations).containsExactly(정자, 강남, 양재);
     }
 
     @DisplayName("지하철 노선에 이미 등록되어있는 역을 등록한다.")
@@ -70,7 +69,7 @@ class LineTest {
 
         // then
         List<Station> stations = line.getStations();
-        assertThat(stations).containsExactlyElementsOf(Arrays.asList(강남, 정자));
+        assertThat(stations).containsExactly(강남, 정자);
     }
 
     @DisplayName("지하철 노선에 등록된 지하철역이 두개일 때 한 역을 제외한다.")
