ea07395d9f9d4ea188fff9113ac28dc177b61a54
monds <suahnn@gmail.com>
2021-01-10 14:31:13 +0900

    refactor: 최단 경로 검색 클래스 추가, 로직 정리

--- src/main/java/nextstep/subway/path/application/PathService.java
+++ src/main/java/nextstep/subway/path/application/PathService.java
@@ -7,9 +7,7 @@ import nextstep.subway.path.dto.PathResponse;
 import nextstep.subway.station.domain.Station;
 import nextstep.subway.station.domain.Stations;
 import org.jgrapht.GraphPath;
-import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
 import org.jgrapht.graph.DefaultWeightedEdge;
-import org.jgrapht.graph.WeightedMultigraph;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
@@ -18,32 +16,24 @@ public class PathService {
 	private final SectionRepository sectionRepository;
 
 	public PathResponse findShortestPath(Station sourceStation, Station targetStation) {
-		if (sourceStation == targetStation) {
-			throw new PathFindException("source station and target station are the same");
-		}
-
-		WeightedMultigraph<Station, DefaultWeightedEdge> graph = new WeightedMultigraph<>(DefaultWeightedEdge.class);
+		checkStationsAreSame(sourceStation, targetStation);
 
 		Sections sections = new Sections(sectionRepository.findAll());
-
 		Stations stations = sections.getStations();
 		checkStationIsInSections(sourceStation, stations);
 		checkStationIsInSections(targetStation, stations);
 
-		stations.forEach(graph::addVertex);
-		sections.forEach(section -> graph.setEdgeWeight(
-			graph.addEdge(section.getUpStation(), section.getDownStation()), section.getDistance()));
-
-
-		DijkstraShortestPath<Station, DefaultWeightedEdge> dijkstraShortestPath = new DijkstraShortestPath<>(graph);
-		GraphPath<Station, DefaultWeightedEdge> shortestPath
-			= dijkstraShortestPath.getPath(sourceStation, targetStation);
-
-		if (shortestPath == null) {
-			throw new PathFindException("source station is not connected to target station");
-		}
+		GraphPath<Station, DefaultWeightedEdge> shortestPath = ShortestPathFinder
+			.findShortestPath(sections, stations, sourceStation, targetStation)
+			.orElseThrow(() -> new PathFindException("source station is not connected to target station"));
 
 		return PathResponse.of(shortestPath.getVertexList(), (int) shortestPath.getWeight());
+	}
+
+	private void checkStationsAreSame(Station sourceStation, Station targetStation) {
+		if (sourceStation == targetStation) {
+			throw new PathFindException("source station and target station are the same");
+		}
 	}
 
 	private void checkStationIsInSections(Station sourceStation, Stations stations) {
---
+++ src/main/java/nextstep/subway/path/application/ShortestPathFinder.java
@@ -0,0 +1,27 @@
+package nextstep.subway.path.application;
+
+import nextstep.subway.line.domain.Sections;
+import nextstep.subway.station.domain.Station;
+import nextstep.subway.station.domain.Stations;
+import org.jgrapht.GraphPath;
+import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
+import org.jgrapht.graph.DefaultWeightedEdge;
+import org.jgrapht.graph.WeightedMultigraph;
+
+import java.util.Optional;
+
+public class ShortestPathFinder {
+
+	public static Optional<GraphPath<Station, DefaultWeightedEdge>> findShortestPath(
+		Sections sections, Stations stations, Station sourceStation, Station targetStation) {
+		WeightedMultigraph<Station, DefaultWeightedEdge> graph = new WeightedMultigraph<>(DefaultWeightedEdge.class);
+
+		stations.forEach(graph::addVertex);
+		sections.forEach(section -> graph.setEdgeWeight(
+			graph.addEdge(section.getUpStation(), section.getDownStation()), section.getDistance()));
+
+		DijkstraShortestPath<Station, DefaultWeightedEdge> dijkstraShortestPath = new DijkstraShortestPath<>(graph);
+
+		return Optional.ofNullable(dijkstraShortestPath.getPath(sourceStation, targetStation));
+	}
+}
