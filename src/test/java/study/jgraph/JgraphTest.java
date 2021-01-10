64a955e3f3647f7beb6d691df01b7b720c8e0cb3
charlloss.dev <charlloss.dev@gmail.com>
2021-01-11 02:15:22 +0900

    test(jgraph): j graph 라이브러리 테스트 및 학습

--- src/test/java/study/jgraph/JgraphTest.java
+++ src/test/java/study/jgraph/JgraphTest.java
@@ -1,5 +1,9 @@
 package study.jgraph;
 
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.List;
+
 import org.jgrapht.GraphPath;
 import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
 import org.jgrapht.alg.shortestpath.KShortestPaths;
@@ -7,49 +11,52 @@ import org.jgrapht.graph.DefaultWeightedEdge;
 import org.jgrapht.graph.WeightedMultigraph;
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
-
-import static org.assertj.core.api.Assertions.assertThat;
-
 public class JgraphTest {
-    @Test
-    public void getDijkstraShortestPath() {
-        String source = "v3";
-        String target = "v1";
-        WeightedMultigraph<String, DefaultWeightedEdge> graph = new WeightedMultigraph(DefaultWeightedEdge.class);
-        graph.addVertex("v1");
-        graph.addVertex("v2");
-        graph.addVertex("v3");
-        graph.setEdgeWeight(graph.addEdge("v1", "v2"), 2);
-        graph.setEdgeWeight(graph.addEdge("v2", "v3"), 2);
-        graph.setEdgeWeight(graph.addEdge("v1", "v3"), 100);
+	@Test
+	public void getDijkstraShortestPath() {
+		String source = "v3";
+		String target = "v1";
+		WeightedMultigraph<String, DefaultWeightedEdge> graph = new WeightedMultigraph(DefaultWeightedEdge.class);
+		graph.addVertex("v1");
+		graph.addVertex("v2");
+		graph.addVertex("v3");
+		graph.setEdgeWeight(graph.addEdge("v1", "v2"), 2);
+		graph.setEdgeWeight(graph.addEdge("v2", "v3"), 2);
+		graph.setEdgeWeight(graph.addEdge("v1", "v3"), 100);
 
-        DijkstraShortestPath dijkstraShortestPath = new DijkstraShortestPath(graph);
-        List<String> shortestPath = dijkstraShortestPath.getPath(source, target).getVertexList();
+		DijkstraShortestPath dijkstraShortestPath = new DijkstraShortestPath(graph);
+		List<String> shortestPath = dijkstraShortestPath.getPath(source, target).getVertexList();
 
-        assertThat(shortestPath.size()).isEqualTo(3);
-    }
+		System.out.println(shortestPath);
+		assertThat(shortestPath.size()).isEqualTo(3);
+	}
 
-    @Test
-    public void getKShortestPaths() {
-        String source = "v3";
-        String target = "v1";
+	@Test
+	public void getKShortestPaths() {
+		String source = "v3";
+		String target = "v1";
 
-        WeightedMultigraph<String, DefaultWeightedEdge> graph = new WeightedMultigraph(DefaultWeightedEdge.class);
-        graph.addVertex("v1");
-        graph.addVertex("v2");
-        graph.addVertex("v3");
-        graph.setEdgeWeight(graph.addEdge("v1", "v2"), 2);
-        graph.setEdgeWeight(graph.addEdge("v2", "v3"), 2);
-        graph.setEdgeWeight(graph.addEdge("v1", "v3"), 100);
+		WeightedMultigraph<String, DefaultWeightedEdge> graph = new WeightedMultigraph(DefaultWeightedEdge.class);
+		graph.addVertex("v1");
+		graph.addVertex("v2");
+		graph.addVertex("v3");
+		graph.setEdgeWeight(graph.addEdge("v1", "v2"), 2);
+		graph.setEdgeWeight(graph.addEdge("v2", "v3"), 2);
+		graph.setEdgeWeight(graph.addEdge("v1", "v3"), 100);
 
-        List<GraphPath> paths = new KShortestPaths(graph, 100).getPaths(source, target);
+		List<GraphPath> paths = new KShortestPaths(graph, 100).getPaths(source, target);
 
-        assertThat(paths).hasSize(2);
-        paths.stream()
-                .forEach(it -> {
-                    assertThat(it.getVertexList()).startsWith(source);
-                    assertThat(it.getVertexList()).endsWith(target);
-                });
-    }
+		System.out.println(paths);
+		System.out.println(paths.size());
+		System.out.println(paths.get(0).getVertexList());
+		System.out.println(paths.get(0).getEdgeList());
+		System.out.println(paths.get(0).getLength());
+		System.out.println(paths.get(0).getWeight());
+		assertThat(paths).hasSize(2);
+		paths.stream()
+			.forEach(it -> {
+				assertThat(it.getVertexList()).startsWith(source);
+				assertThat(it.getVertexList()).endsWith(target);
+			});
+	}
 }
