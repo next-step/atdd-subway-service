f12d4d7fc2d82b950c5b0a282199c0c6a7377d54
charlloss.dev <charlloss.dev@gmail.com>
2021-01-11 16:43:27 +0900

    test(jgraph): 경로 찾기 라이브러리 학습 테스트

--- src/test/java/study/jgraph/JgraphTest.java
+++ src/test/java/study/jgraph/JgraphTest.java
@@ -12,7 +12,42 @@ import org.jgrapht.graph.WeightedMultigraph;
 import org.junit.jupiter.api.Test;
 
 public class JgraphTest {
+
 	@Test
+	public void exceptionPath() {
+		String source = "v6";
+		String target = "v1";
+		WeightedMultigraph<String, DefaultWeightedEdge> graph = new WeightedMultigraph(DefaultWeightedEdge.class);
+		graph.addVertex("v1");
+		graph.addVertex("v2");
+		graph.addVertex("v3");
+		graph.addVertex("v4");
+		graph.addVertex("v5");
+		graph.addVertex("v6");
+
+		graph.setEdgeWeight(graph.addEdge("v1", "v2"), 2);
+		graph.setEdgeWeight(graph.addEdge("v2", "v3"), 2);
+		graph.setEdgeWeight(graph.addEdge("v3", "v4"), 2);
+		graph.setEdgeWeight(graph.addEdge("v4", "v5"), 2);
+		graph.setEdgeWeight(graph.addEdge("v1", "v3"), 100);
+
+		List<GraphPath> paths = new KShortestPaths(graph, 100).getPaths(source, target);
+
+		System.out.println(paths);
+		System.out.println(paths.size());
+		System.out.println(paths.get(0).getVertexList());
+		System.out.println(paths.get(0).getEdgeList());
+		System.out.println(paths.get(0).getLength());
+		System.out.println(paths.get(0).getWeight());
+		//DijkstraShortestPath dijkstraShortestPath = new DijkstraShortestPath(graph);
+		//List<String> shortestPath = dijkstraShortestPath.getPath(source, target).getVertexList();
+
+		//System.out.println(shortestPath);
+		//assertThat(shortestPath.size()).isEqualTo(3);
+	}
+
+	@Test
+
 	public void getDijkstraShortestPath() {
 		String source = "v3";
 		String target = "v1";
@@ -45,13 +80,6 @@ public class JgraphTest {
 		graph.setEdgeWeight(graph.addEdge("v1", "v3"), 100);
 
 		List<GraphPath> paths = new KShortestPaths(graph, 100).getPaths(source, target);
-
-		System.out.println(paths);
-		System.out.println(paths.size());
-		System.out.println(paths.get(0).getVertexList());
-		System.out.println(paths.get(0).getEdgeList());
-		System.out.println(paths.get(0).getLength());
-		System.out.println(paths.get(0).getWeight());
 		assertThat(paths).hasSize(2);
 		paths.stream()
 			.forEach(it -> {
