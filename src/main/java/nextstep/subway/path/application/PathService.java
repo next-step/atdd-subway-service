02571ca082cabd5cd1f8bcca3c0fd88a89cd177a
charlloss.dev <charlloss.dev@gmail.com>
2021-01-11 17:47:04 +0900

    feat(path): 출발역과 도착역이 연결되지 않은 경우 기능구현

--- src/main/java/nextstep/subway/path/application/PathService.java
+++ src/main/java/nextstep/subway/path/application/PathService.java
@@ -3,26 +3,28 @@ package nextstep.subway.path.application;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import nextstep.subway.line.application.LineService;
 import nextstep.subway.path.domain.PathFinder;
 import nextstep.subway.path.dto.PathRequest;
 import nextstep.subway.path.dto.PathResponse;
-import nextstep.subway.station.application.StationService;
 import nextstep.subway.station.domain.Station;
 
 @Service
 @Transactional(readOnly = true)
 public class PathService {
-	private final StationService stationService;
+	private final LineService lineService;
 
-	public PathService(final StationService stationService) {
-		this.stationService = stationService;
+	public PathService(final LineService lineService) {
+		this.lineService = lineService;
 	}
 
 	public PathResponse findPath(PathRequest request) {
-		Station sourceStation = stationService.findStationById(request.getSource());
-		Station targetStation = stationService.findStationById(request.getTarget());
+		Station source = lineService.findStationById(request.getSource());
+		Station target = lineService.findStationById(request.getTarget());
 
-		PathFinder.start(sourceStation, targetStation);
+		PathFinder finder = new PathFinder(lineService.findLineAll());
+
+		finder.selectShortPath(source, target);
 		return null;
 	}
 }
