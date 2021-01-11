20e8c1c873c38410d3893fea2747a81ba407a304
charlloss.dev <charlloss.dev@gmail.com>
2021-01-11 16:42:41 +0900

    test(path): 경로 찾기 예외케이스 테스트

--- src/test/java/nextstep/subway/path/application/PathServiceTest.java
+++ src/test/java/nextstep/subway/path/application/PathServiceTest.java
@@ -10,10 +10,10 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import nextstep.subway.common.exception.NothingException;
 import nextstep.subway.line.application.LineService;
 import nextstep.subway.line.domain.Line;
 import nextstep.subway.path.dto.PathRequest;
-import nextstep.subway.station.application.StationService;
 import nextstep.subway.station.domain.Station;
 
 @DisplayName("First Outside In Test")
@@ -22,33 +22,64 @@ class PathServiceTest {
 
 	@Mock
 	private LineService lineService;
-	@Mock
-	private StationService stationService;
 
 	private PathService pathService;
 
-	private Line line;
+	private Line line1;
+	private Line line2;
 	private Station 시청역;
 	private Station 서초역;
+	private Station 인천역;
+	private Station 주안역;
 
 	@BeforeEach
 	void setUp() {
-		pathService = new PathService(stationService);
+		pathService = new PathService(lineService);
 
 		시청역 = new Station(1L, "시청역");
 		서초역 = new Station(2L, "서초역");
+		인천역 = new Station(3L, "인천역");
+		주안역 = new Station(4L, "주안역");
 
-		line = new Line(1L, "2호선", "green", 시청역, 서초역, 100);
+		line1 = new Line(1L, "1호선", "blue", 인천역, 주안역, 50);
+		line2 = new Line(2L, "2호선", "green", 시청역, 서초역, 100);
 	}
 
 	@DisplayName("경로찾기: [에러]동일한 역 경로검색")
 	@Test
-	void findPathSameStationTest() {
+	void foundPathSameStationTest() {
 		// given
-		when(stationService.findStationById(1L)).thenReturn(시청역);
+		when(lineService.findStationById(1L)).thenReturn(시청역);
 		PathRequest request = new PathRequest(시청역.getId(), 시청역.getId());
 
 		// when // then
-		assertThatExceptionOfType(RuntimeException.class).isThrownBy(() -> pathService.findPath(request));
+		assertThatExceptionOfType(RuntimeException.class)
+			.isThrownBy(() -> pathService.findPath(request));
+	}
+
+	@DisplayName("경로찾기: [에러]존재하지 않는 역 경로검색")
+	@Test
+	void notFoundStationTest() {
+		// given
+		when(lineService.findStationById(1L)).thenReturn(시청역);
+		when(lineService.findStationById(3L)).thenThrow(NothingException.class);
+		PathRequest request = new PathRequest(시청역.getId(), 3L);
+
+		// when // then
+		assertThatExceptionOfType(NothingException.class)
+			.isThrownBy(() -> pathService.findPath(request));
+	}
+
+	@DisplayName("경로찾기: [에러]같은 경로에 없는 역 경로검색")
+	@Test
+	void notFoundPathTest() {
+		// given
+		when(lineService.findStationById(1L)).thenReturn(시청역);
+		when(lineService.findStationById(1L)).thenReturn(인천역);
+		PathRequest request = new PathRequest(시청역.getId(), 인천역.getId());
+
+		// when // then
+		assertThatExceptionOfType(NothingException.class)
+			.isThrownBy(() -> pathService.findPath(request));
 	}
 }
