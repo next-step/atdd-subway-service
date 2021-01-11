147c0a8de80f7a9c23c8a1d893abe36111b801a3
Sang Uk Ahn <suahnn@gmail.com>
2021-01-11 20:35:09 +0900

    refactor: Graph 관련 객체 static 선언으로 변경

--- src/main/java/nextstep/subway/path/application/ShortestPathFinder.java
+++ src/main/java/nextstep/subway/path/application/ShortestPathFinder.java
@@ -10,16 +10,21 @@ import org.jgrapht.graph.DefaultWeightedEdge;
 import org.jgrapht.graph.WeightedMultigraph;
 
 public class ShortestPathFinder {
+    private static final WeightedMultigraph<Station, DefaultWeightedEdge> graph;
+    private static final DijkstraShortestPath<Station, DefaultWeightedEdge> dijkstraShortestPath;
+
+    static {
+        graph = new WeightedMultigraph<>(DefaultWeightedEdge.class);
+        dijkstraShortestPath = new DijkstraShortestPath<>(graph);
+    }
 
     public static Path findShortestPath(
         Sections sections, Stations stations, Station sourceStation, Station targetStation) {
-        WeightedMultigraph<Station, DefaultWeightedEdge> graph = new WeightedMultigraph<>(DefaultWeightedEdge.class);
 
         stations.forEach(graph::addVertex);
         sections.forEach(section -> graph.setEdgeWeight(
             graph.addEdge(section.getUpStation(), section.getDownStation()), section.getDistance()));
 
-        DijkstraShortestPath<Station, DefaultWeightedEdge> dijkstraShortestPath = new DijkstraShortestPath<>(graph);
         GraphPath<Station, DefaultWeightedEdge> shortestPath = dijkstraShortestPath.getPath(sourceStation, targetStation);
         checkPathIsNull(shortestPath);
 
