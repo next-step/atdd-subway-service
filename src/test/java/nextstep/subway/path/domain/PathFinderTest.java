233e1eec4ace085ebb5fecd7ceb81fc7130945dc
charlloss.dev <charlloss.dev@gmail.com>
2021-01-12 21:04:18 +0900

    refactor(path): 도메인 테스트 db 종속 제거 피드백 적용

--- src/test/java/nextstep/subway/path/domain/PathFinderTest.java
+++ src/test/java/nextstep/subway/path/domain/PathFinderTest.java
@@ -3,6 +3,7 @@ package nextstep.subway.path.domain;
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -13,58 +14,36 @@ import org.jgrapht.graph.WeightedMultigraph;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import nextstep.subway.common.exception.NothingException;
-import nextstep.subway.line.application.LineService;
 import nextstep.subway.line.domain.Line;
-import nextstep.subway.line.domain.LineRepository;
-import nextstep.subway.line.dto.LineRequest;
-import nextstep.subway.line.dto.LineResponse;
-import nextstep.subway.station.application.StationService;
 import nextstep.subway.station.domain.Station;
-import nextstep.subway.station.domain.StationRepository;
 
 @DisplayName("경로 찾기, 라인 그래프 생성 테스트")
-@DataJpaTest
 class PathFinderTest {
 
-	@Autowired
-	private LineRepository lineRepository;
-
-	@Autowired
-	private StationRepository stationRepository;
-	private LineService lineService;
-
-	private LineResponse line1;
-	private LineResponse line2;
+	private Line line1;
+	private Line line2;
 
 	private Station 인천역;
 	private Station 소요산역;
 	private Station 시청역;
 	private Station 강남역;
+	private Station 주안역;
+	private Station 동암역;
+	private Station 부평역;
+	private Station 구로역;
 
 	@BeforeEach
 	void setUp() {
-		lineService = new LineService(lineRepository, new StationService(stationRepository));
-
-		인천역 = 전철역_생성("인천역");
-		소요산역 = 전철역_생성("소요산역");
-		시청역 = 전철역_생성("시청역");
-		강남역 = 전철역_생성("강남역");
-
-		line1 = 라인_생성("1호선", "blue", 인천역.getId(), 소요산역.getId(), 500);
-		line2 = 라인_생성("2호선", "green", 시청역.getId(), 강남역.getId(), 300);
-
-		구간_1호선_생성();
+		지하철_초기_데이터_생성();
 	}
 
 	@DisplayName("PathFinder 경로검색 테스트")
 	@Test
 	void createPathFinderTest() {
 		// given
-		List<Line> lines = lineService.findLineAll();
+		List<Line> lines = Arrays.asList(line1, line2);
 		PathFinder finder = new PathFinder(lines);
 
 		// when
@@ -82,7 +61,7 @@ class PathFinderTest {
 	@Test
 	void exceptionPathFinderTest() {
 		// given
-		List<Line> lines = lineService.findLineAll();
+		List<Line> lines = Arrays.asList(line1, line2);
 
 		// when
 		PathFinder finder = new PathFinder(lines);
@@ -98,7 +77,7 @@ class PathFinderTest {
 	@Test
 	void initPathFinderTest() {
 		// given
-		List<Line> lines = lineService.findLineAll();
+		List<Line> lines = Arrays.asList(line1, line2);
 
 		// when
 		PathFinder finder = new PathFinder(lines);
@@ -113,7 +92,7 @@ class PathFinderTest {
 	@Test
 	void getAllStationsTest() {
 		// given
-		List<Line> lines = lineService.findLineAll();
+		List<Line> lines = Arrays.asList(line1, line2);
 
 		// when
 		List<Station> stations = lines.stream()
@@ -131,7 +110,6 @@ class PathFinderTest {
 	@Test
 	void addGraphLineTest() {
 		// given
-		Line line1 = lineService.findLineAll().get(0);
 		WeightedMultigraph<Station, DefaultWeightedEdge> graph = new WeightedMultigraph<>(DefaultWeightedEdge.class);
 
 		// when
@@ -159,14 +137,18 @@ class PathFinderTest {
 		assertThat(shortestPath).isNotNull();
 	}
 
-	private void 구간_1호선_생성() {
-		Line line1 = lineService.findLineAll().get(0);
+	private void 지하철_초기_데이터_생성() {
+		소요산역 = 전철역_생성(1L, "소요산역");
+		인천역 = 전철역_생성(2L, "인천역");
+		시청역 = 전철역_생성(3L, "시청역");
+		강남역 = 전철역_생성(4L, "강남역");
+		주안역 = 전철역_생성(5L, "주안역");
+		동암역 = 전철역_생성(6L, "동암역");
+		부평역 = 전철역_생성(7L, "부평역");
+		구로역 = 전철역_생성(8L, "구로역");
 
-		Station 주안역 = 전철역_생성("주안역");
-		Station 동암역 = 전철역_생성("동암역");
-		Station 부평역 = 전철역_생성("부평역");
-		Station 구로역 = 전철역_생성("구로역");
-
+		line1 = 라인_생성("1호선", "blue", 인천역, 소요산역, 500);
+		line2 = 라인_생성("2호선", "green", 시청역, 강남역, 300);
 		구간추가(line1, 인천역, 주안역, 10);
 		구간추가(line1, 주안역, 동암역, 20);
 		구간추가(line1, 동암역, 부평역, 30);
@@ -177,11 +159,11 @@ class PathFinderTest {
 		line.addSection(upStation, downStation, distance);
 	}
 
-	private Station 전철역_생성(String stationName) {
-		return stationRepository.save(new Station(stationName));
+	private Station 전철역_생성(Long id, String stationName) {
+		return new Station(id, stationName);
 	}
 
-	private LineResponse 라인_생성(String name, String color, Long upId, Long downId, int distance) {
-		return lineService.saveLine(new LineRequest(name, color, upId, downId, distance));
+	private Line 라인_생성(String name, String color, Station upStation, Station downStation, int distance) {
+		return new Line(name, color, upStation, downStation, distance);
 	}
 }
