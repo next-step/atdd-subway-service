a78ad0fa8bbd65f4279b761e42dcd271d21e5e71
byungkyu-ju <noel1227@gmail.com>
2021-01-12 16:45:29 +0900

    refactor: 불필요하게 초기화하던 지하철역을 초기 한번만 생성하도록 변경

--- src/test/java/nextstep/subway/line/domain/LineTest.java
+++ src/test/java/nextstep/subway/line/domain/LineTest.java
@@ -17,21 +17,13 @@ import nextstep.subway.station.domain.Station;
  **/
 @DisplayName("지하철노선 관련 기능")
 class LineTest {
-	Station 강남역;
-	Station 광교역;
-	Station 판교역;
-
-	@BeforeEach
-	void setUp() {
-		// given
-		강남역 = new Station("강남역");
-		광교역 = new Station("광교역");
-		판교역 = new Station("판교역");
-	}
+	Station 강남역 = new Station("강남역");
+	Station 광교역 = new Station("광교역");
+	Station 판교역 = new Station("판교역");
 
 	@DisplayName("노선을_생성한다.")
 	@Test
-	void createLine(){
+	void createLine() {
 		// when
 		Line actual = new Line("신분당선", "red");
 
@@ -42,7 +34,7 @@ class LineTest {
 
 	@DisplayName("노선_생성시_상행역과_하행역_구간_생성")
 	@Test
-	void createLineWithUpStationAndDownStation(){
+	void createLineWithUpStationAndDownStation() {
 		// when
 		Line actual = new Line("신분당선", "red", 강남역, 광교역, 8);
 
@@ -54,7 +46,7 @@ class LineTest {
 
 	@DisplayName("생성한_노선정보_변경")
 	@Test
-	void updateLineInfo(){
+	void updateLineInfo() {
 		// given
 		Line actual = new Line("신분당선", "red", 강남역, 광교역, 8);
 
@@ -68,7 +60,7 @@ class LineTest {
 
 	@DisplayName("기존_노선에_신규노선_추가(상행역 동일, 중간역 추가)")
 	@Test
-	void addStationMiddle(){
+	void addStationMiddle() {
 		// given
 		Line actual = new Line("신분당선", "red", 강남역, 광교역, 8);
 
@@ -82,7 +74,7 @@ class LineTest {
 
 	@DisplayName("기존_노선에_신규노선_추가(하행역 동일, 중간역 추가)")
 	@Test
-	void addStationMiddle2(){
+	void addStationMiddle2() {
 		// given
 		Line actual = new Line("신분당선", "red", 강남역, 광교역, 8);
 
@@ -96,7 +88,7 @@ class LineTest {
 
 	@DisplayName("기존_노선에_신규노선_추가(신규상행역 추가)")
 	@Test
-	void addStationNewStart(){
+	void addStationNewStart() {
 		// given
 		Station 신논현역 = new Station("신논현역");
 		Line actual = new Line("신분당선", "red", 강남역, 광교역, 8);
@@ -111,7 +103,7 @@ class LineTest {
 
 	@DisplayName("기존_노선에_신규노선_추가(신규하행역 추가)")
 	@Test
-	void addStationNewEnd(){
+	void addStationNewEnd() {
 		// given
 		Station 호매실역 = new Station("호매실역");
 		Line actual = new Line("신분당선", "red", 강남역, 광교역, 8);
@@ -126,7 +118,7 @@ class LineTest {
 
 	@DisplayName("기존_노선과_동일한_구간_오류")
 	@Test
-	void addStationDuplicateException(){
+	void addStationDuplicateException() {
 		// given
 		Line actual = new Line("신분당선", "red", 강남역, 광교역, 8);
 
@@ -140,7 +132,7 @@ class LineTest {
 
 	@DisplayName("기존_노선과_일치하는_지하철역_없음")
 	@Test
-	void addStationMustContainExistStationException(){
+	void addStationMustContainExistStationException() {
 		// given
 		Line actual = new Line("신분당선", "red", 강남역, 광교역, 8);
 
@@ -157,7 +149,7 @@ class LineTest {
 
 	@DisplayName("노선내_상행_종점_삭제")
 	@Test
-	void remoteStationStart(){
+	void remoteStationStart() {
 		// given
 		Line actual = new Line("신분당선", "red", 강남역, 광교역, 8);
 		actual.addSection(강남역, 판교역, 3);
@@ -172,7 +164,7 @@ class LineTest {
 
 	@DisplayName("노선내_하행_종점_삭제")
 	@Test
-	void remoteStationEnd(){
+	void remoteStationEnd() {
 		// given
 		Line actual = new Line("신분당선", "red", 강남역, 광교역, 8);
 		actual.addSection(강남역, 판교역, 3);
@@ -187,7 +179,7 @@ class LineTest {
 
 	@DisplayName("노선내_중간역_삭제")
 	@Test
-	void remoteStationMiddle(){
+	void remoteStationMiddle() {
 		// given
 		Line actual = new Line("신분당선", "red", 강남역, 광교역, 8);
 		actual.addSection(강남역, 판교역, 3);
