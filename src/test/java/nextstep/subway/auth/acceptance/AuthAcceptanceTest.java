64abc4cf0dbe9706e01c224442425588d334a2eb
Pilhwan Kim <pilhwankim03@gmail.com>
2021-01-12 10:09:37 +0900

    test: 유효하지 않은 토큰 인수 테스트

--- src/test/java/nextstep/subway/auth/acceptance/AuthAcceptanceTest.java
+++ src/test/java/nextstep/subway/auth/acceptance/AuthAcceptanceTest.java
@@ -44,11 +44,29 @@ public class AuthAcceptanceTest extends AcceptanceTest {
     @DisplayName("Bearer Auth 로그인 실패")
     @Test
     void myInfoWithBadBearerAuth() {
+        // when
+        ExtractableResponse<Response> response = bearer_토큰_인증_요청(MemberAcceptanceTest.EMAIL, "1234");
+
+        // then
+        토큰_인증_실패_검증(response);
     }
 
     @DisplayName("Bearer Auth 유효하지 않은 토큰")
     @Test
     void myInfoWithWrongBearerAuth() {
+
+        // given
+        bearer_토큰_인증_요청(
+                MemberAcceptanceTest.EMAIL, MemberAcceptanceTest.PASSWORD
+        );
+
+        // when
+        String accessToken = "wrong-token";
+        Map<String, String> params = new HashMap<>();
+        ExtractableResponse<Response> response = 내_정보_조회_요청(accessToken, params);
+
+        // then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.UNAUTHORIZED.value());
     }
 
     public static ExtractableResponse<Response> bearer_토큰_인증_요청(String email, String password) {
@@ -57,13 +75,22 @@ public class AuthAcceptanceTest extends AcceptanceTest {
         params.put("password", password);
         ExtractableResponse<Response> response = RestAssured
                 .given().log().all()
-                .auth().preemptive().basic(email, password)
                 .accept(MediaType.APPLICATION_JSON_VALUE)
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
                 .body(params)
                 .when().post("/login/token")
                 .then().log().all().extract();
         return response;
+    }
+
+    public ExtractableResponse<Response> 내_정보_조회_요청(String accessToken, Map<String, String> params) {
+        return RestAssured
+                .given().log().all()
+                .auth().oauth2(accessToken)
+                .body(params)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when().get("/members/me")
+                .then().log().all().extract();
     }
 
     private void 토큰_인증_성공_검증(ExtractableResponse<Response> response) {
@@ -73,4 +100,8 @@ public class AuthAcceptanceTest extends AcceptanceTest {
         assertThat(accessToken).isNotBlank();
     }
 
+    private void 토큰_인증_실패_검증(ExtractableResponse<Response> response) {
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.UNAUTHORIZED.value());
+        assertThat(response.contentType()).isEqualTo(MediaType.APPLICATION_JSON_VALUE);
+    }
 }
