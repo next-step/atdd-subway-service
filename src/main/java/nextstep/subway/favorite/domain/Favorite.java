73e5c201be6005c75fe7e523b866e536aa27397b
monds <suahnn@gmail.com>
2021-01-12 15:28:55 +0900

    feat: 출발역과 도착역이 동일한 즐겨찾기 생성 시 예외처리

--- src/main/java/nextstep/subway/favorite/domain/Favorite.java
+++ src/main/java/nextstep/subway/favorite/domain/Favorite.java
@@ -1,6 +1,5 @@
 package nextstep.subway.favorite.domain;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import nextstep.subway.BaseEntity;
@@ -13,6 +12,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+
+import static com.google.common.base.Preconditions.checkArgument;
 
 @Getter
 @NoArgsConstructor
@@ -36,6 +37,8 @@ public class Favorite extends BaseEntity {
     private Station targetStation;
 
     public Favorite(Member member, Station sourceStation, Station targetStation) {
+        checkArgument(!sourceStation.equals(targetStation), "출발역과 도착역은 같을 수 없습니다.");
+
         this.member = member;
         this.sourceStation = sourceStation;
         this.targetStation = targetStation;
