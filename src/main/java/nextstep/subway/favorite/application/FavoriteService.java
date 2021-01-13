c0fb001811d8c96e4ae0f0146a7837e95d9ef941
charlloss.dev <charlloss.dev@gmail.com>
2021-01-13 20:27:38 +0900

    feat(favorite): 즐겨찾기 목록 조회 및 삭제 기능 개발

--- src/main/java/nextstep/subway/favorite/application/FavoriteService.java
+++ src/main/java/nextstep/subway/favorite/application/FavoriteService.java
@@ -1,11 +1,13 @@
 package nextstep.subway.favorite.application;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import lombok.AllArgsConstructor;
+import nextstep.subway.common.exception.NothingException;
 import nextstep.subway.favorite.domain.Favorite;
 import nextstep.subway.favorite.domain.FavoriteRepository;
 import nextstep.subway.favorite.dto.FavoriteRequest;
@@ -24,17 +26,32 @@ public class FavoriteService {
 	private final StationService stationService;
 	private final FavoriteRepository favoriteRepository;
 
+	@Transactional
 	public FavoriteResponse createFavorite(Long loginMemberId, FavoriteRequest request) {
 		Member member = memberService.findMemberById(loginMemberId);
 
 		Station source = stationService.findStationById(request.getSource());
 		Station target = stationService.findStationById(request.getTarget());
 
-		Favorite favorite = favoriteRepository.save(new Favorite(member, source, target));
-		return FavoriteResponse.of(favorite);
+		Favorite persistFavorite = favoriteRepository.save(new Favorite(member, source, target));
+		return FavoriteResponse.of(persistFavorite);
 	}
 
-	public List<FavoriteResponse> findFavorites(Long id) {
-		return null;
+	public List<FavoriteResponse> findFavorites(Long loginMemberId) {
+		Member member = memberService.findMemberById(loginMemberId);
+		List<Favorite> persistFavorites = favoriteRepository.findAllByMember(member);
+		return persistFavorites.stream()
+			.map(FavoriteResponse::of)
+			.collect(Collectors.toList());
+	}
+
+	@Transactional
+	public void deleteFavoriteById(long id) {
+		Favorite favorite = findFavoriteById(id);
+		favoriteRepository.delete(favorite);
+	}
+
+	private Favorite findFavoriteById(Long id) {
+		return favoriteRepository.findById(id).orElseThrow(NothingException::new);
 	}
 }
--- src/main/java/nextstep/subway/favorite/ui/FavoriteController.java
+++ src/main/java/nextstep/subway/favorite/ui/FavoriteController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -39,8 +40,9 @@ public class FavoriteController {
 		return ResponseEntity.ok(favoriteService.findFavorites(loginMember.getId()));
 	}
 
-	@DeleteMapping
-	public void deleteFavorite() {
-
+	@DeleteMapping("/{id}")
+	public ResponseEntity<?> deleteFavorite(@AuthenticationPrincipal LoginMember loginMember, @PathVariable long id) {
+		favoriteService.deleteFavoriteById(id);
+		return ResponseEntity.noContent().build();
 	}
 }
