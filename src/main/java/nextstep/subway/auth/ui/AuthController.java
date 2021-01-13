e690b2a6e1d18ff7b2c8c30fe07ad522aae1ab07
charlloss.dev <charlloss.dev@gmail.com>
2021-01-13 17:42:44 +0900

    fix(auth): 내부인스턴스 final 적용

--- src/main/java/nextstep/subway/auth/ui/AuthController.java
+++ src/main/java/nextstep/subway/auth/ui/AuthController.java
@@ -13,7 +13,7 @@ import nextstep.subway.auth.dto.TokenResponse;
 
 @RestController
 public class AuthController {
-	private AuthService authService;
+	private final AuthService authService;
 
 	public AuthController(AuthService authService) {
 		this.authService = authService;
