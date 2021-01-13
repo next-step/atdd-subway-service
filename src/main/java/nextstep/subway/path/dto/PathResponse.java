499835110cee3a19fd14abb335eef38fc7b05d17
mkkim90 <mkzzang0928@gmail.com>
2021-01-13 23:03:56 +0900

    test : Outside in 컨트롤러 레이어 구현 테스트

---
+++ src/main/java/nextstep/subway/path/dto/PathResponse.java
@@ -0,0 +1,55 @@
+package nextstep.subway.line.dto;
+
+import nextstep.subway.line.domain.Line;
+import nextstep.subway.station.dto.StationResponse;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class LineResponse {
+    private Long id;
+    private String name;
+    private String color;
+    private List<StationResponse> stations;
+    private LocalDateTime createdDate;
+    private LocalDateTime modifiedDate;
+
+    public LineResponse() {
+    }
+
+    public LineResponse(Long id, String name, String color, List<StationResponse> stations, LocalDateTime createdDate, LocalDateTime modifiedDate) {
+        this.id = id;
+        this.name = name;
+        this.color = color;
+        this.stations = stations;
+        this.createdDate = createdDate;
+        this.modifiedDate = modifiedDate;
+    }
+    public static LineResponse of(Line line) {
+        return new LineResponse(line.getId(), line.getName(), line.getColor(), line.getStationResponses(), line.getCreatedDate(), line.getModifiedDate());
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public List<StationResponse> getStations() {
+        return stations;
+    }
+
+    public LocalDateTime getCreatedDate() {
+        return createdDate;
+    }
+
+    public LocalDateTime getModifiedDate() {
+        return modifiedDate;
+    }
+}
