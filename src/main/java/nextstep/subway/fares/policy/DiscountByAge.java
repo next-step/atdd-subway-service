f1d35fa22380e31229013b7b55079ec4ff999551
Sang Uk Ahn <suahnn@gmail.com>
2021-01-14 22:28:27 +0900

    fix: 요금 정책 운임 계산 버그 수정

--- src/main/java/nextstep/subway/fares/policy/DiscountByAge.java
+++ src/main/java/nextstep/subway/fares/policy/DiscountByAge.java
@@ -3,6 +3,7 @@ package nextstep.subway.fares.policy;
 import lombok.Getter;
 
 import java.util.Arrays;
+import java.util.Optional;
 import java.util.function.IntPredicate;
 
 @Getter
@@ -19,11 +20,9 @@ public enum DiscountByAge {
         this.predicateAge = predicateAge;
     }
 
-    public static double getDiscountRateByAge(int age) {
+    public static Optional<DiscountByAge> getDiscountRateByAge(int age) {
         return Arrays.stream(values())
                 .filter(discountByAge -> discountByAge.predicateAge.test(age))
-                .findFirst()
-                .map(DiscountByAge::getRate)
-                .orElse(0.0);
+                .findFirst();
     }
 }
--- src/main/java/nextstep/subway/fares/policy/DiscountByAgeFarePolicy.java
+++ src/main/java/nextstep/subway/fares/policy/DiscountByAgeFarePolicy.java
@@ -4,15 +4,23 @@ import nextstep.subway.auth.domain.LoginMember;
 import nextstep.subway.fares.domain.Fare;
 import nextstep.subway.path.domain.Path;
 
+import java.util.function.Consumer;
+
 public class DiscountByAgeFarePolicy implements FarePolicy {
     private static final int DEDUCT_FARE = 350;
 
     @Override
     public void calculateFare(Fare fare, Path path, LoginMember loginMember) {
         if (!loginMember.isAnonymous()) {
-            double discountRate = DiscountByAge.getDiscountRateByAge(loginMember.getAge());
-            fare.minus(DEDUCT_FARE);
-            fare.discount(discountRate);
+            DiscountByAge.getDiscountRateByAge(loginMember.getAge())
+                    .ifPresent(discountByAgeConsumer(fare));
         }
+    }
+
+    private Consumer<DiscountByAge> discountByAgeConsumer(Fare fare) {
+        return discountByAge -> {
+            fare.minus(DEDUCT_FARE);
+            fare.discount(discountByAge.getRate());
+        };
     }
 }
