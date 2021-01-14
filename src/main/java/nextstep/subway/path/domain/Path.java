c0cf8107fb9c9385960ae10f1467fa3c14f88d60
mkkim90 <mkzzang0928@gmail.com>
2021-01-14 13:47:32 +0900

    feat : 경로찾기 예외 구현

--- src/main/java/nextstep/subway/path/domain/Path.java
+++ src/main/java/nextstep/subway/path/domain/Path.java
@@ -14,12 +14,26 @@ public class Path {
     private GraphPath<Station, DefaultWeightedEdge> shortestPath;
 
     public Path(List<Line> lines, Station source, Station target) {
+        validateIsSame(source, target);
         WeightedMultigraph<Station, DefaultWeightedEdge> graph = new WeightedMultigraph<Station, DefaultWeightedEdge>(DefaultWeightedEdge.class);
         for (Line line : lines) {
             addSection(graph, line.getSections().getSections());
         }
 
+        validateContain(source, target, graph);
         getShortestPath(source, target, graph);
+    }
+
+    private void validateContain(Station source, Station target, WeightedMultigraph<Station, DefaultWeightedEdge> graph) {
+        if (!graph.containsVertex(source) || !graph.containsVertex(target)) {
+            throw new IllegalArgumentException("존재하지 않은 출발역이나 도착역을 조회 할 수 없습니다.");
+        }
+    }
+
+    private void validateIsSame(Station source, Station target) {
+        if (source == target) {
+            throw new IllegalArgumentException("출발역과 도착역이 같은 경우 최단 경로를 찾을 수 없습니다.");
+        }
     }
 
     private void addSection(WeightedMultigraph<Station, DefaultWeightedEdge> graph, List<Section> sections) {
@@ -33,7 +47,11 @@ public class Path {
     private void getShortestPath(Station source, Station target, WeightedMultigraph<Station, DefaultWeightedEdge> graph) {
         DijkstraShortestPath<Station, DefaultWeightedEdge> dijkstraShortestPath = new DijkstraShortestPath<>(graph);
         shortestPath =  dijkstraShortestPath.getPath(source, target);
+        if (shortestPath == null) {
+            throw new IllegalArgumentException("출발역과 도착역이 연결되어있지 않습니다.");
+        }
     }
+
 
     public List<Station> findShortestPath() {
         return shortestPath.getVertexList();
--- src/test/java/nextstep/subway/path/domain/PathTest.java
+++ src/test/java/nextstep/subway/path/domain/PathTest.java
@@ -1,8 +1,8 @@
 package nextstep.subway.path.domain;
 
 import nextstep.subway.line.domain.Line;
-import nextstep.subway.line.domain.Section;
 import nextstep.subway.station.domain.Station;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -10,32 +10,64 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class PathTest {
-    @DisplayName("경로 찾기")
-    @Test
-    void findPath() {
-        // then
-        Station 강남역 = new Station("강남역");
-        Station 양재역 = new Station("양재역");
-        Station 교대역 = new Station("교대역");
-        Station 남부터미널역 = new Station("남부터미널역");
+    private Station 강남역;
+    private Station 양재역;
+    private Station 교대역;
+    private Station 남부터미널역;
+    private List<Line> lines = new ArrayList<>();
+
+    @BeforeEach
+    void setUp() {
+        강남역 = new Station("강남역");
+        양재역 = new Station("양재역");
+        교대역 = new Station("교대역");
+        남부터미널역 = new Station("남부터미널역");
 
         Line 신분당선 = new Line("신분당선", "red", 강남역, 양재역, 10);
         Line 이호선 = new Line("이호선", "green", 교대역, 강남역, 10);
         Line 삼호선 = new Line("삼호선", "orange", 교대역, 양재역, 5);
 
-        삼호선.addSection(new Section(삼호선, 교대역, 남부터미널역, 3));
-        List<Line> lines = new ArrayList<>();
         lines.add(신분당선);
         lines.add(이호선);
         lines.add(삼호선);
+    }
 
-        // when
+    @DisplayName("경로 찾기")
+    @Test
+    void findPath() {
         Path path = new Path(lines, 강남역, 양재역);
 
-        //then
         assertThat(path.findShortestPath()).containsExactly(강남역, 양재역);
         assertThat(path.findWeight()).isEqualTo(10);
+    }
+
+    @DisplayName("경로 찾기 예외 - 출발역과 도착역이 같은 경우")
+    @Test
+    void isSameSourceAndTarget() {
+        assertThatThrownBy(() -> {
+            new Path(lines, 강남역, 강남역);
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("경로 찾기 예외 -  존재하지 않은 출발역이나 도착역을 조회 할 경우")
+    @Test
+    void validateContain() {
+        Station 왕십리역 = new Station("왕십리역");
+        Station 군자 = new Station("군자역");
+
+        assertThatThrownBy(() -> {
+            new Path(lines, 왕십리역, 군자);
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("경로 찾기 예외 -  출발역과 도착역이 연결이 되어 있지 않은 경우")
+    @Test
+    void validateNotPath() {
+        assertThatThrownBy(() -> {
+            new Path(lines, 양재역, 남부터미널역);
+        }).isInstanceOf(IllegalArgumentException.class);
     }
 }
