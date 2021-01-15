13270e9b7a338579ad2216889585ccb83379cd6e
bss <baekss0409@gmail.com>
2021-01-16 00:26:30 +0900

    feat: 인증기반 로그인 API 구축

--- src/test/java/nextstep/subway/auth/acceptance/AuthAcceptanceTest.java
+++ src/test/java/nextstep/subway/auth/acceptance/AuthAcceptanceTest.java
@@ -1,24 +1,68 @@
 package nextstep.subway.auth.acceptance;
 
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
 import nextstep.subway.AcceptanceTest;
+import nextstep.subway.auth.dto.TokenRequest;
+import nextstep.subway.auth.dto.TokenResponse;
+
+import static nextstep.subway.member.MemberAcceptanceTest.*;
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 
 public class AuthAcceptanceTest extends AcceptanceTest {
+    public static final String EMAIL = "baek@github.com";
+    public static final String PASSWORD = "white";
+
+    @BeforeEach
+    void createMember() {
+        회원_생성을_요청(EMAIL, PASSWORD, AGE);
+    }
 
     @DisplayName("Bearer Auth")
     @Test
     void myInfoWithBearerAuth() {
+        ExtractableResponse<Response> response = 로그인_요청(new TokenRequest("baek@github.com", "white"));
+        assertThat(response.as(TokenResponse.class).getAccessToken()).isNotBlank();
     }
 
     @DisplayName("Bearer Auth 로그인 실패")
     @Test
     void myInfoWithBadBearerAuth() {
+        ExtractableResponse<Response> response = 로그인_요청(new TokenRequest("baek@github.com", "black"));
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.UNAUTHORIZED.value());
     }
 
     @DisplayName("Bearer Auth 유효하지 않은 토큰")
     @Test
     void myInfoWithWrongBearerAuth() {
+        ExtractableResponse<Response> response = 로그인_요청(new TokenRequest("baek@github.com", "white"));
+        ExtractableResponse<Response> result = 내정보_조회_요청("비정상Bearer", response.as(TokenResponse.class).getAccessToken());
+        assertThat(result.statusCode()).isEqualTo(HttpStatus.UNAUTHORIZED.value());
     }
 
+    public ExtractableResponse<Response> 로그인_요청(TokenRequest tokenRequest) {
+        return RestAssured
+                .given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(tokenRequest)
+                .when().post("/login/token")
+                .then().log().all()
+                .extract();
+    }
+
+    public ExtractableResponse<Response> 내정보_조회_요청(String bearerType, String token) {
+        return RestAssured
+                .given().log().all()
+                .header("Authorization", bearerType + token)
+                .when().get("/members/me")
+                .then().log().all()
+                .extract();
+    }
 }
