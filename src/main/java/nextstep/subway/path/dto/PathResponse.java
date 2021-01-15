1f64f0e92d955a7bebba45a5449df7264232db06
slamdunk7575 <slamdunk7575@gmail.com>
2021-01-16 02:54:02 +0900

    (refactor) 노선별 추가 요금 계산 구현

--- src/main/java/nextstep/subway/path/dto/PathResponse.java
+++ src/main/java/nextstep/subway/path/dto/PathResponse.java
@@ -1,5 +1,6 @@
 package nextstep.subway.path.dto;
 
+import nextstep.subway.fare.domain.Fare;
 import nextstep.subway.path.domain.ShortestPath;
 import nextstep.subway.station.dto.StationResponse;
 
@@ -9,17 +10,19 @@ import java.util.stream.Collectors;
 public class PathResponse {
     private List<StationResponse> stations;
     private int distance;
+    private int fare;
 
-    private PathResponse(List<StationResponse> stations, int distance) {
+    private PathResponse(List<StationResponse> stations, int distance, int fare) {
         this.stations = stations;
         this.distance = distance;
+        this.fare = fare;
     }
 
-    public static PathResponse of(ShortestPath shortestPath) {
+    public static PathResponse of(ShortestPath shortestPath, Fare fare) {
         List<StationResponse> pathStations = shortestPath.getStations().stream()
                 .map(StationResponse::of)
                 .collect(Collectors.toList());
-        return new PathResponse(pathStations, shortestPath.getDistance());
+        return new PathResponse(pathStations, shortestPath.getDistance(), fare.getFare());
     }
 
     public List<StationResponse> getStations() {
@@ -28,5 +31,9 @@ public class PathResponse {
 
     public int getDistance() {
         return distance;
+    }
+
+    public int getFare() {
+        return fare;
     }
 }
