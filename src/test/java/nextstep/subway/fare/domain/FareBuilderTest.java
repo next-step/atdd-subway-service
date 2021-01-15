f90faa3d3ba2f204d62caa36de72857550e57853
charlloss.dev <charlloss.dev@gmail.com>
2021-01-15 22:41:47 +0900

    test(fare): 지하철 거리별 요금계산 테스트(질문)
    질문: 50km 이상 구간 요금계산//
         ex)100km
         case1: 전 구간 8km 기준으로 요금 상승?
         case2: 0 ~ 50km는 5km, 50키로 초과분부터 8km 기준으로 요금상승?

---
+++ src/test/java/nextstep/subway/fare/domain/FareBuilderTest.java
@@ -0,0 +1,26 @@
+package nextstep.subway.fare.domain;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class FareBuilderTest {
+
+	/**
+	 * 요금체계 : http://www.seoulmetro.co.kr/kr/page.do?menuIdx=354
+	 * @param distance: 거리
+	 * @param fare: 요금
+	 */
+	@DisplayName("1. 거리별 요금 생성 테스트")
+	@ParameterizedTest
+	@CsvSource(value = {"10:1250", "15:1550", "20:1650", "58:2050"}, delimiter = ':')
+	void createFareTest(int distance, int fare) {
+		// when // given
+		FareBuilder fareBuilder = new FareBuilder(distance);
+
+		// then
+		assertThat(fareBuilder.calculate()).isEqualTo(new Money(fare));
+	}
+}
