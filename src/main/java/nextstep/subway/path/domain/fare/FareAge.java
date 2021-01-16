ff7dcf5c75378d75bc8a143eefbc71f89cd2ffac
charlloss.dev <charlloss.dev@gmail.com>
2021-01-17 02:10:43 +0900

    feat(fareAge): 나이별 요금 정책 enum 추가, 나이별 요금계산 기능구현

---
+++ src/main/java/nextstep/subway/path/domain/fare/FareAge.java
@@ -0,0 +1,32 @@
+package nextstep.subway.path.domain.fare;
+
+import java.util.Arrays;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public enum FareAge {
+	BABY(0, 6, Money.of(0), 0),
+	CHILD(6, 13, Money.of(450), 0.5),
+	STUDENT(13, 18, Money.of(720), 0.8),
+	ADULT(18, 65, Money.of(1250), 1),
+	OLDSTER(65, Integer.MAX_VALUE, Money.of(0), 0);
+
+	private final int minimumAge;
+	private final int maximumAge;
+	private final Money basicFare;
+	private final double discount;
+
+	public static FareAge findFareAge(Integer age) {
+		if (age == null) {
+			return ADULT;
+		}
+		return Arrays.stream(values())
+			.filter(it -> it.minimumAge <= age)
+			.filter(it -> it.maximumAge > age)
+			.findFirst()
+			.orElse(ADULT);
+	}
+}
