93b5490924b8cfe70193a6e8a1e3f7eba5478310
charlloss.dev <charlloss.dev@gmail.com>
2021-01-16 16:59:57 +0900

    test(calculator): 지하철 거리 계산기 객체 변경(enum -> class)

--- src/main/java/nextstep/subway/path/domain/fare/DistanceCalculator.java
+++ src/main/java/nextstep/subway/path/domain/fare/DistanceCalculator.java
@@ -1,44 +1,32 @@
 package nextstep.subway.path.domain.fare;
 
-import java.util.Arrays;
+public class DistanceCalculator {
 
-import nextstep.subway.line.domain.Distance;
+	private static final int SHORT_DISTANCE = 10;
+	private static final int MIDDLE_DISTANCE = 50;
 
-public enum DistanceCalculator {
-
-	SHORT_DISTANCE(10, 0),
-	MIDDLE_DISTANCE(50, 5),
-	LONG_DISTANCE(Integer.MAX_VALUE, 8);
+	private static final int MIDDLE_INCREASE_FARE_DISTANCE = 5;
+	private static final int LONG_INCREASE_FARE_DISTANCE = 8;
 
 	private static final Money BASIC_FARE = Money.of(1250L);
 	private static final int INCREASE_FARE_AMOUNT = 100;
 
-	private final int maxDistance;
-	private final int increaseFareByDistance;
-
-	DistanceCalculator(final int maxDistance, final int increaseFareByDistance) {
-		this.maxDistance = maxDistance;
-		this.increaseFareByDistance = increaseFareByDistance;
+	public static Money apply(int distance) {
+		return calculateOverFare(distance);
 	}
 
-	public static Money apply(Distance distance) {
-		return calculator(distance);
-	}
-
-	public static Money calculator(Distance distance) {
-		DistanceCalculator distanceCalculator = Arrays.stream(values())
-			.filter(it -> it.maxDistance >= distance.getDistance())
-			.findFirst()
-			.orElseThrow(IllegalArgumentException::new);
-
-		return distanceCalculator.calculateOverFare(distance);
-	}
-
-	private Money calculateOverFare(Distance distance) {
-		if (this.increaseFareByDistance == 0) {
+	private static Money calculateOverFare(int distance) {
+		if (SHORT_DISTANCE >= distance) {
 			return BASIC_FARE;
 		}
-		int range = (distance.getDistance() - 1) / this.increaseFareByDistance;
-		return BASIC_FARE.plus((int)((Math.ceil(range) + 1) * INCREASE_FARE_AMOUNT));
+		if (MIDDLE_DISTANCE >= distance) {
+			distance -= SHORT_DISTANCE;
+			int range = (distance - 1) / MIDDLE_INCREASE_FARE_DISTANCE;
+			return calculateOverFare(SHORT_DISTANCE)
+				.plus((int)((Math.ceil(range) + 1) * INCREASE_FARE_AMOUNT));
+		}
+		distance -= MIDDLE_DISTANCE;
+		int range = (distance - 1) / LONG_INCREASE_FARE_DISTANCE;
+		return calculateOverFare(MIDDLE_DISTANCE).plus((int)((Math.ceil(range) + 1) * INCREASE_FARE_AMOUNT));
 	}
 }
