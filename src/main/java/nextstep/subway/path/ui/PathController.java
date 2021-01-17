1bc8ca4d89579c1af885d50b5673d2574a89eed2
Donghwan Noh <namul.bap@gmail.com>
2021-01-18 06:30:58 +0900

    feat(path): Controller를 mock서버 → 구현된 서비스를 사용하도록 수정

--- src/main/java/nextstep/subway/path/ui/PathController.java
+++ src/main/java/nextstep/subway/path/ui/PathController.java
@@ -1,30 +1,25 @@
 package nextstep.subway.path.ui;
 
-import java.time.LocalDateTime;
-import java.util.Arrays;
-
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import nextstep.subway.path.application.PathService;
 import nextstep.subway.path.dto.PathResponse;
-import nextstep.subway.path.dto.PathStationResponse;
 
 @RestController
 public class PathController {
 
+	private final PathService pathService;
+
+	public PathController(PathService pathService) {
+		this.pathService = pathService;
+	}
+
 	@GetMapping(value = "/paths", produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<PathResponse> findPath(@RequestParam Long source, @RequestParam Long target) {
-		PathResponse pathResponse = new PathResponse(
-			Arrays.asList(
-				new PathStationResponse(3L, "교대역", LocalDateTime.now()),
-				new PathStationResponse(4L, "남부터미널역", LocalDateTime.now()),
-				new PathStationResponse(2L, "양재역", LocalDateTime.now())
-			),
-			5L
-		);
-		return ResponseEntity.ok().body(pathResponse);
+		return ResponseEntity.ok().body(pathService.findPath(source, target));
 	}
 }
