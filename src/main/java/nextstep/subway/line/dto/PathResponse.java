8dd12d8279a3203f8eb92993ca90e54154d2e963
bss <baekss0409@gmail.com>
2021-01-19 03:26:47 +0900

    feat: 경로 찾기 인증 기반 로그인 및 요금 조회 API 구축

--- src/main/java/nextstep/subway/line/dto/PathResponse.java
+++ src/main/java/nextstep/subway/line/dto/PathResponse.java
@@ -7,25 +7,31 @@ import java.util.List;
 
 public class PathResponse {
     private List<StationResponse> stations;
-    private double distance;
+    private int distance;
+    private int fare;
 
     public PathResponse() {
     }
 
-    public PathResponse(List<Station> stations, double distance) {
+    public PathResponse(List<Station> stations, int distance, int fare) {
         this.stations = StationResponse.ofList(stations);
         this.distance = distance;
+        this.fare = fare;
     }
 
-    public static PathResponse of(List<Station> stations, double distance) {
-        return new PathResponse(stations, distance);
+    public static PathResponse of(List<Station> stations, int distance, int fare) {
+        return new PathResponse(stations, distance, fare);
     }
 
     public List<StationResponse> getStations() {
         return stations;
     }
 
-    public double getDistance() {
+    public int getDistance() {
         return distance;
+    }
+
+    public int getFare() {
+        return fare;
     }
 }
