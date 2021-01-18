a099412c9a8bbee7e687fc37959a99926c2558f2
byungkyu-ju <noel1227@gmail.com>
2021-01-18 12:08:39 +0900

    feat(AuthAcceptanceTest): 로그인 시도, 로그인 실패 인수테스트 구현

--- src/test/java/nextstep/subway/auth/acceptance/AuthAcceptanceTest.java
+++ src/test/java/nextstep/subway/auth/acceptance/AuthAcceptanceTest.java
@@ -18,9 +18,9 @@ import nextstep.subway.member.MemberAcceptanceTest;
 @DisplayName("로그인 기능")
 public class AuthAcceptanceTest extends AcceptanceTest {
 
-    @DisplayName("로그인을 시도한다")
+    @DisplayName("Bearer Auth")
     @Test
-    void tryLogin(){
+    void myInfoWithBearerAuth() {
         // given
         회원_등록되어_있음(MemberAcceptanceTest.EMAIL, MemberAcceptanceTest.PASSWORD, MemberAcceptanceTest.AGE);
 
@@ -31,21 +31,27 @@ public class AuthAcceptanceTest extends AcceptanceTest {
         로그인_됨(response);
     }
 
-    @DisplayName("Bearer Auth")
-    @Test
-    void myInfoWithBearerAuth() {
-    }
-
     @DisplayName("Bearer Auth 로그인 실패")
     @Test
     void myInfoWithBadBearerAuth() {
+        // when
+        ExtractableResponse<Response> response = 로그인_요청(MemberAcceptanceTest.EMAIL, MemberAcceptanceTest.PASSWORD);
+
+        // then
+        로그인_실패(response);
     }
 
     @DisplayName("Bearer Auth 유효하지 않은 토큰")
     @Test
     void myInfoWithWrongBearerAuth() {
+
+
     }
 
+
+    private void 로그인_실패(ExtractableResponse<Response> response) {
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.UNAUTHORIZED.value());
+    }
 
     private void 회원_등록되어_있음(String email, String password, int age) {
         ExtractableResponse<Response> response = MemberAcceptanceTest.회원_생성을_요청(email, password, age);
@@ -54,6 +60,7 @@ public class AuthAcceptanceTest extends AcceptanceTest {
 
     private void 로그인_됨(ExtractableResponse<Response> response) {
         TokenResponse tokenResponse = response.as(TokenResponse.class);
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
         assertThat(tokenResponse.getAccessToken()).isNotEmpty();
     }
 
@@ -67,8 +74,6 @@ public class AuthAcceptanceTest extends AcceptanceTest {
             .when().post("/login/token")
             .then().log().all().extract();
 
-        // then
-        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
         return response;
     }
 
