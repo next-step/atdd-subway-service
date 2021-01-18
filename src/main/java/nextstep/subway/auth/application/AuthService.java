d88a8e6b5eb70e27b44d2aa03061aee36d35cd28
bss <baekss0409@gmail.com>
2021-01-19 03:40:09 +0900

    refactor: 인증 예외 처리

--- src/main/java/nextstep/subway/auth/application/AuthService.java
+++ src/main/java/nextstep/subway/auth/application/AuthService.java
@@ -29,7 +29,7 @@ public class AuthService {
 
     public LoginMember findMemberByToken(String credentials) {
         if (!jwtTokenProvider.validateToken(credentials)) {
-            throw new RuntimeException("인증 완료 상태만 접근 가능합니다");
+            throw new AuthorizationException("인증 완료 상태만 접근 가능합니다");
         }
 
         String email = jwtTokenProvider.getPayload(credentials);
