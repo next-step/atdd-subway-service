e7304de099ec0073244998ade6f07c0ce92a310c
bss <baekss0409@gmail.com>
2021-01-19 02:17:03 +0900

    feat: 인증 관심사 적용 해제

--- src/main/java/nextstep/subway/SubwayApplication.java
+++ src/main/java/nextstep/subway/SubwayApplication.java
@@ -2,11 +2,9 @@ package nextstep.subway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@EnableAspectJAutoProxy(proxyTargetClass = true)
 @EnableJpaRepositories
 @EnableJpaAuditing
 @SpringBootApplication
