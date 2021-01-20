b56174f1da20e1951a4d20b41a391a7ce6d2e555
shk3029 <shk30290@naver.com>
2021-01-21 02:06:37 +0900

    test: 로그인 유/무, 나이에 따른 인수테스트 추가

--- src/test/java/nextstep/subway/path/acceptance/PathAcceptanceTest.java
+++ src/test/java/nextstep/subway/path/acceptance/PathAcceptanceTest.java
@@ -13,6 +13,7 @@ import org.junit.jupiter.api.Test;
 
 import static nextstep.subway.line.acceptance.LineAcceptanceTest.지하철_노선_등록되어_있음;
 import static nextstep.subway.station.StationAcceptanceTest.지하철역_등록되어_있음;
+import static nextstep.subway.utils.AuthRestAssuredTestUtils.*;
 import static nextstep.subway.utils.LineSectionRestAssuredUtils.지하철_노선에_지하철역_등록_요청;
 import static nextstep.subway.utils.LineSectionRestAssuredUtils.지하철_노선에_지하철역_등록됨;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -20,6 +21,11 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("지하철 경로 조회")
 public class PathAcceptanceTest extends AcceptanceTest {
+
+    private String 이메일주소;
+    private String 패스워드;
+    private String 토큰;
+    private int 나이;
 
     private LineResponse 신분당선;
     private LineResponse 이호선;
@@ -43,6 +49,10 @@ public class PathAcceptanceTest extends AcceptanceTest {
     @BeforeEach
     public void setUp() {
         super.setUp();
+        이메일주소 = "email@email.com";
+        패스워드 = "password";
+        나이 = 15;
+
         강남역 = 지하철역_등록되어_있음("강남역").as(StationResponse.class);
         양재역 = 지하철역_등록되어_있음("양재역").as(StationResponse.class);
         교대역 = 지하철역_등록되어_있음("교대역").as(StationResponse.class);
@@ -56,7 +66,7 @@ public class PathAcceptanceTest extends AcceptanceTest {
     }
 
     @Test
-    @DisplayName("지하철 경로를 조회")
+    @DisplayName("지하철 경로를 조회 (로그인 X)")
     public void searchPath() {
         ExtractableResponse<Response> result = 경로_조회_요청(남부터미널역, 강남역);
 
@@ -66,9 +76,45 @@ public class PathAcceptanceTest extends AcceptanceTest {
         assertThat(path.getFare()).isEqualTo(2850);
     }
 
+    @Test
+    @DisplayName("지하철 경로를 조회 (로그인 O) - 청소년")
+    public void searchPathWithLoginTeenager() {
+        회원_등록됨(이메일주소, 패스워드, 나이);
+        ExtractableResponse<Response> loginResponse = 로그인_요청(이메일주소, 패스워드);
+        토큰 = 로그인_됨(loginResponse);
+
+        ExtractableResponse<Response> result = 로그인_경로_조회_요청(토큰, 남부터미널역, 강남역);
+
+        PathResponse path = result.as(PathResponse.class);
+        assertThat(path.getFare()).isEqualTo(2000);
+    }
+
+    @Test
+    @DisplayName("지하철 경로를 조회 (로그인 O) - 어린이")
+    public void searchPathWithLoginChild() {
+        나이 = 6;
+        회원_등록됨(이메일주소, 패스워드, 나이);
+        ExtractableResponse<Response> loginResponse = 로그인_요청(이메일주소, 패스워드);
+        토큰 = 로그인_됨(loginResponse);
+
+        ExtractableResponse<Response> result = 로그인_경로_조회_요청(토큰, 남부터미널역, 강남역);
+
+        PathResponse path = result.as(PathResponse.class);
+        assertThat(path.getFare()).isEqualTo(1250);
+    }
+
     private ExtractableResponse<Response> 경로_조회_요청(StationResponse source, StationResponse target) {
         return RestAssured.given().log().all()
                 .when()
+                .get("/paths?source={sourceId}&target={targetId}", source.getId(), target.getId())
+                .then().log().all()
+                .extract();
+    }
+
+    private ExtractableResponse<Response> 로그인_경로_조회_요청(String token, StationResponse source, StationResponse target) {
+        return RestAssured.given().log().all()
+                .when()
+                .auth().oauth2(token)
                 .get("/paths?source={sourceId}&target={targetId}", source.getId(), target.getId())
                 .then().log().all()
                 .extract();
