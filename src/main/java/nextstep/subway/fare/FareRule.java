2ed554fe8ca0054067462303880d89306a1825be
shk3029 <shk30290@naver.com>
2021-01-20 14:11:33 +0900

    feat: FareRule (요금계산기), FareRuleTest 추가

---
+++ src/main/java/nextstep/subway/fare/FareRule.java
@@ -0,0 +1,49 @@
+package nextstep.subway.fare;
+
+
+public enum FareRule {
+    추가운임_50km_이내(5, 100),
+    추가운임_50km_초과(8, 100);
+
+    private static final long DEFAULT_FARE = 1250;
+    private static final int DISTANCE_10KM = 10;
+    private static final int DISTANCE_50KM = 50;
+
+    private final int fareDistance;
+    private final long surcharge;
+
+    FareRule(int fareDistance, long surcharge) {
+        this.fareDistance = fareDistance;
+        this.surcharge = surcharge;
+    }
+
+    public int getFareDistance() {
+        return fareDistance;
+    }
+
+    public long getSurcharge() {
+        return surcharge;
+    }
+
+    public static long findFareByDistance(int distance) {
+        long fare = DEFAULT_FARE;
+
+        if (distance > DISTANCE_50KM) {
+            fare += findSurcharge(추가운임_50km_초과, distance - DISTANCE_50KM);
+            distance = DISTANCE_50KM;
+        }
+
+        if (distance > DISTANCE_10KM) {
+            fare += findSurcharge(추가운임_50km_이내, distance - DISTANCE_10KM);
+        }
+
+        return fare;
+    }
+
+    private static long findSurcharge(FareRule fareRule, int distance) {
+        int share = distance / fareRule.getFareDistance();
+        share += distance % fareRule.getFareDistance() == 0 ? 0 : 1;
+        long surcharge = share * fareRule.getSurcharge();
+        return surcharge;
+    }
+}
---
+++ src/test/java/nextstep/subway/fare/FareRuleTest.java
@@ -0,0 +1,43 @@
+package nextstep.subway.fare;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static nextstep.subway.fare.FareRule.findFareByDistance;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+class FareRuleTest {
+
+
+    @DisplayName("요금을 계산한다 : 10km 이내")
+    @Test
+    void calculator_10km() {
+        assertAll(
+                () -> assertThat(findFareByDistance(1)).isEqualTo(1250),
+                () -> assertThat(findFareByDistance(5)).isEqualTo(1250),
+                () -> assertThat(findFareByDistance(10)).isEqualTo(1250)
+        );
+    }
+
+    @DisplayName("요금을 계산한다 : 10km 초과 ~ 50km 이내")
+    @Test
+    void calculator_10km_50km() {
+        assertAll(
+                () -> assertThat(findFareByDistance(11)).isEqualTo(1350),
+                () -> assertThat(findFareByDistance(15)).isEqualTo(1350),
+                () -> assertThat(findFareByDistance(25)).isEqualTo(1550),
+                () -> assertThat(findFareByDistance(50)).isEqualTo(2050)
+        );
+    }
+
+    @DisplayName("요금을 계산한다 : 50km 초과")
+    @Test
+    void calculator_50km() {
+        assertAll(
+                () -> assertThat(findFareByDistance(51)).isEqualTo(2150),
+                () -> assertThat(findFareByDistance(58)).isEqualTo(2150),
+                () -> assertThat(findFareByDistance(59)).isEqualTo(2250)
+        );
+    }
+}
