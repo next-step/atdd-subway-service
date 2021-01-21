988b6656339e70f56b0c24bcc72ac7ccce1568d8
orgojy <giveawesome@gmail.com>
2021-01-21 23:25:02 +0900

    feat: "이용 거리초과 시 추가운임 부과" 인수 테스트 추가/수정

--- src/test/java/nextstep/subway/path/PathAcceptanceTest.java
+++ src/test/java/nextstep/subway/path/PathAcceptanceTest.java
@@ -51,11 +51,11 @@ public class PathAcceptanceTest extends AcceptanceTest {
         남부터미널역 = StationAcceptanceTest.지하철역_등록되어_있음("남부터미널역").as(StationResponse.class);
         인천역 = StationAcceptanceTest.지하철역_등록되어_있음("인천역").as(StationResponse.class);
 
-        신분당선 = 지하철_노선_등록되어_있음("신분당선", "bg-red-600", 강남역.getId(), 양재역.getId(), 1000L);
-        이호선 = 지하철_노선_등록되어_있음("이호선", "bg-green-200", 교대역.getId(), 강남역.getId(), 1000L);
-        삼호선 = 지하철_노선_등록되어_있음("삼호선", "bg-yellow-300", 교대역.getId(), 양재역.getId(), 500L);
+        신분당선 = 지하철_노선_등록되어_있음("신분당선", "bg-red-600", 강남역.getId(), 양재역.getId(), 50);
+        이호선 = 지하철_노선_등록되어_있음("이호선", "bg-green-200", 교대역.getId(), 강남역.getId(), 60);
+        삼호선 = 지하철_노선_등록되어_있음("삼호선", "bg-yellow-300", 교대역.getId(), 양재역.getId(), 60);
 
-        지하철_노선에_지하철역_등록되어_있음(삼호선, 교대역, 남부터미널역, 300L);
+        지하철_노선에_지하철역_등록되어_있음(삼호선, 교대역, 남부터미널역, 30);
     }
 
     @DisplayName("`교대역`에서 `양재역`까지 최단경로를 조회")
@@ -67,8 +67,8 @@ public class PathAcceptanceTest extends AcceptanceTest {
         assertAll(
                 () -> assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value()),
                 () -> assertThat(response.as(PathResponse.class).getStations()).hasSize(3),
-                () -> assertThat(response.as(PathResponse.class).getDistance()).isEqualTo(500L),
-                () -> assertThat(response.as(PathResponse.class).getFare()).isEqualTo(1250 + 100 + 100)
+                () -> assertThat(response.as(PathResponse.class).getDistance()).isEqualTo(60),
+                () -> assertThat(response.as(PathResponse.class).getFare()).isEqualTo(1250 + 800 + 200)
         );
     }
 
@@ -111,11 +111,11 @@ public class PathAcceptanceTest extends AcceptanceTest {
                 .then().log().all().extract();
     }
 
-    private void 지하철_노선에_지하철역_등록되어_있음(LineResponse line, StationResponse upStation, StationResponse downStation, long distance) {
+    private void 지하철_노선에_지하철역_등록되어_있음(LineResponse line, StationResponse upStation, StationResponse downStation, int distance) {
         LineSectionAcceptanceTest.지하철_노선에_지하철역_등록_요청(line, upStation, downStation, distance);
     }
 
-    private LineResponse 지하철_노선_등록되어_있음(String line, String color, Long upStationId, Long downStationId, long distance) {
+    private LineResponse 지하철_노선_등록되어_있음(String line, String color, Long upStationId, Long downStationId, int distance) {
         return LineAcceptanceTest.지하철_노선_등록되어_있음(new LineRequest(line, color, upStationId, downStationId, distance))
                 .as(LineResponse.class);
     }
