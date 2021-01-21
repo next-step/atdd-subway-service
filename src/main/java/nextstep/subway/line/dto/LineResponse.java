3c665ee51cd55db43e1f3bf91043376965f7b46e
orgojy <giveawesome@gmail.com>
2021-01-22 01:11:12 +0900

    feat: "노선별 추가 요금 정책" 인수 테스트 추가/수정

--- src/main/java/nextstep/subway/line/dto/LineResponse.java
+++ src/main/java/nextstep/subway/line/dto/LineResponse.java
@@ -13,23 +13,25 @@ public class LineResponse {
     private String name;
     private String color;
     private List<StationResponse> stations;
+    private int additionalFare;
     private LocalDateTime createdDate;
     private LocalDateTime modifiedDate;
 
     public LineResponse() {
     }
 
-    public LineResponse(Long id, String name, String color, List<StationResponse> stations, LocalDateTime createdDate, LocalDateTime modifiedDate) {
+    public LineResponse(Long id, String name, String color, List<StationResponse> stations, int additionalFare, LocalDateTime createdDate, LocalDateTime modifiedDate) {
         this.id = id;
         this.name = name;
         this.color = color;
         this.stations = stations;
+        this.additionalFare = additionalFare;
         this.createdDate = createdDate;
         this.modifiedDate = modifiedDate;
     }
 
     public static LineResponse of(Line line) {
-        return new LineResponse(line.getId(), line.getName(), line.getColor(), convertStations(line.getStations()), line.getCreatedDate(), line.getModifiedDate());
+        return new LineResponse(line.getId(), line.getName(), line.getColor(), convertStations(line.getStations()), 900, line.getCreatedDate(), line.getModifiedDate());
     }
 
     public Long getId() {
@@ -46,6 +48,10 @@ public class LineResponse {
 
     public List<StationResponse> getStations() {
         return stations;
+    }
+
+    public int getAdditionalFare() {
+        return additionalFare;
     }
 
     public LocalDateTime getCreatedDate() {
