3b4e97450207e0d99d656914f769ab9779131043
shk3029 <shk30290@naver.com>
2021-01-21 23:23:17 +0900

    test: @ParameterizedTest로 변경

--- src/test/java/nextstep/subway/fare/FareAgeRuleTest.java
+++ src/test/java/nextstep/subway/fare/FareAgeRuleTest.java
@@ -1,20 +1,19 @@
 package nextstep.subway.fare;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class FareAgeRuleTest {
 
+    private final int FARE = 1000;
+
     @DisplayName("나이에 따른 요금할인 : 13~19세미만(20%), 6~13미만(50%)")
-    @Test
-    void discountFareByAge() {
-        assertAll(
-                ()-> assertThat(FareAgeRule.discountFareByAge(30,1000)).isEqualTo(1000),
-                ()-> assertThat(FareAgeRule.discountFareByAge(13,1000)).isEqualTo(520),
-                ()-> assertThat(FareAgeRule.discountFareByAge(8,1000)).isEqualTo(325)
-        );
+    @ParameterizedTest
+    @CsvSource(value = {"30:1000","13:520","8:325"}, delimiter = ':')
+    void discountFareByAge(int age, int expected) {
+        assertThat(FareAgeRule.discountFareByAge(age,FARE)).isEqualTo(expected);
     }
 }
--- src/test/java/nextstep/subway/fare/FareDistanceRuleTest.java
+++ src/test/java/nextstep/subway/fare/FareDistanceRuleTest.java
@@ -1,43 +1,33 @@
 package nextstep.subway.fare;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static nextstep.subway.fare.FareDistanceRule.findFareByDistance;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertAll;
 
 class FareDistanceRuleTest {
 
 
     @DisplayName("요금을 계산한다 : 10km 이내")
-    @Test
-    void calculator_10km() {
-        assertAll(
-                () -> assertThat(findFareByDistance(1)).isEqualTo(1250),
-                () -> assertThat(findFareByDistance(5)).isEqualTo(1250),
-                () -> assertThat(findFareByDistance(10)).isEqualTo(1250)
-        );
+    @ParameterizedTest
+    @CsvSource(value = {"1:1250", "5:1250", "10:1250"}, delimiter = ':')
+    void calculator_10km(int distance, int fare) {
+        assertThat(findFareByDistance(distance)).isEqualTo(fare);
     }
 
     @DisplayName("요금을 계산한다 : 10km 초과 ~ 50km 이내")
-    @Test
-    void calculator_10km_50km() {
-        assertAll(
-                () -> assertThat(findFareByDistance(11)).isEqualTo(1350),
-                () -> assertThat(findFareByDistance(15)).isEqualTo(1350),
-                () -> assertThat(findFareByDistance(25)).isEqualTo(1550),
-                () -> assertThat(findFareByDistance(50)).isEqualTo(2050)
-        );
+    @ParameterizedTest
+    @CsvSource(value = {"11:1350", "15:1350", "25:1550", "50:2050"}, delimiter = ':')
+    void calculator_10km_50km(int distance, int fare) {
+        assertThat(findFareByDistance(distance)).isEqualTo(fare);
     }
 
     @DisplayName("요금을 계산한다 : 50km 초과")
-    @Test
-    void calculator_50km() {
-        assertAll(
-                () -> assertThat(findFareByDistance(51)).isEqualTo(2150),
-                () -> assertThat(findFareByDistance(58)).isEqualTo(2150),
-                () -> assertThat(findFareByDistance(59)).isEqualTo(2250)
-        );
+    @ParameterizedTest
+    @CsvSource(value = {"51:2150", "58:2150", "59:2250"}, delimiter = ':')
+    void calculator_50km(int distance, int fare) {
+        assertThat(findFareByDistance(distance)).isEqualTo(fare);
     }
 }
