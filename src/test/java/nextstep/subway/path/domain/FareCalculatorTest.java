4158f46b7377a51f19b072fa0ba8f9273cffeff0
orgojy <giveawesome@gmail.com>
2021-01-22 23:41:08 +0900

    test: "연령별 할인 정책 추가" 관련 FareCalculator 테스트 케이스 추가

--- src/test/java/nextstep/subway/path/domain/FareCalculatorTest.java
+++ src/test/java/nextstep/subway/path/domain/FareCalculatorTest.java
@@ -3,7 +3,9 @@ package nextstep.subway.path.domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
+import static nextstep.subway.path.domain.FareCalculator.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class FareCalculatorTest {
@@ -13,5 +15,29 @@ class FareCalculatorTest {
     @CsvSource(value = {"1:1250", "10:1250", "20:1450", "50:2050", "58:2150", "59:2250"}, delimiter = ':')
     void calculateOf(int distance, int fare) {
         assertThat(FareCalculator.calculateFareOf(distance)).isEqualTo(fare);
+    }
+
+    @DisplayName("연령에 따라서 요금이 할인된다. - 어린이")
+    @ParameterizedTest
+    @ValueSource(ints = {6, 7, 8, 9, 10, 11, 12})
+    void calculateDiscountedFareByAgeForChildren(int age) {
+        // given
+        int fare = FareCalculator.calculateFareOf(100);
+        // when
+        int discountedFareByAge = FareCalculator.calculateDiscountedFareByAge(fare, age);
+        // then
+        assertThat(discountedFareByAge).isEqualTo((int) ((fare - BASIC_DISCOUNT_FARE) * DISCOUNT_RATE_FOR_CHILDREN));
+    }
+
+    @DisplayName("연령에 따라서 요금이 할인된다. - 청소년")
+    @ParameterizedTest
+    @ValueSource(ints = {13, 14, 15, 16, 17, 18})
+    void calculateDiscountedFareByAgeForYouth(int age) {
+        // given
+        int fare = FareCalculator.calculateFareOf(100);
+        // when
+        int discountedFareByAge = FareCalculator.calculateDiscountedFareByAge(fare, age);
+        // then
+        assertThat(discountedFareByAge).isEqualTo((int) ((fare - BASIC_DISCOUNT_FARE) * DISCOUNT_RATE_FOR_YOUTH));
     }
 }
