3c2fdb014b9e786f9ca02038968f99332b79b3de
Mingyu Lee <leemingyu05@gmail.com>
2021-01-23 19:16:27 +0900

    test: 경로 조회 예외 도메인 테스트 - 출발역과 도착역이 같은 경우

--- src/main/java/nextstep/subway/line/domain/PathFinder.java
+++ src/main/java/nextstep/subway/line/domain/PathFinder.java
@@ -18,8 +18,15 @@ public class PathFinder {
 	}
 
 	public SubwayPath getSubwayPath(final Station source, final Station target) {
+		validateFind(source, target);
 		GraphPath graphPath = this.dijkstraShortestPath.getPath(source, target);
 		return new SubwayPath(graphPath.getVertexList(), Double.valueOf(graphPath.getWeight()).intValue());
+	}
+
+	private void validateFind(final Station source, final Station target) {
+		if (source == target) {
+			throw new IllegalArgumentException("출발역과 도착역은 같을 수 없습니다.");
+		}
 	}
 
 	private DijkstraShortestPath build(final List<Line> lines) {
--- src/test/java/nextstep/subway/path/PathTest.java
+++ src/test/java/nextstep/subway/path/PathTest.java
@@ -5,6 +5,7 @@ import static org.assertj.core.api.Assertions.*;
 import java.util.Arrays;
 import java.util.List;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -16,33 +17,57 @@ import nextstep.subway.station.domain.Station;
 @DisplayName("경로 찾기 테스트")
 public class PathTest {
 
+	private Station 강남역;
+	private Station 양재역;
+	private Station 교대역;
+	private Station 남부터미널역;
+
+	private Line 신분당선;
+	private Line 이호선;
+	private Line 삼호선;
+
+	private List<Line> lines;
+	private PathFinder pathFinder;
+	private SubwayPath subwayPath;
+
+	@BeforeEach
+	public void setup() {
+		long id = 1L;
+		강남역 = new Station(id++, "강남역");
+		양재역 = new Station(id++, "양재역");
+		교대역 = new Station(id++, "교대역");
+		남부터미널역 = new Station(id, "남부터미널역");
+
+		신분당선 = new Line("신분당선", "bg-red-600", 강남역, 양재역, 10);
+		이호선 = new Line("이호선", "bg-green-600", 교대역, 강남역, 10);
+		삼호선 = new Line("삼호선", "bg-orange-600", 교대역, 양재역, 5);
+
+		삼호선.addSection(교대역, 남부터미널역, 3);
+
+		lines = Arrays.asList(신분당선, 이호선, 삼호선);
+		pathFinder = new PathFinder(lines);
+
+	}
+
 	@DisplayName("경로 조회")
 	@Test
 	void findPath() {
 		// given
-		long id = 1L;
-		Station 강남역 = new Station(id++, "강남역");
-		Station 양재역 = new Station(id++, "양재역");
-		Station 교대역 = new Station(id++, "교대역");
-		Station 남부터미널역 = new Station(id, "남부터미널역");
-
-		Line 신분당선 = new Line("신분당선", "bg-red-600", 강남역, 양재역, 10);
-		Line 이호선 = new Line("이호선", "bg-green-600", 교대역, 강남역, 10);
-		Line 삼호선 = new Line("삼호선", "bg-orange-600", 교대역, 양재역, 5);
-
-		삼호선.addSection(교대역, 남부터미널역, 3);
-
-		List<Line> lines = Arrays.asList(신분당선, 이호선, 삼호선);
 		int expectedDistance = 5;
-
-		// when
-		PathFinder pathFinder = new PathFinder(lines);
-		SubwayPath subwayPath = pathFinder.getSubwayPath(양재역, 교대역);
+		subwayPath = pathFinder.getSubwayPath(양재역, 교대역);
 
 		// then
 		assertThat(subwayPath.getStations()).containsExactly(양재역, 남부터미널역, 교대역);
 		assertThat(subwayPath.getDistance()).isEqualTo(expectedDistance);
 
+	}
+
+	@DisplayName("경로 조회 예외 - 출발역과 도착역이 같은 경우")
+	@Test
+	void findPathThrowExceptionWhenSameSourceAndTarget() {
+		assertThatIllegalArgumentException().isThrownBy(
+			() -> pathFinder.getSubwayPath(양재역, 양재역)
+		);
 	}
 
 }
