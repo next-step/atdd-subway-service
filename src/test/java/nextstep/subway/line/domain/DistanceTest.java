5f4121eeeab8d70dbe38c576a9b95644646dc6cf
ohgillwhan <soora_genius@naver.com>
2021-06-13 00:17:31 +0900

    test : 거리가 양수이면 생성이 된다

--- src/test/java/nextstep/subway/line/domain/DistanceTest.java
+++ src/test/java/nextstep/subway/line/domain/DistanceTest.java
@@ -2,6 +2,8 @@ package nextstep.subway.line.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
@@ -13,5 +15,12 @@ class DistanceTest {
     void 거리는_음수이면_IllegalArgumentException이_발생한다() {
         assertThatIllegalArgumentException()
                 .isThrownBy(() -> new Distance(-1));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1,2,3,4,5})
+    @DisplayName("거리가 양수이면 생성이 된다")
+    void 거리가_양수이면_생성이_된다(int distance) {
+        assertDoesNotThrow(() -> new Distance(distance));
     }
 }
