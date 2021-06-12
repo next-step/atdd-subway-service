41828eb82fc470c88f1ed68c2f25cfdd8d038020
ohgillwhan <soora_genius@naver.com>
2021-06-12 12:07:29 +0900

    test : 하행선을 연결할 수 있다.

--- src/test/java/nextstep/subway/line/domain/LineTest.java
+++ src/test/java/nextstep/subway/line/domain/LineTest.java
@@ -119,25 +119,21 @@ class LineTest {
                 .containsExactly(강남역.getId(), 양재역.getId(), 판교역.getId());
     }
 
-//    @Test
-//    @DisplayName("하행선에 연결할 수 있다")
-//    void 하행선에_연결할_수_있다() {
-//        // given
-//        stationRepository.saveAll(Arrays.asList(양재역, 판교역, 정자역));
-//
-//        LineRequest 신분당_요청 = new LineRequest("신분당선", "빨간색", 양재역.getId(), 판교역.getId(), 3);
-//        LineResponse 신분당_응답 = lineService.saveLine(신분당_요청);
-//
-//        // when
-//        lineService.addLineStation(신분당_응답.getId(), new SectionRequest(판교역.getId(), 정자역.getId(), 3));
-//
-//        // then
-//        LineResponse lineResponse = lineService.findLineResponseById(신분당_응답.getId());
-//
-//        assertThat(lineResponse.getStations())
-//                .map(StationResponse::getId)
-//                .containsExactly(양재역.getId(), 판교역.getId(), 정자역.getId());
-//    }
+    @Test
+    @DisplayName("하행선에 연결할 수 있다")
+    void 하행선에_연결할_수_있다() {
+        // given
+        Line line = new Line("신분당", "RED", 양재역, 판교역, 3);
+
+        // when
+        line.addSection(판교역, 정자역, 3);
+
+        // then
+
+        assertThat(line.sortedStation().toResponses())
+                .map(StationResponse::getId)
+                .containsExactly(강남역.getId(), 양재역.getId(), 판교역.getId());
+    }
 //
 //    @Test
 //    @DisplayName("신규 노선이 하행성을 넘으면 RuntimeException이 발생한다")
