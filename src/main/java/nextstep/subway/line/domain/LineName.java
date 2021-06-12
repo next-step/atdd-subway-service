0452bd0daa868bbb7fe12f54bc91e3e711d2a1e1
ohgillwhan <soora_genius@naver.com>
2021-06-12 23:50:03 +0900

    feat : 같은 값의 이름은 hashcode와 equals가 같다

--- src/main/java/nextstep/subway/line/domain/LineName.java
+++ src/main/java/nextstep/subway/line/domain/LineName.java
@@ -1,5 +1,7 @@
 package nextstep.subway.line.domain;
 
+import java.util.Objects;
+
 public class LineName {
     private String name;
 
@@ -13,5 +15,18 @@ public class LineName {
         if (name == null || name.trim().length() == 0) {
             throw new IllegalArgumentException("이름은 공백이거나 공백이면 안됩니다");
         }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        LineName lineName = (LineName) o;
+        return Objects.equals(name, lineName.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
     }
 }
--- src/test/java/nextstep/subway/line/domain/LineNameTest.java
+++ src/test/java/nextstep/subway/line/domain/LineNameTest.java
@@ -1,10 +1,12 @@
 package nextstep.subway.line.domain;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
@@ -22,5 +24,23 @@ class LineNameTest {
     @DisplayName("이름이 공백이 아니면 정상이다")
     void 이름이_공백이_아니면_정상이다(String s) {
         assertDoesNotThrow(() -> new LineName(s));
+    }
+
+    @Test
+    @DisplayName("같은 값의 이름은 hashcode와 equals가 같다")
+    void 같은_값의_이름은_hashcode와_equals가_같다() {
+        LineName name1 = new LineName("ABCD");
+        LineName name2 = new LineName("ABCD");
+        LineName name3 = new LineName("A");
+
+        assertThat(name1.hashCode())
+                .isEqualTo(name2.hashCode());
+        assertThat(name1)
+                .isEqualTo(name2);
+
+        assertThat(name2.hashCode())
+                .isNotEqualTo(name3.hashCode());
+        assertThat(name2)
+                .isNotEqualTo(name3);
     }
 }
