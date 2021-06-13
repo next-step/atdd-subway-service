b072c8a13eb0da62cd10b33cc3da898ad5afb492
dhmin5693 <dhmin5693@naver.com>
2021-06-14 02:32:14 +0900

    refactor: `LineService::addLineStation` 로직을 도메인으로 이관

---
+++ src/main/java/nextstep/subway/line/domain/AddLineSectionException.java
@@ -0,0 +1,13 @@
+package nextstep.subway.line.domain;
+
+public class AddLineSectionException extends RuntimeException {
+
+    private static final long serialVersionUID = 4264936228055568097L;
+
+    public AddLineSectionException() {
+    }
+
+    public AddLineSectionException(String message) {
+        super(message);
+    }
+}
--- src/main/java/nextstep/subway/line/domain/Line.java
+++ src/main/java/nextstep/subway/line/domain/Line.java
@@ -1,13 +1,18 @@
 package nextstep.subway.line.domain;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import nextstep.subway.BaseEntity;
 import nextstep.subway.station.domain.Station;
-
-import javax.persistence.*;
-import java.util.ArrayList;
-import java.util.List;
 import org.springframework.util.CollectionUtils;
 
 @Entity
@@ -101,5 +106,50 @@ public class Line extends BaseEntity {
         }
 
         return downStation;
+    }
+
+    public void addSection(Section section) {
+
+        List<Station> stations = getStations();
+
+        Station upStation = section.getUpStation();
+        Station downStation = section.getDownStation();
+
+        boolean isUpStationExisted = stations.stream().anyMatch(it -> it == upStation);
+        boolean isDownStationExisted = stations.stream().anyMatch(it -> it == downStation);
+
+        if (isUpStationExisted && isDownStationExisted) {
+            throw new AddLineSectionException("이미 등록된 구간 입니다.");
+        }
+
+        if (!stations.isEmpty() && stations.stream().noneMatch(it -> it == upStation) &&
+            stations.stream().noneMatch(it -> it == downStation)) {
+            throw new AddLineSectionException("등록할 수 없는 구간 입니다.");
+        }
+
+        if (stations.isEmpty()) {
+            sections.add(section);
+            return;
+        }
+
+        int distance = section.getDistance();
+
+        if (isUpStationExisted) {
+            sections.stream()
+                    .filter(it -> it.getUpStation() == upStation)
+                    .findFirst()
+                    .ifPresent(it -> it.updateUpStation(downStation, distance));
+
+            sections.add(new Section(this, upStation, downStation, distance));
+        } else if (isDownStationExisted) {
+            sections.stream()
+                    .filter(it -> it.getDownStation() == downStation)
+                    .findFirst()
+                    .ifPresent(it -> it.updateDownStation(upStation, distance));
+
+            sections.add(new Section(this, upStation, downStation, distance));
+        } else {
+            throw new AddLineSectionException();
+        }
     }
 }
