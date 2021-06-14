62096224381252928f9ab762f3988c414b99e889
ohgillwhan <soora_genius@naver.com>
2021-06-14 15:14:42 +0900

    refact : 마지막 역도 최단거리 리스트에 포함하여 리턴하도록 변경

--- src/main/java/nextstep/subway/path/dto/LinePathResponse.java
+++ src/main/java/nextstep/subway/path/dto/LinePathResponse.java
@@ -6,7 +6,6 @@ import nextstep.subway.station.domain.Station;
 import nextstep.subway.station.dto.StationResponse;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class LinePathResponse {
     private List<StationResponse> stations;
@@ -17,11 +16,8 @@ public class LinePathResponse {
 
     public LinePathResponse(Line shortDistance, Station source, Station target) {
         this.distance = shortDistance.calcDistanceBetween(source, target).toInt();
-        this.stations = shortDistance.getShortestRoute(source, target)
-                .stream()
-                .filter(item -> item != target)
-                .map(StationResponse::of)
-                .collect(Collectors.toList());
+        this.stations = new StationResponses(shortDistance.getShortestRoute(source, target))
+                .toCollection();
     }
 
     public List<StationResponse> getStations() {
