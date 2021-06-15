dc88230f55b7bf933ac28d6cea6e3550459b388c
ted.kang <ted.kang@kakaocorp.com>
2021-06-16 01:36:42 +0900

    test: 최단거리 계산 테스트 작성 - 연결되지 않은 역과의 최단거리를 조회

--- src/test/java/nextstep/subway/path/application/PathServiceTest.java
+++ src/test/java/nextstep/subway/path/application/PathServiceTest.java
@@ -1,7 +1,12 @@
 package nextstep.subway.path.application;
 
+import nextstep.subway.exception.StationsNotConnectedException;
+import nextstep.subway.line.domain.Distance;
+import nextstep.subway.line.domain.Line;
+import nextstep.subway.line.domain.Section;
 import nextstep.subway.line.domain.SectionRepository;
 import nextstep.subway.path.dto.PathResponse;
+import nextstep.subway.station.domain.Station;
 import nextstep.subway.station.domain.StationRepository;
 import nextstep.subway.station.dto.StationResponse;
 import org.junit.jupiter.api.DisplayName;
@@ -14,6 +19,7 @@ import java.util.Arrays;
 
 import static nextstep.subway.path.domain.PathFinderTest.*;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -30,13 +36,13 @@ class PathServiceTest {
     //given
     when(stationRepository.findAll()).thenReturn(Arrays.asList(강남역, 양재역, 교대역, 남부터미널역));
     when(sectionRepository.findAll()).thenReturn(Arrays.asList(강남_양재_구간, 교대_강남_구간, 교대_남부터미널_구간, 남부터미널_양재_구간));
-    PathService pathService = new PathService(stationRepository, sectionRepository);
+    PathService 최단경로서비스 = new PathService(stationRepository, sectionRepository);
 
     //when
-    PathResponse shortestPath = pathService.findShortestPath(교대역.getId(), 양재역.getId());
+    PathResponse 최단거리응답 = 최단경로서비스.findShortestPath(교대역.getId(), 양재역.getId());
 
     //then
-    assertThat(shortestPath).isEqualTo(new PathResponse(Arrays.asList(StationResponse.of(교대역), StationResponse.of(남부터미널역), StationResponse.of(양재역)), 5D));
+    assertThat(최단거리응답).isEqualTo(new PathResponse(Arrays.asList(StationResponse.of(교대역), StationResponse.of(남부터미널역), StationResponse.of(양재역)), 5D));
   }
 
   @DisplayName("기존 최단거리역이 제거되었을 때")
@@ -45,13 +51,13 @@ class PathServiceTest {
     //given
     when(stationRepository.findAll()).thenReturn(Arrays.asList(강남역, 양재역, 교대역, 남부터미널역));
     when(sectionRepository.findAll()).thenReturn(Arrays.asList(강남_양재_구간, 교대_강남_구간, 교대_남부터미널_구간));
-    PathService pathService = new PathService(stationRepository, sectionRepository);
+    PathService 최단경로서비스 = new PathService(stationRepository, sectionRepository);
 
     //when
-    PathResponse shortestPath = pathService.findShortestPath(교대역.getId(), 양재역.getId());
+    PathResponse 최단거리응답 = 최단경로서비스.findShortestPath(교대역.getId(), 양재역.getId());
 
     //then
-    assertThat(shortestPath).isEqualTo(new PathResponse(Arrays.asList(StationResponse.of(교대역), StationResponse.of(강남역), StationResponse.of(양재역)), 20D));
+    assertThat(최단거리응답).isEqualTo(new PathResponse(Arrays.asList(StationResponse.of(교대역), StationResponse.of(강남역), StationResponse.of(양재역)), 20D));
   }
 
   @DisplayName("출발역과 도착역을 같은 역으로 조회")
@@ -60,13 +66,29 @@ class PathServiceTest {
     //given
     when(stationRepository.findAll()).thenReturn(Arrays.asList(강남역, 양재역, 교대역, 남부터미널역));
     when(sectionRepository.findAll()).thenReturn(Arrays.asList(강남_양재_구간, 교대_강남_구간, 교대_남부터미널_구간, 남부터미널_양재_구간));
-    PathService pathService = new PathService(stationRepository, sectionRepository);
+    PathService 최단거리서비스 = new PathService(stationRepository, sectionRepository);
 
     //when
-    PathResponse shortestPath = pathService.findShortestPath(교대역.getId(), 교대역.getId());
+    PathResponse 최단거리응답 = 최단거리서비스.findShortestPath(교대역.getId(), 교대역.getId());
 
     //then
-    assertThat(shortestPath).isEqualTo(new PathResponse(Arrays.asList(StationResponse.of(교대역)), 0D));
+    assertThat(최단거리응답).isEqualTo(new PathResponse(Arrays.asList(StationResponse.of(교대역)), 0D));
+  }
+
+  @DisplayName("연결되지 않은 역과의 최단거리를 조회")
+  @Test
+  void findShortestPathWithNotConnectedStationTest() {
+    //given
+    Station 서울역 = Station.stationStaticFactoryForTestCode(5L, "서울역");
+    Station 용산역 = Station.stationStaticFactoryForTestCode(6L, "용산역");
+    Line 일호선 = new Line("일호선", "navy", 서울역, 용산역, Distance.from(3));
+    Section 서울역_용산역_구간 = new Section(일호선, 서울역, 용산역, Distance.from(3));
+    when(stationRepository.findAll()).thenReturn(Arrays.asList(강남역, 양재역, 교대역, 남부터미널역, 서울역, 용산역));
+    when(sectionRepository.findAll()).thenReturn(Arrays.asList(강남_양재_구간, 교대_강남_구간, 교대_남부터미널_구간, 남부터미널_양재_구간, 서울역_용산역_구간));
+    PathService 최단거리서비스 = new PathService(stationRepository, sectionRepository);
+
+    //when & then
+    assertThatThrownBy(() -> 최단거리서비스.findShortestPath(교대역.getId(), 서울역.getId())).isInstanceOf(StationsNotConnectedException.class);
   }
 
 
