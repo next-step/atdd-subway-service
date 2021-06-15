0875041dcd845e0f06f75226ae3b2fd702cf0523
ted.kang <ted.kang@kakaocorp.com>
2021-06-16 01:45:20 +0900

    test: PathFinderTest 변수명 한글화

--- src/test/java/nextstep/subway/path/domain/PathFinderTest.java
+++ src/test/java/nextstep/subway/path/domain/PathFinderTest.java
@@ -33,27 +33,27 @@ public class PathFinderTest {
   public static Section 교대_남부터미널_구간 = new Section(삼호선, 교대역, 남부터미널역, Distance.from(3));
   public static Section 남부터미널_양재_구간 = new Section(삼호선, 남부터미널역, 양재역, Distance.from(2));
 
-  private List<Station> wholeStations;
-  private List<Section> wholeSections;
+  private List<Station> 전체_역_목록;
+  private List<Section> 전체_구간_목록;
 
   @BeforeEach
   void setUp() {
-    wholeStations = Arrays.asList(강남역, 양재역, 교대역, 남부터미널역);
+    전체_역_목록 = Arrays.asList(강남역, 양재역, 교대역, 남부터미널역);
 
-    wholeSections = Arrays.asList(강남_양재_구간, 교대_강남_구간, 교대_남부터미널_구간, 남부터미널_양재_구간);
+    전체_구간_목록 = Arrays.asList(강남_양재_구간, 교대_강남_구간, 교대_남부터미널_구간, 남부터미널_양재_구간);
   }
 
   @DisplayName("출발역에서 도착역까지 최단거리로 갈 수 있는 역들을 조회")
   @Test
   void findShortestPathTest() {
     //given
-    PathFinder pathFinder = PathFinder.init(wholeStations, wholeSections);
+    PathFinder 경로_탐색_도메인 = PathFinder.init(전체_역_목록, 전체_구간_목록);
 
     //when
-    Path shortestPath = pathFinder.findShortestPath(교대역.getId(), 양재역.getId());
+    Path 최단경로 = 경로_탐색_도메인.findShortestPath(교대역.getId(), 양재역.getId());
 
     //then
-    assertThat(shortestPath).isEqualTo(new Path(Arrays.asList(교대역, 남부터미널역, 양재역), 5D));
+    assertThat(최단경로).isEqualTo(new Path(Arrays.asList(교대역, 남부터미널역, 양재역), 5D));
   }
 
   @DisplayName("기존 최단거리역이 제거되었을 때")
@@ -63,30 +63,30 @@ public class PathFinderTest {
     Section 강남_양재_구간 = new Section(신분당선, 강남역, 양재역, Distance.from(10));
     Section 교대_강남_구간 = new Section(이호선, 교대역, 강남역, Distance.from(10));
     Section 교대_남부터미널_구간 = new Section(삼호선, 교대역, 남부터미널역, Distance.from(3));
-    List<Section> newWholeSections = new ArrayList<>();
-    newWholeSections.add(교대_강남_구간);
-    newWholeSections.add(강남_양재_구간);
-    newWholeSections.add(교대_남부터미널_구간);
-    PathFinder pathFinder = PathFinder.init(wholeStations, newWholeSections);
+    List<Section> 전체_구간_목록 = new ArrayList<>();
+    전체_구간_목록.add(교대_강남_구간);
+    전체_구간_목록.add(강남_양재_구간);
+    전체_구간_목록.add(교대_남부터미널_구간);
+    PathFinder 경로_탐색_도메인 = PathFinder.init(전체_역_목록, 전체_구간_목록);
 
     //when
-    Path shortestPath = pathFinder.findShortestPath(교대역.getId(), 양재역.getId());
+    Path 최단경로 = 경로_탐색_도메인.findShortestPath(교대역.getId(), 양재역.getId());
 
     //then
-    assertThat(shortestPath).isEqualTo(new Path(Arrays.asList(교대역, 강남역, 양재역), 20D));
+    assertThat(최단경로).isEqualTo(new Path(Arrays.asList(교대역, 강남역, 양재역), 20D));
   }
 
   @DisplayName("출발역과 도착역을 같은 역으로 조회")
   @Test
   void findShortestPathWithSingleStationTest() {
     //given
-    PathFinder pathFinder = PathFinder.init(wholeStations, wholeSections);
+    PathFinder 경로_탐색_도메인 = PathFinder.init(전체_역_목록, 전체_구간_목록);
 
     //when
-    Path shortestPath = pathFinder.findShortestPath(교대역.getId(), 교대역.getId());
+    Path 최단경로 = 경로_탐색_도메인.findShortestPath(교대역.getId(), 교대역.getId());
 
     //then
-    assertThat(shortestPath).isEqualTo(new Path(Arrays.asList(교대역), 0D));
+    assertThat(최단경로).isEqualTo(new Path(Arrays.asList(교대역), 0D));
   }
 
   @DisplayName("연결되지 않은 역과의 최단거리를 조회")
@@ -96,16 +96,16 @@ public class PathFinderTest {
     Station 서울역 = Station.stationStaticFactoryForTestCode(5L, "서울역");
     Station 용산역 = Station.stationStaticFactoryForTestCode(6L, "용산역");
     Line 일호선 = new Line("일호선", "navy", 서울역, 용산역, Distance.from(5));
-    List<Station> newWholeStations = new ArrayList<>(wholeStations);
-    newWholeStations.add(서울역);
-    newWholeStations.add(용산역);
+    List<Station> 전체_역_목록 = new ArrayList<>(this.전체_역_목록);
+    전체_역_목록.add(서울역);
+    전체_역_목록.add(용산역);
     Section 서울역_용산역_구간 = new Section(일호선, 서울역, 용산역, Distance.from(3));
-    List<Section> newWholeSections = new ArrayList<>(wholeSections);
-    newWholeSections.add(서울역_용산역_구간);
-    PathFinder pathFinder = PathFinder.init(newWholeStations, newWholeSections);
+    List<Section> 전체_구간_목록 = new ArrayList<>(this.전체_구간_목록);
+    전체_구간_목록.add(서울역_용산역_구간);
+    PathFinder 경로_탐색_도메인 = PathFinder.init(전체_역_목록, 전체_구간_목록);
 
     //when & then
-    assertThatThrownBy(() -> pathFinder.findShortestPath(교대역.getId(), 서울역.getId())).isInstanceOf(StationsNotConnectedException.class);
+    assertThatThrownBy(() -> 경로_탐색_도메인.findShortestPath(교대역.getId(), 서울역.getId())).isInstanceOf(StationsNotConnectedException.class);
   }
 
   @DisplayName("존재하지 않는 역과의 최단거리를 조회")
@@ -113,10 +113,10 @@ public class PathFinderTest {
   void findShortestPathWithNoneExistStationTest() {
     //given
     Station 서울역 = Station.stationStaticFactoryForTestCode(5L, "서울역");
-    PathFinder pathFinder = PathFinder.init(wholeStations, wholeSections);
+    PathFinder 경로_탐색_도메인 = PathFinder.init(전체_역_목록, 전체_구간_목록);
 
     //when & then
-    assertThatThrownBy(() -> pathFinder.findShortestPath(교대역.getId(), 서울역.getId())).isInstanceOf(StationNotExistException.class);
+    assertThatThrownBy(() -> 경로_탐색_도메인.findShortestPath(교대역.getId(), 서울역.getId())).isInstanceOf(StationNotExistException.class);
   }
 
 }
