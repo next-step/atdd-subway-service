a2e49b68d2b3d3c6939586bf6f5b4804db4e7b03
ted.kang <ted.kang@kakaocorp.com>
2021-06-16 01:25:50 +0900

    feat: 최단거리 계산 서비스 작성 - 서비스 테스트 코드 통과를 위한 로직 작성

--- src/main/java/nextstep/subway/path/dto/PathResponse.java
+++ src/main/java/nextstep/subway/path/dto/PathResponse.java
@@ -1,24 +1,48 @@
 package nextstep.subway.path.dto;
 
+import nextstep.subway.path.domain.Path;
 import nextstep.subway.station.dto.StationResponse;
 
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 public class PathResponse {
 
   private final List<StationResponse> stations;
-  private final Integer distance;
+  private final Double distance;
 
-  public PathResponse(List<StationResponse> stations, Integer distance) {
+  public PathResponse(List<StationResponse> stations, Double distance) {
     this.stations = stations;
     this.distance = distance;
+  }
+
+  public static PathResponse from(Path shortestPath) {
+    List<StationResponse> stationResponses = shortestPath.getStations()
+                                            .stream()
+                                            .map(StationResponse::of)
+                                            .collect(Collectors.toList());
+    return new PathResponse(stationResponses, shortestPath.getDistance());
   }
 
   public List<StationResponse> getStations() {
     return stations;
   }
 
-  public Integer getDistance() {
+  public Double getDistance() {
     return distance;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    PathResponse that = (PathResponse) o;
+    return Objects.equals(stations, that.stations) && Objects.equals(distance, that.distance);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(stations, distance);
   }
 }
