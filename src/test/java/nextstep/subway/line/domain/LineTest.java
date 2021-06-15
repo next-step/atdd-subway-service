6d3fbb015b40a831d78713592fc96df5d3e2ab88
bbbnam <bbbnam@naver.com>
2021-06-16 02:07:32 +0900

    test : 지하철 노선에 구간 추가 로직 Line객체에서 수행되도록 테스트 작성

--- src/test/java/nextstep/subway/line/domain/LineTest.java
+++ src/test/java/nextstep/subway/line/domain/LineTest.java
@@ -18,4 +18,18 @@ class LineTest {
         assertThat(이호선.getStations()).hasSize(2);
         assertThat(이호선.getStations().get(0).getName()).isEqualTo(강남역.getName());
     }
+
+    @DisplayName("지하철 노선에 구간을 추가한다.")
+    @Test
+    void addSection() {
+        Station 강남역 = new Station("강남역");
+        Station 역삼역 = new Station("역삼역");
+        Station 양재역 = new Station("양재역");
+        Line 이호선 = new Line("2호선", "green", 강남역, 역삼역, 10);
+        Section newSection = new Section(이호선, 역삼역, 양재역, 10);
+
+        이호선.addSection(newSection);
+
+        assertThat(이호선.getSections()).hasSize(2);
+    }
 }
