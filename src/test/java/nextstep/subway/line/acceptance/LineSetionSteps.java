3e8c7c387f8af9d21d8a574f7870b78351d9852b
bbbnam <bbbnam@naver.com>
2021-06-17 01:02:31 +0900

    test : 실패 상태코드 리턴 누락부분 수정

--- src/test/java/nextstep/subway/line/acceptance/LineSetionSteps.java
+++ src/test/java/nextstep/subway/line/acceptance/LineSetionSteps.java
@@ -62,6 +62,6 @@ public class LineSetionSteps {
     }
 
     public static void 지하철_노선에_지하철역_제외_실패됨(ExtractableResponse<Response> response) {
-        assertThat(response.statusCode()).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR.value());
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
     }
 }
