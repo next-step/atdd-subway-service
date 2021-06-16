29aa6e8548ca5ecffdb1d7952c339c40a03b69f2
hJJANG <dacapolife87@gmail.com>
2021-06-17 00:37:19 +0900

    feat : Line클래스에 추가될 기능 구현

--- src/main/java/nextstep/subway/line/domain/Line.java
+++ src/main/java/nextstep/subway/line/domain/Line.java
@@ -6,6 +6,8 @@ import nextstep.subway.station.domain.Station;
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.function.Predicate;
 
 @Entity
 public class Line extends BaseEntity {
@@ -52,5 +54,49 @@ public class Line extends BaseEntity {
 
     public List<Section> getSections() {
         return sections;
+    }
+
+    public Station getUpStation() {
+        return sections.get(0).getUpStation();
+    }
+
+    public Station getDownStation() {
+        return sections.get(0).getDownStation();
+    }
+
+    public boolean isEmptySection() {
+        return sections.isEmpty();
+    }
+
+    public boolean isUnableRemoveStatus() {
+        return sections.size() <= 1;
+    }
+
+    public void removeSection(Section section) {
+        sections.remove(section);
+    }
+
+    public Optional<Section> getSection(Predicate<Section> sectionPredicate) {
+        return sections.stream()
+                .filter(sectionPredicate)
+                .findFirst();
+    }
+
+    public void addSection(Section section) {
+        sections.add(section);
+    }
+
+    public void updateUpStationIfSameUpStation(Station upStation, Station downStation, int distance) {
+        sections.stream()
+                .filter(it -> it.getUpStation() == upStation)
+                .findFirst()
+                .ifPresent(it -> it.updateUpStation(downStation, distance));
+    }
+
+    public void updateDownStationIfSameDownStation(Station upStation, Station downStation, int distance) {
+        sections.stream()
+                .filter(it -> it.getDownStation() == downStation)
+                .findFirst()
+                .ifPresent(it -> it.updateDownStation(upStation, distance));
     }
 }
