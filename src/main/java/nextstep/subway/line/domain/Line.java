c9c015853a34019490c1e946f37a9d7738d1aec0
dhmin5693 <dhmin5693@naver.com>
2021-06-17 23:16:15 +0900

    refactor: 불필요 코드 정리

--- src/main/java/nextstep/subway/line/domain/Line.java
+++ src/main/java/nextstep/subway/line/domain/Line.java
@@ -25,11 +25,6 @@ public class Line extends BaseEntity {
     public Line() {
     }
 
-    public Line(String name, String color) {
-        this.name = name;
-        this.color = color;
-    }
-
     public Line(String name, String color, Station upStation, Station downStation, int distance) {
         this.name = name;
         this.color = color;
