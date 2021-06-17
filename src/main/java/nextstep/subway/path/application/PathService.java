f17dc429da9642534d32215074745dab99865180
dhmin5693 <dhmin5693@naver.com>
2021-06-18 00:01:00 +0900

    feat: findPath 뼈대 작성

---
+++ src/main/java/nextstep/subway/path/application/PathService.java
@@ -0,0 +1,12 @@
+package nextstep.subway.path.application;
+
+import nextstep.subway.path.dto.PathResponse;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PathService {
+
+    public PathResponse findPath(Long sourceId, Long targetId) {
+        return null;
+    }
+}
