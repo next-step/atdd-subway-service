697584cf504dee4833923013cfea4955f73cf1c5
hJJANG <dacapolife87@gmail.com>
2021-06-18 14:11:00 +0900

    test : Sections 생성에따른 테스트케이스 삭제및 이동

--- src/test/java/nextstep/subway/line/domain/LineTest.java
+++ src/test/java/nextstep/subway/line/domain/LineTest.java
@@ -9,58 +9,6 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class LineTest {
 
-    @DisplayName("상행역을 조회하자.")
-    @Test
-    void getUpStation() {
-        Station 강남역 = new Station("강남역");
-        Station 잠실역 = new Station("잠실역");
-        Line 지하철2호선 = new Line("2호선", "초록색", 강남역, 잠실역, 200);
-
-        assertThat(지하철2호선.getUpStation()).isEqualTo(강남역);
-    }
-
-    @DisplayName("하행역을 조회하자.")
-    @Test
-    void getDownStation() {
-        Station 강남역 = new Station("강남역");
-        Station 잠실역 = new Station("잠실역");
-        Line 지하철2호선 = new Line("2호선", "초록색", 강남역, 잠실역, 200);
-
-        assertThat(지하철2호선.getDownStation()).isEqualTo(잠실역);
-    }
-
-    @DisplayName("구간정보가 비어있는지 판단하자")
-    @Test
-    void isExist() {
-        Station 강남역 = new Station("강남역");
-        Station 잠실역 = new Station("잠실역");
-        Line 지하철2호선 = new Line("2호선", "초록색", 강남역, 잠실역, 200);
-
-        assertFalse(지하철2호선.isEmptySection());
-    }
-
-    @DisplayName("구간이 1개 이하일 경우 삭제불가능하다.")
-    @Test
-    void isUnableRemoveStatus() {
-        Station 강남역 = new Station("강남역");
-        Station 잠실역 = new Station("잠실역");
-        Line 지하철2호선 = new Line("2호선", "초록색", 강남역, 잠실역, 200);
-
-        assertTrue(지하철2호선.isUnableRemoveStatus());
-    }
-
-    @DisplayName("구간을 추가하자")
-    @Test
-    void addSection() {
-        Station 강남역 = new Station("강남역");
-        Station 삼성역 = new Station("삼성역");
-        Station 잠실역 = new Station("잠실역");
-        Line 지하철2호선 = new Line("2호선", "초록색", 강남역, 잠실역, 200);
-        Section 구간 = new Section(강남역, 삼성역, 100);
-        지하철2호선.addSection(구간);
-        assertThat(지하철2호선.getSections()).hasSize(2);
-    }
-
     @DisplayName("지하철역이 순서대로 조회되는지 확인")
     @Test
     void getStations() {
@@ -72,7 +20,6 @@ class LineTest {
         Section 구간 = new Section(강남역, 삼성역, 100);
 
         지하철2호선.addSection(구간);
-        assertThat(지하철2호선.getSections()).hasSize(2);
         assertThat(지하철2호선.getStations()).containsExactly(강남역, 삼성역, 잠실역);
     }
 }
---
+++ src/test/java/nextstep/subway/line/domain/SectionsTest.java
@@ -0,0 +1,102 @@
+package nextstep.subway.line.domain;
+
+import nextstep.subway.station.domain.Station;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+class SectionsTest {
+
+    @DisplayName("구간을 추가하자(구간이 처음추가될때)")
+    @Test
+    void addFirstSection() {
+        Station 강남역 = new Station("강남역");
+        Station 잠실역 = new Station("잠실역");
+
+        Sections sections = new Sections();
+        sections.addFirstSection(new Section(강남역, 잠실역, 100));
+
+        assertFalse(sections.isEmptySections());
+        assertThat(sections.getStations()).containsExactly(강남역, 잠실역);
+    }
+
+    @DisplayName("구간을 추가하자(구간이 추가될때)")
+    @Test
+    void addSection() {
+        Station 강남역 = new Station("강남역");
+        Station 삼성역 = new Station("삼성역");
+        Station 잠실역 = new Station("잠실역");
+
+        Sections sections = new Sections();
+        sections.addFirstSection(new Section(강남역, 잠실역, 100));
+
+        sections.addSection(new Section(강남역, 삼성역, 40));
+
+        assertFalse(sections.isEmptySections());
+        assertThat(sections.getStations()).containsExactly(강남역, 삼성역, 잠실역);
+    }
+
+    @DisplayName("존재하지않는 구간은 등록할수 없음")
+    @Test
+    void unableAddSectionNotExist() {
+        Station 강남역 = new Station("강남역");
+        Station 삼성역 = new Station("삼성역");
+        Station 종합운동장역 = new Station("종합운동장역");
+        Station 잠실역 = new Station("잠실역");
+
+        Sections sections = new Sections();
+        sections.addFirstSection(new Section(강남역, 잠실역, 100));
+
+        assertThatThrownBy(
+                ()-> sections.addSection(new Section(삼성역, 종합운동장역, 40))
+        ).isInstanceOf(RuntimeException.class);
+    }
+
+    @DisplayName("이미 등록된 구간은 등록할수 없음")
+    @Test
+    void unableAddSectionAlreadyRegist() {
+        Station 강남역 = new Station("강남역");
+
+        Station 잠실역 = new Station("잠실역");
+
+        Sections sections = new Sections();
+        sections.addFirstSection(new Section(강남역, 잠실역, 100));
+
+        assertThatThrownBy(
+                ()-> sections.addSection(new Section(강남역, 잠실역, 40))
+        ).isInstanceOf(RuntimeException.class);
+    }
+
+    @DisplayName("구간에서 역을 삭제하자")
+    @Test
+    void removeStation() {
+        Station 강남역 = new Station("강남역");
+        Station 삼성역 = new Station("삼성역");
+        Station 잠실역 = new Station("잠실역");
+
+        Sections sections = new Sections();
+        sections.addFirstSection(new Section(강남역, 잠실역, 100));
+        sections.addSection(new Section(강남역, 삼성역, 40));
+
+        sections.removeLineStation(삼성역);
+        assertFalse(sections.isEmptySections());
+        assertThat(sections.getStations()).containsExactly(강남역, 잠실역);
+    }
+
+    @DisplayName("구간이 한개밖에 존재하지 않으면 삭제할수 없음")
+    @Test
+    void removeFailStation() {
+        Station 강남역 = new Station("강남역");
+        Station 잠실역 = new Station("잠실역");
+
+        Sections sections = new Sections();
+        sections.addFirstSection(new Section(강남역, 잠실역, 100));
+
+        assertThatThrownBy(
+                ()-> sections.removeLineStation(잠실역)
+        ).isInstanceOf(RuntimeException.class);
+    }
+}
