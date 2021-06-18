530afcc852edc326cf3e66e03c62e423ebeb9270
ohgillwhan <soora_genius@naver.com>
2021-06-18 11:12:29 +0900

    test : 없는 계정으로 로그인시 실패한다

--- src/test/java/nextstep/subway/auth/acceptance/AuthAcceptanceRequest.java
+++ src/test/java/nextstep/subway/auth/acceptance/AuthAcceptanceRequest.java
@@ -16,7 +16,7 @@ import static nextstep.subway.request.AcceptanceTestRequest.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class AuthAcceptanceRequest {
-    public static Executable 로그인_요청_및_검증(TokenRequest request, AuthToken authToken) {
+    public static Executable 로그인_요청_성공됨(TokenRequest request, AuthToken authToken) {
         return () -> {
             TokenResponse tokenResponse = 로그인_요청_및_전체_검증(request);
 
@@ -24,18 +24,26 @@ public class AuthAcceptanceRequest {
         };
     }
     
-    public static Executable 로그인_요청_및_검증(TokenRequest request) {
+    public static Executable 로그인_요청_성공됨(TokenRequest request) {
         return () -> {
             로그인_요청_및_전체_검증(request);
         };
     }
 
+    public static Executable 로그인_요청_실패함(TokenRequest request) {
+        return () -> {
+            ExtractableResponse<Response> response = 로그인_요청(request);
+
+            로그인_요청_실패_검증(response);
+        };
+    }
+
+    private static void 로그인_요청_실패_검증(ExtractableResponse<Response> response) {
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.UNAUTHORIZED.value());
+    }
+
     private static TokenResponse 로그인_요청_및_전체_검증(TokenRequest request) {
-        ExtractableResponse<Response> response;
-        response = post(
-                Given.builder().contentType(ContentType.JSON).body(request).build(),
-                When.builder().uri("/login/token").build()
-        );
+        ExtractableResponse<Response> response = 로그인_요청(request);
 
         로그인_요청_응답됨(response);
 
@@ -43,6 +51,13 @@ public class AuthAcceptanceRequest {
         로그인_요청_본문_검증(tokenResponse);
 
         return tokenResponse;
+    }
+
+    private static ExtractableResponse<Response> 로그인_요청(TokenRequest request) {
+        return post(
+                Given.builder().contentType(ContentType.JSON).body(request).build(),
+                When.builder().uri("/login/token").build()
+        );
     }
 
     private static void 로그인_요청_본문_검증(TokenResponse response) {
--- src/test/java/nextstep/subway/auth/acceptance/AuthAcceptanceTest.java
+++ src/test/java/nextstep/subway/auth/acceptance/AuthAcceptanceTest.java
@@ -1,25 +1,22 @@
 package nextstep.subway.auth.acceptance;
 
-import io.restassured.RestAssured;
-import io.restassured.http.ContentType;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import nextstep.subway.AcceptanceTest;
 import nextstep.subway.auth.dto.TokenRequest;
-import nextstep.subway.auth.dto.TokenResponse;
-import nextstep.subway.member.MemberAcceptanceTest;
 import org.junit.jupiter.api.*;
-import org.junit.jupiter.api.function.Executable;
-import org.springframework.http.HttpStatus;
 
 import java.util.stream.Stream;
 
-import static nextstep.subway.auth.acceptance.AuthAcceptanceRequest.로그인_요청_및_검증;
+import static nextstep.subway.auth.acceptance.AuthAcceptanceRequest.로그인_요청_성공됨;
+import static nextstep.subway.auth.acceptance.AuthAcceptanceRequest.로그인_요청_실패함;
 import static nextstep.subway.member.MemberAcceptanceTest.*;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.DynamicTest.*;
+import static org.junit.jupiter.api.DynamicTest.dynamicTest;
 
 class AuthAcceptanceTest extends AcceptanceTest {
+
+    public static TokenRequest 등록된_계정_토큰_요청 = new TokenRequest(EMAIL, PASSWORD);
+    public static TokenRequest 등록되지_않은_계정_토큰_요청 = new TokenRequest(NEW_EMAIL, NEW_PASSWORD);
 
     @BeforeEach
     public void setUp() {
@@ -31,14 +28,18 @@ class AuthAcceptanceTest extends AcceptanceTest {
     @TestFactory
     @DisplayName("로그인을 시도한다")
     Stream<DynamicTest> 로그인을_시도한다() {
+        AuthToken authToken = new AuthToken();
         return Stream.of(
-                dynamicTest("로그인을 요청 및 검증한다", 로그인_요청_및_검증(new TokenRequest(EMAIL, PASSWORD)))
+                dynamicTest("등록된 계정으로 로그인 시도시 성공한다.", 로그인_요청_성공됨(등록된_계정_토큰_요청, authToken))
         );
     }
 
-    @DisplayName("Bearer Auth 로그인 실패")
-    @Test
-    void myInfoWithBadBearerAuth() {
+    @TestFactory
+    @DisplayName("없는 계정으로 로그인시 실패한다")
+    Stream<DynamicTest> 없는_계정으로_로그인시_실패한다() {
+        return Stream.of(
+                dynamicTest("없는 계정으로 로그인 시도시 실패한다.", 로그인_요청_실패함(등록되지_않은_계정_토큰_요청))
+        );
     }
 
     @DisplayName("Bearer Auth 유효하지 않은 토큰")
