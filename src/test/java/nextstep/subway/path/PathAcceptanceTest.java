b94474d247c31f8ba991b78a81244d0f0e22ab1d
hJJANG <dacapolife87@gmail.com>
2021-06-20 03:02:12 +0900

    test : 예외관련 인수테스트 추가

--- src/test/java/nextstep/subway/path/PathAcceptanceTest.java
+++ src/test/java/nextstep/subway/path/PathAcceptanceTest.java
@@ -30,9 +30,12 @@ public class PathAcceptanceTest extends AcceptanceTest {
     private LineResponse 신분당선;
     private LineResponse 이호선;
     private LineResponse 삼호선;
+    private LineResponse 중앙선;
     private StationResponse 강남역;
     private StationResponse 양재역;
     private StationResponse 교대역;
+    private StationResponse 덕소역;
+    private StationResponse 구리역;
     private StationResponse 남부터미널역;
 
     /**
@@ -49,11 +52,14 @@ public class PathAcceptanceTest extends AcceptanceTest {
         강남역 = StationAcceptanceTest.지하철역_등록되어_있음("강남역").as(StationResponse.class);
         양재역 = StationAcceptanceTest.지하철역_등록되어_있음("양재역").as(StationResponse.class);
         교대역 = StationAcceptanceTest.지하철역_등록되어_있음("교대역").as(StationResponse.class);
+        덕소역 = StationAcceptanceTest.지하철역_등록되어_있음("덕소역").as(StationResponse.class);
+        구리역 = StationAcceptanceTest.지하철역_등록되어_있음("구리역").as(StationResponse.class);
         남부터미널역 = StationAcceptanceTest.지하철역_등록되어_있음("남부터미널역").as(StationResponse.class);
 
         신분당선 = 지하철_노선_등록되어_있음("신분당선", "bg-red-600", 강남역, 양재역, 10);
         이호선 = 지하철_노선_등록되어_있음("이호선", "bg-red-600", 교대역, 강남역, 10);
         삼호선 = 지하철_노선_등록되어_있음("삼호선", "bg-red-600", 교대역, 양재역, 5);
+        중앙선 = 지하철_노선_등록되어_있음("중앙선", "bg-red-600", 덕소역, 구리역, 5);
 
         지하철_노선에_지하철역_등록되어_있음(삼호선, 교대역, 남부터미널역, 3);
     }
@@ -66,6 +72,36 @@ public class PathAcceptanceTest extends AcceptanceTest {
         자허철_최단경로_조회됨(response);
         지하철_경로_역명이_동일함(response);
         지하철_노선_최단경로가_일치함(response);
+    }
+
+    @DisplayName("출발역과 도착역이 같은경우는 경로조회가 안됨")
+    @Test
+    void sameStationSourceAndTarget() {
+        ExtractableResponse<Response> response = 지하철_경로를_조회(강남역, 강남역);
+
+        경로_조회가_실패됨(response);
+    }
+
+    @DisplayName("출발역과 도착역이 연결이되어 있지 않은경우는 경로조회가 안됨")
+    @Test
+    void notConnectedStationSourceAndTarget() {
+        ExtractableResponse<Response> response = 지하철_경로를_조회(강남역, 덕소역);
+
+        경로_조회가_실패됨(response);
+    }
+
+    @DisplayName("존재하지 않은 출발역이나 도착역을 조회할 경우 안됨")
+    @Test
+    void notExistStationSourceAndTarget() {
+
+        StationResponse 잠실역 = new StationResponse(100L, "잠실역");
+        ExtractableResponse<Response> response = 지하철_경로를_조회(잠실역, 강남역);
+
+        경로_조회가_실패됨(response);
+    }
+
+    private void 경로_조회가_실패됨(ExtractableResponse<Response> response) {
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
     }
 
     private void 자허철_최단경로_조회됨(ExtractableResponse<Response> response) {
