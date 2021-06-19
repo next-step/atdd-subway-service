5b2bbef95bd8ec32da51f62ab4c85820292443d0
oper912 <oper912@naver.com>
2021-06-19 18:45:44 +0900

    test: 구간 등록 테스트

--- src/test/java/nextstep/subway/line/domain/wrapper/SectionsTest.java
+++ src/test/java/nextstep/subway/line/domain/wrapper/SectionsTest.java
@@ -9,7 +9,8 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Set;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 class SectionsTest {
     private static final Line 신분당선 = new Line("신분당선", "bg-red-600");
@@ -22,6 +23,7 @@ class SectionsTest {
 
     private Sections sections;
 
+    // 강남 - 양재 - 양재시민의숲 - 청계산입구 - 판교 - 정자 - 미금 - 동천 - 수지구청 - 성복 - 상현 - 광교중앙 - 광교
     @BeforeEach
     void setUp() {
         Sections sections = new Sections();
@@ -41,5 +43,45 @@ class SectionsTest {
 
         // then
         assertThat(sortedStations).containsExactly(강남역, 양재역, 청계산입구역, 판교역, 수지구청역, 광교역);
+    }
+
+    @DisplayName("이미 등록된 역 구간을 등록할 수 없다.")
+    @Test
+    void addFail_alreadyExistsSection() {
+        // given
+        Section given1 = new Section(신분당선, 청계산입구역, 판교역, 8);
+        Section given2 = new Section(신분당선, 강남역, 광교역, 8);
+
+        // when & then
+        assertAll(
+                () -> assertThatThrownBy(() -> sections.registerNewSection(given1)).isInstanceOf(IllegalArgumentException.class),
+                () -> assertThatThrownBy(() -> sections.registerNewSection(given2)).isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @DisplayName("역 사이에 새로운 역을 등록한다.")
+    @Test
+    void registerNewSection() {
+        // given
+        Station 양재시민의숲역 = Station.of(7L, "양재시민의숲역");
+        Section given = new Section(신분당선, 양재역, 양재시민의숲역, 1);
+
+        // when
+        sections.registerNewSection(given);
+
+        // then
+        assertThat(sections.getSortedStations()).containsExactly(강남역, 양재역, 양재시민의숲역, 청계산입구역, 판교역, 수지구청역, 광교역);
+    }
+
+    @DisplayName("역 사이에 새로운 역을 등록할 때 기존의 역 간격보다 큰 간격을 등록할 수 없다.")
+    @Test
+    void registerNewSectionFail() {
+        // given
+        Station 양재시민의숲역 = Station.of(7L, "양재시민의숲역");
+        Section given = new Section(신분당선, 양재역, 양재시민의숲역, 10);
+
+        // when & then
+        assertThatThrownBy(() -> sections.registerNewSection(given))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
