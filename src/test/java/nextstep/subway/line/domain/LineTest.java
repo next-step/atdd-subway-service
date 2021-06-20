b52ab1058282135e1bfb7efeac29b3d5445a99c5
limdingdong <38725277+limdingdong@users.noreply.github.com>
2021-06-20 16:41:48 +0900

    test : 역 추가 테스트 작성

--- src/test/java/nextstep/subway/line/domain/LineTest.java
+++ src/test/java/nextstep/subway/line/domain/LineTest.java
@@ -24,4 +24,23 @@ class LineTest {
         // then
         assertThat(stations.size()).isEqualTo(2);
     }
+
+    @DisplayName("노선에 역 추가")
+    @Test
+    void addStation() {
+        // given
+        Station 강남역 = new Station("강남역");
+        Station 정자역 = new Station("정자역");
+        Line 신분당선 = new Line("신분당선", "red", 강남역, 정자역, 35);
+
+        // when
+        Station 양재역 = new Station("양재역");
+        신분당선.add(강남역, 양재역, 5);
+
+        // then
+        List<Station> stations = 신분당선.stations();
+        assertThat(stations.size()).isEqualTo(3);
+        assertThat(stations.get(0)).isEqualTo(강남역);
+        assertThat(stations.get(2)).isEqualTo(정자역);
+    }
 }
