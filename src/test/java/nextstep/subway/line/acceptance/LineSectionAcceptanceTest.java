23ca2da5a780da306de4c89a70e4d9e8220f8108
younghoon <bblnotbb@yahho.com>
2021-06-21 01:14:31 +0900

    refactor : modify acceptance test for applying line test method

--- src/test/java/nextstep/subway/line/acceptance/LineSectionAcceptanceTest.java
+++ src/test/java/nextstep/subway/line/acceptance/LineSectionAcceptanceTest.java
@@ -32,7 +32,7 @@ public class LineSectionAcceptanceTest extends AcceptanceTest {
         광교역 = StationAcceptanceTest.지하철역_등록되어_있음("광교역").as(StationResponse.class);
 
         LineRequest lineRequest = new LineRequest("신분당선", "bg-red-600", 강남역.getId(), 광교역.getId(), 10);
-        신분당선 = LineAcceptanceTest.지하철_노선_등록되어_있음(lineRequest).as(LineResponse.class);
+        신분당선 = LineTestMethod.지하철_노선_등록되어_있음(lineRequest).as(LineResponse.class);
     }
 
 	@DisplayName("지하철 구간 등록 및 삭제 시나리오")
@@ -50,7 +50,7 @@ public class LineSectionAcceptanceTest extends AcceptanceTest {
 		LineSectionTestMethod.지하철_노선에_지하철역_등록됨(lineSectionAddResponse1);
 		LineSectionTestMethod.지하철_노선에_지하철역_등록됨(lineSectionAddResponse2);
 		// When : 지하철 노선에 등록된 역 목록 조회 요청
-		ExtractableResponse<Response> findLineResponse1 = LineAcceptanceTest.지하철_노선_조회_요청(신분당선);
+		ExtractableResponse<Response> findLineResponse1 = LineTestMethod.지하철_노선_조회_요청(신분당선);
 		// Then : 등록한 지하철 구간이 반영된 역 목록이 조회됨
 		LineSectionTestMethod.지하철_노선에_지하철역_등록됨(findLineResponse1);
 		LineSectionTestMethod.지하철_노선에_지하철역_순서_정렬됨(findLineResponse1, Arrays.asList(정자역, 강남역, 양재역, 광교역));
@@ -59,7 +59,7 @@ public class LineSectionAcceptanceTest extends AcceptanceTest {
 		// Then : 지하철 구간 삭제됨
 		LineSectionTestMethod.지하철_노선에_지하철역_제외됨(removeResponse);
 		// When : 지하철 노선에 등록된 역 목록 조회 요청
-		ExtractableResponse<Response> findLineResponse2 = LineAcceptanceTest.지하철_노선_조회_요청(신분당선);
+		ExtractableResponse<Response> findLineResponse2 = LineTestMethod.지하철_노선_조회_요청(신분당선);
 		// Then : 지하철 구간이 반영된 역 목록이 조회됨
 		LineSectionTestMethod.지하철_노선에_지하철역_순서_정렬됨(findLineResponse2, Arrays.asList(정자역, 강남역, 광교역));
 	}
