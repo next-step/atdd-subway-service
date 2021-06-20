2160686538ec0229d7896d5f54847edbf511c0cd
Insup Jung <31408300+jis1218@users.noreply.github.com>
2021-06-20 20:52:36 +0900

    test(AuthAcceptanceTest): token 이용한 로그인 테스트 구현

--- src/test/java/nextstep/subway/auth/acceptance/AuthAcceptanceTest.java
+++ src/test/java/nextstep/subway/auth/acceptance/AuthAcceptanceTest.java
@@ -1,19 +1,67 @@
 package nextstep.subway.auth.acceptance;
 
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
 import nextstep.subway.AcceptanceTest;
+import nextstep.subway.auth.dto.TokenRequest;
+import nextstep.subway.auth.dto.TokenResponse;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+import static nextstep.subway.member.MemberHelper.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class AuthAcceptanceTest extends AcceptanceTest {
+
+    public static final String EMAIL = "email@email.com";
+    public static final String PASSWORD = "password";
+    public static final String WRONG_PASSWORD = "wrong_password";
+    public static final int AGE = 20;
+
+    @BeforeEach
+    void 미리_생성() {
+        회원_생성을_요청(EMAIL, PASSWORD, AGE);
+    }
 
     @DisplayName("Bearer Auth")
     @Test
     void myInfoWithBearerAuth() {
+        //given
+        TokenRequest tokenRequest = new TokenRequest(EMAIL, PASSWORD);
+
+        //when
+        ExtractableResponse<Response> response = RestAssured.given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .accept(MediaType.APPLICATION_JSON_VALUE)
+                .body(tokenRequest)
+                .when().post("/login/token")
+                .then().log().all().extract();
+
+        //then
+        assertThat(response.jsonPath().getObject(".", TokenResponse.class).getAccessToken()).isNotBlank();
     }
 
     @DisplayName("Bearer Auth 로그인 실패")
     @Test
     void myInfoWithBadBearerAuth() {
+        //given
+        TokenRequest tokenRequest = new TokenRequest(EMAIL, WRONG_PASSWORD);
+
+        //when
+        ExtractableResponse<Response> response = RestAssured.given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .accept(MediaType.APPLICATION_JSON_VALUE)
+                .body(tokenRequest)
+                .when().post("/login/token")
+                .then().log().all().extract();
+
+        //then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.UNAUTHORIZED.value());
+
     }
 
     @DisplayName("Bearer Auth 유효하지 않은 토큰")
