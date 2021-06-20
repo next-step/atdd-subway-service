95fe3c98e4c4108c91122c5c65247e5476080a1d
Insup Jung <31408300+jis1218@users.noreply.github.com>
2021-06-20 22:47:29 +0900

    refactor(AuthAcceptanceTest): 요청 메서드를 AuthHelper 클래스로 추출

--- src/test/java/nextstep/subway/auth/acceptance/AuthAcceptanceTest.java
+++ src/test/java/nextstep/subway/auth/acceptance/AuthAcceptanceTest.java
@@ -1,6 +1,5 @@
 package nextstep.subway.auth.acceptance;
 
-import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import nextstep.subway.AcceptanceTest;
@@ -12,6 +11,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
+import static nextstep.subway.auth.AuthHelper.*;
 import static nextstep.subway.member.MemberHelper.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -34,16 +34,12 @@ public class AuthAcceptanceTest extends AcceptanceTest {
         TokenRequest tokenRequest = new TokenRequest(EMAIL, PASSWORD);
 
         //when
-        ExtractableResponse<Response> response = RestAssured.given().log().all()
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .accept(MediaType.APPLICATION_JSON_VALUE)
-                .body(tokenRequest)
-                .when().post("/login/token")
-                .then().log().all().extract();
+        ExtractableResponse<Response> response = 로그인_요청(tokenRequest);
 
         //then
         assertThat(response.jsonPath().getObject(".", TokenResponse.class).getAccessToken()).isNotBlank();
     }
+
 
     @DisplayName("Bearer Auth 로그인 실패")
     @Test
@@ -52,12 +48,7 @@ public class AuthAcceptanceTest extends AcceptanceTest {
         TokenRequest tokenRequest = new TokenRequest(EMAIL, WRONG_PASSWORD);
 
         //when
-        ExtractableResponse<Response> response = RestAssured.given().log().all()
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .accept(MediaType.APPLICATION_JSON_VALUE)
-                .body(tokenRequest)
-                .when().post("/login/token")
-                .then().log().all().extract();
+        ExtractableResponse<Response> response = 로그인_요청(tokenRequest);
 
         //then
         assertThat(response.statusCode()).isEqualTo(HttpStatus.UNAUTHORIZED.value());
