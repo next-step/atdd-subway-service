4229802590ed8c28fa37f102ec04ab64d6184791
younghoon <bblnotbb@yahho.com>
2021-06-20 17:28:36 +0900

    refactor : move making station response logic into line response class

--- src/main/java/nextstep/subway/line/dto/LineResponse.java
+++ src/main/java/nextstep/subway/line/dto/LineResponse.java
@@ -1,10 +1,16 @@
 package nextstep.subway.line.dto;
 
 import nextstep.subway.line.domain.Line;
+import nextstep.subway.line.domain.Section;
+import nextstep.subway.station.domain.Station;
 import nextstep.subway.station.dto.StationResponse;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class LineResponse {
     private Long id;
@@ -29,6 +35,54 @@ public class LineResponse {
     public static LineResponse of(Line line, List<StationResponse> stations) {
         return new LineResponse(line.getId(), line.getName(), line.getColor(), stations, line.getCreatedDate(), line.getModifiedDate());
     }
+
+    public static LineResponse of(Line line) {
+		List<StationResponse> stations = getStations(line).stream()
+			.map(it -> StationResponse.of(it))
+			.collect(Collectors.toList());
+
+		return new LineResponse(line.getId(), line.getName(), line.getColor(), stations, line.getCreatedDate(), line.getModifiedDate());
+	}
+
+	private static List<Station> getStations(Line line) {
+		if (line.getSections().isEmpty()) {
+			return Arrays.asList();
+		}
+
+		List<Station> stations = new ArrayList<>();
+		Station downStation = findUpStation(line);
+		stations.add(downStation);
+
+		while (downStation != null) {
+			Station finalDownStation = downStation;
+			Optional<Section> nextLineStation = line.getSections().stream()
+				.filter(it -> it.getUpStation() == finalDownStation)
+				.findFirst();
+			if (!nextLineStation.isPresent()) {
+				break;
+			}
+			downStation = nextLineStation.get().getDownStation();
+			stations.add(downStation);
+		}
+
+		return stations;
+	}
+
+	private static Station findUpStation(Line line) {
+		Station downStation = line.getSections().get(0).getUpStation();
+		while (downStation != null) {
+			Station finalDownStation = downStation;
+			Optional<Section> nextLineStation = line.getSections().stream()
+				.filter(it -> it.getDownStation() == finalDownStation)
+				.findFirst();
+			if (!nextLineStation.isPresent()) {
+				break;
+			}
+			downStation = nextLineStation.get().getUpStation();
+		}
+
+		return downStation;
+	}
 
     public Long getId() {
         return id;
