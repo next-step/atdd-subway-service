a9828f8f829a85e37ca55797ccf49931fa8218ab
lkimilhol <lkimilhol@gmail.com>
2021-06-21 22:29:43 +0900

    feat(LineService,Line,Sections): List<Section> 제거
    - Line의 List<Section>을 사용하는 부분 모두 제거
    - Sections 클래스에 isEmpty와 size 메서드 추가
    - 임시로 Sections의 getter 생성

--- src/main/java/nextstep/subway/line/domain/Sections.java
+++ src/main/java/nextstep/subway/line/domain/Sections.java
@@ -33,6 +33,10 @@ public class Sections {
         return sections.isEmpty();
     }
 
+    public List<Section> getSections() {
+        return sections;
+    }
+
     public List<StationResponse> stationResponse() {
         return getStations().toResponse();
     }
@@ -176,5 +180,9 @@ public class Sections {
         return sections.stream()
                 .filter(section -> section.isDownStation(station))
                 .findFirst();
+    }
+
+    public int size() {
+        return sections.size();
     }
 }
