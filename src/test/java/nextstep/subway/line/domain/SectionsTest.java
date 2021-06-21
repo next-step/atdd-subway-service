3943495a88f55a249d1de7485805d757fc732cbf
lkimilhol <lkimilhol@gmail.com>
2021-06-21 19:25:13 +0900

    feat(Sections): List<Section> 일급컬렉션으로 수정
    - 기본적인 메서드 추가

---
+++ src/test/java/nextstep/subway/line/domain/SectionsTest.java
@@ -0,0 +1,35 @@
+package nextstep.subway.line.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class SectionsTest {
+    Sections sections;
+
+    @BeforeEach
+    void setup() {
+        sections = new Sections();
+    }
+
+    @DisplayName("구간 add")
+    @Test
+    void add() {
+        //given
+        //when
+        sections.add(new Section());
+        //then
+        assertThat(sections.isEmpty()).isFalse();
+    }
+
+    @DisplayName("구간 일급 컬렉션 빈 값 리턴")
+    @Test
+    void isEmpty() {
+        //given
+        //when
+        //then
+        assertThat(sections.isEmpty()).isTrue();
+    }
+}
