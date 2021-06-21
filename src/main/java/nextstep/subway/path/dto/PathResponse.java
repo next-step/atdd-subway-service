33ee3d165e8b5c26093cbeb86d621a3ab36bde9f
lenkim <joenggyu0@gmail.com>
2021-06-21 20:59:33 +0900

    feat: Path Service와 Controller 기능 구현

--- src/main/java/nextstep/subway/path/dto/PathResponse.java
+++ src/main/java/nextstep/subway/path/dto/PathResponse.java
@@ -1,17 +1,23 @@
 package nextstep.subway.path.dto;
 
+import nextstep.subway.path.domain.Path;
 import nextstep.subway.station.domain.Station;
 
 import java.util.List;
+import java.util.Objects;
 
 public class PathResponse {
 
-    List<Station> list;
-    int distance;
+    private final List<Station> list;
+    private final int distance;
 
-    public PathResponse(List<Station> list, int distance) {
-        this.list = list;
-        this.distance = distance;
+    public PathResponse(Path shortestPath) {
+        this.list = shortestPath.stations();
+        this.distance = shortestPath.distance();
+    }
+
+    public static PathResponse of(Path shortestPath) {
+        return new PathResponse(shortestPath);
     }
 
     public List<Station> getList() {
@@ -20,5 +26,26 @@ public class PathResponse {
 
     public int getDistance() {
         return distance;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        PathResponse that = (PathResponse) o;
+        return distance == that.distance && Objects.equals(list, that.list);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(list, distance);
+    }
+
+    @Override
+    public String toString() {
+        return "PathResponse{" +
+                "list=" + list +
+                ", distance=" + distance +
+                '}';
     }
 }
--- src/main/java/nextstep/subway/path/ui/PathController.java
+++ src/main/java/nextstep/subway/path/ui/PathController.java
@@ -1,5 +1,8 @@
 package nextstep.subway.path.ui;
 
+import nextstep.subway.path.application.PathService;
+import nextstep.subway.path.domain.Path;
+import nextstep.subway.path.dto.PathResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -8,12 +11,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class PathController {
 
+    private final PathService pathService;
+
+    public PathController(PathService pathService) {
+        this.pathService = pathService;
+    }
+
     @GetMapping(value = "/paths")
-    ResponseEntity getPaths(@RequestParam(value = "source") int source,
-                            @RequestParam(value = "target") int target) {
-        return null;
-
-
+    public ResponseEntity<PathResponse> getShortestPaths(@RequestParam(value = "source") int source,
+                                                         @RequestParam(value = "target") int target) {
+        Path shortestPath = pathService.findShortestPath(source, target);
+        return ResponseEntity.ok().body(PathResponse.of(shortestPath));
     }
 
 }
