1a4a626a45db81f913ee5466211c69725f2d6fb8
tmkwak <tmkwak@hmm21.com>
2021-06-22 12:54:35 +0900

    style : @BeforeEach 맨 위로 이동 (LineAcceptanceTest, LineSectionAcceptanceTest)

--- src/test/java/nextstep/subway/line/acceptance/LineAcceptanceTest.java
+++ src/test/java/nextstep/subway/line/acceptance/LineAcceptanceTest.java
@@ -28,6 +28,18 @@ public class LineAcceptanceTest extends AcceptanceTest {
 	private LineRequest lineRequest1;
 	private LineRequest lineRequest2;
 
+	@BeforeEach
+	public void setUp() {
+		super.setUp();
+
+		// given
+		강남역 = StationAcceptanceTest.지하철역_등록되어_있음("강남역").as(StationResponse.class);
+		광교역 = StationAcceptanceTest.지하철역_등록되어_있음("광교역").as(StationResponse.class);
+
+		lineRequest1 = new LineRequest("신분당선", "bg-red-600", 강남역.getId(), 광교역.getId(), 10);
+		lineRequest2 = new LineRequest("구신분당선", "bg-red-600", 강남역.getId(), 광교역.getId(), 15);
+	}
+
 	public static ExtractableResponse<Response> 지하철_노선_등록되어_있음(LineRequest params) {
 		return 지하철_노선_생성_요청(params);
 	}
@@ -131,18 +143,6 @@ public class LineAcceptanceTest extends AcceptanceTest {
 
 	public static void 지하철_노선_삭제됨(ExtractableResponse<Response> response) {
 		assertThat(response.statusCode()).isEqualTo(HttpStatus.NO_CONTENT.value());
-	}
-
-	@BeforeEach
-	public void setUp() {
-		super.setUp();
-
-		// given
-		강남역 = StationAcceptanceTest.지하철역_등록되어_있음("강남역").as(StationResponse.class);
-		광교역 = StationAcceptanceTest.지하철역_등록되어_있음("광교역").as(StationResponse.class);
-
-		lineRequest1 = new LineRequest("신분당선", "bg-red-600", 강남역.getId(), 광교역.getId(), 10);
-		lineRequest2 = new LineRequest("구신분당선", "bg-red-600", 강남역.getId(), 광교역.getId(), 15);
 	}
 
 	@DisplayName("지하철 노선을 생성한다.")
--- src/test/java/nextstep/subway/line/acceptance/LineSectionAcceptanceTest.java
+++ src/test/java/nextstep/subway/line/acceptance/LineSectionAcceptanceTest.java
@@ -32,6 +32,21 @@ public class LineSectionAcceptanceTest extends AcceptanceTest {
 	private StationResponse 성수역;
 	private StationResponse 뚝섬역;
 
+	@BeforeEach
+	public void setUp() {
+		super.setUp();
+
+		강남역 = StationAcceptanceTest.지하철역_등록되어_있음("강남역").as(StationResponse.class);
+		양재역 = StationAcceptanceTest.지하철역_등록되어_있음("양재역").as(StationResponse.class);
+		정자역 = StationAcceptanceTest.지하철역_등록되어_있음("정자역").as(StationResponse.class);
+		광교역 = StationAcceptanceTest.지하철역_등록되어_있음("광교역").as(StationResponse.class);
+		성수역 = StationAcceptanceTest.지하철역_등록되어_있음("성수역").as(StationResponse.class);
+		뚝섬역 = StationAcceptanceTest.지하철역_등록되어_있음("뚝섬역").as(StationResponse.class);
+
+		LineRequest lineRequest = new LineRequest("신분당선", "bg-red-600", 강남역.getId(), 광교역.getId(), 10);
+		신분당선 = LineAcceptanceTest.지하철_노선_등록되어_있음(lineRequest).as(LineResponse.class);
+	}
+
 	public static ExtractableResponse<Response> 지하철_노선에_지하철역_등록_요청(LineResponse line, StationResponse upStation,
 		StationResponse downStation, int distance) {
 		SectionRequest sectionRequest = new SectionRequest(upStation.getId(), downStation.getId(), distance);
@@ -81,21 +96,6 @@ public class LineSectionAcceptanceTest extends AcceptanceTest {
 
 	public static void 지하철_노선에_지하철역_제외_실패됨(ExtractableResponse<Response> response) {
 		assertThat(response.statusCode()).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR.value());
-	}
-
-	@BeforeEach
-	public void setUp() {
-		super.setUp();
-
-		강남역 = StationAcceptanceTest.지하철역_등록되어_있음("강남역").as(StationResponse.class);
-		양재역 = StationAcceptanceTest.지하철역_등록되어_있음("양재역").as(StationResponse.class);
-		정자역 = StationAcceptanceTest.지하철역_등록되어_있음("정자역").as(StationResponse.class);
-		광교역 = StationAcceptanceTest.지하철역_등록되어_있음("광교역").as(StationResponse.class);
-		성수역 = StationAcceptanceTest.지하철역_등록되어_있음("성수역").as(StationResponse.class);
-		뚝섬역 = StationAcceptanceTest.지하철역_등록되어_있음("뚝섬역").as(StationResponse.class);
-
-		LineRequest lineRequest = new LineRequest("신분당선", "bg-red-600", 강남역.getId(), 광교역.getId(), 10);
-		신분당선 = LineAcceptanceTest.지하철_노선_등록되어_있음(lineRequest).as(LineResponse.class);
 	}
 
 	@DisplayName("지하철_구간_관리")
