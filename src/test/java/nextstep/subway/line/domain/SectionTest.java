9933722655eee8188457b7225011f0d3282273e0
lkimilhol <lkimilhol@gmail.com>
2021-06-22 11:51:10 +0900

    test(SectionTest): 구간 유닛 테스트케이스 추가
    - 구간에 대한 테스트케이스 추가

---
+++ src/test/java/nextstep/subway/line/domain/SectionTest.java
@@ -0,0 +1,80 @@
+package nextstep.subway.line.domain;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import nextstep.subway.station.domain.Station;
+
+class SectionTest {
+
+    Section section;
+    Station upStation;
+    Station downStation;
+
+    @BeforeEach
+    void setup() {
+        Line line = new Line("신분당선"
+                , "빨간색"
+                , new Station("강남역")
+                , new Station("선릉역")
+                , 5
+        );
+
+        upStation = new Station("선릉역");
+        downStation = new Station("한티역");
+
+        section = new Section(line, upStation, downStation, 5);
+    }
+
+    @DisplayName("상행역 비교")
+    @Test
+    void isUpStation() {
+        //given
+        //when
+        //then
+        assertThat(section.isUpStation(upStation)).isTrue();
+    }
+
+    @DisplayName("하행역 비교")
+    @Test
+    void isDownStation() {
+        //given
+        //when
+        //then
+        assertThat(section.isDownStation(downStation)).isTrue();
+    }
+
+    @DisplayName("존재하는 역인지 확인")
+    @Test
+    void isExistsStation() {
+        //given
+        //when
+        //then
+        assertThat(section.isExistsStation(upStation)).isTrue();
+    }
+
+    @DisplayName("상행역 수정")
+    @Test
+    void updateUpStation() {
+        //given
+        Station station = new Station("임시역");
+        //when
+        section.updateUpStation(station, 3);
+        //then
+        assertThat(section.isUpStation(station)).isTrue();
+    }
+
+    @DisplayName("하행역 수정")
+    @Test
+    void updateDownStation() {
+        //given
+        Station station = new Station("임시역");
+        //when
+        section.updateDownStation(station, 3);
+        //then
+        assertThat(section.isDownStation(station)).isTrue();
+    }
+}
