6841960d592a9715d52b41d1de49f9b7598218f6
bbbnam <bbbnam@naver.com>
2021-06-23 01:24:29 +0900

    test : 최단경로 조회시 예외발생(같은역 조회시) 유효성 검사 구현

--- src/main/java/nextstep/subway/path/domain/SubwayNavigation.java
+++ src/main/java/nextstep/subway/path/domain/SubwayNavigation.java
@@ -9,14 +9,24 @@ import java.math.BigDecimal;
 import java.util.List;
 
 public class SubwayNavigation {
+    private static final String EQUAL_STATION_EXCEPTION = "같은 역으로 경로를 조회할 수 없습니다.";
+
     private final ShortestPathAlgorithm<Station, SectionEdge> shortestPath;
 
     public SubwayNavigation(WeightedGraph graph) {
+
         this.shortestPath = new DijkstraShortestPath<>(graph);
     }
 
     public List<Station> getPaths(Station source, Station target) {
+        validateEqualStation(source, target);
         return shortestPath.getPath(source, target).getVertexList();
+    }
+
+    private void validateEqualStation(Station source, Station target) {
+        if (source.equals(target)) {
+            throw new IllegalArgumentException(EQUAL_STATION_EXCEPTION);
+        }
     }
 
     public int getDistance(Station source, Station target) {
