5ef991eeb60b9ae4aa32a724d595931ac1d5acf6
oper912 <oper912@naver.com>
2021-06-22 20:26:20 +0900

    feat: 경로 및 최단 경로 조회 수행 도메인 추가

---
+++ src/main/java/nextstep/subway/path/domain/PathFinder.java
@@ -0,0 +1,80 @@
+package nextstep.subway.path.domain;
+
+import nextstep.subway.line.domain.Section;
+import nextstep.subway.line.domain.wrapper.Distance;
+import nextstep.subway.station.domain.Station;
+import org.jgrapht.GraphPath;
+import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
+import org.jgrapht.graph.DefaultWeightedEdge;
+import org.jgrapht.graph.WeightedMultigraph;
+
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.toMap;
+
+public class PathFinder {
+    private Map<Long, Station> allStations;
+    private WeightedMultigraph<Long, DefaultWeightedEdge> pathGraph;
+
+    private PathFinder(final Map<Long, Station> allStations, final WeightedMultigraph<Long, DefaultWeightedEdge> pathGraph) {
+        this.allStations = allStations;
+        this.pathGraph = pathGraph;
+    }
+
+    public static PathFinder init(final List<Station> allStations, final List<Section> allSections) {
+        WeightedMultigraph<Long, DefaultWeightedEdge> pathGraph = new WeightedMultigraph<>(DefaultWeightedEdge.class);
+        addVertex(pathGraph, allStations);
+        setEdgesWeight(pathGraph, allSections);
+        return new PathFinder(allStationsToMap(allStations), pathGraph);
+    }
+
+    private static void addVertex(final WeightedMultigraph<Long, DefaultWeightedEdge> pathGraph, final List<Station> allStations) {
+        for (Station station : allStations) {
+            pathGraph.addVertex(station.getId());
+        }
+    }
+
+    private static void setEdgesWeight(final WeightedMultigraph<Long, DefaultWeightedEdge> pathGraph, final List<Section> allSections) {
+        for (Section section : allSections) {
+            Station sourceStation = section.getUpStation();
+            Station targetStation = section.getDownStation();
+            Distance sectionDistance = section.getDistance();
+            pathGraph.setEdgeWeight(pathGraph.addEdge(sourceStation.getId(), targetStation.getId()), sectionDistance.getDistance());
+        }
+    }
+
+    private static Map<Long, Station> allStationsToMap(final List<Station> allStations) {
+        return allStations.stream()
+                .collect(toMap(Station::getId, Function.identity()));
+    }
+
+    public Path findShortestPath(final Long sourceStationId, final Long targetStationId) {
+        GraphPath<Long, DefaultWeightedEdge> shortestPath = findPathGraph(sourceStationId, targetStationId);
+        throwIfNotConnectedSection(shortestPath);
+        return new Path(getShortestPathStations(shortestPath.getVertexList()), (int) Math.round(shortestPath.getWeight()));
+    }
+
+    private GraphPath<Long, DefaultWeightedEdge> findPathGraph(final Long sourceStationId, final Long targetStationId) {
+        DijkstraShortestPath<Long, DefaultWeightedEdge> shortestPathFinder = new DijkstraShortestPath<>(pathGraph);
+        try {
+            return shortestPathFinder.getPath(sourceStationId, targetStationId);
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException("등록되어있지 않은 역으로 최단 경로를 찾을 수 없습니다.");
+        }
+    }
+
+    private void throwIfNotConnectedSection(final GraphPath<Long, DefaultWeightedEdge> graphPath) {
+        if (graphPath == null) {
+            throw new IllegalArgumentException("해당 목적지로는 연결된 구간이 없어서 이동이 불가능합니다.");
+        }
+    }
+
+    private List<Station> getShortestPathStations(final List<Long> shortestPathStationIds) {
+        return shortestPathStationIds.stream()
+                .map(stationId -> allStations.get(stationId))
+                .collect(Collectors.toList());
+    }
+}
