f7af7972626a4714a15c094352e1f12fa3116150
oper912 <oper912@naver.com>
2021-06-22 19:32:31 +0900

    test: PathSteps 요청 형태 수정

--- src/test/java/nextstep/subway/path/PathSteps.java
+++ src/test/java/nextstep/subway/path/PathSteps.java
@@ -3,19 +3,20 @@ package nextstep.subway.path;
 import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
+import nextstep.subway.path.dto.PathRequest;
 import org.springframework.http.MediaType;
 
 public class PathSteps {
 
     public static ExtractableResponse<Response> 최단_경로_조회_요청(Long sourceId, Long targetId) {
+        PathRequest params = new PathRequest(sourceId, targetId);
         return RestAssured.given().log().all()
-                .param("source", sourceId)
-                .param("target", targetId)
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .accept(MediaType.APPLICATION_JSON_VALUE)
+                .body(params)
                 .when()
                 .get("/paths")
                 .then().log().all()
                 .extract();
-
     }
 }
