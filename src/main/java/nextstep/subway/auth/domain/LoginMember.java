aa186a55d14fc7304c99ea6d6f7339e7eb7d415c
hJJANG <dacapolife87@gmail.com>
2021-06-24 01:34:19 +0900

    feat : 객체생성 방식에따른 로그인 유무 추가

--- src/main/java/nextstep/subway/auth/domain/LoginMember.java
+++ src/main/java/nextstep/subway/auth/domain/LoginMember.java
@@ -4,14 +4,17 @@ public class LoginMember {
     private Long id;
     private String email;
     private Integer age;
+    private boolean loginStatus;
 
     public LoginMember() {
+        this.loginStatus = false;
     }
 
     public LoginMember(Long id, String email, Integer age) {
         this.id = id;
         this.email = email;
         this.age = age;
+        this.loginStatus = true;
     }
 
     public Long getId() {
@@ -24,5 +27,9 @@ public class LoginMember {
 
     public Integer getAge() {
         return age;
+    }
+
+    public boolean isLogin() {
+        return loginStatus;
     }
 }
