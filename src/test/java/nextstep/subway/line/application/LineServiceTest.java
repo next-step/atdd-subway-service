be76baad13ff34af4b97382c3f33ced5c44662e6
youngjun.jin <yzzzzun@gmail.com>
2021-06-23 19:02:42 +0900

    test: removeSection 인수테스트

--- src/test/java/nextstep/subway/line/application/LineServiceTest.java
+++ src/test/java/nextstep/subway/line/application/LineServiceTest.java
@@ -33,13 +33,16 @@ class LineServiceTest {
 
 	private Station 강남역;
 	private Station 광교역;
+	private Station 양재역;
 	private Line line;
 
 	@BeforeEach
 	void setUp() {
 		this.강남역 = new Station("강남역");
 		this.광교역 = new Station("광교역");
+		this.양재역 = new Station("양재역");
 		this.line = new Line("신분당선", "red", this.강남역, this.광교역, 10);
+		this.line.addSection(양재역, 강남역, 10);
 	}
 
 	@Test
@@ -56,7 +59,7 @@ class LineServiceTest {
 		assertAll(() -> {
 			assertThat(actual.getName()).isEqualTo("신분당선");
 			assertThat(actual.getColor()).isEqualTo("red");
-			assertThat(getStationNames(actual)).containsExactly("강남역", "광교역");
+			assertThat(getStationNames(actual)).containsExactly("양재역", "강남역", "광교역");
 		});
 	}
 
@@ -72,7 +75,18 @@ class LineServiceTest {
 		Mockito.when(this.stationService.findStationById(Mockito.eq(2L))).thenReturn(this.광교역);
 		lineService.addLineStation(1L, sectionRequest);
 
-		assertThat(this.line.getStations()).containsExactly(강남역, 정자역, 광교역);
+		assertThat(this.line.getStations()).containsExactly(양재역, 강남역, 정자역, 광교역);
+	}
+
+	@Test
+	void testRemoveSection() {
+		LineService lineService = new LineService(this.lineRepository, this.stationService);
+
+		Mockito.when(this.lineRepository.findById(Mockito.eq(1L))).thenReturn(Optional.of(this.line));
+		Mockito.when(this.stationService.findStationById(Mockito.eq(1L))).thenReturn(강남역);
+
+		lineService.removeLineStation(1L, 1L);
+		assertThat(line.getStations()).containsExactly(양재역, 광교역);
 	}
 
 	private Stream<String> getStationNames(LineResponse actual) {
