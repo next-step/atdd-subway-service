ed2aff816d1aa6fe4a436f4a6c9264b4a7a20341
lkimilhol <lkimilhol@gmail.com>
2021-06-23 14:48:01 +0900

    refactor(section): 동일 비교를 동등 비교로 수정
    - equals로 비교를 수정

--- src/main/java/nextstep/subway/line/domain/Section.java
+++ src/main/java/nextstep/subway/line/domain/Section.java
@@ -107,4 +107,14 @@ public class Section {
         this.downStation = newSection.upStation;
         this.distance = distance.diff(newSection.distance);
     }
+
+    public void updateUpStation(Section newSection) {
+        this.upStation = newSection.downStation;
+        this.distance = distance.diff(newSection.distance);
+    }
+
+    public void updateDownStation(Section newSection) {
+        this.upStation = newSection.downStation;
+        this.distance = distance.diff(newSection.distance);
+    }
 }
