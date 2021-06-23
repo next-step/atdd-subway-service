26912e3a173d9c53419f3d6450c3d5a26b9e603c
hJJANG <dacapolife87@gmail.com>
2021-06-23 23:46:25 +0900

    feat : 검색된 역을 가지고 구간을 검색하여 최대 추가요금구하는 기능추가

--- src/main/java/nextstep/subway/line/application/PathService.java
+++ src/main/java/nextstep/subway/line/application/PathService.java
@@ -9,6 +9,7 @@ import nextstep.subway.station.dto.StationResponse;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.stream.Collectors;
@@ -18,10 +19,12 @@ public class PathService {
 
     private final LineRepository lineRepository;
     private final StationRepository stationRepository;
+    private final SectionRepository sectionRepository;
 
-    public PathService(LineRepository lineRepository, StationRepository stationRepository) {
+    public PathService(LineRepository lineRepository, StationRepository stationRepository, SectionRepository sectionRepository) {
         this.lineRepository = lineRepository;
         this.stationRepository = stationRepository;
+        this.sectionRepository = sectionRepository;
     }
 
     @Transactional(readOnly = true)
@@ -32,13 +35,37 @@ public class PathService {
         Station sourceStation = findStation(sourceStationId);
         Station targetStation = findStation(targetStationId);
 
-        List<StationResponse> stationResponses = pathFinder.getPath(sourceStation, targetStation)
+        List<Station> stations = pathFinder.getPath(sourceStation, targetStation);
+
+        int extraFare = getMaxExtraFare(stations);
+
+        List<StationResponse> stationResponses = stations
                 .stream()
                 .map(StationResponse::of)
                 .collect(Collectors.toList());
 
         int distance = pathFinder.getWeight(sourceStation, targetStation);
-        return new PathResponse(stationResponses, distance, new Fare(distance));
+        return new PathResponse(stationResponses, distance, new Fare(distance, extraFare));
+    }
+
+    private int getMaxExtraFare(List<Station> stations) {
+        int extraFare = 0;
+
+        List<Section> sections = sectionRepository.findAll();
+        for (int i = 1; i < stations.size(); i++) {
+            Station prevStation = stations.get(i - 1);
+            Station nextStation = stations.get(i);
+
+            extraFare = Math.max(getMaxExtraSectionLine(sections, prevStation, nextStation).getExtraFare(), extraFare);
+        }
+
+        return extraFare;
+    }
+
+    private Line getMaxExtraSectionLine(List<Section> sections, Station prevStation, Station nextStation) {
+        Section maxExtraFareSection = sections.stream().filter(section -> section.isSameSection(prevStation, nextStation))
+                .max(Comparator.comparing(section -> section.getLine().getExtraFare())).get();
+        return maxExtraFareSection.getLine();
     }
 
     private void validation(Long sourceStationId, Long targetStationId) {
