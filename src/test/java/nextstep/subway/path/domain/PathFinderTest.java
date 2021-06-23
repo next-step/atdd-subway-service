c861263857bd5260810658b3f446a3efb9ce93bb
HoonHee Jeong <jhh992000@gmail.com>
2021-06-23 18:42:25 +0900

    refactor: PathFinderTest - 테스트 픽스처 적용
    - 테스트 픽스처란?
      테스트를 위해 객체나 데이터들이 가지고 있는 변경되지 않는 상태

--- src/test/java/nextstep/subway/path/domain/PathFinderTest.java
+++ src/test/java/nextstep/subway/path/domain/PathFinderTest.java
@@ -18,29 +18,45 @@ import org.junit.jupiter.api.Test;
 @DisplayName("지하철 경로 조회 테스트")
 public class PathFinderTest {
 
-    private Station 서울역 = new Station("서울역");
-    private Station 시청역 = new Station("시청역");
-    private Station 종각역 = new Station("종각역");
-    private Station 종로3가역 = new Station("종로3가역");
-    private Station 아현역 = new Station("아현역");
-    private Station 충정로역 = new Station("충정로역");
-    private Station 을지로입구역 = new Station("을지로입구역");
-    private Station 애오개역 = new Station("애오개역");
-    private Station 서대문역 = new Station("서대문역");
-    private Station 광화문역 = new Station("광화문역");
+    private Station 서울역;
+    private Station 시청역;
+    private Station 종각역;
+    private Station 종로3가역;
+    private Station 아현역;
+    private Station 충정로역;
+    private Station 을지로입구역;
+    private Station 애오개역;
+    private Station 서대문역;
+    private Station 광화문역;
 
-    private Line 일호선 = new Line("일호선", "blue");
-    private Line 이호선 = new Line("이호선", "green");
-    private Line 오호선 = new Line("오호선", "purple");
+    private Line 일호선;
+    private Line 이호선;
+    private Line 오호선;
 
     @BeforeEach
     void setup() {
+        서울역 = new Station("서울역");
+        시청역 = new Station("시청역");
+        종각역 = new Station("종각역");
+        종로3가역 = new Station("종로3가역");
+        아현역 = new Station("아현역");
+        충정로역 = new Station("충정로역");
+        을지로입구역 = new Station("을지로입구역");
+        애오개역 = new Station("애오개역");
+        서대문역 = new Station("서대문역");
+        광화문역 = new Station("광화문역");
+
+        일호선 = new Line("일호선", "blue");
         일호선.addSection(new Section(서울역, 시청역, 10));
         일호선.addSection(new Section(시청역, 종각역, 9));
         일호선.addSection(new Section(종각역, 종로3가역, 10));
+
+        이호선 = new Line("이호선", "green");
         이호선.addSection(new Section(아현역, 충정로역, 10));
         이호선.addSection(new Section(충정로역, 시청역, 10));
         이호선.addSection(new Section(시청역, 을지로입구역, 10));
+
+        오호선 = new Line("오호선", "purple");
         오호선.addSection(new Section(애오개역, 충정로역, 10));
         오호선.addSection(new Section(충정로역, 서대문역, 10));
         오호선.addSection(new Section(서대문역, 광화문역, 10));
