5dec143fb619e92c3feed3402a3a44bf572344a7
hJJANG <dacapolife87@gmail.com>
2021-06-23 23:46:51 +0900

    feat : 요금 계산시 추가요금까지 포함

--- src/main/java/nextstep/subway/line/domain/Fare.java
+++ src/main/java/nextstep/subway/line/domain/Fare.java
@@ -8,17 +8,28 @@ public class Fare {
     private static final int STANDARD_SHORT_DISTANCE = 10;
 
     private int distance;
+    private int extraFare;
 
     public Fare(int distance) {
         this.distance = distance;
     }
 
-    public static Fare of(int distance) {
-        return new Fare(distance);
+    public Fare(int distance, int extraFare) {
+        this.distance = distance;
+        this.extraFare = extraFare;
     }
 
     public int getFare() {
         int fare = DEFAULT_FARE;
+
+        fare += calculateOverFare();
+        fare += extraFare;
+
+        return fare;
+    }
+
+    private int calculateOverFare() {
+        int fare = 0;
         if(distance > STANDARD_LONG_DISTANCE) {
             int overDistance = distance - STANDARD_LONG_DISTANCE;
             fare += calculateOverFare(overDistance, 8);
@@ -29,7 +40,6 @@ public class Fare {
             this.distance -= STANDARD_SHORT_DISTANCE;
             fare += calculateOverFare(distance, 5);
         }
-
         return fare;
     }
 
