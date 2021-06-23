99bd7bbefebfa39182d6f9447cdcb249a098e7cf
youngjun.jin <yzzzzun@gmail.com>
2021-06-23 13:29:56 +0900

    test: addSection 인수테스트

--- src/test/java/nextstep/subway/line/application/LineServiceTest.java
+++ src/test/java/nextstep/subway/line/application/LineServiceTest.java
@@ -3,6 +3,7 @@ package nextstep.subway.line.application;
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Optional;
 import java.util.stream.Stream;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -16,6 +17,7 @@ import nextstep.subway.line.domain.Line;
 import nextstep.subway.line.domain.LineRepository;
 import nextstep.subway.line.dto.LineRequest;
 import nextstep.subway.line.dto.LineResponse;
+import nextstep.subway.line.dto.SectionRequest;
 import nextstep.subway.station.application.StationService;
 import nextstep.subway.station.domain.Station;
 import nextstep.subway.station.dto.StationResponse;
@@ -29,15 +31,15 @@ class LineServiceTest {
 	@MockBean
 	private StationService stationService;
 
-	private Station upStation;
-	private Station downStation;
+	private Station 강남역;
+	private Station 광교역;
 	private Line line;
 
 	@BeforeEach
 	void setUp() {
-		this.upStation = new Station("강남역");
-		this.downStation = new Station("광교역");
-		this.line = new Line("신분당선", "red", upStation, downStation, 10);
+		this.강남역 = new Station("강남역");
+		this.광교역 = new Station("광교역");
+		this.line = new Line("신분당선", "red", this.강남역, this.광교역, 10);
 	}
 
 	@Test
@@ -45,8 +47,8 @@ class LineServiceTest {
 		LineService lineService = new LineService(this.lineRepository, this.stationService);
 		LineRequest lineRequest = new LineRequest("신분당선", "red", 1L, 2L, 10);
 
-		Mockito.when(this.stationService.findById(Mockito.eq(1L))).thenReturn(upStation);
-		Mockito.when(this.stationService.findById(Mockito.eq(2L))).thenReturn(downStation);
+		Mockito.when(this.stationService.findById(Mockito.eq(1L))).thenReturn(this.강남역);
+		Mockito.when(this.stationService.findById(Mockito.eq(2L))).thenReturn(this.광교역);
 		Mockito.when(this.lineRepository.save(Mockito.any())).thenReturn(line);
 
 		LineResponse actual = lineService.saveLine(lineRequest);
@@ -56,6 +58,21 @@ class LineServiceTest {
 			assertThat(actual.getColor()).isEqualTo("red");
 			assertThat(getStationNames(actual)).containsExactly("강남역", "광교역");
 		});
+	}
+
+	@Test
+	void testAddSection() {
+		LineService lineService = new LineService(this.lineRepository, this.stationService);
+		SectionRequest sectionRequest = new SectionRequest(3L, 2L, 5);
+
+		Station 정자역 = new Station("정자역");
+
+		Mockito.when(this.lineRepository.findById(Mockito.eq(1L))).thenReturn(Optional.of(this.line));
+		Mockito.when(this.stationService.findStationById(Mockito.eq(3L))).thenReturn(정자역);
+		Mockito.when(this.stationService.findStationById(Mockito.eq(2L))).thenReturn(this.광교역);
+		lineService.addLineStation(1L, sectionRequest);
+
+		assertThat(this.line.getStations()).containsExactly(강남역, 정자역, 광교역);
 	}
 
 	private Stream<String> getStationNames(LineResponse actual) {
