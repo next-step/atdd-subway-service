651c04942e03bb84e1af772fb27beff3c05b562f
ted.kang <ted.kang@kakaocorp.com>
2021-06-25 01:45:03 +0900

    test: 경로의 요금 계산값에 노선의 추가요금이 적용 되었는지 확인하는 테스트

--- src/test/java/nextstep/subway/path/domain/fee/RequireCalculatedFeeTest.java
+++ src/test/java/nextstep/subway/path/domain/fee/RequireCalculatedFeeTest.java
@@ -1,9 +1,15 @@
 package nextstep.subway.path.domain.fee;
 
+import nextstep.subway.line.domain.Fee;
+import nextstep.subway.path.domain.Path;
+import nextstep.subway.station.domain.Station;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
+import java.util.Arrays;
+
+import static nextstep.subway.station.domain.Station.stationStaticFactoryForTestCode;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class RequireCalculatedFeeTest {
@@ -12,7 +18,24 @@ class RequireCalculatedFeeTest {
   @CsvSource(value = {"0:0", "10:1250", "11:1350", "20:1450", "50:2050", "51:2150", "60:2250", "100:2750"}, delimiter = ':')
   @ParameterizedTest
   void requireFeeTest(double givenDistance, long expectFee) {
-    assertThat(RequireFee.getRequireFee(givenDistance)).isEqualTo(expectFee);
+    //given
+    Station givenStation = stationStaticFactoryForTestCode(1L, "교대역");
+    Path givenPath = new Path(Arrays.asList(givenStation), givenDistance, Fee.ZERO_FEE);
+
+    //when & then
+    assertThat(RequireFee.getRequireFee(givenPath)).isEqualTo(expectFee);
+  }
+
+  @DisplayName("추가 요금을 더한 만큼의 요금이 반환된다.")
+  @CsvSource(value = {"400:1650", "900:2150", "0:1250"}, delimiter = ':')
+  @ParameterizedTest
+  void additionalFeeTest(long givenAdditionalFee, long expectFee) {
+    //given
+    Station givenStation = stationStaticFactoryForTestCode(1L, "교대역");
+    Path givenPath = new Path(Arrays.asList(givenStation), 1D, Fee.from(givenAdditionalFee));
+
+    //when & then
+    assertThat(RequireFee.getRequireFee(givenPath)).isEqualTo(expectFee);
   }
 
 }
