aab00795ae198512272e4a2b3ccbc58c3d8e3a48
youngjun.jin <yzzzzun@gmail.com>
2021-06-24 16:09:29 +0900

    test: 최단 경로 반환 인수테스트 구현

---
+++ src/main/java/nextstep/subway/path/ui/PathController.java
@@ -0,0 +1,25 @@
+package nextstep.subway.path.ui;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import nextstep.subway.path.application.PathService;
+import nextstep.subway.path.dto.PathResponse;
+
+@RestController
+public class PathController {
+
+	private final PathService pathService;
+
+	public PathController(PathService pathService) {
+		this.pathService = pathService;
+	}
+
+	@GetMapping("/paths")
+	public ResponseEntity<PathResponse> getShortestPath(@RequestParam long source, @RequestParam long target) {
+		PathResponse shortestPath = this.pathService.findShortestPath(source, target);
+		return ResponseEntity.ok().body(shortestPath);
+	}
+}
---
+++ src/test/java/nextstep/subway/path/application/PathServiceTest.java
@@ -0,0 +1,17 @@
+package nextstep.subway.path.application;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+class PathServiceTest {
+
+	@DisplayName("최단 경로 찾기 테스트")
+	@Test
+	void testFindShortestPath() {
+
+	}
+
+}
