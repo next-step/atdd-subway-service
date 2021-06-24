5ec249ee71bf82e18aeb0fc36836880107bf57b9
fDevJc <fDevJc@gmail.com>
2021-06-25 02:04:46 +0900

    test: LineTest, SectionsTest 추가

---
+++ src/test/java/nextstep/subway/line/domain/SectionsTest.java
@@ -0,0 +1,45 @@
+package nextstep.subway.line.domain;
+
+import nextstep.subway.station.domain.Station;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class SectionsTest {
+    private Line line;
+    private Station firstStation;
+    private Station secondStation;
+    private Station thirdStation;
+    private Station forthStation;
+
+    @BeforeEach
+    void setUp() {
+        firstStation = new Station("1번역");
+        secondStation = new Station("2번역");
+        line = new Line("1번노선", "색깔", firstStation, secondStation, 10);
+        thirdStation = new Station("3번역");
+        forthStation = new Station("4번역");
+    }
+
+    @Test
+    void add() {
+        line.addLineStation(secondStation, thirdStation, 4);
+        assertThat(line.getStations()).isEqualTo(Arrays.asList(firstStation, secondStation, thirdStation));
+    }
+
+    @Test
+    void add_existentStation() {
+        assertThatThrownBy(() -> line.addLineStation(firstStation, secondStation, 4))
+                .isInstanceOf(RuntimeException.class);
+    }
+
+    @Test
+    void add_non_existentStation() {
+        assertThatThrownBy(() -> line.addLineStation(thirdStation, forthStation, 4))
+                .isInstanceOf(RuntimeException.class);
+    }
+}
