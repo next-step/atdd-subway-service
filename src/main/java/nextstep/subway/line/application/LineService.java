1ee75dc7d7d909bdb2113a9b1c0aa39d1bc3d5d6
limdingdong <38725277+limdingdong@users.noreply.github.com>
2021-06-25 08:39:29 +0900

    feat : findPaths api 레이아웃 작성

--- src/main/java/nextstep/subway/line/application/LineService.java
+++ src/main/java/nextstep/subway/line/application/LineService.java
@@ -4,6 +4,7 @@ import nextstep.subway.line.domain.Line;
 import nextstep.subway.line.domain.LineRepository;
 import nextstep.subway.line.dto.LineRequest;
 import nextstep.subway.line.dto.LineResponse;
+import nextstep.subway.line.dto.PathResponse;
 import nextstep.subway.line.dto.SectionRequest;
 import nextstep.subway.station.application.StationService;
 import nextstep.subway.station.domain.Station;
@@ -11,6 +12,7 @@ import nextstep.subway.station.dto.StationResponse;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -74,5 +76,9 @@ public class LineService {
         Line line = findLineById(lineId);
         Station station = stationService.findStationById(stationId);
         line.remove(station);
+    }
+
+    public PathResponse findPaths(Long sourceStationId, Long targetStationId) {
+        return new PathResponse(Collections.emptyList());
     }
 }
