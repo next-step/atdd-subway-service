707344e9eff33dc59615450563f7995b8f1a57ea
ted.kang <ted.kang@kakaocorp.com>
2021-06-25 02:29:21 +0900

    refactor: 연령별 할인 정책 적용을 위해 Authorization header가 있을 때만 동작하는 handlerMethod 추가

--- src/main/java/nextstep/subway/path/ui/PathController.java
+++ src/main/java/nextstep/subway/path/ui/PathController.java
@@ -1,5 +1,7 @@
 package nextstep.subway.path.ui;
 
+import nextstep.subway.auth.domain.AuthenticationPrincipal;
+import nextstep.subway.auth.domain.LoginMember;
 import nextstep.subway.path.application.PathService;
 import nextstep.subway.path.dto.PathResponse;
 import org.springframework.http.MediaType;
@@ -19,10 +21,18 @@ public class PathController {
     this.pathService = pathService;
   }
 
-  @GetMapping(consumes = {MediaType.APPLICATION_JSON_VALUE})
-  public ResponseEntity<PathResponse> findShortestPath(@RequestParam("source") Long sourceStationId, @RequestParam("target") Long targetStationId) {
+  @GetMapping(consumes = {MediaType.APPLICATION_JSON_VALUE}, headers = {"Authorization"})
+  public ResponseEntity<PathResponse> findShortestPath(@AuthenticationPrincipal LoginMember loginMember,
+                                                       @RequestParam("source") Long sourceStationId,
+                                                       @RequestParam("target") Long targetStationId) {
     PathResponse shortestPath = pathService.findShortestPath(sourceStationId, targetStationId);
     return ResponseEntity.ok(shortestPath);
   }
 
+  @GetMapping(consumes = {MediaType.APPLICATION_JSON_VALUE})
+  public ResponseEntity<PathResponse> findShortestPath(@RequestParam("source") Long sourceStationId,
+                                                       @RequestParam("target") Long targetStationId) {
+    PathResponse shortestPath = pathService.findShortestPath(sourceStationId, targetStationId);
+    return ResponseEntity.ok(shortestPath);
+  }
 }
