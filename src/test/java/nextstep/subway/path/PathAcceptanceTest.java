bc0f74f8a08995523462a6f50b10f76133e04b0b
ted.kang <ted.kang@kakaocorp.com>
2021-06-25 03:43:12 +0900

    test: 로그인 회원 연령별 할인 인수테스트

--- src/test/java/nextstep/subway/path/PathAcceptanceTest.java
+++ src/test/java/nextstep/subway/path/PathAcceptanceTest.java
@@ -4,6 +4,7 @@ import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import nextstep.subway.AcceptanceTest;
+import nextstep.subway.auth.dto.TokenResponse;
 import nextstep.subway.line.dto.LineRequest;
 import nextstep.subway.line.dto.LineResponse;
 import nextstep.subway.path.dto.PathResponse;
@@ -19,8 +20,10 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import static nextstep.subway.auth.acceptance.AuthAcceptanceTest.로그인_요청;
 import static nextstep.subway.line.acceptance.LineAcceptanceTest.지하철_노선_등록되어_있음;
 import static nextstep.subway.line.acceptance.LineSectionAcceptanceTest.지하철_노선에_지하철역_등록_요청;
+import static nextstep.subway.member.MemberAcceptanceTest.*;
 import static nextstep.subway.station.StationAcceptanceTest.지하철역_등록되어_있음;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -129,6 +132,56 @@ public class PathAcceptanceTest extends AcceptanceTest {
     최단거리_조회됨(모든_노선_추가요금_경로_탐색_결과, Arrays.asList(시작역, 중간역, 새끝역), 10D, 2_250);
   }
 
+  @DisplayName("연령별 할인 정책 경로를 조회")
+  @Test
+  void findPathWithAgeDiscountTest() {
+    //given 성인회원 가입
+    ExtractableResponse<Response> 성인_회원 = 회원_생성을_요청(EMAIL, PASSWORD, AGE);
+
+    //when 성인회원이 로그인 후 경로 탐색
+    TokenResponse 성인_회원_로그인 = 로그인_요청(EMAIL, PASSWORD).as(TokenResponse.class);
+    ExtractableResponse<Response> 성인_회원_경로_탐색_결과 = 로그인_유저의_최단거리_경로_탐색(성인_회원_로그인.getAccessToken(), 교대역, 양재역);
+
+    //then 기본적인 요금(1,250 원)이 조회된다.
+    최단거리_조회됨(성인_회원_경로_탐색_결과, Arrays.asList(교대역, 남부터미널역, 양재역), 5D, 1_250);
+
+    //given 청소년 회원 가입
+    String teenagerEmail = "teen@teen.com";
+    String teenagerPassword = "teen";
+    ExtractableResponse<Response> 청소년_회원 = 회원_생성을_요청(teenagerEmail, teenagerPassword, 15);
+
+    //when 청소년 회원이 로그인 후 경로 탐색
+    TokenResponse 청소년_회원_로그인 = 로그인_요청(teenagerEmail, teenagerPassword).as(TokenResponse.class);
+    ExtractableResponse<Response> 청소년_회원_경로_탐색_결과 = 로그인_유저의_최단거리_경로_탐색(청소년_회원_로그인.getAccessToken(), 교대역, 양재역);
+
+    //then 기본 요금에서 350원을 공제한 금액에 20%를 할인한 금액이 조회된다.
+    최단거리_조회됨(청소년_회원_경로_탐색_결과, Arrays.asList(교대역, 남부터미널역, 양재역), 5D, 720);
+
+    //given 어린이 회원 가입
+    String childrenEmail = "children@children.com";
+    String childrenPassword = "children";
+    ExtractableResponse<Response> 어린이_회원 = 회원_생성을_요청(childrenEmail, childrenPassword, 9);
+
+    //when 어린이 회원이 로그인 후 경로 탐색
+    TokenResponse 어린이_회원_로그인 = 로그인_요청(childrenEmail, childrenPassword).as(TokenResponse.class);
+    ExtractableResponse<Response> 어린이_회원_경로_탐색_결과 = 로그인_유저의_최단거리_경로_탐색(어린이_회원_로그인.getAccessToken(), 교대역, 양재역);
+
+    //then 기본 요금에서 350원을 공제한 금액에 50%를 할인한 금액이 조회된다.
+    최단거리_조회됨(어린이_회원_경로_탐색_결과, Arrays.asList(교대역, 남부터미널역, 양재역), 5D, 450);
+
+    //given 영유아 회원 가입
+    String toddlerEmail = "toddler@toddler.com";
+    String toddlerPassword = "toddler";
+    ExtractableResponse<Response> 영유아_회원 = 회원_생성을_요청(toddlerEmail, toddlerPassword, 4);
+
+    //when 영유아 회원이 로그인 후 경로 탐색
+    TokenResponse 영유아_회원_로그인 = 로그인_요청(toddlerEmail, toddlerPassword).as(TokenResponse.class);
+    ExtractableResponse<Response> 영유아_회원_경로_탐색_결과 = 로그인_유저의_최단거리_경로_탐색(영유아_회원_로그인.getAccessToken(), 교대역, 양재역);
+
+    //then 0원 조회 (무료)
+    최단거리_조회됨(영유아_회원_경로_탐색_결과, Arrays.asList(교대역, 남부터미널역, 양재역), 5D, 0);
+  }
+
   @DisplayName("경로 탐색 시 최단거리로 갈 수있는 경로를 출발역부터 도착역까지 순서로 반환한다.")
   @Test
   void findPathTest() {
@@ -220,6 +273,17 @@ public class PathAcceptanceTest extends AcceptanceTest {
             .get("/paths")
             .then().log().all()
             .extract();
+  }
 
+  private ExtractableResponse<Response> 로그인_유저의_최단거리_경로_탐색(String token, StationResponse source, StationResponse target) {
+    return RestAssured.given().log().all()
+        .auth().oauth2(token)
+        .queryParam("source", source.getId())
+        .queryParam("target", target.getId())
+        .contentType(MediaType.APPLICATION_JSON_VALUE)
+        .when()
+        .get("/paths")
+        .then().log().all()
+        .extract();
   }
 }
