008979c351f49cef095e7e033416d8f306a19ebe
hJJANG <dacapolife87@gmail.com>
2021-06-25 00:43:16 +0900

    refactor : 불필요한 로그인상태여부 제거

--- src/main/java/nextstep/subway/auth/domain/LoginMember.java
+++ src/main/java/nextstep/subway/auth/domain/LoginMember.java
@@ -4,17 +4,14 @@ public class LoginMember {
     private Long id;
     private String email;
     private Integer age;
-    private boolean loginStatus;
 
     public LoginMember() {
-        this.loginStatus = false;
     }
 
     public LoginMember(Long id, String email, Integer age) {
         this.id = id;
         this.email = email;
         this.age = age;
-        this.loginStatus = true;
     }
 
     public Long getId() {
@@ -27,9 +24,5 @@ public class LoginMember {
 
     public Integer getAge() {
         return age;
-    }
-
-    public boolean isLogin() {
-        return loginStatus;
     }
 }
