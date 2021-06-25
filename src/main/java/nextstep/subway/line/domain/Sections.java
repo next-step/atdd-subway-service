fd19c644c8987997c3ab8440f748c7308705144d
fDevJc <fDevJc@gmail.com>
2021-06-25 23:09:04 +0900

    refactor: 불필요 메서드 삭제

--- src/main/java/nextstep/subway/line/domain/Sections.java
+++ src/main/java/nextstep/subway/line/domain/Sections.java
@@ -15,10 +15,6 @@ public class Sections {
     @OneToMany(mappedBy = "line", cascade = {CascadeType.PERSIST, CascadeType.MERGE}, orphanRemoval = true)
     private List<Section> sections = new ArrayList<>();
 
-    public void add(Section section) {
-        sections.add(section);
-    }
-
     public List<Station> getStations() {
         if (sections.isEmpty()) {
             return Collections.emptyList();
