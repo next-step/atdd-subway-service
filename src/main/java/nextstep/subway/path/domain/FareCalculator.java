a745825ea29f5e6e239563cedf7fcb7426c35bd9
Insup Jung <31408300+jis1218@users.noreply.github.com>
2021-06-25 23:11:57 +0900

    refactor(DistanceFare): enum을 이용한 나이에 따른 요금 정보 클래스 및 기능 추가
     - 의존성 주입을 enum을 통해 함

--- src/main/java/nextstep/subway/path/domain/FareCalculator.java
+++ src/main/java/nextstep/subway/path/domain/FareCalculator.java
@@ -1,13 +1,7 @@
 package nextstep.subway.path.domain;
 
-import nextstep.subway.path.domain.age_policy.AgeFarePolicy;
-import nextstep.subway.path.domain.age_policy.BasicAgeFarePolicy;
-import nextstep.subway.path.domain.age_policy.ChildAgeFarePolicy;
-import nextstep.subway.path.domain.age_policy.TeenAgeFarePolicy;
-import nextstep.subway.path.domain.distance_policy.BasicDistanceFarePolicy;
-import nextstep.subway.path.domain.distance_policy.DistanceFarePolicy;
-import nextstep.subway.path.domain.distance_policy.Over10KmDistanceFarePolicy;
-import nextstep.subway.path.domain.distance_policy.Over50KmDistanceFarePolicy;
+import nextstep.subway.path.domain.age_policy.*;
+import nextstep.subway.path.domain.distance_policy.*;
 
 import java.util.Set;
 
@@ -25,27 +19,11 @@ public class FareCalculator {
     }
 
     private AgeFarePolicy setAgeFarePolicy(int age) {
-        if (6 <= age && age < 13) {
-            return new ChildAgeFarePolicy();
-        }
-
-        if (13 <= age && age < 19) {
-            return new TeenAgeFarePolicy();
-        }
-
-        return new BasicAgeFarePolicy();
+        return AgeFareInformation.ageFarePolicy(age);
     }
 
     private DistanceFarePolicy setDistanceFarePolicy(int distance) {
-        if (distance <= 10) {
-            return new BasicDistanceFarePolicy();
-        }
-
-        if (distance <= 50) {
-            return new Over10KmDistanceFarePolicy(distance);
-        }
-
-        return new Over50KmDistanceFarePolicy(distance);
+        return DistanceFareInformation.distanceFarePolicy(distance);
     }
 
     public int calculate(Set<Long> lineIds) {
