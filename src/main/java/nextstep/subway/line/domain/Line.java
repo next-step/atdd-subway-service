ffa274a9776039e69448cafa36f22fb723459d5d
dohchoi91 <dohchoi91@gmail.com>
2021-06-26 00:44:12 +0900

    refactor: 구간 추가 개선

--- src/main/java/nextstep/subway/line/domain/Line.java
+++ src/main/java/nextstep/subway/line/domain/Line.java
@@ -1,6 +1,7 @@
 package nextstep.subway.line.domain;
 
 import nextstep.subway.BaseEntity;
+import nextstep.subway.line.dto.SectionRequest;
 import nextstep.subway.station.domain.Station;
 
 import javax.persistence.*;
@@ -93,5 +94,42 @@ public class Line extends BaseEntity {
             downStation = nextLineStation.get().getUpStation();
         }
         return downStation;
+    }
+    public void addStation(Station upStation, Station downStation, int distance) {
+        List<Station> stations = getStations();
+        boolean isUpStationExisted = stations.stream().anyMatch(it -> it == upStation);
+        boolean isDownStationExisted = stations.stream().anyMatch(it -> it == downStation);
+
+        if (isUpStationExisted && isDownStationExisted) {
+            throw new RuntimeException("이미 등록된 구간 입니다.");
+        }
+
+        if (!stations.isEmpty() && stations.stream().noneMatch(it -> it == upStation) &&
+            stations.stream().noneMatch(it -> it == downStation)) {
+            throw new RuntimeException("등록할 수 없는 구간 입니다.");
+        }
+
+        if (stations.isEmpty()) {
+            getSections().add(new Section(this, upStation, downStation, distance));
+            return;
+        }
+
+        if (isUpStationExisted) {
+            getSections().stream()
+                .filter(it -> it.getUpStation() == upStation)
+                .findFirst()
+                .ifPresent(it -> it.updateUpStation(downStation, distance));
+
+            getSections().add(new Section(this, upStation, downStation, distance));
+        } else if (isDownStationExisted) {
+            getSections().stream()
+                .filter(it -> it.getDownStation() == downStation)
+                .findFirst()
+                .ifPresent(it -> it.updateDownStation(upStation, distance));
+
+            getSections().add(new Section(this, upStation, downStation, distance));
+        } else {
+            throw new RuntimeException();
+        }
     }
 }
--- src/test/java/nextstep/subway/line/domain/LineTest.java
+++ src/test/java/nextstep/subway/line/domain/LineTest.java
@@ -13,6 +13,8 @@ import nextstep.subway.station.domain.Station;
 class LineTest {
 
     Station 강남역 = new Station("강남역");
+    Station 양재역 = new Station("양재역");
+    Station 정자역 = new Station("정자역");
     Station 광교역 = new Station("광교역");
 
     @Test
@@ -26,5 +28,19 @@ class LineTest {
 
         //then
         assertThat(stations).containsAll(Arrays.asList(강남역, 광교역));
+    }
+
+    @DisplayName("지하철 구간을 등록한다.")
+    @Test
+    void addLineSection() {
+        // given
+        Line 신분당선 = new Line("신분당선", "bg-red-600", 강남역, 광교역, 10);
+        신분당선.addStation(강남역, 양재역, 3);
+
+        // when
+        List<Station> stations = 신분당선.getStations();
+
+        // then
+        assertThat(stations).containsAll(Arrays.asList(강남역, 양재역, 광교역));
     }
 }
