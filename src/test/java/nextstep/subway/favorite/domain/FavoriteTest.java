8f0576f330a0b2ed452be063aa79f8adef8bff1e
HoonHee Jeong <jhh992000@gmail.com>
2021-06-26 09:44:09 +0900

    refactor: 즐겨찾기 생성시 출발역과 도착역이 같은 경우 예외발생, 테스트 추가

--- src/test/java/nextstep/subway/favorite/domain/FavoriteTest.java
+++ src/test/java/nextstep/subway/favorite/domain/FavoriteTest.java
@@ -3,21 +3,30 @@ package nextstep.subway.favorite.domain;
 import static nextstep.subway.station.domain.StationFixtures.광화문역;
 import static nextstep.subway.station.domain.StationFixtures.서대문역;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import nextstep.subway.member.domain.Member;
+import nextstep.subway.path.exception.SameOriginAndDestinationException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 @DisplayName("즐겨찾기 테스트")
 public class FavoriteTest {
 
+    private Member member = new Member(1L, "jhh992000@gmail.com", "1234", 39);
+
     @DisplayName("자신의 즐겨찾기 여부 확인")
     @Test
     void delete() {
-        Member member = new Member(1L, "jhh992000@gmail.com", "1234", 39);
         Favorite favorite = new Favorite(1L, member, 서대문역, 광화문역);
 
         assertThat(favorite.isOwner(2L)).isFalse();
+    }
+
+    @DisplayName("출발역과 도착역이 같은경우 예외 발생")
+    @Test
+    void sameOriginAndDestination() {
+        assertThatThrownBy(() -> new Favorite(member, 서대문역, 서대문역)).isInstanceOf(SameOriginAndDestinationException.class);
     }
 
 }
