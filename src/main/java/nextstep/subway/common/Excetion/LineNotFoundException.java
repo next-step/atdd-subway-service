63d80e4ca0e314fbae3b9383a757a78855217a52
Lee-Chungsun <cndtjs0218@naver.com>
2021-06-26 20:45:28 +0900

    refactor : 상수 제거

--- src/main/java/nextstep/subway/common/Excetion/LineNotFoundException.java
+++ src/main/java/nextstep/subway/common/Excetion/LineNotFoundException.java
@@ -1,10 +1,7 @@
 package nextstep.subway.common.Excetion;
 
 public class LineNotFoundException extends RuntimeException {
-
-    public static final String NOT_REGISTERED_LINE = "등록되지 않은 노선입니다.";
-
     public LineNotFoundException() {
-        super(NOT_REGISTERED_LINE);
+        super("등록되지 않은 노선입니다.");
     }
 }
--- src/main/java/nextstep/subway/common/Excetion/StationNotFoundException.java
+++ src/main/java/nextstep/subway/common/Excetion/StationNotFoundException.java
@@ -1,9 +1,7 @@
 package nextstep.subway.common.Excetion;
 
 public class StationNotFoundException extends RuntimeException {
-    public static final String NOT_REGISTERED_STATION = "등록되지 않은 역입니다.";
-
     public StationNotFoundException() {
-        super(NOT_REGISTERED_STATION);
+        super("등록되지 않은 역입니다.");
     }
 }
