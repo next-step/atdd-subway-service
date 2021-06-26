cd6ec8e003331c6d4f66f04496536abab1937098
limdingdong <38725277+limdingdong@users.noreply.github.com>
2021-06-26 17:16:17 +0900

    feat : PathFinder 클래스 신규작성

---
+++ src/main/java/nextstep/subway/line/domain/PathFinder.java
@@ -0,0 +1,51 @@
+package nextstep.subway.line.domain;
+
+import nextstep.subway.station.domain.Station;
+import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
+import org.jgrapht.graph.DefaultWeightedEdge;
+import org.jgrapht.graph.WeightedMultigraph;
+
+import java.util.List;
+
+public class PathFinder {
+
+    private WeightedMultigraph<Station, DefaultWeightedEdge> graph = new WeightedMultigraph(DefaultWeightedEdge.class);
+
+    public PathFinder(List<Line> lines) {
+        addVertex(lines);
+        addEdge(lines);
+    }
+
+    private void addVertex(List<Line> lines) {
+        for (Line line : lines) {
+            addVertex(line);
+        }
+    }
+
+    private void addVertex(Line line) {
+        for (Station station : line.stations()) {
+            graph.addVertex(station);
+        }
+    }
+
+    private void addEdge(List<Line> lines) {
+        for (Line line : lines) {
+            addEdge(line.sections());
+        }
+    }
+
+    private void addEdge(Sections sections) {
+        for (Section section : sections) {
+            setEdgeWeight(section);
+        }
+    }
+
+    private void setEdgeWeight(Section section) {
+        graph.setEdgeWeight(graph.addEdge(section.getUpStation(), section.getDownStation()), section.getDistance());
+    }
+
+    public List<Station> findPaths(Station source, Station target) {
+        DijkstraShortestPath dijkstraShortestPath = new DijkstraShortestPath(graph);
+        return dijkstraShortestPath.getPath(source, target).getVertexList();
+    }
+}
