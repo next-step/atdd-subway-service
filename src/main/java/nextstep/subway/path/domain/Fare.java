663b9307f0e67839d6c43b8c1dee73f890abc35d
김석진 <7271kim@naver.com>
2021-06-26 13:19:58 +0900

    feature : [Step4] FareTest 통과하도록 기능구현

--- src/main/java/nextstep/subway/path/domain/Fare.java
+++ src/main/java/nextstep/subway/path/domain/Fare.java
@@ -1,6 +1,7 @@
 package nextstep.subway.path.domain;
 
 public class Fare {
+    private static final int BASE_FARE = 1250;
 
     private int distance;
 
@@ -17,7 +18,23 @@ public class Fare {
     }
 
     public int getPrice() {
-        return 0;
+        int fare = BASE_FARE;
+        int firstDistance = distance;
+        Surcharge surcharge = Surcharge.findSurchargeByDistance(firstDistance);
+        while (surcharge != Surcharge.NONE) {
+            int distanceBoundary = surcharge.getDistanceBoundary();
+            int addDistance = surcharge.getAddDistance();
+            int addFare = surcharge.getAddFare();
+            fare += calculateOverFare(firstDistance - distanceBoundary, addDistance, addFare);
+            firstDistance = distanceBoundary;
+            surcharge = Surcharge.findSurchargeByDistance(firstDistance);
+        }
+
+        return fare;
+    }
+
+    private int calculateOverFare(int distance, int divisor, int addFare) {
+        return (int)((Math.ceil((distance) / divisor)) * addFare);
     }
 
 }
