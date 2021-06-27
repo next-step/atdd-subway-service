1a8a405022871ce5cd937a126cfd4747cbb27e35
wrallee <wrallee@naver.com>
2021-06-27 20:20:28 +0900

    feat(auth): Bearer 인증 기능 구현

--- src/test/java/nextstep/subway/auth/acceptance/AuthAcceptanceTest.java
+++ src/test/java/nextstep/subway/auth/acceptance/AuthAcceptanceTest.java
@@ -14,6 +14,7 @@ import io.restassured.response.Response;
 import nextstep.subway.AcceptanceTest;
 import nextstep.subway.auth.dto.TokenRequest;
 import nextstep.subway.auth.dto.TokenResponse;
+import nextstep.subway.member.dto.MemberResponse;
 
 @DisplayName("Auth 관련 인수 테스트")
 public class AuthAcceptanceTest extends AcceptanceTest {
@@ -22,14 +23,26 @@ public class AuthAcceptanceTest extends AcceptanceTest {
     @Test
     void myInfoWithBearerAuth() {
         // given
-        회원_생성_되어_있음(EMAIL, PASSWORD, AGE);
+        ExtractableResponse<Response> joinResponse
+            = 회원_생성_되어_있음(EMAIL, PASSWORD, AGE);
 
         // when
-        ExtractableResponse<Response> response
+        ExtractableResponse<Response> loginResponse
             = 회원_로그인_요청(EMAIL, PASSWORD);
 
+        // when
+        ExtractableResponse<Response> memberInfoResponse
+            = 회원_정보_조회(loginResponse);
+
         // then
-        회원_로그인_토큰_응답됨(response);
+        회원_정보_조회됨(memberInfoResponse);
+    }
+
+    private void 회원_정보_조회됨(ExtractableResponse<Response> memberInfoResponse) {
+        assertThat(memberInfoResponse.statusCode()).isEqualTo(HttpStatus.OK.value());
+        MemberResponse member = memberInfoResponse.as(MemberResponse.class);
+        assertThat(member.getAge()).isEqualTo(AGE);
+        assertThat(member.getEmail()).isEqualTo(EMAIL);
     }
 
     @DisplayName("Bearer Auth 로그인 실패")
@@ -46,25 +59,47 @@ public class AuthAcceptanceTest extends AcceptanceTest {
     @DisplayName("Bearer Auth 유효하지 않은 토큰")
     @Test
     void myInfoWithWrongBearerAuth() {
+        // when
+        ExtractableResponse<Response> response
+            = 회원_정보_조회_잘못된_토큰();
+
+        // then
+        회원_정보_조회_실패(response);
+    }
+
+    private ExtractableResponse<Response> 회원_정보_조회(ExtractableResponse<Response> response) {
+        TokenResponse tokenResponse = response.as(TokenResponse.class);
+        return RestAssured.given().log().all()
+            .auth().oauth2(tokenResponse.getAccessToken())
+            .accept(MediaType.APPLICATION_JSON_VALUE)
+            .when().get("/members/me")
+            .then().log().all().extract();
+    }
+
+    private ExtractableResponse<Response> 회원_정보_조회_잘못된_토큰() {
+        TokenResponse tokenResponse = new TokenResponse("INVALID_TOKEN");
+        return RestAssured.given().log().all()
+            .auth().oauth2(tokenResponse.getAccessToken())
+            .accept(MediaType.APPLICATION_JSON_VALUE)
+            .when().get("/members/me")
+            .then().log().all().extract();
     }
 
     private ExtractableResponse<Response> 회원_로그인_요청(String email, String password) {
         TokenRequest request = new TokenRequest(email, password);
-        ExtractableResponse<Response> response = RestAssured
+        return RestAssured
             .given().log().all()
             .contentType(MediaType.APPLICATION_JSON_VALUE)
             .body(request)
             .when().post("/login/token")
             .then().log().all().extract();
-        return response;
-    }
-
-    private void 회원_로그인_토큰_응답됨(ExtractableResponse<Response> response) {
-        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
-        assertThat(response.as(TokenResponse.class).getAccessToken()).isNotBlank();
     }
 
     private void 회원_로그인_실패(ExtractableResponse<Response> response) {
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.UNAUTHORIZED.value());
+    }
+
+    private void 회원_정보_조회_실패(ExtractableResponse<Response> response) {
         assertThat(response.statusCode()).isEqualTo(HttpStatus.UNAUTHORIZED.value());
     }
 }
