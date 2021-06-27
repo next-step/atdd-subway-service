1c44217f2eaafb37d2267c246402dc43d9754ba7
bbbnam <bbbnam@naver.com>
2021-06-27 22:21:12 +0900

    test : 유효하지 않은 토큰 테스트

--- src/test/java/nextstep/subway/auth/AuthSteps.java
+++ src/test/java/nextstep/subway/auth/AuthSteps.java
@@ -1,11 +1,15 @@
 package nextstep.subway.auth;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -25,6 +29,19 @@ public class AuthSteps {
                 .post("/login/token")
                 .then().log().all()
                 .extract();
+    }
+
+    public static String 만료기간_지난_토큰_발급_요청(String email, String password) {
+        Claims claims = Jwts.claims().setSubject(email);
+        Date now = new Date();
+        Date validity = new Date(now.getTime() - 1);
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(now)
+                .setExpiration(validity)
+                .signWith(SignatureAlgorithm.HS256, "testSecretKey")
+                .compact();
     }
 
     public static void 로그인_됨(ExtractableResponse<Response> response) {
--- src/test/java/nextstep/subway/auth/acceptance/AuthAcceptanceTest.java
+++ src/test/java/nextstep/subway/auth/acceptance/AuthAcceptanceTest.java
@@ -35,12 +35,24 @@ public class AuthAcceptanceTest extends AcceptanceTest {
         로그인_실패됨(response);
     }
 
-    @DisplayName("Bearer Auth 유효하지 않은 토큰")
+    @DisplayName("Bearer Auth 유효하지 않은 토큰1 - 이상한 값(파싱할때 예외발생하는 경우)")
     @Test
-    void myInfoWithWrongBearerAuth() {
+    void myInfoWithWrongBearerAuth1() {
         TestToken token = new TestToken("ABC.ABC.123");
 
         ExtractableResponse<Response> response = 내_정보_조회_요청_유효하지_않은_토큰(token);
+
+        내_정보_조회_실패됨(response);
+    }
+
+    @DisplayName("Bearer Auth 유효하지 않은 토큰2 - 만료시간 지난 토큰")
+    @Test
+    void myInfoWithWrongBearerAuth2() {
+        회원_등록_되어_있음(EMAIL, PASSWORD, AGE);
+        String initToken = 만료기간_지난_토큰_발급_요청(EMAIL, PASSWORD);
+        TestToken testToken = new TestToken(initToken);
+
+        ExtractableResponse<Response> response = 내_정보_조회_요청_유효하지_않은_토큰(testToken);
 
         내_정보_조회_실패됨(response);
     }
