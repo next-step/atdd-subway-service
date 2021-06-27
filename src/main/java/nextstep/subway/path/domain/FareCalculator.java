d0d622b22f62c7c6dfcd529afb2beadd40a7d100
HoonHee Jeong <jhh992000@gmail.com>
2021-06-28 03:58:57 +0900

    refactor: 운임 계산기 매직넘버 상수처리

--- src/main/java/nextstep/subway/path/domain/FareCalculator.java
+++ src/main/java/nextstep/subway/path/domain/FareCalculator.java
@@ -4,6 +4,11 @@ import nextstep.subway.auth.domain.LoginMember;
 
 public class FareCalculator {
 
+    public static final int FIRST_ADDITION_BASE_DISTANCE = 10;
+    public static final int FIRST_ADDITION_INTERVAL = 5;
+    public static final int SECOND_ADDITION_BASE_DISTANCE = 50;
+    public static final int SECOND_ADDITION_INTERVAL = 8;
+
     private final FarePolicy farePolicy;
 
     public FareCalculator(FarePolicy farePolicy) {
@@ -23,24 +28,33 @@ public class FareCalculator {
         if (fare.isFree()) {
             return fare;
         }
-        int addFare = calculateAdditionFare(distance, surcharge);
-        return fare.plus(addFare)
+        return fare.plus(calculateAdditionFare(distance, surcharge))
             .minus(farePolicy.getDiscountAmount())
             .applyDiscountRate(farePolicy.getDiscountRate());
     }
 
-
     private int calculateAdditionFare(int distance, int surcharge) {
-        if (distance > 10 && distance <= 50) {
-            return surcharge + calculateOverFare(5, distance - 10);
+        if (isFirstAdditionDistance(distance)) {
+            return surcharge
+                + calculateOverFare(distance - FIRST_ADDITION_BASE_DISTANCE, FIRST_ADDITION_INTERVAL);
         }
-        if (distance > 50) {
-            return surcharge + (calculateOverFare(5, 50 - 10) + calculateOverFare(8, distance - 50));
+        if (isSecondAdditionDistance(distance)) {
+            return surcharge
+                + calculateOverFare(SECOND_ADDITION_BASE_DISTANCE - FIRST_ADDITION_BASE_DISTANCE, FIRST_ADDITION_INTERVAL)
+                + calculateOverFare(distance - SECOND_ADDITION_BASE_DISTANCE, SECOND_ADDITION_INTERVAL);
         }
         return surcharge;
     }
 
-    private int calculateOverFare(int interval, int distance) {
-        return ((distance - 1) / interval + 1) * 100;
+    private boolean isFirstAdditionDistance(int distance) {
+        return distance > FIRST_ADDITION_BASE_DISTANCE && distance <= SECOND_ADDITION_BASE_DISTANCE;
+    }
+
+    private boolean isSecondAdditionDistance(int distance) {
+        return distance > SECOND_ADDITION_BASE_DISTANCE;
+    }
+
+    private int calculateOverFare(int distance, int interval) {
+        return (((distance - 1) / interval) + 1) * 100;
     }
 }
