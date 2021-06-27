6390fc5d809728693b925a5ff9491b5143750c82
limdingdong <38725277+limdingdong@users.noreply.github.com>
2021-06-27 16:38:29 +0900

    feat : Exception Handling 로직 추가

--- src/main/java/nextstep/subway/line/ui/PathController.java
+++ src/main/java/nextstep/subway/line/ui/PathController.java
@@ -2,6 +2,7 @@ package nextstep.subway.line.ui;
 
 import nextstep.subway.line.application.LineService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,5 +19,10 @@ public class PathController {
     @GetMapping("/paths")
     public ResponseEntity findPaths(@RequestParam Long source, @RequestParam Long target) {
         return ResponseEntity.ok(lineService.findPaths(source, target));
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity handleIllegalArgsException(IllegalArgumentException e) {
+        return ResponseEntity.badRequest().build();
     }
 }
