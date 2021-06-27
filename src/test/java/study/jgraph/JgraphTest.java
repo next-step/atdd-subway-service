e759f742f1eea05dbadd9397605256325277a525
fDevJc <fDevJc@gmail.com>
2021-06-28 02:08:42 +0900

    test: 학습테스트 변경

--- src/test/java/study/jgraph/JgraphTest.java
+++ src/test/java/study/jgraph/JgraphTest.java
@@ -1,5 +1,6 @@
 package study.jgraph;
 
+import nextstep.subway.station.domain.Station;
 import org.jgrapht.GraphPath;
 import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
 import org.jgrapht.alg.shortestpath.KShortestPaths;
@@ -7,6 +8,7 @@ import org.jgrapht.graph.DefaultWeightedEdge;
 import org.jgrapht.graph.WeightedMultigraph;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -21,12 +23,34 @@ public class JgraphTest {
         graph.addVertex("v2");
         graph.addVertex("v3");
         graph.setEdgeWeight(graph.addEdge("v1", "v2"), 2);
+        graph.setEdgeWeight(graph.addEdge("v1", "v2"), 2);
         graph.setEdgeWeight(graph.addEdge("v2", "v3"), 2);
-        graph.setEdgeWeight(graph.addEdge("v1", "v3"), 100);
+        graph.setEdgeWeight(graph.addEdge("v1", "v3"), 5);
 
         DijkstraShortestPath dijkstraShortestPath = new DijkstraShortestPath(graph);
         List<String> shortestPath = dijkstraShortestPath.getPath(source, target).getVertexList();
 
+        assertThat(shortestPath.size()).isEqualTo(3);
+    }
+
+    @Test
+    public void getDijkstraShortestPathWithStation() {
+        Station source = new Station("1번");
+        Station target = new Station("3번");
+        WeightedMultigraph<Station, DefaultWeightedEdge> graph = new WeightedMultigraph(DefaultWeightedEdge.class);
+        graph.addVertex(new Station("1번"));
+        graph.addVertex(new Station("2번"));
+        graph.addVertex(new Station("3번"));
+        graph.setEdgeWeight(graph.addEdge(new Station("1번"), new Station("2번")), 2);
+        graph.setEdgeWeight(graph.addEdge(new Station("1번"), new Station("2번")), 2);
+        graph.setEdgeWeight(graph.addEdge(new Station("2번"), new Station("3번")), 2);
+        graph.setEdgeWeight(graph.addEdge(new Station("1번"), new Station("3번")), 5);
+
+        DijkstraShortestPath dijkstraShortestPath = new DijkstraShortestPath(graph);
+        List<String> shortestPath = dijkstraShortestPath.getPath(source, target).getVertexList();
+
+        assertThat(shortestPath).isEqualTo(Arrays.asList(new Station("1번"),new Station("2번"),new Station("3번")));
+        assertThat(dijkstraShortestPath.getPath(source, target).getWeight()).isEqualTo(4);
         assertThat(shortestPath.size()).isEqualTo(3);
     }
 
