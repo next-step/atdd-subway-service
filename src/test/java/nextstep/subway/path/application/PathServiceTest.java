38f8545c936b4bae4a67cc68fdda64c605b299f1
ttuop <plintcsz@gmail.com>
2021-06-27 23:48:30 +0900

    test: PathServiceTest 테스트 보완

--- src/test/java/nextstep/subway/path/application/PathServiceTest.java
+++ src/test/java/nextstep/subway/path/application/PathServiceTest.java
@@ -22,6 +22,7 @@ import nextstep.subway.path.dto.PathResponse;
 import nextstep.subway.station.domain.Station;
 import nextstep.subway.station.domain.StationRepository;
 
+@SuppressWarnings("NonAsciiCharacters")
 @ExtendWith(MockitoExtension.class)
 public class PathServiceTest {
 
@@ -33,33 +34,35 @@ public class PathServiceTest {
 
     private final Station 강남역 = new Station("강남역");
     private final Station 역삼역 = new Station("역삼역");
-    private final int distance = 100;
+    private final Station 신도림역 = new Station("신도림역");
+    private final Station 양재역 = new Station("양재역");
+    private final Station 교대역 = new Station("교대역");
     private List<Station> stations;
-    private List<Section> sections;
+    private Line line;
 
     @BeforeEach
     void setUp() {
-
         stations = new ArrayList<>();
         stations.add(강남역);
         stations.add(역삼역);
+        stations.add(양재역);
+        stations.add(교대역);
 
-        final Line line = new Line("2호선", "green", 강남역, 역삼역, distance);
-
-        sections = new ArrayList<>();
-        sections.add(new Section(line, 강남역, 역삼역, distance));
+        line = new Line("2호선", "green", 강남역, 역삼역, 1);
+        line.addSection(new Section(line, 역삼역, 양재역, 2));
+        line.addSection(new Section(line, 양재역, 교대역, 3));
+        line.addSection(new Section(line, 교대역, 신도림역, 5));
     }
 
     @DisplayName("최단 경로 초회 기능 테스트")
     @Test
     void findShortestPath() {
         // given
-        when(stationRepository.findAll()).thenReturn(stations);
-        when(sectionRepository.findAll()).thenReturn(sections);
+        when(sectionRepository.findAll()).thenReturn(line.getSections());
         final long source = 1;
         final long target = 2;
         when(stationRepository.findById(source)).thenReturn(Optional.ofNullable(강남역));
-        when(stationRepository.findById(target)).thenReturn(Optional.ofNullable(역삼역));
+        when(stationRepository.findById(target)).thenReturn(Optional.ofNullable(교대역));
         final PathService pathService = new PathService(stationRepository, sectionRepository);
 
         // when
@@ -67,7 +70,7 @@ public class PathServiceTest {
 
         // then
         assertAll(
-            () -> assertThat(pathResponse.getDistance()).isEqualTo(distance),
+            () -> assertThat(pathResponse.getDistance()).isEqualTo(6),
             () -> assertThat(pathResponse.getStations()).isEqualTo(stations)
         );
     }
