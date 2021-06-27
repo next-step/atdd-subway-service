62a366ab915826b1c94f7d715ba7318b96552c67
limdingdong <38725277+limdingdong@users.noreply.github.com>
2021-06-27 16:54:13 +0900

    test : 인수테스트 작성 (경로조회 실패 케이스 추가)  - 출발역과 도착역이 동일한 경우  - 출발역과 도착역이 연결되어 있지 않은 경우

--- src/test/java/nextstep/subway/path/PathAcceptanceTest.java
+++ src/test/java/nextstep/subway/path/PathAcceptanceTest.java
@@ -31,10 +31,13 @@ public class PathAcceptanceTest extends AcceptanceTest {
     private LineResponse 신분당선;
     private LineResponse 이호선;
     private LineResponse 삼호선;
+    private LineResponse 구호선;
     private StationResponse 강남역;
     private StationResponse 양재역;
     private StationResponse 교대역;
     private StationResponse 남부터미널역;
+    private StationResponse 신논현역;
+    private StationResponse 언주역;
 
     /**
      * 교대역    --- *2호선* ---   강남역
@@ -51,10 +54,13 @@ public class PathAcceptanceTest extends AcceptanceTest {
         양재역 = 지하철역_등록되어_있음("양재역").as(StationResponse.class);
         교대역 = 지하철역_등록되어_있음("교대역").as(StationResponse.class);
         남부터미널역 = 지하철역_등록되어_있음("남부터미널역").as(StationResponse.class);
+        신논현역 = 지하철역_등록되어_있음("신논현역").as(StationResponse.class);
+        언주역 = 지하철역_등록되어_있음("언주역").as(StationResponse.class);
 
         신분당선 = 지하철_노선_등록되어_있음("신분당선", "bg-red-600", 강남역.getId(), 양재역.getId(), 10);
-        이호선 = 지하철_노선_등록되어_있음("이호선", "bg-red-600", 교대역.getId(), 강남역.getId(), 10);
-        삼호선 = 지하철_노선_등록되어_있음("삼호선", "bg-red-600", 교대역.getId(), 양재역.getId(), 5);
+        이호선 = 지하철_노선_등록되어_있음("이호선", "bg-green-600", 교대역.getId(), 강남역.getId(), 10);
+        삼호선 = 지하철_노선_등록되어_있음("삼호선", "bg-orange-600", 교대역.getId(), 양재역.getId(), 5);
+        구호선 = 지하철_노선_등록되어_있음("구호선", "bg-gold-600", 신논현역.getId(), 언주역.getId(), 7);
 
         지하철_노선에_지하철역_등록_요청(삼호선, 교대역, 남부터미널역, 3);
     }
@@ -76,6 +82,26 @@ public class PathAcceptanceTest extends AcceptanceTest {
         // when
         StationResponse 미등록역 = new StationResponse(999L, "미등록역", LocalDateTime.now(), LocalDateTime.now());
         ExtractableResponse<Response> response = 최단경로_조회_요청(양재역, 미등록역);
+
+        // then
+        최단경로_조회_실패됨(response);
+    }
+
+    @DisplayName("최단경로 조회 - 출발역과 도착역이 동일한 경우")
+    @Test
+    void findPaths_sourceAndTargetAreSame() {
+        // when
+        ExtractableResponse<Response> response = 최단경로_조회_요청(양재역, 양재역);
+
+        // then
+        최단경로_조회_실패됨(response);
+    }
+
+    @DisplayName("최단경로 조회 - 출발역과 도착역이 연결되어 있지 않은 경우")
+    @Test
+    void findPaths_sourceAndTargetAreNotConnected() {
+        // when
+        ExtractableResponse<Response> response = 최단경로_조회_요청(양재역, 신논현역);
 
         // then
         최단경로_조회_실패됨(response);
