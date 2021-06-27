22d1f3ce775a5c590f0db850fe55ee4d03b8b506
jimin.joo <wnwlals22@naver.com>
2021-06-27 19:44:01 +0900

    test: 최단 경로 찾기 예외 상황 테스트 추가

--- src/test/java/nextstep/subway/path/domain/PathFinderTest.java
+++ src/test/java/nextstep/subway/path/domain/PathFinderTest.java
@@ -1,5 +1,6 @@
 package nextstep.subway.path.domain;
 
+import nextstep.subway.exception.CustomException;
 import nextstep.subway.line.domain.Line;
 import nextstep.subway.station.domain.Station;
 import org.junit.jupiter.api.BeforeEach;
@@ -9,8 +10,9 @@ import org.junit.jupiter.api.Test;
 import java.util.Arrays;
 import java.util.List;
 
+import static nextstep.subway.exception.CustomExceptionMessage.NOT_CONNECTED_SOURCE_AND_TARGET_STATION;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class PathFinderTest {
 
@@ -18,7 +20,9 @@ class PathFinderTest {
     private Station 양재역;
     private Station 교대역;
     private Station 남부터미널역;
-
+    private Station 혼자역;
+    private Station 독립역;
+    private Station 유령역;
 
     private List<Line> lines;
 
@@ -28,17 +32,22 @@ class PathFinderTest {
         양재역 = new Station("양재역");
         교대역 = new Station("교대역");
         남부터미널역 = new Station("남부터미널역");
+        혼자역 = new Station("혼자역");
+        독립역 = new Station("독립역");
+        유령역 = new Station("나는 유령이다");
 
         Line 이호선 = new Line("이호선" ,"RED");
         Line 삼호선 = new Line("삼호선", "GREEN");
         Line 신분당선 = new Line("신분당선", "BLUE");
+        Line 나홀로선 = new Line("나홀로선", "GRAY");
 
         이호선.addSection(교대역, 강남역, 10);
         삼호선.addSection(남부터미널역, 양재역, 2);
         삼호선.addSection(교대역, 남부터미널역, 3);
         신분당선.addSection(강남역, 양재역, 10);
+        나홀로선.addSection(혼자역, 독립역, 20);
 
-        this.lines = Arrays.asList(이호선, 삼호선, 신분당선);
+        this.lines = Arrays.asList(이호선, 삼호선, 신분당선, 나홀로선);
     }
 
 
@@ -53,5 +62,30 @@ class PathFinderTest {
             .isNotEmpty()
             .containsExactly(남부터미널역, 양재역, 강남역);
         assertThat(paths.getTotalDistance()).isEqualTo(12);
+    }
+
+    @DisplayName("연결 되어있지 않은 두 역의 경로를 조회")
+    @Test
+    void checkConnectedBetweenStationsTest() {
+        // when
+        assertThatThrownBy(() -> PathFinder.of(lines).getShortestPaths(남부터미널역, 혼자역))
+            .isInstanceOf(CustomException.class)
+            .hasMessageContaining(NOT_CONNECTED_SOURCE_AND_TARGET_STATION.getMessage());
+    }
+
+    @DisplayName("출발역이 등록되지 않은 경우")
+    @Test
+    void checkNotExistedSourceStation() {
+        assertThatThrownBy(() -> PathFinder.of(lines).getShortestPaths(유령역, 남부터미널역))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessageContaining("graph must contain the source vertex");
+    }
+
+    @DisplayName("도착역이 등록되지 않은 경우")
+    @Test
+    void checkNotExistedTargetStation() {
+        assertThatThrownBy(() -> PathFinder.of(lines).getShortestPaths(남부터미널역, 유령역))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessageContaining("graph must contain the sink vertex");
     }
 }
