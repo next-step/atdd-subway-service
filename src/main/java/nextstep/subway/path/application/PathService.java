2cb284c5b4e881edb0c34e674d9f8c6eecde3bfd
Donghui-Lee <ehdgml3206@naver.com>
2021-06-28 05:43:54 +0900

    refactor: 최단경로 조회 서비스 적용

--- src/main/java/nextstep/subway/path/application/PathService.java
+++ src/main/java/nextstep/subway/path/application/PathService.java
@@ -1,11 +1,22 @@
 package nextstep.subway.path.application;
 
+import static java.util.stream.Collectors.*;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
+
+import org.jgrapht.GraphPath;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import nextstep.subway.line.domain.LineRepository;
+import nextstep.subway.line.domain.Section;
+import nextstep.subway.path.domain.PathFinder;
 import nextstep.subway.path.dto.PathResponse;
+import nextstep.subway.station.domain.Station;
 import nextstep.subway.station.domain.StationRepository;
+import nextstep.subway.station.dto.StationResponse;
 
 @Service
 @Transactional
@@ -19,6 +30,20 @@ public class PathService {
     }
 
     public PathResponse findPath(Long source, Long target) {
-    	return null;
+        final Station sourceStation = stationRepository.findById(source)
+            .orElseThrow(NoSuchElementException::new);
+        final Station targetStation = stationRepository.findById(target)
+            .orElseThrow(NoSuchElementException::new);
+        final List<Section> allSections = lineRepository.findAll().stream()
+            .flatMap(line -> line.getSections().stream())
+            .collect(toList());
+
+        PathFinder pathFinder = PathFinder.of(allSections);
+        GraphPath path = pathFinder.findShortestPath(sourceStation, targetStation);
+        List<Station> stations = path.getVertexList();
+
+        return PathResponse.of(stations.stream()
+            .map(StationResponse::of)
+            .collect(Collectors.toList()), path.getWeight());
     }
 }
--- src/main/java/nextstep/subway/path/dto/PathResponse.java
+++ src/main/java/nextstep/subway/path/dto/PathResponse.java
@@ -1,6 +1,27 @@
 package nextstep.subway.path.dto;
 
+import java.util.List;
+
+import nextstep.subway.station.dto.StationResponse;
+
 public class PathResponse {
-    protected PathResponse() {
+    private List<StationResponse> stations;
+    private Long distance;
+
+    protected PathResponse(List<StationResponse> stations, Long distance) {
+        this.stations = stations;
+        this.distance = distance;
+    }
+
+    public static PathResponse of(List<StationResponse> stations, double distance) {
+        return new PathResponse(stations, (long) distance);
+    }
+
+    public List<StationResponse> getStations() {
+        return stations;
+    }
+
+    public Long getDistance() {
+        return distance;
     }
 }
