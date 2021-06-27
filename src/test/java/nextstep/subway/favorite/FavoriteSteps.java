e3affc0ed2e60bddc7429d563c220344cec5ea08
bbbnam <bbbnam@naver.com>
2021-06-27 23:50:36 +0900

    test : 즐겨찾기 생성 테스트 리팩토링

---
+++ src/test/java/nextstep/subway/favorite/FavoriteSteps.java
@@ -0,0 +1,30 @@
+package nextstep.subway.favorite;
+
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import nextstep.subway.auth.acceptance.TestToken;
+import nextstep.subway.favorite.dto.FavoriteRequest;
+import nextstep.subway.station.dto.StationResponse;
+import org.springframework.http.HttpStatus;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class FavoriteSteps {
+
+    public static ExtractableResponse<Response> 즐겨찾기_생성을_요청(TestToken token, StationResponse source, StationResponse target) {
+        FavoriteRequest favoriteRequest = new FavoriteRequest(source.getId(), target.getId());
+        return RestAssured.given().log().all()
+                .auth()
+                .oauth2(token.getAccessToken())
+                .body(favoriteRequest)
+                .when()
+                .post("/favorites")
+                .then().log().all()
+                .extract();
+    }
+
+    public static void 즐겨찾기_생성됨(ExtractableResponse<Response> response) {
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+    }
+}
