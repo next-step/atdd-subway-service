fcf7dc6413c3aabf5d94c4bede75eb6bb4ad2cb8
bbbnam <bbbnam@naver.com>
2021-06-27 21:04:51 +0900

    test : 등록되지 않은 회원 정보로 로그인시 로그인 실패됨

--- src/test/java/nextstep/subway/auth/AuthSteps.java
+++ src/test/java/nextstep/subway/auth/AuthSteps.java
@@ -3,6 +3,8 @@ package nextstep.subway.auth;
 import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
+import nextstep.subway.auth.dto.TokenResponse;
+import org.assertj.core.api.Assertions;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
@@ -12,6 +14,7 @@ import java.util.Map;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class AuthSteps {
+
     public static ExtractableResponse<Response> 로그인_요청(String email, String password) {
         Map<String, Object> params = new HashMap<>();
         params.put("email", email);
@@ -26,7 +29,18 @@ public class AuthSteps {
                 .extract();
     }
 
-    public static void 로그인_됨(ExtractableResponse<Response> response) {
-        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+    public static void 로그인_됨(TokenResponse tokenResponse) {
+        RestAssured.given().log().all()
+                .auth()
+                .oauth2(tokenResponse.getAccessToken())
+                .accept(MediaType.APPLICATION_JSON_VALUE)
+                .when()
+                .get("/members/me")
+                .then().log().all()
+                .statusCode(HttpStatus.OK.value());
+    }
+
+    public static void 로그인_실패됨(ExtractableResponse<Response> response) {
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.UNAUTHORIZED.value());
     }
 }
--- src/test/java/nextstep/subway/auth/acceptance/AuthAcceptanceTest.java
+++ src/test/java/nextstep/subway/auth/acceptance/AuthAcceptanceTest.java
@@ -4,6 +4,7 @@ import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import nextstep.subway.AcceptanceTest;
 import nextstep.subway.auth.application.AuthServiceTest;
+import nextstep.subway.auth.dto.TokenResponse;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -20,14 +21,17 @@ public class AuthAcceptanceTest extends AcceptanceTest {
     void myInfoWithBearerAuth() {
         회원_등록_되어_있음(EMAIL, PASSWORD, AGE);
 
-        ExtractableResponse<Response> response = 로그인_요청(EMAIL, PASSWORD);
+        TokenResponse tokenResponse = 로그인_요청(EMAIL, PASSWORD).as(TokenResponse.class);
         
-        로그인_됨(response);
+        로그인_됨(tokenResponse);
     }
 
-    @DisplayName("Bearer Auth 로그인 실패")
+    @DisplayName("Bearer Auth 로그인 실패 - 등록되지 않은 회원 정보로 로그인시")
     @Test
     void myInfoWithBadBearerAuth() {
+        ExtractableResponse<Response> response = 로그인_요청(EMAIL, PASSWORD);
+
+        로그인_실패됨(response);
     }
 
     @DisplayName("Bearer Auth 유효하지 않은 토큰")
