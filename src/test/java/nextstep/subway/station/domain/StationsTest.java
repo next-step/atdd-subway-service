2ae01f954a99173a78523fea164022aecdd65083
ohgillwhan <soora_genius@naver.com>
2021-06-28 11:48:10 +0900

    refact : BDD 스타일로 수정

--- src/test/java/nextstep/subway/station/domain/StationsTest.java
+++ src/test/java/nextstep/subway/station/domain/StationsTest.java
@@ -11,13 +11,16 @@ class StationsTest {
 
     @Test
     void getSectionPair() {
+        // given
         Station first = new Station("FIRST");
         Station second = new Station("SECOND");
         Station third = new Station("THIRD");
         Station fourth = new Station("FOURTH");
 
+        // when
         Stations stations = new Stations(Arrays.asList(first, second, third, fourth));
 
+        // then
         assertThat(stations.getSectionPairs())
                 .containsExactly(
                         new StationPair(first, second),
