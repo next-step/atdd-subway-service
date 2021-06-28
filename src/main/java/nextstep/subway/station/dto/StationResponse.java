dfedcdf76e3e4b0589ef6d7980bf4979e9dee65d
JisuLim <sanola3@gmail.com>
2021-06-29 01:00:37 +0900

    refactor: review reflected

--- src/main/java/nextstep/subway/station/dto/StationResponse.java
+++ src/main/java/nextstep/subway/station/dto/StationResponse.java
@@ -14,9 +14,6 @@ public class StationResponse {
         return new StationResponse(station.getId(), station.getName(), station.getCreatedDate(), station.getModifiedDate());
     }
 
-    public StationResponse() {
-    }
-
     public StationResponse(Long id, String name, LocalDateTime createdDate, LocalDateTime modifiedDate) {
         this.id = id;
         this.name = name;
