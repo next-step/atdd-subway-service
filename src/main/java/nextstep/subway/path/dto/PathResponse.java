02d774abac115300fce496c24e646d5b40fddcaf
lkimilhol <lkimilhol@gmail.com>
2021-06-28 10:57:58 +0900

    feat(PathResponse): 응답에 요금 추가

--- src/main/java/nextstep/subway/path/dto/PathResponse.java
+++ src/main/java/nextstep/subway/path/dto/PathResponse.java
@@ -7,21 +7,27 @@ import java.util.List;
 public class PathResponse {
     private List<StationResponse> stations;
     private int distance;
+    private int fee;
 
     public PathResponse() {
     }
 
-    public PathResponse(List<StationResponse> stations, int distance) {
+    public PathResponse(List<StationResponse> stations, int distance, int fee) {
         this.stations = stations;
         this.distance = distance;
+        this.fee = fee;
     }
 
-    public static PathResponse of(List<StationResponse> stationResponses, int distance) {
-        return new PathResponse(stationResponses, distance);
+    public static PathResponse of(List<StationResponse> stationResponses, int distance, int fee) {
+        return new PathResponse(stationResponses, distance, fee);
     }
 
     public int getDistance() {
         return distance;
+    }
+
+    public int getFee() {
+        return fee;
     }
 
     public List<StationResponse> getStations() {
