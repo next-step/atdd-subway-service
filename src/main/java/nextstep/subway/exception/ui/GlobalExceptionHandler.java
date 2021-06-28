8aa5b978bd27430fd24e3c6b78b4f3824a0c08d0
jimin.joo <wnwlals22@naver.com>
2021-06-28 22:05:03 +0900

    refactor(modify Logging)

--- src/main/java/nextstep/subway/exception/ui/GlobalExceptionHandler.java
+++ src/main/java/nextstep/subway/exception/ui/GlobalExceptionHandler.java
@@ -1,6 +1,8 @@
 package nextstep.subway.exception.ui;
 
 import nextstep.subway.exception.CustomException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,21 +12,23 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
-	@ExceptionHandler(DataIntegrityViolationException.class)
-	public ResponseEntity<Void> handleIllegalArgsException(DataIntegrityViolationException e) {
-	    e.printStackTrace();
-		return ResponseEntity.badRequest().build();
-	}
+    private final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
-	@ExceptionHandler(CustomException.class)
-	public ResponseEntity<Void> handleCustomException(CustomException e) {
-	    e.printStackTrace();
-		return ResponseEntity.status(e.getStatus()).build();
-	}
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<Void> handleIllegalArgsException(DataIntegrityViolationException e) {
+        log.error(e.getMessage(), e);
+        return ResponseEntity.badRequest().build();
+    }
 
-	@ExceptionHandler(Exception.class)
-	public ResponseEntity<Void> handleCommonException(Exception e) {
-        e.printStackTrace();
-		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
-	}
+    @ExceptionHandler(CustomException.class)
+    public ResponseEntity<Void> handleCustomException(CustomException e) {
+        log.error(e.getMessage(), e);
+        return ResponseEntity.status(e.getStatus()).build();
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Void> handleCommonException(Exception e) {
+        log.error(e.getMessage(), e);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+    }
 }
