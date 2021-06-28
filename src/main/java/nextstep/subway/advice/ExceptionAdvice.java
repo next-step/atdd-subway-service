6ea55df54f7c6c3927e883905f911a5d0886c833
prodo-developer <wjdals300@gmail.com>
2021-06-29 02:04:36 +0900

    feat : 인증에 필요한 Exception Handler 추가

--- src/main/java/nextstep/subway/advice/ExceptionAdvice.java
+++ src/main/java/nextstep/subway/advice/ExceptionAdvice.java
@@ -1,5 +1,6 @@
 package nextstep.subway.advice;
 
+import nextstep.subway.auth.application.AuthorizationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -21,5 +22,10 @@ public class ExceptionAdvice {
     @ExceptionHandler(RuntimeException.class)
     public ResponseEntity<String> handleRuntimeException(RuntimeException e) {
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+    }
+
+    @ExceptionHandler(AuthorizationException.class)
+    public ResponseEntity<String> handleAuthorizationException(AuthorizationException e) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(e.getMessage());
     }
 }
