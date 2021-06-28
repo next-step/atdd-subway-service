f239fe9cbd578c5ad6213981af2b986c4428fc98
youngjun.jin <yzzzzun@gmail.com>
2021-06-28 17:56:48 +0900

    test: FareCalculator test추가

--- src/test/java/nextstep/subway/path/domain/FareCalculatorTest.java
+++ src/test/java/nextstep/subway/path/domain/FareCalculatorTest.java
@@ -1,17 +1,36 @@
 package nextstep.subway.path.domain;
 
+import org.assertj.core.api.Assertions;
+import org.jgrapht.GraphPath;
+import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
+import org.jgrapht.graph.WeightedMultigraph;
 import org.junit.jupiter.api.Test;
+
+import nextstep.subway.member.domain.Member;
+import nextstep.subway.station.domain.Station;
 
 class FareCalculatorTest {
 
 	@Test
 	void testCalculateFare() {
 
-		// Path path = new Path(null, 16);
-		// LoginMember loginMember = new LoginMember(1L, "email@email.com", 20);
-		//
-		// FareCalculator calculator = new FareCalculator();
-		// calculator.calculateFare(path, loginMember);
+		Member loginMember = new Member("email@email.com", "123", 20);
+		FareCalculator calculator = new FareCalculator();
 
+		WeightedMultigraph<Station, SectionWeightedEdge> pathGraph = new WeightedMultigraph<>(
+			SectionWeightedEdge.class);
+		Station 강남역 = new Station("강남역");
+		Station 역삼역 = new Station("역삼역");
+		pathGraph.addVertex(강남역);
+		pathGraph.addVertex(역삼역);
+		SectionWeightedEdge edge = new SectionWeightedEdge(강남역, 역삼역, 500);
+		pathGraph.addEdge(강남역, 역삼역, edge);
+		pathGraph.setEdgeWeight(edge, 5);
+
+		DijkstraShortestPath<Station, SectionWeightedEdge> shortestPath = new DijkstraShortestPath<>(pathGraph);
+		GraphPath<Station, SectionWeightedEdge> path = shortestPath.getPath(강남역, 역삼역);
+		int fare = calculator.calculateFare(path, loginMember);
+
+		Assertions.assertThat(fare).isEqualTo(1750);
 	}
 }
