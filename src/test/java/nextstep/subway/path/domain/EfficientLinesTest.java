fe78251f7f22a7e39cd5f38db73ddada7c09aa94
ohgillwhan <soora_genius@naver.com>
2021-06-28 14:36:15 +0900

    refact : 반복되는 코드 제거

--- src/test/java/nextstep/subway/path/domain/EfficientLinesTest.java
+++ src/test/java/nextstep/subway/path/domain/EfficientLinesTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -21,19 +22,13 @@ class EfficientLinesTest {
     private Line FOURTH_LINE = new Line("4호선", "4호선", 500, first, second, 3);
     private Line FIFTH_LINE = new Line("5호선", "5호선", 7000, first, second, 3);
 
+    private List<Line> lines = Arrays.asList(FIRST_LINE, SECOND_LINE, THIRD_LINE, FOURTH_LINE, FIFTH_LINE);
+
     @Test
     @DisplayName("갈 수 있는길이 여러개이면 가장 짧고 싼것을 선택한다")
     void 갈_수_있는길이_여러개이면_가장_짧고_싼것을_선택한다() {
         // given
-        EfficientLines lines = new EfficientLines(
-                Arrays.asList(
-                        FIRST_LINE,
-                        SECOND_LINE,
-                        THIRD_LINE,
-                        FOURTH_LINE,
-                        FIFTH_LINE
-                )
-        );
+        EfficientLines lines = new EfficientLines(this.lines);
         StationPair stationPair = new StationPair(second, first);
 
         // when
@@ -49,15 +44,7 @@ class EfficientLinesTest {
     @DisplayName("가장 비싼 운임을 선택한다")
     void 가장_비싼_운임을_선택한다() {
         // given
-        EfficientLines lines = new EfficientLines(
-                Arrays.asList(
-                        FIRST_LINE,
-                        SECOND_LINE,
-                        THIRD_LINE,
-                        FOURTH_LINE,
-                        FIFTH_LINE
-                )
-        );
+        EfficientLines lines = new EfficientLines(this.lines);
 
         // when
         Money expensiveFare = lines.findExpensiveFare();
