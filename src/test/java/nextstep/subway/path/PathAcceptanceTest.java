1822850857e535d9c3dab31807ef3f795196c4a0
KIM GI TAE <c0433c@gmail.com>
2021-06-30 01:29:09 +0900

    feat(path): happy 케이스 인수테스트
    - 최단경로 조회 인수테스트 껍데기 작성

--- src/test/java/nextstep/subway/path/PathAcceptanceTest.java
+++ src/test/java/nextstep/subway/path/PathAcceptanceTest.java
@@ -1,9 +1,33 @@
 package nextstep.subway.path;
 
-import nextstep.subway.AcceptanceTest;
-import org.junit.jupiter.api.DisplayName;
+import java.util.HashMap;
+import java.util.Map;
 
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import nextstep.subway.AcceptanceTest;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 
 @DisplayName("지하철 경로 조회")
 public class PathAcceptanceTest extends AcceptanceTest {
+
+	@DisplayName("최단 경로 조회")
+	@Test
+	void 최단_경로_조회() {
+		// when
+		ExtractableResponse<Response> response = RestAssured
+			.given().log().all()
+			.when().get("/paths?source={source}&target={target}", 1L, 6L)
+			.then().log().all().extract();
+
+		// then
+		Assertions.assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+	}
+
 }
