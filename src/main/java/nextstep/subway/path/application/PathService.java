c08c9fbea894b581fd41194a87ef61af78707130
yjs2952 <yjs2952@naver.com>
2021-06-30 01:21:04 +0900

    fix: PathFinder 직접 생성하도록 변경

--- src/main/java/nextstep/subway/path/application/PathService.java
+++ src/main/java/nextstep/subway/path/application/PathService.java
@@ -1,5 +1,6 @@
 package nextstep.subway.path.application;
 
+import nextstep.subway.line.domain.LineRepository;
 import nextstep.subway.path.domain.PathFinder;
 import nextstep.subway.path.dto.PathResponse;
 import nextstep.subway.station.domain.Station;
@@ -13,16 +14,17 @@ import java.util.List;
 public class PathService {
 
     private final StationRepository stationRepository;
-    private final PathFinder pathFinder;
+    public final LineRepository lineRepository;
 
-    public PathService(StationRepository stationRepository, PathFinder pathFinder) {
+    public PathService(StationRepository stationRepository, LineRepository lineRepository) {
         this.stationRepository = stationRepository;
-        this.pathFinder = pathFinder;
+        this.lineRepository = lineRepository;
     }
 
     public PathResponse findPath(Long sourceId, Long targetId) {
         Station source = stationRepository.findById(sourceId).orElseThrow(() -> new IllegalArgumentException("출발역이 존재하지 않습니다."));
         Station target = stationRepository.findById(targetId).orElseThrow(() -> new IllegalArgumentException("도착역이 존재하지 않습니다."));
+        PathFinder pathFinder = new PathFinder(lineRepository.findAll());
         List<StationResponse> shortestPaths = pathFinder.findPaths(source, target);
         return new PathResponse(shortestPaths, 0);
     }
--- src/main/java/nextstep/subway/path/domain/PathFinder.java
+++ src/main/java/nextstep/subway/path/domain/PathFinder.java
@@ -13,7 +13,6 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
-@Component
 public class PathFinder {
 
     private final DijkstraShortestPath<Station, DefaultWeightedEdge> dijkstraShortestPath;
