1ce540213f055e8438b49ec9fb977e751830595c
bbbnam <bbbnam@naver.com>
2021-06-30 05:39:06 +0900

    test : 최단경로 조회 인수테스트 요금계산을 위해 거리 조건 수정

--- src/test/java/nextstep/subway/path/PathAcceptanceTest.java
+++ src/test/java/nextstep/subway/path/PathAcceptanceTest.java
@@ -30,11 +30,11 @@ public class PathAcceptanceTest extends AcceptanceTest {
     private StationResponse 남부터미널역;
 
     /**
-     * 교대역    --- *2호선* ---   강남역
-     * |                        |
-     * *3호선*                   *신분당선*
-     * |                        |
-     * 남부터미널역  --- *3호선* ---   양재
+     * 교대역   --- *2호선*(10)  ---     강남역
+     * |                                 |
+     * *3호선*(7)                     *신분당선* (4)
+     * |                                |
+     * 남부터미널역 --- *3호선*(10) --- 양재
      */
     @BeforeEach
     public void setUp() {
@@ -45,22 +45,22 @@ public class PathAcceptanceTest extends AcceptanceTest {
         교대역 = StationAcceptanceTest.지하철역_등록되어_있음("교대역").as(StationResponse.class);
         남부터미널역 = StationAcceptanceTest.지하철역_등록되어_있음("남부터미널역").as(StationResponse.class);
 
-        신분당선 = 지하철_노선_등록되어_있음("신분당선", "bg-red-600", 강남역, 양재역, 10);
+        신분당선 = 지하철_노선_등록되어_있음("신분당선", "bg-red-600", 강남역, 양재역, 4);
         이호선 = 지하철_노선_등록되어_있음("이호선", "bg-red-600", 교대역, 강남역, 10);
-        삼호선 = 지하철_노선_등록되어_있음("삼호선", "bg-red-600", 교대역, 양재역, 5);
+        삼호선 = 지하철_노선_등록되어_있음("삼호선", "bg-red-600", 교대역, 양재역, 17);
 
-        지하철_노선에_지하철역_등록되어_있음(삼호선, 교대역, 남부터미널역, 3);
+        지하철_노선에_지하철역_등록되어_있음(삼호선, 교대역, 남부터미널역, 7);
     }
 
     @DisplayName("최단 경로를 조회한다.")
     @Test
     void findShortestPath() {
-        int expectedDistance = 5;
-        int expectedFare = 0;
+        int expectedDistance = 14;
+        int expectedFare = 1350;
         ExtractableResponse<Response> response = 지하철_노선_최단경로_조회_요청(교대역, 양재역);
 
         지하철_노선_최단경로_조회됨(response);
-        지하철_노선_최단경로_목록_정렬됨(response, Arrays.asList(교대역, 남부터미널역, 양재역));
+        지하철_노선_최단경로_목록_정렬됨(response, Arrays.asList(교대역, 강남역, 양재역));
         지하철_노선_최단경로_거리_응답됨(response, expectedDistance);
         지하철_노선_최단경로_이용_요금_응답됨(response, expectedFare);
     }
