71b583419caaba38a1ddf202bf7a2bc6c284971f
lenkim <joenggyu0@gmail.com>
2021-06-29 10:50:36 +0900

    feat: 추가요금이 붙을 경우 테스트 케이스 추가

--- src/test/java/nextstep/subway/path/domain/PathNavigationTest.java
+++ src/test/java/nextstep/subway/path/domain/PathNavigationTest.java
@@ -3,6 +3,7 @@ package nextstep.subway.path.domain;
 import nextstep.subway.line.domain.Line;
 import nextstep.subway.line.domain.Section;
 import nextstep.subway.station.domain.Station;
+import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -17,8 +18,8 @@ class PathNavigationTest {
     PathNavigation sut;
     private Station 강남;
     private Station 광교;
-    private Station 구리;
-    private Station 용산;
+
+
     private List<Line> lines;
     private Station 양재;
     private Station 교대;
@@ -33,8 +34,6 @@ class PathNavigationTest {
     void setUp() {
         강남 = new Station(1L, "강남");
         광교 = new Station(2L, "광교");
-        구리 = new Station(3L, "구리");
-        용산 = new Station(4L, "용산");
         양재 = new Station(5L, "양재");
         교대 = new Station(6L, "교대");
         남부터미널 = new Station(7L, "남부터미널");
@@ -54,7 +53,6 @@ class PathNavigationTest {
         삼호선.addSection(new Section(삼호선, 교대, 남부터미널, 9));
         lines.add(삼호선);
         lines.add(이호선);
-        lines.add(new Line("중앙선", "gs-1123", 0, 구리, 용산, 200));
     }
 
 
@@ -69,6 +67,9 @@ class PathNavigationTest {
 
     @Test
     void IllegalArgumentException_when_path_is_not_connected() {
+        Station 용산 = new Station(4L, "용산");
+        Station 구리 = new Station(3L, "구리");
+        lines.add(new Line("중앙선", "gs-1123", 0, 구리, 용산, 200));
         sut = PathNavigation.by(lines);
 
         assertThatThrownBy(() -> sut.findShortestPath(강남, 용산))
@@ -87,9 +88,7 @@ class PathNavigationTest {
 
     @Test
     void findShortestPath() {
-
         sut = PathNavigation.by(lines);
-
         Path shortestPath = sut.findShortestPath(강남, 남부터미널);
 
         assertThat(shortestPath.stations()).contains(강남, 교대, 남부터미널);
@@ -98,7 +97,7 @@ class PathNavigationTest {
     }
 
     @Test
-    void findShortestPath_25KM_1350Fee() {
+    void when_25KM_1350Fee_findShortestPath() {
         lines.add(이호선);
         sut = PathNavigation.by(lines);
         Path shortestPath = sut.findShortestPath(B, 남부터미널);
@@ -107,7 +106,7 @@ class PathNavigationTest {
     }
 
     @Test
-    void findShortestPath_50KM_2150Fee() {
+    void when_50KM_2150Fee_findShortestPath() {
         Section newSection = new Section(이호선, A, B, 500);
         이호선.addSection(newSection);
         lines.add(이호선);
@@ -118,7 +117,7 @@ class PathNavigationTest {
     }
 
     @Test
-    void findShortestPath_57KM_2150Fee() {
+    void when_57KM_2150Fee_findShortestPath() {
         이호선.addSection(new Section(이호선, A, B, 570));
         lines.add(이호선);
         sut = PathNavigation.by(lines);
@@ -128,7 +127,7 @@ class PathNavigationTest {
     }
 
     @Test
-    void findShortestPath_58KM_2250Fee() {
+    void when_58KM_2250Fee_findShortestPath() {
         이호선.addSection(new Section(이호선, A, B, 580));
         lines.add(이호선);
         sut = PathNavigation.by(lines);
@@ -138,15 +137,64 @@ class PathNavigationTest {
     }
 
     @Test
-    void _일호선은_900원_추가요금이_붙는_호선이다() {
-        Station 양평 = new Station(10L, "양평");
-        Station 가평 = new Station(11L, "가평");
-        Line 경춘선 = new Line("경춘선", "gs-12345", 900, 양평, 가평, 15);
-        lines.add(경춘선);
+    void _특정호선은_추가요금이_붙는다() {
+        Station 양평 = new Station(10L, "양평"), 가평 = new Station(11L, "가평");
+        Station 광나루역 = new Station(12L, "광나루역"), 천호역 = new Station(13L, "천호역");
+        Station 오리역 = new Station(14L, "오리역"), 미금역 = new Station(15L, "미금역");
+
+        Line 경춘선 = new Line("경춘선", "gs-12345", 900, 양평, 가평, 15); //1.5Km
+        Line 사호선 = new Line("사호선", "gs-12245", 500, 광나루역, 천호역, 20); //2Km
+        Line 분당선 = new Line("분당선", "gs-12545", 0, 오리역, 미금역, 20); //2Km
+        lines.addAll(Lists.list(경춘선, 사호선, 분당선));
 
         Path path = PathNavigation.by(lines).findShortestPath(양평, 가평);
         assertThat(path.fee()).isEqualTo(1250 + 900);
+
+        Path path2 = PathNavigation.by(lines).findShortestPath(광나루역, 천호역);
+        assertThat(path2.fee()).isEqualTo(1250 + 500);
+
+        Path path3 = PathNavigation.by(lines).findShortestPath(오리역, 미금역);
+        assertThat(path3.fee()).isEqualTo(1250);
     }
 
-    
+    @Test
+    void 추가요금이_있는_노선은_거리비례제가_적용된다() {
+        Station 양평 = new Station(10L, "양평"), 가평 = new Station(11L, "가평");
+        Station 광나루역 = new Station(12L, "광나루역"), 천호역 = new Station(13L, "천호역");
+        Station 오리역 = new Station(14L, "오리역"), 미금역 = new Station(15L, "미금역");
+        Station 죽전역 = new Station(16L, "죽전역");
+
+        Line 경춘선 = new Line("경춘선", "gs-12345", 900, 양평, 가평, 120); //12Km
+        Line 사호선 = new Line("사호선", "gs-12245", 900, 광나루역, 천호역, 200); //20Km
+        Line 분당선 = new Line("분당선", "gs-12545", 900, 미금역, 오리역, 500); //50Km
+        분당선.addSection(new Section(분당선, 오리역, 죽전역,580)); // 58Km
+        lines.addAll(Lists.list(경춘선, 사호선, 분당선));
+
+        Path path = PathNavigation.by(lines).findShortestPath(양평, 가평);
+        assertThat(path.fee()).isEqualTo(1250 + 900 + 100);
+
+        Path path2 = PathNavigation.by(lines).findShortestPath(광나루역, 천호역);
+        assertThat(path2.fee()).isEqualTo(1250 + 900 + 300);
+
+        Path path3 = PathNavigation.by(lines).findShortestPath(오리역, 미금역);
+        assertThat(path3.fee()).isEqualTo(1250 + 900 + 900);
+
+        Path path4 = PathNavigation.by(lines).findShortestPath(오리역, 죽전역);
+        assertThat(path4.fee()).isEqualTo(1250 + 900 + 1000);
+    }
+
+    @Test
+    void _추가요금이_붙는_호선을_거치는_가장_높은_추가요금이_붙는다() {
+        Station 양평 = new Station(10L, "양평"), 가평 = new Station(11L, "가평");
+        Line 경춘선 = new Line("경춘선", "gs-12345", 900, 양평, 가평, 10);
+
+        Station 상봉 = new Station(4L, "상봉"), 구리 = new Station(3L, "구리");
+        lines.add(new Line("중앙선", "gs-1123", 0, 구리, 상봉, 10));
+
+        경춘선.addSection(new Section(경춘선, 상봉, 양평, 10));
+        lines.add(경춘선);
+
+        Path path = PathNavigation.by(lines).findShortestPath(구리, 가평);
+        assertThat(path.fee()).isEqualTo(1250 + 900);
+    }
 }
