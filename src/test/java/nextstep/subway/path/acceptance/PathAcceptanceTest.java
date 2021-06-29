eca7a84ad8438a1ca5c61647f486409da7ca7841
yjs2952 <yjs2952@naver.com>
2021-06-30 01:26:27 +0900

    test: 출발역과 도착역이 연결되어 있지 않은 경우 예외발생 인수테스트 추가

--- src/test/java/nextstep/subway/path/acceptance/PathAcceptanceTest.java
+++ src/test/java/nextstep/subway/path/acceptance/PathAcceptanceTest.java
@@ -6,8 +6,8 @@ import io.restassured.response.Response;
 import nextstep.subway.AcceptanceTest;
 import nextstep.subway.line.dto.LineRequest;
 import nextstep.subway.line.dto.LineResponse;
+import nextstep.subway.path.dto.PathResponse;
 import nextstep.subway.station.dto.StationResponse;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -17,6 +17,7 @@ import org.springframework.http.MediaType;
 import static nextstep.subway.line.acceptance.LineAcceptanceTest.지하철_노선_등록되어_있음;
 import static nextstep.subway.line.acceptance.LineSectionAcceptanceTest.지하철_노선에_지하철역_등록되어_있음;
 import static nextstep.subway.station.StationAcceptanceTest.지하철역_등록되어_있음;
+import static org.assertj.core.api.Assertions.assertThat;
 
 
 @DisplayName("지하철 경로 조회")
@@ -30,6 +31,7 @@ public class PathAcceptanceTest extends AcceptanceTest {
     private StationResponse 교대역;
     private StationResponse 역삼역;
     private StationResponse 남부터미널역;
+    private StationResponse 공덕역;
 
     /**
      * 교대역    --- *2호선* ---   강남역   -- 역삼역
@@ -46,6 +48,7 @@ public class PathAcceptanceTest extends AcceptanceTest {
         양재역 = 지하철역_등록되어_있음("양재역").as(StationResponse.class);
         교대역 = 지하철역_등록되어_있음("교대역").as(StationResponse.class);
         역삼역 = 지하철역_등록되어_있음("역삼역").as(StationResponse.class);
+        공덕역 = 지하철역_등록되어_있음("공덕역").as(StationResponse.class);
         남부터미널역 = 지하철역_등록되어_있음("남부터미널역").as(StationResponse.class);
 
         신분당선 = 지하철_노선_등록되어_있음(new LineRequest("신분당선", "bg-red-600", 강남역.getId(), 양재역.getId(), 10)).as(LineResponse.class);
@@ -60,30 +63,47 @@ public class PathAcceptanceTest extends AcceptanceTest {
     @DisplayName("역과 역 사이의 최단 경로를 조회한다")
     void findPathTest() {
         // when
-        ExtractableResponse<Response> response = RestAssured
-                .given().log().all()
-                .accept(MediaType.APPLICATION_JSON_VALUE)
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .when().get("/paths?source={source}&target={target}", 남부터미널역.getId(), 역삼역.getId())
-                .then().log().all().extract();
+        ExtractableResponse<Response> response = 최단경로_조회_요청(남부터미널역, 역삼역);
 
         // then
-        Assertions.assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
-        Assertions.assertThat(response.jsonPath().getList("$.stations")).hasSize(3);
+        최단경로_조회됨(response);
+        assertThat(response.jsonPath().getObject(".", PathResponse.class).getStations()).hasSize(4);
     }
 
     @Test
     @DisplayName("출발역과 도착역이 같은 경우 예외가 발생한다")
     void sameSourceTargetTest() {
         // when
-        ExtractableResponse<Response> response = RestAssured
+        ExtractableResponse<Response> response = 최단경로_조회_요청(역삼역, 역삼역);
+
+        // then
+        최단경로_조회_실패됨(response);
+    }
+
+    @Test
+    @DisplayName("출발역과 도착역이 연결되어 있지 않은 경우 예외가 발생한다")
+    void notConnectedSourceTargetTest() {
+        // when
+        ExtractableResponse<Response> response = 최단경로_조회_요청(역삼역, 공덕역);
+
+        // then
+        최단경로_조회_실패됨(response);
+    }
+
+    private void 최단경로_조회됨(ExtractableResponse<Response> response) {
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+    }
+
+    private void 최단경로_조회_실패됨(ExtractableResponse<Response> response) {
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+    }
+
+    private ExtractableResponse<Response> 최단경로_조회_요청(StationResponse source, StationResponse target) {
+        return RestAssured
                 .given().log().all()
                 .accept(MediaType.APPLICATION_JSON_VALUE)
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .when().get("/paths?source={source}&target={target}", 역삼역.getId(), 역삼역.getId())
+                .when().get("/paths?source={source}&target={target}", source.getId(), target.getId())
                 .then().log().all().extract();
-
-        // then
-        Assertions.assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
     }
 }
