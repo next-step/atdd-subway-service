907590b2828dbcafc8539b4f3b05a4bc89b9aa9a
bbbnam <bbbnam@naver.com>
2021-06-30 10:06:56 +0900

    feat : 기본 할인 정책 및 할인 정책 선택 팩토리 생성

--- src/main/java/nextstep/subway/path/domain/policy/discount/DiscountPolicyFactory.java
+++ src/main/java/nextstep/subway/path/domain/policy/discount/DiscountPolicyFactory.java
@@ -1,5 +1,18 @@
 package nextstep.subway.path.domain.policy.discount;
 
-public class DiscountPolicyFactory {
+import nextstep.subway.path.domain.policy.FarePolicy;
 
+public class DiscountPolicyFactory {
+    public static final int BASIC_DISCOUNT_STANDARD = 350;
+
+    public static FarePolicy findPolicy(int age) {
+        if (age >= 13 && age < 19) {
+            return new TeenagerDiscountPolicy();
+        }
+
+        if (age >= 6 && age < 13) {
+            return new ChildrenDiscountPolicy();
+        }
+        return new DefaultDiscountPolicy();
+    }
 }
