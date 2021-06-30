23cdf6dd12f98399c33986092d8402d7d216273f
bbbnam <bbbnam@naver.com>
2021-06-30 10:34:07 +0900

    test : 로그인된 사용자와 로그인하지 않은 사용자 구분하여 인수테스트 분리

--- src/test/java/nextstep/subway/path/PathAcceptanceTest.java
+++ src/test/java/nextstep/subway/path/PathAcceptanceTest.java
@@ -3,6 +3,7 @@ package nextstep.subway.path;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import nextstep.subway.AcceptanceTest;
+import nextstep.subway.auth.acceptance.TestToken;
 import nextstep.subway.line.dto.LineResponse;
 import nextstep.subway.station.StationAcceptanceTest;
 import nextstep.subway.station.dto.StationResponse;
@@ -13,8 +14,10 @@ import org.springframework.test.context.ActiveProfiles;
 
 import java.util.Arrays;
 
+import static nextstep.subway.auth.AuthSteps.로그인_요청;
 import static nextstep.subway.line.acceptance.LineSetionSteps.*;
 import static nextstep.subway.line.acceptance.LineSteps.*;
+import static nextstep.subway.member.MemeberSteps.회원_등록_되어_있음;
 import static nextstep.subway.path.PathSteps.*;
 
 @ActiveProfiles("test")
@@ -28,6 +31,11 @@ public class PathAcceptanceTest extends AcceptanceTest {
     private StationResponse 양재역;
     private StationResponse 교대역;
     private StationResponse 남부터미널역;
+    private String EMAIL = "teenager@email.com";
+    private String PASSWORD = "12345";
+    private int AGE = 15;
+
+    private TestToken 로그인된_청소년;
 
     /**
      * 교대역   --- *2호선*(10)  ---     강남역
@@ -50,14 +58,31 @@ public class PathAcceptanceTest extends AcceptanceTest {
         삼호선 = 지하철_노선_등록되어_있음("삼호선", "bg-red-600", 0, 교대역, 양재역, 17);
 
         지하철_노선에_지하철역_등록되어_있음(삼호선, 교대역, 남부터미널역, 7);
+        회원_등록_되어_있음(EMAIL, PASSWORD, AGE);
+
+        ExtractableResponse<Response> response = 로그인_요청(EMAIL, PASSWORD);
+        로그인된_청소년 = response.as(TestToken.class);
     }
 
-    @DisplayName("최단 경로를 조회한다.")
+    @DisplayName("최단 경로를 조회한다. - 로그인 안했을때")
     @Test
-    void findShortestPath() {
+    void findShortestPath_withNotLogin() {
         int expectedDistance = 14;
         int expectedFare = 1350;
         ExtractableResponse<Response> response = 지하철_노선_최단경로_조회_요청(교대역, 양재역);
+
+        지하철_노선_최단경로_조회됨(response);
+        지하철_노선_최단경로_목록_정렬됨(response, Arrays.asList(교대역, 강남역, 양재역));
+        지하철_노선_최단경로_거리_응답됨(response, expectedDistance);
+        지하철_노선_최단경로_이용_요금_응답됨(response, expectedFare);
+    }
+
+    @DisplayName("최단 경로를 조회한다. - 로그인 했을때 (청소년 할인 해당일때)")
+    @Test
+    void findShortestPath_withLogin() {
+        int expectedDistance = 14;
+        int expectedFare = 1150;
+        ExtractableResponse<Response> response = 지하철_노선_최단경로_조회_요청_with로그인(교대역, 양재역, 로그인된_청소년);
 
         지하철_노선_최단경로_조회됨(response);
         지하철_노선_최단경로_목록_정렬됨(response, Arrays.asList(교대역, 강남역, 양재역));
