49ee16997ff3d77085bfc9c453676e01079e555d
Donghui-Lee <ehdgml3206@naver.com>
2021-06-30 15:18:52 +0900

    refactor: 테스트 성공을 위한 코드 수정

--- src/main/java/nextstep/subway/favorite/application/FavoriteService.java
+++ src/main/java/nextstep/subway/favorite/application/FavoriteService.java
@@ -1,26 +1,41 @@
 package nextstep.subway.favorite.application;
 
+import java.util.NoSuchElementException;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import nextstep.subway.auth.domain.LoginMember;
+import nextstep.subway.favorite.domain.Favorite;
 import nextstep.subway.favorite.domain.FavoriteRepository;
 import nextstep.subway.favorite.dto.FavoriteRequest;
 import nextstep.subway.favorite.dto.FavoriteResponse;
+import nextstep.subway.member.domain.Member;
 import nextstep.subway.member.domain.MemberRepository;
+import nextstep.subway.station.domain.Station;
+import nextstep.subway.station.domain.StationRepository;
 
 @Service
 @Transactional
 public class FavoriteService {
     private final FavoriteRepository favoriteRepository;
     private final MemberRepository memberRepository;
+    private final StationRepository stationRepository;
 
-    public FavoriteService(FavoriteRepository favoriteRepository, MemberRepository memberRepository) {
+    public FavoriteService(FavoriteRepository favoriteRepository, MemberRepository memberRepository, StationRepository stationRepository) {
         this.favoriteRepository = favoriteRepository;
         this.memberRepository = memberRepository;
+        this.stationRepository = stationRepository;
     }
 
     public FavoriteResponse createFavorite(LoginMember loginMember, FavoriteRequest favoriteRequest) {
-        return FavoriteResponse.of(null);
+        Station sourceStation = stationRepository.findById(favoriteRequest.getSource()).orElseThrow(NoSuchElementException::new);
+        Station targetStation = stationRepository.findById(favoriteRequest.getTarget()).orElseThrow(NoSuchElementException::new);
+        Member member = memberRepository.findById(loginMember.getId()).orElseThrow(NoSuchElementException::new);
+
+        Favorite favorite = Favorite.of(member, sourceStation, targetStation);
+        Favorite savedFavorite = favoriteRepository.save(favorite);
+
+        return FavoriteResponse.of(savedFavorite.getId(), savedFavorite.getSource(), savedFavorite.getTarget());
     }
 }
