61487a2598583989a92f1e99ef10decfe2b72d25
taemin <taminging@kakao.com>
2021-06-30 21:54:51 +0900

    test : FavoriteAcceptanceTest 리팩터링

--- src/test/java/nextstep/subway/favorite/FavoriteAcceptanceTest.java
+++ src/test/java/nextstep/subway/favorite/FavoriteAcceptanceTest.java
@@ -40,8 +40,6 @@ public class FavoriteAcceptanceTest extends AcceptanceTest {
 
 	@BeforeEach
 	void favoriteSetUp() {
-		memberAcceptanceTest.회원_생성을_요청("taminging@kakao.com", "taminging", 20);
-		토큰 = authAcceptanceTest.로그인("taminging@kakao.com", "taminging").as(TokenResponse.class);
 		강남역 = stationAcceptanceTest.지하철역_등록되어_있음("강남역").as(StationResponse.class);
 		양재역 = stationAcceptanceTest.지하철역_등록되어_있음("양재역").as(StationResponse.class);
 		교대역 = stationAcceptanceTest.지하철역_등록되어_있음("교대역").as(StationResponse.class);
@@ -54,28 +52,88 @@ public class FavoriteAcceptanceTest extends AcceptanceTest {
 		삼호선 = lineAcceptanceTest.지하철_노선_등록되어_있음(new LineRequest("삼호선", "bg-red-600", 교대역.getId(), 양재역.getId(), 5))
 			.as(LineResponse.class);
 		lineSectionAcceptanceTest.지하철_노선에_지하철역_등록_요청(삼호선, 남부터미널역, 양재역, 3);
+		memberAcceptanceTest.회원_생성을_요청("taminging@kakao.com", "taminging", 20);
+		토큰 = authAcceptanceTest.로그인("taminging@kakao.com", "taminging").as(TokenResponse.class);
 	}
 
 	@DisplayName("즐겨찾기를 관리한다.")
 	@Test
 	void 즐겨찾기_관리() {
 		//생성
-		FavoriteRequest favoriteRequest = new FavoriteRequest(강남역.getId().toString(), 남부터미널역.getId().toString());
-		ExtractableResponse<Response> createResponse = post(favoriteRequest, "/favorite", 토큰.getAccessToken());
-		assertThat(createResponse.statusCode()).isEqualTo(HttpStatus.CREATED.value());
-
-		assertThat(createResponse.body().jsonPath().getString("source.name")).isEqualTo(강남역.getName());
-		assertThat(createResponse.body().jsonPath().getString("target.name")).isEqualTo(남부터미널역.getName());
+		ExtractableResponse<Response> response = 즐겨찾기_생성_요청(강남역.getId(), 남부터미널역.getId());
+		Long id = 즐겨찾기_생성됨(response, 강남역, 남부터미널역);
 
 		//조회
-		ExtractableResponse<Response> selectResponse = get("/favorite", 토큰.getAccessToken());
-		assertThat(selectResponse.statusCode()).isEqualTo(HttpStatus.OK.value());
-		assertThat(selectResponse.body().jsonPath().getString("[0].source.name")).isEqualTo(강남역.getName());
-		assertThat(selectResponse.body().jsonPath().getString("[0].target.name")).isEqualTo(남부터미널역.getName());
+		ExtractableResponse<Response> selectResponse = 즐겨찾기_조회_요청();
+		즐겨찾기_조회_확인(selectResponse);
 
 		//삭제
-		Long id = createResponse.body().jsonPath().getLong("id");
-		ExtractableResponse<Response> deleteResponse = delete("/favorite/" + id, 토큰.getAccessToken());
-		assertThat(deleteResponse.statusCode()).isEqualTo(HttpStatus.NO_CONTENT.value());
+		ExtractableResponse<Response> deleteResponse = 즐겨찾기_삭제_요청(id);
+		즐겨찾기_삭제_확인(deleteResponse);
 	}
+
+	@DisplayName("즐겨찾기 생성")
+	@Test
+	void 즐겨찾기_생성하기() {
+		//when
+		ExtractableResponse<Response> response = 즐겨찾기_생성_요청(강남역.getId(), 남부터미널역.getId());
+		//then
+		즐겨찾기_생성됨(response, 강남역, 남부터미널역);
+	}
+
+	@DisplayName("즐겨찾기 모두 조회")
+	@Test
+	void 즐겨찾기_모두_조회() {
+		//given
+		ExtractableResponse<Response> createResponse = 즐겨찾기_생성_요청(강남역.getId(), 남부터미널역.getId());
+		즐겨찾기_생성됨(createResponse, 강남역, 남부터미널역);
+		//when
+		ExtractableResponse<Response> selectResponse = 즐겨찾기_조회_요청();
+		//then
+		즐겨찾기_조회_확인(selectResponse);
+	}
+
+	@DisplayName("즐겨찾기 삭제")
+	@Test
+	void 즐겨찾기_삭제() {
+		//given
+		ExtractableResponse<Response> createResponse = 즐겨찾기_생성_요청(강남역.getId(), 남부터미널역.getId());
+		Long id = 즐겨찾기_생성됨(createResponse, 강남역, 남부터미널역);
+		//when
+		ExtractableResponse<Response> deleteResponse = 즐겨찾기_삭제_요청(id);
+		//then
+		즐겨찾기_삭제_확인(deleteResponse);
+	}
+
+	ExtractableResponse<Response> 즐겨찾기_생성_요청(Long sourceId, Long targetId) {
+		FavoriteRequest favoriteRequest = new FavoriteRequest(sourceId.toString(), targetId.toString());
+		return post(favoriteRequest, "/favorite", 토큰.getAccessToken());
+	}
+
+	Long 즐겨찾기_생성됨(ExtractableResponse<Response> response, StationResponse source, StationResponse target) {
+		assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+		assertThat(response.body().jsonPath().getString("source.name")).isEqualTo(source.getName());
+		assertThat(response.body().jsonPath().getString("target.name")).isEqualTo(target.getName());
+		return response.body().jsonPath().getLong("id");
+	}
+
+	ExtractableResponse<Response> 즐겨찾기_조회_요청() {
+		return get("/favorite", 토큰.getAccessToken());
+	}
+
+	void 즐겨찾기_조회_확인(ExtractableResponse<Response> response) {
+		assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+	}
+
+
+	ExtractableResponse<Response> 즐겨찾기_삭제_요청(Long id) {
+		return delete("/favorite/" + id, 토큰.getAccessToken());
+	}
+
+	void 즐겨찾기_삭제_확인(ExtractableResponse<Response> response) {
+		assertThat(response.statusCode()).isEqualTo(HttpStatus.NO_CONTENT.value());
+	}
+
+
+
 }
