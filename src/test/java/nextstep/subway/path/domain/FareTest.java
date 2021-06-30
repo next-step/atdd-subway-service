98bd902737d3397a79a5baafffd569a7f896cb15
wrallee <wrallee@naver.com>
2021-06-30 22:08:07 +0900

    test(fare): 연령대별 요금계산 단위 테스트 추가

---
+++ src/test/java/nextstep/subway/path/domain/FareTest.java
@@ -0,0 +1,17 @@
+package nextstep.subway.path.domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("요금 객체 단위 테스트")
+class FareTest {
+    @Test
+    @DisplayName("연령대별 요금을 검증한다.")
+    void adultFare() {
+        Fare fare = new Fare(2050);
+        Assertions.assertThat(fare.adultFare()).isEqualTo(2050);
+        Assertions.assertThat(fare.teenagerFare()).isEqualTo(1360);
+        Assertions.assertThat(fare.childFare()).isEqualTo(850);
+    }
+}
