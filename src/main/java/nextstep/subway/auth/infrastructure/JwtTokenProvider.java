b9df16a876e058e55be385857ef7312aa4454d56
Lee-Chungsun <cndtjs0218@naver.com>
2021-07-01 23:25:06 +0900

    feat : add unabletoken exception

--- src/main/java/nextstep/subway/auth/infrastructure/JwtTokenProvider.java
+++ src/main/java/nextstep/subway/auth/infrastructure/JwtTokenProvider.java
@@ -1,6 +1,7 @@
 package nextstep.subway.auth.infrastructure;
 
 import io.jsonwebtoken.*;
+import nextstep.subway.common.Excetion.UnableTokenException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -36,7 +37,7 @@ public class JwtTokenProvider {
 
             return !claims.getBody().getExpiration().before(new Date());
         } catch (JwtException | IllegalArgumentException e) {
-            return false;
+            throw new UnableTokenException();
         }
     }
 }
