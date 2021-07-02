ab3172b5695b33c820d41dd20608c6d7cdb9c840
KIM GI TAE <c0433c@gmail.com>
2021-07-03 01:09:49 +0900

    refactor(path): 메소드추출을 통해 가독성 있는 소스로 리팩토링
    - 메소드 추출 리팩토링

--- src/main/java/nextstep/subway/path/domain/Path.java
+++ src/main/java/nextstep/subway/path/domain/Path.java
@@ -16,13 +16,8 @@ public class Path {
 	private final Station target;
 
 	public Path(List<Line> lines, Station source, Station target) {
-		if (source.equals(target)) {
-			throw new IllegalArgumentException("출발역과 도착역이 같습니다.");
-		}
-
-		if (lines.stream().flatMap(line -> line.getStations().stream()).distinct().noneMatch(station -> station.equals(source) && station.equals(target))) {
-			throw new IllegalArgumentException("출발역 또는 도착역이 존재하지 않습니다.");
-		}
+		validateSourceIsEqualsToTarget(source, target);
+		validateDoesNotBelongToLines(lines, source, target);
 
 		addVertexes(lines);
 		addEdgeWeights(lines);
@@ -30,6 +25,25 @@ public class Path {
 		this.source = source;
 		this.target = target;
 		dijkstraShortestPath = new DijkstraShortestPath(graph);
+	}
+
+	private void validateConnectedSourceToTarget() {
+		if (dijkstraShortestPath.getPath(source, target) == null) {
+			throw new IllegalArgumentException("출발역과 도착역이 연결되어 있지 않습니다.");
+		}
+	}
+
+	private void validateDoesNotBelongToLines(List<Line> lines, Station source, Station target) {
+		if (lines.stream().flatMap(line -> line.getStations().stream()).distinct().noneMatch(station -> station.equals(
+			source) && station.equals(target))) {
+			throw new IllegalArgumentException("출발역 또는 도착역이 존재하지 않습니다.");
+		}
+	}
+
+	private void validateSourceIsEqualsToTarget(Station source, Station target) {
+		if (source.equals(target)) {
+			throw new IllegalArgumentException("출발역과 도착역이 같습니다.");
+		}
 	}
 
 	private void addEdgeWeights(List<Line> lines) {
@@ -48,12 +62,6 @@ public class Path {
 	public List<Station> getShortestStations() {
 		validateConnectedSourceToTarget();
 		return dijkstraShortestPath.getPath(source, target).getVertexList();
-	}
-
-	private void validateConnectedSourceToTarget() {
-		if (dijkstraShortestPath.getPath(source, target) == null) {
-			throw new IllegalArgumentException("출발역과 도착역이 연결되어 있지 않습니다.");
-		}
 	}
 
 	public int getShortestDistance() {
