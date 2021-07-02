893f83f0213e94dccd9835c48f454aa2d6fbc393
wrallee <wrallee@naver.com>
2021-07-02 17:57:29 +0900

    refactor(payZone): Enum이 자동 계산되도록 로직화

--- src/main/java/nextstep/subway/path/domain/PayZone.java
+++ src/main/java/nextstep/subway/path/domain/PayZone.java
@@ -1,38 +1,35 @@
 package nextstep.subway.path.domain;
 
+import java.util.Arrays;
+
+import nextstep.subway.path.exception.PayZoneNotFoundException;
+
 public enum PayZone {
-    FIRST(0, null),
-    SECOND(10, 5),
-    THIRD(50, 8);
+    FIRST(0, 10, null),
+    SECOND(10, 50, 5),
+    THIRD(50, Double.MAX_VALUE, 8);
 
     private static final int NO_POINT = 0;
 
     double lowerBound;
+    double upperBound;
     Integer divider;
 
-    PayZone(double lowerBound, Integer divider) {
+    PayZone(double lowerBound, double upperBound, Integer divider) {
         this.lowerBound = lowerBound;
+        this.upperBound = upperBound;
         this.divider = divider;
     }
 
     public static PayZone of(double distance) {
-        if (isFirstZone(distance)) {
-            return FIRST;
-        }
-
-        if (isSecondZone(distance)) {
-            return SECOND;
-        }
-
-        return THIRD;
+        return Arrays.stream(values())
+            .filter(z -> z.include(distance))
+            .findAny()
+            .orElseThrow(() -> new PayZoneNotFoundException("어떤 페이존에도 포함되지 않습니다."));
     }
 
-    private static boolean isFirstZone(double distance) {
-        return distance <= SECOND.lowerBound;
-    }
-
-    private static boolean isSecondZone(double distance) {
-        return SECOND.lowerBound < distance && distance <= THIRD.lowerBound;
+    private boolean include(double distance) {
+        return this.lowerBound < distance && distance <= this.upperBound;
     }
 
     public int totalPoint(double distance) {
---
+++ src/main/java/nextstep/subway/path/exception/PayZoneNotFoundException.java
@@ -0,0 +1,7 @@
+package nextstep.subway.path.exception;
+
+public class PayZoneNotFoundException extends RuntimeException {
+    public PayZoneNotFoundException(String message) {
+        super(message);
+    }
+}
