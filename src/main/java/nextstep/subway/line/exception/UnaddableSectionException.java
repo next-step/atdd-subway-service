ce1e3ec5b0c9841f5a7bc9229014acf2a43abdae
jordy-torvalds <jordy-torvalds@kakao.com>
2021-07-03 00:30:00 +0900

    feat: 추가 불가 구간 예외 구

---
+++ src/main/java/nextstep/subway/line/exception/UnaddableSectionException.java
@@ -0,0 +1,7 @@
+package nextstep.subway.line.exception;
+
+public class UnaddableSectionException extends RuntimeException {
+    public UnaddableSectionException(String message) {
+        super(message);
+    }
+}
