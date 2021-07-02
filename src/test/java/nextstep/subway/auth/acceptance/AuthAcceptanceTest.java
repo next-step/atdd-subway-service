807e5c9574c4510931b90c840db30e3e29169f6b
limdingdong <38725277+limdingdong@users.noreply.github.com>
2021-07-02 23:46:02 +0900

    test : 토큰인증 성공 및 실패 인수테스트 작성

--- src/test/java/nextstep/subway/auth/acceptance/AuthAcceptanceTest.java
+++ src/test/java/nextstep/subway/auth/acceptance/AuthAcceptanceTest.java
@@ -5,6 +5,7 @@ import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import nextstep.subway.AcceptanceTest;
 import nextstep.subway.auth.dto.TokenRequest;
+import nextstep.subway.auth.dto.TokenResponse;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -12,43 +13,61 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
 import static nextstep.subway.member.MemberAcceptanceTest.회원_생성을_요청;
+import static nextstep.subway.member.MemberAcceptanceTest.회원_정보_조회됨;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class AuthAcceptanceTest extends AcceptanceTest {
 
+    private static final String EMAIL = "probitanima11@gmail.com";
+    private static final String PASSWORD = "11";
+    private static final String WRONG_PASSWORD = "22";
+    private static final int AGE = 30;
+
     @BeforeEach
     public void setUp() {
         super.setUp();
-
         // given
-        회원_생성을_요청("probitanima11@gmail.com", "11", 30);
+        회원_생성을_요청(EMAIL, PASSWORD, AGE);
     }
 
     @DisplayName("Bearer Auth")
     @Test
     void myInfoWithBearerAuth() {
         // when
-        TokenRequest params = new TokenRequest("probitanima11@gmail.com", "11");
+        TokenRequest params = new TokenRequest(EMAIL, PASSWORD);
         ExtractableResponse<Response> loginResponse = 로그인_요청(params);
+        TokenResponse tokenResponse = loginResponse.as(TokenResponse.class);
+        ExtractableResponse<Response> authResponse = 토큰_인증(tokenResponse);
 
-        // when
+        // then
         로그인_됨(loginResponse);
+        토큰인증_성공(authResponse);
+        회원_정보_조회됨(authResponse, EMAIL, AGE);
     }
 
     @DisplayName("Bearer Auth 로그인 실패")
     @Test
     void myInfoWithBadBearerAuth() {
         // when
-        TokenRequest params = new TokenRequest("probitanima11@gmail.com", "22");
+        TokenRequest params = new TokenRequest(EMAIL, WRONG_PASSWORD);
         ExtractableResponse<Response> loginResponse = 로그인_요청(params);
 
-        // when
+        // then
         로그인_실패됨(loginResponse);
     }
 
     @DisplayName("Bearer Auth 유효하지 않은 토큰")
     @Test
     void myInfoWithWrongBearerAuth() {
+        // when
+        TokenRequest params = new TokenRequest(EMAIL, PASSWORD);
+        ExtractableResponse<Response> loginResponse = 로그인_요청(params);
+        TokenResponse tokenResponse = new TokenResponse("invalid");
+        ExtractableResponse<Response> authResponse = 토큰_인증(tokenResponse);
+
+        // when
+        로그인_됨(loginResponse);
+        토큰인증_실패(authResponse);
     }
 
     public static ExtractableResponse<Response> 로그인_요청(TokenRequest params) {
@@ -68,4 +87,22 @@ public class AuthAcceptanceTest extends AcceptanceTest {
     public static void 로그인_실패됨(ExtractableResponse<Response> response) {
         assertThat(response.statusCode()).isEqualTo(HttpStatus.UNAUTHORIZED.value());
     }
+
+    public static ExtractableResponse<Response> 토큰_인증(TokenResponse tokenResponse) {
+        return RestAssured
+                .given().log().all()
+                .auth().oauth2(tokenResponse.getAccessToken())
+                .accept(MediaType.APPLICATION_JSON_VALUE)
+                .when().get("/members/me")
+                .then().log().all()
+                .extract();
+    }
+
+    public static void 토큰인증_성공(ExtractableResponse<Response> response) {
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+    }
+    public static void 토큰인증_실패(ExtractableResponse<Response> response) {
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+    }
+    
 }
