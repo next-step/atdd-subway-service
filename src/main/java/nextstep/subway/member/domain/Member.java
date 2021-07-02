b9959b5282c0930a74d25985de508ef5de664181
dohchoi91 <dohchoi91@gmail.com>
2021-07-03 01:32:30 +0900

    refactor: 공통코드 패키지 변경

--- src/main/java/nextstep/subway/member/domain/Member.java
+++ src/main/java/nextstep/subway/member/domain/Member.java
@@ -1,7 +1,8 @@
 package nextstep.subway.member.domain;
 
-import nextstep.subway.BaseEntity;
 import nextstep.subway.auth.application.AuthorizationException;
+import nextstep.subway.common.BaseEntity;
+
 import org.apache.commons.lang3.StringUtils;
 
 import javax.persistence.Entity;
