ee073023b382aa30c3c9a4b5dbc8be02a5d865c9
limdingdong <38725277+limdingdong@users.noreply.github.com>
2021-07-03 14:57:29 +0900

    feat : Favorite, FavoriteRepository 신규정의

---
+++ src/main/java/nextstep/subway/favorite/domain/Favorite.java
@@ -0,0 +1,36 @@
+package nextstep.subway.favorite.domain;
+
+import nextstep.subway.BaseEntity;
+import nextstep.subway.member.domain.Member;
+import nextstep.subway.station.domain.Station;
+
+import javax.persistence.*;
+
+@Entity
+public class Favorite extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne(cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "source_station_id")
+    private Station sourceStation;
+
+    @ManyToOne(cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "target_station_id")
+    private Station targetStation;
+
+    protected Favorite() {
+    }
+
+    public Favorite(Member member, Station sourceStation, Station targetStation) {
+        this.member = member;
+        this.sourceStation = sourceStation;
+        this.targetStation = targetStation;
+    }
+}
