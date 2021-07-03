68efdee517d62b8eea0cb2604662271bbe890018
mskangg <funnycoding91@gmail.com>
2021-07-03 11:21:37 +0900

    refactor: 로그인하지 않은 객체도 담을수 있는 객체 추가

---
+++ src/main/java/nextstep/subway/auth/domain/IncompleteLoginMember.java
@@ -0,0 +1,24 @@
+package nextstep.subway.auth.domain;
+
+import nextstep.subway.auth.application.exception.AuthorizationException;
+
+import java.util.Objects;
+
+public class IncompleteLoginMember {
+    private final LoginMember loginMember;
+
+    public IncompleteLoginMember(LoginMember loginMember) {
+        this.loginMember = loginMember;
+    }
+
+    public LoginMember toCompleteLoginMember() {
+        if (!isCompleteLoginMember()) {
+            throw new AuthorizationException("토큰 검증에 실패하여 멤버 정보를 확인할 수 없습니다.");
+        }
+        return loginMember;
+    }
+
+    public boolean isCompleteLoginMember() {
+        return Objects.nonNull(loginMember);
+    }
+}
