d003158d28667e75fa24612e43fc4b5e504cddac
limdingdong <38725277+limdingdong@users.noreply.github.com>
2021-07-03 15:38:42 +0900

    test : FavoriteController 작성, 즐겨찾기 등록 api 구현

---
+++ src/main/java/nextstep/subway/station/ui/FavoriteController.java
@@ -0,0 +1,29 @@
+package nextstep.subway.station.ui;
+
+import nextstep.subway.auth.domain.AuthenticationPrincipal;
+import nextstep.subway.auth.domain.LoginMember;
+import nextstep.subway.station.application.FavoriteService;
+import nextstep.subway.station.dto.FavoriteRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/stations/favorite")
+public class FavoriteController {
+    private final FavoriteService favoriteService;
+
+    public FavoriteController(final FavoriteService favoriteService) {
+        this.favoriteService = favoriteService;
+    }
+
+    @PostMapping
+    public ResponseEntity createFavorite(@AuthenticationPrincipal LoginMember loginMember, @RequestBody FavoriteRequest favoriteRequest) {
+        Long favoriteId = favoriteService.saveFavorite(loginMember, favoriteRequest);
+        return ResponseEntity.ok(URI.create("/stations/favorite/" + favoriteId));
+    }
+}
