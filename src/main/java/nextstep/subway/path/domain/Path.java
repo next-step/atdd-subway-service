f806bfb7473130daa544eac9e570bc60fe7fb6ce
fDevJc <fDevJc@gmail.com>
2021-07-03 22:57:15 +0900

    refactor: add method, field to Path class

--- src/main/java/nextstep/subway/path/domain/Path.java
+++ src/main/java/nextstep/subway/path/domain/Path.java
@@ -1,17 +1,37 @@
 package nextstep.subway.path.domain;
 
+import nextstep.subway.line.domain.Line;
+import nextstep.subway.station.domain.Station;
+
 import java.util.List;
 
 public class Path {
-    private final List stations;
+    private final List<Line> transferLines;
+    private final List<Station> stations;
     private final double distance;
-    public Path(List stations, double distance) {
+
+    public Path(List<Station> stations, double distance) {
         this.stations = stations;
         this.distance = distance;
+        this.transferLines = null;
     }
 
-    public List getStations() {
+    public Path(List<Station> stations, double distance, List<Line> transferLines) {
+        this.stations = stations;
+        this.distance = distance;
+        this.transferLines = transferLines;
+    }
+
+    public static Path of(List<Station> stations, double distance, List<Line> transferLine) {
+        return new Path(stations, distance, transferLine);
+    }
+
+    public List<Station> getStations() {
         return stations;
+    }
+
+    public List<Line> getTransferLines() {
+        return transferLines;
     }
 
     public double getDistance() {
