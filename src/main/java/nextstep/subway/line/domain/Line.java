0a9cf54bf88e779bd4aef6e13775451eb53702fa
jordy-torvalds <jordy-torvalds@kakao.com>
2021-07-03 10:40:48 +0900

    fix: 불필요한 import문 제거 및 생성자에 빠져 있던 color 할당 로직 추

--- src/main/java/nextstep/subway/line/domain/Line.java
+++ src/main/java/nextstep/subway/line/domain/Line.java
@@ -5,10 +5,7 @@ import nextstep.subway.station.domain.Station;
 import nextstep.subway.station.domain.Stations;
 
 import javax.persistence.*;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.Optional;
 
 @Entity
 public class Line extends BaseEntity {
@@ -45,6 +42,7 @@ public class Line extends BaseEntity {
     public Line(long id, String name, String color, Station upStation, Station downStation, Distance distance) {
         this.id = id;
         this.name = name;
+        this.color = color;
         this.sections.add(new Section(this, upStation, downStation, distance));
     }
 
