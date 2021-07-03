774ed38edd2e2d6e0029a08113324a2219b2988f
fDevJc <fDevJc@gmail.com>
2021-07-04 02:17:37 +0900

    refactor: add enum

---
+++ src/main/java/nextstep/subway/path/domain/enums/AgeCategory.java
@@ -0,0 +1,33 @@
+package nextstep.subway.path.domain.enums;
+
+import nextstep.subway.path.domain.FarePolicy;
+
+import java.util.Arrays;
+import java.util.function.DoubleFunction;
+import java.util.function.Predicate;
+
+public enum AgeCategory implements FarePolicy {
+    ADULT(age -> age >= 19, fare -> fare),
+    TEENAGER(age -> age >= 13 && age < 19, fare -> (fare - 350) * 0.8),
+    CHILDREN(age -> age >= 6 && age < 13, fare -> (fare - 350) * 0.5),
+    BABY(age -> age >= 0 && age < 6, age -> Double.valueOf(0));
+    private final Predicate<Integer> predicate;
+    private final DoubleFunction<Double> function;
+
+    AgeCategory(Predicate<Integer> predicate, DoubleFunction<Double> function) {
+        this.predicate = predicate;
+        this.function = function;
+    }
+
+    public static AgeCategory findCategory(int age) {
+        return Arrays.stream(AgeCategory.values())
+                .filter(ageCategory -> ageCategory.predicate.test(age))
+                .findFirst()
+                .get();
+    }
+
+    @Override
+    public double calculate(double fare) {
+        return function.apply(fare);
+    }
+}
