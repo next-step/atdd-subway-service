99b9e7e77270f7b22b4d98b61197bb9eda227355
DESKTOP-T6O4081\lee <lsecret@naver.com>
2021-07-03 14:54:06 +0900

    feat: 최단 거리 경로 조회 테스트 코드 작성
    최단 거리 경로 조회 테스트 코드 작성

--- src/test/java/nextstep/subway/path/PathAcceptanceTest.java
+++ src/test/java/nextstep/subway/path/PathAcceptanceTest.java
@@ -1,9 +1,138 @@
 package nextstep.subway.path;
 
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
 import nextstep.subway.AcceptanceTest;
+import nextstep.subway.line.dto.LineRequest;
+import nextstep.subway.line.dto.LineResponse;
+import nextstep.subway.path.dto.PathResponse;
+import nextstep.subway.station.dto.StationResponse;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static nextstep.subway.line.acceptance.LineAcceptanceTest.지하철_노선_등록되어_있음;
+import static nextstep.subway.line.acceptance.LineSectionAcceptanceTest.지하철_노선에_지하철역_등록되어_있음;
+import static nextstep.subway.station.StationAcceptanceTest.지하철역_등록되어_있음;
+import static org.assertj.core.api.Assertions.assertThat;
 
 
 @DisplayName("지하철 경로 조회")
 public class PathAcceptanceTest extends AcceptanceTest {
+    private LineResponse 신분당선;
+    private LineResponse 이호선;
+    private LineResponse 삼호선;
+    private LineResponse 사호선;
+    private StationResponse 교대역;
+    private StationResponse 강남역;
+    private StationResponse 남부터미널역;
+    private StationResponse 양재역;
+    private StationResponse 광교역;
+    private StationResponse 안산역;
+    private StationResponse 의정부역;
+
+    @BeforeEach
+    public void setUp() {
+        super.setUp();
+
+        교대역 = 지하철역_등록되어_있음("교대역").as(StationResponse.class);
+        남부터미널역 = 지하철역_등록되어_있음("남부터미널역").as(StationResponse.class);
+        강남역 = 지하철역_등록되어_있음("강남역").as(StationResponse.class);
+        양재역 = 지하철역_등록되어_있음("양재역").as(StationResponse.class);
+        광교역 = 지하철역_등록되어_있음("광교역").as(StationResponse.class);
+        안산역 = 지하철역_등록되어_있음("안산역").as(StationResponse.class);
+        의정부역 = 지하철역_등록되어_있음("의정부역").as(StationResponse.class);
+
+        이호선 = 지하철_노선_등록되어_있음(new LineRequest("이호선", "bg-green-600", 교대역.getId(), 강남역.getId(), 10)).as(LineResponse.class);
+        삼호선 = 지하철_노선_등록되어_있음(new LineRequest("삼호선", "bg-red-600", 교대역.getId(), 양재역.getId(), 30)).as(LineResponse.class);
+        신분당선 = 지하철_노선_등록되어_있음(new LineRequest("신분당선", "bg-red-800", 강남역.getId(), 광교역.getId(), 50)).as(LineResponse.class);
+        사호선 = 지하철_노선_등록되어_있음(new LineRequest("사호선", "bg-blue-600", 안산역.getId(), 의정부역.getId(), 100)).as(LineResponse.class);
+
+        지하철_노선에_지하철역_등록되어_있음(삼호선, 교대역, 남부터미널역, 15);
+        지하철_노선에_지하철역_등록되어_있음(신분당선, 강남역, 양재역, 10);
+    }
+
+    @DisplayName("두 역의 최단 거리 경로 조회")
+    @Test
+    void findPath() {
+        //when
+        ExtractableResponse<Response> response = 지하철_노선_경로_조회_요청(교대역.getId(), 양재역.getId());
+
+        //then
+        지하철_노선_경로_응답됨(response, Arrays.asList(교대역, 강남역, 양재역));
+    }
+
+    @DisplayName("두 역의 최단 거리 경로 조회 실패 - 같은 역을 조회 할 경우")
+    @Test
+    void findPathFailBySameStations() {
+        //when
+        ExtractableResponse<Response> response = 지하철_노선_경로_조회_요청(교대역.getId(), 교대역.getId());
+
+        //then
+        지하철_노선_경로_조회_실패됨(response);
+    }
+
+    @DisplayName("두 역의 최단 거리 경로 조회 실패 - 두 역이 연결되어 있지 않을 경우")
+    @Test
+    void findPathFailByNotConnected() {
+        //when
+        ExtractableResponse<Response> response = 지하철_노선_경로_조회_요청(안산역.getId(), 교대역.getId());
+
+        //then
+        지하철_노선_경로_조회_실패됨(response);
+    }
+
+    @DisplayName("두 역의 최단 거리 경로 조회 실패 - 존재하지 않는 출발 역 조회")
+    @Test
+    void findPathFailByNotExistsSourceStation() {
+        //when
+        ExtractableResponse<Response> response = 지하철_노선_경로_조회_요청(100L, 교대역.getId());
+
+        //then
+        지하철_노선_경로_조회_실패됨(response);
+    }
+
+    @DisplayName("두 역의 최단 거리 경로 조회 실패 - 존재하지 않는 도착 역 조회")
+    @Test
+    void findPathFailByNotExistsTargetStation() {
+        //when
+        ExtractableResponse<Response> response = 지하철_노선_경로_조회_요청(교대역.getId(), 100L);
+
+        //then
+        지하철_노선_경로_조회_실패됨(response);
+    }
+
+
+    public static ExtractableResponse<Response> 지하철_노선_경로_조회_요청(long source, long target) {
+        return RestAssured.given().log().all()
+                .accept(MediaType.APPLICATION_JSON_VALUE)
+                .when().get("/paths?source={sourceId}&target={targetId}", source, target)
+                .then().log().all()
+                .extract();
+    }
+
+    public static void 지하철_노선_경로_응답됨(ExtractableResponse<Response> response, List<StationResponse> stationResponses) {
+
+        List<Long> stations = response.as(PathResponse.class).getStations().stream()
+                .map(StationResponse::getId)
+                .collect(Collectors.toList());
+
+        List<Long> responseStations = stationResponses.stream()
+                .map(StationResponse::getId)
+                .collect(Collectors.toList());
+
+        assertThat(stations).containsExactlyElementsOf(responseStations);
+    }
+
+    private static void 지하철_노선_경로_조회_실패됨(ExtractableResponse<Response> response) {
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+    }
+
 }
