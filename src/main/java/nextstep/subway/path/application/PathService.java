eb026bd95f9c1bf2ea13e5933dd1916630b6a3b7
dohchoi91 <dohchoi91@gmail.com>
2021-07-03 09:25:05 +0900

    refactor: 경로 서비스 기능을 경로 도메인으로 수정

--- src/main/java/nextstep/subway/path/application/PathService.java
+++ src/main/java/nextstep/subway/path/application/PathService.java
@@ -3,18 +3,13 @@ package nextstep.subway.path.application;
 import static java.util.stream.Collectors.*;
 
 import java.util.List;
-import java.util.Objects;
 
-import org.jgrapht.GraphPath;
-import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
-import org.jgrapht.graph.DefaultWeightedEdge;
-import org.jgrapht.graph.WeightedMultigraph;
 import org.springframework.stereotype.Service;
 
+import nextstep.subway.line.domain.Line;
 import nextstep.subway.line.domain.LineRepository;
+import nextstep.subway.path.domain.Path;
 import nextstep.subway.path.dto.PathResponse;
-import nextstep.subway.path.exception.DuplicatePathException;
-import nextstep.subway.path.exception.NotConnectedPathException;
 import nextstep.subway.station.domain.Station;
 import nextstep.subway.station.domain.StationRepository;
 import nextstep.subway.station.dto.StationResponse;
@@ -34,36 +29,12 @@ public class PathService {
     public PathResponse findPath(Long source, Long target) {
         Station start = stationRepository.findById(source).orElseThrow(NotFoundStationException::new);
         Station end = stationRepository.findById(target).orElseThrow(NotFoundStationException::new);
+        List<Line> lines = lineRepository.findAll();
+        Path path = new Path(lines);
 
-        if (start.equals(end)) {
-            throw new DuplicatePathException();
-        }
+        List<StationResponse> stations = path.findShortestPath(start, end).stream().map(StationResponse::of).collect(toList());
+        int minDistance = path.findShortestDistance(start, end);
 
-        WeightedMultigraph<Station, DefaultWeightedEdge> map = getPathMap();
-        GraphPath<Station, DefaultWeightedEdge> path = findShortestPath(map, start, end);
-
-        List<StationResponse> collect = path.getVertexList().stream().map(StationResponse::of).collect(toList());
-        return PathResponse.of(collect, path.getWeight());
-    }
-
-    private GraphPath<Station, DefaultWeightedEdge> findShortestPath(WeightedMultigraph<Station, DefaultWeightedEdge> map,
-                                                                     Station start, Station end) {
-        GraphPath<Station, DefaultWeightedEdge> path = new DijkstraShortestPath(map).getPath(start, end);
-        if (Objects.isNull(path)) {
-            throw new NotConnectedPathException();
-        }
-        return path;
-    }
-
-    private WeightedMultigraph<Station, DefaultWeightedEdge> getPathMap() {
-        WeightedMultigraph<Station, DefaultWeightedEdge> map = new WeightedMultigraph(DefaultWeightedEdge.class);
-        lineRepository.findAll().forEach(line -> {
-            line.getStations().forEach(map::addVertex);
-            line.getSections().forEach(section -> {
-                DefaultWeightedEdge edge = map.addEdge(section.getUpStation(), section.getDownStation());
-                map.setEdgeWeight(edge, section.getDistance());
-            });
-        });
-        return map;
+        return PathResponse.of(stations, minDistance);
     }
 }
