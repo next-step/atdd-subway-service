2acaa60f19bc29a6ec982e9e30534851e3c13fd3
limdingdong <38725277+limdingdong@users.noreply.github.com>
2021-07-05 12:48:32 +0900

    feat : 거리비례 요금 계산을 위한 정책클래스 DistanceBasedFarePolicy 추가

---
+++ src/main/java/nextstep/subway/station/domain/DistanceBasedFarePolicy.java
@@ -0,0 +1,45 @@
+package nextstep.subway.station.domain;
+
+import java.util.Arrays;
+
+public enum DistanceBasedFarePolicy {
+
+    DISTANCE_1(10, 50, 5, 100),
+    DISTANCE_2(50, Integer.MAX_VALUE, 8, 100);
+
+    private static final int ZERO_FARE = 0;
+    private static final int DISTANCE_NONE = -1;
+
+    private int minDistance;
+    private int maxDistance;
+    private int chargeUnit;
+    private int overFare;
+
+    DistanceBasedFarePolicy(int minDistance, int maxDistance, int chargeUnit, int overFare) {
+        this.minDistance = minDistance;
+        this.maxDistance = maxDistance;
+        this.chargeUnit = chargeUnit;
+        this.overFare = overFare;
+    }
+
+    public static int overFare(int distance) {
+        return Arrays.stream(values())
+                .mapToInt(policy -> policy.calculateOverFare(distance))
+                .sum();
+    }
+
+    private int calculateOverFare(int distance) {
+        distance = calculateDistancePerUnit(distance);
+        if (distance == DISTANCE_NONE) {
+            return ZERO_FARE;
+        }
+        return (int) (Math.ceil((distance - 1) / chargeUnit) + 1) * overFare;
+    }
+
+    private int calculateDistancePerUnit(int distance) {
+        if (distance < minDistance) {
+            return DISTANCE_NONE;
+        }
+        return Math.min(distance, maxDistance) - minDistance;
+    }
+}
