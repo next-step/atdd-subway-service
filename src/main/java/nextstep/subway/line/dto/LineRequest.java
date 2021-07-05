4caafa7ed765ab21743cc4848417e5875c3e704a
oper912 <oper912@naver.com>
2021-07-06 00:37:12 +0900

    feat: 노선 요청 dto 추가 요금 필드 추가

--- src/main/java/nextstep/subway/line/dto/LineRequest.java
+++ src/main/java/nextstep/subway/line/dto/LineRequest.java
@@ -8,6 +8,7 @@ public class LineRequest {
     private Long upStationId;
     private Long downStationId;
     private int distance;
+    private int AdditionalFare;
 
     public LineRequest() {
     }
@@ -18,6 +19,15 @@ public class LineRequest {
         this.upStationId = upStationId;
         this.downStationId = downStationId;
         this.distance = distance;
+    }
+
+    public LineRequest(String name, String color, Long upStationId, Long downStationId, int distance, int additionalFare) {
+        this.name = name;
+        this.color = color;
+        this.upStationId = upStationId;
+        this.downStationId = downStationId;
+        this.distance = distance;
+        AdditionalFare = additionalFare;
     }
 
     public String getName() {
