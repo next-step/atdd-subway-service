3014f353a047f6e44dea0b6da1b6125b12e059c2
hun <ini8262@itnomads.com>
2021-07-06 00:01:35 +0900

    refac : 코드피드백 반영 - enum 클래스 네이밍 변경

--- src/main/java/nextstep/subway/path/infra/RatePolicyByAge.java
+++ src/main/java/nextstep/subway/path/infra/RatePolicyByAge.java
@@ -5,25 +5,25 @@ import java.util.function.Function;
 
 public class RatePolicyByAge implements RatePolicy {
     private static final int DEDUCTIBLE_AMOUNT = 350;
-    private final AgeGroup ageGroup;
+    private final AgeSpecificRule ageSpecificRule;
 
     public RatePolicyByAge(final int age) {
-        this.ageGroup = AgeGroup.valueOf(age);
+        this.ageSpecificRule = AgeSpecificRule.valueOf(age);
     }
 
     @Override
     public double calc(double fee) {
-        return ageGroup.calc(fee);
+        return ageSpecificRule.calc(fee);
     }
 
-    private enum AgeGroup {
+    private enum AgeSpecificRule {
         CHILDREN(fee -> (fee - DEDUCTIBLE_AMOUNT) - ((fee - DEDUCTIBLE_AMOUNT) / 100) * 50),
         TEENAGER(fee -> (fee - DEDUCTIBLE_AMOUNT) - (((fee - DEDUCTIBLE_AMOUNT) / 100) * 20)),
         ADULT(fee -> fee);
 
         private final Function<Double, Double> discountsFunction;
 
-        AgeGroup(Function<Double, Double> discountsFunction) {
+        AgeSpecificRule(Function<Double, Double> discountsFunction) {
             this.discountsFunction = discountsFunction;
         }
 
@@ -31,7 +31,7 @@ public class RatePolicyByAge implements RatePolicy {
             return discountsFunction.apply(fee);
         }
 
-        private static AgeGroup valueOf(int age) {
+        private static AgeSpecificRule valueOf(int age) {
             if (age < 13) {
                 return CHILDREN;
             }
