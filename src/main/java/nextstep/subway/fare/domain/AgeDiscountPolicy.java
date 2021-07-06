a7b27b9e843cbe1d52c33fc626ac7319808a778a
tmkwak <tmkwak@hmm21.com>
2021-07-06 13:44:42 +0900

    refactor : AgeDiscountPolicy, DistanceFarePolicy static final 제거

--- src/main/java/nextstep/subway/fare/domain/AgeDiscountPolicy.java
+++ src/main/java/nextstep/subway/fare/domain/AgeDiscountPolicy.java
@@ -1,7 +1,7 @@
 package nextstep.subway.fare.domain;
 
 public interface AgeDiscountPolicy {
-	static final int DEFAULT_DISCOUNT = 350;
+	int DEFAULT_DISCOUNT = 350;
 	Fare discount(Fare fare);
 	boolean isAccepted(int age);
 }
--- src/main/java/nextstep/subway/fare/domain/DistanceFarePolicy.java
+++ src/main/java/nextstep/subway/fare/domain/DistanceFarePolicy.java
@@ -3,7 +3,7 @@ package nextstep.subway.fare.domain;
 import nextstep.subway.line.domain.Distance;
 
 public interface DistanceFarePolicy {
-	public static final int DEFAULT_FARE = 1250;
+	int DEFAULT_FARE = 1250;
 
 	Fare calculate(Distance distance);
 	boolean isAccepted(Distance distance);
