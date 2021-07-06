0298e539de7753bb086f32da32c44e7b2706c922
sanggu <tkdrn8578@naver.com>
2021-07-06 22:56:54 +0900

    refactor: 가독성을 위한 method extract 진행

--- src/main/java/nextstep/subway/path/domain/PathFinder.java
+++ src/main/java/nextstep/subway/path/domain/PathFinder.java
@@ -14,19 +14,33 @@ public class PathFinder {
     public static List<Station> findShortest(Station source, Station target, List<Line> lines) {
         WeightedMultigraph<Station, DefaultWeightedEdge> graph = new WeightedMultigraph<>(DefaultWeightedEdge.class);
 
-        for (Line line : lines) {
-            List<Section> sections = line.getSections();
+        addAllLineSectionsToGraph(lines, graph);
 
-            for (Section section : sections) {
-                Station upStation = section.getUpStation();
-                Station downStation = section.getDownStation();
+        return createShortestPaths(source, target, graph);
+    }
 
-                graph.addVertex(upStation);
-                graph.addVertex(downStation);
-                graph.setEdgeWeight(graph.addEdge(upStation, downStation), section.getDistance());
-            }
-        }
+    private static void addAllLineSectionsToGraph(List<Line> lines, WeightedMultigraph<Station, DefaultWeightedEdge> graph) {
+        lines.stream()
+                .map(Line::getSections)
+                .forEach(sections -> addSectionsToGraph(graph, sections));
+    }
 
+    private static List<Station> createShortestPaths(Station source, Station target, WeightedMultigraph<Station, DefaultWeightedEdge> graph) {
         return new DijkstraShortestPath<>(graph).getPath(source, target).getVertexList();
+    }
+
+    private static void addSectionsToGraph(WeightedMultigraph<Station, DefaultWeightedEdge> graph, List<Section> sections) {
+        for (Section section : sections) {
+            addSectionToGraph(graph, section);
+        }
+    }
+
+    private static void addSectionToGraph(WeightedMultigraph<Station, DefaultWeightedEdge> graph, Section section) {
+        Station upStation = section.getUpStation();
+        Station downStation = section.getDownStation();
+
+        graph.addVertex(upStation);
+        graph.addVertex(downStation);
+        graph.setEdgeWeight(graph.addEdge(upStation, downStation), section.getDistance());
     }
 }
