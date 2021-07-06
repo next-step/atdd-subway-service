6aa474db1313654a453f56ce2ee3f3f5c0d0309f
Jiseon Lee <leejoyc9310@gmail.com>
2021-07-06 21:09:38 +0900

    test : PathFinderTest에서 요금관련 테스트 제거

--- src/test/java/nextstep/subway/path/domain/PathFinderTest.java
+++ src/test/java/nextstep/subway/path/domain/PathFinderTest.java
@@ -96,28 +96,4 @@ class PathFinderTest {
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(Message.ERROR_PATH_NOT_FOUND.showText());
     }
-
-    @DisplayName("최단경로 조회 후, 노선의 추가요금 결정")
-    @Test
-    void 노선의_추가요금을_반영() {
-        //when
-        int 육호선_추가금액 = 육호선.getExtraCharge();
-        Path shortestPath = pathFinder.getDijkstraShortestPath(연신내역, 응암역);
-
-        //then
-        assertThat(shortestPath.getFare()).isEqualTo(BASE_FARE + 육호선_추가금액);
-    }
-
-    @DisplayName("환승 시, 최대 추가요금 반영")
-    @Test
-    void 여러_노선_환승시_추가요금_최대값만_반영() {
-        //when
-        int 사호선_추가금액 = 사호선.getExtraCharge();
-        Path shortestPath = pathFinder.getDijkstraShortestPath(연신내역, 회현역);
-
-        //then
-        assertThat(shortestPath.getFare()).isEqualTo(BASE_FARE + 사호선_추가금액);
-        assertThat(shortestPath.getStations()).hasSize(3)
-                .containsExactly(연신내역, 응암역, 회현역);
-    }
 }
