c7b092f13d3332c8ed25bbe785d24a8940db9faa
sanggu <tkdrn8578@naver.com>
2021-07-06 22:47:42 +0900

    refactor: mock 경로 로직을 product 로직으로 변경

--- src/main/java/nextstep/subway/path/domain/PathFinder.java
+++ src/main/java/nextstep/subway/path/domain/PathFinder.java
@@ -1,13 +1,32 @@
 package nextstep.subway.path.domain;
 
 import nextstep.subway.line.domain.Line;
+import nextstep.subway.line.domain.Section;
 import nextstep.subway.station.domain.Station;
+import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
+import org.jgrapht.graph.DefaultWeightedEdge;
+import org.jgrapht.graph.WeightedMultigraph;
 
 import java.util.List;
 
 public class PathFinder {
 
     public static List<Station> findShortest(Station source, Station target, List<Line> lines) {
-        return List.of(source, target, new Station("발산역"));
+        WeightedMultigraph<Station, DefaultWeightedEdge> graph = new WeightedMultigraph<>(DefaultWeightedEdge.class);
+
+        for (Line line : lines) {
+            List<Section> sections = line.getSections();
+
+            for (Section section : sections) {
+                Station upStation = section.getUpStation();
+                Station downStation = section.getDownStation();
+
+                graph.addVertex(upStation);
+                graph.addVertex(downStation);
+                graph.setEdgeWeight(graph.addEdge(upStation, downStation), section.getDistance());
+            }
+        }
+
+        return new DijkstraShortestPath<>(graph).getPath(source, target).getVertexList();
     }
 }
