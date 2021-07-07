05a5c1e84be7fe538f627b56e53c8b83219dea9e
KIM GI TAE <c0433c@gmail.com>
2021-07-08 01:46:25 +0900

    style(노선): 노선별 요금 정책 변수명 변경
    - 이해하기 쉽도록 변수명 변경

--- src/test/java/nextstep/subway/path/unit/LineFarePolicyTest.java
+++ src/test/java/nextstep/subway/path/unit/LineFarePolicyTest.java
@@ -17,14 +17,14 @@ public class LineFarePolicyTest {
 	@DisplayName("노선별 요금 정책 검증")
 	@CsvSource({"100,400,200", "400,0,10", "200,0,6000"})
 	@ParameterizedTest
-	void fare(int a, int b, int c) {
+	void fare(int extraFare1, int extraFare2, int extraFare3) {
 		List<Line> lines = Arrays.asList(
-			new Line("name1", "color1", a, null, null, 1),
-			new Line("name2", "color2", b, null, null, 1),
-			new Line("name3", "color3", c, null, null, 1)
+			new Line("name1", "color1", extraFare1, null, null, 1),
+			new Line("name2", "color2", extraFare2, null, null, 1),
+			new Line("name3", "color3", extraFare3, null, null, 1)
 		);
 		LineFarePolicy lineFarePolicy = new LineFarePolicy(lines);
-		int max = Math.max(Math.max(a, b), c);
+		int max = Math.max(Math.max(extraFare1, extraFare2), extraFare3);
 
 		assertThat(lineFarePolicy.fare()).isEqualTo(max);
 	}
