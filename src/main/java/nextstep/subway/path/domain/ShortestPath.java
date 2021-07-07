c0b7f86d287e34c2a20ccfbf0143020ade2c1ffc
Donghui-Lee <ehdgml3206@naver.com>
2021-07-07 19:34:04 +0900

    feat: 경로 조회 시, 경로들의 운임 비용 계산을 담당하는 클래스 추가

---
+++ src/main/java/nextstep/subway/path/domain/ShortestPath.java
@@ -0,0 +1,51 @@
+package nextstep.subway.path.domain;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.jgrapht.GraphPath;
+
+import nextstep.subway.line.domain.Fare;
+import nextstep.subway.station.domain.Station;
+import nextstep.subway.station.dto.StationResponse;
+
+public class ShortestPath {
+    private final List<Station> stations;
+    private final int distance;
+    private final int fare;
+
+    private ShortestPath(List<Station> stations, int distance, int fare) {
+        this.stations = stations;
+        this.distance = distance;
+        this.fare = fare;
+    }
+
+    public static ShortestPath of(GraphPath<Station, FareWeightedEdge> graph) {
+        return new ShortestPath(graph.getVertexList(), (int) graph.getWeight(), maxFare(graph.getEdgeList()));
+    }
+
+    private static int maxFare(List<FareWeightedEdge> edges) {
+        return edges.stream().map(FareWeightedEdge::getFare)
+                .map(Fare::toNumber)
+                .max(Integer::compareTo).orElse(0);
+    }
+
+    public int calculateFareWithPolicy() {
+        int overFare = fare + DistanceFarePolicy.calculate(distance);
+        return overFare;
+    }
+
+    public List<Station> getStations() {
+        return stations;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+
+    public List<StationResponse> toStationResponse() {
+        return stations.stream()
+                .map(StationResponse::of)
+                .collect(Collectors.toList());
+    }
+}
