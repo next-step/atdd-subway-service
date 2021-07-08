614d0b6089adb22741ad9a6a88d285cfaedcb9bc
dohchoi91 <dohchoi91@gmail.com>
2021-07-08 20:27:00 +0900

    refactor: 사용자, 즐겨찾기 양방향 관계를 단방향으로 변경

--- src/main/java/nextstep/subway/member/domain/Member.java
+++ src/main/java/nextstep/subway/member/domain/Member.java
@@ -2,21 +2,15 @@ package nextstep.subway.member.domain;
 
 import static nextstep.subway.auth.common.Constants.*;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import nextstep.subway.auth.application.AuthorizationException;
-import nextstep.subway.common.BaseEntity;
-import nextstep.subway.favorite.domain.Favorite;
-
-import org.apache.commons.lang3.StringUtils;
-
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
+
+import org.apache.commons.lang3.StringUtils;
+
+import nextstep.subway.auth.application.AuthorizationException;
+import nextstep.subway.common.BaseEntity;
 
 @Entity
 public class Member extends BaseEntity {
@@ -26,9 +20,6 @@ public class Member extends BaseEntity {
     private String email;
     private String password;
     private Integer age;
-
-    @OneToMany(mappedBy = "member", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<Favorite> favorites = new ArrayList<>();
 
     public Member() {
     }
