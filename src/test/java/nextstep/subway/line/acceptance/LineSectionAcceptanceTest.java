98cabade98d4b954520e5f2cbc0a31e76f57f6b9
kimsanggu <tkdrn8578@paymint.co.kr>
2021-07-08 13:59:25 +0900

    refactor: 넘어가는 라인에 대한 개행 추가

--- src/test/java/nextstep/subway/line/acceptance/LineSectionAcceptanceTest.java
+++ src/test/java/nextstep/subway/line/acceptance/LineSectionAcceptanceTest.java
@@ -123,7 +123,12 @@ public class LineSectionAcceptanceTest extends AcceptanceTest {
         return 지하철_노선에_지하철역_등록_요청(line, upStation, downStation, distance);
     }
 
-    public static ExtractableResponse<Response> 지하철_노선에_지하철역_등록_요청(LineResponse line, StationResponse upStation, StationResponse downStation, int distance) {
+    public static ExtractableResponse<Response> 지하철_노선에_지하철역_등록_요청(
+            LineResponse line,
+            StationResponse upStation,
+            StationResponse downStation,
+            int distance
+    ) {
         SectionRequest sectionRequest = new SectionRequest(upStation.getId(), downStation.getId(), distance);
 
         return RestAssured
