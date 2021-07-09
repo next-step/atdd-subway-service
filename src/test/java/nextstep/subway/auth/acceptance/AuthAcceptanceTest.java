4d2df491187cf64d0c1c8f6617cf992b0203b07f
kimsanggu <tkdrn8578@paymint.co.kr>
2021-07-09 15:11:17 +0900

    test: 패스워드가 틀려 로그인 실패하는 경우에 대한 인수테스트 추가

--- src/test/java/nextstep/subway/auth/acceptance/AuthAcceptanceTest.java
+++ src/test/java/nextstep/subway/auth/acceptance/AuthAcceptanceTest.java
@@ -33,30 +33,38 @@ public class AuthAcceptanceTest extends AcceptanceTest {
         토큰이_포함됨(token);
     }
 
-    private void 토큰이_포함됨(TokenResponse token) {
-        assertThat(token.getAccessToken()).isNotBlank();
+    @DisplayName("Bearer Auth 로그인 실패")
+    @Test
+    void myInfoWithBadBearerAuth() {
+        회원_생성을_요청("sgkim94@github.com", "123456", 28);
+        TokenRequest request = new TokenRequest("sgkim94@github.com", "234567");
+
+        // when
+        ExtractableResponse<Response> response = 회원_로그인을_요청(request);
+
+        // then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.UNAUTHORIZED.value());
+    }
+
+    @DisplayName("Bearer Auth 유효하지 않은 토큰")
+    @Test
+    void myInfoWithWrongBearerAuth() {
     }
 
     private void 로그인_성공됨(ExtractableResponse<Response> response) {
         assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
     }
 
+    private void 토큰이_포함됨(TokenResponse token) {
+        assertThat(token.getAccessToken()).isNotBlank();
+    }
+
     private ExtractableResponse<Response> 회원_로그인을_요청(TokenRequest request) {
         return RestAssured
-                    .given().log().all()
-                    .body(request)
-                    .contentType(MediaType.APPLICATION_JSON_VALUE)
-                    .when().post("/login/token")
-                    .then().log().all().extract();
-    }
-
-    @DisplayName("Bearer Auth 로그인 실패")
-    @Test
-    void myInfoWithBadBearerAuth() {
-    }
-
-    @DisplayName("Bearer Auth 유효하지 않은 토큰")
-    @Test
-    void myInfoWithWrongBearerAuth() {
+                .given().log().all()
+                .body(request)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when().post("/login/token")
+                .then().log().all().extract();
     }
 }
