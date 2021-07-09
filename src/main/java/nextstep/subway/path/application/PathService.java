70bc541514de36de1f08a4ce9cfd83bddc1a19fd
younghoon <bblnotbb@yahho.com>
2021-07-09 21:33:23 +0900

    feat : change structure of class

--- src/main/java/nextstep/subway/path/application/PathService.java
+++ src/main/java/nextstep/subway/path/application/PathService.java
@@ -3,14 +3,11 @@ package nextstep.subway.path.application;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
-import org.jgrapht.graph.DefaultWeightedEdge;
-import org.jgrapht.graph.WeightedMultigraph;
 import org.springframework.stereotype.Service;
 
 import nextstep.subway.line.domain.Line;
 import nextstep.subway.line.domain.LineRepository;
-import nextstep.subway.line.domain.Section;
+import nextstep.subway.path.domain.PathFinder;
 import nextstep.subway.path.dto.PathResponse;
 import nextstep.subway.station.application.StationService;
 import nextstep.subway.station.domain.Station;
@@ -30,25 +27,14 @@ public class PathService {
 		List<Line> lines = lineRepository.findAll();
 		Station startStation = stationService.findStationById(sourceId);
 		Station destinationStation = stationService.findStationById(targetId);
-		WeightedMultigraph<String, DefaultWeightedEdge> graph = new WeightedMultigraph(DefaultWeightedEdge.class);
+		PathFinder pathFinder = new PathFinder(lines);
 
-		for (Line line : lines) {
-			for (Station station : line.getStations()) {
-				graph.addVertex(station.getName());
-			}
-
-			for (Section section : line.getSection()) {
-				graph.setEdgeWeight(graph.addEdge(section.getUpStation().getName(), section.getDownStation().getName()), section.getDistance());
-			}
-		}
-
-		DijkstraShortestPath dijkstraShortestPath = new DijkstraShortestPath(graph);
-		List<String> shortestPath = dijkstraShortestPath.getPath(startStation.getName(), destinationStation.getName()).getVertexList();
+		List<String> shortestPath = pathFinder.findPath(startStation, destinationStation);
 
 		List<StationResponse> stationResponses = new ArrayList<>();
 		for (String stationName : shortestPath) {
-			stationResponses.add(StationResponse.of(new Station(stationName)));
+			stationResponses.add(StationResponse.of(stationService.findByName(stationName)));
 		}
-		return new PathResponse(stationResponses, (int) dijkstraShortestPath.getPathWeight(startStation.getName(), destinationStation.getName()));
+		return new PathResponse(stationResponses, pathFinder.findPathLength(startStation, destinationStation));
 	}
 }
