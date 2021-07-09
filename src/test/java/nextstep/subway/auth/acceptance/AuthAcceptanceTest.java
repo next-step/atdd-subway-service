f58e9203bd56f948e34831634a420abe70b8a44a
kimsanggu <tkdrn8578@paymint.co.kr>
2021-07-09 15:09:11 +0900

    refactor: 가독성을 위한 extract method 진행

--- src/test/java/nextstep/subway/auth/acceptance/AuthAcceptanceTest.java
+++ src/test/java/nextstep/subway/auth/acceptance/AuthAcceptanceTest.java
@@ -24,18 +24,30 @@ public class AuthAcceptanceTest extends AcceptanceTest {
         TokenRequest request = new TokenRequest("sgkim94@github.com", "123456");
 
         // when
-        ExtractableResponse<Response> response = RestAssured
-                .given().log().all()
-                .body(request)
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .when().post("/login/token")
-                .then().log().all().extract();
-
+        ExtractableResponse<Response> response = 회원_로그인을_요청(request);
 
         // then
         TokenResponse token = response.body().as(TokenResponse.class);
-        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+
+        로그인_성공됨(response);
+        토큰이_포함됨(token);
+    }
+
+    private void 토큰이_포함됨(TokenResponse token) {
         assertThat(token.getAccessToken()).isNotBlank();
+    }
+
+    private void 로그인_성공됨(ExtractableResponse<Response> response) {
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+    }
+
+    private ExtractableResponse<Response> 회원_로그인을_요청(TokenRequest request) {
+        return RestAssured
+                    .given().log().all()
+                    .body(request)
+                    .contentType(MediaType.APPLICATION_JSON_VALUE)
+                    .when().post("/login/token")
+                    .then().log().all().extract();
     }
 
     @DisplayName("Bearer Auth 로그인 실패")
