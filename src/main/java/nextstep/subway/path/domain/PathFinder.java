f2749f87e7d6a55a8dde1481b9107772680bf1c1
younghoon <bblnotbb@yahho.com>
2021-07-09 18:24:55 +0900

    feat : sepcify path finder class

--- src/main/java/nextstep/subway/path/domain/PathFinder.java
+++ src/main/java/nextstep/subway/path/domain/PathFinder.java
@@ -3,21 +3,38 @@ package nextstep.subway.path.domain;
 import java.util.Arrays;
 import java.util.List;
 
+import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
+import org.jgrapht.graph.DefaultWeightedEdge;
+import org.jgrapht.graph.WeightedMultigraph;
+
 import nextstep.subway.line.domain.Line;
+import nextstep.subway.line.domain.Section;
 import nextstep.subway.station.domain.Station;
 
 public class PathFinder {
-	private List<Line> lines;
+	private DijkstraShortestPath dijkstraShortestPath;;
 
 	public PathFinder(List<Line> lines) {
-		this.lines = lines;
+		WeightedMultigraph<String, DefaultWeightedEdge> graph = new WeightedMultigraph(DefaultWeightedEdge.class);
+		
+		for (Line line : lines) {
+			for (Station station : line.getStations()) {
+				graph.addVertex(station.getName());
+			}
+
+			for (Section section : line.getSection()) {
+				graph.setEdgeWeight(graph.addEdge(section.getUpStation().getName(), section.getDownStation().getName()), section.getDistance());
+			}
+		}
+
+		this.dijkstraShortestPath = new DijkstraShortestPath(graph);
 	}
 
 	public List<String> findPath(Station startStation, Station destinationStation) {
-		return Arrays.asList("신촌역", "홍대입구역");
+		return dijkstraShortestPath.getPath(startStation.getName(), destinationStation.getName()).getVertexList();
 	}
 
 	public int findPathLength(Station startStation, Station destinationStation) {
-		return 7;
+		return (int) dijkstraShortestPath.getPathWeight(startStation.getName(), destinationStation.getName());
 	}
 }
