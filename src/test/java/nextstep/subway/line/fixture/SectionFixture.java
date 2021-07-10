72b518ab652adefabdc5294be67b001bf55c71ff
jordy-torvalds <jordy-torvalds@kakao.com>
2021-07-10 09:43:47 +0900

    refactor: 테스트 픽스쳐를 통한 반복적인 테스트 데이터 생성 제거

---
+++ src/test/java/nextstep/subway/line/fixture/SectionFixture.java
@@ -0,0 +1,28 @@
+package nextstep.subway.line.fixture;
+
+import nextstep.subway.line.domain.Distance;
+import nextstep.subway.line.domain.Section;
+import nextstep.subway.line.domain.Sections;
+
+import static java.util.Arrays.asList;
+import static nextstep.subway.station.fixture.StationFixture.*;
+import static nextstep.subway.line.fixture.LineFixture.*;
+
+public class SectionFixture {
+    public static Section 구간_양평역_영등포구청역 = new Section(1L, 오호선, 양평역, 영등포구청역, new Distance(10));
+    public static Section 구간_영등포구청역_영등포시장역 = new Section(2L, 오호선, 영등포구청역, 영등포시장역, new Distance(5));
+    public static Section 구간_영등포시장역_신길역 = new Section(3L, 오호선, 영등포시장역, 신길역, new Distance(10));
+    public static Section 구간_신길역_여의도역 = new Section(4L, 오호선, 신길역, 여의도역, new Distance(10));
+    public static Section 구간_영등포구청역_당산역 = new Section(5L, 이호선, 영등포구청역, 당산역, new Distance(10));
+    public static Section 구간_신길역_영등포역 = new Section(6L, 오호선, 신길역, 영등포역, new Distance(5));
+    public static Section 구간_야탑역_모란역 = new Section(7L, 신분당선, 야탑역, 모란역, new Distance(5));
+
+    public static Sections 전체_구간 = new Sections(asList(구간_양평역_영등포구청역
+            , 구간_영등포구청역_영등포시장역
+            , 구간_영등포시장역_신길역
+            , 구간_신길역_여의도역
+            , 구간_영등포구청역_당산역
+            , 구간_영등포구청역_영등포시장역
+            , 구간_신길역_영등포역
+            , 구간_야탑역_모란역));
+}
--- src/test/java/nextstep/subway/path/service/PathServiceTest.java
+++ src/test/java/nextstep/subway/path/service/PathServiceTest.java
@@ -1,9 +1,5 @@
 package nextstep.subway.path.service;
 
-import nextstep.subway.line.domain.Distance;
-import nextstep.subway.line.domain.Line;
-import nextstep.subway.line.domain.Section;
-import nextstep.subway.line.domain.Sections;
 import nextstep.subway.line.service.SectionService;
 import nextstep.subway.path.domain.PathFinder;
 import nextstep.subway.path.dto.PathRequest;
@@ -26,7 +22,9 @@ import java.util.List;
 import java.util.stream.Stream;
 
 import static java.util.Arrays.asList;
+import static nextstep.subway.line.fixture.SectionFixture.전체_구간;
 import static nextstep.subway.station.dto.StationResponse.of;
+import static nextstep.subway.station.fixture.StationFixture.*;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 import static org.mockito.Mockito.when;
@@ -42,39 +40,7 @@ class PathServiceTest {
 
     private PathFinder pathFinder = new PathFinder();
 
-    private static Station 양평역 = new Station(1L, "양평역");
-    private static Station 영등포구청역 = new Station(2L, "영등포구청");
-    private static Station 영등포시장역 = new Station(3L, "영등포시장");
-    private static Station 신길역 = new Station(4L, "신길역");
-    private static Station 여의도역 = new Station(5L, "여의도역");
-    private static Station 당산역 = new Station(6L, "당산역");
-    private static Station 영등포역 = new Station(7L, "영등포역");
-    private static Station 야탑역 = new Station(8L, "야탑역");
-    private static Station 모란역 = new Station(9L, "모란역");
     private static Station 대구역 = new Station(Integer.MAX_VALUE, "대구역");
-
-    private static Line 오호선 = new Line(1L, "오호선", "bg-red-600", 양평역, 영등포구청역, new Distance(10));
-    private static Line 이호선 = new Line(2L, "이호선", "bg-red-600", 영등포구청역, 당산역, new Distance(10));
-    private static Line 일호선 = new Line(3L, "일호선", "bg-red-600", 신길역, 영등포역, new Distance(5));
-    private static Line 신분당선 = new Line(4L, "신분당선", "bg-red-600", 야탑역, 모란역, new Distance(5));
-
-
-    private static Section 구간_양평역_영등포구청역 = new Section(1L, 오호선, 양평역, 영등포구청역, new Distance(10));
-    private static Section 구간_영등포구청역_영등포시장역 = new Section(2L, 오호선, 영등포구청역, 영등포시장역, new Distance(5));
-    private static Section 구간_영등포시장역_신길역 = new Section(3L, 오호선, 영등포시장역, 신길역, new Distance(10));
-    private static Section 구간_신길역_여의도역 = new Section(4L, 오호선, 신길역, 여의도역, new Distance(10));
-    private static Section 구간_영등포구청역_당산역 = new Section(5L, 이호선, 영등포구청역, 당산역, new Distance(10));
-    private static Section 구간_신길역_영등포역 = new Section(6L, 오호선, 신길역, 영등포역, new Distance(5));
-    private static Section 구간_야탑역_모란역 = new Section(7L, 신분당선, 야탑역, 모란역, new Distance(5));
-
-    private static Sections 전체_구간 = new Sections(asList(구간_양평역_영등포구청역
-            , 구간_영등포구청역_영등포시장역
-            , 구간_영등포시장역_신길역
-            , 구간_신길역_여의도역
-            , 구간_영등포구청역_당산역
-            , 구간_영등포구청역_영등포시장역
-            , 구간_신길역_영등포역
-            , 구간_야탑역_모란역));
 
     @MethodSource("methodSource_findPath_성공")
     @ParameterizedTest
