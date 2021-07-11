02b17906b53006ccd4527098b32dc0eb1d2d8923
sanggu <tkdrn8578@naver.com>
2021-07-11 22:26:58 +0900

    feat: 즐겨찾기 목록을 조회하는 mock API 및 인수테스트 추가

--- src/test/java/nextstep/subway/favorite/FavoriteAcceptanceTest.java
+++ src/test/java/nextstep/subway/favorite/FavoriteAcceptanceTest.java
@@ -5,6 +5,8 @@ import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import nextstep.subway.AcceptanceTest;
 import nextstep.subway.favorite.dto.FavoriteRequest;
+import nextstep.subway.favorite.dto.FavoriteResponse;
+import nextstep.subway.favorite.dto.FavoriteResponses;
 import nextstep.subway.station.dto.StationResponse;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -14,22 +16,29 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import static nextstep.subway.auth.acceptance.AuthAcceptanceTest.회원_로그인_됨;
 import static nextstep.subway.member.MemberAcceptanceTest.*;
 import static nextstep.subway.station.StationAcceptanceTest.지하철역_등록되어_있음;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("즐겨찾기 관련 기능")
 public class FavoriteAcceptanceTest extends AcceptanceTest {
 
     private String token;
+    private StationResponse 강남역;
+    private StationResponse 광교역;
 
     @Override
     @BeforeEach
     public void setUp() {
         super.setUp();
 
-        지하철역_등록되어_있음("강남역").as(StationResponse.class);
-        지하철역_등록되어_있음("광교역").as(StationResponse.class);
+        강남역 = 지하철역_등록되어_있음("강남역").as(StationResponse.class);
+        광교역 = 지하철역_등록되어_있음("광교역").as(StationResponse.class);
 
         회원_생성을_요청(EMAIL, PASSWORD,28);
         token = 회원_로그인_됨(EMAIL, PASSWORD);
@@ -38,18 +47,41 @@ public class FavoriteAcceptanceTest extends AcceptanceTest {
     @DisplayName("즐겨찾기를 추가한다.")
     @Test
     void createFavorite() {
-        // when
-        FavoriteRequest request = new FavoriteRequest(1L, 5L);
-
         //when
-        ExtractableResponse<Response> response = 즐겨찾기_생성_요청(request);
+        ExtractableResponse<Response> response = 즐겨찾기_생성_요청(new FavoriteRequest(강남역.getId(), 광교역.getId()));
 
         // then
         즐겨찾기_생성됨(response);
     }
 
+    @DisplayName("즐겨찾기를 목록을 조회한다.")
+    @Test
+    void getFavorites() {
+        //given
+        StationResponse 왕십리역 = 지하철역_등록되어_있음("왕십리역").as(StationResponse.class);
+        StationResponse 건대역 = 지하철역_등록되어_있음("건대역").as(StationResponse.class);
+        ExtractableResponse<Response> 첫번쨰_즐겨찾기 = 즐겨찾기_생성되어_있음(new FavoriteRequest(강남역.getId(), 광교역.getId()));
+        ExtractableResponse<Response> 두번째_즐겨찾기 = 즐겨찾기_생성되어_있음(new FavoriteRequest(왕십리역.getId(), 건대역.getId()));
+
+        // when
+        // when
+        ExtractableResponse<Response> response = RestAssured
+                .given().log().all()
+                .when().get("/favorites")
+                .then().log().all().extract();
+
+        // then
+        Assertions.assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+        즐겨찾기_목록_포함됨(response, Arrays.asList(첫번쨰_즐겨찾기, 두번째_즐겨찾기));
+    }
+
+
     private void 즐겨찾기_생성됨(ExtractableResponse<Response> response) {
         Assertions.assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+    }
+
+    private ExtractableResponse<Response> 즐겨찾기_생성되어_있음(FavoriteRequest request) {
+        return  즐겨찾기_생성_요청(request);
     }
 
     private ExtractableResponse<Response> 즐겨찾기_생성_요청(FavoriteRequest request) {
@@ -60,5 +92,17 @@ public class FavoriteAcceptanceTest extends AcceptanceTest {
                     .contentType(MediaType.APPLICATION_JSON_VALUE)
                     .when().post("/favorites")
                     .then().log().all().extract();
+    }
+
+    public static void 즐겨찾기_목록_포함됨(ExtractableResponse<Response> response, List<ExtractableResponse<Response>> createdResponses) {
+        List<Long> expectedFavoriteIds = createdResponses.stream()
+                .map(it -> Long.parseLong(it.header("Location").split("/")[2]))
+                .collect(Collectors.toList());
+
+        List<Long> resultFavoriteIds = response.body().as(FavoriteResponses.class).getFavorites().stream()
+                .map(FavoriteResponse::getId)
+                .collect(Collectors.toList());
+
+        assertThat(resultFavoriteIds).containsAll(expectedFavoriteIds);
     }
 }
