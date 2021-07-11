2b769861d148925e7c0c4faa5e9b508339f9118c
sanggu <tkdrn8578@naver.com>
2021-07-11 23:05:42 +0900

    refactor: private 접근제어자 추가

--- src/main/java/nextstep/subway/favorite/dto/FavoriteResponses.java
+++ src/main/java/nextstep/subway/favorite/dto/FavoriteResponses.java
@@ -6,8 +6,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class FavoriteResponses {
-
-    List<FavoriteResponse> favorites;
+    private List<FavoriteResponse> favorites;
 
     public FavoriteResponses() {
     }
