3ceb8290798cdf1be7b4ab0c591da344b8fe2957
sanggu <tkdrn8578@naver.com>
2021-07-11 22:03:54 +0900

    refactor: mock API를 proudct로 변경

--- src/main/java/nextstep/subway/favorite/controller/FavoriteController.java
+++ src/main/java/nextstep/subway/favorite/controller/FavoriteController.java
@@ -2,6 +2,8 @@ package nextstep.subway.favorite.controller;
 
 import nextstep.subway.auth.domain.AuthenticationPrincipal;
 import nextstep.subway.auth.domain.LoginMember;
+import nextstep.subway.favorite.application.FavoriteService;
+import nextstep.subway.favorite.domain.Favorite;
 import nextstep.subway.favorite.dto.FavoriteRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,8 +15,15 @@ import java.net.URI;
 @RestController
 public class FavoriteController {
 
+    private final FavoriteService favoriteService;
+
+    public FavoriteController(FavoriteService favoriteService) {
+        this.favoriteService = favoriteService;
+    }
+
     @PostMapping("/favorites")
     public ResponseEntity createFavorite(@AuthenticationPrincipal LoginMember loginMember, @RequestBody FavoriteRequest request) {
-        return ResponseEntity.created(URI.create("1")).build();
+        Favorite savedFavorite = favoriteService.save(loginMember, request);
+        return ResponseEntity.created(URI.create("/favorites/" + savedFavorite.getId())).body(savedFavorite);
     }
 }
