25b43c36d999021472a0b3faaf525dc16f7fa3a2
younghoon <bblnotbb@yahho.com>
2021-07-15 00:06:59 +0900

    feat : apply feedback(extract as constant)

--- src/main/java/nextstep/subway/path/domain/FarePolicyFactory.java
+++ src/main/java/nextstep/subway/path/domain/FarePolicyFactory.java
@@ -3,12 +3,15 @@ package nextstep.subway.path.domain;
 import java.util.List;
 
 public class FarePolicyFactory {
+	public static final int SECOND_ADDITIONAL_FARE_TARGET_LENGTH = 50;
+	public static final int FIRST_ADDITIONAL_FARE_TARGET_LENGTH1 = 10;
+
 	public static FarePolicy getFarePolicy(List<AdditionalFareEdge> additionalFareEdges, int length) {
-		if (length > 50) {
+		if (length > SECOND_ADDITIONAL_FARE_TARGET_LENGTH) {
 			return new SecondAdditionalFarePolicy(additionalFareEdges, length);
 		}
 
-		if (length > 10) {
+		if (length > FIRST_ADDITIONAL_FARE_TARGET_LENGTH1) {
 			return new FirstAdditionalFarePolicy(additionalFareEdges, length);
 		}
 
