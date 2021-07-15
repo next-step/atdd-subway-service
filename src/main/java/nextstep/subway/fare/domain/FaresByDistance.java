9fa63c1a06c2131ab7376f88e2d044a025d81a44
jordy-torvalds <jordy-torvalds@kakao.com>
2021-07-15 23:57:31 +0900

    feat: 거리 별 추가 요금 기능 및 테스트 구

---
+++ src/main/java/nextstep/subway/fare/domain/FaresByDistance.java
@@ -0,0 +1,38 @@
+package nextstep.subway.fare.domain;
+
+import nextstep.subway.line.domain.Distance;
+
+import java.util.Arrays;
+
+import static java.lang.Integer.MAX_VALUE;
+
+public enum FaresByDistance {
+    BASIC(new Distance(1), new Distance(10), MAX_VALUE),
+    SHORT(new Distance(11), new Distance(50), 5),
+    LONG(new Distance(51), new Distance(MAX_VALUE), 8);
+
+    public static final Fare BASIC_FARE = new Fare(1250);
+    public static final Fare ADDITIONAL_FARE_UNIT = new Fare(100);
+    private Distance moreDistance;
+    private Distance belowDistance;
+    private int distanceStandard;
+
+    FaresByDistance(Distance moreDistance, Distance belowDistance, int distanceStandard) {
+        this.moreDistance = moreDistance;
+        this.belowDistance = belowDistance;
+        this.distanceStandard = distanceStandard;
+    }
+
+    public static Fare calculate(Distance distance) {
+        int additionalFareDistance = distance.getDividedValue(findDistanceStandard(distance));
+
+        return BASIC_FARE.add(ADDITIONAL_FARE_UNIT.multiply(new Rate(additionalFareDistance)));
+    }
+
+    private static Integer findDistanceStandard(Distance distance) {
+        return Arrays.stream(values())
+                .filter(value -> distance.isMoreBelowThan(value.moreDistance, value.belowDistance))
+                .map(value -> value.distanceStandard)
+                .findFirst().orElseThrow(() -> new IllegalArgumentException("유효한 처리가 아닙니다." + distance));
+    }
+}
---
+++ src/test/java/nextstep/subway/fare/domain/FaresByDistanceTest.java
@@ -0,0 +1,31 @@
+package nextstep.subway.fare.domain;
+
+import nextstep.subway.line.domain.Distance;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DisplayName("거리 별 요금 테스트")
+public class FaresByDistanceTest {
+    @CsvSource( delimiterString = ":",
+            value = {
+                    "10:1250",
+                    "11:1450",
+                    "32:1850",
+                    "50:2250",
+                    "51:1850",
+                    "56:1950"
+            })
+    @ParameterizedTest
+    void calculate_성공(int distanceValue, int expectedFareValue) {
+        // Given
+        Distance distance = new Distance(distanceValue);
+        Fare expectedFare = new Fare(expectedFareValue);
+
+        // when, then
+        assertThat(FaresByDistance.calculate(distance))
+                .isEqualTo(expectedFare);
+    }
+}
