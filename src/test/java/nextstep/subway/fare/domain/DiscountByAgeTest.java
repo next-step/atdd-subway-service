9679ddb12257e2a78747e290e4e799ac9cf9f10a
jordy-torvalds <jordy-torvalds@kakao.com>
2021-07-15 23:57:16 +0900

    feat: 나이별 할인율 도메인 및 테스트 구

---
+++ src/test/java/nextstep/subway/fare/domain/DiscountByAgeTest.java
@@ -0,0 +1,29 @@
+package nextstep.subway.fare.domain;
+
+import nextstep.subway.member.domain.Age;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+@DisplayName("연령 별 할인율 테스트")
+public class DiscountByAgeTest {
+
+
+    @CsvSource(delimiterString=":",
+    value = {
+            "3350:5:3350",
+            "3350:6:1850",
+            "3350:12:1850",
+            "3350:13:2750",
+            "3350:18:2750",
+            "3350:19:3350",
+    })
+    @ParameterizedTest
+    void calculate_성공(int fareArgument, int ageArgument, int expectedResult) {
+        // given
+        Fare fare = new Fare(fareArgument);
+        Age age = new Age(ageArgument);
+
+        DiscountByAge.calculate(fare, age);
+    }
+}
