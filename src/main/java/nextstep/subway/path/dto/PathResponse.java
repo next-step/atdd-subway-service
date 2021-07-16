3c5a2b6de234c22f24cdd9193b5c18a357ab9040
Jeong Minkyo <redwonder@naver.com>
2021-07-16 20:42:29 +0900

    refactor(PathResponse) : 지하철 경로 조회 인수테스트를 수정한다
    지하철 경로 조회시 요금정보도 추가하도록 수정한다

--- src/main/java/nextstep/subway/path/dto/PathResponse.java
+++ src/main/java/nextstep/subway/path/dto/PathResponse.java
@@ -6,9 +6,14 @@ public class PathResponse {
 
 	private StationsResponse stationsResponse;
 	private int distance;
+	private int fare;
 
 	public static PathResponse of(StationsResponse stationsResponse, int distance) {
 		return new PathResponse(stationsResponse, distance);
+	}
+
+	public static PathResponse of(StationsResponse stationsResponse, int distance, int fare) {
+		return new PathResponse(stationsResponse, distance, fare);
 	}
 
 	public PathResponse() {
@@ -20,6 +25,11 @@ public class PathResponse {
 		this.distance = distance;
 	}
 
+	public PathResponse(StationsResponse stationsResponse, int distance, int fare) {
+		this.stationsResponse = stationsResponse;
+		this.distance = distance;
+		this.fare = fare;
+	}
 
 	public StationsResponse getStationsResponse() {
 		return stationsResponse;
@@ -27,5 +37,9 @@ public class PathResponse {
 
 	public int getDistance() {
 		return distance;
+	}
+
+	public int getFare() {
+		return fare;
 	}
 }
