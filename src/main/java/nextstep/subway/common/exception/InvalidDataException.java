71c5ff12498bb05684f2ec3c3b0301673cf83da5
devyonghee <devyonghee@gmail.com>
2021-11-22 02:35:20 +0900

    feat(RestExceptionHandler) : exception handler 추가

---
+++ src/main/java/nextstep/subway/common/exception/InvalidDataException.java
@@ -0,0 +1,8 @@
+package nextstep.subway.common.exception;
+
+public class InvalidDataException extends RuntimeException {
+
+    public InvalidDataException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/nextstep/subway/configuration/RestExceptionHandler.java
@@ -0,0 +1,36 @@
+package nextstep.subway.configuration;
+
+import java.util.Map;
+import java.util.stream.Collectors;
+import nextstep.subway.common.exception.DuplicateDataException;
+import nextstep.subway.common.exception.InvalidDataException;
+import nextstep.subway.common.exception.NotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class RestExceptionHandler {
+
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<Void> handleNotFoundException(NotFoundException e) {
+        return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler({DuplicateDataException.class, InvalidDataException.class})
+    public ResponseEntity<String> handleDuplicateAndInvalidDataException(RuntimeException e) {
+        return ResponseEntity.badRequest().body(e.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleMethodArgumentNotValidException(
+        MethodArgumentNotValidException e) {
+        return ResponseEntity.badRequest().body(
+            e.getFieldErrors()
+                .stream()
+                .collect(Collectors.toMap(FieldError::getField, FieldError::getDefaultMessage))
+        );
+    }
+}
