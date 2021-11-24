efc7e2893d7d78e48d02f491016deeee1deff40d
IW.MOON <mooniw02@gmail.com>
2021-11-24 22:22:56 +0900

    feat: Line Service 로직들을 Line 도메인으로 이동, Sections 를 일급 컬렉션화

---
+++ src/main/java/nextstep/subway/line/domain/Sections.java
@@ -0,0 +1,66 @@
+package nextstep.subway.line.domain;
+
+import nextstep.subway.station.domain.Station;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Embeddable;
+import javax.persistence.FetchType;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Embeddable
+public class Sections {
+
+    @OneToMany(mappedBy = "line", fetch = FetchType.LAZY, orphanRemoval = true, cascade = CascadeType.ALL)
+    private List<Section> sections = new ArrayList<>();
+
+    public void add(Section section) {
+        sections.add(section);
+    }
+
+    public List<Section> getSections() {
+        return sections;
+    }
+
+    public Station getEndUpStation() {
+        return this.sections.get(0).getUpStation();
+    }
+
+    public Optional<Section> getSectionDownStationSame(Station station) {
+        return  this.sections.stream()
+                .filter(it -> it.getDownStation() == station)
+                .findFirst();
+    }
+
+    public Optional<Section> getSectionUpStationSame(Station station) {
+        return  this.sections.stream()
+                .filter(it -> it.getUpStation() == station)
+                .findFirst();
+    }
+
+    public void updateUpStation(Station upStation, Station downStation, int distance) {
+
+        this.sections.stream()
+                .filter(it -> it.getUpStation() == upStation)
+                .findFirst()
+                .ifPresent(it -> it.updateUpStation(downStation, distance));
+    }
+
+    public void updateDownStation(Station upStation, Station downStation, int distance) {
+
+        this.sections.stream()
+                .filter(it -> it.getDownStation() == downStation)
+                .findFirst()
+                .ifPresent(it -> it.updateDownStation(upStation, distance));
+
+    }
+
+    @Override
+    public String toString() {
+        return "Sections{" +
+                "sections=" + sections +
+                '}';
+    }
+}
