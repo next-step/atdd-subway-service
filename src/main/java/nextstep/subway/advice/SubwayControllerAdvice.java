860e36d55516220b4082761b41b00c5759399361
tyanotyrano <tyrano09@gmail.com>
2021-11-24 20:34:28 +0900

    refactor(SubwayControllerAdvice.java): 예외처리를 담당하는 전용 Advice 추가
    예외처리를 담당하는 전용 Advice 를 추가한다.
    - SubwayControllerAdvice : 예외처리를 담당하는 전용 Advice 추가
    - LineController, StationController : ExceptionHandler 메소드 제거

---
+++ src/main/java/nextstep/subway/advice/SubwayControllerAdvice.java
@@ -0,0 +1,15 @@
+package nextstep.subway.advice;
+
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class SubwayControllerAdvice {
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<Void> handleDataIntegrityViolationException(DataIntegrityViolationException exception) {
+        return ResponseEntity.badRequest().build();
+    }
+}
