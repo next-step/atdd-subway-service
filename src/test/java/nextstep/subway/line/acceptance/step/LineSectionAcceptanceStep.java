665b84f9361f0fc3b09c449262d8c7af85540d16
devyonghee <devyonghee@gmail.com>
2021-11-25 11:27:55 +0900

    test(PathAcceptanceTest) : 경로 조회 기능 테스트 추가

--- src/test/java/nextstep/subway/line/acceptance/step/LineSectionAcceptanceStep.java
+++ src/test/java/nextstep/subway/line/acceptance/step/LineSectionAcceptanceStep.java
@@ -15,6 +15,11 @@ import org.springframework.http.MediaType;
 
 public class LineSectionAcceptanceStep {
 
+    public static void 지하철_노선에_지하철역_등록되어_있음(LineResponse line,
+        StationResponse upStation, StationResponse downStation, int distance) {
+        지하철_노선에_지하철역_등록_요청(line, upStation, downStation, distance);
+    }
+
     public static ExtractableResponse<Response> 지하철_노선에_지하철역_등록_요청(LineResponse line,
         StationResponse upStation, StationResponse downStation, int distance) {
         SectionRequest sectionRequest = new SectionRequest(upStation.getId(), downStation.getId(),
