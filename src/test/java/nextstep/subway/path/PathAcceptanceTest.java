1b3ff763c9d70569daf3f2423512a76cd419fdf1
devyonghee <devyonghee@gmail.com>
2021-11-26 02:53:04 +0900

    chore(PathAcceptanceTest) : 불필요한 필드 제거

--- src/test/java/nextstep/subway/path/PathAcceptanceTest.java
+++ src/test/java/nextstep/subway/path/PathAcceptanceTest.java
@@ -28,9 +28,6 @@ import org.junit.jupiter.params.provider.CsvSource;
 @DisplayName("지하철 경로 조회")
 class PathAcceptanceTest extends AcceptanceTest {
 
-    private LineResponse 신분당선;
-    private LineResponse 이호선;
-    private LineResponse 삼호선;
     private StationResponse 강남역;
     private StationResponse 양재역;
     private StationResponse 교대역;
@@ -50,12 +47,12 @@ class PathAcceptanceTest extends AcceptanceTest {
         교대역 = 지하철역_등록되어_있음("교대역").as(StationResponse.class);
         남부터미널역 = 지하철역_등록되어_있음("남부터미널역").as(StationResponse.class);
 
-        신분당선 = 지하철_노선_등록되어_있음(new LineCreateRequest("신분당선", "bg-red-600",
+        지하철_노선_등록되어_있음(new LineCreateRequest("신분당선", "bg-red-600",
             new SectionRequest(강남역.getId(), 양재역.getId(), 10)));
-        이호선 = 지하철_노선_등록되어_있음(new LineCreateRequest("이호선", "bg-red-600",
+        지하철_노선_등록되어_있음(new LineCreateRequest("이호선", "bg-red-600",
             new SectionRequest(교대역.getId(), 강남역.getId(), 10)));
 
-        삼호선 = 지하철_노선_등록되어_있음(new LineCreateRequest("삼호선", "bg-red-600",
+        LineResponse 삼호선 = 지하철_노선_등록되어_있음(new LineCreateRequest("삼호선", "bg-red-600",
             new SectionRequest(교대역.getId(), 양재역.getId(), 5)));
         지하철_노선에_지하철역_등록되어_있음(삼호선, 교대역, 남부터미널역, 3);
     }
