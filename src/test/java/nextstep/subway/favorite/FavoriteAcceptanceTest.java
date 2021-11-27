ee65de6bb01ce73f811279aa17366a6fe125c1fe
devyonghee <devyonghee@gmail.com>
2021-11-27 13:49:21 +0900

    doc(FavoriteAcceptanceTest): 즐겨찾기 시나리오 추가

--- src/test/java/nextstep/subway/favorite/FavoriteAcceptanceTest.java
+++ src/test/java/nextstep/subway/favorite/FavoriteAcceptanceTest.java
@@ -1,8 +1,28 @@
 package nextstep.subway.favorite;
 
 import nextstep.subway.AcceptanceTest;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 
 @DisplayName("즐겨찾기 관련 기능")
 class FavoriteAcceptanceTest extends AcceptanceTest {
+
+    @BeforeEach
+    void setUp() {
+//        Given 지하철역 등록되어 있음
+//        And 지하철 노선 등록되어 있음
+//        And 지하철 노선에 지하철역 등록되어 있음
+//        And 회원 등록되어 있음
+//        And 로그인 되어있음
+    }
+
+    @DisplayName("즐겨찾기를 관리")
+    void manageFavorite() {
+//        When 즐겨찾기 생성을 요청
+//        Then 즐겨찾기 생성됨
+//        When 즐겨찾기 목록 조회 요청
+//        Then 즐겨찾기 목록 조회됨
+//        When 즐겨찾기 삭제 요청
+//        Then 즐겨찾기 삭제됨
+    }
 }
