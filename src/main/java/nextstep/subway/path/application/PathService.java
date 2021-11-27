e06589fe1aadc10279d567a24d006040cd18b1b8
IW.MOON <mooniw02@gmail.com>
2021-11-27 20:35:39 +0900

    refactor: 메서드 로직 분리

--- src/main/java/nextstep/subway/path/application/PathService.java
+++ src/main/java/nextstep/subway/path/application/PathService.java
@@ -4,6 +4,7 @@ import nextstep.subway.line.domain.Line;
 import nextstep.subway.line.domain.LineRepository;
 import nextstep.subway.path.domain.Path;
 import nextstep.subway.path.domain.PathFinder;
+import nextstep.subway.station.application.StationService;
 import nextstep.subway.station.domain.Station;
 import nextstep.subway.station.domain.StationRepository;
 import org.springframework.stereotype.Service;
@@ -15,18 +16,23 @@ public class PathService {
 
     private final StationRepository stationRepository;
     private final LineRepository lineRepository;
+    private final StationService stationService;
 
-    public PathService(StationRepository stationRepository, LineRepository lineRepository) {
+    public PathService(StationRepository stationRepository, LineRepository lineRepository, StationService stationService) {
         this.stationRepository = stationRepository;
         this.lineRepository = lineRepository;
+        this.stationService = stationService;
     }
 
     public Path getShortestPath(int source, int target){
-        Station sourceStation = stationRepository.findById((long) source).orElseThrow(() ->new IllegalArgumentException("존재하지 않는 역입니다."));
-        Station targetStation = stationRepository.findById((long) target).orElseThrow(() ->new IllegalArgumentException("존재하지 않는 역입니다."));
+        Station sourceStation = stationService.findStationById((long) source);
+        Station targetStation = stationService.findStationById((long) target);
 
         List<Line> lines = lineRepository.findAll();
         PathFinder pathFinder = PathFinder.from(lines);
         return pathFinder.getShortestPath(sourceStation, targetStation);
     }
+
+
+
 }
