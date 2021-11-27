9db04bc62eacf3a4904d2315f4954f90a35cbd9b
devyonghee <devyonghee@gmail.com>
2021-11-28 02:01:43 +0900

    feat(Favorite): 즐겨찾기 도메인 추가

--- src/main/java/nextstep/subway/favorite/domain/Favorite.java
+++ src/main/java/nextstep/subway/favorite/domain/Favorite.java
@@ -1,31 +1,71 @@
 package nextstep.subway.favorite.domain;
 
-import nextstep.subway.member.domain.Member;
+import io.jsonwebtoken.lang.Assert;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.ForeignKey;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import nextstep.subway.station.domain.Station;
 
+@Entity
 public class Favorite {
 
-    private Station source;
-    private Station target;
-    private Member member;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-    public static Favorite from(Station source, Station target) {
-        return new Favorite();
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "source_station_id", foreignKey = @ForeignKey(name = "fk_favorite_source_station"), nullable = false)
+    private Station source;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "target_station_id", foreignKey = @ForeignKey(name = "fk_favorite_target_station"), nullable = false)
+    private Station target;
+
+    @Column
+    private long memberId;
+
+    protected Favorite() {
+    }
+
+    private Favorite(Station source, Station target, long memberId) {
+        validateStations(source, target);
+        this.source = source;
+        this.target = target;
+        this.memberId = memberId;
+    }
+
+    public static Favorite from(Station source, Station target, long memberId) {
+        return new Favorite(source, target, memberId);
     }
 
     public long id() {
-        return 0;
+        return id;
     }
 
     public Station source() {
-        return null;
+        return source;
     }
 
     public Station target() {
-        return null;
+        return target;
     }
 
-    public Member member() {
-        return null;
+    public long memberId() {
+        return memberId;
     }
+
+    private void validateStations(Station source, Station target) {
+        Assert.notNull(source, "출발역은 필수입니다.");
+        Assert.notNull(target, "도착역은 필수입니다.");
+        if (source.equals(target)) {
+            throw new IllegalArgumentException(
+                String.format("출발역(%s)과 도착역(%s)은 달라야 합니다.", source, target));
+        }
+    }
+
 }
