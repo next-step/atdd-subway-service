b973732d3b08056c6525d0dd890112f7c3fcaaed
peter-alethio <peter@alethio.io>
2021-11-29 11:05:32 +0900

    refactor: 픽스쳐 추가

--- src/test/java/nextstep/subway/path/application/PathFinderTest.java
+++ src/test/java/nextstep/subway/path/application/PathFinderTest.java
@@ -1,26 +1,13 @@
 package nextstep.subway.path.application;
 
-import nextstep.subway.line.domain.Line;
-import nextstep.subway.line.domain.Section;
-import nextstep.subway.line.domain.Sections;
 import nextstep.subway.path.dto.PathResult;
-import nextstep.subway.station.domain.Station;
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
-
+import static nextstep.subway.path.PathFixture.*;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 class PathFinderTest {
-
-    private static final Line 이호선 = new Line("이호선", "red");
-    private static final Station 교대역 = new Station("교대역");
-    private static final Station 양재역 = new Station("양재역");
-    private static final Station 선릉역 = new Station("선릉역");
-    private static final Sections 구간 = new Sections(Arrays.asList(
-            new Section(이호선, 교대역, 선릉역, 10),
-            new Section(이호선, 선릉역, 양재역, 10)));
 
     @Test
     void findShortestPath_최단_경로를_조회한다() {
