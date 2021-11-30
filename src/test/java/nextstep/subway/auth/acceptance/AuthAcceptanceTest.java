bab9570ccd70a633d32e1043a8be72bad7753687
peter-alethio <peter@alethio.io>
2021-11-30 22:18:31 +0900

    test: 로그인 인수 테스트

--- src/test/java/nextstep/subway/auth/acceptance/AuthAcceptanceTest.java
+++ src/test/java/nextstep/subway/auth/acceptance/AuthAcceptanceTest.java
@@ -1,14 +1,30 @@
 package nextstep.subway.auth.acceptance;
 
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
 import nextstep.subway.AcceptanceTest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static nextstep.subway.auth.step.AuthStep.*;
+
 public class AuthAcceptanceTest extends AcceptanceTest {
+
+    private static final String 이메일 = "email@email.com";
+    private static final String 비밀번호 = "password";
+    private static final int 나이 = 10;
 
     @DisplayName("Bearer Auth")
     @Test
     void myInfoWithBearerAuth() {
+        // given
+        회원_등록되어_있음(이메일, 비밀번호, 나이);
+
+        // when
+        ExtractableResponse<Response> response = 로그인_요청(이메일, 비밀번호);
+
+        // then
+        로그인_됨(response);
     }
 
     @DisplayName("Bearer Auth 로그인 실패")
---
+++ src/test/java/nextstep/subway/auth/step/AuthStep.java
@@ -0,0 +1,34 @@
+package nextstep.subway.auth.step;
+
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import nextstep.subway.auth.dto.TokenRequest;
+import nextstep.subway.auth.dto.TokenResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+import static nextstep.subway.member.MemberAcceptanceTest.회원_생성을_요청;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class AuthStep {
+
+    public static void 회원_등록되어_있음(String email, String password, Integer age) {
+        회원_생성을_요청(email, password, age);
+    }
+
+    public static void 로그인_됨(ExtractableResponse<Response> response) {
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+        assertThat(response.as(TokenResponse.class).getAccessToken()).isNotNull();
+    }
+
+    public static ExtractableResponse<Response> 로그인_요청(String email, String password) {
+        return RestAssured
+                .given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(new TokenRequest(email, password))
+                .when().post("/login/token")
+                .then().log().all()
+                .extract();
+    }
+}
