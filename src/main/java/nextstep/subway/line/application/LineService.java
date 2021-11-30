41fe9f6a424872a3fdbc51668017d12c2cef2672
Wangki Lee <lwk3007@gmail.com>
2021-11-30 20:26:59 +0900

    refactor: lineRequest toLine 추가

--- src/main/java/nextstep/subway/line/application/LineService.java
+++ src/main/java/nextstep/subway/line/application/LineService.java
@@ -30,8 +30,7 @@ public class LineService {
     public LineResponse saveLine(LineRequest request) {
         Station upStation = stationService.findById(request.getUpStationId());
         Station downStation = stationService.findById(request.getDownStationId());
-        Line persistLine = lineRepository.save(
-            new Line(request.getName(), request.getColor(), upStation, downStation, request.getDistance()));
+        Line persistLine = lineRepository.save(request.toLine(upStation, downStation));
         List<StationResponse> stations = convertToStationResponses(persistLine.getStations());
         return LineResponse.of(persistLine, stations);
     }
