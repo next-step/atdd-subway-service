6b89375a61c5a2c3acd8ff359f931e9d66479348
cus4703 <cus4703@gmail.com>
2021-12-01 00:41:08 +0900

    feat: 게스트 모드 생성

--- src/main/java/nextstep/subway/auth/application/AuthService.java
+++ src/main/java/nextstep/subway/auth/application/AuthService.java
@@ -31,7 +31,7 @@ public class AuthService {
 
     public LoginMember findMemberByToken(String credentials) {
         if (!jwtTokenProvider.validateToken(credentials)) {
-            throw new MemberException(ErrorCode.BAD_ARGUMENT, "유효하지 않은 토큰입니다.");
+            return LoginMember.ofGuest();
         }
 
         String email = jwtTokenProvider.getPayload(credentials);
--- src/main/java/nextstep/subway/auth/domain/LoginMember.java
+++ src/main/java/nextstep/subway/auth/domain/LoginMember.java
@@ -1,6 +1,13 @@
 package nextstep.subway.auth.domain;
 
 public class LoginMember {
+
+    private static final LoginMember GUEST = new LoginMember();
+    private static final int CHILD_AGE_MIN = 6;
+    private static final int CHILD_AGE_MAX = 13;
+    private static final int YOUTH_AGE_MIN = 13;
+    private static final int YOUTH_AGE_MAX = 19;
+
     private Long id;
     private String email;
     private Integer age;
@@ -14,6 +21,10 @@ public class LoginMember {
         this.age = age;
     }
 
+    public static LoginMember ofGuest() {
+        return GUEST;
+    }
+
     public Long getId() {
         return id;
     }
@@ -25,4 +36,17 @@ public class LoginMember {
     public Integer getAge() {
         return age;
     }
+
+    public boolean isGuest() {
+        return this == GUEST;
+    }
+
+    public boolean isChild() {
+        return age >= CHILD_AGE_MIN && age < CHILD_AGE_MAX;
+    }
+
+    public boolean isYouth() {
+        return age >= YOUTH_AGE_MIN && age < YOUTH_AGE_MAX;
+    }
+
 }
