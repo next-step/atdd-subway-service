d0c389313c7f6a200c660d1efcf4cf69fa76a277
cus4703 <cus4703@gmail.com>
2021-12-01 00:58:14 +0900

    feat: 노선 정보 필드 추가

--- src/main/java/nextstep/subway/path/domain/SectionEdge.java
+++ src/main/java/nextstep/subway/path/domain/SectionEdge.java
@@ -1,25 +1,28 @@
 package nextstep.subway.path.domain;
 
+import nextstep.subway.line.domain.line.Line;
 import nextstep.subway.line.domain.section.Section;
 import nextstep.subway.station.domain.Station;
 import org.jgrapht.graph.DefaultWeightedEdge;
 
 public class SectionEdge extends DefaultWeightedEdge {
 
+    private Line line;
     private Station source;
     private Station target;
     private double weight;
 
     public SectionEdge() {}
 
-    public SectionEdge(Station source, Station target, double weight) {
+    public SectionEdge(Line line, Station source, Station target, double weight) {
+        this.line = line;
         this.source = source;
         this.target = target;
         this.weight = weight;
     }
 
     public static SectionEdge of(Section section) {
-        return new SectionEdge(section.getUpStation(), section.getDownStation(), section.getDistance());
+        return new SectionEdge(section.getLine(), section.getUpStation(), section.getDownStation(), section.getDistance());
     }
 
     @Override
@@ -35,5 +38,9 @@ public class SectionEdge extends DefaultWeightedEdge {
     @Override
     public Station getTarget() {
         return target;
+    }
+
+    public Line getLine() {
+        return line;
     }
 }
