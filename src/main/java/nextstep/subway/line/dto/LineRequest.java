4513d90dd0ea034246041b44fdbab727ff635ba4
cus4703 <cus4703@gmail.com>
2021-12-01 00:52:15 +0900

    feat: 운임 비용 필드 추가

--- src/main/java/nextstep/subway/line/dto/LineRequest.java
+++ src/main/java/nextstep/subway/line/dto/LineRequest.java
@@ -2,12 +2,15 @@ package nextstep.subway.line.dto;
 
 import nextstep.subway.line.domain.line.Line;
 
+import java.math.BigDecimal;
+
 public class LineRequest {
     private String name;
     private String color;
     private Long upStationId;
     private Long downStationId;
     private int distance;
+    private BigDecimal pare;
 
     public LineRequest() {
     }
@@ -18,6 +21,15 @@ public class LineRequest {
         this.upStationId = upStationId;
         this.downStationId = downStationId;
         this.distance = distance;
+    }
+
+    public LineRequest(String name, String color, Long upStationId, Long downStationId, int distance, BigDecimal pare) {
+        this.name = name;
+        this.color = color;
+        this.upStationId = upStationId;
+        this.downStationId = downStationId;
+        this.distance = distance;
+        this.pare = pare;
     }
 
     public String getName() {
