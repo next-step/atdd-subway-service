608ba7c4e311da46853e688cc63e301aa105bd5e
jerry92k <jens92.k@gmail.com>
2021-11-30 15:54:21 +0900

    refactor(Sections): List<Section>을 Sections 일급컬렉션으로 래핑

--- src/main/java/nextstep/subway/line/domain/Line.java
+++ src/main/java/nextstep/subway/line/domain/Line.java
@@ -16,8 +16,8 @@ public class Line extends BaseEntity {
     private String name;
     private String color;
 
-    @OneToMany(mappedBy = "line", cascade = {CascadeType.PERSIST, CascadeType.MERGE}, orphanRemoval = true)
-    private List<Section> sections = new ArrayList<>();
+    @Embedded
+    private Sections sections = new Sections();
 
     public Line() {
     }
