0fc9f7908dfa2db3be764d9253eef736c719d089
ChangSubKwak <paradise1732@gmail.com>
2021-12-02 20:36:47 +0900

    feat: 정적팩터리 메서드 추가, 예외 문자열 상수 선언, 역목록 가져오기 / 역제거하기 기능 추가

--- src/main/java/nextstep/subway/line/domain/Line.java
+++ src/main/java/nextstep/subway/line/domain/Line.java
@@ -1,19 +1,26 @@
 package nextstep.subway.line.domain;
 
 import nextstep.subway.BaseEntity;
+import nextstep.subway.line.dto.LineRequest;
 import nextstep.subway.station.domain.Station;
 
 import javax.persistence.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 @Entity
 public class Line extends BaseEntity {
+    private static final String CANNOT_REMOVE_IF_SECTION_IS_ONLY_ONE_EXCEPTION_STATEMENT = "구간이 하나 이므로 삭제할 수 없습니다.";
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @Column(unique = true)
     private String name;
+
     private String color;
 
     @OneToMany(mappedBy = "line", cascade = {CascadeType.PERSIST, CascadeType.MERGE}, orphanRemoval = true)
@@ -31,6 +38,14 @@ public class Line extends BaseEntity {
         this.name = name;
         this.color = color;
         sections.add(new Section(this, upStation, downStation, distance));
+    }
+
+    public static Line of(String name, String color, Station upStation, Station downStation, int distance) {
+        return new Line(name, color, upStation, downStation, distance);
+    }
+
+    public static Line from(LineRequest lineRequest) {
+        return new Line(lineRequest.getName(), lineRequest.getColor());
     }
 
     public void update(Line line) {
@@ -52,5 +67,72 @@ public class Line extends BaseEntity {
 
     public List<Section> getSections() {
         return sections;
+    }
+
+    public List<Station> getStations() {
+        if (getSections().isEmpty()) {
+            return Arrays.asList();
+        }
+
+        List<Station> stations = new ArrayList<>();
+        Station downStation = findUpStation();
+        stations.add(downStation);
+
+        Optional<Section> nextLineStation = findSectionBasedOnUpStation(downStation);
+        while (downStation != null && nextLineStation.isPresent()) {
+            downStation = nextLineStation.get().getDownStation();
+            stations.add(downStation);
+            nextLineStation = findSectionBasedOnUpStation(downStation);
+        }
+
+        return stations;
+    }
+
+    private Station findUpStation() {
+        Station downStation = getSections().get(0).getUpStation();
+        Optional<Section> nextLineStation = findSectionBasedOnDownStation(downStation);
+        while (downStation != null && nextLineStation.isPresent()) {
+            downStation = nextLineStation.get().getUpStation();
+            nextLineStation = findSectionBasedOnDownStation(downStation);
+        }
+        return downStation;
+    }
+
+    public void removeLineStation(Station station) {
+        if (getSections().size() <= 1) {
+            throw new RuntimeException(CANNOT_REMOVE_IF_SECTION_IS_ONLY_ONE_EXCEPTION_STATEMENT);
+        }
+
+        Optional<Section> upLineStation = findSectionBasedOnUpStation(station);
+        Optional<Section> downLineStation = findSectionBasedOnDownStation(station);
+
+        addSectionIfBothStationsOf(upLineStation, downLineStation);
+
+        upLineStation.ifPresent(it -> getSections().remove(it));
+        downLineStation.ifPresent(it -> getSections().remove(it));
+    }
+
+    private void addSectionIfBothStationsOf(final Optional<Section> upLineStation, final Optional<Section> downLineStation) {
+        if (upLineStation.isPresent() && downLineStation.isPresent()) {
+            Section downLineSection = downLineStation.get();
+            Section upLineSection = upLineStation.get();
+            getSections().add(Section.of(this,
+                downLineSection.getUpStation(),
+                upLineSection.getDownStation(),
+                upLineSection.getDistance() + downLineSection.getDistance())
+            );
+        }
+    }
+
+    private Optional<Section> findSectionBasedOnUpStation(Station station) {
+        return getSections().stream()
+            .filter(it -> it.getUpStation() == station)
+            .findFirst();
+    }
+
+    private Optional<Section> findSectionBasedOnDownStation(Station station) {
+        return getSections().stream()
+            .filter(it -> it.getDownStation() == station)
+            .findFirst();
     }
 }
