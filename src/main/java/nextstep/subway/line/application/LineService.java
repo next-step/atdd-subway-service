1a6ce4c4ac1ae72964c31f6f6ceedc8ce09048e9
IW.MOON <mooniw02@gmail.com>
2021-12-02 22:53:05 +0900

    feat: Line에 추가요금 항목 추가

--- src/main/java/nextstep/subway/line/application/LineService.java
+++ src/main/java/nextstep/subway/line/application/LineService.java
@@ -29,7 +29,7 @@ public class LineService {
     public LineResponse saveLine(LineRequest request) {
         Station upStation = stationService.findById(request.getUpStationId());
         Station downStation = stationService.findById(request.getDownStationId());
-        Line persistLine = lineRepository.save(new Line(request.getName(), request.getColor(), upStation, downStation, request.getDistance()));
+        Line persistLine = lineRepository.save(new Line(request.getName(), request.getColor(), upStation, downStation, request.getDistance(), request.getAdditionalFee()));
         return LineResponse.from(persistLine);
     }
 
