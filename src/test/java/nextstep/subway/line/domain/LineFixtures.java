556a91cc5a0b59cbd4a0b037544292a31cc6059e
wooobo <irooori89@gmail.com>
2021-12-02 15:55:29 +0900

    feat : `LineFixtures`, `StationFixtures` 추가

---
+++ src/test/java/nextstep/subway/line/domain/LineFixtures.java
@@ -0,0 +1,7 @@
+package nextstep.subway.line.domain;
+
+public class LineFixtures {
+
+    public static Line 이호선 = new Line("2호선", "RED");
+
+}
---
+++ src/test/java/nextstep/subway/line/domain/StationFixtures.java
@@ -0,0 +1,12 @@
+package nextstep.subway.line.domain;
+
+import nextstep.subway.station.domain.Station;
+
+public class StationFixtures {
+
+    public static Station 잠실 = new Station("잠실");
+    public static Station 잠실나루 = new Station("잠실나루");
+    public static Station 강변 = new Station("강변");
+    public static Station 구의 = new Station("구의");
+
+}
