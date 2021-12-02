555594fc79982e3d14768a6adb2ceea80b343f0f
IW.MOON <mooniw02@gmail.com>
2021-12-02 22:50:44 +0900

    refactor: ExceptionHandler에 메세지 추가

--- src/main/java/nextstep/subway/common/exception/ErrorControllerAdvice.java
+++ src/main/java/nextstep/subway/common/exception/ErrorControllerAdvice.java
@@ -1,5 +1,6 @@
 package nextstep.subway.common.exception;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -8,7 +9,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class ErrorControllerAdvice {
 
     @ExceptionHandler(IllegalArgumentException.class)
-    public ResponseEntity IllegalArgumentExceptionHandler() {
-        return ResponseEntity.badRequest().build();
+    public ResponseEntity IllegalArgumentExceptionHandler(Exception e) {
+        return new ResponseEntity(e.getMessage(), HttpStatus.BAD_REQUEST);
     }
 }
