3b0ffa551f82e5cca8e70f5591ed76d00456c2e4
DESKTOP-HG83T1I\Chaser <a>
2021-12-03 03:36:23 +0900

    refactor(Domain, application): 요금계산 로직을 별도 도메인으로 분리

--- src/main/java/nextstep/subway/path/application/PathService.java
+++ src/main/java/nextstep/subway/path/application/PathService.java
@@ -2,23 +2,18 @@ package nextstep.subway.path.application;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.NoSuchElementException;
 import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 
 import nextstep.subway.line.application.LineService;
-import nextstep.subway.line.domain.Line;
+import nextstep.subway.path.domain.FareCalculator;
 import nextstep.subway.path.dto.PathAnalysisKey;
 import nextstep.subway.path.dto.PathResponse;
 import nextstep.subway.path.dto.PathStationDto;
 import nextstep.subway.path.dto.ShortestPathInfo;
 import nextstep.subway.path.infrastructure.PathAnalysis;
-import nextstep.subway.policy.DiscountPolicyFactory;
 import nextstep.subway.policy.domain.Price;
-import nextstep.subway.policy.price.DistancePricePolicy;
-import nextstep.subway.policy.price.LinePricePolicy;
-import nextstep.subway.policy.price.PricePolicy;
 import nextstep.subway.station.application.StationService;
 import nextstep.subway.station.domain.Station;
 
@@ -33,6 +28,22 @@ public class PathService {
     }
 
     public PathResponse searchShortestPath(Long sourceStationId, Long targetStationId) {
+        ShortestPathInfo shortestPathInfo = findShortestPath(sourceStationId, targetStationId);
+
+        Price totalFare = FareCalculator.calculate(shortestPathInfo.getDistance(), shortestPathInfo.getLines());
+
+        return createPathResponse(shortestPathInfo, totalFare);
+    }
+
+    public PathResponse searchShortestPath(Long sourceStationId, Long targetStationId, Integer age) {
+        ShortestPathInfo shortestPathInfo = findShortestPath(sourceStationId, targetStationId);
+
+        Price discoutnAcceptedPrice = FareCalculator.calculate(shortestPathInfo.getDistance(), shortestPathInfo.getLines(), age);
+
+        return createPathResponse(shortestPathInfo, discoutnAcceptedPrice);
+    }
+
+    private ShortestPathInfo findShortestPath(Long sourceStationId, Long targetStationId) {
         vaildateShortestPath(sourceStationId, targetStationId);
 
         Station source = stationService.findById(sourceStationId);
@@ -41,54 +52,13 @@ public class PathService {
         PathAnalysis pathAnalysis = PathAnalysis.of(lineService.findAllSections());
 
         ShortestPathInfo shortestPathInfo = pathAnalysis.findShortestPaths(source, target);
-
-        List<Line> lines = shortestPathInfo.getLines();
-
-        PricePolicy linePricePolicy = new LinePricePolicy(lines);
-        PricePolicy distancePricePolicy = new DistancePricePolicy(shortestPathInfo.getDistance());
-
-        List<PricePolicy> pricePolicys = List.of(linePricePolicy, distancePricePolicy);
-
-        // when
-        Price totalFare = pricePolicys.stream()
-                                        .map(PricePolicy::apply)
-                                        .reduce((seed, result) -> seed.plus(result))
-                                        .orElseThrow(() -> new NoSuchElementException("계산되는 운임이 없습니다."));
-
-        return createPathResponse(shortestPathInfo, totalFare);
+        return shortestPathInfo;
     }
 
     private void vaildateShortestPath(Long sourceStationId, Long targetStationId) {
         if (sourceStationId.equals(targetStationId)) {
             throw new IllegalArgumentException("시작역과 도착역이 같습니다.");
         }
-    }
-
-    public PathResponse searchShortestPath(Long sourceStationId, Long targetStationId, Integer age) {
-        vaildateShortestPath(sourceStationId, targetStationId);
-
-        Station source = stationService.findById(sourceStationId);
-        Station target = stationService.findById(targetStationId);
-
-        PathAnalysis pathAnalysis = PathAnalysis.of(lineService.findAllSections());
-
-        ShortestPathInfo shortestPathInfo = pathAnalysis.findShortestPaths(source, target);
-
-        List<Line> lines = shortestPathInfo.getLines();
-
-        PricePolicy linePricePolicy = new LinePricePolicy(lines);
-        PricePolicy distancePricePolicy = new DistancePricePolicy(shortestPathInfo.getDistance());
-
-        List<PricePolicy> pricePolicys = List.of(linePricePolicy, distancePricePolicy);
-
-        // when
-        Price totalFare = pricePolicys.stream()
-                                        .map(PricePolicy::apply)
-                                        .reduce((seed, result) -> seed.plus(result))
-                                        .orElseThrow(() -> new NoSuchElementException("계산되는 운임이 없습니다."));
-
-        Price discoutnAcceptedPrice = DiscountPolicyFactory.generate(age).apply(totalFare);
-        return createPathResponse(shortestPathInfo, discoutnAcceptedPrice);
     }
 
     private PathResponse createPathResponse(ShortestPathInfo shortestPathInfo, Price fare) {
