20fde03237eb5b9e480c3cb955b9742008b1dc72
IW.MOON <mooniw02@gmail.com>
2021-12-02 22:48:44 +0900

    feat: 거리, 연령대에 따른 요금을 계산하기 위한 Path Service 변경

--- src/main/java/nextstep/subway/path/application/PathService.java
+++ src/main/java/nextstep/subway/path/application/PathService.java
@@ -14,23 +14,22 @@ import java.util.List;
 @Service
 public class PathService {
 
-    private final StationRepository stationRepository;
     private final LineRepository lineRepository;
     private final StationService stationService;
 
-    public PathService(StationRepository stationRepository, LineRepository lineRepository, StationService stationService) {
-        this.stationRepository = stationRepository;
+    public PathService(LineRepository lineRepository, StationService stationService) {
         this.lineRepository = lineRepository;
         this.stationService = stationService;
     }
 
-    public Path getShortestPath(int source, int target){
+    public Path getShortestPath(int source, int target, int age){
         Station sourceStation = stationService.findStationById((long) source);
         Station targetStation = stationService.findStationById((long) target);
 
         List<Line> lines = lineRepository.findAll();
         PathFinder pathFinder = PathFinder.from(lines);
-        return pathFinder.getShortestPath(sourceStation, targetStation);
+        System.out.println("pathFinder = " + pathFinder);
+        return pathFinder.getShortestPath(sourceStation, targetStation, age);
     }
 
 
--- src/main/java/nextstep/subway/path/domain/PathFinder.java
+++ src/main/java/nextstep/subway/path/domain/PathFinder.java
@@ -1,21 +1,23 @@
 package nextstep.subway.path.domain;
 
+import nextstep.subway.fee.AdditionalFeeByDistance;
 import nextstep.subway.line.domain.Line;
+import nextstep.subway.line.domain.Section;
 import nextstep.subway.station.domain.Station;
 import nextstep.subway.station.domain.Stations;
+import org.jgrapht.GraphPath;
 import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
-import org.jgrapht.graph.DefaultWeightedEdge;
 import org.jgrapht.graph.WeightedMultigraph;
 
 import java.util.List;
 
 public class PathFinder {
 
-    private final WeightedMultigraph<Station, DefaultWeightedEdge> graph;
-    private final DijkstraShortestPath<Station, DefaultWeightedEdge> dijkstraShortestPath;
+    private final WeightedMultigraph<Station, LineWeightedEdge> graph;
+    private final DijkstraShortestPath<Station, LineWeightedEdge> dijkstraShortestPath;
 
     private PathFinder(List<Line> lines) {
-        graph = new WeightedMultigraph<>(DefaultWeightedEdge.class);
+        graph = new WeightedMultigraph<>(LineWeightedEdge.class);
         createPath(lines);
         dijkstraShortestPath = new DijkstraShortestPath<>(graph);
     }
@@ -24,20 +26,33 @@ public class PathFinder {
         return new PathFinder(lines);
     }
 
-    public Path getShortestPath(Station source, Station target) {
-        return new Path(new Stations(dijkstraShortestPath.getPath(source, target).getVertexList()),
-                (int) dijkstraShortestPath.getPath(source, target).getWeight());
+    public Path getShortestPath(Station source, Station target, int age) {
+
+        GraphPath<Station, LineWeightedEdge> path = dijkstraShortestPath.getPath(source, target);
+        int fee = AdditionalFeeByDistance.getFee(path);
+        return new Path(new Stations(path.getVertexList()),(int) path.getWeight(), fee, age);
     }
 
     private void createPath(List<Line> lines) {
         lines.forEach(
-                line -> line.getStations().forEach(
-                        graph::addVertex));
+                line -> line.getStations()
+                            .forEach(graph::addVertex));
         lines.forEach(
-                line -> line.getSections().forEach(
-                        section -> graph.setEdgeWeight(
-                                graph.addEdge(section.getUpStation(), section.getDownStation()), section.getDistance())));
-
+                line -> line.getSections()
+                            .forEach(section -> addEdge(line, section)));
     }
 
+    private void addEdge(Line line, Section section) {
+        LineWeightedEdge lineWeightedEdge = new LineWeightedEdge(line);
+        this.graph.addEdge(section.getUpStation(), section.getDownStation(), lineWeightedEdge);
+        this.graph.setEdgeWeight(lineWeightedEdge, section.getDistance() );
+    }
+
+    @Override
+    public String toString() {
+        return "PathFinder{" +
+                "graph=" + graph +
+                ", dijkstraShortestPath=" + dijkstraShortestPath +
+                '}';
+    }
 }
