fe8ed10e3a0a372f28235c1a89cfe87407fda1f0
jerry92k <jens92.k@gmail.com>
2021-12-02 11:09:47 +0900

    test: jgrapht 학습 테스트

--- src/test/java/study/jgraph/JgraphTest.java
+++ src/test/java/study/jgraph/JgraphTest.java
@@ -2,16 +2,19 @@ package study.jgraph;
 
 import org.jgrapht.GraphPath;
 import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
-import org.jgrapht.alg.shortestpath.KShortestPaths;
+
+import org.jgrapht.alg.shortestpath.YenKShortestPath;
 import org.jgrapht.graph.DefaultWeightedEdge;
 import org.jgrapht.graph.WeightedMultigraph;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class JgraphTest {
+
     @Test
     public void getDijkstraShortestPath() {
         String source = "v3";
@@ -31,6 +34,49 @@ public class JgraphTest {
     }
 
     @Test
+    public void getShortestPathWhenSourceAndTargetSame() {
+        String source = "v3";
+        String target = "v3";
+        WeightedMultigraph<String, DefaultWeightedEdge> graph = new WeightedMultigraph(DefaultWeightedEdge.class);
+        graph.addVertex("v1");
+        graph.addVertex("v2");
+        graph.addVertex("v3");
+        graph.setEdgeWeight(graph.addEdge("v1", "v2"), 2);
+        graph.setEdgeWeight(graph.addEdge("v2", "v3"), 2);
+        graph.setEdgeWeight(graph.addEdge("v1", "v3"), 100);
+
+        DijkstraShortestPath dijkstraShortestPath = new DijkstraShortestPath(graph);
+        List<String> shortestPath = dijkstraShortestPath.getPath(source, target).getVertexList();
+        System.out.println(shortestPath);
+        assertThat(shortestPath.size()).isEqualTo(1);
+    }
+
+    @Test
+    public void getNotOnlyOneBestPath() {
+        String source = "v3";
+        String target = "v1";
+        WeightedMultigraph<String, DefaultWeightedEdge> graph = new WeightedMultigraph(DefaultWeightedEdge.class);
+        graph.addVertex("v1");
+        graph.addVertex("v2");
+        graph.addVertex("v4");
+        graph.addVertex("v3");
+
+        graph.setEdgeWeight(graph.addEdge("v1", "v2"), 2);
+        graph.setEdgeWeight(graph.addEdge("v1", "v4"), 2);
+        graph.setEdgeWeight(graph.addEdge("v4", "v3"), 2);
+        graph.setEdgeWeight(graph.addEdge("v2", "v3"), 2);
+
+
+        DijkstraShortestPath dijkstraShortestPath = new DijkstraShortestPath(graph);
+        List<String> shortestPath = dijkstraShortestPath.getPath(source, target).getVertexList();
+        /*
+         source를 기준으로 edge들을 iterate 하며 찾을때, addEdge된 순서대로 찾으므로, v4가 포함된 경로가 먼저 모두 add 되었기 때문에
+         v3 -> v4 -> v1, v3 -> v2 -> v1 두 경로의 비용이 같아도 v3 -> v4 -> v1 경로로 결정됨
+         */
+        assertThat(shortestPath).containsExactlyElementsOf(Arrays.asList("v3","v4","v1"));
+    }
+
+    @Test
     public void getKShortestPaths() {
         String source = "v3";
         String target = "v1";
@@ -43,13 +89,17 @@ public class JgraphTest {
         graph.setEdgeWeight(graph.addEdge("v2", "v3"), 2);
         graph.setEdgeWeight(graph.addEdge("v1", "v3"), 100);
 
-        List<GraphPath> paths = new KShortestPaths(graph, 100).getPaths(source, target);
+        // KShortestSimplePaths는 jgrapht-core 1.5.0에서 deprecated 됨. 대신 YenKShortestPath를 사용하도록 가이드 됨
+        List<GraphPath> paths = new YenKShortestPath(graph).getPaths(source, target,100);
 
         assertThat(paths).hasSize(2);
         paths.stream()
                 .forEach(it -> {
+                    System.out.println(it.getVertexList());
                     assertThat(it.getVertexList()).startsWith(source);
                     assertThat(it.getVertexList()).endsWith(target);
                 });
     }
+
+
 }
