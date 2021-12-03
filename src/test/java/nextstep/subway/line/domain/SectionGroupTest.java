16d83da454f0ea8d1cedc7e5b9d7ab3a7afe1298
김도영 <95doyoung@gmail.com>
2021-12-03 14:17:34 +0900

    test (SectionGroupTest) :  역 목록 조회시 상행역 부터 하행역 순으로 정렬 되어하는 테스트 케이스 추가

--- src/test/java/nextstep/subway/line/domain/SectionGroupTest.java
+++ src/test/java/nextstep/subway/line/domain/SectionGroupTest.java
@@ -1,16 +1,42 @@
 package nextstep.subway.line.domain;
 
+import nextstep.subway.station.domain.Station;
+import nextstep.subway.station.domain.StationRepository;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.Arrays;
+import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
 class SectionGroupTest {
+    @Autowired
+    private LineRepository lineRepository;
+    @Autowired
+    private StationRepository stationRepository;
 
+    @DisplayName("역목록은 상행역 부터 하행역 순으로 정렬 되어야 한다.")
     @Test
     void getStations() {
         // given
-        final Line line = new Line();
+        final Station firstStation = stationRepository.save(Station.of("1번"));
+        final Station secondStation = stationRepository.save(Station.of("2번"));
+        final Station thirdStation = stationRepository.save(Station.of("3번"));
+        final Station forthStation = stationRepository.save(Station.of("4번"));
+        final Station fifthStation = stationRepository.save(Station.of("5번"));
+        final Line line = lineRepository.save(Line.of("노선이름", "색상", Arrays.asList(
+                Section.of(thirdStation, forthStation, 10),
+                Section.of(forthStation, fifthStation, 10),
+                Section.of(firstStation, secondStation, 10),
+                Section.of(secondStation, thirdStation, 10)
+        )));
         // when
+        final List<Station> stationsOrderByUptoDown = line.getStations();
         // then
+        assertThat(stationsOrderByUptoDown).containsExactlyElementsOf(Arrays.asList(firstStation, secondStation, thirdStation, forthStation, fifthStation));
     }
 }
