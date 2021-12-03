da26b6922f0061358320040f715d8180b1cb2284
songseongbin <ppsosqq@gmail.com>
2021-12-03 14:19:11 +0900

    refactor(Line, LineService): 노선에서 역 목록 불러오기 함수 리팩토링
    - LineService에 있던 로직 Line으로 이동 및 리팩토링

--- src/main/java/nextstep/subway/line/domain/Line.java
+++ src/main/java/nextstep/subway/line/domain/Line.java
@@ -1,11 +1,24 @@
 package nextstep.subway.line.domain;
 
+import static java.util.stream.Collectors.toList;
+import static java.util.stream.Collectors.toSet;
+
+import com.google.common.collect.Lists;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Stream;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import nextstep.subway.BaseEntity;
 import nextstep.subway.station.domain.Station;
-
-import javax.persistence.*;
-import java.util.ArrayList;
-import java.util.List;
+import org.springframework.util.CollectionUtils;
 
 @Entity
 public class Line extends BaseEntity {
@@ -36,6 +49,29 @@ public class Line extends BaseEntity {
     public void update(Line line) {
         this.name = line.getName();
         this.color = line.getColor();
+    }
+
+    public List<Station> getStations() {
+        if (CollectionUtils.isEmpty(sections)) {
+            return Lists.newArrayList();
+        }
+
+        return Stream.of(getUpStations(), getDownStations())
+            .flatMap(Collection::stream)
+            .distinct()
+            .collect(toList());
+    }
+
+    private Set<Station> getUpStations() {
+        return this.sections.stream()
+            .map(Section::getUpStation)
+            .collect(toSet());
+    }
+
+    private Set<Station> getDownStations() {
+        return this.sections.stream()
+            .map(Section::getDownStation)
+            .collect(toSet());
     }
 
     public Long getId() {
