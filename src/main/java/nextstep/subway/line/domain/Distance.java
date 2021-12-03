e02b7cf20728f411ba2e214343cdc01f8d2fee79
wooobo <irooori89@gmail.com>
2021-12-03 21:53:50 +0900

    feat `Distance` 예외처리 함수 분리

--- src/main/java/nextstep/subway/line/domain/Distance.java
+++ src/main/java/nextstep/subway/line/domain/Distance.java
@@ -1,11 +1,13 @@
 package nextstep.subway.line.domain;
 
+import javax.persistence.Column;
 import javax.persistence.Embeddable;
 import nextstep.subway.common.exception.InvalidParameterException;
 
 @Embeddable
 public class Distance {
 
+    @Column
     private int distance;
 
     protected Distance() {
@@ -23,19 +25,19 @@ public class Distance {
         return distance;
     }
 
-    public void minus(Distance distance) {
-        if (this.distance <= distance.distance) {
-            throw new InvalidParameterException("역과 역 사이의 거리보다 좁은 거리를 입력해주세요");
-        }
+    public void minus(Integer distance) {
+        validGreaterThan(distance);
 
-        this.distance -= distance.distance;
+        this.distance -= distance;
     }
 
     public void plus(Distance distance) {
         this.distance += distance.distance;
     }
 
-    public void zero() {
-        this.distance = 0;
+    private void validGreaterThan(Integer distance) {
+        if (this.distance <= distance) {
+            throw new InvalidParameterException("역과 역 사이의 거리보다 좁은 거리를 입력해주세요");
+        }
     }
 }
