e78c7edc6b5246788ab822d46bc20926feba1bc6
vicjed <vicjed@icloud.com>
2021-12-03 21:20:36 +0900

    test(Line) : 세션 일급컬렉션 추가로 해당 로직 Sections로 이동

--- src/main/java/nextstep/subway/line/domain/Line.java
+++ src/main/java/nextstep/subway/line/domain/Line.java
@@ -3,9 +3,11 @@ package nextstep.subway.line.domain;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
+import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -24,10 +26,8 @@ public class Line extends BaseEntity {
     private String name;
     private String color;
 
-    @OneToMany(mappedBy = "line", cascade = {CascadeType.PERSIST,
-        CascadeType.MERGE}, orphanRemoval = true)
-    private List<Section> sections = new ArrayList<>();
-
+    @Embedded
+    Sections sections = new Sections();
 
     public Line(String name, String color) {
         this.name = name;
@@ -37,7 +37,7 @@ public class Line extends BaseEntity {
     public Line(String name, String color, Station upStation, Station downStation, int distance) {
         this.name = name;
         this.color = color;
-        sections.add(new Section(this, upStation, downStation, distance));
+        sections.addSection(new Section(this, upStation, downStation, distance));
     }
 
     public Line(final long id, String name, String color) {
@@ -49,51 +49,7 @@ public class Line extends BaseEntity {
     }
 
     public void addSection(Section section) {
-        if (sections.contains(section)) {
-            return;
-        }
-        section.setLine(this);
-        sections.add(section);
-    }
-
-
-    public List<Station> getStations() {
-        if (sections.isEmpty()) {
-            return Arrays.asList();
-        }
-
-        List<Station> stations = new ArrayList<>();
-        Station downStation = findUpStation();
-        stations.add(downStation);
-
-        while (downStation != null) {
-            Station finalDownStation = downStation;
-            Optional<Section> nextLineStation = sections.stream()
-                .filter(it -> it.getUpStation() == finalDownStation)
-                .findFirst();
-            if (!nextLineStation.isPresent()) {
-                break;
-            }
-            downStation = nextLineStation.get().getDownStation();
-            stations.add(downStation);
-        }
-
-        return stations;
-    }
-
-    public Station findUpStation() {
-        Station downStation = sections.get(0).getUpStation();
-        while (downStation != null) {
-            Station finalDownStation = downStation;
-            Optional<Section> nextLineStation = sections.stream()
-                .filter(it -> it.getDownStation() == finalDownStation)
-                .findFirst();
-            if (!nextLineStation.isPresent()) {
-                break;
-            }
-            downStation = nextLineStation.get().getUpStation();
-        }
-        return downStation;
+        sections.addSection(section);
     }
 
     public void update(Line line) {
@@ -114,6 +70,27 @@ public class Line extends BaseEntity {
     }
 
     public List<Section> getSections() {
-        return sections;
+        return sections.getSections();
+    }
+
+    public List<Station> getStations() {
+        return sections.getStations();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Line line = (Line) o;
+        return Objects.equals(id, line.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
--- src/test/java/nextstep/subway/line/domain/LineTest.java
+++ src/test/java/nextstep/subway/line/domain/LineTest.java
@@ -1,44 +1,8 @@
 package nextstep.subway.line.domain;
 
-import static nextstep.subway.line.domain.SectionTest.일호선_구간_강남역_신촌역;
-import static nextstep.subway.line.domain.SectionTest.일호선_구간_신촌역_역삼역;
-import static nextstep.subway.line.domain.StationTest.강남역;
-import static nextstep.subway.line.domain.StationTest.신촌역;
-import static nextstep.subway.line.domain.StationTest.역삼역;
-
-import java.util.List;
-import nextstep.subway.station.domain.Station;
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.Test;
-
 class LineTest {
 
     public static final Line 일호선 = new Line(1L, "ONE_LINE", "BLUE");
     public static final Line 이호선 = new Line(2L, "TWO_LINE", "GREEN");
 
-    @Test
-    void 첫번째역을_찾는다() {
-        // given
-        일호선.addSection(일호선_구간_강남역_신촌역);
-        일호선.addSection(일호선_구간_신촌역_역삼역);
-
-        // when
-        Station upStation = 일호선.findUpStation();
-
-        // then
-        Assertions.assertThat(upStation).isEqualTo(강남역);
-    }
-
-    @Test
-    void 정렬된_역을_확인한다() {
-        // given
-        일호선.addSection(일호선_구간_강남역_신촌역);
-        일호선.addSection(일호선_구간_신촌역_역삼역);
-
-        // when
-        List<Station> stations = 일호선.getStations();
-
-        // then
-        Assertions.assertThat(stations).containsExactly(강남역,신촌역,역삼역);
-    }
 }
