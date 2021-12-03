0c09cf298296e7a789899b2e1b3b3ff26592aebf
moon <hoopster@airi.kr>
2021-12-03 23:27:12 +0900

    refac (PathResponse) : Response Dto 거리 반환 타입 정수형으로 리팩토링

--- src/main/java/nextstep/subway/path/dto/PathResponse.java
+++ src/main/java/nextstep/subway/path/dto/PathResponse.java
@@ -2,17 +2,22 @@ package nextstep.subway.path.dto;
 
 import java.util.List;
 
-import nextstep.subway.line.domain.Distance;
+import nextstep.subway.path.domain.Path;
 import nextstep.subway.station.dto.StationResponse;
 
 public class PathResponse {
 
 	private final List<StationResponse> stations;
-	private final Distance distance;
+	private final int distance;
 
-	public PathResponse(final List<StationResponse> stations, final Distance distance) {
+	private PathResponse(final List<StationResponse> stations, final int distance) {
 		this.stations = stations;
 		this.distance = distance;
+	}
+
+	public static PathResponse from(Path shortestPath) {
+		return new PathResponse(StationResponse.listOf(shortestPath.getStations())
+			, shortestPath.getDistance());
 	}
 
 	public List<StationResponse> getStations() {
@@ -20,6 +25,6 @@ public class PathResponse {
 	}
 
 	public int getDistance() {
-		return distance.getDistance();
+		return distance;
 	}
 }
