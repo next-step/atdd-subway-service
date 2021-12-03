1f2f0dfcee762273ebebbe22d05d863a184cacf9
peter-alethio <peter@alethio.io>
2021-12-03 14:09:13 +0900

    feat: 거리별 추가 요금 계산 추가

--- src/main/java/nextstep/subway/path/domain/Fare.java
+++ src/main/java/nextstep/subway/path/domain/Fare.java
@@ -10,6 +10,22 @@ public class Fare {
         return DEFAULT_FARE.plus(lineFare);
     }
 
+    public static Fare of(int lineFare, int distance) {
+        int distanceFare = calculateOverFare(distance);
+        return DEFAULT_FARE.plus(lineFare + distanceFare);
+    }
+
+    private static int calculateOverFare(int distance) {
+        int fare = 0;
+        if (distance > 10 && distance <= 50) {
+            fare += (int) ((Math.ceil(((distance - 10) - 1) / 5) + 1) * 100);
+        }
+        if (distance > 50) {
+            fare += (int) ((Math.ceil(((distance - 50) - 1) / 8) + 1) * 100) + 800;
+        }
+        return fare;
+    }
+
     private Fare plus(int lineFare) {
         return new Fare(this.value + lineFare);
     }
--- src/test/java/nextstep/subway/path/domain/FareTest.java
+++ src/test/java/nextstep/subway/path/domain/FareTest.java
@@ -1,6 +1,11 @@
 package nextstep.subway.path.domain;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -9,5 +14,24 @@ class FareTest {
     @Test
     void create_기본요금에서_추가요금을_더한다() {
         assertThat(Fare.of(700).getValue()).isEqualTo(1950);
+    }
+
+    @ParameterizedTest
+    @MethodSource("거리별_추가요금")
+    void create_거리별_추가요금을_더한다(int distance, int totalFare) {
+        Fare fare = Fare.of(0, distance);
+        assertThat(fare.getValue()).isEqualTo(totalFare);
+    }
+
+    private static Stream<Arguments> 거리별_추가요금() {
+        return Stream.of(
+                Arguments.of(10, 1250),
+                Arguments.of(11, 1350),
+                Arguments.of(15, 1350),
+                Arguments.of(16, 1450),
+                Arguments.of(50, 2050),
+                Arguments.of(51, 2150),
+                Arguments.of(58, 2150),
+                Arguments.of(59, 2250));
     }
 }
