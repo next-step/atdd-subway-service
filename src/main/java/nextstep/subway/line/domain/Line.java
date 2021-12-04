078e9ed9b72e56469ccfd0328a1bcaff2338931f
ChangSubKwak <paradise1732@gmail.com>
2021-12-04 14:52:07 +0900

    fix: 비지니스 로직을 Sections로 이전함에 따른 수정

--- src/main/java/nextstep/subway/line/domain/Line.java
+++ src/main/java/nextstep/subway/line/domain/Line.java
@@ -1,19 +1,15 @@
 package nextstep.subway.line.domain;
 
-import nextstep.subway.BaseEntity;
-import nextstep.subway.line.dto.LineRequest;
-import nextstep.subway.station.domain.Station;
+import java.util.*;
 
 import javax.persistence.*;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
+
+import nextstep.subway.*;
+import nextstep.subway.line.dto.*;
+import nextstep.subway.station.domain.*;
 
 @Entity
 public class Line extends BaseEntity {
-    private static final String CANNOT_REMOVE_IF_SECTION_IS_ONLY_ONE_EXCEPTION_STATEMENT = "구간이 하나 이므로 삭제할 수 없습니다.";
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -23,8 +19,8 @@ public class Line extends BaseEntity {
 
     private String color;
 
-    @OneToMany(mappedBy = "line", cascade = {CascadeType.PERSIST, CascadeType.MERGE}, orphanRemoval = true)
-    private List<Section> sections = new ArrayList<>();
+    @Embedded
+    private Sections sections = new Sections();
 
     public Line() {
     }
@@ -34,13 +30,13 @@ public class Line extends BaseEntity {
         this.color = color;
     }
 
-    public Line(String name, String color, Station upStation, Station downStation, int distance) {
+    public Line(String name, String color, Station upStation, Station downStation, Distance distance) {
         this.name = name;
         this.color = color;
-        sections.add(new Section(this, upStation, downStation, distance));
+        sections.addSection(this, upStation, downStation, distance);
     }
 
-    public static Line of(String name, String color, Station upStation, Station downStation, int distance) {
+    public static Line of(String name, String color, Station upStation, Station downStation, Distance distance) {
         return new Line(name, color, upStation, downStation, distance);
     }
 
@@ -65,74 +61,34 @@ public class Line extends BaseEntity {
         return color;
     }
 
-    public List<Section> getSections() {
-        return sections;
-    }
-
-    public List<Station> getStations() {
-        if (getSections().isEmpty()) {
-            return Arrays.asList();
-        }
-
-        List<Station> stations = new ArrayList<>();
-        Station downStation = findUpStation();
-        stations.add(downStation);
-
-        Optional<Section> nextLineStation = findSectionBasedOnUpStation(downStation);
-        while (downStation != null && nextLineStation.isPresent()) {
-            downStation = nextLineStation.get().getDownStation();
-            stations.add(downStation);
-            nextLineStation = findSectionBasedOnUpStation(downStation);
-        }
-
-        return stations;
-    }
-
-    private Station findUpStation() {
-        Station downStation = getSections().get(0).getUpStation();
-        Optional<Section> nextLineStation = findSectionBasedOnDownStation(downStation);
-        while (downStation != null && nextLineStation.isPresent()) {
-            downStation = nextLineStation.get().getUpStation();
-            nextLineStation = findSectionBasedOnDownStation(downStation);
-        }
-        return downStation;
+    public List<Station> stations() {
+        return sections.stations();
     }
 
     public void removeLineStation(Station station) {
-        if (getSections().size() <= 1) {
-            throw new RuntimeException(CANNOT_REMOVE_IF_SECTION_IS_ONLY_ONE_EXCEPTION_STATEMENT);
-        }
-
-        Optional<Section> upLineStation = findSectionBasedOnUpStation(station);
-        Optional<Section> downLineStation = findSectionBasedOnDownStation(station);
-
-        addSectionIfBothStationsOf(upLineStation, downLineStation);
-
-        upLineStation.ifPresent(it -> getSections().remove(it));
-        downLineStation.ifPresent(it -> getSections().remove(it));
+        sections.removeLineStation(this, station);
     }
 
-    private void addSectionIfBothStationsOf(final Optional<Section> upLineStation, final Optional<Section> downLineStation) {
-        if (upLineStation.isPresent() && downLineStation.isPresent()) {
-            Section downLineSection = downLineStation.get();
-            Section upLineSection = upLineStation.get();
-            getSections().add(Section.of(this,
-                downLineSection.getUpStation(),
-                upLineSection.getDownStation(),
-                upLineSection.getDistance() + downLineSection.getDistance())
-            );
-        }
+    public void addSection(Station upStation, Station downStation, Distance distance) {
+        sections.addSection(this, upStation, downStation, distance);
     }
 
-    private Optional<Section> findSectionBasedOnUpStation(Station station) {
-        return getSections().stream()
-            .filter(it -> it.getUpStation() == station)
-            .findFirst();
+    public Sections sections() {
+        return sections;
     }
 
-    private Optional<Section> findSectionBasedOnDownStation(Station station) {
-        return getSections().stream()
-            .filter(it -> it.getDownStation() == station)
-            .findFirst();
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (!(o instanceof Line))
+            return false;
+        Line line = (Line)o;
+        return Objects.equals(name, line.name) && Objects.equals(color, line.color);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, color);
     }
 }
