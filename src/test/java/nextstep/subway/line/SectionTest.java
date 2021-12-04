3e9e532b0460aa3fdc50c01c468f8642a96a4d03
ChangSubKwak <paradise1732@gmail.com>
2021-12-04 14:41:48 +0900

    fix: 거리값을 포장함에 따른 수정

--- src/test/java/nextstep/subway/line/SectionTest.java
+++ src/test/java/nextstep/subway/line/SectionTest.java
@@ -1,20 +1,20 @@
 package nextstep.subway.line;
 
-import nextstep.subway.AcceptanceTest;
-import nextstep.subway.line.domain.Line;
-import nextstep.subway.line.domain.Section;
-import nextstep.subway.station.domain.Station;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.*;
 
-class SectionTest extends AcceptanceTest {
+import nextstep.subway.line.domain.*;
+import nextstep.subway.station.domain.*;
+
+class SectionTest {
     private final Line line = new Line("신분당선", "bg-red-600");
     private final Station 강남역 = Station.from("강남역");
     private final Station 광교역 = Station.from("광교역");
     private final Station 양재역 = Station.from("양재역");
-    private final int distance = 10;
+
+    private final Distance oneDistance = Distance.from(1);
+    private final Distance distance = Distance.from(10);
     private final Section section = Section.of(line, 강남역, 광교역, distance);
 
     @DisplayName("디폴트 생성자를 호출하면 Section 객체를 반환한다.")
@@ -38,21 +38,21 @@ class SectionTest extends AcceptanceTest {
     @DisplayName("구간의 상행역을 수정하는 updateUpStation함수를 호출하면 상행역을 변경하며, 기존 거리에서 새로운 거리를 제외한 거리로 변경한다.")
     @Test
     void updateUpStationTest() {
-        section.updateUpStation(양재역, 1);
+        section.updateUpStation(양재역, oneDistance);
 
         assertThat(section.getUpStation()).isEqualTo(양재역);
         assertThat(section.getDownStation()).isEqualTo(광교역);
-        assertThat(section.getDistance()).isEqualTo(distance - 1);
+        assertThat(section.getDistance()).isEqualTo(distance.subtract(oneDistance));
     }
 
     @DisplayName("구간의 상행역을 수정하는 updateDownStation함수를 호출하면 상행역을 변경하며, 기존 거리에서 새로운 거리를 제외한 거리로 변경한다.")
     @Test
     void updateDownStationTest() {
-        section.updateDownStation(양재역, 1);
+        section.updateDownStation(양재역, oneDistance);
 
         assertThat(section.getUpStation()).isEqualTo(강남역);
         assertThat(section.getDownStation()).isEqualTo(양재역);
-        assertThat(section.getDistance()).isEqualTo(distance - 1);
+        assertThat(section.getDistance()).isEqualTo(distance.subtract(oneDistance));
     }
 
 }
