5d584e9c521c908e4af1cae0838b8fd8a63aadd6
ChangSubKwak <paradise1732@gmail.com>
2021-12-04 14:49:18 +0900

    feat: LineReponse의 콜렉션 객체의 생성 메서드 작성

--- src/main/java/nextstep/subway/line/dto/LineResponse.java
+++ src/main/java/nextstep/subway/line/dto/LineResponse.java
@@ -1,11 +1,11 @@
 package nextstep.subway.line.dto;
 
-import nextstep.subway.line.domain.Line;
-import nextstep.subway.station.dto.StationResponse;
+import java.time.*;
+import java.util.*;
+import java.util.stream.*;
 
-import java.time.LocalDateTime;
-import java.util.List;
-import java.util.stream.Collectors;
+import nextstep.subway.line.domain.*;
+import nextstep.subway.station.dto.*;
 
 public class LineResponse {
     private Long id;
@@ -18,7 +18,8 @@ public class LineResponse {
     public LineResponse() {
     }
 
-    public LineResponse(Long id, String name, String color, List<StationResponse> stations, LocalDateTime createdDate, LocalDateTime modifiedDate) {
+    public LineResponse(Long id, String name, String color, List<StationResponse> stations, LocalDateTime createdDate,
+        LocalDateTime modifiedDate) {
         this.id = id;
         this.name = name;
         this.color = color;
@@ -28,7 +29,8 @@ public class LineResponse {
     }
 
     public static LineResponse of(Line line, List<StationResponse> stations) {
-        return new LineResponse(line.getId(), line.getName(), line.getColor(), stations, line.getCreatedDate(), line.getModifiedDate());
+        return new LineResponse(line.getId(), line.getName(), line.getColor(), stations, line.getCreatedDate(),
+            line.getModifiedDate());
     }
 
     public static LineResponse of(Line line) {
@@ -36,13 +38,19 @@ public class LineResponse {
             line.getId(),
             line.getName(),
             line.getColor(),
-            line.getStations()
+            line.stations()
                 .stream()
                 .map(StationResponse::of)
                 .collect(Collectors.toList()),
             line.getCreatedDate(),
             line.getModifiedDate()
         );
+    }
+
+    public static List<LineResponse> ofList(List<Line> lines) {
+        return lines.stream()
+            .map(LineResponse::of)
+            .collect(Collectors.toList());
     }
 
     public Long getId() {
