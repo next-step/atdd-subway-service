b8dae1d243e3798860c3200e29197fcb5e99fa7a
ChangSubKwak <paradise1732@gmail.com>
2021-12-04 14:43:34 +0900

    fix: 값 비교를 위한 Station클래스의 비교 기준 변경

--- src/main/java/nextstep/subway/station/domain/Station.java
+++ src/main/java/nextstep/subway/station/domain/Station.java
@@ -37,8 +37,7 @@ public class Station extends BaseEntity {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Station station = (Station) o;
-        return Objects.equals(id, station.id) &&
-                Objects.equals(name, station.name);
+        return Objects.equals(name, station.name);
     }
 
     @Override
