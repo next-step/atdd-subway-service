f331acf6b575138948a17c812ea0411a6de15272
songseongbin <ppsosqq@gmail.com>
2021-12-05 21:29:08 +0900

    fix(PathController): 최단경로 조회 API URL 수정
    - path -> paths

--- src/main/java/nextstep/subway/path/ui/PathController.java
+++ src/main/java/nextstep/subway/path/ui/PathController.java
@@ -1,5 +1,6 @@
 package nextstep.subway.path.ui;
 
+import nextstep.subway.path.application.PathService;
 import nextstep.subway.path.dto.PathResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/path")
+@RequestMapping("/paths")
 public class PathController {
 
     @GetMapping
