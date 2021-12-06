46ed09f3e178fc49cfbd54d12eb029bf99b7448e
y2o2u2n <y2o2u2n@gmail.com>
2021-12-06 22:58:39 +0900

    feat: 토큰 발급 인수 테스트 (내 정보 조회)

--- src/main/java/nextstep/subway/auth/application/AuthService.java
+++ src/main/java/nextstep/subway/auth/application/AuthService.java
@@ -1,18 +1,20 @@
 package nextstep.subway.auth.application;
 
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import nextstep.subway.auth.domain.LoginMember;
 import nextstep.subway.auth.dto.TokenRequest;
 import nextstep.subway.auth.dto.TokenResponse;
 import nextstep.subway.auth.infrastructure.JwtTokenProvider;
 import nextstep.subway.member.domain.Member;
 import nextstep.subway.member.domain.MemberRepository;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional(readOnly = true)
 public class AuthService {
-    private MemberRepository memberRepository;
-    private JwtTokenProvider jwtTokenProvider;
+    private final MemberRepository memberRepository;
+    private final JwtTokenProvider jwtTokenProvider;
 
     public AuthService(MemberRepository memberRepository, JwtTokenProvider jwtTokenProvider) {
         this.memberRepository = memberRepository;
@@ -29,7 +31,7 @@ public class AuthService {
 
     public LoginMember findMemberByToken(String credentials) {
         if (!jwtTokenProvider.validateToken(credentials)) {
-            return new LoginMember();
+            throw new AuthorizationException();
         }
 
         String email = jwtTokenProvider.getPayload(credentials);
--- src/test/java/nextstep/subway/auth/acceptance/AuthAcceptanceTest.java
+++ src/test/java/nextstep/subway/auth/acceptance/AuthAcceptanceTest.java
@@ -3,16 +3,21 @@ package nextstep.subway.auth.acceptance;
 import static nextstep.subway.member.MemberAcceptanceTest.*;
 import static nextstep.subway.member.TestMember.*;
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 
+import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import nextstep.subway.AcceptanceTest;
 import nextstep.subway.auth.dto.TokenRequest;
 import nextstep.subway.auth.dto.TokenResponse;
+import nextstep.subway.member.TestMember;
+import nextstep.subway.member.dto.MemberResponse;
 
 @DisplayName("로그인 기능")
 public class AuthAcceptanceTest extends AcceptanceTest {
@@ -43,6 +48,34 @@ public class AuthAcceptanceTest extends AcceptanceTest {
 		로그인_실패됨(response);
 	}
 
+	@DisplayName("유효한 토큰으로 내 정보를 조회한다.")
+	@Test
+	void me() {
+		// given
+		회원_생성되어_있음(윤준석);
+		TokenResponse token = 로그인_요청(윤준석.getEmail(), 윤준석.getPassword()).as(TokenResponse.class);
+
+		// when
+		ExtractableResponse<Response> response = 내_정보_조회_요청(token);
+
+		// then
+		내_정보_조회됨(response, 윤준석);
+	}
+
+	@DisplayName("유효하지 않은 토큰으로 내 정보를 조회하면 실패한다.")
+	@Test
+	void meFail() {
+		// given
+		회원_생성되어_있음(윤준석);
+		로그인_요청(윤준석.getEmail(), 윤준석.getPassword()).as(TokenResponse.class);
+
+		// when
+		ExtractableResponse<Response> response = 내_정보_조회_요청("wrong-token");
+
+		// then
+		내_정보_조회_실패됨(response);
+	}
+
 	private ExtractableResponse<Response> 로그인_요청(String email, String password) {
 		TokenRequest tokenRequest = new TokenRequest(email, password);
 		return post("/login/token", tokenRequest);
@@ -59,19 +92,28 @@ public class AuthAcceptanceTest extends AcceptanceTest {
 		assertThat(response.statusCode()).isEqualTo(HttpStatus.UNAUTHORIZED.value());
 	}
 
-	@DisplayName("Bearer Auth")
-	@Test
-	void myInfoWithBearerAuth() {
+	private ExtractableResponse<Response> 내_정보_조회_요청(TokenResponse token) {
+		return 내_정보_조회_요청(token.getAccessToken());
 	}
 
-	@DisplayName("Bearer Auth 로그인 실패")
-	@Test
-	void myInfoWithBadBearerAuth() {
+	private ExtractableResponse<Response> 내_정보_조회_요청(String accessToken) {
+		return RestAssured.given().log().all()
+			.auth().oauth2(accessToken)
+			.accept(MediaType.APPLICATION_JSON_VALUE)
+			.when().get("/members/me")
+			.then().log().all()
+			.extract();
 	}
 
-	@DisplayName("Bearer Auth 유효하지 않은 토큰")
-	@Test
-	void myInfoWithWrongBearerAuth() {
+	private void 내_정보_조회됨(ExtractableResponse<Response> response, TestMember expectedMember) {
+		MemberResponse actualMember = response.as(MemberResponse.class);
+
+		assertAll(
+			() -> assertThat(actualMember.getEmail()).isEqualTo(expectedMember.getEmail()),
+			() -> assertThat(actualMember.getAge()).isEqualTo(expectedMember.getAge()));
 	}
 
+	private void 내_정보_조회_실패됨(ExtractableResponse<Response> response) {
+		assertThat(response.statusCode()).isEqualTo(HttpStatus.UNAUTHORIZED.value());
+	}
 }
