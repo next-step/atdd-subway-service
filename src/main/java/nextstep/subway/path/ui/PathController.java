247e16ec4233379b3bb6ac0d24fd488fe262edf6
ch3224bin <ahrocksil@gmail.com>
2021-12-06 19:45:17 +0900

    fix : URL 수정 /path -> /paths

--- src/main/java/nextstep/subway/path/ui/PathController.java
+++ src/main/java/nextstep/subway/path/ui/PathController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController()
-@RequestMapping("/path")
+@RequestMapping("/paths")
 public class PathController {
     private final PathService pathService;
 
