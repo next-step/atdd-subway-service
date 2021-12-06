2844ef2abaa428a2db9d0ed5b2dbd1ee6d5eb62d
moon <hoopster@airi.kr>
2021-12-06 19:51:14 +0900

    feat (MemberContoller, AuthenticationPrincipalArgumentResolver) : 인증 토큰 검증 기능 추가

--- src/main/java/nextstep/subway/member/domain/Member.java
+++ src/main/java/nextstep/subway/member/domain/Member.java
@@ -1,57 +1,62 @@
 package nextstep.subway.member.domain;
 
-import nextstep.subway.BaseEntity;
-import nextstep.subway.auth.application.AuthorizationException;
-import org.apache.commons.lang3.StringUtils;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import org.apache.commons.lang3.StringUtils;
+
+import nextstep.subway.BaseEntity;
+import nextstep.subway.auth.application.AuthorizationException;
+
 @Entity
 public class Member extends BaseEntity {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private String email;
-    private String password;
-    private Integer age;
 
-    public Member() {
-    }
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
-    public Member(String email, String password, Integer age) {
-        this.email = email;
-        this.password = password;
-        this.age = age;
-    }
+	private String email;
 
-    public Long getId() {
-        return id;
-    }
+	private String password;
 
-    public String getEmail() {
-        return email;
-    }
+	private Integer age;
 
-    public String getPassword() {
-        return password;
-    }
+	protected Member() {
+	}
 
-    public Integer getAge() {
-        return age;
-    }
+	public Member(String email, String password, Integer age) {
+		this.email = email;
+		this.password = password;
+		this.age = age;
+	}
 
-    public void update(Member member) {
-        this.email = member.email;
-        this.password = member.password;
-        this.age = member.age;
-    }
+	public Long getId() {
+		return id;
+	}
 
-    public void checkPassword(String password) {
-        if (!StringUtils.equals(this.password, password)) {
-            throw new AuthorizationException();
-        }
-    }
+	public String getEmail() {
+		return email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void update(Member member) {
+		this.email = member.email;
+		this.password = member.password;
+		this.age = member.age;
+	}
+
+	public void checkPassword(String password) {
+		if (!StringUtils.equals(this.password, password)) {
+			throw new AuthorizationException();
+		}
+	}
 }
