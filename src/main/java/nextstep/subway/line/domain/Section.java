b393bd9e036fcae14056df4b566ff4d2f28bcfa5
lovethefeel <lovethefeel@gmail.com>
2021-12-08 01:28:28 +0900

    feat: 구간 객체 단위테스트 추가

--- src/main/java/nextstep/subway/line/domain/Section.java
+++ src/main/java/nextstep/subway/line/domain/Section.java
@@ -10,28 +10,41 @@ public class Section {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne(cascade = CascadeType.PERSIST)
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.PERSIST)
     @JoinColumn(name = "line_id")
     private Line line;
 
-    @ManyToOne(cascade = CascadeType.PERSIST)
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.PERSIST)
     @JoinColumn(name = "up_station_id")
     private Station upStation;
 
-    @ManyToOne(cascade = CascadeType.PERSIST)
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.PERSIST)
     @JoinColumn(name = "down_station_id")
     private Station downStation;
 
-    private int distance;
+    @Embedded
+    private Distance distance;
 
-    public Section() {
+    protected Section() {
     }
 
-    public Section(Line line, Station upStation, Station downStation, int distance) {
+    private Section(final Line line, final Station upStation, final Station downStation, final Distance distance) {
         this.line = line;
         this.upStation = upStation;
         this.downStation = downStation;
         this.distance = distance;
+    }
+
+    private Section(final Station upStation, final Station downStation, final Distance distance) {
+        this(null, upStation, downStation, distance);
+    }
+
+    public static Section of(final Line line, final Station upStation, final Station downStation, final Distance distance) {
+        return new Section(line, upStation, downStation, distance);
+    }
+
+    public static Section of(final Station upStation, final Station downStation, final Distance distance) {
+        return new Section(upStation, downStation, distance);
     }
 
     public Long getId() {
@@ -51,22 +64,51 @@ public class Section {
     }
 
     public int getDistance() {
-        return distance;
+        return this.distance.getDistance();
     }
 
-    public void updateUpStation(Station station, int newDistance) {
-        if (this.distance <= newDistance) {
-            throw new RuntimeException("역과 역 사이의 거리보다 좁은 거리를 입력해주세요");
-        }
-        this.upStation = station;
-        this.distance -= newDistance;
+    public void addLine(Line line) {
+        this.line = line;
     }
 
-    public void updateDownStation(Station station, int newDistance) {
-        if (this.distance <= newDistance) {
-            throw new RuntimeException("역과 역 사이의 거리보다 좁은 거리를 입력해주세요");
-        }
-        this.downStation = station;
-        this.distance -= newDistance;
+    public boolean isUpStationAndTargetDownStationEquals(Section targetSection) {
+        return this.upStation.equals(targetSection.downStation);
+    }
+
+    public boolean isDownStationEquals(Section targetSection) {
+        return this.downStation.equals(targetSection.downStation);
+    }
+
+    public boolean isDownStationAndTargetUpStationEquals(Section targetSection) {
+        return this.downStation.equals(targetSection.upStation);
+    }
+
+    public boolean isUpStationEquals(Section targetSection) {
+        return this.upStation.equals(targetSection.upStation);
+    }
+
+    public void changeDownStation(Section targetSection) {
+        this.downStation = targetSection.downStation;
+    }
+
+    public void changeUpStation(Section targetSection) {
+        this.upStation = targetSection.upStation;
+    }
+
+    public boolean isDownStationEquals(Station station) {
+        return this.downStation.equals(station);
+    }
+
+    public boolean isUpStationEquals(Station station) {
+        return this.upStation.equals(station);
+    }
+
+    public void merge(Section targetSection) {
+        this.downStation = targetSection.downStation;
+        this.distance = this.distance.plus(targetSection.distance);
+    }
+
+    public void minusDistance(Section targetSection) {
+        this.distance = this.distance.minus(targetSection.distance);
     }
 }
