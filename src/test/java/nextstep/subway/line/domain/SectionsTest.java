a072c5a559c54b8783e72e8214183793bc7c8542
shryu <>
2021-12-08 02:36:00 +0900

    test : sections 구간 추가 테스트 작성

--- src/test/java/nextstep/subway/line/domain/SectionsTest.java
+++ src/test/java/nextstep/subway/line/domain/SectionsTest.java
@@ -2,27 +2,50 @@ package nextstep.subway.line.domain;
 
 import nextstep.subway.station.domain.Station;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.*;
+
 public class SectionsTest {
+    Line line;
+    Sections sections;
+    Station upStation;
+    Station downStation;
+
+    @BeforeEach
+    void setUp() {
+        line = new Line("2호선", "green");
+        upStation = new Station("강남역");
+        downStation = new Station("잠실역");
+        sections = new Sections(new Section(line, upStation, downStation, 10));
+    }
 
     @DisplayName("노선 역 리스트 조회")
     @Test
     void getStations() {
-        // Given
-        Line line = new Line("2호선", "green");
-        Station upStation = new Station("강남역");
-        Station downStation = new Station("잠실역");
-        Sections sections = new Sections(new Section(line, upStation, downStation, 10));
-
         // When
         List<Station> stations =  sections.getStations();
 
         // Then
-        Assertions.assertThat(stations).containsAll(Arrays.asList(upStation, downStation));
+        assertThat(stations).containsAll(Arrays.asList(upStation, downStation));
+    }
+
+    @DisplayName("노선 역 추가")
+    @Test
+    void addLineStation() {
+        // Given
+        Station newStation = new Station("역삼역");
+
+        // When
+        sections.addLineStations(new Section(line, upStation, newStation, 3));
+
+        // Then
+        List<Station> stations = sections.getStations();
+        assertThat(stations).containsAll(Arrays.asList(upStation, newStation, downStation));
     }
 }
