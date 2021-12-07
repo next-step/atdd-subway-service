07b9261ebc2183d8fa3be77c7624e72f9d54c577
HeeSeok Noh <nhs0912@nate.com>
2021-12-07 22:06:17 +0900

    test : 최단거리 라이브러리 테스트 추가

--- src/test/java/nextstep/subway/path/domain/PathTest.java
+++ src/test/java/nextstep/subway/path/domain/PathTest.java
@@ -6,8 +6,15 @@ import nextstep.subway.line.domain.Line;
 import nextstep.subway.line.domain.Section;
 import nextstep.subway.station.domain.Station;
 import org.assertj.core.api.Assertions;
+import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
+import org.jgrapht.graph.DefaultWeightedEdge;
+import org.jgrapht.graph.WeightedMultigraph;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("Path 테스트")
 class PathTest {
@@ -28,6 +35,26 @@ class PathTest {
                     new Path(dangsanStation, dangsanStation);
                 }).isInstanceOf(InputDataErrorException.class)
                 .hasMessageContaining(InputDataErrorCode.THERE_IS_SAME_STATIONS.errorMessage());
+    }
+
+    @Test
+    @DisplayName("최단거리 외부라이브러리 테스트")
+    public void getDijkstraShortestPath() {
+        WeightedMultigraph<String, DefaultWeightedEdge> graph
+                = new WeightedMultigraph(DefaultWeightedEdge.class);
+        graph.addVertex("v1");
+        graph.addVertex("v2");
+        graph.addVertex("v3");
+        graph.setEdgeWeight(graph.addEdge("v1", "v2"), 2);
+        graph.setEdgeWeight(graph.addEdge("v2", "v3"), 2);
+        graph.setEdgeWeight(graph.addEdge("v1", "v3"), 100);
+
+        DijkstraShortestPath dijkstraShortestPath
+                = new DijkstraShortestPath(graph);
+        List<String> shortestPath
+                = dijkstraShortestPath.getPath("v3", "v1").getVertexList();
+
+        assertThat(shortestPath.size()).isEqualTo(3);
     }
 
 }
