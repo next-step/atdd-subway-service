f1df3667005b523a374cce7f69ce480355e8d348
wooobo <irooori89@gmail.com>
2021-12-08 03:09:30 +0900

    test : `SubwayGraphTest` 실패 테스트 추가

---
+++ src/test/java/nextstep/subway/path/infrastructure/SubwayGraphTest.java
@@ -0,0 +1,46 @@
+package nextstep.subway.path.infrastructure;
+
+import static nextstep.subway.path.domain.PathFixtures.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+import nextstep.subway.common.exception.InvalidParameterException;
+import nextstep.subway.line.domain.Line;
+import nextstep.subway.path.dto.PathResultV2;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+
+class SubwayGraphTest {
+
+    private SubwayGraph subwayGraph;
+
+    @BeforeEach
+    void setUp() {
+        // given
+        List<SectionEdge> sectionEdges = getSectionEdges();
+        subwayGraph = new SubwayGraph(sectionEdges);
+    }
+
+    @Test
+    @DisplayName("출발역과 도착역이 연결이 되어 있지 않은 경우")
+    void 출발역_도착역_연결되어_있지_않음_실패() {
+        // when
+        SubwayPath subwayPath = new SubwayPath(subwayGraph);
+
+        // then
+        assertThrows(InvalidParameterException.class, () -> subwayPath.getShortestPath(강남, 천호));
+    }
+
+    private List<SectionEdge> getSectionEdges() {
+        List<Line> lines = 전체구간();
+        return lines.stream()
+            .map(Line::getSections)
+            .flatMap(Collection::stream)
+            .map(SectionEdge::of)
+            .collect(Collectors.toList());
+    }
+}
