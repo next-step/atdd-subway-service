f607fba4f3473302bb6ed7d160b8f9fd7ad96a38
lovethefeel <lovethefeel@gmail.com>
2021-12-08 01:18:23 +0900

    refactor: 구간 항목 추가

--- src/main/java/nextstep/subway/DataLoaderConfig.java
+++ src/main/java/nextstep/subway/DataLoaderConfig.java
@@ -1,8 +1,7 @@
 package nextstep.subway;
 
 import com.google.common.collect.Lists;
-import nextstep.subway.line.domain.Line;
-import nextstep.subway.line.domain.LineRepository;
+import nextstep.subway.line.domain.*;
 import nextstep.subway.member.domain.Member;
 import nextstep.subway.member.domain.MemberRepository;
 import nextstep.subway.station.domain.Station;
@@ -22,15 +21,18 @@ public class DataLoaderConfig implements CommandLineRunner {
     }
 
     @Override
-    public void run(String... args) throws Exception {
-        Station 강남역 = new Station("강남역");
-        Station 교대역 = new Station("교대역");
-        Station 양재역 = new Station("양재역");
-        Station 남부터미널역 = new Station("남부터미널역");
+    public void run(String... args) {
+        Station 강남역 = Station.from("강남역");
+        Station 교대역 = Station.from("교대역");
+        Station 양재역 = Station.from("양재역");
 
-        Line 신분당선 = new Line("신분당선", "red lighten-1", 강남역, 양재역, 10);
-        Line 이호선 = new Line("2호선", "green lighten-1", 교대역, 강남역, 10);
-        Line 삼호선 = new Line("3호선", "orange darken-1", 교대역, 양재역, 10);
+        Section 첫번째구간 = Section.of(강남역, 양재역, Distance.of(10));
+        Section 두번째구간 = Section.of(교대역, 강남역, Distance.of(10));
+        Section 세번째구간 = Section.of(교대역, 양재역, Distance.of(10));
+
+        Line 신분당선 = Line.of("신분당선", "red lighten-1", Sections.from(첫번째구간));
+        Line 이호선 = Line.of("2호선", "green lighten-1", Sections.from(두번째구간));
+        Line 삼호선 = Line.of("3호선", "orange darken-1", Sections.from(세번째구간));
 
         lineRepository.saveAll(Lists.newArrayList(신분당선, 이호선, 삼호선));
 
