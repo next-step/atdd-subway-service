c615394c11f77eae2f3a7c0c3b9f0586429e55ba
lovethefeel <lovethefeel@gmail.com>
2021-12-08 01:26:33 +0900

    feat: 테스트 픽스쳐 메소드 추가

---
+++ src/test/java/nextstep/subway/fixture/TestStationFactory.java
@@ -0,0 +1,88 @@
+package nextstep.subway.fixture;
+
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import nextstep.subway.station.dto.StationResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+public class TestStationFactory {
+
+    public static ExtractableResponse<Response> 지하철_역_생성_요청(final String name) {
+        Map<String, String> params = new HashMap<>();
+        params.put("name", name);
+
+        // when
+        return RestAssured.given().log().all()
+                .body(params)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when()
+                .post("/stations")
+                .then().log().all()
+                .extract();
+    }
+
+    public static ExtractableResponse<Response> 지히철_역들_조회_요청() {
+        return RestAssured.given().log().all()
+                .when()
+                .get("/stations")
+                .then().log().all()
+                .extract();
+    }
+
+    public static Long 지하철_역_ID_추출(final ExtractableResponse<Response> createResponse1) {
+        return Arrays.asList(createResponse1).stream()
+                .map(res -> Long.parseLong(res.header("Location").split("/")[2]))
+                .findFirst().get();
+    }
+
+    public static void 지하철_역_생성됨(ExtractableResponse<Response> response) {
+        assertAll(
+                () -> assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value()),
+                () -> assertThat(response.header("Location")).isNotBlank()
+        );
+
+    }
+
+    public static void 지하철_역_생성_실패됨(ExtractableResponse<Response> response) {
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+    }
+
+    public static void 지하철_역_응답됨(ExtractableResponse<Response> response) {
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+    }
+
+    public static void 지하철_역_목록_포함됨(ExtractableResponse<Response> createResponse1, ExtractableResponse<Response> createResponse2
+            , ExtractableResponse<Response> response) {
+        List<Long> expectedLineIds = Arrays.asList(createResponse1, createResponse2).stream()
+                .map(res -> Long.parseLong(res.header("Location").split("/")[2]))
+                .collect(Collectors.toList());
+        List<Long> resultLineIds = response.jsonPath().getList(".", StationResponse.class).stream()
+                .map(StationResponse::getId)
+                .collect(Collectors.toList());
+        assertThat(resultLineIds).containsAll(expectedLineIds);
+    }
+
+    public static ExtractableResponse<Response> 지하철_역_제거_요청(final ExtractableResponse<Response> createResponse) {
+        final Long id = 지하철_역_ID_추출(createResponse);
+        return RestAssured.given().log().all()
+                .when()
+                .delete("/stations/"+id)
+                .then().log().all()
+                .extract();
+    }
+
+    public static void 지하철_역_삭제됨(ExtractableResponse<Response> response) {
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.NO_CONTENT.value());
+    }
+}
