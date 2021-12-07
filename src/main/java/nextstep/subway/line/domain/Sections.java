2bb33c77be67288b5984195bc4f23f0520773aaa
devsigner9920 <devsigner9920@icloud.com>
2021-12-07 22:35:48 +0900

    refactor: 구간 일급 컬렉션에 도메인 로직 추가

---
+++ src/main/java/nextstep/subway/line/domain/Sections.java
@@ -0,0 +1,197 @@
+package nextstep.subway.line.domain;
+
+import nextstep.subway.station.domain.Station;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Embeddable;
+import javax.persistence.OneToMany;
+import java.util.*;
+
+@Embeddable
+public class Sections {
+  private static final int DELETABLE_SIZE = 2;
+  @OneToMany(mappedBy = "line", cascade = {CascadeType.PERSIST, CascadeType.MERGE}, orphanRemoval = true)
+  private final List<Section> sections = new ArrayList<>();
+
+  protected Sections() {
+  }
+
+  public Sections(List<Section> sections) {
+    sections.addAll(sections);
+  }
+
+  public void add(Section section) {
+    if (isUpdateSection(section)) {
+      checkAddSectionValidation(section);
+      updateSection(section);
+    }
+    sections.add(section);
+  }
+
+  public void remove(Station station) {
+    checkRemovableSections(station);
+    if (isEndPointStation(station)) {
+      removeEndPointSectionByStation(station);
+      return;
+    }
+
+    removeAndUpdateSectionByStation(station);
+  }
+
+  public Stations getOrderedStations() {
+    if (sections.isEmpty()) {
+      return new Stations(Collections.emptyList());
+    }
+
+    List<Station> stations = new ArrayList<>(Arrays.asList(upEndPoint().getUpStation(), upEndPoint().getDownStation()));
+    while (!hasDownEndPointStation(stations)) {
+      stations.add(findNextLinkedStation(stations));
+    }
+
+    return new Stations(stations);
+  }
+
+  private void checkRemovableSections(Station station) {
+    if (sections.size() < DELETABLE_SIZE) {
+      throw new RuntimeException("구간이 하나일 경우, 지하철 역을 제거할 수 없습니다.");
+    }
+    if (!getOrderedStations().contains(station)) {
+      throw new RuntimeException("노선에 등록되지 않은 역은 제거할 수 없습니다.");
+    }
+  }
+
+  private boolean isEndPointStation(Station station) {
+    return isUpEndPointStation(station)
+        || isDownEndPointStation(station);
+  }
+
+  private void removeEndPointSectionByStation(Station station) {
+    if (isUpEndPointStation(station)) {
+      sections.removeIf(section -> section.equals(upEndPoint()));
+      return;
+    }
+
+    sections.removeIf(section -> section.equals(downEndPoint()));
+  }
+
+  private void removeAndUpdateSectionByStation(Station station) {
+    Section removeTargetSection = sections.stream()
+        .filter(section -> section.getUpStation().equals(station))
+        .findFirst()
+        .orElseThrow(IllegalStateException::new);
+    sections.remove(removeTargetSection);
+    updateByRemoveTargetSection(station, removeTargetSection);
+  }
+
+  private void updateSection(Section newSection) {
+    if (hasMatchUpStation(newSection)) {
+      updateMatchedUpSideSection(newSection);
+      return;
+    }
+
+    updateMatchedDownSideSection(newSection);
+  }
+
+  private boolean hasMatchUpStation(Section section) {
+    return sections.stream()
+        .map(Section::getUpStation)
+        .anyMatch(station -> station.equals(section.getUpStation()));
+  }
+
+  private void updateByRemoveTargetSection(Station station, Section removeTargetSection) {
+    sections.stream()
+        .filter(section -> section.getDownStation().equals(station))
+        .findFirst()
+        .ifPresent(section -> section.updateDownStation(removeTargetSection.getDownStation(), removeTargetSection.getDistance()));
+  }
+
+  private void checkAddSectionValidation(Section section) {
+    if (!containsStation(section)) {
+      throw new RuntimeException("등록할 수 없는 구간 입니다.");
+    }
+
+    if (containSection(section)) {
+      throw new RuntimeException("이미 등록된 구간 입니다.");
+    }
+  }
+
+  private void updateMatchedUpSideSection(Section newSection) {
+    sections.stream()
+        .filter(section -> section.getUpStation().equals(newSection.getUpStation()))
+        .findFirst()
+        .ifPresent(section -> section.updateUpSideSection(newSection));
+  }
+
+  private void updateMatchedDownSideSection(Section newSection) {
+    sections.stream()
+        .filter(section -> section.getDownStation().equals(newSection.getDownStation()))
+        .findFirst()
+        .ifPresent(section -> section.updateDownSideSection(newSection));
+  }
+
+  private boolean containSection(Section section) {
+    return sections.stream()
+        .anyMatch(existSection -> existSection.isMatch(section));
+  }
+
+  private boolean containsStation(Section section) {
+    List<Station> stationList = new ArrayList<>(Arrays.asList(section.getUpStation(), section.getDownStation()));
+    return getOrderedStations().containsAny(stationList);
+  }
+
+  private boolean isUpdateSection(Section section) {
+    return !sections.isEmpty()
+        && !isUpEndPointStation(section.getDownStation())
+        && !isDownEndPointStation(section.getUpStation());
+  }
+
+  private boolean isDownEndPointStation(Station station) {
+    return downEndPoint().getDownStation().equals(station);
+  }
+
+  private boolean isUpEndPointStation(Station station) {
+    return upEndPoint().getUpStation().equals(station);
+  }
+
+  private Section upEndPoint() {
+    return sections.stream()
+        .filter(this::isUpEndPoint)
+        .findAny()
+        .orElseThrow(IllegalStateException::new);
+  }
+
+  private boolean isUpEndPoint(Section parentSection) {
+    return sections.stream()
+        .map(Section::getDownStation)
+        .noneMatch(station -> station.equals(parentSection.getUpStation()));
+  }
+
+  private Section downEndPoint() {
+    return sections.stream()
+        .filter(this::isDownEndPoint)
+        .findAny()
+        .orElseThrow(IllegalStateException::new);
+  }
+
+  private boolean isDownEndPoint(Section parentSection) {
+    return sections.stream()
+        .map(Section::getUpStation)
+        .noneMatch(station -> station.equals(parentSection.getDownStation()));
+  }
+
+  private boolean hasDownEndPointStation(List<Station> stations) {
+    return stationsLastElement(stations).equals(downEndPoint().getDownStation());
+  }
+
+  private Station stationsLastElement(List<Station> stations) {
+    return stations.get(stations.size() - 1);
+  }
+
+  private Station findNextLinkedStation(List<Station> stations) {
+    return sections.stream()
+        .filter(section -> section.getUpStation().equals(stationsLastElement(stations)))
+        .findAny()
+        .orElseThrow(IllegalArgumentException::new)
+        .getDownStation();
+  }
+}
--- src/main/java/nextstep/subway/station/dto/StationResponse.java
+++ src/main/java/nextstep/subway/station/dto/StationResponse.java
@@ -3,40 +3,48 @@ package nextstep.subway.station.dto;
 import nextstep.subway.station.domain.Station;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class StationResponse {
-    private Long id;
-    private String name;
-    private LocalDateTime createdDate;
-    private LocalDateTime modifiedDate;
+  private Long id;
+  private String name;
+  private LocalDateTime createdDate;
+  private LocalDateTime modifiedDate;
 
-    public static StationResponse of(Station station) {
-        return new StationResponse(station.getId(), station.getName(), station.getCreatedDate(), station.getModifiedDate());
-    }
+  public static StationResponse of(Station station) {
+    return new StationResponse(station.getId(), station.getName(), station.getCreatedDate(), station.getModifiedDate());
+  }
 
-    public StationResponse() {
-    }
+  public static List<StationResponse> ofList(List<Station> stations) {
+    return stations.stream()
+        .map(StationResponse::of)
+        .collect(Collectors.toList());
+  }
 
-    public StationResponse(Long id, String name, LocalDateTime createdDate, LocalDateTime modifiedDate) {
-        this.id = id;
-        this.name = name;
-        this.createdDate = createdDate;
-        this.modifiedDate = modifiedDate;
-    }
+  public StationResponse() {
+  }
 
-    public Long getId() {
-        return id;
-    }
+  public StationResponse(Long id, String name, LocalDateTime createdDate, LocalDateTime modifiedDate) {
+    this.id = id;
+    this.name = name;
+    this.createdDate = createdDate;
+    this.modifiedDate = modifiedDate;
+  }
 
-    public String getName() {
-        return name;
-    }
+  public Long getId() {
+    return id;
+  }
 
-    public LocalDateTime getCreatedDate() {
-        return createdDate;
-    }
+  public String getName() {
+    return name;
+  }
 
-    public LocalDateTime getModifiedDate() {
-        return modifiedDate;
-    }
+  public LocalDateTime getCreatedDate() {
+    return createdDate;
+  }
+
+  public LocalDateTime getModifiedDate() {
+    return modifiedDate;
+  }
 }
