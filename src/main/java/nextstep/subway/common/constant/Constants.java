1f321650f7da3b43257cf03a924821114ac026d1
lovethefeel <lovethefeel@gmail.com>
2021-12-08 01:17:35 +0900

    feat: 공통 상수 생성

---
+++ src/main/java/nextstep/subway/common/constant/Constants.java
@@ -0,0 +1,8 @@
+package nextstep.subway.common.constant;
+
+public class Constants {
+    public static final int INT_ONE = 1;
+    public static final int INT_ZERO = 0;
+
+    private Constants(){}
+}
