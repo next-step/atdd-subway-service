742d6ec8d9397fb44203b7a0f7fee8d930de9d72
devsigner9920 <devsigner9920@icloud.com>
2021-12-07 22:34:43 +0900

    refactor: 노선 엔티티 리팩터링

--- src/main/java/nextstep/subway/line/domain/Line.java
+++ src/main/java/nextstep/subway/line/domain/Line.java
@@ -1,56 +1,78 @@
 package nextstep.subway.line.domain;
 
 import nextstep.subway.BaseEntity;
+import nextstep.subway.line.application.Distance;
 import nextstep.subway.station.domain.Station;
 
 import javax.persistence.*;
-import java.util.ArrayList;
 import java.util.List;
 
 @Entity
 public class Line extends BaseEntity {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    @Column(unique = true)
-    private String name;
-    private String color;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
 
-    @OneToMany(mappedBy = "line", cascade = {CascadeType.PERSIST, CascadeType.MERGE}, orphanRemoval = true)
-    private List<Section> sections = new ArrayList<>();
+  @Column(unique = true)
+  private String name;
 
-    public Line() {
-    }
+  private String color;
 
-    public Line(String name, String color) {
-        this.name = name;
-        this.color = color;
-    }
+  @Embedded
+  private Sections sections = new Sections();
 
-    public Line(String name, String color, Station upStation, Station downStation, int distance) {
-        this.name = name;
-        this.color = color;
-        sections.add(new Section(this, upStation, downStation, distance));
-    }
+  public Line() {
+  }
 
-    public void update(Line line) {
-        this.name = line.getName();
-        this.color = line.getColor();
-    }
+  public Line(String name, String color) {
+    this.name = name;
+    this.color = color;
+  }
 
-    public Long getId() {
-        return id;
-    }
+  public Line(Long id, String name, String color, Section section) {
+    this.id = id;
+    this.name = name;
+    this.color = color;
+    sections.add(section);
+  }
 
-    public String getName() {
-        return name;
-    }
+  public Line(String name, String color, Station upStation, Station downStation, Distance distance) {
+    this.name = name;
+    this.color = color;
+    sections.add(new Section(this, upStation, downStation, distance));
+  }
 
-    public String getColor() {
-        return color;
-    }
+  public void update(Line line) {
+    this.name = line.getName();
+    this.color = line.getColor();
+  }
 
-    public List<Section> getSections() {
-        return sections;
-    }
+  public void addSection(Section newSection) {
+    newSection.addLine(this);
+    sections.add(newSection);
+  }
+
+  public void removeStation(Station station) {
+    sections.remove(station);
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getColor() {
+    return color;
+  }
+
+  public Sections getSections() {
+    return sections;
+  }
+
+  public List<Station> getStations() {
+    return sections.getOrderedStations().getStations();
+  }
 }
