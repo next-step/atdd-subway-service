290c46997c51c2432991fb8e8dd898b31251b229
eedys1234 <eedys1234>
2021-12-08 08:12:37 +0900

    feat(PathAcceptanceTest) : 최단경로 인수테스트 작성

---
+++ src/main/java/nextstep/subway/ExceptionAdvice.java
@@ -0,0 +1,16 @@
+package nextstep.subway;
+
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ExceptionAdvice {
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity handleIllegalArgsException(DataIntegrityViolationException e) {
+        return ResponseEntity.badRequest().build();
+    }
+}
