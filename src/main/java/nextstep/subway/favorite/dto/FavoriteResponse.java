f67fb62f52b379e6a31eb087160505b4db7def4b
moon <hoopster@airi.kr>
2021-12-07 11:06:17 +0900

    feat (FavoriteResponse) : 응답 Dto 파싱 메소드 추가

--- src/main/java/nextstep/subway/favorite/dto/FavoriteResponse.java
+++ src/main/java/nextstep/subway/favorite/dto/FavoriteResponse.java
@@ -1,5 +1,9 @@
 package nextstep.subway.favorite.dto;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import nextstep.subway.favorite.domain.Favorite;
 import nextstep.subway.station.domain.Station;
 
 public class FavoriteResponse {
@@ -7,7 +11,17 @@ public class FavoriteResponse {
 	private Long id;
 	private Station source;
 	private Station target;
-	
+
+	public FavoriteResponse(final Long id, final Station source, final Station target) {
+		this.id = id;
+		this.source = source;
+		this.target = target;
+	}
+
+	public static FavoriteResponse of(final Favorite favorite) {
+		return new FavoriteResponse(favorite.getId(), favorite.getSource(), favorite.getSource());
+	}
+
 	public Long getId() {
 		return id;
 	}
@@ -18,5 +32,11 @@ public class FavoriteResponse {
 
 	public Station getTarget() {
 		return target;
+	}
+
+	public static List<FavoriteResponse> ofList(List<Favorite> favorites) {
+		return favorites.stream()
+			.map(FavoriteResponse::of)
+			.collect(Collectors.toList());
 	}
 }
