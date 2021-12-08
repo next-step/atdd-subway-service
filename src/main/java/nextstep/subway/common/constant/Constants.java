2401d2141da59ba893cb3692ebcfe9ba50c77338
lovethefeel <lovethefeel@gmail.com>
2021-12-09 02:30:16 +0900

    refactor: 피드백 반영 사용하지 않는 클래스 파일 삭제

--- src/main/java/nextstep/subway/common/constant/Constants.java
+++
@@ -1,8 +0,0 @@
-package nextstep.subway.common.constant;
-
-public class Constants {
-    public static final int INT_ONE = 1;
-    public static final int INT_ZERO = 0;
-
-    private Constants(){}
-}
