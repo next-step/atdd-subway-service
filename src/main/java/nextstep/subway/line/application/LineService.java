9c4e0003154f5413fed9f2b267ea6ccc3ef5b018
Kelicia91 <kelicia91@gmail.com>
2021-12-09 00:59:11 +0900

    refactor: remove duplicated code

--- src/main/java/nextstep/subway/line/application/LineService.java
+++ src/main/java/nextstep/subway/line/application/LineService.java
@@ -49,7 +49,7 @@ public class LineService {
     }
 
     public void updateLine(Long id, LineRequest lineUpdateRequest) {
-        Line persistLine = lineRepository.findById(id).orElseThrow(RuntimeException::new);
+        Line persistLine = findLineById(id);
         persistLine.update(lineUpdateRequest.getName(), lineUpdateRequest.getColor());
     }
 
