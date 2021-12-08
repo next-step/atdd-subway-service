afe7a8dcdaf9314d4ccdea28d77d196d8a61c3f4
Jeongkyo Kim <chavitak@gmail.com>
2021-12-08 20:18:33 +0900

    style: 가독성을 위해 변수 사이에 라인을 추가한다

--- src/main/java/nextstep/subway/line/domain/Line.java
+++ src/main/java/nextstep/subway/line/domain/Line.java
@@ -13,8 +13,10 @@ public class Line extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @Column(unique = true)
     private String name;
+
     private String color;
 
     @Embedded
