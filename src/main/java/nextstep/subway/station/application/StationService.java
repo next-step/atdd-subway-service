8eb5a32e5ab3c732d155f3d710410ae0aff0669f
donkey <rlaehdzlsla@gmail.com>
2021-12-08 15:07:20 +0900

    refactor: stationService 중복 메서드 제거

--- src/main/java/nextstep/subway/station/application/StationService.java
+++ src/main/java/nextstep/subway/station/application/StationService.java
@@ -5,7 +5,6 @@ import nextstep.subway.station.domain.StationRepository;
 import nextstep.subway.station.dto.StationRequest;
 import nextstep.subway.station.dto.StationResponse;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -36,10 +35,6 @@ public class StationService {
     }
 
     public Station findStationById(Long id) {
-        return stationRepository.findById(id).orElseThrow(RuntimeException::new);
-    }
-
-    public Station findById(Long id) {
         return stationRepository.findById(id).orElseThrow(RuntimeException::new);
     }
 }
