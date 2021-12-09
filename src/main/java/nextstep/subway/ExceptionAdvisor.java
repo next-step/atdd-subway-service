0bf58311b98e8580c63ccc7a83604d6be4a4e906
songseongbin <ppsosqq@gmail.com>
2021-12-09 17:57:25 +0900

    fix(ExceptionAdvisor): ExceptionAdvisor 추가
    - IllegalArgumentException에 대해 BadRequest를 응답하도록 추가

---
+++ src/main/java/nextstep/subway/ExceptionAdvisor.java
@@ -0,0 +1,14 @@
+package nextstep.subway;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ExceptionAdvisor {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException e) {
+        return ResponseEntity.badRequest().body(e.getMessage());
+    }
+}
