fe0909af6c198f9d524a1d1fccfb607e2937880b
wooobo <irooori89@gmail.com>
2021-12-10 18:43:49 +0900

    feat : `Favorite`, `Favorites` 도메인 추가

--- src/main/java/nextstep/subway/member/domain/Member.java
+++ src/main/java/nextstep/subway/member/domain/Member.java
@@ -1,5 +1,9 @@
 package nextstep.subway.member.domain;
 
+import java.util.List;
+import java.util.Objects;
+import javax.persistence.Column;
+import javax.persistence.Embedded;
 import nextstep.subway.BaseEntity;
 import nextstep.subway.auth.application.AuthorizationException;
 import org.apache.commons.lang3.StringUtils;
@@ -15,9 +19,16 @@ public class Member extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(unique = true)
     private String email;
+
     private String password;
+
     private Integer age;
+
+    @Embedded
+    private final Favorites favorites = Favorites.of();
 
     protected Member() {
     }
@@ -54,5 +65,39 @@ public class Member extends BaseEntity {
         if (!StringUtils.equals(this.password, password)) {
             throw new AuthorizationException();
         }
+    }
+
+    public void addFavorite(Favorite favorite) {
+        this.favorites.add(favorite);
+    }
+
+    public List<Favorite> getFavorites() {
+        return favorites.getFavorites();
+    }
+
+    public void removeFavoriteById(Long favoriteId) {
+        favorites.removeById(favoriteId);
+    }
+
+    public List<Long> getFavoriteStationIds() {
+        return favorites.getFavoriteStationIds();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Member member = (Member) o;
+
+        return Objects.equals(id, member.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, email, password, age, favorites);
     }
 }
