2c940ad1196d247d6b69648a90ed8d3aa1641e0f
shryu <>
2021-12-11 05:13:34 +0900

    feat : path service 구현

---
+++ src/main/java/nextstep/subway/path/application/PathService.java
@@ -0,0 +1,33 @@
+package nextstep.subway.path.application;
+
+import nextstep.subway.line.domain.Line;
+import nextstep.subway.line.domain.LineRepository;
+import nextstep.subway.path.domain.PathFinder;
+import nextstep.subway.path.dto.PathResponse;
+import nextstep.subway.station.domain.Station;
+import nextstep.subway.station.domain.StationRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PathService {
+    private final LineRepository lineRepository;
+    private final StationRepository stationRepository;
+
+    public PathService(LineRepository lineRepository, StationRepository stationRepository) {
+        this.lineRepository = lineRepository;
+        this.stationRepository = stationRepository;
+    }
+
+    public PathResponse findPath(Long sourceId, Long targetId) {
+        List<Line> lines = lineRepository.findAll();
+        Station sourceStation = findStationById(sourceId);
+        Station targetStation = findStationById(targetId);
+        return PathFinder.findPath(lines, sourceStation, targetStation);
+    }
+
+    public Station findStationById(Long id) {
+        return stationRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Station을 조회할 수 없습니다."));
+    }
+}
