2e1bf8823ba14d3e4de8773c4b97aaa16494f404
haedoang <haedoang@gmail.com>
2021-12-10 10:57:45 +0900

    feat(discount): 연령별 할인액 계산 구현

---
+++ src/main/java/nextstep/subway/auth/domain/Stranger.java
@@ -0,0 +1,8 @@
+package nextstep.subway.auth.domain;/** 
+ * packageName : nextstep.subway.auth.domain
+ * fileName : Stranger
+ * author : haedoang
+ * date : 2021-12-10
+ * description : 
+ */public class Stranger {
+}
---
+++ src/main/java/nextstep/subway/auth/domain/User.java
@@ -0,0 +1,8 @@
+package nextstep.subway.auth.domain;/** 
+ * packageName : nextstep.subway.auth.domain
+ * fileName : User
+ * author : haedoang
+ * date : 2021-12-10
+ * description : 
+ */public class User {
+}
