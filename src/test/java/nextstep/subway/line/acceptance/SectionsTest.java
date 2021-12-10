d22680d6c0877e8f728eed0955044b1470879859
All-ForOne <ddabizz@gmail.com>
2021-12-10 23:31:22 +0900

    refact: addStation 메소드 중복 제거

--- src/test/java/nextstep/subway/line/acceptance/SectionsTest.java
+++ src/test/java/nextstep/subway/line/acceptance/SectionsTest.java
@@ -17,6 +17,11 @@ public class SectionsTest {
         Line line = new Line("1호선", "red", upStation, downStation, 10);
         line.addStation(upStation, new Station("부천역"), 5);
         line.addStation(downStation, new Station("온수역"), 5);
-        assertThat(line.getStations().stream().map(s -> s.getName()).collect(Collectors.toList())).containsExactly("소사역", "부천역", "역곡역", "온수역");
+
+        assertThat(line.getStations()
+                .stream()
+                .map(s -> s.getName())
+                .collect(Collectors.toList()))
+                .containsExactly("소사역", "부천역", "역곡역", "온수역");
     }
 }
