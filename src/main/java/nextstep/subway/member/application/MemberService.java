1a683bd6ef1cd5f81c5213d300335a0ca5cbb3f1
moon <hoopster@airi.kr>
2021-12-10 16:56:36 +0900

    refac : 정적팩토리 메소드를 사용하도록 리팩토링

--- src/main/java/nextstep/subway/member/application/MemberService.java
+++ src/main/java/nextstep/subway/member/application/MemberService.java
@@ -1,36 +1,44 @@
 package nextstep.subway.member.application;
 
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import nextstep.subway.auth.exception.AuthorizationException;
 import nextstep.subway.member.domain.Member;
 import nextstep.subway.member.domain.MemberRepository;
 import nextstep.subway.member.dto.MemberRequest;
 import nextstep.subway.member.dto.MemberResponse;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional(readOnly = true)
 public class MemberService {
-    private MemberRepository memberRepository;
+	private final MemberRepository memberRepository;
 
-    public MemberService(MemberRepository memberRepository) {
-        this.memberRepository = memberRepository;
-    }
+	public MemberService(final MemberRepository memberRepository) {
+		this.memberRepository = memberRepository;
+	}
 
-    public MemberResponse createMember(MemberRequest request) {
-        Member member = memberRepository.save(request.toMember());
-        return MemberResponse.of(member);
-    }
+	@Transactional
+	public MemberResponse createMember(MemberRequest request) {
+		Member member = memberRepository.save(request.toMember());
+		return MemberResponse.of(member);
+	}
 
-    public MemberResponse findMember(Long id) {
-        Member member = memberRepository.findById(id).orElseThrow(RuntimeException::new);
-        return MemberResponse.of(member);
-    }
+	public MemberResponse findMember(Long id) {
+		Member member = memberRepository.findById(id)
+			.orElseThrow(AuthorizationException::new);
+		return MemberResponse.of(member);
+	}
 
-    public void updateMember(Long id, MemberRequest param) {
-        Member member = memberRepository.findById(id).orElseThrow(RuntimeException::new);
-        member.update(param.toMember());
-    }
+	@Transactional
+	public void updateMember(Long id, MemberRequest param) {
+		Member member = memberRepository.findById(id)
+			.orElseThrow(AuthorizationException::new);
+		member.update(param.toMember());
+	}
 
-    public void deleteMember(Long id) {
-        memberRepository.deleteById(id);
-    }
+	@Transactional
+	public void deleteMember(Long id) {
+		memberRepository.deleteById(id);
+	}
 }
