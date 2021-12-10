ccabd298540370f61d4170b6da7bef5d1af27bf3
All-ForOne <ddabizz@gmail.com>
2021-12-10 23:31:22 +0900

    refact: addStation 메소드 중복 제거

--- src/main/java/nextstep/subway/line/domain/Section.java
+++ src/main/java/nextstep/subway/line/domain/Section.java
@@ -70,6 +70,14 @@ public class Section implements Comparable<Section> {
         this.distance -= newDistance;
     }
 
+    public void updateStation(Station upStation, Station downStation, int distance) {
+        if (this.upStation.equals(upStation)) {
+            updateUpStation(downStation, distance);
+            return;
+        }
+        updateDownStation(upStation, distance);
+    }
+
     @Override
     public int compareTo(Section o) {
         if (this.upStation.equals(o.downStation)) {
--- src/main/java/nextstep/subway/line/domain/Sections.java
+++ src/main/java/nextstep/subway/line/domain/Sections.java
@@ -22,75 +22,65 @@ public class Sections {
         }
         Collections.sort(sections);
         List<Station> stations = new ArrayList<>();
-        stations.add(findUpStation());
+        stations.add(findFirstStation());
         stations.addAll(sections.stream()
                 .map(s -> s.getDownStation())
                 .collect(Collectors.toList()));
         return stations;
     }
 
-    private Station findUpStation() {
+    private Station findFirstStation() {
         return sections.get(0).getUpStation();
     }
 
     public void addStation(Line line, Station upStation, Station downStation, int distance) {
-        List<Station> stations = getStations();
-        if (stations.isEmpty()) {
+        if (getStations().isEmpty()) {
             sections.add(new Section(line, upStation, downStation, distance));
             return;
         }
 
-        validateAddStation(stations, upStation, downStation);
-
-        if (isUpStationExisted(stations, upStation)) {
-            sections.stream()
-                    .filter(it -> it.getUpStation() == upStation)
-                    .findFirst()
-                    .ifPresent(it -> it.updateUpStation(downStation, distance));
-
-            sections.add(new Section(line, upStation, downStation, distance));
-        } else if (isDownStationExisted(stations, downStation)) {
-            sections.stream()
-                    .filter(it -> it.getDownStation() == downStation)
-                    .findFirst()
-                    .ifPresent(it -> it.updateDownStation(upStation, distance));
-
-            sections.add(new Section(line, upStation, downStation, distance));
-        }
+        validateAddStation(upStation, downStation);
+        findConnectedSection(upStation, downStation).ifPresent(s -> s.updateStation(upStation, downStation, distance));
+        sections.add(new Section(line, upStation, downStation, distance));
     }
 
-    private void validateAddStation(List<Station> stations, Station upStation, Station downStation) {
-        boolean isUpStationExisted = isUpStationExisted(stations, upStation);
-        boolean isDownStationExisted = isDownStationExisted(stations, downStation);
+    private Optional<Section> findConnectedSection(Station upStation, Station downStation) {
+        Optional<Section> findUpStation = getUpStation(upStation);
+        if (findUpStation.isPresent()) {
+            return findUpStation;
+        }
+
+        return getDownStation(downStation);
+    }
+
+    private void validateAddStation(Station upStation, Station downStation) {
+        boolean isUpStationExisted = isUpStationExisted(upStation);
+        boolean isDownStationExisted = isDownStationExisted(downStation);
 
         if (isUpStationExisted && isDownStationExisted) {
-            throw new RuntimeException("이미 등록된 구간 입니다.");
+            throw new IllegalArgumentException("이미 등록된 구간 입니다.");
         }
 
-        if (!stations.isEmpty() && !isUpStationExisted && !isDownStationExisted) {
-            throw new RuntimeException("등록할 수 없는 구간 입니다.");
+        if (!getStations().isEmpty() && !isUpStationExisted && !isDownStationExisted) {
+            throw new IllegalArgumentException("등록할 수 없는 구간 입니다.");
         }
     }
 
-    private boolean isDownStationExisted(List<Station> stations, Station downStation) {
-        return stations.stream()
-                .anyMatch(it -> it == downStation);
+    private boolean isDownStationExisted(Station downStation) {
+        return getStations().stream()
+                .anyMatch(it -> it.equals(downStation));
     }
 
-    private boolean isUpStationExisted(List<Station> stations, Station upStation) {
-        return stations.stream()
-                .anyMatch(it -> it == upStation);
+    private boolean isUpStationExisted(Station upStation) {
+        return getStations().stream()
+                .anyMatch(it -> it.equals(upStation));
     }
 
     public void removeLineStation(Line line, Station station) {
         validateRemoveLineStation();
 
-        Optional<Section> upLineStation = sections.stream()
-                .filter(it -> it.getUpStation() == station)
-                .findFirst();
-        Optional<Section> downLineStation = sections.stream()
-                .filter(it -> it.getDownStation() == station)
-                .findFirst();
+        Optional<Section> upLineStation = getUpStation(station);
+        Optional<Section> downLineStation = getDownStation(station);
 
         if (upLineStation.isPresent() && downLineStation.isPresent()) {
             Station newUpStation = downLineStation.get().getUpStation();
@@ -103,9 +93,21 @@ public class Sections {
         downLineStation.ifPresent(it -> sections.remove(it));
     }
 
+    private Optional<Section> getUpStation(Station station) {
+        return sections.stream()
+                .filter(it -> it.getUpStation().equals(station))
+                .findFirst();
+    }
+
+    private Optional<Section> getDownStation(Station downStation) {
+        return sections.stream()
+                .filter(it -> it.getDownStation().equals(downStation))
+                .findFirst();
+    }
+
     private void validateRemoveLineStation() {
         if (sections.size() <= 1) {
-            throw new RuntimeException();
+            throw new IllegalArgumentException("구간이 1개만 남아있어 삭제할 수 없습니다.");
         }
     }
 }
