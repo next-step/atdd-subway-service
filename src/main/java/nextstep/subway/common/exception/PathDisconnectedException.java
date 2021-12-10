c5b2b82b3fb9b8ea4dd17f251b705b97f1ec2e45
lovethefeel <lovethefeel@gmail.com>
2021-12-11 04:47:56 +0900

    feat: 출발역과 도착역이 연결되어 있지 않은 경우 예외처리

---
+++ src/main/java/nextstep/subway/common/exception/PathDisconnectedException.java
@@ -0,0 +1,9 @@
+package nextstep.subway.common.exception;
+
+public class PathDisconnectedException extends RuntimeException{
+    private static final String NOT_LINK_REQUEST_PATH = "요청한 경로는 연결되어 있지 않습니다.";
+
+    public PathDisconnectedException() {
+        super(NOT_LINK_REQUEST_PATH);
+    }
+}
