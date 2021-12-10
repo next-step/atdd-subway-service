7fb48dd0fcd688677fe976e4ab7ee57738e1472f
ChangSubKwak <paradise1732@gmail.com>
2021-12-11 02:35:33 +0900

    feat: 노선 요청 클래스(LineRequest)에 요금 필드 추가에 따른 생성자 추가 및 변경

--- src/main/java/nextstep/subway/line/dto/LineRequest.java
+++ src/main/java/nextstep/subway/line/dto/LineRequest.java
@@ -1,27 +1,32 @@
 package nextstep.subway.line.dto;
 
-import nextstep.subway.line.domain.Line;
-
 public class LineRequest {
     private String name;
     private String color;
     private Long upStationId;
     private Long downStationId;
     private int distance;
+    private int extraFare;
 
     public LineRequest() {
     }
 
-    private LineRequest(String name, String color, Long upStationId, Long downStationId, int distance) {
+    private LineRequest(String name, String color, Long upStationId, Long downStationId, int distance, int extraFare) {
         this.name = name;
         this.color = color;
         this.upStationId = upStationId;
         this.downStationId = downStationId;
         this.distance = distance;
+        this.extraFare = extraFare;
     }
 
     public static LineRequest of(String name, String color, Long upStationId, Long downStationId, int distance) {
-        return new LineRequest(name, color, upStationId, downStationId, distance);
+        return new LineRequest(name, color, upStationId, downStationId, distance, 0);
+    }
+
+    public static LineRequest of(String name, String color, Long upStationId, Long downStationId, int distance,
+        int extraFare) {
+        return new LineRequest(name, color, upStationId, downStationId, distance, extraFare);
     }
 
     public String getName() {
@@ -44,7 +49,7 @@ public class LineRequest {
         return distance;
     }
 
-    public Line toLine() {
-        return new Line(name, color);
+    public int getExtraFare() {
+        return extraFare;
     }
 }
