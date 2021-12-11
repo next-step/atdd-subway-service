80ab4583c1120ef6c43e5f1e4df403c248898b5b
JiHun Hong <hongji3354@gmail.com>
2021-12-11 23:44:12 +0900

    test : PathFinderTest

--- src/test/java/nextstep/subway/domain/path/domain/PathFinderTest.java
+++ src/test/java/nextstep/subway/domain/path/domain/PathFinderTest.java
@@ -1,37 +1,22 @@
-package nextstep.subway.domain.path.application;
+package nextstep.subway.domain.path.domain;
 
-import nextstep.subway.domain.line.application.LineService;
 import nextstep.subway.domain.line.domain.Distance;
 import nextstep.subway.domain.line.domain.Line;
 import nextstep.subway.domain.path.dto.PathFinderRequest;
-import nextstep.subway.domain.path.dto.PathFinderResponse;
 import nextstep.subway.domain.path.exception.NotConnectedStation;
 import nextstep.subway.domain.path.exception.SameDepartureAndArrivalStationException;
 import nextstep.subway.domain.path.exception.StationNotFoundException;
 import nextstep.subway.domain.station.domain.Station;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Arrays;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.Mockito.when;
 
-@ExtendWith(MockitoExtension.class)
-class PathServiceTest {
-
-    @InjectMocks
-    private PathService pathService;
-
-    @Mock
-    private LineService lineService;
-
+class PathFinderTest {
 
     @Test
     @DisplayName("최단 경로 조회")
@@ -46,16 +31,14 @@ class PathServiceTest {
         final Line 삼호선 = new Line("삼호선", "bg-orange-200", 교대역, 양재역, new Distance(5));
         삼호선.addSection(교대역, 남부터미널역, new Distance(3));
 
-        when(lineService.findAll()).thenReturn(Arrays.asList(신분당선, 이호선, 삼호선));
-
         // when
-        final PathFinderRequest pathFinderRequest = new PathFinderRequest(교대역.getId(), 양재역.getId());
-        final PathFinderResponse pathFinderResponse = pathService.findPaths(pathFinderRequest);
+        final PathFinder pathFinder = new PathFinder(Arrays.asList(신분당선, 이호선, 삼호선));
+        final Route shortestRoute = pathFinder.findShortestRoute(교대역.getId(), 양재역.getId());
 
         // then
         assertAll(() -> {
-            assertThat(pathFinderResponse.getStations()).extracting("name").containsExactly("교대역","남부터미널역","양재역");
-            assertThat(pathFinderResponse.getDistance()).isEqualTo(5);
+            assertThat(shortestRoute.getStations()).extracting("name").containsExactly("교대역","남부터미널역","양재역");
+            assertThat(shortestRoute.getDistance()).isEqualTo(5);
         });
     }
 
@@ -67,12 +50,10 @@ class PathServiceTest {
         final Station 교대역 = new Station(2L, "교대역");
         final Line 이호선 = new Line("이호선", "bg-green-400", 교대역, 강남역, new Distance(10));
 
-        when(lineService.findAll()).thenReturn(Arrays.asList(이호선));
-
         // when
-        final PathFinderRequest pathFinderRequest = new PathFinderRequest(교대역.getId(), 교대역.getId());
+        final PathFinder pathFinder = new PathFinder(Arrays.asList(이호선));
         assertThrows(SameDepartureAndArrivalStationException.class,
-                () -> pathService.findPaths(pathFinderRequest));
+                () -> pathFinder.findShortestRoute(교대역.getId(), 교대역.getId()));
     }
 
     @Test
@@ -86,12 +67,11 @@ class PathServiceTest {
         final Line 이호선 = new Line("이호선", "bg-green-400", 교대역, 강남역, new Distance(10));
         final Line 구호선 = new Line("구호선", "bg-partridge-400", 동작역, 신논현역, new Distance(10));
 
-        when(lineService.findAll()).thenReturn(Arrays.asList(이호선, 구호선));
+        final PathFinder pathFinder = new PathFinder(Arrays.asList(이호선, 구호선));
 
         // when
-        final PathFinderRequest pathFinderRequest = new PathFinderRequest(교대역.getId(), 동작역.getId());
         assertThrows(NotConnectedStation.class,
-                () -> pathService.findPaths(pathFinderRequest));
+                () -> pathFinder.findShortestRoute(교대역.getId(), 동작역.getId()));
     }
 
     @Test
@@ -102,11 +82,11 @@ class PathServiceTest {
         final Station 교대역 = new Station(2L, "교대역");
         final Line 이호선 = new Line("이호선", "bg-green-400", 교대역, 강남역, new Distance(10));
 
-        when(lineService.findAll()).thenReturn(Arrays.asList(이호선));
+        final PathFinder pathFinder = new PathFinder(Arrays.asList( 이호선));
 
         // when
         final PathFinderRequest pathFinderRequest = new PathFinderRequest(교대역.getId(), 100L);
         assertThrows(StationNotFoundException.class,
-                () -> pathService.findPaths(pathFinderRequest));
+                () -> pathFinder.findShortestRoute(교대역.getId(), 100L));
     }
 }
