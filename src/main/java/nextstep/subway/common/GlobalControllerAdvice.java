82915fbcad5c577f38fe614f43385d9faa05da93
Chloe <page.prologue@gmail.com>
2021-12-11 15:19:43 +0900

    refactor : 예외 응답 클래스 추가

--- src/main/java/nextstep/subway/common/GlobalControllerAdvice.java
+++ src/main/java/nextstep/subway/common/GlobalControllerAdvice.java
@@ -8,38 +8,54 @@ import nextstep.subway.station.application.exception.StationNotFoundException;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 @RestControllerAdvice
 public class GlobalControllerAdvice {
 
+    @ResponseStatus(BAD_REQUEST)
     @ExceptionHandler(DataIntegrityViolationException.class)
-    public ResponseEntity handler(DataIntegrityViolationException e) {
-        return ResponseEntity.badRequest().build();
+    public ResponseEntity<ApiError> handler(DataIntegrityViolationException e) {
+        ApiError apiError = new ApiError(BAD_REQUEST, e.getLocalizedMessage(), e.getMessage());
+        return ResponseEntity.status(apiError.getStatus()).body(apiError);
     }
 
+    @ResponseStatus(NOT_FOUND)
     @ExceptionHandler(StationNotFoundException.class)
-    public ResponseEntity handler(StationNotFoundException e) {
-        return ResponseEntity.notFound().build();
+    public ResponseEntity<ApiError> handler(StationNotFoundException e) {
+        ApiError apiError = new ApiError(NOT_FOUND, e.getLocalizedMessage(), e.getMessage());
+        return ResponseEntity.status(apiError.getStatus()).body(apiError);
     }
 
+    @ResponseStatus(NOT_FOUND)
     @ExceptionHandler(SectionNotFoundException.class)
-    public ResponseEntity handler(SectionNotFoundException e) {
-        return ResponseEntity.notFound().build();
+    public ResponseEntity<ApiError> handler(SectionNotFoundException e) {
+        ApiError apiError = new ApiError(NOT_FOUND, e.getLocalizedMessage(), e.getMessage());
+        return ResponseEntity.status(apiError.getStatus()).body(apiError);
     }
 
+    @ResponseStatus(NOT_FOUND)
     @ExceptionHandler(LineNotFoundException.class)
-    public ResponseEntity handler(LineNotFoundException e) {
-        return ResponseEntity.notFound().build();
+    public ResponseEntity<ApiError> handler(LineNotFoundException e) {
+        ApiError apiError = new ApiError(NOT_FOUND, e.getLocalizedMessage(), e.getMessage());
+        return ResponseEntity.status(apiError.getStatus()).body(apiError);
     }
 
+    @ResponseStatus(BAD_REQUEST)
     @ExceptionHandler(InvalidSectionException.class)
-    public ResponseEntity handler(InvalidSectionException e) {
-        return ResponseEntity.badRequest().build();
+    public ResponseEntity<ApiError> handler(InvalidSectionException e) {
+        ApiError apiError = new ApiError(BAD_REQUEST, e.getLocalizedMessage(), e.getMessage());
+        return ResponseEntity.status(apiError.getStatus()).body(apiError);
     }
 
+    @ResponseStatus(BAD_REQUEST)
     @ExceptionHandler(InvalidPathException.class)
-    public ResponseEntity handler(InvalidPathException e) {
-        return ResponseEntity.badRequest().build();
+    public ResponseEntity<ApiError> handler(InvalidPathException e) {
+        ApiError apiError = new ApiError(BAD_REQUEST, e.getLocalizedMessage(), e.getMessage());
+        return ResponseEntity.status(apiError.getStatus()).body(apiError);
     }
 }
