cdc9648d21beb17b25c7e1a3125c30b21d69c22a
Junhwan Kim <junhwan.tech@gmail.com>
2021-12-11 11:14:52 +0900

    refactor: 경로 조회 인수 테스트 구현  - Happy Path와 최소한의 dto 클래스만 추가

---
+++ src/main/java/nextstep/subway/path/dto/PathResponse.java
@@ -0,0 +1,12 @@
+package nextstep.subway.path.dto;
+
+import java.util.Collections;
+import java.util.List;
+import nextstep.subway.station.domain.Station;
+
+public class PathResponse {
+
+    public List<Station> getStations() {
+        return Collections.emptyList();
+    }
+}
