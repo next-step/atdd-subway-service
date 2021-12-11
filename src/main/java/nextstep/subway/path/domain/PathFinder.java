ab1a5644c143056bdb72df2f92859fe6b45a7d65
condingknowjam <hanwoodww@gmail.com>
2021-12-11 15:41:14 +0900

    refactor: 변수명 수정

--- src/main/java/nextstep/subway/path/domain/PathFinder.java
+++ src/main/java/nextstep/subway/path/domain/PathFinder.java
@@ -9,24 +9,24 @@ import nextstep.subway.line.domain.Section;
 import nextstep.subway.station.domain.Station;
 
 public class PathFinder {
-	private static SubwayGraph graph2;
+	private static SubwayGraph graph;
 
 	private PathFinder() {
 	}
 
 	public static PathFinder create(List<Station> stations, List<Section> sections) {
-		graph2 = new SubwayGraph(SectionEdge.class);
-		stations.stream().forEach(station -> graph2.addVertex(station));
-		sections.stream().forEach(section -> graph2.addSectionEdge(section));
+		graph = new SubwayGraph(SectionEdge.class);
+		stations.stream().forEach(station -> graph.addVertex(station));
+		sections.stream().forEach(section -> graph.addSectionEdge(section));
 		return new PathFinder();
 	}
 
 	public List<Station> findShortestPathVertexes(Station source, Station target) {
-		return new DijkstraShortestPath(graph2).getPath(source, target).getVertexList();
+		return new DijkstraShortestPath(graph).getPath(source, target).getVertexList();
 	}
 
 	public Distance findShortestPathDistance(Station source, Station target) {
-		return Distance.of(new DijkstraShortestPath(graph2).getPath(source, target).getWeight());
+		return Distance.of(new DijkstraShortestPath(graph).getPath(source, target).getWeight());
 	}
 
 }
