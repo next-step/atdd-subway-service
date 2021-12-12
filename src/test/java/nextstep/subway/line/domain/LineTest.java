5fc538fdcdf573e96f69a685646e09b427165602
Sung Du Kim <sungdu.alex.kim@gmail.com>
2021-12-12 15:01:27 +0900

    refactor(LineTest): @DataJpaTest 애노테이션 제거

--- src/test/java/nextstep/subway/line/domain/LineTest.java
+++ src/test/java/nextstep/subway/line/domain/LineTest.java
@@ -5,26 +5,16 @@ import static org.junit.jupiter.api.Assertions.assertAll;
 
 import nextstep.subway.station.domain.Station;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
-@DataJpaTest
 public class LineTest {
-
-    @Autowired
-    private LineRepository lineRepository;
 
     @Test
     void create_noSection() {
         // given
         final Line line = new Line("신분당선", "bg-red-600");
 
-        // when
-        lineRepository.save(line);
-
-        // then
+        // when, then
         assertAll(
-            () -> assertThat(line.getId()).isNotNull(),
             () -> assertThat(line.getName()).isEqualTo("신분당선"),
             () -> assertThat(line.getColor()).isEqualTo("bg-red-600"),
             () -> assertThat(line.getStationsInOrder()).isEmpty()
@@ -38,12 +28,8 @@ public class LineTest {
         final Station downStation = new Station("광교역");
         final Line line = new Line("신분당선", "bg-red-600", upStation, downStation, 12);
 
-        // when
-        lineRepository.save(line);
-
-        // then
+        // when, then
         assertAll(
-            () -> assertThat(line.getId()).isNotNull(),
             () -> assertThat(line.getName()).isEqualTo("신분당선"),
             () -> assertThat(line.getColor()).isEqualTo("bg-red-600"),
             () -> assertThat(line.getStationsInOrder().size()).isEqualTo(2)
