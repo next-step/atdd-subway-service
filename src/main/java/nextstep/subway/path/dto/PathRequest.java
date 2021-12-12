5d3d4a2b5615572469d680ea5325ff9181f68d64
minho <lights8615@gmail.com>
2021-12-12 15:20:04 +0900

    feat(step2): 출발역과 도착역이 같은 경우 에러

--- src/main/java/nextstep/subway/path/dto/PathRequest.java
+++ src/main/java/nextstep/subway/path/dto/PathRequest.java
@@ -1,12 +1,22 @@
 package nextstep.subway.path.dto;
 
+import nextstep.subway.common.exception.SubwayErrorCode;
+import nextstep.subway.common.exception.SubwayException;
+
 public class PathRequest {
     private long source;
     private long target;
 
     public PathRequest(long source, long target) {
+        validate(source, target);
         this.source = source;
         this.target = target;
+    }
+
+    private void validate(long source, long target) {
+        if (source == target) {
+            throw new SubwayException(SubwayErrorCode.SAME_SOURCE_AND_TARGET);
+        }
     }
 
     public long getSource() {
