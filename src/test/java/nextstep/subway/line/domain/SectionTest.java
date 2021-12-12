b381739723f4f3b075925a96d4cd6d65d48a3574
Junhong-Kim <jhk1005jhk@gmail.com>
2021-12-12 21:51:16 +0900

    test: Sections 단위 테스트 추가

--- src/test/java/nextstep/subway/line/domain/SectionTest.java
+++ src/test/java/nextstep/subway/line/domain/SectionTest.java
@@ -95,4 +95,57 @@ class SectionTest {
         // when & then
         assertThat(구간.isEqualDownStation(강남역)).isFalse();
     }
+
+    @DisplayName("상행역 구간 삭제 테스트")
+    @Test
+    void deleteByUpStation() {
+        // given
+        Section 신규_구간 = Section.of(신분당선, 강남역, 양재역, 5);
+        신분당선.getSections().addSection(신규_구간);
+
+        // when
+        신분당선.getSections().deleteByUpStation(강남역);
+
+        // then
+        assertThat(신분당선.getStations().getStations()).containsExactly(양재역, 광교역);
+    }
+
+
+    @DisplayName("하행역 구간 삭제 테스트")
+    @Test
+    void deleteByDownStation() {
+        // given
+        Section 신규_구간 = Section.of(신분당선, 강남역, 양재역, 5);
+        신분당선.getSections().addSection(신규_구간);
+
+        // when
+        신분당선.getSections().deleteByDownStation(광교역);
+
+        // then
+        assertThat(신분당선.getStations().getStations()).containsExactly(강남역, 양재역);
+    }
+
+    @DisplayName("중간역 구간 삭제 테스트")
+    @Test
+    void deleteByBetweenStation() {
+        // given
+        Section 신규_구간 = Section.of(신분당선, 강남역, 양재역, 5);
+        신분당선.getSections().addSection(신규_구간);
+
+        // when
+        신분당선.getSections().deleteByBetweenStation(양재역);
+
+        // then
+        assertThat(신분당선.getStations().getStations()).containsExactly(강남역, 광교역);
+    }
+
+    @DisplayName("첫번째 구간 조회 테스트")
+    @Test
+    void getFirstSection() {
+        // when
+        Section 첫번째_구간 = 신분당선.getSections().getFirstSection();
+
+        // then
+        assertThat(첫번째_구간).isEqualTo(구간);
+    }
 }
