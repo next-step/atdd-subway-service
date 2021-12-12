0c7724adce928a54dcc1663bfff5dd1629db8d8c
Honghee Choi <vvshinevv@nhnsoft.com>
2021-12-12 17:57:26 +0900

    feat: addLineSection 리팩토링

--- src/main/java/nextstep/subway/line/domain/Line.java
+++ src/main/java/nextstep/subway/line/domain/Line.java
@@ -3,9 +3,14 @@ package nextstep.subway.line.domain;
 import nextstep.subway.BaseEntity;
 import nextstep.subway.station.domain.Station;
 
-import javax.persistence.*;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
@@ -81,6 +86,65 @@ public class Line extends BaseEntity {
         return downStation;
     }
 
+    public void addToLineStation(Station upStation, Station downStation, int distance) {
+        List<Station> stations = this.getStations();
+        validateAddLineStation(stations, upStation, downStation);
+
+        if (stations.isEmpty()) {
+            this.getSections().add(new Section(this, upStation, downStation, distance));
+            return;
+        }
+
+        updateUpStation(stations, upStation, downStation, distance);
+        updateDownStation(stations, upStation, downStation, distance);
+    }
+
+    private void updateUpStation(List<Station> stations, Station upStation, Station downStation, int distance) {
+        if (isUpStationExisted(stations, upStation)) {
+            this.getSections().stream()
+                    .filter(it -> it.getUpStation() == upStation)
+                    .findFirst()
+                    .ifPresent(it -> it.updateUpStation(downStation, distance));
+
+            this.getSections().add(new Section(this, upStation, downStation, distance));
+        }
+    }
+
+    private void updateDownStation(List<Station> stations, Station upStation, Station downStation, int distance) {
+        if (isDownStationExisted(stations, downStation)) {
+            this.getSections().stream()
+                    .filter(it -> it.getDownStation() == downStation)
+                    .findFirst()
+                    .ifPresent(it -> it.updateDownStation(upStation, distance));
+
+            this.getSections().add(new Section(this, upStation, downStation, distance));
+        }
+    }
+
+    private boolean isUpStationExisted(List<Station> stations, Station upStation) {
+        return stations.stream().anyMatch(it -> it == upStation);
+    }
+
+    private boolean isDownStationExisted(List<Station> stations, Station downStation) {
+        return stations.stream().anyMatch(it -> it == downStation);
+    }
+
+    private void validateAddLineStation(List<Station> stations, Station upStation, Station downStation) {
+        if (isUpStationExisted(stations, upStation) && isDownStationExisted(stations, downStation)) {
+            throw new RuntimeException("이미 등록된 구간 입니다.");
+        }
+
+        if (isNotStationMatched(stations, upStation, downStation)) {
+            throw new RuntimeException("등록할 수 없는 구간 입니다.");
+        }
+    }
+
+    private boolean isNotStationMatched(List<Station> stations, Station upStation, Station downStation) {
+        return !stations.isEmpty() &&
+                stations.stream().noneMatch(it -> it == upStation) &&
+                stations.stream().noneMatch(it -> it == downStation);
+    }
+
     public Long getId() {
         return id;
     }
@@ -96,4 +160,6 @@ public class Line extends BaseEntity {
     public List<Section> getSections() {
         return sections;
     }
+
+
 }
