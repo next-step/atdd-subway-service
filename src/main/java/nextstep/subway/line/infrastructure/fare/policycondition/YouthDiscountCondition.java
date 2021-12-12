f6eb5bd6dea420fa2e7d8d39d51deeb6bef6bcd7
wooobo <irooori89@gmail.com>
2021-12-13 04:02:55 +0900

    feat : `연령별 정책` 모듈 구현체 추가

---
+++ src/main/java/nextstep/subway/line/infrastructure/fare/policycondition/YouthDiscountCondition.java
@@ -0,0 +1,26 @@
+package nextstep.subway.line.infrastructure.fare.policycondition;
+
+import java.math.BigDecimal;
+import nextstep.subway.line.domain.Money;
+import nextstep.subway.line.domain.policycondition.AgeDiscountFareCondition;
+import nextstep.subway.member.domain.Age;
+import nextstep.subway.member.domain.AgeGroup;
+
+public class YouthDiscountCondition implements AgeDiscountFareCondition {
+
+    private static final AgeGroup AGE_TYPE = AgeGroup.YOUTH;
+    private static final BigDecimal DISCOUNT_RATE = BigDecimal.valueOf(0.2);
+
+    public static YouthDiscountCondition of() {
+        return new YouthDiscountCondition();
+    }
+
+    @Override
+    public Money discountFare(Money money, Age age) {
+        if (!AGE_TYPE.isMatch(age.getAge())) {
+            return money;
+        }
+
+        return calculate(money, DISCOUNT_RATE);
+    }
+}
