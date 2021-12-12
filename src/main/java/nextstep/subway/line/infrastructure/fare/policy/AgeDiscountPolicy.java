02be2d417e8555d9ba31f7df18a92894989608d4
wooobo <irooori89@gmail.com>
2021-12-13 04:02:11 +0900

    feat : `AgeDiscountFarePolicy`, `DistanceFarePolicy` 정책 구현체 추가

---
+++ src/main/java/nextstep/subway/line/infrastructure/fare/policy/AgeDiscountPolicy.java
@@ -0,0 +1,21 @@
+package nextstep.subway.line.infrastructure.fare.policy;
+
+import java.util.Arrays;
+import java.util.List;
+import nextstep.subway.line.domain.policycondition.AgeDiscountFareCondition;
+import nextstep.subway.line.domain.policy.AgeDiscountFarePolicy;
+import nextstep.subway.line.infrastructure.fare.policycondition.ChildDiscountCondition;
+import nextstep.subway.line.infrastructure.fare.policycondition.YouthDiscountCondition;
+
+public class AgeDiscountPolicy extends AgeDiscountFarePolicy {
+
+    private static final List<AgeDiscountFareCondition> CONDITIONS = Arrays.asList(
+        YouthDiscountCondition.of(),
+        ChildDiscountCondition.of()
+    );
+
+    @Override
+    protected List<AgeDiscountFareCondition> getConditions() {
+        return CONDITIONS;
+    }
+}
---
+++ src/main/java/nextstep/subway/line/infrastructure/fare/policy/DistancePolicy.java
@@ -0,0 +1,23 @@
+package nextstep.subway.line.infrastructure.fare.policy;
+
+import java.util.Arrays;
+import java.util.List;
+import nextstep.subway.line.domain.policy.DistanceFarePolicy;
+import nextstep.subway.line.domain.policycondition.DistancePolicyCondition;
+import nextstep.subway.line.infrastructure.fare.policycondition.DefaultDistanceFareCondition;
+import nextstep.subway.line.infrastructure.fare.policycondition.DistanceFareFirstSectionCondition;
+import nextstep.subway.line.infrastructure.fare.policycondition.DistanceFareSecondSectionCondition;
+
+public class DistancePolicy extends DistanceFarePolicy {
+
+    private static final List<DistancePolicyCondition> CONDITIONS = Arrays.asList(
+        DefaultDistanceFareCondition.of(),
+        DistanceFareFirstSectionCondition.of(),
+        DistanceFareSecondSectionCondition.of()
+    );
+
+    @Override
+    public List<DistancePolicyCondition> getConditions() {
+        return CONDITIONS;
+    }
+}
