c528341708d3903985aa8f5270b6290463dd52c3
wooobo <irooori89@gmail.com>
2021-12-13 04:06:14 +0900

    feat : `FarePolicyHandler`를 `@Bean` 객체로 등록

---
+++ src/main/java/nextstep/subway/config/SubwayFarePolicyConfig.java
@@ -0,0 +1,39 @@
+package nextstep.subway.config;
+
+import nextstep.subway.line.application.FarePolicyHandler;
+import nextstep.subway.line.domain.policy.LineAdditionalFarePolicy;
+import nextstep.subway.line.infrastructure.fare.policy.FarePolicyHandlerImpl;
+import nextstep.subway.line.domain.policy.BaseFarePolicy;
+import nextstep.subway.line.infrastructure.fare.policy.AgeDiscountPolicy;
+import nextstep.subway.line.infrastructure.fare.policy.DistancePolicy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SubwayFarePolicyConfig {
+
+    @Bean
+    public FarePolicyHandler subwayFarePolicyHandler() {
+        FarePolicyHandler subwayFarePolicyHandler = new FarePolicyHandlerImpl();
+        subwayFarePolicyHandler.link(additionalFarePolicy());
+        subwayFarePolicyHandler.link(distanceFarePolicy());
+        subwayFarePolicyHandler.link(ageDiscountPolicy());
+
+        return subwayFarePolicyHandler;
+    }
+
+    @Bean
+    public BaseFarePolicy additionalFarePolicy() {
+        return new LineAdditionalFarePolicy();
+    }
+
+    @Bean
+    public BaseFarePolicy distanceFarePolicy() {
+        return new DistancePolicy();
+    }
+
+    @Bean
+    public BaseFarePolicy ageDiscountPolicy() {
+        return new AgeDiscountPolicy();
+    }
+}
