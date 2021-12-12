ee6f119952b75c647a804a1b9147c011e69febc4
Wangki Lee <lwk3007@gmail.com>
2021-12-12 15:56:43 +0900

    refactor: enum 안에서 나이에 대한 검증 하도록 변경

--- src/main/java/nextstep/subway/member/domain/MemberAgeType.java
+++ src/main/java/nextstep/subway/member/domain/MemberAgeType.java
@@ -1,7 +1,22 @@
 package nextstep.subway.member.domain;
 
+import java.util.Arrays;
+import java.util.function.Function;
+
 public enum MemberAgeType {
-    NONE, KID, ADOLESCENT;
+    NONE(age -> age < 6 || age >= 19),
+    KID(age -> age >= 6 && age < 13),
+    ADOLESCENT(age -> age >= 13 && age < 19);
+
+    private final Function<Integer, Boolean> expression;
+
+    MemberAgeType(Function<Integer, Boolean> expression) {
+        this.expression = expression;
+    }
+
+    private boolean check(int age) {
+        return expression.apply(age);
+    }
 
     public boolean isNone() {
         return this == NONE;
@@ -12,22 +27,9 @@ public enum MemberAgeType {
     }
 
     public static MemberAgeType getMemberAgeType(int age) {
-        if (isKidAge(age)) {
-            return MemberAgeType.KID;
-        }
-
-        if (isAdolescentAge(age)) {
-            return MemberAgeType.ADOLESCENT;
-        }
-
-        return MemberAgeType.NONE;
-    }
-
-    private static boolean isKidAge(int age) {
-        return age >= 6 && age < 13;
-    }
-
-    private static boolean isAdolescentAge(int age) {
-        return age >= 13 && age < 19;
+        return Arrays.stream(values())
+            .filter(ageType -> ageType.check(age))
+            .findFirst()
+            .orElse(NONE);
     }
 }
