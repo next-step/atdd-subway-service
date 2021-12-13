935d6499e76f3a54ea3dc5ab24b96f5dd4e28d04
jennie267 <jennieee267@gmail.com>
2021-12-14 01:41:40 +0900

    refactor : member관련 정리

--- src/main/java/nextstep/subway/member/application/MemberService.java
+++ src/main/java/nextstep/subway/member/application/MemberService.java
@@ -11,7 +11,7 @@ import nextstep.subway.member.dto.MemberResponse;
 @Service
 @Transactional
 public class MemberService {
-    private MemberRepository memberRepository;
+    private final MemberRepository memberRepository;
 
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
@@ -29,11 +29,13 @@ public class MemberService {
     
     @Transactional(readOnly = true)
     public Member findById(Long id) {
-        return memberRepository.findById(id).orElseThrow(RuntimeException::new);
+        return memberRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("계정이 없습니다."));
     }
+    
 
     public void updateMember(Long id, MemberRequest param) {
-        Member member = memberRepository.findById(id).orElseThrow(RuntimeException::new);
+        Member member = findById(id);
         member.update(param.toMember());
     }
 
