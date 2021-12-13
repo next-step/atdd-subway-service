fbd726cd0c0a5a84d04c069b803da8c3e5408a5f
eedys1234 <eedys1234>
2021-12-13 20:38:46 +0900

    chore : 피드백 적용

--- src/main/java/nextstep/subway/path/domain/Path.java
+++ src/main/java/nextstep/subway/path/domain/Path.java
@@ -2,16 +2,17 @@ package nextstep.subway.path.domain;
 
 import nextstep.subway.line.domain.Distance;
 import nextstep.subway.line.domain.Line;
+import nextstep.subway.path.infra.PathFinderStrategy;
 import nextstep.subway.station.domain.Station;
 
 import java.util.List;
 
 public class Path {
 
-    private final PathFinder pathFinder;
+    private final PathFinderStrategy pathFinderStrategy;
 
-    public Path(PathFinder pathFinder) {
-        this.pathFinder = pathFinder;
+    public Path(PathFinderStrategy pathFinderStrategy) {
+        this.pathFinderStrategy = pathFinderStrategy;
     }
 
     private void validateCorrectSourceAndTarget(Station source, Station target) {
@@ -22,11 +23,11 @@ public class Path {
 
     public List<Station> findPaths(List<Line> lines, Station source, Station target) {
         validateCorrectSourceAndTarget(source, target);
-        return this.pathFinder.findStations(lines, source, target);
+        return this.pathFinderStrategy.findStations(lines, source, target);
     }
 
     public Distance findPathWeight(List<Line> lines, Station source, Station target) {
         validateCorrectSourceAndTarget(source, target);
-        return this.pathFinder.findDistance(lines, source, target);
+        return this.pathFinderStrategy.findDistance(lines, source, target);
     }
 }
