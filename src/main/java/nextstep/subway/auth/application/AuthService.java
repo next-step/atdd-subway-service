b0eb964ced23eb737edc52d08de03b32e20f01e5
kelicia91 <kelicia91@gmail.com>
2021-12-14 01:15:40 +0900

    fix: missing exception message

--- src/main/java/nextstep/subway/auth/application/AuthService.java
+++ src/main/java/nextstep/subway/auth/application/AuthService.java
@@ -29,7 +29,7 @@ public class AuthService {
 
     public LoginMember findMemberByToken(String credentials) {
         if (!jwtTokenProvider.validateToken(credentials)) {
-            throw new AuthorizationException();
+            throw new AuthorizationException("유효하지 않은 토큰입니다.");
         }
 
         String email = jwtTokenProvider.getPayload(credentials);
