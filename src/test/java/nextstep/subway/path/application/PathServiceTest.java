ae65a0830071e2645ec177f122d53cd435908480
minseoklim <mslim8803@naver.com>
2021-12-14 01:09:54 +0900

    test: Outside-in 테스트 코드 추가

---
+++ src/test/java/nextstep/subway/path/application/PathServiceTest.java
@@ -0,0 +1,76 @@
+package nextstep.subway.path.application;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import nextstep.subway.line.domain.Line;
+import nextstep.subway.line.domain.LineRepository;
+import nextstep.subway.line.domain.Section;
+import nextstep.subway.path.dto.PathResponse;
+import nextstep.subway.station.application.StationService;
+import nextstep.subway.station.domain.Station;
+import nextstep.subway.station.dto.StationResponse;
+
+@ExtendWith(MockitoExtension.class)
+class PathServiceTest {
+    @Mock
+    private LineRepository lineRepository;
+    @Mock
+    private StationService stationService;
+    @InjectMocks
+    private PathService pathService;
+
+    private List<Line> lines;
+    private Station 강남역;
+    private Station 양재역;
+    private Station 교대역;
+    private Station 남부터미널역;
+
+    /**
+     * 교대역    --- *2호선* ---   강남역
+     * |                        |
+     * *3호선*                   *신분당선*
+     * |                        |
+     * 남부터미널역 --- *3호선* --- 양재역
+     */
+    @BeforeEach
+    void setUp() {
+        // given
+        강남역 = new Station("강남역");
+        양재역 = new Station("양재역");
+        교대역 = new Station("교대역");
+        남부터미널역 = new Station("남부터미널역");
+
+        final Line 신분당선 = new Line("신분당선", "red", 강남역, 양재역, 10);
+        final Line 이호선 = new Line("2호선", "green", 교대역, 강남역, 10);
+        final Line 삼호선 = new Line("3호선", "orange", 교대역, 양재역, 5);
+        삼호선.addSection(new Section(삼호선, 교대역, 남부터미널역, 3));
+
+        lines = Arrays.asList(신분당선, 이호선, 삼호선);
+    }
+
+    @Test
+    void computeShortestDistance() {
+        // given
+        when(lineRepository.findAll()).thenReturn(lines);
+        when(stationService.findById(anyLong())).thenReturn(남부터미널역).thenReturn(강남역);
+
+        // when
+        final PathResponse pathResponse = pathService.computeShortestDistance(1L, 2L);
+
+        // then
+        assertThat(pathResponse.getStations())
+            .containsExactly(StationResponse.of(남부터미널역), StationResponse.of(양재역), StationResponse.of(강남역));
+        assertThat(pathResponse.getDistance()).isEqualTo(12);
+    }
+}
