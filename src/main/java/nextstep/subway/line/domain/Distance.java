a9c58c2c42c5d10fda0e4ec0986464e59a9e6440
All-ForOne <ddabizz@gmail.com>
2021-12-14 01:16:21 +0900

    feat: equals & hashcode override

--- src/main/java/nextstep/subway/line/domain/Distance.java
+++ src/main/java/nextstep/subway/line/domain/Distance.java
@@ -1,6 +1,7 @@
 package nextstep.subway.line.domain;
 
 import javax.persistence.Embeddable;
+import java.util.Objects;
 
 @Embeddable
 public class Distance {
@@ -25,5 +26,18 @@ public class Distance {
             throw new IllegalArgumentException("역과 역 사이의 거리보다 좁은 거리를 입력해주세요");
         }
         return new Distance(this.distance - newDistance);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Distance distance1 = (Distance) o;
+        return distance == distance1.distance;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(distance);
     }
 }
