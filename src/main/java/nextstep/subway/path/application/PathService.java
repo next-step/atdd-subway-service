1296c4b9f99ab008b7bf39cab201a0eb16aaa515
donkey <rlaehdzlsla@gmail.com>
2021-12-13 23:04:27 +0900

    refactor: 조회용 서비스 readOnly = true 설정

--- src/main/java/nextstep/subway/path/application/PathService.java
+++ src/main/java/nextstep/subway/path/application/PathService.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
-@Transactional
+@Transactional(readOnly = true)
 public class PathService {
 
     private final LineService lineService;
