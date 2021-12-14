8431c8a886188509d8a3b7dcf7589ce7d70c53a3
Chloe <page.prologue@gmail.com>
2021-12-14 18:02:39 +0900

    test : 지하철 요금 할인 정책 테스트 작성

---
+++ src/test/java/nextstep/subway/path/domain/fare/DiscountPolicyTest.java
@@ -0,0 +1,24 @@
+package nextstep.subway.path.domain.fare;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+class DiscountPolicyTest {
+
+    private final DiscountPolicy discountPolicy = new DiscountPolicy();
+
+    @ParameterizedTest(name = "연령별 할인: [{0}]={1}")
+    @CsvSource(value = {"2, 0", "7, 450", "15, 720", "40, 1250", "71, 0"})
+    void getOverDistance50km(int age, int excepted) {
+        // given
+        Fare fare = new Fare();
+
+        // when
+        int discountFare = discountPolicy.getDiscountFare(age, fare);
+
+        // then
+        assertThat(discountFare).isEqualTo(excepted);
+    }
+}
