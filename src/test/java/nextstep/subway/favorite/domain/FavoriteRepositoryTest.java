ce160f237eeaeb33d24d816fb49a30bf7a4d3a9d
lovethefeel <lovethefeel@gmail.com>
2021-12-15 02:55:04 +0900

    test: 즐겨찾기 기능 레파지토리 테스트 추가

---
+++ src/test/java/nextstep/subway/favorite/domain/FavoriteRepositoryTest.java
@@ -0,0 +1,91 @@
+package nextstep.subway.favorite.domain;
+
+import nextstep.subway.line.domain.*;
+import nextstep.subway.member.domain.Member;
+import nextstep.subway.member.domain.MemberRepository;
+import nextstep.subway.station.domain.Station;
+import nextstep.subway.station.domain.StationRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+@DataJpaTest
+class FavoriteRepositoryTest {
+    @Autowired
+    LineRepository lineRepository;
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Autowired
+    StationRepository stationRepository;
+
+    @Autowired
+    FavoriteRepository favoriteRepository;
+
+    @DisplayName("즐겨찾기를 저장한다.")
+    @Test
+    void myFavoriteSave() {
+        // given
+        final Member member = memberRepository.save(Member.of("email@email.com", "1234", 10));
+        final Station source = stationRepository.save(Station.from("잠실역"));
+        final Station target = stationRepository.save(Station.from("강변역"));
+        final Section section = Section.of(source, target, Distance.of(10));
+        lineRepository.save(Line.of("이호선", "blue", Sections.from(Arrays.asList(section))));
+        final Favorite actual = Favorite.of(member, source, target);
+
+        // when
+        final Favorite expected = favoriteRepository.save(actual);
+
+        // then
+        assertAll(
+                () -> assertThat(actual).isEqualTo(expected)
+        );
+    }
+
+    @DisplayName("즐겨찾기 목록을 조회한다.")
+    @Test
+    void myFavoriteSearch() {
+        // given
+        final Member member = memberRepository.save(Member.of("email@email.com", "1234", 10));
+        final Station source = stationRepository.save(Station.from("잠실역"));
+        final Station target = stationRepository.save(Station.from("강변역"));
+        final Section section = Section.of(source, target, Distance.of(10));
+        lineRepository.save(Line.of("이호선", "blue", Sections.from(Arrays.asList(section))));
+        final Favorite actual = favoriteRepository.save(Favorite.of(member, source, target));
+
+        // when
+        final Favorite expected = favoriteRepository.findById(actual.getId()).get();
+
+        // then
+        assertAll(
+                () -> assertThat(actual).isEqualTo(expected)
+        );
+    }
+
+    @DisplayName("즐겨찾기를 삭제한다.")
+    @Test
+    void myFavoriteDelete() {
+        // given
+        final Member member = memberRepository.save(Member.of("email@email.com", "1234", 10));
+        final Station source = stationRepository.save(Station.from("잠실역"));
+        final Station target = stationRepository.save(Station.from("강변역"));
+        final Section section = Section.of(source, target, Distance.of(10));
+        lineRepository.save(Line.of("이호선", "blue", Sections.from(Arrays.asList(section))));
+        final Favorite actual = favoriteRepository.save(Favorite.of(member, source, target));
+        final Long favoriteId = actual.getId();
+
+        // when
+        favoriteRepository.delete(actual);
+        final Favorite expected = favoriteRepository.findById(favoriteId).orElse(null);
+
+        // then
+        assertThat(expected).isNull();
+    }
+}
