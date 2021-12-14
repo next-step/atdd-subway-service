fa7ee342d9e4b20408cc3a1066f2a8fb6aba5b6c
lovethefeel <lovethefeel@gmail.com>
2021-12-15 02:55:47 +0900

    Refactor: 상위 CustomeException 클래스 추가

---
+++ src/main/java/nextstep/subway/common/CustomException.java
@@ -0,0 +1,12 @@
+package nextstep.subway.common;
+
+public class CustomException extends RuntimeException {
+
+    public CustomException() {
+        super();
+    }
+
+    public CustomException(String message) {
+        super(message);
+    }
+}
