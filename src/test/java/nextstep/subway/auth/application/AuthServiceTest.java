bd9d48aae6cacb6126d57ad6a1b18979c50be805
lovethefeel <lovethefeel@gmail.com>
2021-12-15 03:01:55 +0900

    test: mockito를 활용한 findMemberByToken 메소드 테스트 추가

--- src/test/java/nextstep/subway/auth/application/AuthServiceTest.java
+++ src/test/java/nextstep/subway/auth/application/AuthServiceTest.java
@@ -1,11 +1,13 @@
 package nextstep.subway.auth.application;
 
+import nextstep.subway.auth.domain.LoginMember;
 import nextstep.subway.member.domain.Member;
 import nextstep.subway.member.domain.MemberRepository;
 import nextstep.subway.auth.dto.TokenRequest;
 import nextstep.subway.auth.dto.TokenResponse;
 import nextstep.subway.auth.infrastructure.JwtTokenProvider;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
@@ -35,13 +37,26 @@ public class AuthServiceTest {
         authService = new AuthService(memberRepository, jwtTokenProvider);
     }
 
+    @DisplayName("MockitoExtension을 활용하여 사용자 로그인 토큰을 얻는다.")
     @Test
     void login() {
-        when(memberRepository.findByEmail(anyString())).thenReturn(Optional.of(new Member(EMAIL, PASSWORD, AGE)));
+        when(memberRepository.findByEmail(anyString())).thenReturn(Optional.of(Member.of(EMAIL, PASSWORD, AGE)));
         when(jwtTokenProvider.createToken(anyString())).thenReturn("TOKEN");
 
-        TokenResponse token = authService.login(new TokenRequest(EMAIL, PASSWORD));
+        TokenResponse token = authService.login(TokenRequest.of(EMAIL, PASSWORD));
 
         assertThat(token.getAccessToken()).isNotBlank();
+    }
+
+    @DisplayName("MockitoExtension을 활용하여 사용자 토큰을 통해 사용자 정보를 얻는다.")
+    @Test
+    void findMemberByToken() {
+        when(jwtTokenProvider.validateToken(anyString())).thenReturn(true);
+        when(jwtTokenProvider.getPayload(anyString())).thenReturn("TOKEN");
+        when(memberRepository.findByEmail(anyString())).thenReturn(Optional.of(Member.of(EMAIL, PASSWORD, AGE)));
+
+        LoginMember loginMember = authService.findMemberByToken("TOKEN");
+
+        assertThat(loginMember.getEmail()).isEqualTo(EMAIL);
     }
 }
