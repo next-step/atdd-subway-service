a232bd28a515a3daef3c00a9c96b8b4d7c53b6a2
minseoklim <mslim8803@naver.com>
2021-12-14 22:39:38 +0900

    test(auth): 로그인 인수테스트 추가

--- src/test/java/nextstep/subway/auth/acceptance/AuthAcceptanceTest.java
+++ src/test/java/nextstep/subway/auth/acceptance/AuthAcceptanceTest.java
@@ -1,10 +1,46 @@
 package nextstep.subway.auth.acceptance;
 
-import nextstep.subway.AcceptanceTest;
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import nextstep.subway.AcceptanceTest;
+import nextstep.subway.member.MemberAcceptanceTest;
+import nextstep.subway.utils.RequestUtil;
+import nextstep.subway.utils.StatusCodeCheckUtil;
+
 public class AuthAcceptanceTest extends AcceptanceTest {
+    private String email;
+    private String password;
+    private int age;
+
+    @Override
+    @BeforeEach
+    public void setUp() {
+        super.setUp();
+
+        // given
+        email = "mslim@naver.com";
+        password = "12345678";
+        age = 20;
+        회원_등록되어_있음(email, password, age);
+    }
+
+    @Test
+    void 로그인() {
+        // when
+        final ExtractableResponse<Response> response = 로그인_요청(email, password);
+
+        // then
+        로그인_됨(response);
+    }
 
     @DisplayName("Bearer Auth")
     @Test
@@ -21,4 +57,20 @@ public class AuthAcceptanceTest extends AcceptanceTest {
     void myInfoWithWrongBearerAuth() {
     }
 
+    private static void 회원_등록되어_있음(final String email, final String password, final int age) {
+        MemberAcceptanceTest.회원_생성을_요청(email, password, age);
+    }
+
+    private static ExtractableResponse<Response> 로그인_요청(final String email, final String password) {
+        Map<String, Object> params = new HashMap<>();
+        params.put("email", email);
+        params.put("password", password);
+
+        return RequestUtil.post("/login/token", params);
+    }
+
+    private static void 로그인_됨(final ExtractableResponse<Response> response) {
+        StatusCodeCheckUtil.ok(response);
+        assertThat(response.jsonPath().getObject("accessToken", String.class)).isNotNull();
+    }
 }
