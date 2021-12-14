1b1b2930abbd099131bc4f55620f71416cf645b2
wooobo <irooori89@gmail.com>
2021-12-14 09:47:26 +0900

    refactor : 변수위치 변경

--- src/main/java/nextstep/subway/line/domain/fare/policycondition/AgeDiscountFareCondition.java
+++ src/main/java/nextstep/subway/line/domain/fare/policycondition/AgeDiscountFareCondition.java
@@ -6,12 +6,12 @@ import nextstep.subway.member.domain.Age;
 
 public interface AgeDiscountFareCondition {
 
+    int DEDUCTIBLE_AMOUNT = 350;
+
     default Money calculate(Money money, BigDecimal discountRate) {
         Money discount = money.minus(DEDUCTIBLE_AMOUNT).multiply(discountRate);
         return money.minus(DEDUCTIBLE_AMOUNT + discount.toInt());
     }
-
-    int DEDUCTIBLE_AMOUNT = 350;
 
     Money discountFare(Money money, Age age);
 }
