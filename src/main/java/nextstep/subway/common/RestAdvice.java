18aa523a7a280947eaf0f857bf65aa2f9eeae360
lovethefeel <lovethefeel@gmail.com>
2021-12-15 03:11:55 +0900

    refactor: 상위 CustomExceion 추가

--- src/main/java/nextstep/subway/common/RestAdvice.java
+++ src/main/java/nextstep/subway/common/RestAdvice.java
@@ -1,6 +1,6 @@
 package nextstep.subway.common;
 
-import nextstep.subway.common.exception.*;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,57 +9,20 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class RestAdvice {
-
-    @ExceptionHandler
-    public ResponseEntity<ErrorResponse> handleRuntimeException(RuntimeException e) {
+    @ExceptionHandler(CustomException.class)
+    public ResponseEntity<ErrorResponse> handleCustomeException(CustomException e) {
         ErrorResponse errorResponse = new ErrorResponse(HttpStatus.BAD_REQUEST, e.getMessage());
         return new ResponseEntity<>(errorResponse, new HttpHeaders(), errorResponse.getStatus());
     }
 
-    @ExceptionHandler
-    public ResponseEntity<ErrorResponse> handleNoSectionDeleteException(NoSectionDeleteException e) {
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<ErrorResponse> handleDataIntegrityViolationException(DataIntegrityViolationException e) {
         ErrorResponse errorResponse = new ErrorResponse(HttpStatus.BAD_REQUEST, e.getMessage());
         return new ResponseEntity<>(errorResponse, new HttpHeaders(), errorResponse.getStatus());
     }
 
-    @ExceptionHandler
-    public ResponseEntity<ErrorResponse> handleNotFoundException(NotFoundException e) {
-        ErrorResponse errorResponse = new ErrorResponse(HttpStatus.BAD_REQUEST, e.getMessage());
-        return new ResponseEntity<>(errorResponse, new HttpHeaders(), errorResponse.getStatus());
-    }
-
-    @ExceptionHandler
-    public ResponseEntity<ErrorResponse> handleOneSectionDeleteException(OneSectionDeleteException e) {
-        ErrorResponse errorResponse = new ErrorResponse(HttpStatus.BAD_REQUEST, e.getMessage());
-        return new ResponseEntity<>(errorResponse, new HttpHeaders(), errorResponse.getStatus());
-    }
-
-    @ExceptionHandler
-    public ResponseEntity<ErrorResponse> handlePathDisconnectedException(PathDisconnectedException e) {
-        ErrorResponse errorResponse = new ErrorResponse(HttpStatus.BAD_REQUEST, e.getMessage());
-        return new ResponseEntity<>(errorResponse, new HttpHeaders(), errorResponse.getStatus());
-    }
-
-    @ExceptionHandler
-    public ResponseEntity<ErrorResponse> handlePathSameException(PathSameException e) {
-        ErrorResponse errorResponse = new ErrorResponse(HttpStatus.BAD_REQUEST, e.getMessage());
-        return new ResponseEntity<>(errorResponse, new HttpHeaders(), errorResponse.getStatus());
-    }
-
-    @ExceptionHandler
-    public ResponseEntity<ErrorResponse> handleRegisterAllIncludeException(RegisterAllIncludeException e) {
-        ErrorResponse errorResponse = new ErrorResponse(HttpStatus.BAD_REQUEST, e.getMessage());
-        return new ResponseEntity<>(errorResponse, new HttpHeaders(), errorResponse.getStatus());
-    }
-
-    @ExceptionHandler
-    public ResponseEntity<ErrorResponse> handleRegisterDistanceException(RegisterDistanceException e) {
-        ErrorResponse errorResponse = new ErrorResponse(HttpStatus.BAD_REQUEST, e.getMessage());
-        return new ResponseEntity<>(errorResponse, new HttpHeaders(), errorResponse.getStatus());
-    }
-
-    @ExceptionHandler
-    public ResponseEntity<ErrorResponse> handleRegisterNotAllIncludeException(RegisterNotAllIncludeException e) {
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<ErrorResponse> handleIllegalArgumentException(IllegalArgumentException e) {
         ErrorResponse errorResponse = new ErrorResponse(HttpStatus.BAD_REQUEST, e.getMessage());
         return new ResponseEntity<>(errorResponse, new HttpHeaders(), errorResponse.getStatus());
     }
