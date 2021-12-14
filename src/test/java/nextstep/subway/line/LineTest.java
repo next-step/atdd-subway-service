66faa6ea9faf7af13be37710620225802913db6b
박창혁 <changchang73@pionnet.co.kr>
2021-12-15 02:03:25 +0900

    test: 단위 테스트 작성

---
+++ src/test/java/nextstep/subway/line/LineTest.java
@@ -0,0 +1,40 @@
+package nextstep.subway.line;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+public class LineTest {
+    @DisplayName("노선 정보 수정")
+    @Test
+    void updateLine() {
+        // given
+
+        // when
+
+        // then
+
+    }
+    @DisplayName("노선에 역 추가")
+    @Test
+    void addLineStation() {
+        // given
+
+        // when
+
+        // then
+
+    }
+
+    @DisplayName("노선에 역 삭제")
+    @Test
+    void removeLineStation() {
+        // given
+
+        // when
+
+        // then
+
+    }
+}
---
+++ src/test/java/nextstep/subway/line/SectionsTest.java
@@ -0,0 +1,18 @@
+package nextstep.subway.line;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+public class SectionsTest {
+    @DisplayName("지하철 역 목록 ")
+    @Test
+    void getStations() {
+        //given
+
+        //when
+
+        //then
+    }
+}
