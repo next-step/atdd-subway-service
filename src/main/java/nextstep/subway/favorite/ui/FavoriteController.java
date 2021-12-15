2b62e4548123a01cc116f1b888280cd1334192c0
devsigner9920 <devsigner9920@icloud.com>
2021-12-15 11:02:30 +0900

    refactor: controller 레이어 필드 final 및 ResponseEntity 제네릭 명시

--- src/main/java/nextstep/subway/favorite/ui/FavoriteController.java
+++ src/main/java/nextstep/subway/favorite/ui/FavoriteController.java
@@ -15,14 +15,14 @@ import java.util.List;
 @RestController
 @RequestMapping("/favorites")
 public class FavoriteController {
-  private FavoriteService favoriteService;
+  private final FavoriteService favoriteService;
 
   public FavoriteController(FavoriteService favoriteService) {
     this.favoriteService = favoriteService;
   }
 
   @PostMapping
-  public ResponseEntity createFavorite(@AuthenticationPrincipal LoginMember loginMember, @RequestBody FavoriteRequest request) {
+  public ResponseEntity<Void> createFavorite(@AuthenticationPrincipal LoginMember loginMember, @RequestBody FavoriteRequest request) {
     FavoriteResponse favorite = favoriteService.createFavorite(loginMember.getId(), request);
     return ResponseEntity.created(URI.create("/favorites/" + favorite.getId())).build();
   }
@@ -34,7 +34,7 @@ public class FavoriteController {
   }
 
   @DeleteMapping("/{favoriteId}")
-  public ResponseEntity deleteFavorite(@AuthenticationPrincipal LoginMember loginMember, @PathVariable Long favoriteId) {
+  public ResponseEntity<Void> deleteFavorite(@AuthenticationPrincipal LoginMember loginMember, @PathVariable Long favoriteId) {
     favoriteService.deleteFavorite(favoriteId, loginMember.getId());
     return ResponseEntity.noContent().build();
   }
