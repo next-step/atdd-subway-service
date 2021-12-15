8ee1f4c319c07c90fa7ee0ac745563f19ae7c937
신민준 <jxin95@gmail.com>
2021-12-15 17:09:00 +0900

    feat: 구간의 거리를 Distance 클래스로 분리

---
+++ src/main/java/nextstep/subway/line/domain/Distance.java
@@ -0,0 +1,40 @@
+package nextstep.subway.line.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+@Embeddable
+public class Distance {
+
+    private static final int MIN_DISTANCE = 0;
+
+    @Column
+    private int distance;
+
+    public Distance() {
+    }
+
+    public Distance(final int distance) {
+        validate(distance);
+        this.distance = distance;
+    }
+
+    private void validate(final int distance) {
+        if (MIN_DISTANCE >= distance) {
+            throw new IllegalArgumentException("입력한 구간의 길이가 옳바르지 않습니다.");
+        }
+    }
+
+    public Distance add(final Distance distance) {
+        return new Distance(this.distance + distance.getDistance());
+    }
+
+    public Distance subtract(final Distance distance) {
+        return new Distance(this.distance - distance.getDistance());
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+
+}
