32f76a46fdf37a2c93112d14d8fea5e60badda6d
신민준 <jxin95@gmail.com>
2021-12-15 17:13:52 +0900

    feat: 공통 RESTful API 호출 테스트 추가

--- src/test/java/nextstep/subway/AcceptanceTest.java
+++ src/test/java/nextstep/subway/AcceptanceTest.java
@@ -1,14 +1,17 @@
 package nextstep.subway;
 
 import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
 import nextstep.subway.utils.DatabaseCleanup;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.MediaType;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class AcceptanceTest {
+public abstract class AcceptanceTest {
     @LocalServerPort
     int port;
 
@@ -20,4 +23,50 @@ public class AcceptanceTest {
         RestAssured.port = port;
         databaseCleanup.execute();
     }
+
+    public ExtractableResponse<Response> get(String uri) {
+        return RestAssured.given()
+                .log().all()
+                .accept(MediaType.APPLICATION_JSON_VALUE)
+                .when()
+                .get(uri)
+                .then()
+                .log().all()
+                .extract();
+    }
+
+    public ExtractableResponse<Response> post(String uri, Object params) {
+        return RestAssured.given()
+                .log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(params)
+                .when()
+                .post(uri)
+                .then()
+                .log().all()
+                .extract();
+    }
+
+    public ExtractableResponse<Response> put(String uri, Object params) {
+        return RestAssured.given()
+                .log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(params)
+                .when()
+                .put(uri)
+                .then()
+                .log().all()
+                .extract();
+    }
+
+    public ExtractableResponse<Response> delete(String uri) {
+        return RestAssured.given()
+                .log().all()
+                .when()
+                .delete(uri)
+                .then()
+                .log().all()
+                .extract();
+    }
+
 }
