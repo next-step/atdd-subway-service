ce053933506e14f89dceb808c20fc2c95898461d
lovethefeel <lovethefeel@gmail.com>
2021-12-17 03:52:06 +0900

    refactor: 최단거리 조회용 인터페이스 호출 부분 수정

--- src/main/java/nextstep/subway/path/domain/PathFinder.java
+++ src/main/java/nextstep/subway/path/domain/PathFinder.java
@@ -1,45 +1,32 @@
 package nextstep.subway.path.domain;
 
 import nextstep.subway.common.exception.NotFoundException;
-import nextstep.subway.common.exception.PathDisconnectedException;
 import nextstep.subway.common.exception.PathSameException;
 import nextstep.subway.line.domain.Line;
 import nextstep.subway.path.dto.PathResponse;
 import nextstep.subway.station.domain.Station;
-import org.springframework.util.CollectionUtils;
 
 import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class PathFinder {
-    private final Path path = new DijkstraShortestPath();
-    private final List<Station> stations;
+    private final PathStrategy pathStrategy = new DijkstraShortest();
+    private final List<Line> lines;
 
-    private PathFinder(final List<Line> lines){
-        creteGraph(lines);
-        this.stations = lines.stream()
-                .map(Line::getStations)
-                .flatMap(Collection::stream)
-                .distinct()
-                .collect(Collectors.toList());
+    private PathFinder(final List<Line> lines) {
+        this.lines = lines;
     }
 
     public static PathFinder from(final List<Line> lines) {
         return new PathFinder(lines);
     }
 
-    private void creteGraph(final List<Line> lines){
-        path.createEdge(lines);
-        path.createVertex(lines);
-    }
-
     public PathResponse findShortestPath(final Station source, final Station target) {
         validateSameStation(source, target);
         validateFoundBothStations(source, target);
-        final List<Station> stations = path.getVertexes(source, target);
-        validateConnectedStations(stations);
-        return PathResponse.of(stations, path.getWeight(source, target));
+        final Path path = pathStrategy.getShortestPath(lines, source, target);
+        return PathResponse.from(path);
     }
 
     private void validateSameStation(final Station sourceStation, final Station targetStation) {
@@ -48,13 +35,12 @@ public class PathFinder {
         }
     }
 
-    private void validateConnectedStations(final List<Station> stations) {
-        if (CollectionUtils.isEmpty(stations)) {
-            throw new PathDisconnectedException();
-        }
-    }
-
     private void validateFoundBothStations(final Station sourceStation, final Station targetStation) {
+        final List<Station> stations = lines.stream()
+                .map(Line::getStations)
+                .flatMap(Collection::stream)
+                .distinct()
+                .collect(Collectors.toList());
         if (!stations.contains(sourceStation) || !stations.contains(targetStation)) {
             throw new NotFoundException();
         }
