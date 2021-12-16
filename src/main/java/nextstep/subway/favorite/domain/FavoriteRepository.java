dea66fcf63092b72df7c0edd5722b9bd53725432
lovethefeel <lovethefeel@gmail.com>
2021-12-17 03:45:43 +0900

    refactor: 사용자의 속한 즐겨찾기 조회 시 Optional 추가

--- src/main/java/nextstep/subway/favorite/domain/FavoriteRepository.java
+++ src/main/java/nextstep/subway/favorite/domain/FavoriteRepository.java
@@ -3,7 +3,9 @@ package nextstep.subway.favorite.domain;
 import nextstep.subway.member.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface FavoriteRepository extends JpaRepository<Favorite, Long> {
-    Favorite findByIdAndMember(Long id, Member member);
+    Optional<Favorite> findByIdAndMember(Long id, Member member);
     void deleteByIdAndMember(Long id, Member member);
 }
