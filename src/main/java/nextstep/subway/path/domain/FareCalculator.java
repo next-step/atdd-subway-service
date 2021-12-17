f9d6d266c2d4c3a9448bed37bc5370501ca8c12b
jennie267 <jennieee267@gmail.com>
2021-12-17 23:09:09 +0900

    test : 거리별 요금 계산 테스트추가 fix : 거리별 요금 잘못계산하는로직 수정

--- src/main/java/nextstep/subway/path/domain/FareCalculator.java
+++ src/main/java/nextstep/subway/path/domain/FareCalculator.java
@@ -2,8 +2,8 @@ package nextstep.subway.path.domain;
 
 public class FareCalculator {
     private static final int DEFAULT_FARE = 1_250;
-    private static final int OVER_DISTANCE_MIN = 10;
-    private static final int OVER_DISTANCE_MAX = 50;
+    private static final int FARE_RANGE_MIN = 10;
+    private static final int FARE_RANGE_MAX = 50;
     private static final int OVER_PER_DISTANCE_KM = 5;
     private static final int MAX_OVER_PER_DISTANCE_KM = 8;
     private static final int EXTRA_FARE = 100;
@@ -12,17 +12,16 @@ public class FareCalculator {
     }
     
     public static int calculator(int distance) {
-        if (distance <= OVER_DISTANCE_MIN) {
-            return DEFAULT_FARE;
+        if (distance > FARE_RANGE_MIN && distance <= FARE_RANGE_MAX) {
+            return DEFAULT_FARE + calculateOverFare(distance - FARE_RANGE_MIN, OVER_PER_DISTANCE_KM);
         }
-        if (distance > OVER_DISTANCE_MIN && distance <= OVER_DISTANCE_MAX) {
-            return calculateOverFare(distance, OVER_PER_DISTANCE_KM);
-        }
-        if (distance > OVER_DISTANCE_MAX) {
-            return calculateOverFare(distance, MAX_OVER_PER_DISTANCE_KM);
+        if (distance > FARE_RANGE_MAX) {
+            int firstRangeFare = calculateOverFare(FARE_RANGE_MAX - FARE_RANGE_MIN, OVER_PER_DISTANCE_KM);
+            int secondRangeFare = calculateOverFare(distance - FARE_RANGE_MAX, MAX_OVER_PER_DISTANCE_KM);
+            return DEFAULT_FARE + firstRangeFare + secondRangeFare;
         }
         
-        return 0;
+        return DEFAULT_FARE;
     }
     
     private static int calculateOverFare(int distance, int overDistance) {
