da001f13f212562a6d0dcf809b2cf6d0c841f6ea
신민준 <jxin95@gmail.com>
2021-12-17 17:01:21 +0900

    refactor: List로 전달 받는 부분을 일급 컬렉션으로 교체

--- src/test/java/study/unit/MockitoTest.java
+++ src/test/java/study/unit/MockitoTest.java
@@ -4,13 +4,10 @@ import com.google.common.collect.Lists;
 import nextstep.subway.line.application.LineService;
 import nextstep.subway.line.domain.Line;
 import nextstep.subway.line.domain.LineRepository;
-import nextstep.subway.line.dto.LineResponse;
+import nextstep.subway.line.dto.LineResponses;
 import nextstep.subway.station.application.StationService;
-import nextstep.subway.station.domain.StationRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
@@ -28,9 +25,9 @@ public class MockitoTest {
         LineService lineService = new LineService(lineRepository, stationService);
 
         // when
-        List<LineResponse> responses = lineService.findLines();
+        LineResponses responses = lineService.findLines();
 
         // then
-        assertThat(responses).hasSize(1);
+        assertThat(responses.size()).isEqualTo(1);
     }
 }
