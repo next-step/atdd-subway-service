961bbd77c807fe4f6fdaf30e14da7a98fb5dc1eb
shryu <>
2021-12-17 19:59:07 +0900

    feat : 인터페이스 return 객체 수정

--- src/main/java/nextstep/subway/path/domain/ShortestPathCalculator.java
+++ src/main/java/nextstep/subway/path/domain/ShortestPathCalculator.java
@@ -1,13 +1,12 @@
 package nextstep.subway.path.domain;
 
 import nextstep.subway.line.domain.Section;
+import nextstep.subway.path.dto.PathResponse;
 import nextstep.subway.station.domain.Station;
-import org.jgrapht.GraphPath;
 
 import java.util.List;
-import java.util.Optional;
 
 @FunctionalInterface
 public interface ShortestPathCalculator {
-    Optional<GraphPath> calculatePath(List<Section> sections, Station sourceStation, Station targetStation);
+    PathResponse calculatePath(List<Section> sections, Station sourceStation, Station targetStation);
 }
