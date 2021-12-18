9ac94fbe386059f475e198c8d77b3bb97b531a03
sungwoo <ssw890418@gmail.com>
2021-12-18 12:04:42 +0900

    refactor:예외처리 헨들러 추가

--- src/main/java/nextstep/subway/path/ui/PathController.java
+++ src/main/java/nextstep/subway/path/ui/PathController.java
@@ -3,6 +3,7 @@ package nextstep.subway.path.ui;
 import nextstep.subway.path.application.PathService;
 import nextstep.subway.path.dto.PathResponse;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,4 +22,8 @@ public class PathController {
         return ResponseEntity.ok(pathService.findPath(source, target));
     }
 
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity handleRuntimeException(RuntimeException e) {
+        return ResponseEntity.badRequest().build();
+    }
 }
