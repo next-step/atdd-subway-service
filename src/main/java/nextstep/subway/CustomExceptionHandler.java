ef31dc07bb25305f9f7da22d5a34cbf459dddc2b
shryu <>
2021-12-18 16:52:31 +0900

    feat : exceptionHandler에 AuthorizationException 추가

--- src/main/java/nextstep/subway/CustomExceptionHandler.java
+++ src/main/java/nextstep/subway/CustomExceptionHandler.java
@@ -1,6 +1,8 @@
 package nextstep.subway;
 
+import nextstep.subway.auth.application.AuthorizationException;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -20,5 +22,10 @@ public class CustomExceptionHandler {
     @ExceptionHandler(RuntimeException.class)
     public ResponseEntity handleRuntimeException(RuntimeException e) {
         return ResponseEntity.badRequest().body(e.getMessage());
+    }
+
+    @ExceptionHandler(AuthorizationException.class)
+    public ResponseEntity handleAuthorizationException(AuthorizationException e) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(e.getMessage());
     }
 }
