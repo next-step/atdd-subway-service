0bcc71d5464b3571592461388b1eb828a00aa78b
shryu <>
2021-12-18 21:34:18 +0900

    refactor : Optional 안티패턴 리팩터링

--- src/main/java/nextstep/subway/line/domain/Sections.java
+++ src/main/java/nextstep/subway/line/domain/Sections.java
@@ -4,6 +4,7 @@ import nextstep.subway.station.domain.Station;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Embeddable;
+import javax.persistence.EntityNotFoundException;
 import javax.persistence.OneToMany;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -118,6 +119,7 @@ public class Sections {
         if (sections.size() <= MINIMUM_SECTIONS_SIZE) {
             throw new RuntimeException();
         }
+        List<Station> stations = getStations();
 
         Optional<Section> upLineStationSection = sections.stream()
                 .filter(section -> section.getUpStation().equals(station))
@@ -126,11 +128,8 @@ public class Sections {
                 .filter(section -> section.getDownStation().equals(station))
                 .findFirst();
 
-        if (upLineStationSection.isPresent() && downLineStationSection.isPresent()) {
-            Station newUpStation = downLineStationSection.get().getUpStation();
-            Station newDownStation = upLineStationSection.get().getDownStation();
-            int newDistance = upLineStationSection.get().getDistance() + downLineStationSection.get().getDistance();
-            sections.add(new Section(getLine(), newUpStation, newDownStation, newDistance));
+        if (!isFirstStation(station, stations) && !isLastStation(station, stations)) {
+            removeBetweenSectionStation(station);
         }
 
         upLineStationSection.ifPresent(section -> sections.remove(section));
@@ -139,5 +138,29 @@ public class Sections {
 
     private Line getLine() {
         return sections.get(FIRST_INDEX).getLine();
+    }
+
+    private boolean isFirstStation(Station station, List<Station> stations) {
+        return stations.get(FIRST_INDEX).equals(station);
+    }
+
+    private boolean isLastStation(Station station, List<Station> stations) {
+        return stations.get(stations.size()-1).equals(station);
+    }
+
+    private void removeBetweenSectionStation(Station station) {
+        Section upLineStationSection = sections.stream()
+                .filter(section -> section.getUpStation().equals(station))
+                .findFirst()
+                .orElseThrow(() -> new EntityNotFoundException("일치하는 구간이 없습니다."));
+        Section downLineStationSection = sections.stream()
+                .filter(section -> section.getDownStation().equals(station))
+                .findFirst()
+                .orElseThrow(() -> new EntityNotFoundException("일치하는 구간이 없습니다."));
+
+        Station newUpStation = downLineStationSection.getUpStation();
+        Station newDownStation = upLineStationSection.getDownStation();
+        int newDistance = upLineStationSection.getDistance() + downLineStationSection.getDistance();
+        sections.add(new Section(getLine(), newUpStation, newDownStation, newDistance));
     }
 }
