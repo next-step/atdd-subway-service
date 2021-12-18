3dd96d539f762f38ca3ef87664736c160d2a1814
shryu <>
2021-12-18 20:27:17 +0900

    feat : favorite entity 생성

---
+++ src/main/java/nextstep/subway/favorite/domain/Favorite.java
@@ -0,0 +1,54 @@
+package nextstep.subway.favorite.domain;
+
+import nextstep.subway.member.domain.Member;
+import nextstep.subway.station.domain.Station;
+
+import javax.persistence.*;
+
+@Entity
+public class Favorite {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "source_station_id")
+    private Station source;
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "target_station_id")
+    private Station target;
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    protected Favorite() {
+    }
+
+    public Favorite(Station source, Station target, Member member) {
+        this.source = source;
+        this.target = target;
+        setMember(member);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Station getSource() {
+        return source;
+    }
+
+    public Station getTarget() {
+        return target;
+    }
+
+    public void setMember(Member member) {
+        if (this.member != null && this.member.equals(member)) {
+            return;
+        }
+        if (this.member != null && !this.member.equals(member)) {
+            this.member.removeFavorite(this);
+        }
+        this.member = member;
+        member.addFavorite(this);
+    }
+}
