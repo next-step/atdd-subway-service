a60da173a74a8a7e6e65cf91e6a9c76552324ebc
sungwoo <ssw890418@gmail.com>
2021-12-18 11:58:20 +0900

    feat:최단경로 조회 기능 단위테스트 추가

--- src/test/java/nextstep/subway/line/domain/LineTest.java
+++ src/test/java/nextstep/subway/line/domain/LineTest.java
@@ -2,8 +2,9 @@ package nextstep.subway.line.domain;
 
 import static nextstep.subway.station.domain.StationTest.*;
 
-class LineTest {
+public class LineTest {
 
-    static Line 신분당선 = new Line("신분당선","red", 강남역, 광교역, 10);
-
+    public static Line 신분당선 = new Line("신분당선","red", 강남역, 양재역, 10);
+    public static Line 이호선 = new Line("이호선","green", 교대역, 강남역, 10);
+    public static Line 삼호선 = new Line("삼호선","orange", 교대역, 양재역, 5);
 }
