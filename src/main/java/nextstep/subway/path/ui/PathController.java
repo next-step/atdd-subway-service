79c80bce4a9e67a4cb0c4414ebdae1fcb884a1f4
koola97620 <koola97620@nate.com>
2021-12-19 03:07:55 +0900

    refactor: 피드백 반영

--- src/main/java/nextstep/subway/path/ui/PathController.java
+++ src/main/java/nextstep/subway/path/ui/PathController.java
@@ -19,13 +19,6 @@ public class PathController {
 
     @GetMapping
     public ResponseEntity findShortestPath(@RequestParam Long source, @RequestParam Long target) {
-//        PathResponse response = PathResponse.of(
-//                Arrays.asList(
-//                        PathResponse.StationResponse.of(3L,"A", LocalDateTime.now()),
-//                        PathResponse.StationResponse.of(4L,"B", LocalDateTime.now()),
-//                        PathResponse.StationResponse.of(2L,"C", LocalDateTime.now())
-//                ), 5
-//        );
         return ResponseEntity.ok(pathService.findShortestPath(source, target));
     }
 }
