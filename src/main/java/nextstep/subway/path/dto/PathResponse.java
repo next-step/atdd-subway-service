3d1863fbb4cf2d6f04721fc9a9714bd91532368d
shryu <>
2021-12-19 19:46:29 +0900

    feat : 경로 response 요금 필드 추가

--- src/main/java/nextstep/subway/path/dto/PathResponse.java
+++ src/main/java/nextstep/subway/path/dto/PathResponse.java
@@ -6,8 +6,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class PathResponse {
+    private static final int BASE_FARE = 1_250;
     private List<StationResponse> stations = new ArrayList<>();
     private int distance;
+    private int fare = BASE_FARE;
 
     protected PathResponse() {
     }
@@ -17,11 +19,21 @@ public class PathResponse {
         this.distance = distance;
     }
 
+    public PathResponse(List<StationResponse> stations, int distance, int fare) {
+        this.stations = stations;
+        this.distance = distance;
+        this.fare = fare;
+    }
+
     public List<StationResponse> getStations() {
         return stations;
     }
 
     public int getDistance() {
         return distance;
+    }
+
+    public int getFare() {
+        return fare;
     }
 }
