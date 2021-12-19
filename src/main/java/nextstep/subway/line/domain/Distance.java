89176770c9b291c7cb99fa40a14cbe93a715d3fc
shryu <>
2021-12-19 18:41:23 +0900

    feat : plus 메서드 추가, 유효성 검증 메서드 수정

--- src/main/java/nextstep/subway/line/domain/Distance.java
+++ src/main/java/nextstep/subway/line/domain/Distance.java
@@ -4,6 +4,7 @@ import javax.persistence.Embeddable;
 
 @Embeddable
 public class Distance {
+    private static final int MINIMUM_DISTANCE = 1;
     private int distance;
 
     public Distance(int distance) {
@@ -17,14 +18,19 @@ public class Distance {
         return distance;
     }
 
-    public Distance minus(int newDistance) {
-        validateGreaterThanNewDistance(newDistance);
-        return new Distance(distance - newDistance);
+    public Distance minus(int distanceToMinus) {
+        validateNewDistance(distance - distanceToMinus);
+        return new Distance(distance - distanceToMinus);
     }
 
-    private void validateGreaterThanNewDistance(int newDistance) {
-        if (this.distance <= newDistance) {
-            throw new RuntimeException("역과 역 사이의 거리보다 좁은 거리를 입력해주세요");
+    public Distance plus(int distanceToPlus) {
+        validateNewDistance(distance + distanceToPlus);
+        return new Distance(distance + distanceToPlus);
+    }
+
+    private void validateNewDistance(int newDistance) {
+        if (newDistance < MINIMUM_DISTANCE) {
+            throw new RuntimeException("거리는 최소 1이상이어야 합니다.");
         }
     }
 }
