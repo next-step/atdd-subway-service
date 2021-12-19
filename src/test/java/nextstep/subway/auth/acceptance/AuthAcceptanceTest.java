95a90ec3087eacc2cca18db4a0f67690f81553ef
박창혁 <changchang73@pionnet.co.kr>
2021-12-19 19:25:54 +0900

    test : 인증 인수테스트 수정

--- src/test/java/nextstep/subway/auth/acceptance/AuthAcceptanceTest.java
+++ src/test/java/nextstep/subway/auth/acceptance/AuthAcceptanceTest.java
@@ -1,95 +1,87 @@
 package nextstep.subway.auth.acceptance;
 
+import static nextstep.subway.auth.acceptance.AuthAcceptanceTestHelper.*;
 import static nextstep.subway.member.MemberAcceptanceTestHelper.*;
-import static org.assertj.core.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 
-import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import nextstep.subway.AcceptanceTest;
-import nextstep.subway.auth.application.AuthorizationException;
-import nextstep.subway.auth.dto.TokenRequest;
 
-@DisplayName("로그인 기능")
+@DisplayName("인증 기능")
 public class AuthAcceptanceTest extends AcceptanceTest {
     public static String email = "changchang743@gmail.com";
     public static String password = "1234";
     public static int age = 29;
 
-    @DisplayName("로그인을 통한 토큰 발급 (Bearer Auth)")
-    @Test
-    void myInfoWithBearerAuth() {
-        // given
-        ExtractableResponse<Response> 회원_등록_응답 = 회원_등록되어_있음(email, password, age);
+    @Override
+    @BeforeEach
+    public void setUp() {
+        super.setUp();
+        회원_등록되어_있음(email, password, age);
+    }
 
+    @DisplayName("로그인 성공 - 토큰 발급")
+    @Test
+    void loginAndGetToken() {
         // when
         ExtractableResponse<Response> 로그인_응답 = 로그인_요청(email, password);
 
-        // then
+        //then
         로그인_됨(로그인_응답);
     }
 
+    @DisplayName("내 정보 조회 (Bearer Auth)")
+    @Test
+    void myInfoWithBearerAuth() {
+        // given
+        ExtractableResponse<Response> 로그인_응답 = 로그인_요청(email, password);
+        String 토큰 = 토큰_정보(로그인_응답);
+
+        // when
+        ExtractableResponse<Response> 내_정보_조회_응답 = 내_정보_조회_요청(토큰);
+
+        // then
+        내_정보_조회_됨(내_정보_조회_응답, email, password);
+    }
 
     @DisplayName("등록되어 있지 않은 이메일 로그인 시도 (Bearer Auth 로그인 실패)")
     @Test
     void myInfoWithBadBearerAuthToWrongEmail() {
-        assertThatExceptionOfType(AuthorizationException.class)
-            .isThrownBy(() -> {
-                // given
-                ExtractableResponse<Response> 회원_등록_응답 = 회원_등록되어_있음(email, password, age);
-                String wrongEmail = "wrongEmail@gmail.com";
+        // given
+        String wrongEmail = "wrongEmail@gmail.com";
 
-                // when
-                ExtractableResponse<Response> 로그인_응답 = 로그인_요청(wrongEmail, password);
-            });
+        // when
+        ExtractableResponse<Response> 로그인_응답 = 로그인_요청(wrongEmail, password);
+
+        // then
+        로그인_실패됨(로그인_응답);
     }
 
     @DisplayName("비밀번호가 일치 않은 로그인 시도 (Bearer Auth 로그인 실패)")
     @Test
     void myInfoWithBadBearerAuthToWrongPassword() {
-        assertThatExceptionOfType(AuthorizationException.class)
-            .isThrownBy(() -> {
-                // given
-                ExtractableResponse<Response> 회원_등록_응답 = 회원_등록되어_있음(email, password, age);
-                String wrongPassword = "wrongPassword";
+        // given
+        String wrongPassword = "wrongPassword";
 
-                // when
-                ExtractableResponse<Response> 로그인_응답 = 로그인_요청(email, wrongPassword);
-            });
+        // when
+        ExtractableResponse<Response> 로그인_응답 = 로그인_요청(email, wrongPassword);
+
+        // then
+        로그인_실패됨(로그인_응답);
     }
 
-    @DisplayName("유효하지 않은 토큰으로 로그인 시도 (Bearer Auth 유효하지 않은 토큰)")
+    @DisplayName("유효하지 않은 토큰으로 내 정보 조회 (Bearer Auth 유효하지 않은 토큰)")
     @Test
     void myInfoWithWrongBearerAuth() {
-        assertThatExceptionOfType(AuthorizationException.class)
-            .isThrownBy(() -> {
-                // given
-                ExtractableResponse<Response> 회원_등록_응답 = 회원_등록되어_있음(email, password, age);
+        // when
+        ExtractableResponse<Response> 내_정보_조회_응답 = 내_정보_조회_요청("wrongToken");
 
-                // when
-            });
-    }
-
-    private void 로그인_됨(ExtractableResponse<Response> response) {
-        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
-
-        String token = response.jsonPath().getObject("accessToken", String.class);
-        assertThat(token).isNotNull();
-    }
-
-    private ExtractableResponse<Response> 로그인_요청(String email, String password) {
-        TokenRequest token = new TokenRequest(email, password);
-
-        return RestAssured.given().log().all()
-            .contentType(MediaType.APPLICATION_JSON_VALUE)
-            .body(token)
-            .when().post("login/token")
-            .then().log().all().extract();
+        // then
+        인증_실패됨(내_정보_조회_응답);
     }
 
 }
---
+++ src/test/java/nextstep/subway/auth/acceptance/AuthAcceptanceTestHelper.java
@@ -0,0 +1,61 @@
+package nextstep.subway.auth.acceptance;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import nextstep.subway.auth.dto.TokenRequest;
+import nextstep.subway.auth.dto.TokenResponse;
+import nextstep.subway.member.dto.MemberResponse;
+
+public class AuthAcceptanceTestHelper {
+    public static void 인증_실패됨(ExtractableResponse<Response> response) {
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.UNAUTHORIZED.value());
+    }
+
+    public static ExtractableResponse<Response> 로그인_요청(String email, String password) {
+        TokenRequest token = new TokenRequest(email, password);
+
+        return RestAssured.given().log().all()
+            .contentType(MediaType.APPLICATION_JSON_VALUE)
+            .body(token)
+            .when().post("login/token")
+            .then().log().all().extract();
+    }
+
+    public static void 로그인_됨(ExtractableResponse<Response> response) {
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+
+        String token = response.as(TokenResponse.class).getAccessToken();
+        assertThat(token).isNotNull();
+    }
+
+    public static void 로그인_실패됨(ExtractableResponse<Response> response) {
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.UNAUTHORIZED.value());
+    }
+
+    public static void 내_정보_조회_됨(ExtractableResponse<Response> response, String email, String age) {
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+
+        MemberResponse memberResponse = response.as(MemberResponse.class);
+        assertThat(memberResponse.getEmail()).isEqualTo(email);
+        assertThat(memberResponse.getAge()).isEqualTo(age);
+    }
+
+    public static ExtractableResponse<Response> 내_정보_조회_요청(String token) {
+        return RestAssured
+            .given().log().all()
+            .auth().oauth2(token)
+            .accept(MediaType.APPLICATION_JSON_VALUE)
+            .when().get("/members/me")
+            .then().log().all().extract();
+    }
+
+    public static String 토큰_정보(ExtractableResponse<Response> response) {
+        return response.as(TokenResponse.class).getAccessToken();
+    }
+}
