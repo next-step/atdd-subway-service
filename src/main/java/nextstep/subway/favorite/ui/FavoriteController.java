3d62dd739f0a7e399df13c3e0240eef853e00722
lovethefeel <lovethefeel@gmail.com>
2021-12-20 22:54:33 +0900

    refactor: 로그인 멤버 아이디로 사용자 정보 처리 하도록 수정

--- src/main/java/nextstep/subway/favorite/ui/FavoriteController.java
+++ src/main/java/nextstep/subway/favorite/ui/FavoriteController.java
@@ -5,8 +5,6 @@ import nextstep.subway.auth.domain.LoginMember;
 import nextstep.subway.favorite.application.FavoriteService;
 import nextstep.subway.favorite.dto.FavoriteRequest;
 import nextstep.subway.favorite.dto.FavoriteResponse;
-import nextstep.subway.member.application.MemberService;
-import nextstep.subway.member.domain.Member;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,30 +14,25 @@ import java.net.URI;
 @RequestMapping("favorites")
 public class FavoriteController {
     private final FavoriteService favoriteService;
-    private final MemberService memberService;
 
-    public FavoriteController(final FavoriteService favoriteService, MemberService memberService) {
+    public FavoriteController(final FavoriteService favoriteService) {
         this.favoriteService = favoriteService;
-        this.memberService = memberService;
     }
 
     @PostMapping("")
     public ResponseEntity<FavoriteResponse> createLine(@AuthenticationPrincipal LoginMember loginMember, @RequestBody FavoriteRequest favoriteRequest) {
-        final Member member = memberService.findMemberById(loginMember.getId());
-        final FavoriteResponse favoriteResponse = favoriteService.saveFavorite(member, favoriteRequest);
+        final FavoriteResponse favoriteResponse = favoriteService.saveFavorite(loginMember, favoriteRequest);
         return ResponseEntity.created(URI.create("/favorites/" + favoriteResponse.getId())).body(favoriteResponse);
     }
 
     @GetMapping("{id}")
     public ResponseEntity<FavoriteResponse> findLineById(@AuthenticationPrincipal LoginMember loginMember, @PathVariable Long id) {
-        final Member member = memberService.findMemberById(loginMember.getId());
-        return ResponseEntity.ok(favoriteService.findLineResponseById(member, id));
+        return ResponseEntity.ok(favoriteService.findLineResponseById(loginMember, id));
     }
 
     @DeleteMapping("{id}")
     public ResponseEntity<Void> deleteLine(@AuthenticationPrincipal LoginMember loginMember, @PathVariable Long id) {
-        final Member member = memberService.findMemberById(loginMember.getId());
-        favoriteService.deleteLineById(member, id);
+        favoriteService.deleteLineById(loginMember, id);
         return ResponseEntity.noContent().build();
     }
 }
