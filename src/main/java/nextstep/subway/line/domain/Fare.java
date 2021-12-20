97113b1c823de25532cde394216ba40bf286f8a7
lovethefeel <lovethefeel@gmail.com>
2021-12-20 23:12:10 +0900

    feat: 요금 객체 생성

---
+++ src/main/java/nextstep/subway/line/domain/Fare.java
@@ -0,0 +1,61 @@
+package nextstep.subway.line.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import java.math.BigDecimal;
+import java.util.Objects;
+
+@Embeddable
+public class Fare {
+    @Column
+    private BigDecimal fare;
+
+    protected Fare() {
+    }
+
+    private Fare(final BigDecimal fare) {
+        this.fare = fare;
+    }
+
+    public static Fare from(final BigDecimal fare) {
+        return new Fare(fare);
+    }
+
+    public static Fare from(final Long fare) {
+        return new Fare(new BigDecimal(fare));
+    }
+
+    public static Fare from(final int fare) {
+        return new Fare(new BigDecimal(fare));
+    }
+
+    public BigDecimal value() {
+        return fare;
+    }
+
+    public Fare minus(final Fare deduction) {
+        return new Fare(this.fare.subtract(deduction.fare));
+    }
+
+    public Fare multiply(final Fare childDiscountPercent) {
+        final BigDecimal amount = this.fare.multiply(childDiscountPercent.fare);
+        return new Fare(amount.stripTrailingZeros());
+    }
+
+    public Fare plus(final Fare fare) {
+        return new Fare(this.fare.add(fare.fare));
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Fare fare1 = (Fare) o;
+        return Objects.equals(fare, fare1.fare);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(fare);
+    }
+}
