d7bcbe91d6121580b047d6d69b5ed0e56949f40d
lovethefeel <lovethefeel@gmail.com>
2021-12-20 23:07:25 +0900

    test: 추가 요금 필드 추가

--- src/test/java/study/unit/UnitTest.java
+++ src/test/java/study/unit/UnitTest.java
@@ -1,9 +1,6 @@
 package study.unit;
 
-import nextstep.subway.line.domain.Distance;
-import nextstep.subway.line.domain.Line;
-import nextstep.subway.line.domain.Section;
-import nextstep.subway.line.domain.Sections;
+import nextstep.subway.line.domain.*;
 import nextstep.subway.station.domain.Station;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -20,8 +17,8 @@ public class UnitTest {
         Station upStation = Station.from("강남역");
         Station downStation = Station.from("광교역");
         Section section = Section.of(upStation, downStation, Distance.of(10));
-        Line line = Line.of("신분당선", "RED", Sections.from(section));
-        Line newLine = Line.of(newName, "GREEN");
+        Line line = Line.of("신분당선", "RED", Sections.from(section), Fare.from(900));
+        Line newLine = Line.of(newName, "GREEN", Fare.from(900));
 
         // when
         line.update(newLine);
