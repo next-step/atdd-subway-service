2a5c183f2f7cc8c5eef954e785bf66e1a97cd5a3
JiHun Hong <hongji3354@gmail.com>
2021-12-20 09:50:45 +0900

    refactor : 청소년, 어린이 판단하는 기능을 age field가 있는 곳에서 제공하도록 변경

--- src/main/java/nextstep/subway/domain/auth/domain/User.java
+++ src/main/java/nextstep/subway/domain/auth/domain/User.java
@@ -1,6 +1,12 @@
 package nextstep.subway.domain.auth.domain;
 
 public class User {
+
+    private static final int TEENAGER_MINIMUM_AGE = 13;
+    private static final int TEENAGER_MAXIMUM_AGE = 18;
+    private static final int CHILDREN_MINIMUM_AGE = 6;
+    private static final int CHILDREN_MAXIMUM_AGE = 12;
+
     private Long id;
     private String email;
     private Integer age;
@@ -12,6 +18,14 @@ public class User {
         this.id = id;
         this.email = email;
         this.age = age;
+    }
+
+    public boolean isChildren() {
+        return this.age >= CHILDREN_MINIMUM_AGE && this.age <= CHILDREN_MAXIMUM_AGE;
+    }
+
+    public boolean isTeenager() {
+        return this.age >= TEENAGER_MINIMUM_AGE && this.age <= TEENAGER_MAXIMUM_AGE;
     }
 
     public User(final Integer age) {
--- src/main/java/nextstep/subway/domain/path/domain/AgeFare.java
+++ src/main/java/nextstep/subway/domain/path/domain/AgeFare.java
@@ -4,10 +4,6 @@ import nextstep.subway.domain.auth.domain.User;
 
 public class AgeFare {
 
-    private static final int TEENAGER_MINIMUM_AGE = 13;
-    private static final int TEENAGER_MAXIMUM_AGE = 18;
-    private static final int CHILDREN_MINIMUM_AGE = 6;
-    private static final int CHILDREN_MAXIMUM_AGE = 12;
     private static final double TEENAGER_DISCOUNT_RATE = 0.2;
     private static final double CHILDREN_DISCOUNT_RATE = 0.5;
     private static final int DEFAULT_DISCOUNT_FARE = 350;
@@ -16,21 +12,13 @@ public class AgeFare {
     }
 
     public static int calculateAgeFare(int fare, User user) {
-        if (isTeenager(user)) {
+        if (user.isTeenager()) {
             return discountAmountByAge(fare, TEENAGER_DISCOUNT_RATE);
         }
-        if (isChildren(user)) {
+        if (user.isChildren()) {
             return discountAmountByAge(fare, CHILDREN_DISCOUNT_RATE);
         }
         return fare;
-    }
-
-    private static boolean isChildren(final User user) {
-        return user.getAge() >= CHILDREN_MINIMUM_AGE && user.getAge() <= CHILDREN_MAXIMUM_AGE;
-    }
-
-    private static boolean isTeenager(final User user) {
-        return user.getAge() >= TEENAGER_MINIMUM_AGE && user.getAge() <= TEENAGER_MAXIMUM_AGE;
     }
 
     private static int discountAmountByAge(int fare, double discountRate) {
