6f5a44013ed390e23a3cca7a3f32a0d6f08f9f40
lovethefeel <lovethefeel@gmail.com>
2021-12-20 22:54:39 +0900

    refactor: 로그인 멤버 아이디로 사용자 정보 처리 하도록 수정

--- src/main/java/nextstep/subway/favorite/domain/FavoriteRepository.java
+++ src/main/java/nextstep/subway/favorite/domain/FavoriteRepository.java
@@ -1,11 +1,10 @@
 package nextstep.subway.favorite.domain;
 
-import nextstep.subway.member.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface FavoriteRepository extends JpaRepository<Favorite, Long> {
-    Optional<Favorite> findByIdAndMember(Long id, Member member);
-    void deleteByIdAndMember(Long id, Member member);
+    Optional<Favorite> findByIdAndMemberId(Long id, Long memberId);
+    void deleteByIdAndMemberId(Long id, Long memberId);
 }
