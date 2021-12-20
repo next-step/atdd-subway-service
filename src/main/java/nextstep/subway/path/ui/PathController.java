c50f66c1e91cf0b10b61f8c0818d63cbeb9915dc
lovethefeel <lovethefeel@gmail.com>
2021-12-20 22:58:50 +0900

    refactor: 로그인 체크 기능 추가

--- src/main/java/nextstep/subway/path/ui/PathController.java
+++ src/main/java/nextstep/subway/path/ui/PathController.java
@@ -1,5 +1,7 @@
 package nextstep.subway.path.ui;
 
+import nextstep.subway.auth.domain.AuthenticationPrincipal;
+import nextstep.subway.auth.domain.LoginMember;
 import nextstep.subway.path.application.PathService;
 import nextstep.subway.path.dto.PathResponse;
 import org.springframework.http.ResponseEntity;
@@ -18,7 +20,7 @@ public class PathController {
     }
 
     @GetMapping("")
-    public ResponseEntity<PathResponse> findShortestPath(@RequestParam Long source, @RequestParam Long target) {
-        return ResponseEntity.ok(pathService.findShortestPath(source, target));
+    public ResponseEntity<PathResponse> findShortestPath(@AuthenticationPrincipal LoginMember loginMember, @RequestParam Long source, @RequestParam Long target) {
+        return ResponseEntity.ok(pathService.findShortestPath(loginMember, source, target));
     }
 }
