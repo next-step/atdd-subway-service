e1e0d974bbec4988529c0e7cd5e00d2179db8140
lovethefeel <lovethefeel@gmail.com>
2021-12-20 22:54:57 +0900

    refactor: 로그인 멤버 아이디로 사용자 정보 처리 하도록 수정

--- src/main/java/nextstep/subway/favorite/application/FavoriteService.java
+++ src/main/java/nextstep/subway/favorite/application/FavoriteService.java
@@ -1,13 +1,14 @@
 package nextstep.subway.favorite.application;
 
+import nextstep.subway.auth.domain.LoginMember;
 import nextstep.subway.common.exception.NotFoundException;
 import nextstep.subway.favorite.domain.Favorite;
 import nextstep.subway.favorite.domain.FavoriteRepository;
 import nextstep.subway.favorite.dto.FavoriteRequest;
 import nextstep.subway.favorite.dto.FavoriteResponse;
-import nextstep.subway.member.domain.Member;
 import nextstep.subway.station.application.StationService;
 import nextstep.subway.station.domain.Station;
+import nextstep.subway.station.dto.StationResponse;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -23,21 +24,23 @@ public class FavoriteService {
     }
 
     @Transactional
-    public FavoriteResponse saveFavorite(final Member loginMember, final FavoriteRequest request) {
+    public FavoriteResponse saveFavorite(final LoginMember loginMember, final FavoriteRequest request) {
         final Station source = stationService.findStationById(request.getSource());
         final Station target = stationService.findStationById(request.getTarget());
-        final Favorite persistFavorite = favoriteRepository.save(Favorite.of(loginMember, source, target));
-        return FavoriteResponse.from(persistFavorite);
+        final Favorite persistFavorite = favoriteRepository.save(Favorite.of(loginMember.getId(), source.getId(), target.getId()));
+        return FavoriteResponse.of(persistFavorite, StationResponse.of(source), StationResponse.of(target));
     }
 
-    public FavoriteResponse findLineResponseById(final Member loginMember, final Long id) {
-        final Favorite favorite = favoriteRepository.findByIdAndMember(id, loginMember)
+    public FavoriteResponse findLineResponseById(final LoginMember loginMember, final Long id) {
+        final Favorite favorite = favoriteRepository.findByIdAndMemberId(id, loginMember.getId())
                 .orElseThrow(NotFoundException::new);
-        return FavoriteResponse.from(favorite);
+        final Station source = stationService.findStationById(favorite.getSourceStationId());
+        final Station target = stationService.findStationById(favorite.getTargetStationId());
+        return FavoriteResponse.of(favorite, StationResponse.of(source), StationResponse.of(target));
     }
 
     @Transactional
-    public void deleteLineById(final Member loginMember, final Long id) {
-        favoriteRepository.deleteByIdAndMember(id, loginMember);
+    public void deleteLineById(final LoginMember loginMember, final Long id) {
+        favoriteRepository.deleteByIdAndMemberId(id, loginMember.getId());
     }
 }
