a8615504957dfc91070f0d71cc97ade890a7ee37
lovethefeel <lovethefeel@gmail.com>
2021-12-20 22:56:39 +0900

    refactor: 게스트 사용자 생성자 추가

--- src/main/java/nextstep/subway/auth/domain/LoginMember.java
+++ src/main/java/nextstep/subway/auth/domain/LoginMember.java
@@ -1,8 +1,12 @@
 package nextstep.subway.auth.domain;
 
 public class LoginMember {
+
+    private static final LoginMember GUEST = new LoginMember();
+
     private Long id;
     private String email;
+
     private Integer age;
 
     public LoginMember() {
@@ -14,11 +18,27 @@ public class LoginMember {
         this.age = age;
     }
 
+    public static LoginMember of(String email, int age) {
+        return new LoginMember(null, email, age);
+    }
+
+    public static LoginMember from() {
+        return GUEST;
+    }
+
     public Long getId() {
         return id;
     }
 
     public String getEmail() {
         return email;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public boolean isGuest() {
+        return this == GUEST;
     }
 }
