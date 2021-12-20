4a9f29c57989d9ec2d2732162c1c7784ad7eb0b9
lovethefeel <lovethefeel@gmail.com>
2021-12-20 23:02:51 +0900

    test: 수정된 서비스의 대한 Mocking을 위한 테스트 로직 수정

--- src/test/java/nextstep/subway/path/PathServiceTest.java
+++ src/test/java/nextstep/subway/path/PathServiceTest.java
@@ -1,8 +1,11 @@
 package nextstep.subway.path;
 
 import com.google.common.collect.Lists;
+import nextstep.subway.auth.domain.LoginMember;
 import nextstep.subway.line.domain.*;
 import nextstep.subway.path.application.PathService;
+import nextstep.subway.path.domain.Path;
+import nextstep.subway.path.domain.PathStrategy;
 import nextstep.subway.path.dto.PathResponse;
 import nextstep.subway.station.application.StationService;
 import nextstep.subway.station.domain.Station;
@@ -12,10 +15,14 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyList;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class PathServiceTest {
@@ -26,40 +33,53 @@ public class PathServiceTest {
      * |                        |
      * 남부터미널역  --- *3호선* ---   양재
      */
-    @DisplayName("mockito의 MockitoExtension을 활용한 가짜 협력 객체 사용하여 경로조회 테스트한다.")
+    @DisplayName("mockito의 MockitoExtension을 활용한 가짜 협력 객체 사용하여 로그인한 사옹자의 경로조회 테스트한다.")
     @Test
     void findShortestPath() {
         // given
+        final LoginMember loginMember = new LoginMember(1L, "test@gmail.com", 20);
         LineRepository lineRepository = mock(LineRepository.class);
         StationService stationService = mock(StationService.class);
+        PathStrategy pathStrategy = mock(PathStrategy.class);
 
-        when(stationService.findStationById(1L)).thenReturn(Station.from("교대역"));
-        when(stationService.findStationById(2L)).thenReturn(Station.from("양재역"));
+        final Station 교대역 = Station.from(1L, "교대역");
+        final Station 양재역 = Station.from(2L, "양재역");
+        final Station 강남역 = Station.from(3L, "강남역");
+        final Station 남부터미널역 = Station.from(4L, "남부터미널역");
+        final Section 삼호선구간1 = new Section(1L, null, 교대역, 남부터미널역, Distance.of(10));
+        final Section 삼호선구간2 = new Section(2L, null, 남부터미널역, 양재역, Distance.of(10));
+        final Line 이호선 = new Line(1L, "이호선", "blue",
+                Sections.from(Section.of(교대역, 강남역, Distance.of(20))), Fare.from(400));
+        final Line 삼호선 = new Line(2L, "삼호선", "blue", Sections.from(Lists.newArrayList(삼호선구간1, 삼호선구간2)), Fare.from(200));
+        삼호선구간1.addLine(삼호선);
+        삼호선구간2.addLine(삼호선);
+        final Line 신분당선 = new Line(3L, "신분당선", "blue", Sections.from(
+                Section.of(강남역, 양재역, Distance.of(20))), Fare.from(300));
+        final List<Section> sections = Lists.newArrayList(
+                new Section(1L, 삼호선, 교대역, 남부터미널역, Distance.of(10)),
+                new Section(2L, 삼호선, 남부터미널역, 양재역, Distance.of(10)));
+        final List<Station> stations = Lists.newArrayList(
+                교대역, 남부터미널역, 양재역);
+
+        when(stationService.findStationById(1L)).thenReturn(교대역);
+        when(stationService.findStationById(2L)).thenReturn(양재역);
         when(lineRepository.findAll())
-                .thenReturn(Lists.newArrayList(
-                        Line.of("이호선", "blue", Sections.from(
-                                Section.of(Station.from("교대역"), Station.from("강남역"), Distance.of(10)))
-                        ),
-                        Line.of("삼호선", "blue", Sections.from(Lists.newArrayList(
-                                Section.of(Station.from("교대역"), Station.from("남부터미널역"), Distance.of(5)),
-                                Section.of(Station.from("남부터미널역"), Station.from("양재역"), Distance.of(3))))
-                        ),
-                        Line.of("신분당선", "blue", Sections.from(
-                                        Section.of(Station.from("강남역"), Station.from("양재역"), Distance.of(10)))
-                        )
-                ));
+                .thenReturn(Lists.newArrayList(이호선, 삼호선, 신분당선));
+        lenient().when(pathStrategy.getShortestPath(anyList(), any(), any())).thenReturn(Path.of(20, sections, stations));
+
         PathService pathService = new PathService(lineRepository, stationService);
 
         // when
-        PathResponse responses = pathService.findShortestPath(1L, 2L);
+        PathResponse responses = pathService.findShortestPath(loginMember, 교대역.getId(), 양재역.getId());
 
         // then
         assertAll(
                 () -> assertThat(responses.getStations()).hasSize(3),
-                () -> assertThat(responses.getDistance()).isEqualTo(8.0),
+                () -> assertThat(responses.getDistance()).isEqualTo(20),
+                () -> assertThat(responses.getFare()).isEqualTo(new BigDecimal("1650")),
                 () -> assertThat(responses.getStations()).containsExactly(
-                        StationResponse.of(Station.from("교대역")), StationResponse.of(Station.from("남부터미널역")),
-                        StationResponse.of(Station.from("양재역")))
+                        StationResponse.of(교대역), StationResponse.of(남부터미널역),
+                        StationResponse.of(양재역))
         );
     }
 }
