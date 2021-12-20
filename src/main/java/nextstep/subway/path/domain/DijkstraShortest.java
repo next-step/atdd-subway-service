78e34c246dd47ea44ae4dedca4238ad3f9949657
lovethefeel <lovethefeel@gmail.com>
2021-12-20 22:50:25 +0900

    refactor: pathService에서 인터페이스 주입을 통한 분리를 위해 클래스 수정

--- src/main/java/nextstep/subway/path/domain/DijkstraShortest.java
+++ src/main/java/nextstep/subway/path/domain/DijkstraShortest.java
@@ -1,6 +1,9 @@
 package nextstep.subway.path.domain;
 
+import nextstep.subway.common.exception.NotFoundException;
+import nextstep.subway.common.exception.PathSameException;
 import nextstep.subway.line.domain.Line;
+import nextstep.subway.line.domain.Section;
 import nextstep.subway.station.domain.Station;
 import org.jgrapht.GraphPath;
 import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
@@ -11,6 +14,7 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 public class DijkstraShortest implements PathStrategy {
 
@@ -18,14 +22,17 @@ public class DijkstraShortest implements PathStrategy {
     private final DijkstraShortestPath<Station, DefaultWeightedEdge> dijkstraShortestPath = new DijkstraShortestPath(graph);
 
     public Path getShortestPath(final List<Line> lines, final Station source, final Station target) {
+        validateSameStation(source, target);
+        validateFoundBothStations(lines, source, target);
         createEdge(lines);
         createVertex(lines);
+        final GraphPath<Station, DefaultWeightedEdge> graphPath = dijkstraShortestPath.getPath(source, target);
         final List<Station> stations = getVertexes(source, target);
-        return Path.of(getWeight(source, target), stations);
+        final List<Section> sections = findSections(lines, stations);
+        return Path.of(getWeight(graphPath).intValue(), sections, stations);
     }
 
-    public Double getWeight(final Station source, final Station target) {
-        final GraphPath<Station, DefaultWeightedEdge> graphPath = dijkstraShortestPath.getPath(source, target);
+    public Double getWeight(final GraphPath<Station, DefaultWeightedEdge> graphPath) {
         if (Objects.isNull(graphPath)) {
             return Double.NaN;
         }
@@ -45,7 +52,7 @@ public class DijkstraShortest implements PathStrategy {
                 .map(Line::getSectionList)
                 .flatMap(Collection::stream)
                 .forEach(section ->
-                        graph.setEdgeWeight(graph.addEdge(section.getUpStation(), section.getDownStation()), section.getDistance().getDistance()));
+                        graph.setEdgeWeight(graph.addEdge(section.getUpStation(), section.getDownStation()), section.getDistance().getValue()));
     }
 
     public List<Station> getVertexes(final Station source, final Station target) {
@@ -55,4 +62,33 @@ public class DijkstraShortest implements PathStrategy {
         }
         return path.getVertexList();
     }
+
+    private void validateSameStation(final Station sourceStation, final Station targetStation) {
+        if (sourceStation.equals(targetStation)) {
+            throw new PathSameException();
+        }
+    }
+
+    private void validateFoundBothStations(final List<Line> lines, final Station sourceStation, final Station targetStation) {
+        final List<Station> stations = lines.stream()
+                .map(Line::getStations)
+                .flatMap(Collection::stream)
+                .distinct()
+                .collect(Collectors.toList());
+        if (!stations.contains(sourceStation) || !stations.contains(targetStation)) {
+            throw new NotFoundException();
+        }
+    }
+
+    public List<Section> findSections(final List<Line> lines, final List<Station> stations) {
+        List<Section> sections = lines.stream()
+                .flatMap(line -> line.getSectionList()
+                        .stream())
+                .collect(Collectors.toList());
+
+        return sections.stream()
+                .filter(section -> section.isSameSection(stations))
+                .collect(Collectors.toList());
+    }
+
 }
