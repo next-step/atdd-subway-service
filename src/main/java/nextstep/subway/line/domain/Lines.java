fc6720b7a45eeb680d332c0983ee37ffbcc19714
신민준 <jxin95@gmail.com>
2021-12-22 01:41:19 +0900

    feat: 노선 전체에 대한 클래스 추가

---
+++ src/main/java/nextstep/subway/line/domain/Lines.java
@@ -0,0 +1,35 @@
+package nextstep.subway.line.domain;
+
+import nextstep.subway.station.domain.Station;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Lines {
+
+    List<Line> lines = new ArrayList<>();
+
+    public Lines() {
+    }
+
+    public Lines(final List<Line> lines) {
+        this.lines = lines;
+    }
+
+    public List<Section> sections() {
+        return lines
+                .stream()
+                .flatMap(line -> line.getSections().getSections().stream())
+                .collect(Collectors.toList());
+    }
+
+    public List<Station> stations() {
+        return lines
+                .stream()
+                .flatMap(line -> line.getStations().stream())
+                .distinct()
+                .collect(Collectors.toList());
+    }
+
+}
