a3b2f039e8a1217aa3645d736f897f8d890931c7
koola97620 <koola97620@nate.com>
2021-12-21 13:49:18 +0900

    feat: 피드백 반영

--- src/main/java/nextstep/subway/path/exception/SameStartEndStationException.java
+++ src/main/java/nextstep/subway/path/exception/SameStartEndStationException.java
@@ -1,7 +1,7 @@
 package nextstep.subway.path.exception;
 
 public class SameStartEndStationException extends RuntimeException {
-    public SameStartEndStationException(String message) {
-        super(message);
+    public SameStartEndStationException() {
+        super("출발역과 도착역이 달라야합니다.");
     }
 }
