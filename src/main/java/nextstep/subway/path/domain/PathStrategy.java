b81cdacbda507ecbcc86dfbc202b97ba474c42bf
lovethefeel <lovethefeel@gmail.com>
2021-12-22 02:03:51 +0900

    refactor: 필드 오타 수정

--- src/main/java/nextstep/subway/path/domain/PathStrategy.java
+++ src/main/java/nextstep/subway/path/domain/PathStrategy.java
@@ -6,5 +6,5 @@ import nextstep.subway.station.domain.Station;
 import java.util.List;
 
 public interface PathStrategy {
-    Path getShortestPath(final List<Line> stations, final Station source, final Station target);
+    Path getShortestPath(final List<Line> lines, final Station source, final Station target);
 }
