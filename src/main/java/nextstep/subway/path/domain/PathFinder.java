49593d6cfecac516ae63a12e5cb008ebc4722475
박창혁 <changchang73@pionnet.co.kr>
2021-12-22 20:48:43 +0900

    refactor : 노선 추가금 예외처리 추가

--- src/main/java/nextstep/subway/path/domain/PathFinder.java
+++ src/main/java/nextstep/subway/path/domain/PathFinder.java
@@ -7,6 +7,7 @@ import org.jgrapht.GraphPath;
 import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
 
 import nextstep.subway.line.domain.Line;
+import nextstep.subway.path.PathFinderException;
 import nextstep.subway.station.domain.Station;
 
 public class PathFinder {
@@ -14,6 +15,7 @@ public class PathFinder {
     public static final String ERROR_DUPLICATE_STATION = "출발역과 도착역이 같습니다.";
     public static final String ERROR_START_STATION_NOT_FOUND = "출발역이 찾을 수 없습니다.";
     public static final String ERROR_END_STATION_NOT_FOUND = "도착역을 찾을 수 없습니다.";
+    public static final String ERROR_GET_LINE_FARE = "노선 추가금 정보 조회 시 오류가 발생했습니다.";
 
     private final List<Line> lines;
     private final SubwayGraph subwayGraph;
@@ -40,7 +42,7 @@ public class PathFinder {
         return sectionEdges.stream()
             .map(sectionEdge -> getLinesFare(sectionEdge.getUpStation(), sectionEdge.getDownStation()))
             .max(Integer::compareTo)
-            .orElseThrow(IllegalArgumentException::new);
+            .orElseThrow(() -> new PathFinderException(ERROR_GET_LINE_FARE));
     }
 
     private int getLinesFare(Station upStation, Station downStation) {
@@ -48,7 +50,7 @@ public class PathFinder {
             .filter(line -> line.isContainsUpStationAndDownStation(upStation, downStation))
             .map(Line::getFare)
             .max(Integer::compareTo)
-            .orElseThrow(IllegalArgumentException::new);
+            .orElseThrow(() -> new PathFinderException(ERROR_GET_LINE_FARE));
     }
 
     private void checkStations(Station source, Station target) {
