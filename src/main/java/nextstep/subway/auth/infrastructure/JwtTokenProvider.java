36781f3da5ef566b9e6974f7c077eeec147a0bd4
daehyun0 <dd70296@gmail.com>
2021-12-22 14:27:00 +0900

    refactor: 유효하지 않은 토큰으로 요청이 들어올 경우 예외 발생하도록 수정

--- src/main/java/nextstep/subway/auth/infrastructure/JwtTokenProvider.java
+++ src/main/java/nextstep/subway/auth/infrastructure/JwtTokenProvider.java
@@ -10,6 +10,7 @@ import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import nextstep.subway.common.exception.Exceptions;
 
 @Component
 public class JwtTokenProvider {
@@ -41,7 +42,7 @@ public class JwtTokenProvider {
 
             return !claims.getBody().getExpiration().before(new Date());
         } catch (JwtException | IllegalArgumentException e) {
-            return false;
+            throw Exceptions.TOKEN_IS_NOT_ELIGIBLE.getException();
         }
     }
 }
