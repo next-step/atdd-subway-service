11e3e3ab4989ff0b796dbac80d7ecd155fafd455
daehyun0 <dd70296@gmail.com>
2021-12-22 14:42:35 +0900

    refactor: FavoriteService.createFavorite에서 유효성 검사 추가

--- src/main/java/nextstep/subway/favorite/application/FavoriteService.java
+++ src/main/java/nextstep/subway/favorite/application/FavoriteService.java
@@ -1,12 +1,14 @@
 package nextstep.subway.favorite.application;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import nextstep.subway.auth.domain.LoginMember;
+import nextstep.subway.common.exception.BadParameterException;
 import nextstep.subway.common.exception.Exceptions;
 import nextstep.subway.favorite.domain.Favorite;
 import nextstep.subway.favorite.domain.FavoriteRepository;
@@ -33,11 +35,26 @@ public class FavoriteService {
 
 	@Transactional
 	public FavoriteResponse saveFavorite(LoginMember loginMember, FavoriteCreateRequest favoriteCreateRequest) {
+		validateOnSaveFavorite(favoriteCreateRequest);
 		Station source = findStationById(favoriteCreateRequest.getSource());
 		Station target = findStationById(favoriteCreateRequest.getTarget());
 		Member member = findMemberById(loginMember.getId());
 		Favorite favorite = favoriteRepository.save(new Favorite(source, target, member));
 		return FavoriteResponse.of(favorite);
+	}
+
+	private void validateOnSaveFavorite(FavoriteCreateRequest favoriteCreateRequest) {
+		if (Objects.isNull(favoriteCreateRequest.getSource())) {
+			throw new BadParameterException("즐겨찾기를 등록하기 위해 출발지를 입력해주세요.");
+		}
+
+		if (Objects.isNull(favoriteCreateRequest.getTarget())) {
+			throw new BadParameterException("즐겨찾기를 등록하기 위해 도착지를 입력해주세요.");
+		}
+
+		if (favoriteCreateRequest.getSource().equals(favoriteCreateRequest.getTarget())) {
+			throw new BadParameterException("출발지와 도착지가 같아 즐겨찾기를 등록할 수 없습니다.");
+		}
 	}
 
 	public List<FavoriteResponse> findFavorites(LoginMember loginMember) {
