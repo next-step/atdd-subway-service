6aacd6aba611a280c089bbde6841cb55c237380d
박창혁 <changchang73@pionnet.co.kr>
2021-12-22 21:57:19 +0900

    feat : 할인정책 enum 추가

---
+++ src/main/java/nextstep/subway/path/domain/DiscountPolicy.java
@@ -0,0 +1,41 @@
+package nextstep.subway.path.domain;
+
+import java.util.stream.Stream;
+
+public enum DiscountPolicy {
+    BABY(6, 0.0),
+    CHILD(13, 0.5),
+    KIDS(19, 0.7),
+    ADULT(999, 1.0);
+
+    private final double rate;
+    private final double age;
+
+    DiscountPolicy(double age, double rate) {
+        this.age = age;
+        this.rate = rate;
+    }
+
+    public static DiscountPolicy of(int age) {
+        return Stream.of(values())
+            .filter(it -> it.is(age))
+            .findFirst()
+            .orElse(ADULT);
+    }
+
+    public double reduce(int fare) {
+        return fare * rate;
+    }
+
+    private boolean is(int age) {
+        return this.age > age;
+    }
+
+    public double getRate() {
+        return rate;
+    }
+
+    public double getAge() {
+        return age;
+    }
+}
