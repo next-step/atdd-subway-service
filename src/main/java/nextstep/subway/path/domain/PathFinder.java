4565aca24330e7cb0a8e20e8c78ea913271d0e58
shryu <>
2021-12-24 01:15:16 +0900

    feat : 노선 추가요금 response 추가 로직 구현

--- src/main/java/nextstep/subway/path/domain/PathFinder.java
+++ src/main/java/nextstep/subway/path/domain/PathFinder.java
@@ -4,11 +4,15 @@ import nextstep.subway.line.domain.Line;
 import nextstep.subway.line.domain.Section;
 import nextstep.subway.path.dto.PathResponse;
 import nextstep.subway.station.domain.Station;
+import nextstep.subway.station.dto.StationResponse;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class PathFinder {
+    private static final int NONE_EXTRA_FARE = 0;
     private final ShortestPathCalculator shortestPathCalculator;
 
     public PathFinder(ShortestPathCalculator shortestPathCalculator) {
@@ -18,8 +22,9 @@ public class PathFinder {
     public PathResponse findPath(List<Line> lines, Station sourceStation, Station targetStation) {
         validateSourceEqualsTarget(sourceStation, targetStation);
         List<Section> sections = findSections(lines);
-
-        return shortestPathCalculator.calculatePath(sections, sourceStation, targetStation);
+        PathResponse pathResponse = shortestPathCalculator.calculatePath(sections, sourceStation, targetStation);
+        pathResponse.addOverFare(findContainLines(sections, pathResponse.getStations()));
+        return pathResponse;
     }
 
     private void validateSourceEqualsTarget(Station sourceStation, Station targetStation) {
@@ -34,5 +39,38 @@ public class PathFinder {
                 .map(line -> line.getSections())
                 .forEach(sections::addAll);
         return sections;
+    }
+
+    private int findContainLines(List<Section> sections, List<StationResponse> stations) {
+        Set<Line> lines = new HashSet<>();
+        for (int i = 0; i < stations.size()-1; i++) {
+            Section containSection = findSection(sections, stations.get(i), stations.get(i+1));
+            lines.add(containSection.getLine());
+        }
+        return findExtraFare(lines);
+    }
+
+    private Section findSection(List<Section> sections, StationResponse station, StationResponse nextStation) {
+        return sections.stream()
+                .filter(section -> section.getUpStation().getId().equals(station.getId()))
+                .filter(section -> section.getDownStation().getId().equals(nextStation.getId()))
+                .findFirst()
+                .orElseGet(() -> findSectionReverse(sections, station, nextStation));
+    }
+
+    private Section findSectionReverse(List<Section> sections, StationResponse station, StationResponse nextStation) {
+        return sections.stream()
+                .filter(section -> section.getUpStation().getId().equals(nextStation.getId()))
+                .filter(section -> section.getDownStation().getId().equals(station.getId()))
+                .findFirst()
+                .orElseThrow(RuntimeException::new);
+    }
+
+    private int findExtraFare(Set<Line> containLines) {
+        return containLines.stream()
+                .filter(line -> line.getExtraFare() > NONE_EXTRA_FARE)
+                .mapToInt(Line::getExtraFare)
+                .max()
+                .orElse(NONE_EXTRA_FARE);
     }
 }
