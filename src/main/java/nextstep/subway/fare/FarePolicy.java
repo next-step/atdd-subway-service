4372bf77dd20caf7e8530e315fc0d7dcd687e520
shryu <>
2021-12-24 02:14:25 +0900

    feat : 연령별 요금 계산 구현

--- src/main/java/nextstep/subway/fare/FarePolicy.java
+++ src/main/java/nextstep/subway/fare/FarePolicy.java
@@ -4,4 +4,11 @@ public class FarePolicy {
     public static int calculateDistanceOverFare(int distance) {
         return DistanceFarePolicy.calculateOverFare(distance);
     }
+
+    public static int calculateDiscountAgeFare(Integer memberAge, int fare) {
+        if (memberAge != null) {
+            return (int) Math.ceil(DiscountAgeFarePolicy.calculateDiscountFare(memberAge, fare));
+        }
+        return fare;
+    }
 }
