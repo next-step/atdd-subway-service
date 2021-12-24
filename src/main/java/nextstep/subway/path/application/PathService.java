65ec13e71ab1ef46af81ff4b45cc5ef2d5b073c2
lovethefeel <lovethefeel@gmail.com>
2021-12-25 01:26:09 +0900

    refactor: 외부 라이브러리가 변경되어도 영향없도록 수정

--- src/main/java/nextstep/subway/path/application/PathService.java
+++ src/main/java/nextstep/subway/path/application/PathService.java
@@ -4,7 +4,10 @@ import nextstep.subway.auth.domain.LoginMember;
 import nextstep.subway.line.domain.Fare;
 import nextstep.subway.line.domain.Line;
 import nextstep.subway.line.domain.LineRepository;
-import nextstep.subway.path.domain.*;
+import nextstep.subway.path.domain.Path;
+import nextstep.subway.path.domain.PathFareAge;
+import nextstep.subway.path.domain.PathFareDistance;
+import nextstep.subway.path.domain.PathStrategy;
 import nextstep.subway.path.dto.PathResponse;
 import nextstep.subway.station.application.StationService;
 import nextstep.subway.station.domain.Station;
@@ -18,19 +21,19 @@ import java.util.List;
 public class PathService {
     private final LineRepository lineRepository;
     private final StationService stationService;
-    private final PathFinder pathFinder = new PathFinder();
+    private final PathStrategy pathStrategy;
 
-    public PathService(final LineRepository lineRepository, final StationService stationService) {
+    public PathService(final LineRepository lineRepository, final StationService stationService, final PathStrategy pathStrategy) {
         this.lineRepository = lineRepository;
         this.stationService = stationService;
+        this.pathStrategy = pathStrategy;
     }
 
     public PathResponse findShortestPath(final LoginMember loginMember, final Long source, final Long target) {
         final Station sourceStation = stationService.findStationById(source);
         final Station targetStation = stationService.findStationById(target);
         final List<Line> lines = lineRepository.findAll();
-        pathFinder.setPathStrategy(new DijkstraPathFinder());
-        final Path path = pathFinder.findShortestPath(lines, sourceStation, targetStation);
+        final Path path = pathStrategy.getShortestPath(lines, sourceStation, targetStation);
         final Fare distanceFare = PathFareDistance.of(path);
         final Fare ageFare = PathFareAge.of(loginMember);
         return PathResponse.of(path, distanceFare.plus(ageFare));
