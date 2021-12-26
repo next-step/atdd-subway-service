4a561bbd07667ed06430e50eb6b8078309e1dc43
shryu <>
2021-12-26 22:13:59 +0900

    test : 거리별, 연령별 인수테스트 추가 작성

--- src/test/java/nextstep/subway/path/PathAcceptanceTest.java
+++ src/test/java/nextstep/subway/path/PathAcceptanceTest.java
@@ -10,6 +10,7 @@ import nextstep.subway.line.acceptance.LineAcceptanceTest;
 import nextstep.subway.line.acceptance.LineSectionAcceptanceTest;
 import nextstep.subway.line.dto.LineRequest;
 import nextstep.subway.line.dto.LineResponse;
+import nextstep.subway.member.domain.Member;
 import nextstep.subway.path.dto.PathResponse;
 import nextstep.subway.station.StationAcceptanceTest;
 import nextstep.subway.station.dto.StationResponse;
@@ -43,13 +44,15 @@ public class PathAcceptanceTest extends AcceptanceTest {
     private StationResponse 대전역;
     private StationResponse 사당역;
     private StationResponse 서초역;
+    private Member 어린이;
+    private Member 청소년;
 
     /**
-     * 교대역    --- *2호선* ---   강남역
-     * |                        |
-     * *3호선*                   *신분당선*
-     * |                        |
-     * 남부터미널역  --- *3호선* ---   양재
+     * 교대역    --- *2호선*(50) ---   강남역   --- *4호선*(20) ---   사당역
+     * |                              |
+     * *3호선*(3)                   *신분당선*(10)
+     * |                              |
+     * 남부터미널역  --- *3호선*(37) ---  양재
      */
     @BeforeEach
     public void setUp() {
@@ -68,6 +71,12 @@ public class PathAcceptanceTest extends AcceptanceTest {
         삼호선 = 지하철_노선_등록되어_있음("삼호선", "bg-red-600", 교대역, 양재역, 40, 0);
         사호선 = 지하철_노선_등록되어_있음("사호선", "bg-red-600", 사당역, 서초역, 30, 0);
         지하철_노선에_지하철역_등록되어_있음(삼호선, 교대역, 남부터미널역, 3);
+        지하철_노선에_지하철역_등록되어_있음(사호선, 강남역, 사당역, 20);
+
+        어린이 = new Member("email@email.com", "password", 12);
+        청소년 = new Member("email1@email.com", "password", 18);
+        회원_생성을_요청(어린이.getEmail(), 어린이.getPassword(), 어린이.getAge());
+        회원_생성을_요청(청소년.getEmail(), 청소년.getPassword(), 청소년.getAge());
     }
 
     @DisplayName("경로 조회")
@@ -116,6 +125,58 @@ public class PathAcceptanceTest extends AcceptanceTest {
         ExtractableResponse<Response> response = 지하철_경로_조회_요청(남부터미널역, 대전역, null);
         // Then 에러가 발생함
         잘못된_요청_응답됨(response, "Station을 조회할 수 없습니다.");
+    }
+
+    @DisplayName("지하철 구간의 최단경로, 거리, 금액을 조회 (어린이 할인 적용, 10km 초과, 50km 까지 추가요금 적용)")
+    @Test
+    void findShortestPath_어린이_할인_10km_초과_50km_까지() {
+        // Given
+        String token = 로그인_요청(어린이.getEmail(), 어린이.getPassword()).body().as(TokenResponse.class).getAccessToken();
+        // When 지하철 경로 조회 요청
+        PathResponse pathResponse = 지하철_경로_조회_요청(남부터미널역, 강남역, token).body().as(PathResponse.class);
+        // And 지하철 경로 길이가 예상과 같음
+        지하철_경로_길이_같음(pathResponse, 47);
+        // And 지하철 요금이 예상과 같음
+        지하철_요금_조회됨(pathResponse, 1_100);
+    }
+
+    @DisplayName("지하철 구간의 최단경로, 거리, 금액을 조회 (청소년 할인 적용, 10km 초과, 50km 까지 추가요금 적용)")
+    @Test
+    void findShortestPath_청소년_할인_10km_초과_50km_까지() {
+        // Given
+        String token = 로그인_요청(청소년.getEmail(), 청소년.getPassword()).body().as(TokenResponse.class).getAccessToken();
+        // When 지하철 경로 조회 요청
+        PathResponse pathResponse = 지하철_경로_조회_요청(남부터미널역, 강남역, token).body().as(PathResponse.class);
+        // And 지하철 경로 길이가 예상과 같음
+        지하철_경로_길이_같음(pathResponse, 47);
+        // And 지하철 요금이 예상과 같음
+        지하철_요금_조회됨(pathResponse, 1_760);
+    }
+
+    @DisplayName("지하철 구간의 최단경로, 거리, 금액을 조회 (어린이 할인 적용, 50km 초과 추가요금 적용)")
+    @Test
+    void findShortestPath_어린이_할인_50km_초과() {
+        // Given
+        String token = 로그인_요청(어린이.getEmail(), 어린이.getPassword()).body().as(TokenResponse.class).getAccessToken();
+        // When 지하철 경로 조회 요청
+        PathResponse pathResponse = 지하철_경로_조회_요청(남부터미널역, 사당역, token).body().as(PathResponse.class);
+        // And 지하철 경로 길이가 예상과 같음
+        지하철_경로_길이_같음(pathResponse, 67);
+        // And 지하철 요금이 예상과 같음
+        지하철_요금_조회됨(pathResponse, 1_250);
+    }
+
+    @DisplayName("지하철 구간의 최단경로, 거리, 금액을 조회 (청소년 할인 적용, 50km 초과 추가요금 적용)")
+    @Test
+    void findShortestPath_청소년_할인_50km_초과() {
+        // Given
+        String token = 로그인_요청(청소년.getEmail(), 청소년.getPassword()).body().as(TokenResponse.class).getAccessToken();
+        // When 지하철 경로 조회 요청
+        PathResponse pathResponse = 지하철_경로_조회_요청(남부터미널역, 사당역, token).body().as(PathResponse.class);
+        // And 지하철 경로 길이가 예상과 같음
+        지하철_경로_길이_같음(pathResponse, 67);
+        // And 지하철 요금이 예상과 같음
+        지하철_요금_조회됨(pathResponse, 2_000);
     }
 
     private void 지하철_경로_길이_같음(PathResponse pathResponse, int expectedDistance) {
