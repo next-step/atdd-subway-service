52ce515376c5596b5c4ddee06427fde1c5dd96a2
yunhalee05 <yunhalee0506@naver.com>
2022-02-18 16:50:54 +0900

    Test: 로그인 인수테스트 작성

--- src/test/java/nextstep/subway/auth/acceptance/AuthAcceptanceTest.java
+++ src/test/java/nextstep/subway/auth/acceptance/AuthAcceptanceTest.java
@@ -1,10 +1,43 @@
 package nextstep.subway.auth.acceptance;
 
+import static nextstep.subway.member.MemberAcceptanceTest.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
 import nextstep.subway.AcceptanceTest;
+import nextstep.subway.auth.dto.TokenRequest;
+import nextstep.subway.member.MemberAcceptanceTest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 
+@DisplayName("로그인 기능")
 public class AuthAcceptanceTest extends AcceptanceTest {
+
+    @DisplayName("로그인을 시도한다.")
+    @Test
+    void login(){
+        // given 회원 등록되어 있음
+        ExtractableResponse<Response> createResponse = 회원_생성을_요청(EMAIL, PASSWORD, AGE);
+        회원_생성됨(createResponse);
+
+        // when 로그인 요청
+        TokenRequest request = new TokenRequest(EMAIL, PASSWORD);
+        ExtractableResponse<Response> response = RestAssured
+            .given().log().all()
+            .contentType(MediaType.APPLICATION_JSON_VALUE)
+            .body(request)
+            .when().post("/post/token")
+            .then().log().all()
+            .extract();
+
+        // then 로그인 됨
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+        assertThat(response.jsonPath().getString("accessToken")).isNotBlank();
+    }
 
     @DisplayName("Bearer Auth")
     @Test
