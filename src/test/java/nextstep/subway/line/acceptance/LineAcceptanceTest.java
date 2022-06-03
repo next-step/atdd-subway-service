4c1cd22a8a905a8855918cd2dc0cac615c2e6e3a
KangSeongHyo <abc9023@naver.com>
2022-06-03 20:32:02 +0900

    refactor : TestFactory 사용하여 테스트 그룹화 처리 및 테스트 시나리오 분리 & 수정

--- src/test/java/nextstep/subway/line/acceptance/LineAcceptanceTest.java
+++ src/test/java/nextstep/subway/line/acceptance/LineAcceptanceTest.java
@@ -3,6 +3,7 @@ package nextstep.subway.line.acceptance;
 import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
+import java.util.stream.Stream;
 import nextstep.subway.AcceptanceTest;
 import nextstep.subway.line.dto.LineRequest;
 import nextstep.subway.line.dto.LineResponse;
@@ -10,7 +11,9 @@ import nextstep.subway.station.StationAcceptanceTest;
 import nextstep.subway.station.dto.StationResponse;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DynamicTest;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
@@ -19,11 +22,16 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.DynamicTest.dynamicTest;
 
 @DisplayName("지하철 노선 관련 인수테스트")
 public class LineAcceptanceTest extends AcceptanceTest {
+    private static final int ID = 2;
+
+    private StationResponse 교대역;
     private StationResponse 강남역;
     private StationResponse 광교역;
+    private LineRequest 이호선;
     private LineRequest 신분당선;
     private LineRequest 구신분당선;
     private LineRequest 뉴신분당선;
@@ -33,9 +41,11 @@ public class LineAcceptanceTest extends AcceptanceTest {
         super.setUp();
 
         // given
+        교대역 = StationAcceptanceTest.지하철역_등록되어_있음("교대역").as(StationResponse.class);
         강남역 = StationAcceptanceTest.지하철역_등록되어_있음("강남역").as(StationResponse.class);
         광교역 = StationAcceptanceTest.지하철역_등록되어_있음("광교역").as(StationResponse.class);
 
+        이호선 = new LineRequest("이호선", "bg-green-600", 강남역.getId(), 교대역.getId(), 10);
         신분당선 = new LineRequest("신분당선", "bg-red-600", 강남역.getId(), 광교역.getId(), 10);
         구신분당선 = new LineRequest("구신분당선", "bg-red-600", 강남역.getId(), 광교역.getId(), 15);
         뉴신분당선 = new LineRequest("뉴신분당선", "bg-red-500", 강남역.getId(), 광교역.getId(), 9);
@@ -46,65 +56,117 @@ public class LineAcceptanceTest extends AcceptanceTest {
      *
      *   Background 
      *     Given 지하철역 등록되어 있음
-     *     And 지하철 노선 등록되어 있음
      *
      *   Scenario: 지하철 노선을 관리
-     *     When 지하철 노선 등록 요청
-     *     Then 지하철 노선 등록됨
-     *     when 지하철 노선을 조회하면
-     *     Then 등록한 노선을 확인할 수 있음
-     *     When 지하철 노선 목록 조회 하면
-     *     Then 등록한 노선들을 확인 할 수 있음
-     *     When 등록된 노선을 중복으로 등록요청 하면
-     *     Then 지하철 노선 등록 실패함
-     *     When 지하철 노선을 수정 요청하면
-     *     Then 등록된 노선이 수정됨
-     *     When 지하철 노선을 삭제 요청하면
-     *     Thren 요청한 등록된 노선이 삭제됨
+     *     When n개의 노선들을 노선등록 요청하면
+     *     Then 노선들이 등록된다.
+     *     When 노선목록조회 요청하면
+     *     Then 노선목록을 응답받는다.
+     *     And  노선목록에서 n개의 노선을 확인 할 수 있다.
+     *
+     *     When 노선삭제 요청하면
+     *     Then 등록된 노선이 삭제된다.
+     *     When 노선목록조회 요청하면
+     *     Then 노선목록을 응답받는다.
+     *     And  노선목록에서 노선이 삭제된 것을 확인할 수 있다.
+     *
+     *     When 노선수정 요청하면
+     *     Then 노선의 정보가 수정된다.
+     *     When 노선조회 요청하면
+     *     Then 노선을 응답받는다.
+     *     And  노선에서 수정된 정보를 확인 할 수 있다.
      * */
     @DisplayName("지하철 노선을 관리한다.")
-    @Test
-    void featureLine(){
+    @TestFactory
+    Stream<DynamicTest> manageLine() {
+        return Stream.of(
+                dynamicTest("노선등록 요청으로 노선들을 등록하면 노선목록조회 요청시 등록한 노선들을 응답 받는다.", () -> {
+                    // when
+                    ExtractableResponse<Response> createResponse1 = 지하철_노선_생성_요청(이호선);
+                    ExtractableResponse<Response> createResponse2 = 지하철_노선_생성_요청(신분당선);
+
+                    // then
+                    지하철_노선_생성됨(createResponse1);
+                    지하철_노선_생성됨(createResponse2);
+
+                    // when
+                    ExtractableResponse<Response> getAllResponse = 지하철_노선_목록_조회_요청();
+
+                    // then
+                    지하철_노선_목록_응답됨(getAllResponse);
+                    지하철_노선_목록_포함됨(getAllResponse, Arrays.asList(createResponse1, createResponse2));
+                }),
+
+                dynamicTest("노선삭제 요청으로 노선을 삭제하고 노선목록조회 요청시 삭제한 노선은 목록에 없다.", () -> {
+
+                    //given
+                    ExtractableResponse<Response> createResponse = 지하철_노선_생성_요청(구신분당선);
+
+                    // when
+                    ExtractableResponse<Response> deleteResponse = 지하철_노선_제거_요청(createResponse);
+
+                    // then
+                    지하철_노선_삭제됨(deleteResponse);
+
+                    // when
+                    ExtractableResponse<Response> getAllResponse = 지하철_노선_목록_조회_요청();
+
+                    // then
+                    지하철_노선_목록_포함되지_않음(getAllResponse, createResponse);
+
+                }),
+
+                dynamicTest("노선수정 요청으로 노선을 수정하면 조회시 수정여부를 확인 할 수 있다.", () -> {
+
+                    // given
+                    ExtractableResponse<Response> createResponse = 지하철_노선_생성_요청(구신분당선);
+
+                    // when
+                    ExtractableResponse<Response> updateResponse = 지하철_노선_수정_요청(createResponse, 뉴신분당선);
+
+                    // then
+                    지하철_노선_수정됨(updateResponse);
+
+                    // when
+                    ExtractableResponse<Response> getResponse = 지하철_노선_조회_요청(createResponse);
+
+                    // then
+                    지하철_노선_수정_정보_확인(getResponse, 뉴신분당선);
+                })
+        );
+
+    }
+
+
+    /**
+     * Feature: 지하철 노선 관련 기능
+     *
+     *   Background
+     *     Given 지하철역 등록되어 있음
+     *
+     *   Scenario: 지하철 노선을 관리 (실패)
+     *     When 노선등록 요청하면
+     *     Then 노선이 등록된다.
+     *     When 등록된 노선으로 등록요청 하면
+     *     Then 노선 등록에 실패한다.
+     * */
+    @DisplayName("지하철 노선을 관리한다.(실패)")
+    @TestFactory
+    void manageLine_fail(){
+
         // when
         ExtractableResponse<Response> createResponse1 = 지하철_노선_생성_요청(신분당선);
-        ExtractableResponse<Response> createResponse2 = 지하철_노선_생성_요청(구신분당선);
 
         // then
         지하철_노선_생성됨(createResponse1);
-        지하철_노선_생성됨(createResponse2);
-
-        //when
-        ExtractableResponse<Response> getResponse = 지하철_노선_조회_요청(createResponse1);
-
-        // then
-        지하철_노선_응답됨(getResponse);
 
         // when
-        ExtractableResponse<Response> getAllResponse = 지하철_노선_목록_조회_요청();
+        ExtractableResponse<Response> createResponse2 = 지하철_노선_생성_요청(신분당선);
 
         // then
-        지하철_노선_목록_응답됨(getAllResponse);
-        지하철_노선_목록_포함됨(getAllResponse, Arrays.asList(createResponse1, createResponse2));
-
-        // when
-        ExtractableResponse<Response> failResponse = 지하철_노선_생성_요청(신분당선);
-
-        // then
-        지하철_노선_생성_실패됨(failResponse);
-
-        // when
-        ExtractableResponse<Response> updateResponse = 지하철_노선_수정_요청(createResponse1, 뉴신분당선);
-
-        // then
-        지하철_노선_수정됨(updateResponse);
-
-        // when
-        ExtractableResponse<Response> deleteResponse = 지하철_노선_제거_요청(createResponse1);
-
-        // then
-        지하철_노선_삭제됨(deleteResponse);
-
+        지하철_노선_생성_실패됨(createResponse2);
     }
+
 
     public static ExtractableResponse<Response> 지하철_노선_등록되어_있음(LineRequest params) {
         return 지하철_노선_생성_요청(params);
@@ -190,7 +252,7 @@ public class LineAcceptanceTest extends AcceptanceTest {
 
     public static void 지하철_노선_목록_포함됨(ExtractableResponse<Response> response, List<ExtractableResponse<Response>> createdResponses) {
         List<Long> expectedLineIds = createdResponses.stream()
-                .map(it -> Long.parseLong(it.header("Location").split("/")[2]))
+                .map(createdResponse -> Long.parseLong(createdResponse.header("Location").split("/")[ID]))
                 .collect(Collectors.toList());
 
         List<Long> resultLineIds = response.jsonPath().getList(".", LineResponse.class).stream()
@@ -198,7 +260,25 @@ public class LineAcceptanceTest extends AcceptanceTest {
                 .collect(Collectors.toList());
 
         assertThat(resultLineIds).containsAll(expectedLineIds);
+    }   
+    
+    public static void 지하철_노선_목록_포함되지_않음(ExtractableResponse<Response> response, ExtractableResponse<Response> createResponse) {
+        Long expectedLineId = Long.parseLong(createResponse.header("Location").split("/")[ID]);
+
+        List<Long> resultLineIds = response.jsonPath().getList(".", LineResponse.class).stream()
+                .map(LineResponse::getId)
+                .collect(Collectors.toList());
+
+        assertThat(resultLineIds).doesNotContain(expectedLineId);
     }
+
+    private void 지하철_노선_수정_정보_확인(ExtractableResponse<Response> response, LineRequest request) {
+        LineResponse lineResponse = response.body().as(LineResponse.class);
+
+        assertThat(lineResponse.getName()).isEqualTo(request.getName());
+        assertThat(lineResponse.getColor()).isEqualTo(request.getColor());
+    }
+
 
     public static void 지하철_노선_수정됨(ExtractableResponse<Response> response) {
         assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
