f2beb6dcc4ae3607130ade508012093dfeb4e22b
ssonsh <chosunci@gmail.com>
2022-06-03 13:38:00 +0900

    test (UnitTest) : 노선 Line의 색상변경 테스트 코드 추가

--- src/test/java/study/unit/UnitTest.java
+++ src/test/java/study/unit/UnitTest.java
@@ -1,6 +1,7 @@
 package study.unit;
 
 import nextstep.subway.line.domain.Line;
+import nextstep.subway.line.domain.LineColor;
 import nextstep.subway.line.domain.LineName;
 import nextstep.subway.station.domain.Station;
 import org.junit.jupiter.api.DisplayName;
@@ -10,8 +11,10 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("단위 테스트")
 class UnitTest {
+
     @Test
-    void update() {
+    @DisplayName("노선명을 변경할 수 있다.")
+    void update01() {
         // given
         String newName = "구분당선";
 
@@ -25,5 +28,23 @@ class UnitTest {
 
         // then
         assertThat(line.getName()).isEqualTo(LineName.from(newName));
+    }
+
+    @Test
+    @DisplayName("노선색상을 변경할 수 있다.")
+    void update02() {
+        // given
+        String newColor = "BLUE";
+
+        Station upStation = new Station("강남역");
+        Station downStation = new Station("광교역");
+        Line line = Line.of("신분당선", "RED", upStation, downStation, 10);
+        Line newLine = Line.of("신신분당선", newColor);
+
+        // when
+        line.update(newLine);
+
+        // then
+        assertThat(line.getColor()).isEqualTo(LineColor.from(newColor));
     }
 }
