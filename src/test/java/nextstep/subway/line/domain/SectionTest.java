fca6cc262c88de819e9f0c132fd9be3968608cf4
limwoobin <drogba02@naver.com>
2022-06-05 22:53:03 +0900

    test: 구간에 대한 단위테스트를 추가하라

--- src/test/java/nextstep/subway/line/domain/SectionTest.java
+++ src/test/java/nextstep/subway/line/domain/SectionTest.java
@@ -1,5 +1,6 @@
 package nextstep.subway.line.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -46,6 +47,50 @@ class SectionTest {
         assertAll(
             () -> assertTrue(하행역과_같음),
             () -> assertFalse(하행역과_다름)
+        );
+    }
+
+    @DisplayName("하행역이 포함된 구간이 추가되면 역의 상행이"
+        + " 추가되는 역으로 변경되며 거리도 차감된다")
+    @Test
+    void update_upStation_test() {
+        // given
+        Station 문래역 = new Station("문래");
+        Station 대림역 = new Station("대림");
+
+        Section section = new Section(new Line(), 문래역, 대림역, 10);
+        Station 신도림역 = new Station("신도림");
+
+        // when
+        section.updateUpStation(신도림역, 5);
+
+        // then
+        assertAll(
+            () -> assertThat(section.getUpStation()).isEqualTo(신도림역),
+            () -> assertThat(section.getDownStation()).isEqualTo(대림역),
+            () -> assertThat(section.getDistance()).isEqualTo(5)
+        );
+    }
+
+    @DisplayName("상행역이 포함된 구간이 추가되면 역의 상행이"
+        + " 추가되는 역으로 변경되며 거리도 차감된다")
+    @Test
+    void update_downStation_test() {
+        // given
+        Station 문래역 = new Station("문래");
+        Station 대림역 = new Station("대림");
+
+        Section section = new Section(new Line(), 문래역, 대림역, 10);
+        Station 신도림역 = new Station("신도림");
+
+        // when
+        section.updateDownStation(신도림역, 5);
+
+        // then
+        assertAll(
+            () -> assertThat(section.getUpStation()).isEqualTo(문래역),
+            () -> assertThat(section.getDownStation()).isEqualTo(신도림역),
+            () -> assertThat(section.getDistance()).isEqualTo(5)
         );
     }
 }
