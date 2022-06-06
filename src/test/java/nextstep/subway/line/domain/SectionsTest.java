a424ad3e691148df102b12c50998f313157a1487
minzzang <minjjang1117@gmail.com>
2022-06-06 18:22:41 +0900

    test(SectionsTest): 상행역에서 하행역까지 모든 역을 가져온다

---
+++ src/test/java/nextstep/subway/line/domain/SectionsTest.java
@@ -0,0 +1,44 @@
+package nextstep.subway.line.domain;
+
+import nextstep.subway.station.domain.Station;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class SectionsTest {
+    private Sections sections;
+
+    @BeforeEach
+    void setUp() {
+        sections = new Sections();
+
+        Station upStation = new Station("판교역");
+        Station downStation = new Station("정자역");
+
+        Station upStation2 = new Station("강남역");
+        Station downStation2 = new Station("판교역");
+
+        sections.add(new Section(upStation, downStation, 10));
+        sections.add(new Section(upStation2, downStation2, 10));
+    }
+
+    @Test
+    void 상행역에서_하행역까지_모든_역을_가져온다() {
+        // given
+        Station upStation = new Station("강남역");
+        Station downStation = new Station("정자역");
+        // when
+        List<Station> stations = sections.getStations();
+        // then
+        assertAll(
+                () -> assertThat(stations.get(0)).isEqualTo(upStation),
+                () -> assertThat(stations.get(stations.size() - 1)).isEqualTo(downStation)
+        );
+    }
+
+
+}
