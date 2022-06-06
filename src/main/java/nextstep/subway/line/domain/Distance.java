743c5d34e770f23872304514d4ad7fc016ac8272
heowc <heowc1992@gmail.com>
2022-06-05 19:49:58 -0700

    test(Distance): Distance 테스트 생성

--- src/main/java/nextstep/subway/line/domain/Distance.java
+++ src/main/java/nextstep/subway/line/domain/Distance.java
@@ -1,6 +1,7 @@
 package nextstep.subway.line.domain;
 
 import javax.persistence.Embeddable;
+import java.util.Objects;
 
 @Embeddable
 public class Distance {
@@ -12,5 +13,22 @@ public class Distance {
     }
 
     protected Distance() {
+    }
+
+    public void minus(Distance other) {
+
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        final Distance distance1 = (Distance) o;
+        return distance == distance1.distance;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(distance);
     }
 }
