459ec10299b55807df00f3815fb24ec5ec4acd93
heowc <heowc1992@gmail.com>
2022-06-05 19:50:59 -0700

    feat(Distance): Distance 구현

--- src/main/java/nextstep/subway/line/domain/Distance.java
+++ src/main/java/nextstep/subway/line/domain/Distance.java
@@ -16,7 +16,10 @@ public class Distance {
     }
 
     public void minus(Distance other) {
-
+        if (this.distance <= other.distance) {
+            throw new RuntimeException("역과 역 사이의 거리보다 좁은 거리를 입력해주세요");
+        }
+        this.distance -= other.distance;
     }
 
     @Override
