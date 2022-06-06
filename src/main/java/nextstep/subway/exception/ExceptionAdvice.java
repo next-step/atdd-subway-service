591d865840094bb371f09cbb669d92e54c9812c9
limwoobin <drogba02@naver.com>
2022-06-06 18:31:19 +0900

    refactor: DataIntegrityViolationException 예외에 대해 advice 로 처리하라

--- src/main/java/nextstep/subway/exception/ExceptionAdvice.java
+++ src/main/java/nextstep/subway/exception/ExceptionAdvice.java
@@ -1,5 +1,6 @@
 package nextstep.subway.exception;
 
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -10,5 +11,10 @@ public class ExceptionAdvice extends ResponseEntityExceptionHandler {
     @ExceptionHandler(SubwayCustomException.class)
     public ResponseEntity handleException(SubwayCustomException e) {
         return new ResponseEntity<>(e.getMessage() , e.getStatus());
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity handleIllegalArgsException(DataIntegrityViolationException e) {
+        return ResponseEntity.badRequest().build();
     }
 }
