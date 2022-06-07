a4a5bfe13e41fc703f186771957cda9d00a4b94e
minzzang <minjjang1117@gmail.com>
2022-06-07 23:41:22 +0900

    refactor(LineService): 삭제할 역이 포함된 구간을 삭제한다

--- src/test/java/nextstep/subway/line/domain/LineTest.java
+++ src/test/java/nextstep/subway/line/domain/LineTest.java
@@ -18,7 +18,11 @@ class LineTest {
         upStation = new Station("강남역");
         downStation = new Station("판교역");
 
+        Station upStation2 = new Station("판교역");
+        Station downStation2 = new Station("정자역");
+
         line = new Line("신분당선", "bg-red-600", upStation, downStation, 10);
+        line.addSection(new Section(upStation2, downStation2, 5));
     }
 
     @Test
@@ -26,7 +30,7 @@ class LineTest {
         // when
         List<Station> stations = line.getStations();
         // then
-        assertThat(stations).hasSize(2);
+        assertThat(stations).hasSize(3);
     }
 
     @Test
@@ -38,6 +42,6 @@ class LineTest {
         line.removeSection(station);
 
         // then
-        assertThat(line.getSections()).isEmpty();
+        assertThat(line.getSections()).hasSize(1);
     }
 }
