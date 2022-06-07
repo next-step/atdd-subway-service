b0b1cb4a7bfbab0eb47ffd2893f70c2b67b7ca11
heowc <heowc1992@gmail.com>
2022-06-07 10:19:23 +0900

    refactor(Line): 테스트 리팩토링
    - 누락된 테스트 추가(distance 관련)

--- src/test/java/nextstep/subway/line/domain/LineTest.java
+++ src/test/java/nextstep/subway/line/domain/LineTest.java
@@ -1,6 +1,7 @@
 package nextstep.subway.line.domain;
 
 import nextstep.subway.station.domain.Station;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -12,17 +13,21 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 @DisplayName("Line 클래스 테스트")
 class LineTest {
 
+    Station 양재시민의숲역;
+    Station 정자역;
 
+    Line 신분당선;
+
+    @BeforeEach
+    void setUp() {
+        양재시민의숲역 = new Station("양재시민의숲역");
+        정자역 = new Station("정자역");
+        신분당선 = new Line("신분당선", "bg-red-600", 양재시민의숲역, 정자역, 10);
+    }
 
     @DisplayName("Line을 만들고 기본적으로 포함된 역을 확인한다")
     @Test
     void createInstance() {
-        Station 양재시민의숲역 = new Station("양재시민의숲역");
-        Station 정자역 = new Station("정자역");
-
-        Line 신분당선 = new Line("신분당선", "bg-red-600", 양재시민의숲역, 정자역, 10);
-
-
         List<Station> stations = 신분당선.getStations();
         assertThat(stations).containsExactly(양재시민의숲역, 정자역);
     }
@@ -30,10 +35,6 @@ class LineTest {
     @DisplayName("상행역이 포함된 구간에 새로운 구간을 추가하여 정렬된 역을 확인한다")
     @Test
     void addSectionWithRelocateUpStation() {
-        Station 양재시민의숲역 = new Station("양재시민의숲역");
-        Station 정자역 = new Station("정자역");
-        Line 신분당선 = new Line("신분당선", "bg-red-600", 양재시민의숲역, 정자역, 10);
-
         Station 판교역 = new Station("판교역");
         신분당선.addSection(양재시민의숲역, 판교역, 5);
 
@@ -44,10 +45,6 @@ class LineTest {
     @DisplayName("하행역이 포함된 구간에 새로운 구간을 추가하여 정렬된 역을 확인한다")
     @Test
     void addSectionWithRelocateDownStation() {
-        Station 양재시민의숲역 = new Station("양재시민의숲역");
-        Station 정자역 = new Station("정자역");
-        Line 신분당선 = new Line("신분당선", "bg-red-600", 양재시민의숲역, 정자역, 10);
-
         Station 광교역 = new Station("광교역");
         신분당선.addSection(정자역, 광교역, 5);
 
@@ -55,16 +52,24 @@ class LineTest {
         assertThat(stations).containsExactly(양재시민의숲역, 정자역, 광교역);
     }
 
+    @DisplayName("추가하려는 구간이 기존 구간의 길이보다 커 예외가 발생")
+    @Test
+    void addSectionWithOverflowDistance() {
+        Station 판교역 = new Station("판교역");
+
+        assertThatThrownBy(() -> {
+            신분당선.addSection(양재시민의숲역, 판교역, 11);
+        }).isInstanceOf(RuntimeException.class)
+        .hasMessageContaining("역과 역 사이의 거리보다 좁은 거리를 입력해주세요");
+    }
+
     @DisplayName("상하행 어디에도 포함되지 않는 구간을 추가하려 하면 예외가 발생한다")
     @Test
     void addSectionWithNonMatch() {
-        Station 양재시민의숲역 = new Station("양재시민의숲역");
-        Station 정자역 = new Station("정자역");
-        Line 신분당선 = new Line("신분당선", "bg-red-600", 양재시민의숲역, 정자역, 10);
+        Station 광교역 = new Station("광교역");
+        Station 광교중악역 = new Station("광교중악역");
 
         assertThatThrownBy(() -> {
-            Station 광교역 = new Station("광교역");
-            Station 광교중악역 = new Station("광교중악역");
             신분당선.addSection(광교중악역, 광교역, 5);
         }).isInstanceOf(RuntimeException.class)
         .hasMessageContaining("등록할 수 없는 구간 입니다.");
@@ -73,10 +78,6 @@ class LineTest {
     @DisplayName("등록하려는 구간이 상하행 기존에 포함되어 있다면 예외가 발생한다")
     @Test
     void addSectionWithExist() {
-        Station 양재시민의숲역 = new Station("양재시민의숲역");
-        Station 정자역 = new Station("정자역");
-        Line 신분당선 = new Line("신분당선", "bg-red-600", 양재시민의숲역, 정자역, 10);
-
         assertThatThrownBy(() -> {
             신분당선.addSection(정자역, 양재시민의숲역, 5);
         }).isInstanceOf(RuntimeException.class)
