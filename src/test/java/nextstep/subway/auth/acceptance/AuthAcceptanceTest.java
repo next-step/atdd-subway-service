85b8f5254d7b2f0eff9ecff8aacdee21b33defac
KangSeongHyo <abc9023@naver.com>
2022-06-07 20:32:37 +0900

    test : 로그인 요청에 대한 인수테스트 추가

--- src/test/java/nextstep/subway/auth/acceptance/AuthAcceptanceTest.java
+++ src/test/java/nextstep/subway/auth/acceptance/AuthAcceptanceTest.java
@@ -1,10 +1,52 @@
 package nextstep.subway.auth.acceptance;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
 import nextstep.subway.AcceptanceTest;
+import nextstep.subway.auth.dto.TokenRequest;
+import nextstep.subway.auth.dto.TokenResponse;
+import nextstep.subway.member.MemberAcceptanceTest;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.MediaType;
 
-public class AuthAcceptanceTest extends AcceptanceTest {
+@DisplayName("인증 관련 인수테스트")
+class AuthAcceptanceTest extends AcceptanceTest {
+    private TokenRequest 로그인정보;
+
+    @BeforeEach
+    public void setUp(){
+        super.setUp();
+
+        //given
+        MemberAcceptanceTest.회원_생성을_요청(MemberAcceptanceTest.EMAIL, MemberAcceptanceTest.PASSWORD, MemberAcceptanceTest.AGE);
+        로그인정보 = new TokenRequest(MemberAcceptanceTest.EMAIL, MemberAcceptanceTest.PASSWORD);
+    }
+
+    /**
+     * Feature: 로그인 기능
+     *
+     *   Scenario: 로그인을 시도한다.
+     *     Given 회원 등록되어 있음
+     *     When 로그인 요청
+     *     Then 로그인 됨
+     * */
+    @Test
+    @DisplayName("로그인을 시도한다.")
+    void login(){
+
+        //when
+        TokenResponse tokenResponse = 로그인_요청(로그인정보);
+
+        //then
+        토큰_검증(tokenResponse);
+
+    }
+
 
     @DisplayName("Bearer Auth")
     @Test
@@ -19,6 +61,23 @@ public class AuthAcceptanceTest extends AcceptanceTest {
     @DisplayName("Bearer Auth 유효하지 않은 토큰")
     @Test
     void myInfoWithWrongBearerAuth() {
+    }
+
+    private void 토큰_검증(TokenResponse tokenResponse) {
+        assertThat(tokenResponse.getAccessToken()).isNotBlank();
+    }
+    private TokenResponse 로그인_요청(TokenRequest tokenRequest){
+
+        ExtractableResponse<Response> response = RestAssured
+                .given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .accept(MediaType.APPLICATION_JSON_VALUE)
+                .body(tokenRequest)
+                .when().post(" /login/token")
+                .then().log().all()
+                .extract();
+        return response.as(TokenResponse.class);
+
     }
 
 }
