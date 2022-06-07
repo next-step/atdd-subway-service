df66f9ce7e07125815954e65f5d874ee1e1436fb
ssonsh <chosunci@gmail.com>
2022-06-08 07:57:20 +0900

    feat(FavoriteController) : 즐겨찾기 삭제에 대한 API 생성
    - 즐겨찾기 삭제 처리의 결과는 NO_CONTENT

--- src/main/java/nextstep/subway/favorite/ui/FavoriteController.java
+++ src/main/java/nextstep/subway/favorite/ui/FavoriteController.java
@@ -8,7 +8,9 @@ import nextstep.subway.favorite.application.FavoriteService;
 import nextstep.subway.favorite.dto.FavoriteRequest;
 import nextstep.subway.favorite.dto.FavoriteResponse;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -34,5 +36,11 @@ public class FavoriteController {
     @GetMapping
     public ResponseEntity<List<FavoriteResponse>> showFavorites(@AuthenticationPrincipal LoginMember loginMember) {
         return ResponseEntity.ok().body(favoriteService.showFavorites(loginMember));
+    }
+
+    @DeleteMapping("/{favoriteId}")
+    public ResponseEntity<Void> removeFavorite(@AuthenticationPrincipal LoginMember loginMember,
+                                               @PathVariable Long favoriteId) {
+        return ResponseEntity.noContent().build();
     }
 }
