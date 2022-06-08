c71dcab6784f97f956b767158eb043d6f4059d94
heowc <heowc1992@gmail.com>
2022-06-09 07:49:24 +0900

    test(PathFinderTest): PathFinder 테스트 추가

--- src/main/java/nextstep/subway/path/domain/PathFinder.java
+++ src/main/java/nextstep/subway/path/domain/PathFinder.java
@@ -3,9 +3,11 @@ package nextstep.subway.path.domain;
 import nextstep.subway.line.domain.Line;
 import nextstep.subway.station.domain.Station;
 
+import java.util.List;
+
 public class PathFinder {
 
-    public PathFinder(Line line) {
+    public PathFinder(List<Line> lines) {
 
     }
 
