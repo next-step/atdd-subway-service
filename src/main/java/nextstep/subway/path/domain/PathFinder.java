7c322126bf977b19ac9490b71016b3c2139759fd
heowc <heowc1992@gmail.com>
2022-06-09 06:54:12 +0900

    feat(PathFinder): PathFinder 생성

---
+++ src/main/java/nextstep/subway/path/domain/PathFinder.java
@@ -0,0 +1,15 @@
+package nextstep.subway.path.domain;
+
+import nextstep.subway.line.domain.Line;
+import nextstep.subway.station.domain.Station;
+
+public class PathFinder {
+
+    public PathFinder(Line line) {
+
+    }
+
+    public Path findShortestPath(Station source, Station target) {
+        return null;
+    }
+}
