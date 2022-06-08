c4777ac10a28f956dafcb8487c7b2701dc35f8b8
KangSeongHyo <abc9023@naver.com>
2022-06-08 20:35:07 +0900

    feat : Favorite 엔티티 추가

---
+++ src/main/java/nextstep/subway/favorite/domain/Favorite.java
@@ -0,0 +1,32 @@
+package nextstep.subway.favorite.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.ForeignKey;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import nextstep.subway.BaseEntity;
+import nextstep.subway.station.domain.Station;
+
+@Entity
+public class Favorite extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "source_id", foreignKey = @ForeignKey(name = "fk_favorite_to_source"))
+    private Station source;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "target_id", foreignKey = @ForeignKey(name = "fk_favorite_to_target"))
+    private Station target;
+
+    protected Favorite() {
+    }
+
+}
