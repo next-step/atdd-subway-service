f05803eea7d65cfe7c420cf184106b6f391db5f8
KangSeongHyo <abc9023@naver.com>
2022-06-08 21:33:34 +0900

    feat : 즐겨찾기 Service 클래스 추가 및 생성 기능 추가

---
+++ src/main/java/nextstep/subway/favorite/application/FavoriteService.java
@@ -0,0 +1,33 @@
+package nextstep.subway.favorite.application;
+
+import nextstep.subway.auth.domain.LoginMember;
+import nextstep.subway.favorite.domain.Favorite;
+import nextstep.subway.favorite.domain.FavoriteRepository;
+import nextstep.subway.member.application.MemberService;
+import nextstep.subway.member.domain.Member;
+import nextstep.subway.station.application.StationService;
+import nextstep.subway.station.domain.Station;
+import org.springframework.stereotype.Service;
+
+@Service
+public class FavoriteService {
+
+    private final FavoriteRepository favoriteRepository;
+    private final MemberService memberService;
+    private final StationService stationService;
+
+    public FavoriteService(FavoriteRepository favoriteRepository,
+                           MemberService memberService,
+                           StationService stationService) {
+        this.favoriteRepository = favoriteRepository;
+        this.memberService = memberService;
+        this.stationService = stationService;
+    }
+
+    public void createFavorite(LoginMember loginMember, Long sourceId, Long targetId){
+        Member member = memberService.findMemberById(loginMember.getId());
+        Station source = stationService.findStationById(sourceId);
+        Station target = stationService.findStationById(targetId);
+        favoriteRepository.save(new Favorite(source,target,member));
+    }
+}
