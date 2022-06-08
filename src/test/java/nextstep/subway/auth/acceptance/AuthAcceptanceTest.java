2c7672e373dfe673fe35ce63bd5a5e4824f92d36
KangSeongHyo <abc9023@naver.com>
2022-06-08 18:06:04 +0900

    test : Bearer Auth 로그인 실패 및 Bearer Auth 유효하지 않은 토큰 테스트 추가

--- src/test/java/nextstep/subway/auth/acceptance/AuthAcceptanceTest.java
+++ src/test/java/nextstep/subway/auth/acceptance/AuthAcceptanceTest.java
@@ -13,6 +13,7 @@ import nextstep.subway.member.dto.MemberResponse;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
 @DisplayName("인증 관련 인수테스트")
@@ -22,6 +23,7 @@ class AuthAcceptanceTest extends AcceptanceTest {
     private static final int AGE = 15;
 
     private TokenRequest 로그인정보;
+    private TokenRequest 미등록정보;
 
     @BeforeEach
     public void setUp(){
@@ -30,6 +32,7 @@ class AuthAcceptanceTest extends AcceptanceTest {
         //given
         MemberAcceptanceTest.회원_생성을_요청(EMAIL, PASSWORD, AGE);
         로그인정보 = new TokenRequest(EMAIL, PASSWORD);
+        미등록정보 = new TokenRequest("test@email.com", "1234");
     }
 
     /**
@@ -46,7 +49,7 @@ class AuthAcceptanceTest extends AcceptanceTest {
     void login(){
 
         //when
-        TokenResponse tokenResponse = 로그인_요청(로그인정보);
+        TokenResponse tokenResponse = 로그인_요청(로그인정보).as(TokenResponse.class);
 
         //then
         토큰_검증(tokenResponse);
@@ -55,14 +58,14 @@ class AuthAcceptanceTest extends AcceptanceTest {
 
 
     /**
-     * Feature: 나의 정보 조회기능
-     *
+     * Feature: 엑세스토큰 인증 기능
+     * 
      *   Background
      *     Given 회원 등록되어 있음
      *
-     *   Scenario: 로그인 정보를 확인한다.
-     *     given 로그인 되어있음
-     *     When  나의정보를 요청하면
+     *   Scenario: 나의정보를 확인한다.
+     *     Given 로그인 되어있음 (엑세스토큰 발급)
+     *     When  나의정보를 요청하면 (with 엑세스토큰)
      *     Then  나의정보가 응답됨
      * */
     @DisplayName("Bearer Auth")
@@ -70,23 +73,55 @@ class AuthAcceptanceTest extends AcceptanceTest {
     void myInfoWithBearerAuth() {
 
         //given
-        TokenResponse tokenResponse = 로그인_요청(로그인정보);
+        TokenResponse tokenResponse = 로그인_요청(로그인정보).as(TokenResponse.class);
 
         //when
-        MemberResponse response = 나의정보_요청(tokenResponse.getAccessToken());
+        MemberResponse response = 나의정보_요청(tokenResponse.getAccessToken()).as(MemberResponse.class);
 
         //then
         나의정보_조회됨(response);
     }
 
+
+    /**
+     * Feature: 인증토큰
+     **
+     *   Scenario: 로그인 실패
+     *     When  회원 등록되지 않는 로그인 요청하면
+     *     Then  로그인 실패됨
+     * */
     @DisplayName("Bearer Auth 로그인 실패")
     @Test
     void myInfoWithBadBearerAuth() {
+
+        //when
+        ExtractableResponse<Response> response = 로그인_요청(미등록정보);
+
+        //then
+        로그인_실패됨(response);
     }
 
+
+
+    /**
+     * Feature: 나의 정보 조회기능 (인증불가 토근)
+     *
+     *   Scenario: 나의 정보를 조회한다.(인증불가 토큰)
+     *     Given 인증불가 토근으로
+     *     When  나의정보를 요청하면
+     *     Then  나의정보 조회 실패됨
+     * */
     @DisplayName("Bearer Auth 유효하지 않은 토큰")
     @Test
     void myInfoWithWrongBearerAuth() {
+        //given
+        String invalidToken = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJlbWFpbEBlbWFpbC5jb20iLCJpYXQiOjE2NTQ2NzQ0NzcsImV4cCI6MTY1NDY3ODA3N30.JbtFjzck0lLRGTKNR7JvD71k_YJhPwBriHHrB2";
+
+        //when
+        ExtractableResponse<Response> response = 나의정보_요청(invalidToken);
+
+        //then
+        나의정보_조회_실패됨(response);
     }
 
     private void 토큰_검증(TokenResponse tokenResponse) {
@@ -99,9 +134,17 @@ class AuthAcceptanceTest extends AcceptanceTest {
         assertThat(response.getAge()).isEqualTo(AGE);
     }
 
-    private TokenResponse 로그인_요청(TokenRequest tokenRequest){
+    private void 나의정보_조회_실패됨(ExtractableResponse<Response> response) {
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+    }
 
-        ExtractableResponse<Response> response = RestAssured
+    private void 로그인_실패됨(ExtractableResponse<Response> response) {
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+    }
+
+    private ExtractableResponse<Response> 로그인_요청(TokenRequest tokenRequest){
+
+        return RestAssured
                 .given().log().all()
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
                 .accept(MediaType.APPLICATION_JSON_VALUE)
@@ -109,13 +152,10 @@ class AuthAcceptanceTest extends AcceptanceTest {
                 .when().post(" /login/token")
                 .then().log().all()
                 .extract();
-        return response.as(TokenResponse.class);
-
     }
 
-    private MemberResponse 나의정보_요청(String accessToken){
-
-        ExtractableResponse<Response> response = RestAssured
+    private ExtractableResponse<Response> 나의정보_요청(String accessToken){
+        return RestAssured
                 .given().log().all()
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
                 .accept(MediaType.APPLICATION_JSON_VALUE)
@@ -123,7 +163,6 @@ class AuthAcceptanceTest extends AcceptanceTest {
                 .when().get("/members/me")
                 .then().log().all()
                 .extract();
-        return response.as(MemberResponse.class);
     }
 
 }
