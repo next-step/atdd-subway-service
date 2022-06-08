76bb6bdbff9594cfeb7082e120ae1b2e496b6f83
ssonsh <chosunci@gmail.com>
2022-06-08 12:46:40 +0900

    test(FavoriteServiceTest) : 즐겨찾기 목록 조회에 대한 테스트 코드 작성

--- src/test/java/nextstep/subway/favorite/application/FavoriteServiceTest.java
+++ src/test/java/nextstep/subway/favorite/application/FavoriteServiceTest.java
@@ -4,10 +4,14 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doReturn;
 
+import java.util.List;
 import nextstep.subway.auth.domain.LoginMember;
 import nextstep.subway.favorite.domain.Favorite;
 import nextstep.subway.favorite.dto.FavoriteRequest;
 import nextstep.subway.favorite.dto.FavoriteResponse;
+import nextstep.subway.member.domain.Member;
+import nextstep.subway.station.domain.Station;
+import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -32,5 +36,20 @@ class FavoriteServiceTest {
 
         // then
         assertThat(response).isNotNull();
+    }
+
+    @DisplayName("즐겨찾기 목록 조회 Mockito 활용")
+    @Test
+    void showFavorite() {
+        // given
+        Favorite favorite = Favorite.of(Station.of(1L, "강남역"), Station.of(2L, "판교역"), new Member());
+        List<FavoriteResponse> favoriteResponses = Lists.newArrayList(FavoriteResponse.from(favorite));
+        doReturn(favoriteResponses).when(favoriteService).showFavorites(any(LoginMember.class));
+
+        // when
+        List<FavoriteResponse> responses = favoriteService.showFavorites(new LoginMember());
+
+        // then
+        assertThat(responses).hasSize(1);
     }
 }
