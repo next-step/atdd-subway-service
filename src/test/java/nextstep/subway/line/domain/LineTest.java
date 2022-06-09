406cdc86651cae2c60e2fa8132a436d3cece6ca5
jhsong2580 <thdwmdgns@naver.com>
2022-06-10 00:20:03 +0900

    test(refactor): 불필요한 상속 제거

--- src/test/java/nextstep/subway/line/domain/LineTest.java
+++ src/test/java/nextstep/subway/line/domain/LineTest.java
@@ -4,15 +4,23 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.Arrays;
 import java.util.List;
-import nextstep.subway.DomainTest;
 import nextstep.subway.station.domain.Station;
+import nextstep.subway.station.domain.StationRepository;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 
-public class LineTest extends DomainTest {
+@DataJpaTest
+public class LineTest {
 
+    @Autowired
+    public LineRepository lineRepository;
+
+    @Autowired
+    public StationRepository stationRepository;
     Station 강남역;
     Station 정자역;
     Line 신분당선;
@@ -24,9 +32,7 @@ public class LineTest extends DomainTest {
         신분당선 = 노선_저장하기("신분당선", "red", 강남역, 정자역, 10);
     }
 
-    /**
-     * background given Line에 정자역 ~ 강남역 을 저장하고 when station순서를 출력할때 then 순서대로 출력이된다.
-     */
+
     @Test
     public void 노선에_포함된_역_순서대로_받기() {
         //when
@@ -36,12 +42,7 @@ public class LineTest extends DomainTest {
         역_목록_순서_체크(역_목록, Arrays.asList("강남역", "정자역"));
     }
 
-    /**
-     * background
-        * given Line에 정자역 ~ 강남역 을 저장하고
-     * when 양재역 ~ 강남역을 추가하고 목록 조회시
-     * then 순서대로 출력이된다.
-     */
+
     @Test
     public void 노선에_역_추가하기_후방() {
         //given
@@ -55,12 +56,6 @@ public class LineTest extends DomainTest {
         역_목록_순서_체크(역_목록, Arrays.asList("강남역", "양재역", "정자역"));
     }
 
-    /**
-     * background
-        * given Line에 정자역 ~ 강남역 을 저장하고
-     * when 정자역 ~ 양재역을 추가하고 목록 조회시
-     * then 순서대로 출력이된다.
-     */
     @Test
     public void 노선에_역_추가하기_전방() {
         //given
@@ -74,12 +69,7 @@ public class LineTest extends DomainTest {
         역_목록_순서_체크(역_목록, Arrays.asList("강남역", "양재역", "정자역"));
     }
 
-    /**
-     * background
-        * given Line에 정자역 ~ 강남역 을 저장하고
-     * when 광교역 ~ 정자역을 추가하고 목록 조회시
-     * then 순서대로 출력이된다.
-     */
+
     @Test
     public void 노선에_역_추가하기_노선의_시작() {
         //given
@@ -93,12 +83,7 @@ public class LineTest extends DomainTest {
         역_목록_순서_체크(역_목록, Arrays.asList("강남역", "정자역", "광교역"));
     }
 
-    /**
-     * background
-        * given Line에 정자역 ~ 강남역 을 저장하고
-     * when 강남역 ~ 선정릉역을 추가하고 목록 조회시
-     * then 순서대로 출력이된다.
-     */
+
     @Test
     public void 노선에_역_추가하기_노선의_끝() {
         //given
@@ -109,16 +94,10 @@ public class LineTest extends DomainTest {
         List<Station> 역_목록 = 신분당선.getStations();
 
         //then
-        역_목록_순서_체크(역_목록, Arrays.asList("선정릉역","강남역", "정자역"));
+        역_목록_순서_체크(역_목록, Arrays.asList("선정릉역", "강남역", "정자역"));
     }
 
-    /**
-     * background
-        * given Line에 정자역 ~ 강남역 을 저장하고
-     * given 강남역 ~ 선정릉역을 추가 line에 추가한 뒤
-     * when 강남역 삭제시
-     * then 정상 삭제가 되고 순서가 정상적으로 출력된다.
-     */
+
     @Test
     public void 노선_가운데_역_삭제하기() {
         //given
@@ -133,13 +112,7 @@ public class LineTest extends DomainTest {
         역_목록_순서_체크(역_목록, Arrays.asList("선정릉역", "정자역"));
     }
 
-    /**
-     * background
-        * given Line에 정자역 ~ 강남역 을 저장하고
-     * given 강남역 ~ 선정릉역을 추가 line에 추가한 뒤
-     * when 정자역 삭제시
-     * then 정상 삭제가 되고 순서가 정상적으로 출력된다.
-     */
+
     @Test
     public void 노선_시작역_삭제하기() {
         //given
@@ -154,13 +127,7 @@ public class LineTest extends DomainTest {
         역_목록_순서_체크(역_목록, Arrays.asList("선정릉역", "강남역"));
     }
 
-    /**
-     * background
-        * given Line에 정자역 ~ 강남역 을 저장하고
-     * given 강남역 ~ 선정릉역을 추가 line에 추가한 뒤
-     * when 선정릉역 삭제시
-     * then 정상 삭제가 되고 순서가 정상적으로 출력된다.
-     */
+
     @Test
     public void 노선_마지막역_삭제하기() {
         //given
@@ -175,15 +142,9 @@ public class LineTest extends DomainTest {
         역_목록_순서_체크(역_목록, Arrays.asList("강남역", "정자역"));
     }
 
-    /**
-     * background
-        * given Line에 정자역 ~ 강남역 을 저장하고
-     * given 강남역 ~ 선정릉역을 추가 line에 추가한 뒤
-     * when 동탄역 삭제시
-     * then 삭제가 불가능하다.
-     */
+
     @Test
-    public void 노선_없는역_삭제하기_에러발생 (){
+    public void 노선_없는역_삭제하기_에러발생() {
         //given
         Station 선정릉역 = 지하철역_저장하기("선정릉역");
         Station 동탄역 = 지하철역_저장하기("동탄역");
@@ -193,14 +154,29 @@ public class LineTest extends DomainTest {
         assertThatThrownBy(() -> 신분당선.removeStation(동탄역)).isInstanceOf(RuntimeException.class);
     }
 
-    /**
-     * background
-     * given Line에 정자역 ~ 강남역 을 저장하고
-     * when 정자역 삭제시
-     * then 삭제가 불가능하다.
-     */
+
     @Test
-    public void 노선_단일구간_삭제하기_에러발생 (){
+    public void 노선_단일구간_삭제하기_에러발생() {
         assertThatThrownBy(() -> 신분당선.removeStation(강남역)).isInstanceOf(RuntimeException.class);
+    }
+
+
+    private Station 지하철역_저장하기(String 지하철역이름) {
+        Station 저장할_지하철역 = new Station(지하철역이름);
+        stationRepository.save(저장할_지하철역);
+        return 저장할_지하철역;
+    }
+
+    private Line 노선_저장하기(String 노선이름, String 노선색깔, Station 상향_종착지_지하철역, Station 하향_종착지_지하철역,
+        int 거리) {
+        Line 저장할_노선 = new Line(노선이름, 노선색깔, 상향_종착지_지하철역, 하향_종착지_지하철역, 거리);
+        lineRepository.save(저장할_노선);
+        return 저장할_노선;
+    }
+
+    private void 역_목록_순서_체크(List<Station> 실제_역목록_순서, List<String> 기대하는_역목록_이름) {
+        Assertions.assertThat(실제_역목록_순서)
+            .extracting("name")
+            .containsExactlyElementsOf(기대하는_역목록_이름);
     }
 }
