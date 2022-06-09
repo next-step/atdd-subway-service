9e78ad6ffeaac68eaa542a9e4728bd0e6d1a1d44
Celina <luv.mins99@gmail.com>
2022-06-09 11:20:58 +0900

    feat(Distance): 거리 원시값 포장

---
+++ src/main/java/nextstep/subway/line/domain/Distance.java
@@ -0,0 +1,62 @@
+package nextstep.subway.line.domain;
+
+import java.util.Objects;
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+@Embeddable
+public class Distance {
+    private static final long MIN_DISTANCE = 1;
+    @Column
+    private int distance;
+
+    protected Distance() {
+    }
+
+    public Distance(int distance) {
+        invalidInputCheck(distance);
+        this.distance = distance;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+
+    public Distance addDistance(Distance targetDistance) {
+        return new Distance(this.distance + targetDistance.distance);
+    }
+
+    public Distance subtractDistance(Distance targetDistance) {
+        validDistanceCheck(targetDistance);
+        return new Distance(this.distance - targetDistance.distance);
+    }
+
+    private void invalidInputCheck(long distance) {
+        if (distance < MIN_DISTANCE) {
+            throw new IllegalArgumentException("구간은 최소 " + MIN_DISTANCE + " 이상의 값이어야 합니다.");
+        }
+    }
+
+    private void validDistanceCheck(Distance newDistance) {
+        if (this.distance <= newDistance.distance) {
+            throw new IllegalArgumentException("기존 역 사이 길이보다 크거나 같으면 추가할 수 없음");
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Distance distance1 = (Distance) o;
+        return distance == distance1.distance;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(distance);
+    }
+}
