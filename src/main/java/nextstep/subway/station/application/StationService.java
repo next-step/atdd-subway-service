473831cc678a79bf5968a881ef0f90544afc764b
heowc <heowc1992@gmail.com>
2022-06-09 09:16:13 +0900

    fix(StationService): Station이 없는 경우 예외 처리 변경

--- src/main/java/nextstep/subway/station/application/StationService.java
+++ src/main/java/nextstep/subway/station/application/StationService.java
@@ -1,5 +1,6 @@
 package nextstep.subway.station.application;
 
+import nextstep.subway.station.domain.NotFoundStationException;
 import nextstep.subway.station.domain.Station;
 import nextstep.subway.station.domain.StationRepository;
 import nextstep.subway.station.dto.StationRequest;
@@ -36,10 +37,10 @@ public class StationService {
     }
 
     public Station findStationById(Long id) {
-        return stationRepository.findById(id).orElseThrow(RuntimeException::new);
+        return stationRepository.findById(id).orElseThrow(NotFoundStationException::new);
     }
 
     public Station findById(Long id) {
-        return stationRepository.findById(id).orElseThrow(RuntimeException::new);
+        return stationRepository.findById(id).orElseThrow(NotFoundStationException::new);
     }
 }
