9a71ff7c4e2b4329adb42da78a6a8e25f9778d93
heowc <heowc1992@gmail.com>
2022-06-09 09:17:35 +0900

    feat(PathService): 최단 경로 조회 구현

---
+++ src/main/java/nextstep/subway/path/application/PathService.java
@@ -0,0 +1,43 @@
+package nextstep.subway.path.application;
+
+import nextstep.subway.line.application.LineService;
+import nextstep.subway.line.domain.Line;
+import nextstep.subway.path.domain.Path;
+import nextstep.subway.path.domain.PathFinder;
+import nextstep.subway.path.dto.PathResponse;
+import nextstep.subway.station.application.StationService;
+import nextstep.subway.station.domain.Station;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static java.util.Objects.requireNonNull;
+
+@Service
+public class PathService {
+
+    private final LineService lineService;
+    private final StationService stationService;
+
+    public PathService(LineService lineService, StationService stationService) {
+        this.lineService = lineService;
+        this.stationService = stationService;
+    }
+
+    @Transactional(readOnly = true)
+    public PathResponse findShortestPath(Long sourceStationId, Long targetStationId) {
+        requireNonNull(sourceStationId, "sourceStationId");
+        requireNonNull(targetStationId, "targetStationId");
+        PathFinder pathFinder = createPathFinder();
+        Station sourceStation = stationService.findStationById(sourceStationId);
+        Station targetStation = stationService.findStationById(targetStationId);
+        Path shortestPath = pathFinder.findShortestPath(sourceStation, targetStation);
+        return new PathResponse(shortestPath);
+    }
+
+    private PathFinder createPathFinder() {
+        List<Line> allLines = lineService.findAllLines();
+        return new PathFinder(allLines);
+    }
+}
--- src/main/java/nextstep/subway/path/dto/PathResponse.java
+++ src/main/java/nextstep/subway/path/dto/PathResponse.java
@@ -1,17 +1,28 @@
 package nextstep.subway.path.dto;
 
+import nextstep.subway.path.domain.Path;
 import nextstep.subway.station.dto.StationResponse;
 
-import java.util.Collections;
 import java.util.List;
 
 public class PathResponse {
 
+    private List<StationResponse> stations;
+    private int distance;
+
+    protected PathResponse() {
+    }
+
+    public PathResponse(Path path) {
+        this.stations = StationResponse.of(path.getStations());
+        this.distance = path.getDistance();
+    }
+
     public int getDistance() {
-        return 0;
+        return distance;
     }
 
     public List<StationResponse> getStations() {
-        return Collections.emptyList();
+        return stations;
     }
 }
