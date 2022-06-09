a89946f8f208f0211ca998b903aea4dc67de96af
ssonsh <chosunci@gmail.com>
2022-06-09 20:27:40 +0900

    feat(FareSectionType) : 이동 거리의 FareSectionType 구간에 따른 요금 정책 타입 정의, 정책 타입에 대한 테스트 코드 작성
    - distance 거리에 따른 요금타입을 반환할 수 있다.

---
+++ src/main/java/nextstep/subway/fare/FareSectionType.java
@@ -0,0 +1,25 @@
+package nextstep.subway.fare;
+
+public enum FareSectionType {
+    BASIC(0, 0),
+    SECTION_10_TO_50(10, 5),
+    SECTION_OVER_50(50, 8);
+
+    private int minDistance;
+    private int additionalFareUnit;
+
+    FareSectionType(int minDistance, int additionalFareUnit) {
+        this.minDistance = minDistance;
+        this.additionalFareUnit = additionalFareUnit;
+    }
+
+    public static FareSectionType findTypeByDistance(int distance) {
+        if (SECTION_OVER_50.minDistance < distance) {
+            return SECTION_OVER_50;
+        }
+        if (distance > SECTION_10_TO_50.minDistance) {
+            return SECTION_10_TO_50;
+        }
+        return BASIC;
+    }
+}
---
+++ src/test/java/nextstep/subway/fare/FareSectionTypeTest.java
@@ -0,0 +1,43 @@
+package nextstep.subway.fare;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class FareSectionTypeTest {
+
+    @DisplayName("10km 이내 distance 의 FareSectionType은 BASIC 이다.")
+    @ParameterizedTest
+    @ValueSource(ints = {1, 5, 9, 10})
+    void findFareSectionType01(int distance) {
+        // given && when
+        FareSectionType fareSectionType = FareSectionType.findTypeByDistance(distance);
+
+        // then
+        assertThat(fareSectionType).isEqualTo(FareSectionType.BASIC);
+    }
+
+    @DisplayName("10km 초과 ~ 50km 까지 사이의 distance 의 FareSectionType은 SECTION_10_TO_50 이다.")
+    @ParameterizedTest
+    @ValueSource(ints = {11, 20, 30, 40, 50})
+    void findFareSectionType02(int distance) {
+        // given && when
+        FareSectionType fareSectionType = FareSectionType.findTypeByDistance(distance);
+
+        // then
+        assertThat(fareSectionType).isEqualTo(FareSectionType.SECTION_10_TO_50);
+    }
+
+    @DisplayName("50km 초과한 distance 의 FareSectionType은 SECTION_OVER_50 이다.")
+    @ParameterizedTest
+    @ValueSource(ints = {51, 60, 100})
+    void findFareSectionType03(int distance) {
+        // given && when
+        FareSectionType fareSectionType = FareSectionType.findTypeByDistance(distance);
+
+        // then
+        assertThat(fareSectionType).isEqualTo(FareSectionType.SECTION_OVER_50);
+    }
+}
