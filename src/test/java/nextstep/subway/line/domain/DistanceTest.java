0a062c47a760fe66323ab7aea7f6b8c22058d17e
minzzang <minjjang1117@gmail.com>
2022-06-10 01:28:12 +0900

    test: 구간의 길이를 계산한다

--- src/test/java/nextstep/subway/line/domain/DistanceTest.java
+++ src/test/java/nextstep/subway/line/domain/DistanceTest.java
@@ -1,8 +1,10 @@
 package nextstep.subway.line.domain;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class DistanceTest {
@@ -14,5 +16,31 @@ class DistanceTest {
         assertThatThrownBy(() ->
                 new Distance(distance)
         ).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 구간의_길이를_뺀다() {
+        // given
+        Distance target = new Distance(10);
+        Distance distance = new Distance(7);
+
+        // when
+        Distance result = target.minus(distance);
+
+        // then
+        assertThat(result.value()).isEqualTo(3);
+    }
+
+    @Test
+    void 구간의_길이를_더한다() {
+        // given
+        Distance target = new Distance(10);
+        Distance distance = new Distance(10);
+
+        // when
+        Distance result = target.plus(distance);
+
+        // then
+        assertThat(result.value()).isEqualTo(20);
     }
 }
