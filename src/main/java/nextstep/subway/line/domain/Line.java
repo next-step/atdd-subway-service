0acac72c630e2a977abe827e95b0b0420307e68e
Celina <luv.mins99@gmail.com>
2022-06-10 02:25:44 +0900

    refactor(Line): 미사용 메서드 삭제

--- src/main/java/nextstep/subway/line/domain/Line.java
+++ src/main/java/nextstep/subway/line/domain/Line.java
@@ -52,10 +52,6 @@ public class Line extends BaseEntity {
         return color;
     }
 
-    public List<Section> getSections() {
-        return sections.getSections();
-    }
-
     public List<Station> getStations() {
         return sections.getStations();
     }
