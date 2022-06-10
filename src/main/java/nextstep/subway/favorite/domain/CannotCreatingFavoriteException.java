ac7c22c3e2691e656160364fafe3775422992c82
heowc <heowc1992@gmail.com>
2022-06-10 12:37:33 +0900

    test(FavoriteTest): Favorite 도메인 테스트 추가

---
+++ src/main/java/nextstep/subway/favorite/domain/CannotCreatingFavoriteException.java
@@ -0,0 +1,12 @@
+package nextstep.subway.favorite.domain;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class CannotCreatingFavoriteException extends RuntimeException {
+
+    public CannotCreatingFavoriteException(String message) {
+        super(message);
+    }
+}
