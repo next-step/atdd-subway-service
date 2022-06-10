22b83253f6a08b68ca9f6bbc47cccec46a9325c9
otakjong <tj3828@naver.com>
2022-06-11 01:08:33 +0900

    test: RestAssured api 호출 공통 메서드 분리

--- src/test/java/nextstep/subway/AcceptanceTest.java
+++ src/test/java/nextstep/subway/AcceptanceTest.java
@@ -1,11 +1,14 @@
 package nextstep.subway;
 
 import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
 import nextstep.subway.utils.DatabaseCleanup;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.MediaType;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class AcceptanceTest {
@@ -19,5 +22,42 @@ public class AcceptanceTest {
     public void setUp() {
         RestAssured.port = port;
         databaseCleanup.execute();
+    }
+
+    public static ExtractableResponse<Response> get(String path, Object... pathParams) {
+        return RestAssured
+            .given().log().all()
+            .accept(MediaType.APPLICATION_JSON_VALUE)
+            .when().get(path, pathParams)
+            .then().log().all()
+            .extract();
+    }
+
+    public static ExtractableResponse<Response> post(Object requestBody, String path, Object... pathParams) {
+        return RestAssured
+            .given().log().all()
+            .contentType(MediaType.APPLICATION_JSON_VALUE)
+            .body(requestBody)
+            .when().post(path, pathParams)
+            .then().log().all()
+            .extract();
+    }
+
+    public static ExtractableResponse<Response> put(Object requestBody, String path, Object... pathParams) {
+        return RestAssured
+            .given().log().all()
+            .contentType(MediaType.APPLICATION_JSON_VALUE)
+            .body(requestBody)
+            .when().put(path, pathParams)
+            .then().log().all()
+            .extract();
+    }
+
+    public static ExtractableResponse<Response> delete(String path, Object... pathParams) {
+        return RestAssured
+            .given().log().all()
+            .when().delete(path, pathParams)
+            .then().log().all()
+            .extract();
     }
 }
