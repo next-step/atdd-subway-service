be80c4097eb95a61702b4af435a54f2b9153888f
heowc <heowc1992@gmail.com>
2022-06-10 11:56:18 +0900

    feat(Favorite): 즐겨찾기 도메인 구현

--- src/main/java/nextstep/subway/favorite/domain/Favorite.java
+++ src/main/java/nextstep/subway/favorite/domain/Favorite.java
@@ -1,32 +1,42 @@
 package nextstep.subway.favorite.domain;
 
+import nextstep.subway.BaseEntity;
 import nextstep.subway.member.domain.Member;
 import nextstep.subway.station.domain.Station;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
-public class Favorite {
+public class Favorite extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @ManyToOne
+    private Member member;
+
+    @ManyToOne
+    private Station source;
+
+    @ManyToOne
+    private Station target;
+
     public Favorite(Member member, Station sourceStation, Station targetStation) {
+        this.member = member;
+        this.source = sourceStation;
+        this.target = targetStation;
     }
 
     public Member getMember() {
-        return null;
+        return member;
     }
 
     public Station getSource() {
-        return null;
+        return source;
     }
 
     public Station getTarget() {
-        return null;
+        return target;
     }
 }
