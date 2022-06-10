e6bb08552de35dd9f4570963a2b5fe003b5fd0fd
otakjong <tj3828@naver.com>
2022-06-11 01:29:53 +0900

    test: Distance 단위 테스트 작성

--- src/main/java/nextstep/subway/line/domain/Distance.java
+++ src/main/java/nextstep/subway/line/domain/Distance.java
@@ -53,4 +53,21 @@ public class Distance {
         return Objects.hash(distance);
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Distance distance1 = (Distance) o;
+        return distance == distance1.distance;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(distance);
+    }
+
 }
