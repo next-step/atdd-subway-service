6b2c3885e604f49fe7f3607662e55bcc12f4fcc8
kwonyongil <ksgkms23@likelion.org>
2022-06-10 20:00:38 +0900

    feat(subway) 최단경로 조회 인수테스트 작성 추가

---
+++ src/main/java/nextstep/subway/path/ui/PathController.java
@@ -0,0 +1,18 @@
+package nextstep.subway.path.ui;
+
+import nextstep.subway.path.dto.PathResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/paths")
+public class PathController {
+    @GetMapping
+    public ResponseEntity<PathResponse> findShortestPath(@RequestParam Long source, @RequestParam Long target) {
+        PathResponse pathResponse = new PathResponse();
+        return ResponseEntity.ok().body(pathResponse);
+    }
+}
---
+++ src/test/java/nextstep/subway/path/acceptance/PathAcceptanceTest.java
@@ -0,0 +1,71 @@
+package nextstep.subway.path.acceptance;
+
+import static nextstep.subway.line.acceptance.LineRestAssured.지하철_노선_등록되어_있음;
+import static nextstep.subway.line.acceptance.LineRestAssured.지하철_노선_목록_조회_요청;
+import static nextstep.subway.line.acceptance.LineSectionRestAssured.지하철_노선에_지하철역_등록_요청;
+import static nextstep.subway.path.acceptance.PathRestAssured.지하철_경로_최단거리_요청;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import nextstep.subway.AcceptanceTest;
+import nextstep.subway.line.dto.LineRequest;
+import nextstep.subway.line.dto.LineResponse;
+import nextstep.subway.station.StationAcceptanceTest;
+import nextstep.subway.station.dto.StationResponse;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+
+
+@DisplayName("지하철 경로 조회")
+class PathAcceptanceTest extends AcceptanceTest {
+    private LineResponse 신분당선;
+    private LineResponse 이호선;
+    private LineResponse 삼호선;
+    private StationResponse 강남역;
+    private StationResponse 양재역;
+    private StationResponse 교대역;
+    private StationResponse 남부터미널역;
+
+    /**
+     * 교대역    --- *2호선* (10) ---   강남역
+     * |                             |
+     * *3호선* (3)                  *신분당선* (10)
+     * |                             |
+     * 남부터미널역  --- *3호선* (2) --- 양재
+     */
+    @BeforeEach
+    public void setUp() {
+        super.setUp();
+
+        강남역 = StationAcceptanceTest.지하철역_등록되어_있음("강남역").as(StationResponse.class);
+        양재역 = StationAcceptanceTest.지하철역_등록되어_있음("양재역").as(StationResponse.class);
+        교대역 = StationAcceptanceTest.지하철역_등록되어_있음("교대역").as(StationResponse.class);
+        남부터미널역 = StationAcceptanceTest.지하철역_등록되어_있음("남부터미널역").as(StationResponse.class);
+
+        신분당선 = 지하철_노선_등록되어_있음(new LineRequest("신분당선", "bg-red-600", 강남역.getId(), 양재역.getId(), 10)).as(LineResponse.class);
+        이호선 = 지하철_노선_등록되어_있음(new LineRequest("이호선", "bg-red-600", 교대역.getId(), 강남역.getId(), 10)).as(LineResponse.class);
+        삼호선 = 지하철_노선_등록되어_있음(new LineRequest("삼호선", "bg-red-600", 교대역.getId(), 양재역.getId(), 5)).as(LineResponse.class);
+
+        지하철_노선에_지하철역_등록_요청(삼호선, 교대역, 남부터미널역, 3);
+    }
+
+    /**
+     * When 지하철 경로에 출발지와 목적지로 최단경로를 조회하면
+     * Then 최단 거리가 조회된다.
+     */
+    @DisplayName("지하철 경로에 출발지와 목적지로 최단경로를 조회하면 입력하면 최단 거리가 조회된다.")
+    @Test
+    void findShortestPath() {
+        // when
+        ExtractableResponse<Response> response = 지하철_경로_최단거리_요청(교대역.getId(), 양재역.getId());
+        // then
+        지하철_경로_최단거리_응답됨(response);
+    }
+
+    private void 지하철_경로_최단거리_응답됨(ExtractableResponse<Response> response) {
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+    }
+}
