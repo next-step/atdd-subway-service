2e7832d41a647ceeaa0dd68b29a289c6e85fbbaf
jhsong2580 <thdwmdgns@naver.com>
2022-06-11 18:17:59 +0900

    test: 지하철 도면 생성

--- src/test/java/nextstep/subway/path/PathAcceptanceTest.java
+++ src/test/java/nextstep/subway/path/PathAcceptanceTest.java
@@ -1,9 +1,56 @@
 package nextstep.subway.path;
 
+import static nextstep.subway.station.StationAcceptanceTest.지하철역_등록되어_있음;
+import static nextstep.subway.utils.LineAcceptanceHelper.지하철_노선_등록되어_있음;
+import static nextstep.subway.utils.LineSectionApiHelper.지하철_노선에_지하철역_등록_요청;
+
 import nextstep.subway.AcceptanceTest;
+import nextstep.subway.line.dto.LineRequest;
+import nextstep.subway.line.dto.LineResponse;
+import nextstep.subway.station.dto.StationResponse;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 
 
 @DisplayName("지하철 경로 조회")
 public class PathAcceptanceTest extends AcceptanceTest {
+
+    StationResponse 강남역;
+    StationResponse 양재역;
+    StationResponse 교대역;
+    StationResponse 남부터미널역;
+    LineResponse 신분당선;
+    LineResponse 이호선;
+    LineResponse 삼호선;
+
+    /**
+     * 교대역    --- *2호선* ---   강남역
+     * |                        |
+     * *3호선*                   *신분당선*
+     * |                        |
+     * 남부터미널역  --- *3호선* ---   양재
+     */
+
+    @BeforeEach
+    public void setUp() {
+        super.setUp();
+
+        강남역 = 지하철역_등록되어_있음("강남역").as(StationResponse.class);
+        양재역 = 지하철역_등록되어_있음("양재역").as(StationResponse.class);
+        교대역 = 지하철역_등록되어_있음("교대역").as(StationResponse.class);
+        남부터미널역 = 지하철역_등록되어_있음("남부터미널역").as(StationResponse.class);
+
+        신분당선 = 지하철_노선_등록되어_있음(
+            new LineRequest("신분당선", "bg-red-600", 강남역.getId(), 양재역.getId(), 10))
+            .as(LineResponse.class);
+        이호선 = 지하철_노선_등록되어_있음(
+            new LineRequest("이호선", "bg-red-600", 교대역.getId(), 강남역.getId(), 10))
+            .as(LineResponse.class);
+        삼호선 = 지하철_노선_등록되어_있음(
+            new LineRequest("삼호선", "bg-red-600", 남부터미널역.getId(), 양재역.getId(), 10))
+            .as(LineResponse.class);
+
+        지하철_노선에_지하철역_등록_요청(삼호선, 교대역, 남부터미널역, 9);
+    }
+
 }
