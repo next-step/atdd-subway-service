b75a1bd16a3e7c8b3ef29911d556d803e7be8321
Donghee Han <handh0413@gmail.com>
2022-06-11 17:17:56 +0900

    refactor: 구간 제거 테스트 추가

--- src/test/java/nextstep/subway/line/domain/LineTest.java
+++ src/test/java/nextstep/subway/line/domain/LineTest.java
@@ -1,5 +1,6 @@
 package nextstep.subway.line.domain;
 
+import nextstep.subway.exception.InvalidSectionException;
 import nextstep.subway.line.dto.LineResponse;
 import nextstep.subway.station.domain.Station;
 import nextstep.subway.station.dto.StationResponse;
@@ -10,6 +11,7 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LineTest {
     private Station A역;
@@ -97,5 +99,74 @@ class LineTest {
                 new StationResponse(D역.getId(), D역.getName()),
                 new StationResponse(E역.getId(), E역.getName()),
                 new StationResponse(F역.getId(), F역.getName()));
+    }
+
+    @DisplayName("중간 지하철 역 삭제")
+    @Test
+    void deleteStation01() {
+        // given
+        Line line = new Line("2호선", "초록", A역, C역, 15);
+
+        // when
+        line.addSection(new Section(B역, C역, 5));
+        line.deleteStation(B역);
+
+        // then
+        Sections sections = line.getSections();
+        assertThat(sections.getList()).hasSize(1);
+    }
+
+    @DisplayName("상행 종점역 지하철 역삭제")
+    @Test
+    void deleteStation02() {
+        // given
+        Line line = new Line("2호선", "초록", A역, C역, 15);
+        line.addSection(new Section(B역, C역, 5));
+
+        // when
+        line.deleteStation(A역);
+
+        // then
+        Sections sections = line.getSections();
+        assertThat(sections.getList()).hasSize(1);
+    }
+
+    @DisplayName("하행 종점역 지하철 역삭제")
+    @Test
+    void deleteStation03() {
+        // given
+        Line line = new Line("2호선", "초록", A역, C역, 15);
+        line.addSection(new Section(B역, C역, 5));
+
+        // when
+        line.deleteStation(C역);
+
+        // then
+        Sections sections = line.getSections();
+        assertThat(sections.getList()).hasSize(1);
+    }
+
+    @DisplayName("하나만 남은 구간은 삭제 불가")
+    @Test
+    void deleteStation04() {
+        // given
+        Line line = new Line("2호선", "초록", A역, C역, 15);
+
+        // when, then
+        assertThatThrownBy(() -> {
+            line.deleteStation(A역);
+        }).isInstanceOf(InvalidSectionException.class);
+    }
+
+    @DisplayName("하나만 남은 구간은 삭제 불가")
+    @Test
+    void deleteStation05() {
+        // given
+        Line line = new Line("2호선", "초록", A역, C역, 15);
+
+        // when, then
+        assertThatThrownBy(() -> {
+            line.deleteStation(C역);
+        }).isInstanceOf(InvalidSectionException.class);
     }
 }
