7abad67ddd280628816cfb3b6ff9c4b3a0e29d1e
toughchb <ggang2000@gmail.com>
2022-06-12 01:41:17 +0900

    feat(PathService): 최단거리 조회 테스트 성공

--- src/main/java/nextstep/subway/path/application/PathService.java
+++ src/main/java/nextstep/subway/path/application/PathService.java
@@ -1,8 +1,13 @@
 package nextstep.subway.path.application;
 
+import java.util.List;
+import nextstep.subway.line.domain.Line;
 import nextstep.subway.line.domain.LineRepository;
+import nextstep.subway.path.domain.Path;
+import nextstep.subway.path.domain.PathFinder;
 import nextstep.subway.path.dto.PathResponse;
 import nextstep.subway.station.application.StationService;
+import nextstep.subway.station.domain.Station;
 
 public class PathService {
 
@@ -16,6 +21,11 @@ public class PathService {
     }
 
     public PathResponse findShortestPath(Long sourceStationId, Long targetStationId) {
-        return new PathResponse();
+        Station sourceStation = stationService.findStationById(sourceStationId);
+        Station targetStation = stationService.findStationById(targetStationId);
+        List<Line> lines = lineRepository.findAll();
+        PathFinder pathFinder = new PathFinder(lines);
+        Path shortestPath = pathFinder.findShortest(sourceStation, targetStation);
+        return new PathResponse(shortestPath);
     }
 }
--- src/main/java/nextstep/subway/path/dto/PathResponse.java
+++ src/main/java/nextstep/subway/path/dto/PathResponse.java
@@ -1,11 +1,28 @@
 package nextstep.subway.path.dto;
 
 import java.util.List;
+import java.util.stream.Collectors;
+import nextstep.subway.path.domain.Path;
 import nextstep.subway.station.dto.StationResponse;
 
 public class PathResponse {
     private List<StationResponse> stations;
     private int distance;
+
+    public PathResponse() {
+    }
+
+    public PathResponse(List<StationResponse> stations, int distance) {
+        this.stations = stations;
+        this.distance = distance;
+    }
+
+    public PathResponse(Path path) {
+       stations = path.getShortestPath().stream().
+                map(station -> StationResponse.of(station)).
+                collect(Collectors.toList());
+        distance = path.getShortestDistance();
+    }
 
     public List<StationResponse> getStations() {
         return this.stations;
@@ -13,5 +30,13 @@ public class PathResponse {
 
     public int getDistance() {
         return this.distance;
+    }
+
+    public void setStations(List<StationResponse> stations) {
+        this.stations = stations;
+    }
+
+    public void setDistance(int distance) {
+        this.distance = distance;
     }
 }
