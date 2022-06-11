4528fb22a53262890d9fcd6e688724e17ffb3e03
jhsong2580 <thdwmdgns@naver.com>
2022-06-11 19:27:17 +0900

    test: mocking을 통해 인수테스트생성

--- src/test/java/nextstep/subway/path/PathAcceptanceTest.java
+++ src/test/java/nextstep/subway/path/PathAcceptanceTest.java
@@ -3,17 +3,32 @@ package nextstep.subway.path;
 import static nextstep.subway.station.StationAcceptanceTest.지하철역_등록되어_있음;
 import static nextstep.subway.utils.LineAcceptanceHelper.지하철_노선_등록되어_있음;
 import static nextstep.subway.utils.LineSectionApiHelper.지하철_노선에_지하철역_등록_요청;
+import static nextstep.subway.utils.PathApiHelper.지하철_경로_조회_요청;
+import static nextstep.subway.utils.PathAssertionHelper.최단경로_결과_확인;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import java.util.Arrays;
 import nextstep.subway.AcceptanceTest;
+import nextstep.subway.line.application.PathService;
 import nextstep.subway.line.dto.LineRequest;
 import nextstep.subway.line.dto.LineResponse;
+import nextstep.subway.line.dto.PathResponse;
+import nextstep.subway.station.domain.Station;
 import nextstep.subway.station.dto.StationResponse;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 
 @DisplayName("지하철 경로 조회")
 public class PathAcceptanceTest extends AcceptanceTest {
+
+    @MockBean
+    private PathService pathService;
 
     StationResponse 강남역;
     StationResponse 양재역;
@@ -41,16 +56,36 @@ public class PathAcceptanceTest extends AcceptanceTest {
         남부터미널역 = 지하철역_등록되어_있음("남부터미널역").as(StationResponse.class);
 
         신분당선 = 지하철_노선_등록되어_있음(
-            new LineRequest("신분당선", "bg-red-600", 강남역.getId(), 양재역.getId(), 10))
-            .as(LineResponse.class);
-        이호선 = 지하철_노선_등록되어_있음(
-            new LineRequest("이호선", "bg-red-600", 교대역.getId(), 강남역.getId(), 10))
-            .as(LineResponse.class);
+            new LineRequest("신분당선", "bg-red-600", 강남역.getId(), 양재역.getId(), 7)).as(
+            LineResponse.class);
+        이호선 = 지하철_노선_등록되어_있음(new LineRequest("이호선", "bg-red-600", 교대역.getId(), 강남역.getId(), 9)).as(
+            LineResponse.class);
         삼호선 = 지하철_노선_등록되어_있음(
-            new LineRequest("삼호선", "bg-red-600", 남부터미널역.getId(), 양재역.getId(), 10))
-            .as(LineResponse.class);
+            new LineRequest("삼호선", "bg-red-600", 남부터미널역.getId(), 양재역.getId(), 8)).as(
+            LineResponse.class);
 
-        지하철_노선에_지하철역_등록_요청(삼호선, 교대역, 남부터미널역, 9);
+        지하철_노선에_지하철역_등록_요청(삼호선, 교대역, 남부터미널역, 3);
     }
 
+    /**
+     * 교대역    --- *2호선* ---   강남역
+     * |                        |
+     * *3호선*                   *신분당선*
+     * |                        |
+     * 남부터미널역  --- *3호선* ---   양재
+     * background given : 위와같은 지하철 도면일때 when 교대~양재역 까지의 거리 및 경로를 조회했을때 then 교대~남부터미널~양재 경로로 안내된다.
+     */
+    @Test
+    public void 지하철_경로_조회하기() {
+        //given
+        when(pathService.findPath(any(Long.class), any(Long.class))).thenReturn(PathResponse.of(
+            Arrays.asList(new Station("교대역"), new Station("남부터미널역"), new Station("양재역")), 21));
+
+        //when
+        ExtractableResponse<Response> 지하철_경로_조회_요청_response = 지하철_경로_조회_요청(교대역.getId(),
+            양재역.getId());
+
+        //then
+        최단경로_결과_확인(지하철_경로_조회_요청_response, Arrays.asList("교대역", "남부터미널역", "양재역"), 21);
+    }
 }
--- src/test/java/nextstep/subway/utils/PathAssertionHelper.java
+++ src/test/java/nextstep/subway/utils/PathAssertionHelper.java
@@ -1,17 +1,23 @@
 package nextstep.subway.utils;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import java.util.List;
-import nextstep.subway.line.dto.PathResponse;
 
 public class PathAssertionHelper {
 
-    public static void 최단경로_결과_확인(ExtractableResponse<Response> response,
-        List<PathResponse> pathResponses) {
-        assertThat(1).isEqualTo(1);
+    public static void 최단경로_결과_확인(ExtractableResponse<Response> response, List<String> stations,
+        int distance) {
+        assertAll(
+            () -> assertThat(response.jsonPath().getList("stations")).hasSize(3).extracting("name")
+                .containsExactlyElementsOf(stations),
+            () -> assertThat(response.jsonPath().get("distance").toString()).isEqualTo(
+                String.valueOf(distance)));
+
+        ;
     }
 
 }
