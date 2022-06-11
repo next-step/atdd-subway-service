af97c49293f65d67a500c756d58df241ce13a0e5
jhsong2580 <thdwmdgns@naver.com>
2022-06-11 18:29:00 +0900

    feat: 경로조회 함수 생성(mock 위함)

---
+++ src/main/java/nextstep/subway/line/application/PathService.java
@@ -0,0 +1,13 @@
+package nextstep.subway.line.application;
+
+import nextstep.subway.line.dto.PathResponse;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PathService {
+
+
+    public PathResponse findPath(Long source, Long target) {
+        return null;
+    }
+}
