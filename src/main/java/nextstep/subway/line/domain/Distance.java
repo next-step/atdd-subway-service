6314bc23657bafb9c3b40ff56e7798d5bd9ef12a
minzzang <minjjang1117@gmail.com>
2022-06-11 11:30:39 +0900

    refactor(Distance): 생성자 validation 위치 변경

--- src/main/java/nextstep/subway/line/domain/Distance.java
+++ src/main/java/nextstep/subway/line/domain/Distance.java
@@ -12,11 +12,11 @@ public class Distance {
     }
 
     public Distance(int value) {
+        validateDistance(value);
         this.value = value;
-        validateDistance();
     }
 
-    private void validateDistance() {
+    private void validateDistance(int value) {
         if (value <= MIN_DISTANCE) {
             throw new IllegalArgumentException("구간의 길이는 0보다 커야합니다.");
         }
