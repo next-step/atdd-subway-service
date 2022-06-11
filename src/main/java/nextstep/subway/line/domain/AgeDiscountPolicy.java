3dd01b3de3b7ab8d1aa7256c224533c675cdb8c2
heowc <heowc1992@gmail.com>
2022-06-11 17:37:00 +0900

    feat(AgeDiscountPolicy): AgeDiscountPolicy 구현

--- src/main/java/nextstep/subway/line/domain/AgeDiscountPolicy.java
+++ src/main/java/nextstep/subway/line/domain/AgeDiscountPolicy.java
@@ -1,13 +1,35 @@
 package nextstep.subway.line.domain;
 
+import java.util.Arrays;
+
 public enum AgeDiscountPolicy {
-    ;
+    OLD(65, 100),
+    ADULT(19, 0),
+    TEENAGER(13, 20),
+    CHILDREN(6, 50),
+    TODDLER(0, 100);
+
+    private final int age;
+    private final double discount;
+
+    AgeDiscountPolicy(int age, int discount) {
+        this.age = age;
+        this.discount = discount;
+    }
 
     public static AgeDiscountPolicy of(int age) {
-        return null;
+        return Arrays.stream(values())
+                     .filter(it -> it.age <= age)
+                     .findFirst()
+                     .orElseThrow(IllegalArgumentException::new);
     }
 
     public Fare discount(Fare fare) {
-        return null;
+        if (discount == 0) {
+            return fare;
+        }
+        int fareValue = fare.getValue().intValue();
+        Fare discountFare = new Fare((int) (fareValue * (discount / 100)));
+        return fare.minus(discountFare);
     }
 }
