342308991bfaecf800d9fc8fc53c7d7772eadea9
gunan <power4889@gmail.com>
2022-06-12 17:13:15 +0900

    refactor: 메소드 분리

--- src/main/java/nextstep/subway/path/domain/PathFinder.java
+++ src/main/java/nextstep/subway/path/domain/PathFinder.java
@@ -22,9 +22,24 @@ public class PathFinder {
     public PathResponse findPath(List<Station> stations, List<Line> lines, Station source, Station target) {
         WeightedMultigraph<Station, DefaultWeightedEdge> graph = new WeightedMultigraph<>(DefaultWeightedEdge.class);
         addVertex(graph, stations);
-        lines.forEach(line -> setEdgeWeight(graph, line.getSections().getSections()));
-        DijkstraShortestPath<Station, DefaultWeightedEdge> dijkstraShortestPath = new DijkstraShortestPath<>(graph);
-        GraphPath<Station, DefaultWeightedEdge> graphPath = dijkstraShortestPath.getPath(source, target);
+        setEdgeWeight(graph, lines);
+        return pathResponse(graph, source, target);
+    }
+
+    private void addVertex(WeightedMultigraph<Station, DefaultWeightedEdge> graph, List<Station> stations) {
+        stations.forEach(graph::addVertex);
+    }
+
+    private void setEdgeWeight(WeightedMultigraph<Station, DefaultWeightedEdge> graph, List<Line> lines) {
+        lines.forEach(line -> setEdgeWeightSections(graph, line.getSections().getSections()));
+    }
+
+    private void setEdgeWeightSections(WeightedMultigraph<Station, DefaultWeightedEdge> graph, List<Section> sections) {
+        sections.forEach(section -> graph.setEdgeWeight(graph.addEdge(section.getUpStation(), section.getDownStation()), section.getDistance()));
+    }
+
+    private PathResponse pathResponse(WeightedMultigraph<Station, DefaultWeightedEdge> graph, Station source, Station target) {
+        GraphPath<Station, DefaultWeightedEdge> graphPath = new DijkstraShortestPath<>(graph).getPath(source, target);
         if (graphPath == null) {
             throw new ErrorCodeException(ErrorCode.SOURCE_NOT_CONNECT_TARGET);
         }
@@ -32,15 +47,5 @@ public class PathFinder {
                 .map(StationResponse::of)
                 .collect(Collectors.toList());
         return new PathResponse(path, (int) graphPath.getWeight());
-    }
-
-    private void addVertex(WeightedMultigraph<Station, DefaultWeightedEdge> graph, List<Station> stations) {
-        stations.forEach(graph::addVertex);
-    }
-
-    private void setEdgeWeight(WeightedMultigraph<Station, DefaultWeightedEdge> graph, List<Section> sections) {
-        sections.forEach(section -> {
-            graph.setEdgeWeight(graph.addEdge(section.getUpStation(), section.getDownStation()), section.getDistance());
-        });
     }
 }
