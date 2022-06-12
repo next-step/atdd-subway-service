de5738feee27d3ea03b367e84a237233776ac94d
jhsong2580 <thdwmdgns@naver.com>
2022-06-12 13:25:06 +0900

    refactor: unchecked Exception들에 대해 상세 에러 작성 및 advice생성

--- src/main/java/nextstep/subway/exception/handler/ApiHandler.java
+++ src/main/java/nextstep/subway/exception/handler/ApiHandler.java
@@ -1,6 +1,7 @@
 package nextstep.subway.exception.handler;
 
 import nextstep.subway.exception.PathFindException;
+import nextstep.subway.exception.SectionManagerException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -9,6 +10,18 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class ApiHandler {
     @ExceptionHandler
     public ResponseEntity PathFindExceptionHandler(PathFindException e){
+        return ResponseEntity.badRequest()
+            .build();
+    }
+
+    @ExceptionHandler
+    public ResponseEntity SectionManagerExceptionHandler(SectionManagerException e){
+        return ResponseEntity.badRequest()
+            .build();
+    }
+
+    @ExceptionHandler
+    public ResponseEntity IllegalArgumentExceptionHandler(IllegalArgumentException e){
         return ResponseEntity.badRequest()
             .build();
     }
