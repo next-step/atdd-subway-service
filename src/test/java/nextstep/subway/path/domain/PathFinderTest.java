6cf5232cd9067c1100732b6fdccd348aaa29bc34
minzzang <minjjang1117@gmail.com>
2022-06-13 00:06:50 +0900

    test(PathFinderTest): 최단 경로 목록을 반환한다

---
+++ src/test/java/nextstep/subway/path/domain/PathFinderTest.java
@@ -0,0 +1,44 @@
+package nextstep.subway.path.domain;
+
+import nextstep.subway.line.domain.Line;
+import nextstep.subway.station.domain.Station;
+import org.jgrapht.GraphPath;
+import org.jgrapht.graph.DefaultWeightedEdge;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static nextstep.subway.path.application.PathServiceTest.getLines;
+import static nextstep.subway.path.application.PathServiceTest.getStation;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+@DisplayName("최단 경로 찾기")
+class PathFinderTest {
+    private List<Line> lines;
+    private PathFinder pathFinder;
+    private Station upStation;
+    private Station downStation;
+
+    @BeforeEach
+    void setUp() {
+        lines = getLines();
+        pathFinder = new PathFinder();
+        upStation = getStation(1L, "강남역");
+        downStation = getStation(4L, "교대역");
+    }
+
+    @Test
+    void 최단_경로_목록을_반환한다() {
+        // when
+        GraphPath<Station, DefaultWeightedEdge> result = pathFinder.findShortestPath(lines, upStation, downStation);
+
+        // then
+        assertAll(
+                () -> assertThat(result.getVertexList()).hasSize(4),
+                () -> assertThat(result.getWeight()).isEqualTo(9)
+        );
+    }
+}
