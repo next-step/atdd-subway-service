b9723b583cb85b70bc6ef9d47379d71c2716096d
Donghee Han <handh0413@gmail.com>
2022-06-12 14:12:59 +0900

    feat: 경로 조회에 대한 예외 로직 추가

--- src/main/java/nextstep/subway/path/domain/PathFinder.java
+++ src/main/java/nextstep/subway/path/domain/PathFinder.java
@@ -1,9 +1,11 @@
 package nextstep.subway.path.domain;
 
+import nextstep.subway.exception.InvalidPathException;
 import nextstep.subway.line.domain.Line;
 import nextstep.subway.line.domain.Section;
 import nextstep.subway.line.domain.Sections;
 import nextstep.subway.station.domain.Station;
+import org.jgrapht.GraphPath;
 import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
 import org.jgrapht.graph.DefaultWeightedEdge;
 import org.jgrapht.graph.WeightedMultigraph;
@@ -33,11 +35,26 @@ public class PathFinder {
         shortestPath = new DijkstraShortestPath<>(graph);
     }
 
-    public List<Station> findShortestPath(Station source, Station destination) {
-        return shortestPath.getPath(source, destination).getVertexList();
+    public List<Station> findShortestStationList(Station source, Station destination) {
+        return findShortestPath(source, destination).getVertexList();
     }
 
     public double findShortestPathLength(Station source, Station destination) {
-        return shortestPath.getPathWeight(source, destination);
+        return findShortestPath(source, destination).getWeight();
+    }
+
+    private GraphPath<Station, DefaultWeightedEdge> findShortestPath(Station source, Station destination) {
+        validatePaths(source, destination);
+        GraphPath<Station, DefaultWeightedEdge> path = shortestPath.getPath(source, destination);
+        if (path == null) {
+            throw new InvalidPathException("존재하지 않는 경로입니다.");
+        }
+        return path;
+    }
+
+    private void validatePaths(Station source, Station destination) {
+        if (source.equals(destination)) {
+            throw new InvalidPathException("출발역과 도착역은 달라야 합니다.");
+        }
     }
 }
--- src/test/java/nextstep/subway/path/domain/PathFinderTest.java
+++ src/test/java/nextstep/subway/path/domain/PathFinderTest.java
@@ -1,5 +1,6 @@
 package nextstep.subway.path.domain;
 
+import nextstep.subway.exception.InvalidPathException;
 import nextstep.subway.line.domain.Line;
 import nextstep.subway.line.domain.Section;
 import nextstep.subway.station.domain.Station;
@@ -10,17 +11,21 @@ import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class PathFinderTest {
     private Station 종합운동장;
     private Station 잠실새내;
     private Station 잠실;
     private Station 석촌;
+    private Station 천호;
+    private Station 하남시청;
 
     private Line 이호선;
+    private Line 오호선;
     private Line 팔호선;
 
-    @DisplayName("PathFinder 생성")
+    @DisplayName("최단 경로 객체 생성")
     @Test
     void create() {
         // when
@@ -37,7 +42,7 @@ class PathFinderTest {
         PathFinder pathFinder = new PathFinder(createLines());
 
         // when
-        List<Station> stations = pathFinder.findShortestPath(종합운동장, 석촌);
+        List<Station> stations = pathFinder.findShortestStationList(종합운동장, 석촌);
 
         // then
         assertThat(stations).hasSize(4);
@@ -57,17 +62,45 @@ class PathFinderTest {
         assertThat(length).isEqualTo(30);
     }
 
+    @DisplayName("출발역과 도착역이 같은 경우")
+    @Test
+    void findShortestPath_exception01() {
+        // given
+        PathFinder pathFinder = new PathFinder(createLines());
+
+        // when, then
+        assertThatThrownBy(() -> {
+            pathFinder.findShortestStationList(종합운동장, 종합운동장);
+        }).isInstanceOf(InvalidPathException.class);
+    }
+
+    @DisplayName("출발역과 도착역이 연결되지 않은 경우")
+    @Test
+    void findShortestPath_exception02() {
+        // given
+        PathFinder pathFinder = new PathFinder(createLines());
+
+        // when, then
+        assertThatThrownBy(() -> {
+            pathFinder.findShortestStationList(종합운동장, 하남시청);
+        }).isInstanceOf(InvalidPathException.class);
+    }
+
     private List<Line> createLines() {
         종합운동장 = new Station(1L, "종합운동장");
         잠실새내 = new Station(2L, "잠실새내");
         잠실 = new Station(3L, "잠실");
         석촌 = new Station(4L, "석촌");
+        천호 = new Station(5L, "천호");
+        하남시청 = new Station(5L, "하남시청");
 
         이호선 = new Line("2호선", "green", 종합운동장, 잠실새내, 10);
         이호선.addSection(new Section(잠실새내, 잠실, 10));
 
+        오호선 = new Line("5호선", "purple", 천호, 하남시청, 100);
+
         팔호선 = new Line("8호선", "pink", 잠실, 석촌, 10);
 
-        return Arrays.asList(이호선, 팔호선);
+        return Arrays.asList(이호선, 오호선, 팔호선);
     }
 }
