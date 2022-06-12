8cd036a75565b39c6a2139177cafd7d74ead0114
minzzang <minjjang1117@gmail.com>
2022-06-12 22:12:33 +0900

    feat: 최단 경로를 조회한다

--- src/main/java/nextstep/subway/path/dto/PathResponse.java
+++ src/main/java/nextstep/subway/path/dto/PathResponse.java
@@ -1,12 +1,26 @@
 package nextstep.subway.path.dto;
 
+import nextstep.subway.station.domain.Station;
 import nextstep.subway.station.dto.StationResponse;
 
 import java.util.List;
 
+import static java.util.stream.Collectors.toList;
+
 public class PathResponse {
     private List<StationResponse> stations;
     private int distance;
+
+    public PathResponse(List<Station> stations, int distance) {
+        this.stations = mapToStationResponse(stations);
+        this.distance = distance;
+    }
+
+    private List<StationResponse> mapToStationResponse(List<Station> stations) {
+        return stations.stream()
+                .map(StationResponse::of)
+                .collect(toList());
+    }
 
     public List<StationResponse> getStations() {
         return stations;
