93cbb74aa8d2e47472a0204dbd32dbb030c93dde
sy001 <suyounlake@naver.com>
2022-06-12 17:37:53 +0900

    test: PathFinder 도메인 관련 테스트케이스 작성

--- src/main/java/nextstep/subway/path/domain/PathFinder.java
+++ src/main/java/nextstep/subway/path/domain/PathFinder.java
@@ -2,14 +2,22 @@ package nextstep.subway.path.domain;
 
 import nextstep.subway.line.domain.Line;
 import nextstep.subway.station.domain.Station;
+import org.jgrapht.graph.DefaultWeightedEdge;
+import org.jgrapht.graph.WeightedMultigraph;
 
 import java.util.List;
 
 public class PathFinder {
+    private static WeightedMultigraph<Station, DefaultWeightedEdge> routeGraph;
+
     public PathFinder(List<Line> lines) {
     }
 
     public Path findPath(Station sourceStation, Station targetStation) {
         return null;
+    }
+
+    public static WeightedMultigraph<Station, DefaultWeightedEdge> getRouteGraph() {
+        return routeGraph;
     }
 }
---
+++ src/test/java/nextstep/subway/path/domain/PathFinderTest.java
@@ -0,0 +1,107 @@
+package nextstep.subway.path.domain;
+
+import nextstep.subway.line.domain.Line;
+import nextstep.subway.station.domain.Station;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static nextstep.subway.utils.FactoryMethods.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class PathFinderTest {
+    private Line 신분당선;
+    private Line 이호선;
+    private Line 삼호선;
+    private Station 강남역;
+    private Station 양재역;
+    private Station 교대역;
+    private Station 남부터미널역;
+
+    /**
+     * 교대역  --- *2호선* (10)--   강남역
+     * |                        |
+     * *3호선* (3)               *신분당선* (10)
+     * |                        |
+     * 남부터미널역 - *3호선* (2) -  양재
+     */
+    @BeforeEach
+    void setUp() {
+        강남역 = createStation("강남역");
+        양재역 = createStation("양재역");
+        교대역 = createStation("교대역");
+        남부터미널역 = createStation("남부터미널역");
+
+        신분당선 = createLine("신분당선", "rbg-red-600", 강남역, 양재역, 10);
+        이호선 = createLine("이호선", "rbg-red-600", 교대역, 강남역, 10);
+        삼호선 = createLine("삼호선", "rbg-red-600", 교대역, 양재역, 5);
+        삼호선.addSection(createSection(교대역, 남부터미널역, 2));
+    }
+
+    @Test
+    @DisplayName("PathFinder 생성")
+    void PathFinder_생성() {
+        //when
+        PathFinder pathFinder = new PathFinder(Arrays.asList(신분당선, 이호선, 삼호선));
+
+        //then
+        assertAll(
+                () -> assertThat(pathFinder.getRouteGraph().vertexSet()).containsExactly(양재역, 강남역, 교대역, 남부터미널역),
+                () -> assertThat(pathFinder.getRouteGraph().edgeSet().size()).isEqualTo(4)
+        );
+    }
+
+    @Test
+    @DisplayName("최단 경로 탐색")
+    void 최단경로탐색() {
+        //given
+        PathFinder pathFinder = new PathFinder(Arrays.asList(신분당선, 이호선, 삼호선));
+
+        //when
+        Path path = pathFinder.findPath(교대역, 양재역);
+
+        //then
+        assertAll(
+                () -> assertThat(path.getStations()).containsExactly(교대역, 남부터미널역, 양재역),
+                () -> assertThat(path.getDistance()).isEqualTo(5)
+        );
+    }
+
+    @Test
+    @DisplayName("최단 경로 탐색 실패 : 출발역과 도착역이 같은 경우")
+    void 최단경로탐색_실패_출발역과_도착역이_같음() {
+        //given
+        PathFinder pathFinder = new PathFinder(Arrays.asList(신분당선, 이호선, 삼호선));
+
+        //then
+        assertThrows(IllegalArgumentException.class, () -> pathFinder.findPath(교대역, 교대역));
+    }
+
+    @Test
+    @DisplayName("최단 경로 탐색 실패 : 출발역과 도착역이 연결이 되어 있지 않은 경우")
+    void 최단경로탐색_실패_출발역과_도착역이_연결되지_않음() {
+        //given
+        Station 부평역 = createStation("부평역");
+        Station 인천시청역 = createStation("인천시청역");
+        Line 인천선 = createLine("신분당선", "rbg-red-600", 부평역, 인천시청역, 10);
+        PathFinder pathFinder = new PathFinder(Arrays.asList(신분당선, 이호선, 삼호선, 인천선));
+
+        //then
+        assertThrows(IllegalArgumentException.class, () -> pathFinder.findPath(교대역, 부평역));
+    }
+
+    @Test
+    @DisplayName("최단 경로 탐색 실패 : 존재하지 않은 출발역이나 도착역을 조회 할 경우")
+    void 최단경로탐색_실패_존재하지_않는_역() {
+        //given
+        Station 존재하지않는역 = createStation("존재하지않는역");
+        PathFinder pathFinder = new PathFinder(Arrays.asList(신분당선, 이호선, 삼호선));
+
+        //then
+        assertThrows(IllegalArgumentException.class, () -> pathFinder.findPath(교대역, 존재하지않는역));
+    }
+}
