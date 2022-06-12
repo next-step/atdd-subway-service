a0bf20b96dc9d8117f21463fd58e4f5c3b1f03dd
minzzang <minjjang1117@gmail.com>
2022-06-12 18:32:35 +0900

    test(PathServiceTest): 최단 거리 조회 서비스 테스트 작성

--- src/main/java/nextstep/subway/path/application/PathService.java
+++ src/main/java/nextstep/subway/path/application/PathService.java
@@ -1,10 +1,16 @@
 package nextstep.subway.path.application;
 
+import nextstep.subway.line.domain.LineRepository;
 import nextstep.subway.path.dto.PathResponse;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PathService {
+    private final LineRepository lineRepository;
+
+    public PathService(LineRepository lineRepository) {
+        this.lineRepository = lineRepository;
+    }
 
     public PathResponse findShortestPath(Long source, Long target) {
         return new PathResponse();
---
+++ src/test/java/nextstep/subway/path/application/PathServiceTest.java
@@ -0,0 +1,65 @@
+package nextstep.subway.path.application;
+
+import nextstep.subway.line.domain.Line;
+import nextstep.subway.line.domain.LineRepository;
+import nextstep.subway.line.domain.Section;
+import nextstep.subway.path.dto.PathResponse;
+import nextstep.subway.station.domain.Station;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+
+@DisplayName("지하철 최단 경로 조회 로직")
+@ExtendWith(MockitoExtension.class)
+class PathServiceTest {
+    @Mock
+    private LineRepository lineRepository;
+
+    private List<Line> lines;
+
+    @BeforeEach
+    void setUp() {
+        Station 강남역 = getStation(1L, "강남역");
+        Station 양재역 = getStation(2L, "양재역");
+        Station 남부터미널역 = getStation(3L, "남부터미널역");
+        Station 교대역 = getStation(4L, "교대역");
+
+        Line 신분당선 = new Line("신분당선", "red", 강남역, 양재역, 4);
+        Line 삼호선 = new Line("삼호선", "orange", 교대역, 남부터미널역, 3);
+        Line 이호선 = new Line("이호선", "green", 교대역, 강남역, 10);
+
+        삼호선.addSection(new Section(남부터미널역, 양재역, 2));
+
+        lines = Arrays.asList(신분당선, 삼호선, 이호선);
+    }
+
+    private Station getStation(long id, String name) {
+        Station station = new Station(name);
+        ReflectionTestUtils.setField(station, "id", id);
+
+        return station;
+    }
+
+    @Test
+    void 최단_경로를_조회한다() {
+        // given
+        given(lineRepository.findAll()).willReturn(lines);
+        PathService pathService = new PathService(lineRepository);
+
+        // when
+        PathResponse response = pathService.findShortestPath(1L, 4L);
+
+        // then
+        assertThat(response.getDistance()).isEqualTo(9);
+    }
+}
