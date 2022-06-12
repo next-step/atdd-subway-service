637ced2d928804a4344375cfadb2c03ac4d468a7
minzzang <minjjang1117@gmail.com>
2022-06-13 00:21:19 +0900

    feat(Lines): 전체 노선 등록된 역을 조회한다

---
+++ src/main/java/nextstep/subway/line/domain/Lines.java
@@ -0,0 +1,22 @@
+package nextstep.subway.line.domain;
+
+import nextstep.subway.station.domain.Station;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Lines {
+    private final List<Line> elements = new ArrayList<>();
+
+    public Lines(List<Line> lines) {
+        elements.addAll(lines);
+    }
+
+    public List<Station> getAllStations() {
+        return elements.stream()
+                .flatMap(line -> line.getStations().stream())
+                .distinct()
+                .collect(Collectors.toList());
+    }
+}
---
+++ src/test/java/nextstep/subway/line/domain/LinesTest.java
@@ -0,0 +1,30 @@
+package nextstep.subway.line.domain;
+
+import nextstep.subway.station.domain.Station;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static nextstep.subway.path.application.PathServiceTest.getLines;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DisplayName("전체 노선에 대한 기능")
+public class LinesTest {
+    private Lines lines;
+
+    @BeforeEach
+    void setUp() {
+        lines = new Lines(getLines());
+    }
+
+    @Test
+    void 전체_노선에_등록된_역을_조회한다() {
+        // when
+        List<Station> stations =  lines.getAllStations();
+
+        // then
+        assertThat(stations).hasSize(4);
+    }
+}
