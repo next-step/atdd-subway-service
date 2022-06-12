9b02b6e48b36dabf7afbf5193df895b50d8a965b
tyakamyz <tyakamyz@naver.com>
2022-06-12 17:34:38 +0900

    feat: Favorite Entity 생성

---
+++ src/main/java/nextstep/subway/favorite/domain/Favorite.java
@@ -0,0 +1,48 @@
+package nextstep.subway.favorite.domain;
+
+import nextstep.subway.BaseEntity;
+import nextstep.subway.member.domain.Member;
+import nextstep.subway.station.domain.Station;
+
+import javax.persistence.*;
+
+@Entity
+public class Favorite extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "source_station_id")
+    private Station source;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "target_station_id")
+    private Station target;
+
+    protected Favorite() {
+
+    }
+
+    public Favorite(Member member, Station source, Station target) {
+        new Favorite(null, member, source, target);
+    }
+
+    public Favorite(Long id, Member member, Station source, Station target) {
+        this.id = id;
+        this.member = member;
+        this.source = source;
+        this.target = target;
+    }
+
+    @Override
+    public String toString() {
+        return "Favorite{" +
+                "id=" + id +
+                '}';
+    }
+}
