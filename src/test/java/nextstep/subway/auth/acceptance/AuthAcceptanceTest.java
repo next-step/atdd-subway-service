28846af650aebb0aaafc1b5021538ad93c3243ab
mond <revive29@gmail.com>
2022-06-13 21:46:46 +0900

    test(AuthAcceptanceTest): 멤버의 정보로 로그인을 하면 정상적으로 Bearer 토큰을 받아온다

--- src/test/java/nextstep/subway/auth/acceptance/AuthAcceptanceTest.java
+++ src/test/java/nextstep/subway/auth/acceptance/AuthAcceptanceTest.java
@@ -1,10 +1,35 @@
 package nextstep.subway.auth.acceptance;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.mockito.Mockito.when;
+
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import java.util.Optional;
 import nextstep.subway.AcceptanceTest;
+import nextstep.subway.auth.dto.TokenRequest;
+import nextstep.subway.auth.dto.TokenResponse;
+import nextstep.subway.member.domain.Member;
+import nextstep.subway.member.domain.MemberRepository;
+import org.assertj.core.api.AbstractIntegerAssert;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 
+@ExtendWith(MockitoExtension.class)
 public class AuthAcceptanceTest extends AcceptanceTest {
+    private final TokenRequest 몬드 = new TokenRequest("mond@mond.com", "younggun");
+
+    @MockBean
+    private MemberRepository memberRepository;
 
     /**
      *  Given 멤버가 주어지고
@@ -15,10 +40,14 @@ public class AuthAcceptanceTest extends AcceptanceTest {
     @DisplayName("멤버의 정보로 로그인을 하면 정상적으로 Bearer 토큰을 받아온다")
     void myInfoWithBearerAuth() {
         // given
+        Member mond = new Member("mond@mond.com", "younggun", 10);
+        when(memberRepository.findByEmail("mond@mond.com")).thenReturn(Optional.of(mond));
 
         // when
+        ExtractableResponse<Response> 로그인_요청_결과 = 로그인_요청(몬드);
 
         // then
+        로그인_요청_성공(로그인_요청_결과);
     }
 
     /**
@@ -45,4 +74,20 @@ public class AuthAcceptanceTest extends AcceptanceTest {
         // then
     }
 
+    private ExtractableResponse<Response> 로그인_요청(TokenRequest 로그인정보) {
+        return RestAssured.given().log().all()
+                .body(로그인정보)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when().post("/login/token")
+                .then().log().all()
+                .extract();
+    }
+
+    private void 로그인_요청_성공(ExtractableResponse<Response> 로그인_요청_결과) {
+        TokenResponse 응답_토큰 = 로그인_요청_결과.as(TokenResponse.class);
+        assertAll(
+                () -> assertThat(로그인_요청_결과.statusCode()).isEqualTo(HttpStatus.OK.value()),
+                () -> assertThat(응답_토큰.getAccessToken()).isNotNull()
+        );
+    }
 }
