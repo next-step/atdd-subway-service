aeb63f2a60fabd0e533bde9a3d7b1e3869f8949c
minzzang <minjjang1117@gmail.com>
2022-06-13 21:35:45 +0900

    feat: 노선에 지하철역이 포함되어 있지 않은 경우 최단경로를 구할 수 없다

--- src/main/java/nextstep/subway/path/domain/PathFinder.java
+++ src/main/java/nextstep/subway/path/domain/PathFinder.java
@@ -11,6 +11,8 @@ import org.jgrapht.graph.WeightedMultigraph;
 import java.util.Objects;
 
 public class PathFinder {
+    private static final String MUST_NOT_BE_NULL = "null일 수 없습니다.";
+
     private WeightedMultigraph<Station, DefaultWeightedEdge> graph;
 
     public PathFinder() {
@@ -18,19 +20,31 @@ public class PathFinder {
     }
 
     public GraphPath<Station, DefaultWeightedEdge> findShortestPath(Lines lines, Station upStation, Station downStation) {
-        validateVertex(upStation, downStation);
+        validate(lines, upStation, downStation);
         addVertex(lines);
         assignEdgeWeight(lines);
 
         return getPath(upStation, downStation);
     }
 
-    private void validateVertex(Station upStation, Station downStation) {
-        Objects.requireNonNull(upStation, "역이 없습니다.");
-        Objects.requireNonNull(downStation, "역이 없습니다.");
+    private void validate(Lines lines, Station upStation, Station downStation) {
+        Objects.requireNonNull(lines, MUST_NOT_BE_NULL) ;
+        Objects.requireNonNull(upStation, MUST_NOT_BE_NULL);
+        Objects.requireNonNull(downStation, MUST_NOT_BE_NULL);
 
+        validateVertex(upStation, downStation);
+        validateStationIsIncluded(lines, upStation, downStation);
+    }
+
+    private void validateVertex(Station upStation, Station downStation) {
         if (upStation.match(downStation)) {
             throw new IllegalArgumentException("출발역과 도착역이 같을 수 없습니다.");
+        }
+    }
+
+    private void validateStationIsIncluded(Lines lines, Station upStation, Station downStation) {
+        if (lines.notContainsAll(upStation, downStation)) {
+            throw new IllegalArgumentException("노선에 지하철역이 모두 포함되어 있지 않습니다.");
         }
     }
 
