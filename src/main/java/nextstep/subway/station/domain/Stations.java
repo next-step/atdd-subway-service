3bb81fb5ef387622bc271220982ca00582fa4b66
14km <k.connor614@gmail.com>
2022-06-14 01:16:48 +0900

    style: 코드 포맷팅 수정

--- src/main/java/nextstep/subway/station/domain/Stations.java
+++ src/main/java/nextstep/subway/station/domain/Stations.java
@@ -21,7 +21,7 @@ public class Stations {
                 .collect(Collectors.toList());
     }
 
-    public boolean isNotEmpty(){
+    public boolean isNotEmpty() {
         return !stations.isEmpty();
     }
 }
