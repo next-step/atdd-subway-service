c4239a52879fb69b0f3e8502175a7ec65e30fe64
iamjunsulee <mellow0924@gmail.com>
2022-06-14 01:24:07 +0900

    test(PathAcceptanceTest): 최단 경로 탐색 관련 인수테스트 작성

---
+++ src/main/java/nextstep/subway/path/dto/PathResponse.java
@@ -0,0 +1,13 @@
+package nextstep.subway.path.dto;
+
+import java.util.List;
+import nextstep.subway.station.dto.StationResponse;
+
+public class PathResponse {
+    private List<StationResponse> stations;
+    private double distance;
+
+    public List<StationResponse> getStations() {
+        return stations;
+    }
+}
