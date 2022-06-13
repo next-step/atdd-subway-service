cf44187637a9df408a957a039685e150ade2814b
mond <revive29@gmail.com>
2022-06-13 21:22:20 +0900

    test(AuthAcceptanceTest): 인수테스트 뼈대 작성

--- src/test/java/nextstep/subway/auth/acceptance/AuthAcceptanceTest.java
+++ src/test/java/nextstep/subway/auth/acceptance/AuthAcceptanceTest.java
@@ -6,19 +6,43 @@ import org.junit.jupiter.api.Test;
 
 public class AuthAcceptanceTest extends AcceptanceTest {
 
-    @DisplayName("Bearer Auth")
+    /**
+     *  Given 멤버가 주어지고
+     *  When 해당 멤버 id / password로 로그인을 하면
+     *  Then Bearer 토큰을 받아온다
+     */
     @Test
+    @DisplayName("멤버의 정보로 로그인을 하면 정상적으로 Bearer 토큰을 받아온다")
     void myInfoWithBearerAuth() {
+        // given
+
+        // when
+
+        // then
     }
 
-    @DisplayName("Bearer Auth 로그인 실패")
+    /**
+     *  When 올바르지 않은 id / password로 로그인을 하면
+     *  Then 로그인이 실패한다
+     */
     @Test
+    @DisplayName("올바르지 않은 정보로 로그인을 하면 로그인이 실패한다")
     void myInfoWithBadBearerAuth() {
+        // when
+
+        // then
     }
 
-    @DisplayName("Bearer Auth 유효하지 않은 토큰")
+    /**
+     *  When 올바르지 않은 Bearer 토큰으로 호출하면
+     *  Then 조회할 수 없다
+     */
     @Test
+    @DisplayName("올바르지 않는 토큰으로 조회 할 수 없다")
     void myInfoWithWrongBearerAuth() {
+        // when
+
+        // then
     }
 
 }
