8a2c8d4e13bc46710e8cf47be1374bba5e2ff64f
14km <k.connor614@gmail.com>
2022-06-14 01:12:05 +0900

    feat: Messages 추가

---
+++ src/main/java/nextstep/subway/common/Messages.java
@@ -0,0 +1,7 @@
+package nextstep.subway.common;
+
+public class Messages {
+    public static final String ALREADY_REGISTERED_STATION = "[ERROR] 이미 등록된 구간 입니다.";
+    public static final String UNREGISTERED_STATION = "[ERROR] 등록할 수 없는 구간 입니다.";
+
+}
