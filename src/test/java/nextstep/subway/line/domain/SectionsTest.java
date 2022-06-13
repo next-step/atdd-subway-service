e1054dde64a9f6efee07d2c754f8ade7dbb80a5a
chj1768 <chj1768@gmail.com>
2022-06-13 21:30:36 +0900

    refactor: Sections 유닛테스트 케이스별로 정리

--- src/test/java/nextstep/subway/line/domain/SectionsTest.java
+++ src/test/java/nextstep/subway/line/domain/SectionsTest.java
@@ -20,62 +20,109 @@ class SectionsTest {
         교대역 = new Station("교대역");
         양재역 = new Station("양재역");
         남부터미널역 = new Station("남부터미널역");
-        고속터미널역 = new Station("고속터미널");
-        삼호선 = new Line("3호선", "orange darken-1", 교대역, 양재역, 10);
-
+        고속터미널역 = new Station("고속터미널역");
+        삼호선 = new Line("3호선", "orange darken-1");
         sections = new Sections();
+        sections.add(new Section(삼호선, 교대역, 양재역, 10));
     }
 
     @Test
-    void getStations_is_not_empty() {
+    void 포함된_역_목록_조회() {
         //when
-        sections.add(new Section(삼호선, 고속터미널역, 교대역, 3));
-        sections.add(new Section(삼호선, 교대역, 남부터미널역, 3));
-        sections.add(new Section(삼호선, 남부터미널역, 양재역, 3));
+        //then
+        assertThat(sections.getStations()).isNotEmpty().containsExactlyInAnyOrder(교대역, 양재역);
+    }
+
+    @Test
+    void 상행_종점_하행_종점_사이에_구간_삭제() {
+        //given
+        상행_종점_하행_종점_사이에_구간_추가();
+
+        //when
+        sections.removeLineStation(삼호선, 남부터미널역);
 
         //then
-        assertThat(sections.getStations()).isNotEmpty().containsExactlyInAnyOrder(교대역, 양재역, 남부터미널역, 고속터미널역);
+        assertThat(sections.getStations()).containsExactlyInAnyOrder(교대역, 양재역);
     }
 
     @Test
-    void remove_is_not_empty() {
+    void 하행_종점_축소() {
         //given
-        sections.add(new Section(삼호선, 고속터미널역, 교대역, 3));
-        sections.add(new Section(삼호선, 교대역, 남부터미널역, 3));
-        sections.add(new Section(삼호선, 남부터미널역, 양재역, 3));
+        상행_종점_하행_종점_사이에_구간_추가();
 
         //when
         sections.removeLineStation(삼호선, 양재역);
 
         //then
-        assertThat(sections.getStations()).containsExactlyInAnyOrder(교대역, 남부터미널역, 고속터미널역);
+        assertThat(sections.getStations()).containsExactlyInAnyOrder(교대역, 남부터미널역);
     }
 
     @Test
-    void add_is_not_empty() {
+    void 상행_종점_축소() {
         //given
-        sections.add(new Section(삼호선, 고속터미널역, 교대역, 3));
-        sections.add(new Section(삼호선, 교대역, 남부터미널역, 3));
-        sections.add(new Section(삼호선, 남부터미널역, 양재역, 3));
+        상행_종점_하행_종점_사이에_구간_추가();
 
         //when
-        Station 신사역 = new Station("신사역");
-        sections.addLineStation(삼호선, 신사역, 고속터미널역, 5);
+        sections.removeLineStation(삼호선, 교대역);
 
         //then
-        assertThat(sections.getStations()).containsExactlyInAnyOrder(양재역, 교대역, 신사역, 남부터미널역, 고속터미널역);
+        assertThat(sections.getStations()).containsExactlyInAnyOrder(남부터미널역, 양재역);
     }
 
     @Test
-    void getStations_is_empty() {
+    void 상행_종점_하행_종점_사이에_구간_추가() {
+        //when
+        sections.addLineStation(삼호선, 교대역, 남부터미널역, 5);
+
+        //then
+        assertThat(sections.getStations()).containsExactlyInAnyOrder(교대역, 남부터미널역, 양재역);
+    }
+
+    @Test
+    void 하행_종점_연장() {
+        //when
+        sections.addLineStation(삼호선, 남부터미널역, 교대역, 5);
+
+        //then
+        assertThat(sections.getStations()).containsExactlyInAnyOrder(남부터미널역, 교대역, 양재역);
+    }
+
+    @Test
+    void 상행_종점_연장() {
+        //when
+        sections.addLineStation(삼호선, 양재역, 남부터미널역, 5);
+
+        //then
+        assertThat(sections.getStations()).containsExactlyInAnyOrder(교대역, 양재역, 남부터미널역);
+    }
+
+    @Test
+    void 구간_비었을_때_포함된_역_목록_조회() {
+        //when
+        sections = new Sections();
         //then
         assertThat(sections.getStations()).isEmpty();
     }
 
     @Test
-    void remove_is_empty() {
+    void 구간이_비어있으면_역을_삭제할_수_없다() {
+        //when
+        sections = new Sections();
         //then
         assertThatThrownBy(() -> sections.removeLineStation(삼호선, 양재역)).isExactlyInstanceOf(RuntimeException.class);
     }
 
+    @Test
+    void 이미_포함된_구간은_추가할_수_없다() {
+        //then
+        assertThatThrownBy(() -> sections.addLineStation(삼호선, 교대역, 양재역, 20)).isExactlyInstanceOf(
+                RuntimeException.class);
+    }
+
+    @Test
+    void 구간_내_상행_하행_역_중_하나라도_포함되어_있지_않으면_추가할_수_없다() {
+        //then
+        assertThatThrownBy(() -> sections.addLineStation(삼호선, 남부터미널역, 고속터미널역, 1)).isExactlyInstanceOf(
+                RuntimeException.class);
+    }
 }
