f8554b2b9081db7dc34efd10557e1c5d0ad8b3e3
iamjunsulee <mellow0924@gmail.com>
2022-06-13 23:30:09 +0900

    test(JgraphTest): 역과 역 사이 최단거리 학습 테스트 작성

--- src/test/java/study/jgraph/JgraphTest.java
+++ src/test/java/study/jgraph/JgraphTest.java
@@ -1,10 +1,13 @@
 package study.jgraph;
 
+import java.util.stream.Collectors;
+import nextstep.subway.station.domain.Station;
 import org.jgrapht.GraphPath;
 import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
 import org.jgrapht.alg.shortestpath.KShortestPaths;
 import org.jgrapht.graph.DefaultWeightedEdge;
 import org.jgrapht.graph.WeightedMultigraph;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -51,5 +54,35 @@ public class JgraphTest {
                     assertThat(it.getVertexList()).startsWith(source);
                     assertThat(it.getVertexList()).endsWith(target);
                 });
+    }
+
+    @Test
+    @DisplayName("역과 역사이 최단거리를 구한다.")
+    void getDijkstraShortestPathWithStation() {
+        Station 공덕역 = new Station("공덕역");
+        Station 여의도역 = new Station("여의도역");
+        Station 합정역 = new Station("합정역");
+        Station 당산역 = new Station("당산역");
+
+        WeightedMultigraph<Station, DefaultWeightedEdge> graph = new WeightedMultigraph<>(DefaultWeightedEdge.class);
+        graph.addVertex(공덕역);
+        graph.addVertex(여의도역);
+        graph.addVertex(합정역);
+        graph.addVertex(당산역);
+        graph.setEdgeWeight(graph.addEdge(공덕역, 합정역), 4);
+        graph.setEdgeWeight(graph.addEdge(합정역, 당산역), 1);
+        graph.setEdgeWeight(graph.addEdge(공덕역, 여의도역), 4);
+        graph.setEdgeWeight(graph.addEdge(여의도역, 당산역), 2);
+
+        DijkstraShortestPath<Station, DefaultWeightedEdge> dijkstraShortestPath = new DijkstraShortestPath<>(graph);
+        GraphPath<Station, DefaultWeightedEdge> path = dijkstraShortestPath.getPath(공덕역, 당산역);
+        List<Station> paths = path.getVertexList();
+        List<String> names = paths.stream()
+                .map(Station::getName)
+                .collect(Collectors.toList());
+
+        assertThat(paths.size()).isEqualTo(3);
+        assertThat(names).containsExactly("공덕역", "합정역", "당산역");
+        assertThat(path.getWeight()).isEqualTo(5);
     }
 }
