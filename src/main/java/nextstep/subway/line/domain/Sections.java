27f3cd0f83ee9daea321dad39cf92afd4e3052c6
chj1768 <chj1768@gmail.com>
2022-06-13 17:40:08 +0900

    refactor: line 내 구간들이 포함하고 있는 역 목록 조회 가능하도록 Sections 에 로직 추가, 유닛 테스트 추가

--- src/main/java/nextstep/subway/line/domain/Sections.java
+++ src/main/java/nextstep/subway/line/domain/Sections.java
@@ -1,10 +1,14 @@
 package nextstep.subway.line.domain;
 
+import nextstep.subway.station.domain.Station;
+
 import javax.persistence.CascadeType;
 import javax.persistence.Embeddable;
 import javax.persistence.OneToMany;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 @Embeddable
 public class Sections {
@@ -24,5 +28,43 @@ public class Sections {
 
     public void add(Section section) {
         this.sectionList.add(section);
+    }
+
+    public List<Station> getStations() {
+        if (this.sectionList.isEmpty()) {
+            return Arrays.asList();
+        }
+
+        List<Station> stations = new ArrayList<>();
+        Station downStation = findUpStation();
+        stations.add(downStation);
+
+        while (downStation != null) {
+            Station finalDownStation = downStation;
+            Optional<Section> nextLineStation =
+                    this.sectionList.stream().filter(it -> it.getUpStation() == finalDownStation).findFirst();
+            if (!nextLineStation.isPresent()) {
+                break;
+            }
+            downStation = nextLineStation.get().getDownStation();
+            stations.add(downStation);
+        }
+
+        return stations;
+    }
+
+    private Station findUpStation() {
+        Station downStation = this.sectionList.get(0).getUpStation();
+        while (downStation != null) {
+            Station finalDownStation = downStation;
+            Optional<Section> nextLineStation =
+                    this.sectionList.stream().filter(it -> it.getDownStation() == finalDownStation).findFirst();
+            if (!nextLineStation.isPresent()) {
+                break;
+            }
+            downStation = nextLineStation.get().getUpStation();
+        }
+
+        return downStation;
     }
 }
---
+++ src/test/java/nextstep/subway/line/domain/SectionsTest.java
@@ -0,0 +1,45 @@
+package nextstep.subway.line.domain;
+
+import nextstep.subway.station.domain.Station;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class SectionsTest {
+    private Sections sections;
+    private Station 교대역;
+    private Station 양재역;
+    private Station 남부터미널역;
+    private Station 고속터미널역;
+    private Line 삼호선;
+
+    @BeforeEach
+    void setUp() {
+        교대역 = new Station("교대역");
+        양재역 = new Station("양재역");
+        남부터미널역 = new Station("남부터미널역");
+        고속터미널역 = new Station("고속터미널");
+        삼호선 = new Line("3호선", "orange darken-1", 교대역, 양재역, 10);
+
+        sections = new Sections();
+    }
+
+    @Test
+    void getStations_is_not_empty() {
+        //when
+        sections.add(new Section(삼호선, 고속터미널역, 교대역, 3));
+        sections.add(new Section(삼호선, 교대역, 남부터미널역, 3));
+        sections.add(new Section(삼호선, 남부터미널역, 양재역, 3));
+
+        //then
+        assertThat(sections.getStations()).isNotEmpty().containsExactlyInAnyOrder(교대역, 양재역, 남부터미널역, 고속터미널역);
+    }
+
+    @Test
+    void getStations_is_empty() {
+        //then
+        assertThat(sections.getStations()).isEmpty();
+    }
+
+}
