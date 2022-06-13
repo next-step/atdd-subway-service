377b0f0098247f9679e88636279d55891d3c7f18
tyakamyz <tyakamyz@naver.com>
2022-06-13 16:35:34 +0900

    rename: 오타 수정

--- src/main/java/nextstep/subway/favorite/domain/FavoriteRepository.java
+++ src/main/java/nextstep/subway/favorite/domain/FavoriteRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface FavoriteRepository extends JpaRepository<Favorite, Long> {
-    List<Favorite> findAllByMemberId(Long MemberId);
+    List<Favorite> findAllByMemberId(Long memberId);
 }
