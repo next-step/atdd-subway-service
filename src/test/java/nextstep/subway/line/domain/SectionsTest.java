9a8936bf1302b43b6136b82119b844a91bddb362
sangho lee <sangholee.dev@gmail.com>
2022-06-13 22:45:09 +0900

    refactor: Sections 테스트 추가

---
+++ src/test/java/nextstep/subway/line/domain/SectionsTest.java
@@ -0,0 +1,46 @@
+package nextstep.subway.line.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Arrays;
+import java.util.List;
+import nextstep.subway.station.domain.Station;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class SectionsTest {
+
+    Station 강남역;
+    Station 판교역;
+    Station 광교역;
+    Line 신분당선;
+    Section 강남역_판교역;
+    Section 판교역_광교역;
+
+    @BeforeEach
+    void setUp() {
+        강남역 = new Station("강남역");
+        판교역 = new Station("판교역");
+        광교역 = new Station("광교역");
+        신분당선 = new Line("신분당선", "bg-red-600");
+        강남역_판교역 = new Section(신분당선, 강남역, 판교역, 5);
+        판교역_광교역 = new Section(신분당선, 판교역, 광교역, 8);
+    }
+
+    @Test
+    void getStations() {
+        // given
+        final Sections sections = new Sections(Arrays.asList(강남역_판교역, 판교역_광교역));
+
+        // when
+        final List<Station> stations = sections.getStations();
+
+        // then
+        assertAll(
+                () -> assertThat(stations).hasSize(3),
+                () -> assertThat(stations).containsExactly(강남역, 판교역, 광교역)
+        );
+
+    }
+}
