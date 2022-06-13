9918d32f25a13dade17b3e0c3c6aa9d575df6076
14km <k.connor614@gmail.com>
2022-06-14 02:07:51 +0900

    feat: 에러메세지 추가

--- src/main/java/nextstep/subway/common/Messages.java
+++ src/main/java/nextstep/subway/common/Messages.java
@@ -4,4 +4,7 @@ public class Messages {
     public static final String ALREADY_REGISTERED_STATION = "[ERROR] 이미 등록된 구간 입니다.";
     public static final String UNREGISTERED_STATION = "[ERROR] 등록할 수 없는 구간 입니다.";
 
+    public static final String NOT_FOUND_REMOVE_STATION = "[ERROR] 노선 삭제할 노선은 최소 1개이상 존재해야합니다.";
+    public static final String NOT_MATCH_REMOVE_STATION = "[ERROR] 삭제할 노선의 정보가 없습니다.";
+
 }
