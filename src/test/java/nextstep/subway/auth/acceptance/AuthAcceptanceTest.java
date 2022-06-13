349c53da1b797050236e02b0127bb8ae1ebaee93
Donghee Han <handh0413@gmail.com>
2022-06-13 20:33:15 +0900

    test: 로그인 성공 인수 테스트 작성

--- src/test/java/nextstep/subway/auth/acceptance/AuthAcceptanceTest.java
+++ src/test/java/nextstep/subway/auth/acceptance/AuthAcceptanceTest.java
@@ -1,14 +1,35 @@
 package nextstep.subway.auth.acceptance;
 
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
 import nextstep.subway.AcceptanceTest;
+import nextstep.subway.auth.dto.TokenRequest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 
+import static nextstep.subway.member.MemberAcceptanceTest.회원_생성을_요청;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DisplayName("로그인 기능")
 public class AuthAcceptanceTest extends AcceptanceTest {
+    public static final String EMAIL = "email@email.com";
+    public static final String PASSWORD = "password";
+    public static final int AGE = 20;
 
-    @DisplayName("Bearer Auth")
+    @DisplayName("로그인을 성공한다.")
     @Test
     void myInfoWithBearerAuth() {
+        // given
+        회원_생성을_요청(EMAIL, PASSWORD, AGE);
+
+        // when
+        ExtractableResponse<Response> response = 로그인_요청(new TokenRequest(EMAIL, PASSWORD));
+
+        // then
+        로그인_성공(response);
     }
 
     @DisplayName("Bearer Auth 로그인 실패")
@@ -21,4 +42,16 @@ public class AuthAcceptanceTest extends AcceptanceTest {
     void myInfoWithWrongBearerAuth() {
     }
 
+    public static ExtractableResponse<Response> 로그인_요청(TokenRequest params) {
+        return RestAssured
+                .given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(params)
+                .when().post("/login/token")
+                .then().log().all().extract();
+    }
+
+    public static void 로그인_성공(ExtractableResponse<Response> response) {
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+    }
 }
