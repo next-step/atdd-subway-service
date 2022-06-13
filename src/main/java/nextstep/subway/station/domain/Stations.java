854c1e7c415e425f0ec0a467ec058ba60248b14c
14km <k.connor614@gmail.com>
2022-06-13 13:35:02 +0900

    feat: Stations 도메인 추가

--- src/main/java/nextstep/subway/station/domain/Stations.java
+++ src/main/java/nextstep/subway/station/domain/Stations.java
@@ -2,6 +2,7 @@ package nextstep.subway.station.domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Stations {
     private final List<Station> stations = new ArrayList<>();
@@ -12,5 +13,15 @@ public class Stations {
 
     public List<Station> getStations() {
         return stations;
+    }
+
+    public List<Station> distinctStations() {
+        return stations.stream()
+                .distinct()
+                .collect(Collectors.toList());
+    }
+
+    public boolean isNotEmpty(){
+        return !stations.isEmpty();
     }
 }
