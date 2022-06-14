d262d9292503f68c5f2e3eedebce0aab875861ce
kms <kmmin78@gmail.com>
2022-06-15 00:21:56 +0900

    fix step2 테스트 픽스쳐 생성 수정

--- src/test/java/nextstep/subway/path/PathAcceptanceTest.java
+++ src/test/java/nextstep/subway/path/PathAcceptanceTest.java
@@ -23,7 +23,6 @@ public class PathAcceptanceTest extends AcceptanceTest {
     private StationResponse 샛강역;
     private StationResponse 여의나루역;
 
-
     @BeforeEach
     public void setUp() {
         super.setUp();
@@ -34,12 +33,12 @@ public class PathAcceptanceTest extends AcceptanceTest {
         샛강역 = StationAcceptanceTest.지하철역_등록되어_있음("샛강역").as(StationResponse.class);
         여의나루역 = StationAcceptanceTest.지하철역_등록되어_있음("여의나루역").as(StationResponse.class);
 
-        일호선 = 지하철_노선_등록되어_있음("일호선", "bg-blue-600", 노량진역, 대방역, 7);
+        일호선 = 지하철_노선_등록되어_있음("일호선", "bg-blue-600", 노량진역, 신길역, 10);
         구호선 = 지하철_노선_등록되어_있음("구호선", "bg-yellow-600", 노량진역, 샛강역, 5);
-        오호선 = 지하철_노선_등록되어_있음("오호선", "bg-purple-600", 신길역, 여의도역, 10);
+        오호선 = 지하철_노선_등록되어_있음("오호선", "bg-purple-600", 신길역, 여의나루역, 12);
 
-        지하철_노선에_지하철역_등록_요청(일호선, 노량진역, 신길역, 10);
+        지하철_노선에_지하철역_등록_요청(일호선, 노량진역, 대방역, 7);
         지하철_노선에_지하철역_등록_요청(구호선, 샛강역, 여의도역, 8);
-        지하철_노선에_지하철역_등록_요청(오호선, 신길역, 여의나루역, 12);
+        지하철_노선에_지하철역_등록_요청(오호선, 신길역, 여의도역, 10);
     }
 }
