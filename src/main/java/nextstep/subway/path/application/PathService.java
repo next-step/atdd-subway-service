8047871de27e11bc10295c10e331766da8d29c37
iamjunsulee <mellow0924@gmail.com>
2022-06-14 10:03:53 +0900

    feat(PathController, PathService): controller 작성

---
+++ src/main/java/nextstep/subway/path/application/PathService.java
@@ -0,0 +1,11 @@
+package nextstep.subway.path.application;
+
+import nextstep.subway.path.dto.PathResponse;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PathService {
+    public PathResponse findShortestPath(Long sourceStationId, Long targetStationId) {
+        return null;
+    }
+}
