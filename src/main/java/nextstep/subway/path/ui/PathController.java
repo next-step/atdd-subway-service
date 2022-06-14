a69f11b02de14b380bae70db76cfd5a3ee40e805
Celina <luv.mins99@gmail.com>
2022-06-14 13:05:06 +0900

    feat(PathController): 최단 경로 조회 기능 구현
    - 예외 처리 추가

--- src/main/java/nextstep/subway/path/ui/PathController.java
+++ src/main/java/nextstep/subway/path/ui/PathController.java
@@ -1,20 +1,42 @@
 package nextstep.subway.path.ui;
 
+import nextstep.subway.common.NotFoundException;
 import nextstep.subway.path.application.PathService;
+import nextstep.subway.path.dto.PathRequest;
 import nextstep.subway.path.dto.PathResponse;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/paths")
 public class PathController {
 
-    private PathService pathService;
+    private final PathService pathService;
 
-    @GetMapping("/paths")
+    public PathController(final PathService pathService) {
+        this.pathService = pathService;
+    }
+
+    @GetMapping
     public ResponseEntity<PathResponse> findShortestPath(@RequestParam Long source, @RequestParam Long target) {
-        PathResponse pathResponse = pathService.findPath(source, target);
-        return ResponseEntity.ok().body(pathResponse);
+        PathRequest pathRequest = new PathRequest(source, target);
+        return ResponseEntity.ok().body(pathService.findPath(pathRequest));
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity handleIllegalArgumentException(IllegalArgumentException e) {
+        return ResponseEntity.badRequest().body(e.getMessage());
+    }
+
+    @ResponseStatus(value = HttpStatus.NOT_FOUND)
+    @ExceptionHandler(NotFoundException.class)
+    public String handleNotFoundException(NotFoundException e) {
+        return e.getMessage();
     }
 }
