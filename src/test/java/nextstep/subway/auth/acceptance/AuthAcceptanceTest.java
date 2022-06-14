42f74399b491e239e753adcdbf446f9bb9c091c2
otakjong <tj3828@naver.com>
2022-06-14 16:15:23 +0900

    test: Auth 인수테스트 작성

--- src/test/java/nextstep/subway/auth/acceptance/AuthAcceptanceTest.java
+++ src/test/java/nextstep/subway/auth/acceptance/AuthAcceptanceTest.java
@@ -1,24 +1,59 @@
 package nextstep.subway.auth.acceptance;
 
+import static nextstep.subway.member.MemberAcceptanceTest.AGE;
+import static nextstep.subway.member.MemberAcceptanceTest.EMAIL;
+import static nextstep.subway.member.MemberAcceptanceTest.PASSWORD;
+import static nextstep.subway.member.MemberAcceptanceTest.회원_생성을_요청;
+import static nextstep.subway.member.MemberAcceptanceTest.회원_정보_조회_요청;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
 import nextstep.subway.AcceptanceTest;
+import nextstep.subway.auth.dto.TokenRequest;
+import nextstep.subway.auth.dto.TokenResponse;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
 
 public class AuthAcceptanceTest extends AcceptanceTest {
+
+    private ExtractableResponse<Response> defaultMemberResponse;
+
+    @BeforeEach
+    public void setUp() {
+        super.setUp();
+        defaultMemberResponse = 회원_생성을_요청(EMAIL, PASSWORD, AGE);
+    }
 
     @DisplayName("Bearer Auth")
     @Test
     void myInfoWithBearerAuth() {
+        ExtractableResponse<Response> response = 토큰_생성이_요청됨(EMAIL, PASSWORD);
+
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+        assertThat(response.as(TokenResponse.class).getAccessToken()).isNotEmpty();
     }
 
     @DisplayName("Bearer Auth 로그인 실패")
     @Test
     void myInfoWithBadBearerAuth() {
+        ExtractableResponse<Response> response = 토큰_생성이_요청됨("tj3828@naver.com", "failPassword");
+
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.UNAUTHORIZED.value());
     }
 
     @DisplayName("Bearer Auth 유효하지 않은 토큰")
     @Test
     void myInfoWithWrongBearerAuth() {
+        ExtractableResponse<Response> response = 회원_정보_조회_요청(defaultMemberResponse);
+
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.UNAUTHORIZED.value());
+    }
+
+    private ExtractableResponse<Response> 토큰_생성이_요청됨(String email, String password) {
+        return post(new TokenRequest(email, password), "/login/token");
     }
 
 }
