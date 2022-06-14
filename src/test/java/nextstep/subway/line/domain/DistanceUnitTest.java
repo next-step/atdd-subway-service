5a75932713025216f64e3783380f0a6ea8657a34
yong2ss <korea92co@gmail.com>
2022-06-14 22:36:14 +0900

    refac(DistanceUnitTest.java) : Test 관련 문구 추가

--- src/test/java/nextstep/subway/line/domain/DistanceUnitTest.java
+++ src/test/java/nextstep/subway/line/domain/DistanceUnitTest.java
@@ -2,6 +2,7 @@ package nextstep.subway.line.domain;
 
 import nextstep.subway.line.exception.DistanceException;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -17,12 +18,14 @@ public class DistanceUnitTest {
     }
 
     @Test
+    @DisplayName("길이 10에서 5를 빼면 5가 된다.")
     void minus() {
         distance_10.minus(5);
         assertThat(distance_10.getDistance()).isEqualTo(5);
     }
 
     @Test
+    @DisplayName("길이 10에서 10보다 같거나 큰 수를 빼면 뺄 수 없다.")
     void minus_exception() {
         assertThatThrownBy(() -> {
             distance_10.minus(10);
