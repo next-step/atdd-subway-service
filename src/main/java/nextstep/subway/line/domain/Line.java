da021145d7cece411118dd4d13b0e55b60bb6f16
tasklet1579 <tasklet1579@gmail.com>
2022-06-14 15:43:42 +0900

    refactor : 지하철 구간 삭제 기존 로직 리팩토링

--- src/main/java/nextstep/subway/line/domain/Line.java
+++ src/main/java/nextstep/subway/line/domain/Line.java
@@ -146,6 +146,49 @@ public class Line extends BaseEntity {
         }
     }
 
+    public void removeLineStation(Station station) {
+        validateStationCount();
+        validateStationInclusion(station);
+
+        if (isUpStation(station)) {
+            Section section = sections.stream()
+                                      .filter(sec -> sec.getUpStation().equals(station))
+                                      .findFirst().orElseThrow(RuntimeException::new);
+            sections.stream()
+                    .filter(sec -> sec.getDownStation().equals(station))
+                    .findFirst()
+                    .ifPresent(sec -> sec.updateDownStation(section.getDownStation(), -section.getDistance()));
+            sections.remove(section);
+            return;
+        }
+
+        Section section = sections.stream()
+                                  .filter(sec -> sec.getDownStation().equals(station))
+                                  .findFirst().orElseThrow(RuntimeException::new);
+
+        sections.stream()
+                .filter(sec -> sec.getUpStation().equals(station))
+                .findFirst()
+                .ifPresent(sec -> sec.updateUpStation(section.getUpStation(), -section.getDistance()));
+        sections.remove(section);
+    }
+
+    public void validateStationCount() {
+        if (sections.size() <= 1) {
+            throw new RuntimeException("마지막 구간은 삭제할 수 없습니다.");
+        }
+    }
+
+    public void validateStationInclusion(Station station) {
+        if (getStations().stream().noneMatch(st -> st.equals(station))) {
+            throw new RuntimeException(String.format("%s 노선에 %s 역이 존재하지 않습니다.", name, station.getName()));
+        }
+    }
+
+    public boolean isUpStation(Station station) {
+        return sections.stream().anyMatch(section -> section.getUpStation().equals(station));
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
