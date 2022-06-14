e949ff8c36a10be8bdddb05bff8e4c418be5cb33
jhsong2580 <thdwmdgns@naver.com>
2022-06-14 12:38:13 +0900

    feat: foreign key naming convention

--- src/main/java/nextstep/subway/favorite/domain/Favorite.java
+++ src/main/java/nextstep/subway/favorite/domain/Favorite.java
@@ -2,9 +2,11 @@ package nextstep.subway.favorite.domain;
 
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
+import javax.persistence.ForeignKey;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import nextstep.subway.member.domain.Member;
 import nextstep.subway.station.domain.Station;
@@ -17,12 +19,15 @@ public class Favorite {
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "SOURCE_STATION_ID", foreignKey = @ForeignKey(name = "fk_Favorite_Source_Station"))
     private Station source;
 
     @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "TARGET_STATION_ID", foreignKey = @ForeignKey(name = "fk_Favorite_Target_Station"))
     private Station target;
 
     @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "MEMBER_ID", foreignKey = @ForeignKey(name = "fk_Favorite_Member"))
     private Member member;
 
     protected Favorite() {
