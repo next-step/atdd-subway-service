2fffacc4499b0ee7133a5b731c383f6169e82746
Celina <luv.mins99@gmail.com>
2022-06-14 12:58:59 +0900

    feat(PathResponse): 최단 경로 조회 응답 객체 구현

--- src/main/java/nextstep/subway/path/dto/PathResponse.java
+++ src/main/java/nextstep/subway/path/dto/PathResponse.java
@@ -1,18 +1,31 @@
 package nextstep.subway.path.dto;
 
 import java.util.List;
+import java.util.stream.Collectors;
 import nextstep.subway.station.domain.Station;
+import nextstep.subway.station.dto.StationResponse;
 
 public class PathResponse {
-    private final List<Station> stations;
-    private final int distance;
+    private List<StationResponse> stations;
+    private int distance;
 
-    public PathResponse(final List<Station> stations, final int distance) {
+    public PathResponse() {
+
+    }
+
+    private PathResponse(final List<StationResponse> stations, final int distance) {
         this.stations = stations;
         this.distance = distance;
     }
 
-    public List<Station> getStations() {
+    public static PathResponse of(final List<Station> stations, final double distance) {
+        List<StationResponse> stationResponseList = stations.stream()
+                .map(StationResponse::of)
+                .collect(Collectors.toList());
+        return new PathResponse(stationResponseList, (int) distance);
+    }
+
+    public List<StationResponse> getStations() {
         return stations;
     }
 
