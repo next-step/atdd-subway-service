52e8f2fe7903877a98ad91e6e466a2bb037d79fe
mannue <sppe12@gmail.com>
2022-06-16 02:07:50 +0900

    test(AuthController, AuthAcceptanceTest): 사용자가 등록된 상태에서의 요청 과 비 등록 사용자인 경우의 요청

--- src/main/java/nextstep/subway/auth/ui/AuthController.java
+++ src/main/java/nextstep/subway/auth/ui/AuthController.java
@@ -3,6 +3,7 @@ package nextstep.subway.auth.ui;
 import nextstep.subway.auth.application.AuthService;
 import nextstep.subway.auth.dto.TokenRequest;
 import nextstep.subway.auth.dto.TokenResponse;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,13 +11,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AuthController {
-    private AuthService authService;
+    private final AuthService authService;
 
     public AuthController(AuthService authService) {
         this.authService = authService;
     }
 
-    @PostMapping("/login/token")
+    @PostMapping(value = "/login/token", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<TokenResponse> login(@RequestBody TokenRequest request) {
         TokenResponse token = authService.login(request);
         return ResponseEntity.ok().body(token);
--- src/test/java/nextstep/subway/auth/acceptance/AuthAcceptanceTest.java
+++ src/test/java/nextstep/subway/auth/acceptance/AuthAcceptanceTest.java
@@ -1,19 +1,83 @@
 package nextstep.subway.auth.acceptance;
 
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
 import nextstep.subway.AcceptanceTest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static nextstep.subway.member.MemberAcceptanceTest.회원_생성됨;
+import static nextstep.subway.member.MemberAcceptanceTest.회원_생성을_요청;
+import static org.assertj.core.api.Assertions.assertThat;
+
 
 public class AuthAcceptanceTest extends AcceptanceTest {
 
+    /**
+     * Given : 회원 등록이 되어 있음
+     * When : 로그인 요청
+     * Then : 로그인 됨
+     */
     @DisplayName("Bearer Auth")
     @Test
     void myInfoWithBearerAuth() {
+        // Given
+        final String PASSWORD = "password";
+        final String EMAIL = "email@email.com";
+        ExtractableResponse<Response> createResponse = 회원_생성을_요청(EMAIL, PASSWORD, 20);
+        회원_생성됨(createResponse);
+
+        // When
+        ExtractableResponse<Response> response = 로그인_요청(암호_이메일_입력(PASSWORD, EMAIL));
+
+        // Then
+        로그인_됨(response);
     }
 
+    /**
+     * Given : 비회원 사용자가
+     * When : 로그인 시도 시
+     * Then : 로그인이 실패 된다.
+     */
     @DisplayName("Bearer Auth 로그인 실패")
     @Test
     void myInfoWithBadBearerAuth() {
+        // Given
+        final String INVALID_PASSWORD = "badPassword";
+        final String INVALID_EMAIL = "bad@email.com";
+
+        //when
+        ExtractableResponse<Response> response = 로그인_요청(암호_이메일_입력(INVALID_PASSWORD, INVALID_EMAIL));
+
+        //Then
+        로그인_실패됨(response);
+    }
+
+    /**
+     * Given : 등록된 사용자 이지만
+     * When: 정보가 일치하지 않으면
+     * Then: 로그인이 실패 된다.
+     */
+    @DisplayName("등록된 사용자 이지만 정보가 일치 하지 않으면 로그인이 실패")
+    @Test
+    void invalidUserInfoWithBearerAuth() {
+        // Given
+        final String PASSWORD = "password";
+        final String EMAIL = "email@email.com";
+        ExtractableResponse<Response> createResponse = 회원_생성을_요청(EMAIL, PASSWORD, 20);
+        회원_생성됨(createResponse);
+
+        // When
+        ExtractableResponse<Response> response = 로그인_요청(암호_이메일_입력("noMatch", EMAIL));
+
+        //Then
+        로그인_실패됨(response);
     }
 
     @DisplayName("Bearer Auth 유효하지 않은 토큰")
@@ -21,4 +85,31 @@ public class AuthAcceptanceTest extends AcceptanceTest {
     void myInfoWithWrongBearerAuth() {
     }
 
+    public static void 로그인_실패됨(ExtractableResponse<Response> response) {
+        assertThat(HttpStatus.valueOf(response.statusCode())).isEqualTo(HttpStatus.UNAUTHORIZED);
+    }
+
+    public static void 로그인_됨(ExtractableResponse<Response> response) {
+        assertThat(HttpStatus.valueOf(response.statusCode())).isEqualTo(HttpStatus.OK);
+        String accessToken = response.jsonPath().get("accessToken");
+        assertThat(accessToken).isNotBlank();
+    }
+
+    public static ExtractableResponse<Response> 로그인_요청(Map<String, String> body) {
+        return RestAssured.given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when()
+                .body(body)
+                .post("/login/token")
+                .then()
+                .log().all()
+                .extract();
+    }
+
+    public static Map<String, String> 암호_이메일_입력(final String password, final String email) {
+        return new HashMap<String, String>() {{
+            put("password", password);
+            put("email", email);
+        }};
+    }
 }
