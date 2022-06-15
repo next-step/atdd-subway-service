d98e13682422ae4628bc63d98ac79b57c5840b74
tyakamyz <tyakamyz@naver.com>
2022-06-15 22:23:22 +0900

    rename: 나이정책과 거리정책을 구분하기 위해 클래스명 변경

--- src/main/java/nextstep/subway/path/domain/FareDistancePolicy.java
+++ src/main/java/nextstep/subway/path/domain/FareDistancePolicy.java
@@ -3,7 +3,7 @@ package nextstep.subway.path.domain;
 import java.util.Arrays;
 import java.util.Optional;
 
-public enum FarePolicy {
+public enum FareDistancePolicy {
     MORE_THAN_10_AND_LESS_THAN_50(10, 50, 5, 100),
     MORE_THAN_50(50, Integer.MAX_VALUE, 8, 100);
 
@@ -14,19 +14,19 @@ public enum FarePolicy {
     private final int overFare;
     private final int distanceStandardValue;
 
-    FarePolicy(int min, int max, int distanceStandardValue, int overFare) {
+    FareDistancePolicy(int min, int max, int distanceStandardValue, int overFare) {
         this.min = min;
         this.max = max;
         this.overFare = overFare;
         this.distanceStandardValue = distanceStandardValue;
     }
 
-    public static Optional<FarePolicy> findFarePolicyByDistance(int distance) {
+    public static Optional<FareDistancePolicy> findFarePolicyByDistance(int distance) {
         return Arrays.stream(values()).filter(value -> value.isRange(distance)).findFirst();
     }
 
     private boolean isRange(int distance) {
-        return min < distance && max <= distance;
+        return distance > min && distance <= max;
     }
 
     public int getMin() {
