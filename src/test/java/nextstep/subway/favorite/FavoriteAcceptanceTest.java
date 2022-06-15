ef098ed8043a06e47335feb2aafd6ce5805d55ae
gunan <power4889@gmail.com>
2022-06-15 13:32:32 +0900

    test: 즐겨찾기 관리 인수테스트

--- src/test/java/nextstep/subway/favorite/FavoriteAcceptanceTest.java
+++ src/test/java/nextstep/subway/favorite/FavoriteAcceptanceTest.java
@@ -1,8 +1,128 @@
 package nextstep.subway.favorite;
 
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
 import nextstep.subway.AcceptanceTest;
+import nextstep.subway.favorite.dto.FavoriteRequest;
+import nextstep.subway.line.dto.LineRequest;
+import nextstep.subway.line.dto.LineResponse;
+import nextstep.subway.station.dto.StationResponse;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+import static nextstep.subway.auth.acceptance.AuthAcceptanceTest.로그인_되어있음;
+import static nextstep.subway.line.acceptance.LineAcceptanceTest.지하철_노선_등록되어_있음;
+import static nextstep.subway.line.acceptance.LineSectionAcceptanceTest.지하철_노선에_지하철역_등록_요청;
+import static nextstep.subway.member.MemberAcceptanceTest.*;
+import static nextstep.subway.station.StationAcceptanceTest.지하철역_등록되어_있음;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("즐겨찾기 관련 기능")
 public class FavoriteAcceptanceTest extends AcceptanceTest {
+    private LineResponse 신분당선;
+    private LineResponse 이호선;
+    private LineResponse 삼호선;
+    private StationResponse 강남역;
+    private StationResponse 양재역;
+    private StationResponse 교대역;
+    private StationResponse 남부터미널역;
+    private RequestSpecification 사용자;
+
+    /**
+     * Given 지하철역 등록되어 있음
+     * And 지하철 노선 등록되어 있음
+     * And 지하철 노선에 지하철역 등록되어 있음
+     * And 회원 등록되어 있음
+     * And 로그인 되어있음
+     */
+    @BeforeEach
+    public void setUp() {
+        super.setUp();
+
+        // given
+        강남역 = 지하철역_등록되어_있음("강남역").as(StationResponse.class);
+        양재역 = 지하철역_등록되어_있음("양재역").as(StationResponse.class);
+        교대역 = 지하철역_등록되어_있음("교대역").as(StationResponse.class);
+        남부터미널역 = 지하철역_등록되어_있음("남부터미널역").as(StationResponse.class);
+
+        신분당선 = 지하철_노선_등록되어_있음(new LineRequest("신분당선", "bg-red-600", 강남역.getId(), 양재역.getId(), 10)).as(LineResponse.class);
+        이호선 = 지하철_노선_등록되어_있음(new LineRequest("이호선", "bg-red-600", 교대역.getId(), 강남역.getId(), 10)).as(LineResponse.class);
+        삼호선 = 지하철_노선_등록되어_있음(new LineRequest("삼호선", "bg-red-600", 교대역.getId(), 양재역.getId(), 5)).as(LineResponse.class);
+
+        지하철_노선에_지하철역_등록_요청(삼호선, 교대역, 남부터미널역, 3);
+
+        회원_등록되어_있음(EMAIL, PASSWORD, AGE);
+        사용자 = 로그인_되어있음(EMAIL, PASSWORD);
+    }
+
+    /**
+     * When 즐겨찾기 생성을 요청
+     * Then 즐겨찾기 생성됨
+     * When 즐겨찾기 목록 조회 요청
+     * Then 즐겨찾기 목록 조회됨
+     * When 즐겨찾기 삭제 요청
+     * Then 즐겨찾기 삭제됨
+     */
+    @DisplayName("즐겨찾기를 관리한다")
+    @Test
+    void manageFavorite() {
+        // when
+        ExtractableResponse<Response> 즐겨찾기_생성_응답 = 즐겨찾기_생성_요청(사용자, 강남역, 남부터미널역);
+
+        // then
+        String 즐겨찾기 = 즐겨찾기_생성됨(즐겨찾기_생성_응답);
+
+        // when
+        ExtractableResponse<Response> 즐겨찾기_목록_조회_응답 = 즐겨찾기_목록_조회_요청(사용자);
+
+        // then
+        즐겨찾기_목록_조회됨(즐겨찾기_목록_조회_응답);
+
+        // when
+        ExtractableResponse<Response> 즐겨찾기_삭제_응답 = 즐겨찾기_삭제_요청(사용자, 즐겨찾기);
+
+        // then
+        즐겨찾기_삭제됨(즐겨찾기_삭제_응답);
+    }
+
+    public static ExtractableResponse<Response> 즐겨찾기_생성_요청(RequestSpecification authorization, StationResponse source, StationResponse target) {
+        return authorization
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(new FavoriteRequest(source.getId(), target.getId()))
+                .when().post("favorites")
+                .then().log().all()
+                .extract();
+    }
+
+    public static String 즐겨찾기_생성됨(ExtractableResponse<Response> response) {
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+        return response.header("location");
+    }
+
+    public static ExtractableResponse<Response> 즐겨찾기_목록_조회_요청(RequestSpecification authorization) {
+        return authorization
+                .accept(MediaType.APPLICATION_JSON_VALUE)
+                .when().get("favorites")
+                .then().log().all()
+                .extract();
+    }
+
+    public static void 즐겨찾기_목록_조회됨(ExtractableResponse<Response> response) {
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+    }
+
+    public static ExtractableResponse<Response> 즐겨찾기_삭제_요청(RequestSpecification authorization, String location) {
+        return authorization
+                .when().delete(location)
+                .then().log().all()
+                .extract();
+    }
+
+    public static void 즐겨찾기_삭제됨(ExtractableResponse<Response> response) {
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.NO_CONTENT.value());
+    }
 }
