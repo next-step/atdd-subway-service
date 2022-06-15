510b004e14ec30564c0e656d04b2ec9d05a13435
limwoobin <drogba02@naver.com>
2022-06-15 20:17:57 +0900

    refactor: 요금계산 로직을 간소화하라

--- src/main/java/nextstep/subway/fare/domain/FareCalculator.java
+++ src/main/java/nextstep/subway/fare/domain/FareCalculator.java
@@ -3,14 +3,13 @@ package nextstep.subway.fare.domain;
 import nextstep.subway.auth.domain.Customer;
 import nextstep.subway.auth.domain.LoginMember;
 import nextstep.subway.fare.domain.age.AgeFareType;
-import nextstep.subway.fare.domain.age.AgePolicy;
 import nextstep.subway.fare.domain.distance.DistanceFareType;
 import nextstep.subway.fare.domain.distance.DistancePolicy;
 import nextstep.subway.path.domain.Path;
 
 public class FareCalculator {
 
-    public static Fare of(Path path, Customer customer) {
+    public static Fare calculate(Path path, Customer customer) {
         if (customer instanceof LoginMember) {
             return calculatorByLoggedIn(customer, path);
         }
@@ -20,22 +19,20 @@ public class FareCalculator {
 
     private static Fare calculatorByLoggedIn(Customer customer, Path path) {
         LoginMember loginMember = (LoginMember) customer;
-        DistancePolicy distancePolicy = DistanceFareType.getDistancePolicy(path.getDistance());
-        Fare fareByDistance = new Fare(distancePolicy.calculate(path.getDistance()));
-
-        AgePolicy agePolicy = AgeFareType.getAgePolicy(loginMember.getAge());
-        Fare totalFare = fareByDistance.plus(new Fare(agePolicy.calculate()));
-        totalFare = totalFare.plus(new Fare(path.getMostExpensiveLineFare()));
-        totalFare = new Fare((int) (totalFare.getValue() * (100 - agePolicy.discountRate()) * 0.01));
-        return totalFare;
+        Fare distanceFare = calculatePathFare(path);
+        return AgeFareType.calculateTotalFare(loginMember.getAge(), distanceFare);
     }
 
     private static Fare calculatorByNonLoggedIn(Customer customer, Path path) {
-        DistancePolicy distancePolicy = DistanceFareType.getDistancePolicy(path.getDistance());
-        Fare fareByDistance = new Fare(distancePolicy.calculate(path.getDistance()));
-
-        Fare totalFare = fareByDistance.plus(new Fare(customer.DEFAULT_FARE));
+        Fare distanceFare = calculatePathFare(path);
+        Fare totalFare = distanceFare.plus(new Fare(customer.DEFAULT_FARE));
         totalFare = totalFare.plus(new Fare(path.getMostExpensiveLineFare()));
         return totalFare;
+    }
+
+    private static Fare calculatePathFare(Path path) {
+        DistancePolicy distancePolicy = DistanceFareType.getDistancePolicy(path.getDistance());
+        Fare distanceFare = new Fare(distancePolicy.calculate(path.getDistance()));
+        return distanceFare.plus(new Fare(path.getMostExpensiveLineFare()));
     }
 }
