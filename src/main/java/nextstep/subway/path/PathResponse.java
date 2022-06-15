4a47396ff1040c04326f5bfd5c89fb866cc1e5f1
chj1768 <chj1768@gmail.com>
2022-06-16 01:00:16 +0900

    feat: PathController 생성

--- src/main/java/nextstep/subway/path/PathResponse.java
+++ src/main/java/nextstep/subway/path/PathResponse.java
@@ -1,11 +1,25 @@
 package nextstep.subway.path;
 
+import nextstep.subway.station.domain.Station;
 import nextstep.subway.station.dto.StationResponse;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class PathResponse {
+    private final List<StationResponse> stations;
+    private final long distance;
+
+    public PathResponse(List<Station> list, long distance) {
+        this.stations = list.stream().map(StationResponse::of).collect(Collectors.toList());
+        this.distance = distance;
+    }
+
+    public long getDistance() {
+        return distance;
+    }
+
     public List<StationResponse> getStations() {
-        return null;
+        return stations;
     }
 }
---
+++ src/main/java/nextstep/subway/path/ui/PathController.java
@@ -0,0 +1,23 @@
+package nextstep.subway.path.ui;
+
+import nextstep.subway.path.PathResponse;
+import nextstep.subway.path.application.PathService;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/paths")
+public class PathController {
+    private final PathService pathService;
+
+    public PathController(PathService pathService) {
+        this.pathService = pathService;
+    }
+
+    @GetMapping(consumes = { MediaType.APPLICATION_JSON_VALUE })
+    public ResponseEntity<PathResponse> get(@RequestParam long source, @RequestParam long target) {
+        return ResponseEntity.ok(pathService.get(source, target));
+    }
+}
