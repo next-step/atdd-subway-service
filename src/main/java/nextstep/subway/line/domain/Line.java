55a3158da96692db34c88472ebddc4fa7c4661a6
tasklet1579 <tasklet1579@gmail.com>
2022-06-15 13:38:58 +0900

    refactor : 지하철 구간 일급 컬렉션 사용 및 관련 로직 삭제

--- src/main/java/nextstep/subway/line/domain/Line.java
+++ src/main/java/nextstep/subway/line/domain/Line.java
@@ -15,8 +15,8 @@ public class Line extends BaseEntity {
     private String name;
     private String color;
 
-    @OneToMany(mappedBy = "line", cascade = {CascadeType.PERSIST, CascadeType.MERGE}, orphanRemoval = true)
-    private List<Section> sections = new ArrayList<>();
+    @Embedded
+    private final Sections sections = new Sections();
 
     public Line() {
     }
@@ -29,7 +29,7 @@ public class Line extends BaseEntity {
     public Line(String name, String color, Station upStation, Station downStation, int distance) {
         this.name = name;
         this.color = color;
-        sections.add(new Section(this, upStation, downStation, distance));
+        sections.addSection(new Section(this, upStation, downStation, distance));
     }
 
     public void update(Line line) {
@@ -49,145 +49,59 @@ public class Line extends BaseEntity {
         return color;
     }
 
-    public List<Section> getSections() {
-        return sections;
-    }
-
     public List<Station> getStations() {
-        if (sections.size() == 0) {
+        if (sections.isEmpty()) {
             return Collections.emptyList();
         }
-
-        List<Station> stations = new ArrayList<>();
-        Station station = findDepartureStation();
-        stations.add(station);
-
-        while (station != null) {
-            Station finalStation = station;
-            Optional<Section> departure = sections.stream()
-                                                  .filter(sec -> sec.getUpStation().equals(finalStation))
-                                                  .findFirst();
-            if (!departure.isPresent()) {
-                break;
-            }
-            station = departure.get().getDownStation();
-            stations.add(station);
-        }
-
-        return stations;
-    }
-
-    public Station findDepartureStation() {
-        Station station = sections.get(0).getUpStation();
-
-        while (station != null) {
-            Station finalStation = station;
-            Optional<Section> departure = sections.stream()
-                                                  .filter(section -> section.getDownStation().equals(finalStation))
-                                                  .findFirst();
-            if (!departure.isPresent()) {
-                break;
-            }
-            station = departure.get().getUpStation();
-        }
-
-        return station;
+        return sections.getStations();
     }
 
     public void addLineStation(Section section) {
-        List<Station> stations = getStations();
-        if (stations.isEmpty()) {
-            sections.add(section);
-            return;
-        }
-
-        boolean isUpStationExisted = isUpStationExisted(stations, section);
-        boolean isDownStationExisted = isDownStationExisted(stations, section);
-        validateRequestSection(isUpStationExisted, isDownStationExisted);
-        validateRequestParameter(stations, section);
-
-        if (isUpStationExisted) {
-            sections.stream()
-                    .filter(it -> it.getUpStation() == section.getUpStation())
-                    .findFirst()
-                    .ifPresent(it -> it.updateUpStation(section.getDownStation(), section.getDistance()));
-
-            sections.add(section);
-        } else if (isDownStationExisted) {
-            sections.stream()
-                    .filter(it -> it.getDownStation() == section.getDownStation())
-                    .findFirst()
-                    .ifPresent(it -> it.updateDownStation(section.getUpStation(), section.getDistance()));
-
-            sections.add(section);
-        } else {
-            throw new RuntimeException();
-        }
-    }
-
-    public boolean isUpStationExisted(List<Station> stations, Section section) {
-        return stations.stream().anyMatch(it -> it == section.getUpStation());
-    }
-
-    public boolean isDownStationExisted(List<Station> stations, Section section) {
-        return stations.stream().anyMatch(it -> it == section.getDownStation());
-    }
-
-    public void validateRequestSection(boolean isUpStationExisted, boolean isDownStationExisted) {
-        if (isUpStationExisted && isDownStationExisted) {
-            throw new RuntimeException("이미 등록된 구간 입니다.");
-        }
-    }
-
-    public void validateRequestParameter(List<Station> stations, Section section) {
-        if (!stations.isEmpty() && stations.stream().noneMatch(it -> it == section.getUpStation()) &&
-                stations.stream().noneMatch(it -> it == section.getDownStation())) {
-            throw new RuntimeException("등록할 수 없는 구간 입니다.");
-        }
+        sections.addSection(section);
     }
 
     public void removeLineStation(Station station) {
-        validateStationCount();
-        validateStationInclusion(station);
-
-        if (isUpStation(station)) {
-            Section section = sections.stream()
-                                      .filter(sec -> sec.getUpStation().equals(station))
-                                      .findFirst().orElseThrow(RuntimeException::new);
-            sections.stream()
-                    .filter(sec -> sec.getDownStation().equals(station))
-                    .findFirst()
-                    .ifPresent(sec -> sec.updateDownStation(section.getDownStation(), -section.getDistance()));
-            sections.remove(section);
-            return;
-        }
-
-        Section section = sections.stream()
-                                  .filter(sec -> sec.getDownStation().equals(station))
-                                  .findFirst().orElseThrow(RuntimeException::new);
-
-        sections.stream()
-                .filter(sec -> sec.getUpStation().equals(station))
-                .findFirst()
-                .ifPresent(sec -> sec.updateUpStation(section.getUpStation(), -section.getDistance()));
-        sections.remove(section);
+//        validateStationCount();
+//        validateStationInclusion(station);
+//
+//        if (isUpStation(station)) {
+//            Section section = sections.stream()
+//                                      .filter(sec -> sec.getUpStation().equals(station))
+//                                      .findFirst().orElseThrow(RuntimeException::new);
+//            sections.stream()
+//                    .filter(sec -> sec.getDownStation().equals(station))
+//                    .findFirst()
+//                    .ifPresent(sec -> sec.updateDownStation(section.getDownStation(), -section.getDistance()));
+//            sections.remove(section);
+//            return;
+//        }
+//
+//        Section section = sections.stream()
+//                                  .filter(sec -> sec.getDownStation().equals(station))
+//                                  .findFirst().orElseThrow(RuntimeException::new);
+//
+//        sections.stream()
+//                .filter(sec -> sec.getUpStation().equals(station))
+//                .findFirst()
+//                .ifPresent(sec -> sec.updateUpStation(section.getUpStation(), -section.getDistance()));
+//        sections.remove(section);
     }
 
     public void validateStationCount() {
-        if (sections.size() <= 1) {
-            throw new RuntimeException("마지막 구간은 삭제할 수 없습니다.");
-        }
+//        if (sections.size() <= 1) {
+//            throw new RuntimeException("마지막 구간은 삭제할 수 없습니다.");
+//        }
     }
 
     public void validateStationInclusion(Station station) {
-        if (getStations().stream().noneMatch(st -> st.equals(station))) {
-            throw new RuntimeException(String.format("%s 노선에 %s 역이 존재하지 않습니다.", name, station.getName()));
-        }
+//        if (getStations().stream().noneMatch(st -> st.equals(station))) {
+//            throw new RuntimeException(String.format("%s 노선에 %s 역이 존재하지 않습니다.", name, station.getName()));
+//        }
     }
 
-    public boolean isUpStation(Station station) {
-        return sections.stream().anyMatch(section -> section.getUpStation().equals(station));
-    }
+//    public boolean isUpStation(Station station) {
+//        return sections.stream().anyMatch(section -> section.getUpStation().equals(station));
+//    }
 
     @Override
     public boolean equals(Object o) {
