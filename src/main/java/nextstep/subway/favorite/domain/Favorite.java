1313d1033c02729ed7fd140bd7c63836b2d923aa
soob-forest <dltntnqo@naver.com>
2022-06-16 01:59:03 +0900

    docs: Favorite 도메인 요구사항 추가

---
+++ src/main/java/nextstep/subway/favorite/domain/Favorite.java
@@ -0,0 +1,68 @@
+package nextstep.subway.favorite.domain;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import nextstep.subway.member.domain.Member;
+import nextstep.subway.station.domain.Station;
+
+@Entity
+public class Favorite {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "source_id")
+    private Station source;
+
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "target_id")
+    private Station target;
+
+    public static class Builder {
+        private Member member;
+        private Station source;
+        private Station target;
+
+
+        public Builder() {
+        }
+
+        public Builder member(Member member) {
+            this.member = member;
+            return this;
+        }
+
+        public Builder source(Station station) {
+            this.source = station;
+            return this;
+        }
+
+        public Builder target(Station station) {
+            this.target = station;
+            return this;
+        }
+
+        public Favorite build() {
+            return new Favorite(member, target, source);
+        }
+    }
+
+    protected Favorite() {
+    }
+
+    public Favorite(Member member, Station source, Station target) {
+        this.member = member;
+        this.source = source;
+        this.target = target;
+    }
+}
