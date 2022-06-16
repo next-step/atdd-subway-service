d5afce96cfffade3e7c7b9262c9991c21687928e
gunan <power4889@gmail.com>
2022-06-16 22:05:02 +0900

    refactor: 반복되는 메소드 추출

--- src/main/java/nextstep/subway/member/application/MemberService.java
+++ src/main/java/nextstep/subway/member/application/MemberService.java
@@ -23,16 +23,20 @@ public class MemberService {
 
     @Transactional(readOnly = true)
     public MemberResponse findMember(Long id) {
-        Member member = memberRepository.findById(id).orElseThrow(RuntimeException::new);
+        Member member = findById(id);
         return MemberResponse.of(member);
     }
 
     public void updateMember(Long id, MemberRequest param) {
-        Member member = memberRepository.findById(id).orElseThrow(RuntimeException::new);
+        Member member = findById(id);
         member.update(param.toMember());
     }
 
     public void deleteMember(Long id) {
         memberRepository.deleteById(id);
+    }
+
+    private Member findById(Long id) {
+        return memberRepository.findById(id).orElseThrow(RuntimeException::new);
     }
 }
