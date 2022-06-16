98a07478657ea6bf978ff13ba68e54a40f5343d5
iamjunsulee <mellow0924@gmail.com>
2022-06-16 11:31:16 +0900

    test(MemberAcceptanceTest): 내정보관련 기능 인수 테스트 작성

--- src/test/java/nextstep/subway/member/MemberAcceptanceTest.java
+++ src/test/java/nextstep/subway/member/MemberAcceptanceTest.java
@@ -1,17 +1,20 @@
 package nextstep.subway.member;
 
+import static nextstep.subway.auth.acceptance.AuthAcceptanceTest.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
 import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
+
 import nextstep.subway.AcceptanceTest;
 import nextstep.subway.member.dto.MemberRequest;
 import nextstep.subway.member.dto.MemberResponse;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 public class MemberAcceptanceTest extends AcceptanceTest {
     public static final String EMAIL = "email@email.com";
@@ -45,10 +48,77 @@ public class MemberAcceptanceTest extends AcceptanceTest {
         회원_삭제됨(deleteResponse);
     }
 
+    /**
+     * Feature: 회원 정보 관련 기능
+     *
+     * Background
+     * Given 회원 등록되어 있음
+     * And 로그인 되어 있음
+     *
+     * Scenario: 내 정보 관리
+     * When 내 정보 조회 요청함
+     * Then 내 정보 조회됨
+     * When 내 정보 수정 요청함
+     * Then 내 정보 수정됨
+     *
+     * Given 수정된 정보로 로그인 되어 있음
+     * When 내 정보 삭제 요청함
+     * Then 내 정보 삭제됨
+     */
     @DisplayName("나의 정보를 관리한다.")
     @Test
     void manageMyInfo() {
+        //given
+        회원_생성을_요청(EMAIL, PASSWORD, AGE);
+        String 사용자_인증_토큰 = 로그인_되어_있음(EMAIL, PASSWORD);
 
+        //when
+        ExtractableResponse<Response> 내_정보_조회_응답_결과 = 내_정보_조회_요청(사용자_인증_토큰);
+        //then
+        회원_정보_조회됨(내_정보_조회_응답_결과, EMAIL, AGE);
+
+        //when
+        ExtractableResponse<Response> 내_정보_수정_응답_결과 = 내_정보_수정_요청(사용자_인증_토큰, NEW_EMAIL, NEW_PASSWORD, NEW_AGE);
+        //then
+        회원_정보_수정됨(내_정보_수정_응답_결과);
+
+        //given
+        사용자_인증_토큰 = 로그인_되어_있음(NEW_EMAIL, NEW_PASSWORD);
+        //when
+        ExtractableResponse<Response> 내_정보_삭제_응답_결과 = 내_정보_삭제_요청(사용자_인증_토큰);
+        //then
+        회원_삭제됨(내_정보_삭제_응답_결과);
+    }
+
+    private ExtractableResponse<Response> 내_정보_삭제_요청(String accessToken) {
+        return RestAssured
+                .given().log().all()
+                .auth().oauth2(accessToken)
+                .when().delete("/members/me")
+                .then().log().all()
+                .extract();
+    }
+
+    private ExtractableResponse<Response> 내_정보_수정_요청(String accessToken, String email, String password, int age) {
+        MemberRequest memberRequest = new MemberRequest(email, password, age);
+        return RestAssured
+                .given().log().all()
+                .auth().oauth2(accessToken)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(memberRequest)
+                .when().put("/members/me")
+                .then().log().all()
+                .extract();
+    }
+
+    public static ExtractableResponse<Response> 내_정보_조회_요청(String accessToken) {
+        return RestAssured
+                .given().log().all()
+                .auth().oauth2(accessToken)
+                .accept(MediaType.APPLICATION_JSON_VALUE)
+                .when().get("/members/me")
+                .then().log().all()
+                .extract();
     }
 
     public static ExtractableResponse<Response> 회원_생성을_요청(String email, String password, Integer age) {
