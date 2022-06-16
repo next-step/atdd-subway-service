377bf1f0d5a18acf7a91eb4b291e2f07712c8ee4
Donghee Han <handh0413@gmail.com>
2022-06-16 18:17:11 +0900

    refactor: StationResponses를 이용한 리팩토링

--- src/main/java/nextstep/subway/path/application/PathService.java
+++ src/main/java/nextstep/subway/path/application/PathService.java
@@ -1,18 +1,14 @@
 package nextstep.subway.path.application;
 
-import nextstep.subway.line.domain.Distance;
 import nextstep.subway.line.domain.LineRepository;
 import nextstep.subway.path.domain.PathFinder;
 import nextstep.subway.path.dto.PathRequest;
 import nextstep.subway.path.dto.PathResponse;
 import nextstep.subway.station.application.StationService;
 import nextstep.subway.station.domain.Station;
-import nextstep.subway.station.dto.StationResponse;
+import nextstep.subway.station.dto.StationResponses;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 @Transactional
@@ -29,11 +25,8 @@ public class PathService {
         Station source = stationService.findById(pathRequest.getSource());
         Station target = stationService.findById(pathRequest.getTarget());
         PathFinder path = new PathFinder(lineRepository.findAll());
-        List<StationResponse> shortestStationList = path.findShortestStationList(source, target)
-                .stream()
-                .map(StationResponse::of)
-                .collect(Collectors.toList());
+        StationResponses stationResponses = new StationResponses(path.findShortestStationList(source, target));
         int shortestPathLength = path.findShortestPathLength(source, target);
-        return new PathResponse(shortestStationList, shortestPathLength);
+        return new PathResponse(stationResponses.getList(), shortestPathLength);
     }
 }
