404bf8c542d6052fdd652ae37db6ee120982f73c
Yul Jeon <notkhaki@gmail.com>
2022-06-16 15:23:24 +0900

    feat(PathFinder, PathFinderTest): handle exceptional cases when getting path

--- src/main/java/nextstep/subway/PathFinder.java
+++ src/main/java/nextstep/subway/PathFinder.java
@@ -2,6 +2,7 @@ package nextstep.subway;
 
 import nextstep.subway.line.domain.Line;
 import nextstep.subway.station.domain.Station;
+import org.jgrapht.GraphPath;
 import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
 import org.jgrapht.graph.DefaultWeightedEdge;
 import org.jgrapht.graph.WeightedMultigraph;
@@ -10,13 +11,13 @@ import java.util.List;
 
 public class PathFinder {
 
-    private final WeightedMultigraph<Station, DefaultWeightedEdge> graph = new WeightedMultigraph(DefaultWeightedEdge.class);
-    private final DijkstraShortestPath dijkstraShortestPath;
+    private final WeightedMultigraph<Station, DefaultWeightedEdge> graph = new WeightedMultigraph<>(DefaultWeightedEdge.class);
+    private final DijkstraShortestPath<Station, DefaultWeightedEdge> dijkstraShortestPath;
 
     public PathFinder(List<Line> lines) {
 //        configureGraph(lines);
         configureGraphNew(lines);
-        dijkstraShortestPath = new DijkstraShortestPath(graph);
+        dijkstraShortestPath = new DijkstraShortestPath<>(graph);
     }
 
     private void configureGraph(List<Line> lines) {
@@ -40,7 +41,27 @@ public class PathFinder {
         lines.forEach(line -> line.configure(graph));
     }
 
-    public List<Station> findRoute(Station source, Station target) {
-        return dijkstraShortestPath.getPath(source, target).getVertexList();
+    public GraphPath<Station, DefaultWeightedEdge> getPath(Station source, Station target) {
+        isSame(source, target);
+        isExistent(source, target);
+
+        GraphPath<Station, DefaultWeightedEdge> path = dijkstraShortestPath.getPath(source, target);
+        if (path == null) {
+            throw new IllegalArgumentException("출발역과 도착역은 연결되어 있어야 합니다.");
+        }
+
+        return path;
+    }
+
+    private void isSame(Station source, Station target) {
+        if (source.equals(target)) {
+            throw new IllegalArgumentException("출발역과 도착역은 달라야 합니다.");
+        }
+    }
+
+    private void isExistent(Station source, Station target) {
+        if (!graph.containsVertex(source) || !graph.containsVertex(target)) {
+            throw new IllegalArgumentException("존재하지 않는 역입니다.");
+        }
     }
 }
--- src/test/java/nextstep/subway/PathFinderTest.java
+++ src/test/java/nextstep/subway/PathFinderTest.java
@@ -2,14 +2,16 @@ package nextstep.subway;
 
 import nextstep.subway.line.domain.Line;
 import nextstep.subway.station.domain.Station;
+import org.jgrapht.GraphPath;
+import org.jgrapht.graph.DefaultWeightedEdge;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
-import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class PathFinderTest {
 
@@ -17,9 +19,12 @@ public class PathFinderTest {
     Station 양재역;
     Station 교대역;
     Station 남부터미널역;
+    Station 사당역;
+    Station 동작역;
     Line 신분당선;
     Line 이호선;
     Line 삼호선;
+    Line 사호선;
 
     /**
      * 교대역     --- *2호선* ---  강남역
@@ -34,24 +39,62 @@ public class PathFinderTest {
         양재역 = new Station("양재역");
         교대역 = new Station("교대역");
         남부터미널역 = new Station("남부터미널역");
+        사당역 = new Station("사당역");
+        동작역 = new Station("동작역");
 
         신분당선 = new Line("신분당선", "bg-red-600", 강남역, 양재역, 10);
         이호선 = new Line("이호선", "bg-red-600", 교대역, 강남역, 10);
         삼호선 = new Line("삼호선", "bg-red-600", 교대역, 양재역, 5);
+        사호선 = new Line("사호선", "bg-red-600", 사당역, 동작역, 3);
 
-        삼호선.addSection(교대역, 남부터미널역, 3);
+        삼호선.addSection(교대역, 남부터미널역, 3); // 남부터미널역-(2m)-양재역
     }
 
-    @DisplayName("출발 역부터 도착 역까지 최단 경로를 조회한다.")
+    @DisplayName("출발역부터 도착역까지 최단 경로를 조회한다.")
     @Test
     void 경로_조회() {
         // given
         PathFinder pathFinder = new PathFinder(Arrays.asList(신분당선, 이호선, 삼호선));
 
         // when
-        List<Station> route = pathFinder.findRoute(강남역, 남부터미널역);
+        GraphPath<Station, DefaultWeightedEdge> path = pathFinder.getPath(강남역, 남부터미널역);
 
         // then
-        assertThat(route).containsExactly(강남역, 양재역, 남부터미널역);
+        assertThat(path.getVertexList()).containsExactly(강남역, 양재역, 남부터미널역);
+        assertThat(path.getWeight()).isEqualTo(12);
+    }
+
+    @DisplayName("출발역과 도착역에 같은 역을 입력한다.")
+    @Test
+    void 경로_조회_예외_1() {
+        // given
+        PathFinder pathFinder = new PathFinder(Arrays.asList(신분당선, 이호선, 삼호선));
+
+        // when
+        assertThatThrownBy(() -> pathFinder.getPath(강남역, 강남역))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("연결되어 있지 않은 출발역과 도착역을 입력한다.")
+    @Test
+    void 경로_조회_예외_2() {
+        PathFinder pathFinder = new PathFinder(Arrays.asList(신분당선, 이호선, 삼호선, 사호선));
+
+        assertThatThrownBy(() -> pathFinder.getPath(강남역, 사당역))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("존재하지 않는 출발역이나 도착역을 입력한다.")
+    @Test
+    void 경로_조회_예외_3() {
+        PathFinder pathFinder = new PathFinder(Arrays.asList(신분당선, 이호선, 삼호선));
+
+        assertThatThrownBy(() -> pathFinder.getPath(강남역, 사당역))
+                .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> pathFinder.getPath(사당역, 강남역))
+                .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> pathFinder.getPath(사당역, 동작역))
+                .isInstanceOf(IllegalArgumentException.class);
+
     }
 }
