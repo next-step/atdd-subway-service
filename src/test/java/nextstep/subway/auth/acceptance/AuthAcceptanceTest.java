416725185c53f3f1b42bec7013baec5d89d5c73d
chj1768 <chj1768@gmail.com>
2022-06-17 01:30:41 +0900

    test: 토큰 발급 기능 인수 테스트 작성

--- src/test/java/nextstep/subway/auth/acceptance/AuthAcceptanceTest.java
+++ src/test/java/nextstep/subway/auth/acceptance/AuthAcceptanceTest.java
@@ -1,24 +1,77 @@
 package nextstep.subway.auth.acceptance;
 
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
 import nextstep.subway.AcceptanceTest;
+import nextstep.subway.auth.dto.TokenRequest;
+import nextstep.subway.member.MemberAcceptanceTest;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class AuthAcceptanceTest extends AcceptanceTest {
+    public static final String EMAIL = "email@email.com";
+    public static final String PASSWORD = "password";
+    public static final int AGE = 20;
+
+    @BeforeEach
+    public void setUp() {
+        super.setUp();
+
+        //given
+        ExtractableResponse<Response> createResponse = MemberAcceptanceTest.회원_생성을_요청(EMAIL, PASSWORD, AGE);
+        MemberAcceptanceTest.회원_생성됨(createResponse);
+    }
 
     @DisplayName("Bearer Auth")
     @Test
     void myInfoWithBearerAuth() {
+        //when
+        ExtractableResponse<Response> response = 로그인_요청(EMAIL, PASSWORD);
+
+        //then
+        로그인_성공(response);
     }
 
     @DisplayName("Bearer Auth 로그인 실패")
     @Test
     void myInfoWithBadBearerAuth() {
+        //when
+        ExtractableResponse<Response> response = 로그인_요청("유효하지 않은 이메일", PASSWORD);
+
+        //then
+        인증_실패(response);
     }
 
     @DisplayName("Bearer Auth 유효하지 않은 토큰")
     @Test
     void myInfoWithWrongBearerAuth() {
+        //when
+        ExtractableResponse<Response> memberResponse = MemberAcceptanceTest.내_정보_조회_요청("invalid accessToken");
+
+        //then
+        인증_실패(memberResponse);
+    }
+
+    public static ExtractableResponse<Response> 로그인_요청(String email, String password) {
+        TokenRequest tokenRequest = new TokenRequest(email, password);
+
+        return RestAssured.given().log().all().contentType(MediaType.APPLICATION_JSON_VALUE).body(tokenRequest).when()
+                .post("/login/token").then().log().all().extract();
+    }
+
+    public static void 로그인_성공(ExtractableResponse<Response> response) {
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+        assertThat(response.jsonPath().getString("accessToken")).isNotBlank();
+    }
+
+    public static void 인증_실패(ExtractableResponse<Response> response) {
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.UNAUTHORIZED.value());
     }
 
 }
--- src/test/java/nextstep/subway/member/MemberAcceptanceTest.java
+++ src/test/java/nextstep/subway/member/MemberAcceptanceTest.java
@@ -66,23 +66,22 @@ public class MemberAcceptanceTest extends AcceptanceTest {
     public static ExtractableResponse<Response> 회원_정보_조회_요청(ExtractableResponse<Response> response) {
         String uri = response.header("Location");
 
-        return RestAssured
-                .given().log().all()
-                .accept(MediaType.APPLICATION_JSON_VALUE)
-                .when().get(uri)
-                .then().log().all()
-                .extract();
+        return RestAssured.given().log().all().accept(MediaType.APPLICATION_JSON_VALUE).when().get(uri).then().log()
+                .all().extract();
     }
 
-    public static ExtractableResponse<Response> 회원_정보_수정_요청(ExtractableResponse<Response> response, String email, String password, Integer age) {
+    public static ExtractableResponse<Response> 내_정보_조회_요청(String accessToken) {
+        return RestAssured.given().log().all().auth().oauth2(accessToken).accept(MediaType.APPLICATION_JSON_VALUE)
+                .when().get("/members/me").then().log().all().extract();
+    }
+
+    public static ExtractableResponse<Response> 회원_정보_수정_요청(
+            ExtractableResponse<Response> response, String email, String password, Integer age) {
         String uri = response.header("Location");
         MemberRequest memberRequest = new MemberRequest(email, password, age);
 
-        return RestAssured
-                .given().log().all()
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .body(memberRequest)
-                .when().put(uri)
+        return RestAssured.given().log().all().contentType(MediaType.APPLICATION_JSON_VALUE).body(memberRequest).when()
+                .put(uri)
                 .then().log().all()
                 .extract();
     }
