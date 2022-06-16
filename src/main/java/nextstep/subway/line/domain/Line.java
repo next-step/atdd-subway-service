a45f5d41ec5a29d7d4f4641c5e53ce5da26d02fc
jhsong2580 <thdwmdgns@naver.com>
2022-06-16 14:34:56 +0900

    feat: price getter 추가

--- src/main/java/nextstep/subway/line/domain/Line.java
+++ src/main/java/nextstep/subway/line/domain/Line.java
@@ -120,4 +120,8 @@ public class Line extends BaseEntity {
     public List<Section> getSections() {
         return sections.getSections();
     }
+
+    public int getPrice() {
+        return price;
+    }
 }
