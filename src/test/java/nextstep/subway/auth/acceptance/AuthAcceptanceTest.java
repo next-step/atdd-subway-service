5323a4009f0c4ed906ef1adec31e486bf37c7ff3
mannue <sppe12@gmail.com>
2022-06-17 00:17:40 +0900

    feat(AuthAcceptanceTest): 잘못된 토큰 으로 요청시 에러발생 하는 테스트 추가

--- src/test/java/nextstep/subway/auth/acceptance/AuthAcceptanceTest.java
+++ src/test/java/nextstep/subway/auth/acceptance/AuthAcceptanceTest.java
@@ -56,7 +56,7 @@ public class AuthAcceptanceTest extends AcceptanceTest {
         ExtractableResponse<Response> response = 로그인_요청(암호_이메일_입력(INVALID_PASSWORD, INVALID_EMAIL));
 
         //Then
-        로그인_실패됨(response);
+        인증_실패됨(response);
     }
 
     /**
@@ -77,15 +77,25 @@ public class AuthAcceptanceTest extends AcceptanceTest {
         ExtractableResponse<Response> response = 로그인_요청(암호_이메일_입력("noMatch", EMAIL));
 
         //Then
-        로그인_실패됨(response);
+        인증_실패됨(response);
     }
 
+    /**
+     * When 유효하지 않은 토큰으로 사용자 정보 요청 시
+     * Then 인증이 실패 된다.
+     */
     @DisplayName("Bearer Auth 유효하지 않은 토큰")
     @Test
     void myInfoWithWrongBearerAuth() {
+
+        // When
+        ExtractableResponse<Response> response = 변조된_인증_정보로_사용자_정보_요청("abcdefg");
+
+        // Then
+        인증_실패됨(response);
     }
 
-    public static void 로그인_실패됨(ExtractableResponse<Response> response) {
+    public static void 인증_실패됨(ExtractableResponse<Response> response) {
         assertThat(HttpStatus.valueOf(response.statusCode())).isEqualTo(HttpStatus.UNAUTHORIZED);
     }
 
@@ -103,6 +113,16 @@ public class AuthAcceptanceTest extends AcceptanceTest {
                 .post("/login/token")
                 .then()
                 .log().all()
+                .extract();
+    }
+    public static ExtractableResponse<Response> 변조된_인증_정보로_사용자_정보_요청(final String badToken) {
+        return RestAssured.given().log().all()
+                .auth().oauth2(badToken)
+                .accept(MediaType.APPLICATION_JSON_VALUE)
+                .when()
+                .get("/members/me")
+                .then().log().all()
+                .statusCode(HttpStatus.UNAUTHORIZED.value())
                 .extract();
     }
 
