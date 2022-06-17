7c6a62a490e7a2cc7db0291fa4bff104e49821b3
tasklet1579 <tasklet1579@gmail.com>
2022-06-17 11:46:34 +0900

    test : 지하철 경로 조회 테스트 코드 재작성

--- src/main/java/nextstep/subway/path/application/PathService.java
+++ src/main/java/nextstep/subway/path/application/PathService.java
@@ -1,20 +1,20 @@
 package nextstep.subway.path.application;
 
 import nextstep.subway.path.dto.PathResponse;
-import nextstep.subway.station.domain.Station;
-import nextstep.subway.station.dto.StationResponse;
+import nextstep.subway.station.application.StationService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 public class PathService {
+    private final StationService stationService;
+
+    @Autowired
+    public PathService(StationService stationService) {
+        this.stationService = stationService;
+    }
+
     public PathResponse findPath(Long source, Long target) {
-        List<Station> stations = Arrays.asList(new Station("강남역"), new Station("광교역"));
-        return new PathResponse(stations.stream()
-                                        .map(StationResponse::of)
-                                        .collect(Collectors.toList()), 40);
+        return new PathResponse(stationService.findAllStations(), 50);
     }
 }
--- src/test/java/nextstep/subway/path/application/PathServiceTest.java
+++ src/test/java/nextstep/subway/path/application/PathServiceTest.java
@@ -1,16 +1,15 @@
 package nextstep.subway.path.application;
 
 import nextstep.subway.path.dto.PathResponse;
+import nextstep.subway.station.application.StationService;
 import nextstep.subway.station.domain.Station;
-import nextstep.subway.station.dto.StationResponse;
+import nextstep.subway.station.domain.StationRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
@@ -18,21 +17,20 @@ import static org.mockito.Mockito.when;
 @ExtendWith(MockitoExtension.class)
 class PathServiceTest {
     @Mock
-    private PathService pathService;
+    private StationRepository stationRepository;
 
     @Test
     void findPath_sameLine() {
         // given
-        List<Station> stations = Arrays.asList(new Station("선릉역"), new Station("정자역"), new Station("수원역"));
-        List<StationResponse> responses = stations.stream().map(StationResponse::of).collect(Collectors.toList());
-        when(pathService.findPath(1L, 3L)).thenReturn(new PathResponse(responses, 50));
+        when(stationRepository.findAll()).thenReturn(Arrays.asList(new Station("선릉역"), new Station("정자역"), new Station("수원역")));
 
         // when
+        StationService stationService = new StationService(stationRepository);
+        PathService pathService = new PathService(stationService);
         PathResponse result = pathService.findPath(1L, 3L);
-        List<String> names = result.getStations().stream().map(StationResponse::getName).collect(Collectors.toList());
 
         // then
-        assertThat(names).containsExactly("선릉역", "정자역", "수원역");
+        assertThat(result.getStations()).hasSize(3);
         assertThat(result.getDistance()).isEqualTo(50);
     }
 }
