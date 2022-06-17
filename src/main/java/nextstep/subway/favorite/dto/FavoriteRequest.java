d6f453f9ea1fa3a2ae7a749442ee69d84ff073c4
piopoi <kangsw1988@gmail.com>
2022-06-18 00:55:27 +0900

    feat(favorite): 즐겨찾기 DTO Class 추가

--- src/main/java/nextstep/subway/favorite/dto/FavoriteRequest.java
+++ src/main/java/nextstep/subway/favorite/dto/FavoriteRequest.java
@@ -1,5 +1,9 @@
 package nextstep.subway.favorite.dto;
 
+import nextstep.subway.favorite.domain.Favorite;
+import nextstep.subway.member.domain.Member;
+import nextstep.subway.station.domain.Station;
+
 public class FavoriteRequest {
     private Long sourceStationId;
     private Long targetStationId;
@@ -18,5 +22,9 @@ public class FavoriteRequest {
 
     public Long getTargetStationId() {
         return targetStationId;
+    }
+
+    public Favorite toFavorite(Member member, Station source, Station target) {
+        return new Favorite(member, source, target);
     }
 }
