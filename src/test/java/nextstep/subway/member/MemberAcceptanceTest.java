bc43f6f556e199e0aa8a60b924259c73471ae953
chj1768 <chj1768@gmail.com>
2022-06-17 16:16:40 +0900

    refactor: 공통 static 변수 사용, private scope 로 사용하는 경우 접근자 수정

--- src/test/java/nextstep/subway/member/MemberAcceptanceTest.java
+++ src/test/java/nextstep/subway/member/MemberAcceptanceTest.java
@@ -12,14 +12,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
+import static nextstep.subway.auth.acceptance.AuthAcceptanceTest.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class MemberAcceptanceTest extends AcceptanceTest {
-    public static final String EMAIL = "email@email.com";
-    public static final String PASSWORD = "password";
     public static final String NEW_EMAIL = "newemail@email.com";
     public static final String NEW_PASSWORD = "newpassword";
-    public static final int AGE = 20;
     public static final int NEW_AGE = 21;
 
     @DisplayName("회원 정보를 관리한다.")
