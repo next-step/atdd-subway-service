556d20ba5612eaf59f08b261c81a3df78a439b84
minzzang <minjjang1117@gmail.com>
2022-06-19 00:36:52 +0900

    test(AuthAcceptanceTest): 로그인을 성공한다

--- src/test/java/nextstep/subway/auth/acceptance/AuthAcceptanceTest.java
+++ src/test/java/nextstep/subway/auth/acceptance/AuthAcceptanceTest.java
@@ -1,24 +1,71 @@
 package nextstep.subway.auth.acceptance;
 
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
 import nextstep.subway.AcceptanceTest;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import nextstep.subway.auth.dto.TokenRequest;
+import nextstep.subway.auth.dto.TokenResponse;
+import org.junit.jupiter.api.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 
+import java.util.stream.Stream;
+
+import static nextstep.subway.member.MemberAcceptanceTest.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.DynamicTest.dynamicTest;
+
+@DisplayName("Bearer Auth 인증 관련 기능")
 public class AuthAcceptanceTest extends AcceptanceTest {
+    private TokenRequest tokenRequest;
 
-    @DisplayName("Bearer Auth")
-    @Test
-    void myInfoWithBearerAuth() {
+    @BeforeEach
+    public void setUp() {
+        super.setUp();
+
+        tokenRequest = new TokenRequest(EMAIL, PASSWORD);
     }
 
-    @DisplayName("Bearer Auth 로그인 실패")
-    @Test
-    void myInfoWithBadBearerAuth() {
+    @TestFactory
+    Stream<DynamicTest> 인증_관련_기능_시나리오() {
+        return Stream.of(
+                dynamicTest("로그인을 성공한다.", this::로그인을_성공한다),
+                dynamicTest("비밀번호가 일치하지 않으면 로그인에 실패한다.", this::비밀번호가_일치하지_않으면_로그인에_실패한다),
+                dynamicTest("유효하지 않은 토큰으로 api를 호출하면 예외가 발생한다", this::유효하지_않은_토큰으로_api를_호출하면_예외가_발생한다)
+        );
     }
 
-    @DisplayName("Bearer Auth 유효하지 않은 토큰")
     @Test
-    void myInfoWithWrongBearerAuth() {
+    private void 로그인을_성공한다() {
+        // given
+        회원_생성을_요청(EMAIL, PASSWORD, AGE);
+        // when
+        ExtractableResponse<Response> 로그인_요청_응답 = 로그인_요청(tokenRequest);
+        // then
+        로그인_됨(로그인_요청_응답);
     }
 
+    @Test
+    private void 비밀번호가_일치하지_않으면_로그인에_실패한다() {
+    }
+
+    @Test
+    private void 유효하지_않은_토큰으로_api를_호출하면_예외가_발생한다() {
+    }
+
+    private static ExtractableResponse<Response> 로그인_요청(TokenRequest tokenRequest) {
+        return RestAssured
+                .given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(tokenRequest)
+                .when().post("/login/token")
+                .then().log().all()
+                .extract();
+    }
+
+    private static void 로그인_됨(ExtractableResponse<Response> response) {
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+        assertThat(response.as(TokenResponse.class).getAccessToken()).isNotNull();
+    }
 }
