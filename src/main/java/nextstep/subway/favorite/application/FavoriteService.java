e4140c69ca20088d1d669d5c378b4523fa66d1d9
chj1768 <chj1768@gmail.com>
2022-06-18 23:18:12 +0900

    refactor: service 에서 service 의존하던 부분 repository interface 의존하도록 변경

--- src/main/java/nextstep/subway/favorite/application/FavoriteService.java
+++ src/main/java/nextstep/subway/favorite/application/FavoriteService.java
@@ -4,47 +4,57 @@ import nextstep.subway.favorite.domain.Favorite;
 import nextstep.subway.favorite.domain.FavoriteRepository;
 import nextstep.subway.favorite.dto.FavoriteRequest;
 import nextstep.subway.favorite.dto.FavoriteResponse;
-import nextstep.subway.member.application.MemberService;
 import nextstep.subway.member.domain.Member;
-import nextstep.subway.station.application.StationService;
+import nextstep.subway.member.domain.MemberRepository;
 import nextstep.subway.station.domain.Station;
+import nextstep.subway.station.domain.StationRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.stream.Collectors;
 
 @Service
 @Transactional
 public class FavoriteService {
     private final FavoriteRepository favoriteRepository;
-    private final MemberService memberService;
-    private final StationService stationService;
+    private final MemberRepository memberRepository;
+    private final StationRepository stationRepository;
 
     public FavoriteService(
-            FavoriteRepository favoriteRepository, MemberService memberService, StationService stationService) {
+            FavoriteRepository favoriteRepository, MemberRepository memberRepository,
+            StationRepository stationRepository) {
         this.favoriteRepository = favoriteRepository;
-        this.memberService = memberService;
-        this.stationService = stationService;
+        this.memberRepository = memberRepository;
+        this.stationRepository = stationRepository;
     }
 
     public FavoriteResponse create(long loginMemberId, FavoriteRequest request) {
-        Member member = memberService.findById(loginMemberId);
-        Station source = stationService.findStationById(request.getSourceStationId());
-        Station target = stationService.findStationById(request.getTargetStationId());
+        Member member = findMemberById(loginMemberId);
+        Station source = findStationById(request.getSourceStationId());
+        Station target = findStationById(request.getTargetStationId());
         Favorite persistFavorite = favoriteRepository.save(new Favorite(member, source, target));
         return FavoriteResponse.of(persistFavorite);
     }
 
+    private Station findStationById(long stationId) {
+        return stationRepository.findById(stationId).orElseThrow(NoSuchElementException::new);
+    }
+
+    private Member findMemberById(long loginMemberId) {
+        return memberRepository.findById(loginMemberId).orElseThrow(NoSuchElementException::new);
+    }
+
     @Transactional(readOnly = true)
     public List<FavoriteResponse> list(long loginMemberId) {
-        Member member = memberService.findById(loginMemberId);
+        Member member = findMemberById(loginMemberId);
         List<Favorite> list = favoriteRepository.findAllByMember(member);
         return list.stream().map(FavoriteResponse::of).collect(Collectors.toList());
     }
 
     public void delete(long loginMemberId, long id) {
-        Member member = memberService.findById(loginMemberId);
+        Member member = findMemberById(loginMemberId);
         favoriteRepository.deleteByMemberAndId(member, id);
     }
 }
