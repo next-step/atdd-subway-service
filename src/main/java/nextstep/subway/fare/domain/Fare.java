d319a000e1c0c26a2a98701d38e876992da8ed53
soob-forest <dltntnqo@naver.com>
2022-06-18 17:23:26 +0900

    feat(Fare): 요금을 나타내기 위한 값 객체

---
+++ src/main/java/nextstep/subway/fare/domain/Fare.java
@@ -0,0 +1,55 @@
+package nextstep.subway.fare.domain;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+import javax.persistence.Embeddable;
+
+@Embeddable
+public class Fare implements Comparable<Fare> {
+    private final BigDecimal value;
+
+    protected Fare() {
+        this.value = BigDecimal.ZERO;
+    }
+
+    private Fare(BigDecimal value) {
+        this.value = value;
+    }
+
+    public static Fare from(double value) {
+        return new Fare(BigDecimal.valueOf(value));
+    }
+
+    public BigDecimal getValue() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Fare fare = (Fare) o;
+        return Objects.equals(value, fare.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
+
+    @Override
+    public String toString() {
+        return "Fare{" +
+                "value=" + value.toString() +
+                '}';
+    }
+
+    @Override
+    public int compareTo(Fare target) {
+        return value.compareTo(target.value);
+    }
+}
