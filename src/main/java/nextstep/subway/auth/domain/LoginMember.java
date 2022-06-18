443da7989565c65ff5dc2eb1278924085c5b0081
iamjunsulee <mellow0924@gmail.com>
2022-06-19 08:08:59 +0900

    fix(LoginMember, MemberController): AuthAcceptanceTest 실패케이스 관련 수정

--- src/main/java/nextstep/subway/auth/domain/LoginMember.java
+++ src/main/java/nextstep/subway/auth/domain/LoginMember.java
@@ -1,12 +1,18 @@
 package nextstep.subway.auth.domain;
 
 public class LoginMember {
+    private static final Long DEFAULT_ID = 0L;
+    private static final String DEFAULT_EMAIL = "";
+    private static final Integer DEFAULT_AGE = 0;
+
     private Long id;
     private String email;
     private Integer age;
 
     public LoginMember() {
-        this.age = 0;
+        this.id = DEFAULT_ID;
+        this.email = DEFAULT_EMAIL;
+        this.age = DEFAULT_AGE;
     }
 
     public LoginMember(Long id, String email, Integer age) {
@@ -25,5 +31,10 @@ public class LoginMember {
 
     public Integer getAge() {
         return age;
+    }
+
+    public boolean isEqualDefaultValue() {
+        return this.age.equals(DEFAULT_AGE) && this.email.equals(DEFAULT_EMAIL)
+                && this.id.equals(DEFAULT_ID);
     }
 }
