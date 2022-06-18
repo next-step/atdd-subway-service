841f7358d0d6578da27663eda828d0918fe498e5
tasklet1579 <tasklet1579@gmail.com>
2022-06-18 11:26:02 +0900

    test : 지하철 경로 조회

--- src/test/java/nextstep/subway/path/PathAcceptanceTest.java
+++ src/test/java/nextstep/subway/path/PathAcceptanceTest.java
@@ -1,16 +1,26 @@
 package nextstep.subway.path;
 
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
 import nextstep.subway.AcceptanceTest;
 import nextstep.subway.line.acceptance.LineAcceptanceTest;
 import nextstep.subway.line.acceptance.LineSectionAcceptanceTest;
 import nextstep.subway.line.dto.LineRequest;
 import nextstep.subway.line.dto.LineResponse;
+import nextstep.subway.path.dto.PathResponse;
 import nextstep.subway.station.StationAcceptanceTest;
 import nextstep.subway.station.dto.StationResponse;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("지하철 경로 조회")
 public class PathAcceptanceTest extends AcceptanceTest {
@@ -21,6 +31,7 @@ public class PathAcceptanceTest extends AcceptanceTest {
     private StationResponse 양재역;
     private StationResponse 교대역;
     private StationResponse 남부터미널역;
+    private StationResponse 서울역;
 
     @BeforeEach
     void beforeEach() {
@@ -30,6 +41,7 @@ public class PathAcceptanceTest extends AcceptanceTest {
         양재역 = StationAcceptanceTest.지하철역_등록되어_있음("양재역").as(StationResponse.class);
         교대역 = StationAcceptanceTest.지하철역_등록되어_있음("교대역").as(StationResponse.class);
         남부터미널역 = StationAcceptanceTest.지하철역_등록되어_있음("남부터미널역").as(StationResponse.class);
+        서울역 = StationAcceptanceTest.지하철역_등록되어_있음("서울역").as(StationResponse.class);
 
         LineRequest lineRequest1 = new LineRequest("신분당선", "bg-red-600", 강남역.getId(), 양재역.getId(), 10);
         LineRequest lineRequest2 = new LineRequest("이호선", "bg-red-600", 강남역.getId(), 교대역.getId(), 15);
@@ -39,7 +51,9 @@ public class PathAcceptanceTest extends AcceptanceTest {
         이호선 = LineAcceptanceTest.지하철_노선_등록되어_있음(lineRequest2).as(LineResponse.class);
         삼호선 = LineAcceptanceTest.지하철_노선_등록되어_있음(lineRequest3).as(LineResponse.class);
 
+        LineSectionAcceptanceTest.지하철_노선에_지하철역_등록_요청(신분당선, 양재역, 서울역, 30);
         LineSectionAcceptanceTest.지하철_노선에_지하철역_등록_요청(삼호선, 교대역, 남부터미널역, 3);
+        LineSectionAcceptanceTest.지하철_노선에_지하철역_등록_요청(삼호선, 남부터미널역, 서울역, 10);
     }
 
     /*
@@ -52,6 +66,11 @@ public class PathAcceptanceTest extends AcceptanceTest {
     @DisplayName("같은 지하철 노선에 포함된 역 사이의 최단거리 경로를 조회한다.")
     @Test
     void findPath_sameLine() {
+        // when
+        ExtractableResponse<Response> response = 출발역_도착역_최단거리_경로_조회(강남역.getId(), 서울역.getId());
+
+        // then
+        출발역_도착역_최단거리_거리_경로_확인(response, 12, 강남역, 양재역, 서울역);
     }
 
     /*
@@ -64,23 +83,67 @@ public class PathAcceptanceTest extends AcceptanceTest {
     @DisplayName("서로 다른 지하철 노선에 포함된 역 사이의 최단거리 경로를 조회한다.")
     @Test
     void findPath_differentLines() {
+        // when
+        ExtractableResponse<Response> response = 출발역_도착역_최단거리_경로_조회(강남역.getId(), 남부터미널역.getId());
+
+        // then
+        출발역_도착역_최단거리_거리_경로_확인(response, 18, 강남역, 교대역, 남부터미널역);
     }
 
     @DisplayName("출발역과 도착역이 같은 경우 최단거리 경로를 조회할 수 없다.")
     @Test
     void findPath_throwsException_ifStationsAreSame() {
+        // when
+        ExtractableResponse<Response> response = 출발역_도착역_최단거리_경로_조회(강남역.getId(), 강남역.getId());
 
+        // then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR.value());
     }
 
     @DisplayName("출발역과 도착역이 연결이 되어 있지 않으면 최단거리 경로를 조회할 수 없다.")
     @Test
     void findPath_throwsException_ifStationsAreNotConnected() {
+        // given
+        LineSectionAcceptanceTest.지하철_노선에_지하철역_제외_요청(신분당선, 서울역);
+        LineSectionAcceptanceTest.지하철_노선에_지하철역_제외_요청(삼호선, 교대역);
+        LineSectionAcceptanceTest.지하철_노선에_지하철역_제외_요청(삼호선, 양재역);
 
+        // when
+        ExtractableResponse<Response> response = 출발역_도착역_최단거리_경로_조회(강남역.getId(), 남부터미널역.getId());
+
+        // then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR.value());
     }
 
     @DisplayName("존재하지 않은 출발역이나 도착역으로는 최단거리 경로를 조회할 수 없다.")
     @Test
     void findPath_throwsException_ifFromOrToNonExistentStation() {
+        // given
+        LineSectionAcceptanceTest.지하철_노선에_지하철역_제외_요청(삼호선, 남부터미널역);
 
+        // when
+        ExtractableResponse<Response> response = 출발역_도착역_최단거리_경로_조회(강남역.getId(), 남부터미널역.getId());
+
+        // then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR.value());
+    }
+
+    public static ExtractableResponse<Response> 출발역_도착역_최단거리_경로_조회(Long source, Long target) {
+        Map<String, Long> param = new HashMap<>();
+        param.put("source", source);
+        param.put("target", target);
+
+        return RestAssured
+                .given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when().queryParams(param).get("/paths")
+                .then().log().all()
+                .extract();
+    }
+
+    void 출발역_도착역_최단거리_거리_경로_확인(ExtractableResponse<Response> response, double distance, StationResponse... stations) {
+        PathResponse path = response.as(PathResponse.class);
+        assertThat(path.getStationResponses()).containsExactly(stations);
+        assertThat(path.getDistance()).isEqualTo(distance);
     }
 }
