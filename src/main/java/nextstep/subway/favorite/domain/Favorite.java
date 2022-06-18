cf34bc5abf17a694c5dac4c0f6f4d7506555b768
sy001 <suyounlake@naver.com>
2022-06-18 10:29:25 +0900

    feat: Favorite 도메인, Repository 클래스 작성

---
+++ src/main/java/nextstep/subway/favorite/domain/Favorite.java
@@ -0,0 +1,45 @@
+package nextstep.subway.favorite.domain;
+
+import nextstep.subway.BaseEntity;
+import nextstep.subway.member.domain.Member;
+import nextstep.subway.station.domain.Station;
+
+import javax.persistence.*;
+
+@Entity
+public class Favorite extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "source_id")
+    private Station source;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "target_id")
+    private Station target;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    protected Favorite() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Station getSource() {
+        return source;
+    }
+
+    public Station getTarget() {
+        return target;
+    }
+
+    public Member getMember() {
+        return member;
+    }
+}
