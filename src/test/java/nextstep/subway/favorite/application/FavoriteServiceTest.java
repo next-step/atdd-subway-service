df6d89b2b528f094a14b64bf8d363a0ca2c0e141
piopoi <kangsw1988@gmail.com>
2022-06-18 21:41:02 +0900

    test(FavoriteServiceTest): @DataJpaTest 제거

--- src/test/java/nextstep/subway/favorite/application/FavoriteServiceTest.java
+++ src/test/java/nextstep/subway/favorite/application/FavoriteServiceTest.java
@@ -22,10 +22,8 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 @ExtendWith(MockitoExtension.class)
-@DataJpaTest
 class FavoriteServiceTest {
     public static final Long 사용자_ID = 1L;
     public static final Long 강남역_ID = 1L;
