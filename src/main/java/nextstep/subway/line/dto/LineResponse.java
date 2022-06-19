f5fd4c8982fc52f339eeadd60e66dd3535501b8c
Philip <pch4623@gmail.com>
2022-06-19 14:09:59 +0900

    feedback: 120 컬럼 길이 넘는 경우 줄바꿈

--- src/main/java/nextstep/subway/line/dto/LineResponse.java
+++ src/main/java/nextstep/subway/line/dto/LineResponse.java
@@ -18,7 +18,8 @@ public class LineResponse {
     public LineResponse() {
     }
 
-    public LineResponse(Long id, String name, String color, List<StationResponse> stations, LocalDateTime createdDate, LocalDateTime modifiedDate) {
+    public LineResponse(Long id, String name, String color, List<StationResponse> stations,
+                        LocalDateTime createdDate, LocalDateTime modifiedDate) {
         this.id = id;
         this.name = name;
         this.color = color;
@@ -28,11 +29,13 @@ public class LineResponse {
     }
 
     public static LineResponse of(Line line) {
-        return new LineResponse(line.getId(), line.getName(), line.getColor(), getStationResponses(line), line.getCreatedDate(), line.getModifiedDate());
+        return new LineResponse(line.getId(), line.getName(), line.getColor(), getStationResponses(line),
+                line.getCreatedDate(), line.getModifiedDate());
     }
 
     public static LineResponse of(Line line, List<StationResponse> stations) {
-        return new LineResponse(line.getId(), line.getName(), line.getColor(), stations, line.getCreatedDate(), line.getModifiedDate());
+        return new LineResponse(line.getId(), line.getName(), line.getColor(), stations,
+                line.getCreatedDate(), line.getModifiedDate());
     }
 
     public static List<StationResponse> getStationResponses(Line line) {
