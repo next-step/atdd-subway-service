8be9fa4123305d479311c26726f9a67588ee2764
mond <revive29@gmail.com>
2022-06-20 00:31:44 +0900

    refactor(Fee): 상수 이름 및 중복 메소드 개선

--- src/main/java/nextstep/subway/path/domain/Fee.java
+++ src/main/java/nextstep/subway/path/domain/Fee.java
@@ -6,10 +6,10 @@ import nextstep.subway.line.domain.ExtraCharge;
 import nextstep.subway.line.domain.Line;
 
 public class Fee {
-    private static final int FIRST_SECTION_EXTRA_STANDARD = 10;
-    private static final int FIRST_SECTION_DISTANCE_STANDARD = 5;
-    private static final int SECOND_SECTION_EXTRA_STANDARD = 50;
-    private static final int SECOND_SECTION_DISTANCE_STANDARD = 8;
+    private static final int FIRST_SECTION_OVER_KR_STANDARD = 10;
+    private static final int FIRST_SECTION_DISTANCE_PER_KM_STANDARD = 5;
+    private static final int SECOND_SECTION_OVER_KR_STANDARD = 50;
+    private static final int SECOND_SECTION_DISTANCE_PER_KM_STANDARD = 8;
     private static final int DEFAULT_FEE = 1250;
     private static final int EXTRA_CHARGE = 100;
 
@@ -20,34 +20,37 @@ public class Fee {
     }
 
     public static Fee of(int distance) {
-        int fee = DEFAULT_FEE;
-        fee += calculateOverFirstSection(Math.min(distance, SECOND_SECTION_EXTRA_STANDARD));
-        fee += calculateOverSecondSection(distance);
+        int fee = calculateFee(distance);
         return new Fee(fee);
     }
 
     public static Fee of(int distance, Set<Line> lines) {
-        int fee = DEFAULT_FEE;
-        fee += calculateOverFirstSection(Math.min(distance, SECOND_SECTION_EXTRA_STANDARD));
-        fee += calculateOverSecondSection(distance);
+        int fee = calculateFee(distance);
         fee += calculateLineExtraCharge(lines);
         return new Fee(fee);
     }
 
+    private static int calculateFee(int distance) {
+        int fee = DEFAULT_FEE;
+        fee += calculateOverFirstSection(Math.min(distance, SECOND_SECTION_OVER_KR_STANDARD));
+        fee += calculateOverSecondSection(distance);
+        return fee;
+    }
+
     private static int calculateOverFirstSection(int distance) {
-        return calculateExtraCharge(distance, FIRST_SECTION_EXTRA_STANDARD, FIRST_SECTION_DISTANCE_STANDARD);
+        return calculateExtraCharge(distance, FIRST_SECTION_OVER_KR_STANDARD, FIRST_SECTION_DISTANCE_PER_KM_STANDARD);
     }
 
     private static int calculateOverSecondSection(int distance) {
-        return calculateExtraCharge(distance, SECOND_SECTION_EXTRA_STANDARD, SECOND_SECTION_DISTANCE_STANDARD);
+        return calculateExtraCharge(distance, SECOND_SECTION_OVER_KR_STANDARD, SECOND_SECTION_DISTANCE_PER_KM_STANDARD);
     }
 
-    private static int calculateExtraCharge(int distance, int extraStandard, int distanceStandard) {
-        if (distance <= extraStandard) {
+    private static int calculateExtraCharge(int distance, int overKilometer, int perDistance) {
+        if (distance <= overKilometer) {
             return 0;
         }
-        distance -= extraStandard;
-        return (int) (Math.ceil((distance - 1) / distanceStandard) + 1) * EXTRA_CHARGE;
+        distance -= overKilometer;
+        return (int) (Math.ceil((distance - 1) / perDistance) + 1) * EXTRA_CHARGE;
     }
 
     private static int calculateLineExtraCharge(Set<Line> lines) {
