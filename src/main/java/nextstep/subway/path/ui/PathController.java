287ca63691f7cb7034c76f3c1db1a42e884ffefe
chj1768 <chj1768@gmail.com>
2022-06-20 01:50:20 +0900

    feat: JgraphPathFinder infra 로 패키지 이동

--- src/main/java/nextstep/subway/path/ui/PathController.java
+++ src/main/java/nextstep/subway/path/ui/PathController.java
@@ -1,7 +1,7 @@
 package nextstep.subway.path.ui;
 
-import nextstep.subway.path.PathResponse;
 import nextstep.subway.path.application.PathService;
+import nextstep.subway.path.dto.PathResponse;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
