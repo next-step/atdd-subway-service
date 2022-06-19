182dbc50e6c9ceea87e70f85b1bd8600c1cc48aa
chj1768 <chj1768@gmail.com>
2022-06-20 03:23:30 +0900

    feat: PathFinder 구현체 내 edge 가 어디 호선에 속한 간선인지 확인해주는 edge/Line 맵 추가하여 최단 경로 계산시 최단 호선도 알려주도록 필드 추가 Lines 내에선 가장 비싼 추가요금을 가진 호선 알려주는 메서드 추가

--- src/main/java/nextstep/subway/path/dto/ShortestPath.java
+++ src/main/java/nextstep/subway/path/dto/ShortestPath.java
@@ -1,15 +1,19 @@
 package nextstep.subway.path.dto;
 
+import nextstep.subway.line.domain.Line;
+import nextstep.subway.line.domain.Lines;
 import nextstep.subway.station.domain.Station;
 
 import java.util.List;
 
 public class ShortestPath {
     private final List<Station> stations;
+    private final Lines lines;
     private final long distance;
 
-    public ShortestPath(List<Station> stations, long distance) {
+    public ShortestPath(List<Station> stations, Lines lines, long distance) {
         this.stations = stations;
+        this.lines = lines;
         this.distance = distance;
     }
 
@@ -19,5 +23,9 @@ public class ShortestPath {
 
     public List<Station> getStations() {
         return stations;
+    }
+
+    public Line mostExpensive() {
+        return this.lines.mostExpensive();
     }
 }
--- src/main/java/nextstep/subway/path/infrastructure/JGraphPathFinder.java
+++ src/main/java/nextstep/subway/path/infrastructure/JGraphPathFinder.java
@@ -13,18 +13,28 @@ import org.jgrapht.graph.DefaultWeightedEdge;
 import org.jgrapht.graph.WeightedMultigraph;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 @Service
 public class JGraphPathFinder implements PathFinder {
     @Override
     public ShortestPath getShortestPath(PathRequest pathRequest) {
+        Map<DefaultWeightedEdge, Line> edgeLineMap = new HashMap<>();
         DijkstraShortestPath<Station, DefaultWeightedEdge> dijkstraShortestPath =
-                new DijkstraShortestPath<>(graph(pathRequest.getLines()));
+                new DijkstraShortestPath<>(graph(pathRequest.getLines(), edgeLineMap));
         GraphPath<Station, DefaultWeightedEdge> graphPath =
                 dijkstraShortestPath.getPath(pathRequest.getSource(), pathRequest.getTarget());
-        return new ShortestPath(graphPath.getVertexList(), (long) graphPath.getWeight());
+
+        return new ShortestPath(graphPath.getVertexList(), new Lines(
+                graphPath.getEdgeList().stream().map(edgeLineMap::get).distinct().collect(Collectors.toList())),
+                (long) graphPath.getWeight());
+
     }
 
-    private WeightedMultigraph<Station, DefaultWeightedEdge> graph(Lines lines) {
+    private WeightedMultigraph<Station, DefaultWeightedEdge> graph(
+            Lines lines, Map<DefaultWeightedEdge, Line> weightedEdgeSectionMap) {
         WeightedMultigraph<Station, DefaultWeightedEdge> graph = new WeightedMultigraph<>(DefaultWeightedEdge.class);
         for (Line line : lines) {
             for (Section section : line.getSections()) {
@@ -32,7 +42,9 @@ public class JGraphPathFinder implements PathFinder {
                 Station v2 = section.getDownStation();
                 graph.addVertex(v1);
                 graph.addVertex(v2);
-                graph.setEdgeWeight(graph.addEdge(v1, v2), section.getDistance());
+                DefaultWeightedEdge edge = graph.addEdge(v1, v2);
+                graph.setEdgeWeight(edge, section.getDistance());
+                weightedEdgeSectionMap.put(edge, line);
             }
         }
         return graph;
