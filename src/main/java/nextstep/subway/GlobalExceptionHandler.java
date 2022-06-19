dee1e13591daf95836bd495c5b2ae2ecb24ae66f
kms <kmmin78@gmail.com>
2022-06-20 00:54:45 +0900

    feat step3 토큰 발급 기능

--- src/main/java/nextstep/subway/GlobalExceptionHandler.java
+++ src/main/java/nextstep/subway/GlobalExceptionHandler.java
@@ -1,9 +1,12 @@
 package nextstep.subway;
 
+import nextstep.subway.auth.application.AuthorizationException;
+import nextstep.subway.auth.exceptions.NotValidTokenException;
 import nextstep.subway.path.exceptions.SourceAndTargetSameException;
 import nextstep.subway.path.exceptions.SourceNotConnectedWithTargetException;
 import nextstep.subway.path.exceptions.StationNotExistException;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -13,7 +16,12 @@ public class GlobalExceptionHandler {
     @ExceptionHandler({DataIntegrityViolationException.class, SourceAndTargetSameException.class,
             SourceNotConnectedWithTargetException.class,
             StationNotExistException.class})
-    public ResponseEntity handleIllegalArgsException(final Exception e) {
+    public ResponseEntity handleBadRequestException(final Exception e) {
         return ResponseEntity.badRequest().build();
+    }
+
+    @ExceptionHandler({AuthorizationException.class, NotValidTokenException.class})
+    public ResponseEntity handleUnauthorizedException(final Exception e) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
     }
 }
