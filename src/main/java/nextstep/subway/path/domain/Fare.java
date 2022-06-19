82dbe741096bc3ab3b4e7d855aa7c6129e63d02f
sy001 <suyounlake@naver.com>
2022-06-19 18:49:19 +0900

    feat: 노선별 추가 요금 정책 기능 구현

--- src/main/java/nextstep/subway/path/domain/Fare.java
+++ src/main/java/nextstep/subway/path/domain/Fare.java
@@ -1,15 +1,32 @@
 package nextstep.subway.path.domain;
 
+import nextstep.subway.line.domain.Line;
+import nextstep.subway.line.domain.Section;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Fare {
     private int value;
 
     private Fare(Path path) {
-        this.value = calculateOverFare(path.getDistance());
+        this.value = calculateOverFare(path.getDistance()) + calculateLineFare(path.getSections());
     }
 
     private int calculateOverFare(int distance) {
         FareDistanceType distanceType = FareDistanceType.typeOf(distance);
         return distanceType.calculateFare(distance);
+    }
+
+    private int calculateLineFare(List<Section> sections) {
+        List<Line> lines = sections.stream()
+                .map(section -> section.getLine())
+                .distinct()
+                .collect(Collectors.toList());
+        return lines.stream()
+                .map(line -> line.getAdditionalFare())
+                .max(Integer::compareTo)
+                .orElseThrow(IllegalArgumentException::new);
     }
 
     public static Fare from(Path path) {
