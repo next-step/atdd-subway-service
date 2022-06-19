4c6e317386b896279a321a0dd7060f4c0df48201
DongGyuJang <cdgu1129@knou.ac.kr>
2022-06-20 03:37:02 +0900

    feat: 예외 로직 추가

--- src/main/java/nextstep/subway/path/domain/Path.java
+++ src/main/java/nextstep/subway/path/domain/Path.java
@@ -20,10 +20,26 @@ public class Path {
     private final WeightedMultigraph<Station, DefaultWeightedEdge> graph = new WeightedMultigraph(DefaultWeightedEdge.class);
 
     public PathResponse find(List<Line> lines, Station sourceStation, Station targetStation) {
+        validationSameStation(sourceStation, targetStation);
+
         init(lines);
+
         GraphPath<Station, DefaultWeightedEdge> path = findPath(sourceStation, targetStation);
+        validationPathNull(path);
 
         return new PathResponse(path.getVertexList(), path.getWeight());
+    }
+
+    private void validationPathNull(GraphPath<Station, DefaultWeightedEdge> path) {
+        if (path == null) {
+            throw new RuntimeException("역 사이에 경로가 존재하지 않습니다.");
+        }
+    }
+
+    private void validationSameStation(Station sourceStation, Station targetStation) {
+        if (sourceStation.equals(targetStation)) {
+            throw new RuntimeException("출발역과 도착역과 같습니다.");
+        }
     }
 
     private void init(List<Line> lines) {
--- src/main/java/nextstep/subway/path/ui/PathController.java
+++ src/main/java/nextstep/subway/path/ui/PathController.java
@@ -4,6 +4,7 @@ import nextstep.subway.member.application.MemberService;
 import nextstep.subway.member.dto.MemberRequest;
 import nextstep.subway.member.dto.MemberResponse;
 import nextstep.subway.path.apllication.PathService;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.repository.query.Param;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -23,5 +24,10 @@ public class PathController {
                                     @RequestParam("target") Long target) {
 
         return ResponseEntity.ok(pathService.findPath(source, target));
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity handleIllegalArgsException(RuntimeException e) {
+        return ResponseEntity.badRequest().build();
     }
 }
