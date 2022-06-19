34dbea13a8ee7f117b2a2111924219d0359cb171
ssamzag <ssamzag@gmail.com>
2022-06-19 23:43:43 +0900

    refactor: 파라미터명 n -> age 변경

--- src/main/java/nextstep/subway/line/domain/Discount.java
+++ src/main/java/nextstep/subway/line/domain/Discount.java
@@ -4,10 +4,10 @@ import java.util.Arrays;
 import java.util.function.Predicate;
 
 public enum Discount {
-    ADULT(0, 0, n -> n == null || n >= 19),
-    CHILD(50, 350, n -> n >= 6 && n < 13),
-    TEENAGER(20, 350, n -> n >= 13 && n < 19),
-    FREE(100, 0, n -> n < 6);
+    ADULT(0, 0, age -> age == null || age >= 19),
+    CHILD(50, 350, age -> age >= 6 && age < 13),
+    TEENAGER(20, 350, age -> age >= 13 && age < 19),
+    FREE(100, 0, age -> age < 6);
 
     private final int discountRate;
     private final int discount;
