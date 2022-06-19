9ad69b4c7421c200750054351416327d2e497818
millennion-jin <Jadestern2@>
2022-06-20 01:35:33 +0900

    feat(Sections) : Sections 로 옮길 로직들의 메서드 생성

--- src/main/java/nextstep/subway/line/domain/Sections.java
+++ src/main/java/nextstep/subway/line/domain/Sections.java
@@ -5,9 +5,26 @@ import java.util.List;
 import javax.persistence.CascadeType;
 import javax.persistence.Embeddable;
 import javax.persistence.OneToMany;
+import nextstep.subway.station.domain.Station;
 
 @Embeddable
 public class Sections {
     @OneToMany(mappedBy = "line", cascade = {CascadeType.PERSIST, CascadeType.MERGE}, orphanRemoval = true)
     private List<Section> sections = new ArrayList<>();
+
+    public List<Station> getStations() {
+        return null;
+    }
+
+    public void addSection(Station upStation, Station downStation, int distance) {
+
+    }
+
+    public void removeSection(Station station) {
+
+    }
+
+    public Station findUpStation() {
+        return null;
+    }
 }
