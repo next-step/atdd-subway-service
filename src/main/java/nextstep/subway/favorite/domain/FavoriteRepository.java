42952d6b572edec8816340365a8fb8d0645f651d
minzzang <minjjang1117@gmail.com>
2022-06-20 00:35:56 +0900

    test(FavoriteServiceTest): 즐겨찾기 목록을 조회한다

--- src/main/java/nextstep/subway/favorite/domain/FavoriteRepository.java
+++ src/main/java/nextstep/subway/favorite/domain/FavoriteRepository.java
@@ -1,5 +1,11 @@
 package nextstep.subway.favorite.domain;
 
+import nextstep.subway.member.domain.Member;
+
+import java.util.List;
+
 public interface FavoriteRepository {
     Favorite save(Favorite favorite);
+
+    List<Favorite> findAllByMember(Member member);
 }
--- src/test/java/nextstep/subway/favorite/application/FavoriteServiceTest.java
+++ src/test/java/nextstep/subway/favorite/application/FavoriteServiceTest.java
@@ -2,18 +2,23 @@ package nextstep.subway.favorite.application;
 
 import nextstep.subway.favorite.domain.FavoriteRepository;
 import nextstep.subway.favorite.dto.FavoriteRequest;
+import nextstep.subway.favorite.dto.FavoriteResponse;
 import nextstep.subway.favorite.infrastructure.InMemoryFavoriteRepository;
 import nextstep.subway.member.application.MemberService;
+import nextstep.subway.member.domain.Member;
 import nextstep.subway.member.infrastructure.InMemoryMemberRepository;
 import nextstep.subway.station.application.StationService;
 import nextstep.subway.station.infra.InMemoryStationRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 class FavoriteServiceTest {
     private FavoriteService favoriteService;
+    private Member member;
     private Long source;
     private Long target;
 
@@ -23,6 +28,8 @@ class FavoriteServiceTest {
         target = 2L;
 
         MemberService memberService = new MemberService(new InMemoryMemberRepository());
+        member = memberService.findById(1L);
+
         StationService stationService = new StationService(new InMemoryStationRepository());
         FavoriteRepository favoriteRepository = new InMemoryFavoriteRepository();
         favoriteService = new FavoriteService(memberService, stationService, favoriteRepository);
@@ -34,9 +41,18 @@ class FavoriteServiceTest {
         FavoriteRequest request = new FavoriteRequest(source, target);
 
         // when
-        Long result = favoriteService.createFavorite(1L, request);
+        Long result = favoriteService.createFavorite(member.getId(), request);
 
         // then
         assertThat(result).isNotNull();
+    }
+
+    @Test
+    void 즐겨찾기_목록을_조회한다() {
+        // when
+        List<FavoriteResponse> result = favoriteService.findFavorites(member.getId());
+
+        // then
+        assertThat(result).hasSize(2);
     }
 }
