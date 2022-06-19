b9e453ef1b0981b869236d4a1005266669395606
minzzang <minjjang1117@gmail.com>
2022-06-19 20:59:06 +0900

    test(AuthServiceTest): authService test 리팩토링

--- src/test/java/nextstep/subway/auth/infrastructure/JwtTokenProviderTest.java
+++ src/test/java/nextstep/subway/auth/infrastructure/JwtTokenProviderTest.java
@@ -6,16 +6,16 @@ import org.junit.jupiter.api.Test;
 import static nextstep.subway.auth.application.AuthServiceTest.EMAIL;
 import static org.assertj.core.api.Assertions.assertThat;
 
-class JwtTokenProviderTest {
-    private static String secretKey = "E1MTYyMzkwMjJ9.ih1aovtQShabQ7l0cINw4k1fagApg3qLW";
-    private static long validityInMilliseconds = 6000L;
+public class JwtTokenProviderTest {
+    public static String SECRET_KEY = "E1MTYyMzkwMjJ9.ih1aovtQShabQ7l0cINw4k1fagApg3qLW";
+    public static long EXPIRE_LENGTH = 6000L;
 
     private JwtTokenProvider jwtTokenProvider;
     private String jwtToken;
 
     @BeforeEach
     void setUp() {
-        jwtTokenProvider = new JwtTokenProvider(secretKey, validityInMilliseconds);
+        jwtTokenProvider = new JwtTokenProvider(SECRET_KEY, EXPIRE_LENGTH);
         jwtToken = jwtTokenProvider.createToken(EMAIL);
     }
 
