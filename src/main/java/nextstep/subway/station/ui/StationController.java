0d2d00552032686d5e9852b1e029304aad272d85
서윤철 <woody@29cm.co.kr>
2022-06-20 02:36:28 +0900

    refactor : ResponseEntity 제네릭 선언 추가

--- src/main/java/nextstep/subway/station/ui/StationController.java
+++ src/main/java/nextstep/subway/station/ui/StationController.java
@@ -30,7 +30,7 @@ public class StationController {
     }
 
     @DeleteMapping("/stations/{id}")
-    public ResponseEntity deleteStation(@PathVariable Long id) {
+    public ResponseEntity<Void> deleteStation(@PathVariable Long id) {
         stationService.deleteStationById(id);
         return ResponseEntity.noContent().build();
     }
