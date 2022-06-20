d6a205aef6885a674c15436c44606dea853e1e77
minzzang <minjjang1117@gmail.com>
2022-06-20 12:04:59 +0900

    refactor(Member): email, password not null 설정

--- src/main/java/nextstep/subway/member/domain/Member.java
+++ src/main/java/nextstep/subway/member/domain/Member.java
@@ -12,8 +12,9 @@ public class Member extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(unique = true)
+    @Column(unique = true, nullable = false)
     private String email;
+    @Column(nullable = false)
     private String password;
     private Integer age;
 
