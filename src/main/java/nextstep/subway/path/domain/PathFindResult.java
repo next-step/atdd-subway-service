721ea0c6328371899a453b054e7d0ae4519ad636
mmtos <js223kr92@naver.com>
2022-06-21 06:20:02 +0900

    review-feedback : PathFindResult로 거리 및 노선별 할인 역할 이동

--- src/main/java/nextstep/subway/path/domain/PathFindResult.java
+++ src/main/java/nextstep/subway/path/domain/PathFindResult.java
@@ -1,8 +1,5 @@
 package nextstep.subway.path.domain;
 
-import static nextstep.subway.path.domain.OverFareCalculator.calculateOverFareByDistance;
-import static nextstep.subway.path.domain.OverFareCalculator.calculateOverFareByLine;
-
 import java.util.List;
 import java.util.Set;
 import nextstep.subway.auth.domain.LoginMember;
@@ -13,6 +10,17 @@ import nextstep.subway.station.domain.Station;
  * 경로 검색 결과를 리턴하는 도메인 객체 (도메인에서 DTO를 의존하는 상황을 피하기 위함)
  */
 public class PathFindResult {
+    private static final int FIRST_OVER_FARE_RANGE_START = 10;
+    private static final int FIRST_OVER_FARE_RANGE_END = 50;
+    private static final int FIRST_OVER_FARE_RANGE_INTERVAL = 5;
+    private static final int FIRST_OVER_FARE_RANGE_FARE_UNIT = 100;
+
+    private static final int SECOND_OVER_FARE_RANGE_START = 50;
+    private static final int SECOND_OVER_FARE_RANGE_INTERVAL = 8;
+    private static final int SECOND_OVER_FARE_RANGE_FARE_UNIT = 100;
+
+    private static final int NO_OVER_FARE = 0;
+
     private List<Station> stations;
 
     private Set<Line> lines;
@@ -33,14 +41,48 @@ public class PathFindResult {
     }
 
     public void applyFarePolicy(LoginMember loginMember) {
-        int overFareByDistance = calculateOverFareByDistance(this);
-        int overFareByLine = calculateOverFareByLine(this);
+        int overFareByDistance = calculateOverFareByDistance();
+        int overFareByLine = calculateOverFareByLine();
         int middleSum = this.fare.getValue() + overFareByDistance + overFareByLine;
         if (loginMember.getId() == null) {
             this.fare = SubwayFare.of(middleSum);
             return;
         }
         this.fare = loginMember.discountFareByAge(SubwayFare.of(middleSum));
+    }
+
+    public int calculateOverFareByDistance() {
+        return calculateOverFareInFirstRange()
+                + calculateOverFareInSecondRange();
+    }
+
+    private int calculateOverFareInFirstRange() {
+        int overDistance = Math.min(distance, FIRST_OVER_FARE_RANGE_END) - FIRST_OVER_FARE_RANGE_START;
+        if (!hasOverDistance(overDistance)) {
+            return NO_OVER_FARE;
+        }
+        return (int) ((Math.ceil((overDistance - 1) / FIRST_OVER_FARE_RANGE_INTERVAL) + 1)
+                * FIRST_OVER_FARE_RANGE_FARE_UNIT);
+    }
+
+    private boolean hasOverDistance(int overDistance) {
+        return overDistance > 0;
+    }
+
+    private int calculateOverFareInSecondRange() {
+        int overDistance = distance - SECOND_OVER_FARE_RANGE_START;
+        if (!hasOverDistance(overDistance)) {
+            return NO_OVER_FARE;
+        }
+        return (int) ((Math.ceil((overDistance - 1) / SECOND_OVER_FARE_RANGE_INTERVAL) + 1)
+                * SECOND_OVER_FARE_RANGE_FARE_UNIT);
+    }
+
+    public int calculateOverFareByLine() {
+        return lines.stream()
+                .mapToInt(Line::getExtraCharge)
+                .max()
+                .getAsInt();
     }
 
     public List<Station> getStations() {
--- src/test/java/nextstep/subway/path/domain/PathFindResultTest.java
+++ src/test/java/nextstep/subway/path/domain/PathFindResultTest.java
@@ -1,10 +1,9 @@
 package nextstep.subway.path.domain;
 
 import static java.util.Collections.emptyList;
-import static java.util.Collections.emptySet;
 import static nextstep.subway.path.domain.PathFindServiceTest.노선생성;
 import static org.assertj.core.api.Assertions.assertThat;
-
+import nextstep.subway.auth.domain.LoginMember;
 import nextstep.subway.line.domain.Line;
 import nextstep.subway.station.domain.Station;
 import org.assertj.core.util.Sets;
@@ -12,27 +11,27 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-class OverFareCalculatorTest {
-
+class PathFindResultTest {
     Station 강남역 = new Station("강남역");
     Station 역삼역 = new Station("역삼역");
     Line 이호선 = 노선생성("이호선", "green", 강남역, 역삼역, 5, 500);
     Line 삼호선 = 노선생성("삼호선", "orange", 강남역, 역삼역, 5, 800);
+    Line 추가요금없는노선 = 노선생성("삼호선", "orange", 강남역, 역삼역, 5, 0);
+    LoginMember 비회원 = new LoginMember();
 
     @ParameterizedTest
     @CsvSource(delimiterString = ":", value = {"5:0", "10:0", "11:100", "20:200", "50:800", "51:900", "66:1000",
             "67:1100"})
     void 거리별_추가요금_계산_테스트(String distance, String expectedOverFare) {
-        PathFindResult pathFindResult = new PathFindResult(emptyList(), emptySet(), Integer.parseInt(distance));
-        int overFare = OverFareCalculator.calculateOverFareByDistance(pathFindResult);
-        assertThat(overFare).isEqualTo(Integer.parseInt(expectedOverFare));
+        PathFindResult pathFindResult = new PathFindResult(emptyList(), Sets.newLinkedHashSet(추가요금없는노선), Integer.parseInt(distance));
+        pathFindResult.applyFarePolicy(비회원);
+        assertThat(pathFindResult.getFare()).isEqualTo(SubwayFare.DEFAULT_FARE.plus(Integer.parseInt(expectedOverFare)));
     }
 
     @Test
     void 노선별_추가요금_계산_테스트() {
         PathFindResult pathFindResult = new PathFindResult(emptyList(), Sets.newLinkedHashSet(이호선, 삼호선), 10);
-        int overFare = OverFareCalculator.calculateOverFareByLine(pathFindResult);
-        assertThat(overFare).isEqualTo(삼호선.getExtraCharge());
+        pathFindResult.applyFarePolicy(비회원);
+        assertThat(pathFindResult.getFare()).isEqualTo(SubwayFare.DEFAULT_FARE.plus(삼호선.getExtraCharge()));
     }
-
 }
