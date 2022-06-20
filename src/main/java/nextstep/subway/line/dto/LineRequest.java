b809752f08fa41cc494bff222489772473f52c65
Celina <luv.mins99@gmail.com>
2022-06-20 12:36:21 +0900

    feat(extraFare): 노선에 추가 요금 필드 추가

--- src/main/java/nextstep/subway/line/dto/LineRequest.java
+++ src/main/java/nextstep/subway/line/dto/LineRequest.java
@@ -6,16 +6,22 @@ public class LineRequest {
     private Long upStationId;
     private Long downStationId;
     private int distance;
+    private int extraFare;
 
     public LineRequest() {
     }
 
     public LineRequest(String name, String color, Long upStationId, Long downStationId, int distance) {
+        this(name, color, upStationId, downStationId, distance, 0);
+    }
+
+    public LineRequest(String name, String color, Long upStationId, Long downStationId, int distance, int extraFare) {
         this.name = name;
         this.color = color;
         this.upStationId = upStationId;
         this.downStationId = downStationId;
         this.distance = distance;
+        this.extraFare = extraFare;
     }
 
     public String getName() {
@@ -36,5 +42,9 @@ public class LineRequest {
 
     public int getDistance() {
         return distance;
+    }
+
+    public int getExtraFare() {
+        return extraFare;
     }
 }
