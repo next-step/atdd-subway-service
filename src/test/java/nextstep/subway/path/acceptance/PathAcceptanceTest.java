51f649fb0d807160e1f548adfb846b8fdbd02fc1
songsimo <chy.song89@gmail.com>
2022-06-21 00:34:51 +0900

    docs: 요금 조회 요구사항 정리

--- src/test/java/nextstep/subway/path/acceptance/PathAcceptanceTest.java
+++ src/test/java/nextstep/subway/path/acceptance/PathAcceptanceTest.java
@@ -11,7 +11,6 @@ import java.util.Arrays;
 import nextstep.subway.AcceptanceTest;
 import nextstep.subway.line.dto.LineRequest;
 import nextstep.subway.line.dto.LineResponse;
-import nextstep.subway.station.StationAcceptanceTest;
 import nextstep.subway.station.dto.StationResponse;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
