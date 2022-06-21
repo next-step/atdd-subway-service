5d7a79843bbc96626ec335b19ff3919944153015
piopoi <kangsw1988@gmail.com>
2022-06-22 02:11:47 +0900

    test(DistanceFarePolicy): 거리별 요금정책 비즈니스로직 변경 및 단위테스트 추가

--- src/main/java/nextstep/subway/path/domain/DistanceFarePolicy.java
+++ src/main/java/nextstep/subway/path/domain/DistanceFarePolicy.java
@@ -1,31 +1,38 @@
 package nextstep.subway.path.domain;
 
+import static nextstep.subway.path.domain.DistanceFarePolicy.DistanceFarePolicyConstants.*;
+
 import java.util.Arrays;
+import java.util.function.Function;
 
 public enum DistanceFarePolicy {
-    LOW(1, 10, 0, 0, DistanceFarePolicyConstants.DEFAULT_FARE),
-    MIDDLE(11, 50, 5, 100, DistanceFarePolicyConstants.DEFAULT_FARE),
-    HIGH(51, 178, 8, 100, DistanceFarePolicyConstants.DEFAULT_HIGH_FARE);
+    LOW(1, 10,
+            distance -> DEFAULT_FARE),
+    MIDDLE(11, 50,
+            distance -> DEFAULT_FARE + calculateOverFare(distance, 11, MIDDLE_UNIT, OVERFARE_PER_UNIT)),
+    HIGH(51, 178,
+            distance -> DEFAULT_HIGH_FARE + calculateOverFare(distance, 51, HIGH_UNIT, OVERFARE_PER_UNIT));
 
-    private static class DistanceFarePolicyConstants {
-        private static final int DEFAULT_FARE = 1250;
-        private static final int DEFAULT_HIGH_FARE = DEFAULT_FARE + (int) (
-                (Math.ceil((MIDDLE.endDistance - MIDDLE.startDistance) / MIDDLE.calculateUnit) + 1)
-                        * MIDDLE.overFarePerUnit);
+    public static class DistanceFarePolicyConstants {
+        public static final int OVERFARE_PER_UNIT = 100;
+        public static final int OVERFARE_PER_UNIT_MIN = 0;
+        public static final int OVERFARE_MIN = 0;
+        public static final int MIDDLE_UNIT = 5;
+        public static final int HIGH_UNIT = 8;
+        public static final int DEFAULT_FARE = 1250;
+        public static final int DEFAULT_HIGH_FARE = DEFAULT_FARE + (int) (
+                (Math.ceil((MIDDLE.endDistance - MIDDLE.startDistance) / MIDDLE_UNIT) + 1)
+                        * OVERFARE_PER_UNIT);
     }
 
     private final int startDistance;
     private final int endDistance;
-    private final int calculateUnit;
-    private final int overFarePerUnit;
-    private final int defaultFare;
+    private final Function<Integer, Integer> expression;
 
-    DistanceFarePolicy(int startDistance, int endDistance, int calculateUnit, int overFarePerUnit, int defaultFare) {
+    DistanceFarePolicy(int startDistance, int endDistance, Function<Integer, Integer> expression) {
         this.startDistance = startDistance;
         this.endDistance = endDistance;
-        this.calculateUnit = calculateUnit;
-        this.overFarePerUnit = overFarePerUnit;
-        this.defaultFare = defaultFare;
+        this.expression = expression;
     }
 
     public static DistanceFarePolicy findByDistance(int distance) {
@@ -40,10 +47,14 @@ public enum DistanceFarePolicy {
     }
 
     public int calculate(int distance) {
-        int fare = defaultFare;
-        if (overFarePerUnit > 0) {
+        return expression.apply(distance);
+    }
+
+    private static int calculateOverFare(int distance, int startDistance, int calculateUnit, int overFarePerUnit) {
+        int fare = OVERFARE_MIN;
+        if (overFarePerUnit > OVERFARE_PER_UNIT_MIN) {
             int calculateDistance = distance - startDistance + 1;
-            fare += (int) ((Math.ceil((calculateDistance - 1) / calculateUnit) + 1) * overFarePerUnit);
+            fare = (int) ((Math.ceil((calculateDistance - 1) / calculateUnit) + 1) * overFarePerUnit);
         }
         return fare;
     }
