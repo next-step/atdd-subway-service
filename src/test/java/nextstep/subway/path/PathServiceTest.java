067230774277557dd728c22491889a86e15c0b98
boss.back <boss.back@kakaocorp.com>
2022-06-22 08:45:35 +0900

    feat: PathService Test 예외 처리 추가

--- src/test/java/nextstep/subway/path/PathServiceTest.java
+++ src/test/java/nextstep/subway/path/PathServiceTest.java
@@ -2,6 +2,7 @@ package nextstep.subway.path;
 
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
@@ -36,6 +37,7 @@ public class PathServiceTest {
     private Station 강남역;
     private Station 양재역;
     private Station 남부터미널역;
+    private Station 부산역;
     private List<Section> 모든구간;
 
     @BeforeEach
@@ -43,6 +45,7 @@ public class PathServiceTest {
         강남역 = new Station("강남역");
         양재역 = new Station("양재역");
         남부터미널역 = new Station("남부터미널역");
+        부산역 = new Station("부산역");
         모든구간 = new ArrayList<>();
     }
 
@@ -61,5 +64,47 @@ public class PathServiceTest {
         assertThat(shortestPath).isNotNull();
         assertThat(shortestPath.getStations())
             .hasSameElementsAs(Arrays.asList(강남역, 양재역, 남부터미널역));
+    }
+
+    @DisplayName("출발역과 도착역이 같은 경우 최단 경로 조회를 실패한다.")
+    @Test
+    public void findShortestPathWithSameStation() {
+        //given
+        PathService pathService = new PathService(stationService, pathFinder, lineRepository);
+        when(stationService.findStationById(1l)).thenReturn(강남역);
+        when(pathFinder.findShortestPath(모든구간, 강남역, 강남역)).thenThrow(IllegalArgumentException.class);
+
+        //when
+        //then
+        assertThatThrownBy(() -> pathService.findShortestPath(1l, 1l))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("출발역과 도착역이 연결되어 있지 않으면 최단 경로 조회를 실패한다.")
+    @Test
+    public void findShortestPathWithNotConnectedStation() {
+        //given
+        PathService pathService = new PathService(stationService, pathFinder, lineRepository);
+        when(stationService.findStationById(1l)).thenReturn(강남역);
+        when(stationService.findStationById(3l)).thenReturn(부산역);
+        when(pathFinder.findShortestPath(모든구간, 강남역, 부산역)).thenThrow(IllegalArgumentException.class);
+        //when
+        //then
+        assertThatThrownBy(() -> pathService.findShortestPath(1l, 3l))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("존재하지 않은 출발역이나 도착역을 조회 할 경우 최단 경로 조회를 실패한다.")
+    @Test
+    public void findShortestPathWithNotExistStation() {
+        //given
+        PathService pathService = new PathService(stationService, pathFinder, lineRepository);
+        when(stationService.findStationById(1l)).thenReturn(강남역);
+        when(stationService.findStationById(9999l)).thenReturn(null);
+        when(pathFinder.findShortestPath(모든구간, 강남역, null)).thenThrow(IllegalArgumentException.class);
+        //when
+        //then
+        assertThatThrownBy(() -> pathService.findShortestPath(1l, 9999l))
+            .isInstanceOf(IllegalArgumentException.class);
     }
 }
