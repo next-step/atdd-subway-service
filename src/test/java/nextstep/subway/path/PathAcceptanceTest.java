b1f98495e1a1ebc73f497fc1e86df42cd8a25aa8
kbh0581 <kbh0581@gmail.com>
2022-06-22 06:46:23 +0900

    wip: 인수테스트정의

--- src/test/java/nextstep/subway/path/PathAcceptanceTest.java
+++ src/test/java/nextstep/subway/path/PathAcceptanceTest.java
@@ -3,11 +3,12 @@ package nextstep.subway.path;
 import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
+import java.util.HashMap;
+import java.util.Map;
 import nextstep.subway.AcceptanceTest;
 import nextstep.subway.line.acceptance.LineAcceptanceTest;
 import nextstep.subway.line.dto.LineRequest;
 import nextstep.subway.line.dto.LineResponse;
-import nextstep.subway.path.dto.PathRequest;
 import nextstep.subway.station.StationAcceptanceTest;
 import nextstep.subway.station.dto.StationResponse;
 import org.junit.jupiter.api.BeforeEach;
@@ -43,17 +44,23 @@ public class PathAcceptanceTest extends AcceptanceTest {
         교대역 = StationAcceptanceTest.지하철역_등록되어_있음("교대역").as(StationResponse.class);
         남부터미널역 = StationAcceptanceTest.지하철역_등록되어_있음("남부터미널역").as(StationResponse.class);
 
+        final ExtractableResponse<Response> 지하철_노선에_구간_등록_요청 = LineAcceptanceTest.지하철_노선_생성_요청(신분당선, 강남역, 양재역, 3);
+
         신분당선 = 지하철_노선_등록되어_있음("신분당선", "bg-red-600", 강남역, 양재역, 10);
         이호선 = 지하철_노선_등록되어_있음("이호선", "bg-red-600", 교대역, 강남역, 10);
         삼호선 = 지하철_노선_등록되어_있음("삼호선", "bg-red-600", 교대역, 양재역, 5);
+
+        //지하철_노선_구간_등록_요청
+
+        //LineAcceptanceTest.지하철_노선_생성_요청(삼호선, 교대역, 남부터미널역, 3);
 
     }
 
 
     @Test
     void test() {
+        final ExtractableResponse<Response> 최단거리_경로를_구한다 = 최단거리_경로를_구한다(강남역, 교대역);
 
-        최단거리_경로를_구한다(new PathRequest(1L,2L));
     }
 
     private LineResponse 지하철_노선_등록되어_있음(String name, String color, StationResponse upStation, StationResponse downStation, int distance) {
@@ -62,15 +69,20 @@ public class PathAcceptanceTest extends AcceptanceTest {
     }
 
 
-    public static ExtractableResponse<Response> 최단거리_경로를_구한다(PathRequest params) {
+    public static ExtractableResponse<Response> 최단거리_경로를_구한다(StationResponse 시작역, StationResponse 도착역) {
+        Map<String, Long> params = new HashMap<>();
+        params.put("source", 시작역.getId());
+        params.put("target", 도착역.getId());
+
         return RestAssured
                 .given().log().all()
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .param("source", params.getSource())
-                .param("target", params.getTarget())
+                .params(params)
                 .when().get("/paths")
                 .then().log().all().extract();
     }
+
+    //private static ExtractableResponse<Void> 지하철_노선_구간_등록_요청()
 
 
 
