77dacab99f0c24280ff5741920f7ffc3e8b6abdb
Celina <luv.mins99@gmail.com>
2022-06-21 11:35:19 +0900

    test(DistanceFareTest): 테스트 클래스명 변경
    - FareTest -> DistanceFareTest

--- src/test/java/nextstep/subway/fare/domain/DistanceFareTest.java
+++ src/test/java/nextstep/subway/fare/domain/DistanceFareTest.java
@@ -5,13 +5,16 @@ import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-class FareTest {
+class DistanceFareTest {
 
     @ParameterizedTest(name = "{0}km 구간의 요금은 {1}원이다")
     @CsvSource(value = {"10:1250", "50:2050", "58:2150", "100:2750"}, delimiter = ':')
     void calculateFare(int distance, int expected) {
-        // given & when
-        int actual = Fare.calculateFare(distance);
+        // given
+        DistanceFare farePolicy = new DistanceFare();
+
+        // when
+        int actual = farePolicy.calculateFare(distance);
 
         // then
         assertThat(actual).isEqualTo(expected);
