73a2a4a0893507e452ffcbbfb731cdd6bf217ef9
Ttungga <torb2002@gmail.com>
2022-06-22 22:20:26 +0900

    test: add GivenSubway and make PathAcceptanceTest use it

---
+++ src/test/java/nextstep/subway/GivenSubway.java
@@ -0,0 +1,46 @@
+package nextstep.subway;
+
+import nextstep.subway.line.acceptance.LineAcceptanceTest;
+import nextstep.subway.line.acceptance.LineSectionAcceptanceTest;
+import nextstep.subway.line.dto.LineRequest;
+import nextstep.subway.line.dto.LineResponse;
+import nextstep.subway.station.StationAcceptanceTest;
+import nextstep.subway.station.dto.StationResponse;
+
+public class GivenSubway {
+    public final StationResponse 강남역;
+    public final StationResponse 양재역;
+    public final StationResponse 교대역;
+    public final StationResponse 남부터미널역;
+    public final StationResponse 여의도역;
+    public final StationResponse 샛강역;
+
+    public final LineResponse 신분당선;
+    public final LineResponse 이호선;
+    public final LineResponse 삼호선;
+    public final LineResponse 구호선;
+
+    public GivenSubway() {
+        강남역 = StationAcceptanceTest.지하철역_등록되어_있음("강남역").as(StationResponse.class);
+        양재역 = StationAcceptanceTest.지하철역_등록되어_있음("양재역").as(StationResponse.class);
+        교대역 = StationAcceptanceTest.지하철역_등록되어_있음("교대역").as(StationResponse.class);
+        남부터미널역 = StationAcceptanceTest.지하철역_등록되어_있음("남부터미널역").as(StationResponse.class);
+        여의도역 = StationAcceptanceTest.지하철역_등록되어_있음("여의도역").as(StationResponse.class);
+        샛강역 = StationAcceptanceTest.지하철역_등록되어_있음("샛강역").as(StationResponse.class);
+
+        신분당선 = LineAcceptanceTest.지하철_노선_등록되어_있음(
+                        new LineRequest("신분당선", "red", 강남역.getId(), 양재역.getId(), 10))
+                .as(LineResponse.class);
+        이호선 = LineAcceptanceTest.지하철_노선_등록되어_있음(
+                        new LineRequest("이호선", "green", 교대역.getId(), 강남역.getId(), 10))
+                .as(LineResponse.class);
+        삼호선 = LineAcceptanceTest.지하철_노선_등록되어_있음(
+                        new LineRequest("삼호선", "orange", 교대역.getId(), 양재역.getId(), 10))
+                .as(LineResponse.class);
+        구호선 = LineAcceptanceTest.지하철_노선_등록되어_있음(
+                        new LineRequest("구호선", "brown", 여의도역.getId(), 샛강역.getId(), 10))
+                .as(LineResponse.class);
+
+        LineSectionAcceptanceTest.지하철_노선에_지하철역_등록_요청(삼호선, 교대역, 남부터미널역, 3);
+    }
+}
--- src/test/java/nextstep/subway/path/PathAcceptanceTest.java
+++ src/test/java/nextstep/subway/path/PathAcceptanceTest.java
@@ -6,13 +6,8 @@ import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import nextstep.subway.AcceptanceTest;
-import nextstep.subway.line.acceptance.LineAcceptanceTest;
-import nextstep.subway.line.acceptance.LineSectionAcceptanceTest;
-import nextstep.subway.line.dto.LineRequest;
-import nextstep.subway.line.dto.LineResponse;
+import nextstep.subway.GivenSubway;
 import nextstep.subway.path.dto.PathResponse;
-import nextstep.subway.station.StationAcceptanceTest;
-import nextstep.subway.station.dto.StationResponse;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -21,49 +16,19 @@ import org.springframework.http.MediaType;
 
 @DisplayName("지하철 경로 조회")
 public class PathAcceptanceTest extends AcceptanceTest {
-    private LineResponse 신분당선;
-    private LineResponse 이호선;
-    private LineResponse 삼호선;
-    private LineResponse 구호선;
-    private StationResponse 강남역;
-    private StationResponse 양재역;
-    private StationResponse 교대역;
-    private StationResponse 남부터미널역;
-    private StationResponse 여의도역;
-    private StationResponse 샛강역;
+    private GivenSubway 지하철;
 
     @BeforeEach
     public void setUp() {
         super.setUp();
-
-        강남역 = StationAcceptanceTest.지하철역_등록되어_있음("강남역").as(StationResponse.class);
-        양재역 = StationAcceptanceTest.지하철역_등록되어_있음("양재역").as(StationResponse.class);
-        교대역 = StationAcceptanceTest.지하철역_등록되어_있음("교대역").as(StationResponse.class);
-        남부터미널역 = StationAcceptanceTest.지하철역_등록되어_있음("남부터미널역").as(StationResponse.class);
-        여의도역 = StationAcceptanceTest.지하철역_등록되어_있음("여의도역").as(StationResponse.class);
-        샛강역 = StationAcceptanceTest.지하철역_등록되어_있음("샛강역").as(StationResponse.class);
-
-        신분당선 = LineAcceptanceTest.지하철_노선_등록되어_있음(
-                        new LineRequest("신분당선", "red", 강남역.getId(), 양재역.getId(), 10))
-                .as(LineResponse.class);
-        이호선 = LineAcceptanceTest.지하철_노선_등록되어_있음(
-                        new LineRequest("이호선", "green", 교대역.getId(), 강남역.getId(), 10))
-                .as(LineResponse.class);
-        삼호선 = LineAcceptanceTest.지하철_노선_등록되어_있음(
-                        new LineRequest("삼호선", "orange", 교대역.getId(), 양재역.getId(), 10))
-                .as(LineResponse.class);
-        구호선 = LineAcceptanceTest.지하철_노선_등록되어_있음(
-                        new LineRequest("구호선", "brown", 여의도역.getId(), 샛강역.getId(), 10))
-                .as(LineResponse.class);
-
-        LineSectionAcceptanceTest.지하철_노선에_지하철역_등록_요청(삼호선, 교대역, 남부터미널역, 3);
+        지하철 = new GivenSubway();
     }
 
     @DisplayName("경로를 조회한다")
     @Test
     void getPath() {
         // when
-        final ExtractableResponse<Response> response = 경로_조회_요청(강남역.getId(), 남부터미널역.getId());
+        final ExtractableResponse<Response> response = 경로_조회_요청(지하철.강남역.getId(), 지하철.남부터미널역.getId());
 
         // then
         final PathResponse path = 경로가_응답됨(response);
@@ -76,7 +41,7 @@ public class PathAcceptanceTest extends AcceptanceTest {
     @Test
     void getPathWithSameStations() {
         // when
-        final ExtractableResponse<Response> response = 경로_조회_요청(강남역.getId(), 강남역.getId());
+        final ExtractableResponse<Response> response = 경로_조회_요청(지하철.강남역.getId(), 지하철.강남역.getId());
 
         // then
         경로_조회_실패됨(response);
@@ -86,7 +51,7 @@ public class PathAcceptanceTest extends AcceptanceTest {
     @Test
     void getNotExistedPath() {
         // when
-        final ExtractableResponse<Response> response = 경로_조회_요청(강남역.getId(), 여의도역.getId());
+        final ExtractableResponse<Response> response = 경로_조회_요청(지하철.강남역.getId(), 지하철.여의도역.getId());
 
         // then
         경로_조회_실패됨(response);
@@ -96,7 +61,7 @@ public class PathAcceptanceTest extends AcceptanceTest {
     @Test
     void getPathWithNotExistedStation() {
         // when
-        final ExtractableResponse<Response> response = 경로_조회_요청(강남역.getId(), 0L);
+        final ExtractableResponse<Response> response = 경로_조회_요청(지하철.강남역.getId(), 0L);
 
         // then
         경로_조회_실패됨(response);
