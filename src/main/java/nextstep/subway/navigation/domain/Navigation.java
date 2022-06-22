b71135e9b5eb44c14fcc6fa0a68297e1d3ff726d
14km <k.connor614@gmail.com>
2022-06-22 22:55:23 +0900

    feat: SectionEdge 추가

--- src/main/java/nextstep/subway/navigation/domain/Navigation.java
+++ src/main/java/nextstep/subway/navigation/domain/Navigation.java
@@ -1,11 +1,11 @@
 package nextstep.subway.navigation.domain;
 
+import nextstep.subway.fare.application.FareCalculatorService;
 import nextstep.subway.line.domain.Line;
 import nextstep.subway.navigation.dto.NavigationResponse;
 import nextstep.subway.station.domain.Station;
 import org.jgrapht.GraphPath;
 import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
-import org.jgrapht.graph.DefaultWeightedEdge;
 import org.jgrapht.graph.WeightedMultigraph;
 
 import java.util.List;
@@ -14,11 +14,28 @@ import static nextstep.subway.common.Messages.NOT_CONNECTED_SOURCE_TARGET_STATIO
 
 public class Navigation {
 
-    private final WeightedMultigraph<Station, DefaultWeightedEdge> graph = new WeightedMultigraph<>(DefaultWeightedEdge.class);
-    private GraphPath<Station, DefaultWeightedEdge> path;
+    private final WeightedMultigraph<Station, SectionEdge> graph = new WeightedMultigraph<>(SectionEdge.class);
+    private GraphPath<Station, SectionEdge> path;
 
     public static Navigation of(List<Line> persistLines) {
         return new Navigation(persistLines);
+    }
+
+    private Navigation(List<Line> persistLines) {
+        persistLines.forEach(this::settingGraph);
+    }
+
+    private void settingGraph(Line line) {
+        line.getSections().getSections().forEach(section -> {
+            Station upStation = section.getUpStation();
+            Station downStation = section.getDownStation();
+            graph.addVertex(upStation);
+            graph.addVertex(downStation);
+
+            SectionEdge sectionEdge = graph.addEdge(upStation, downStation);
+            sectionEdge.addSection(section);
+            graph.setEdgeWeight(sectionEdge, section.getDistance());
+        });
     }
 
     public NavigationResponse findShortest(Station sourceStation, Station targetStation) {
@@ -27,26 +44,22 @@ public class Navigation {
 
         List<Station> vertexList = path.getVertexList();
         int distance = (int) path.getWeight();
-        return NavigationResponse.of(vertexList, distance);
+        int fare = FareCalculatorService.calculate(distance) + maxLineExtraFare(path);
+
+        return NavigationResponse.of(vertexList, distance, fare);
     }
 
-    private Navigation(List<Line> persistLines) {
-        persistLines.forEach(this::settingGraph);
-    }
-    
-    private void settingGraph(Line line) {
-        line.getSections().getSections().forEach(section -> {
-            Station upStation = section.getUpStation();
-            Station downStation = section.getDownStation();
-            graph.addVertex(upStation);
-            graph.addVertex(downStation);
-            graph.setEdgeWeight(graph.addEdge(upStation, downStation), section.getDistance());
-        });
-    }
-
-    private void checkFindShortestPath(GraphPath<Station, DefaultWeightedEdge> path) {
+    private void checkFindShortestPath(GraphPath<Station, SectionEdge> path) {
         if (path == null) {
             throw new IllegalArgumentException(NOT_CONNECTED_SOURCE_TARGET_STATION);
         }
+    }
+
+    private int maxLineExtraFare(GraphPath<Station, SectionEdge> path) {
+        return path.getEdgeList().stream()
+                .map(SectionEdge::getLine)
+                .mapToInt(Line::getExtraFare)
+                .max()
+                .orElse(0);
     }
 }
