2fc8b99b2caf7c3738ca8ce58c8c7a93bf35afee
wenodev <logicalstark1105@gmail.com>
2022-06-23 15:19:47 +0900

    test: 로그인 인수테스트 작성한다

---
+++ src/test/java/nextstep/subway/auth/acceptance/AuthAcceptanceResponse.java
@@ -0,0 +1,13 @@
+package nextstep.subway.auth.acceptance;
+
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import org.springframework.http.HttpStatus;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class AuthAcceptanceResponse {
+    public static void 로그인_성공(ExtractableResponse<Response> response) {
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+    }
+}
--- src/test/java/nextstep/subway/auth/acceptance/AuthAcceptanceTest.java
+++ src/test/java/nextstep/subway/auth/acceptance/AuthAcceptanceTest.java
@@ -1,14 +1,34 @@
 package nextstep.subway.auth.acceptance;
 
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
 import nextstep.subway.AcceptanceTest;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static nextstep.subway.auth.acceptance.AuthAcceptanceRequest.로그인_요청;
+import static nextstep.subway.auth.acceptance.AuthAcceptanceResponse.로그인_성공;
+import static nextstep.subway.member.MemberAcceptanceTest.회원_생성을_요청;
+
 public class AuthAcceptanceTest extends AcceptanceTest {
 
-    @DisplayName("Bearer Auth")
+    private final String 등록된_이메일 = "valid@nextstep.com";
+    private final String 등록된_비밀번호 = "password";
+    private final int 나이 = 20;
+
+    @BeforeEach
+    public void setUp() {
+        super.setUp();
+        회원_생성을_요청(등록된_이메일, 등록된_비밀번호, 나이);
+    }
+
+    @DisplayName("계정정보가 유효하다면 로그인 성공한다")
     @Test
-    void myInfoWithBearerAuth() {
+    void loginWithValidAccountInfo() {
+        ExtractableResponse<Response> response = 로그인_요청(등록된_이메일, 등록된_비밀번호);
+
+        로그인_성공(response);
     }
 
     @DisplayName("Bearer Auth 로그인 실패")
