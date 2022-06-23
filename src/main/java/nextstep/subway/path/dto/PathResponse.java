31e0f4b8c2dd97fb17c40b576fd349f1527425fe
bestsilver <dkvkxm99@gmail.com>
2022-06-24 03:30:39 +0900

    feat: dto, controller, service 구현

--- src/main/java/nextstep/subway/path/dto/PathResponse.java
+++ src/main/java/nextstep/subway/path/dto/PathResponse.java
@@ -1,20 +1,28 @@
 package nextstep.subway.path.dto;
 
+import nextstep.subway.station.domain.Station;
 import nextstep.subway.station.dto.StationResponse;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class PathResponse {
-    private int distance;
+
     private List<StationResponse> stations;
+    private int distance;
 
     public PathResponse() {
 
     }
 
-    public PathResponse(int distance, List<StationResponse> stations) {
-        this.distance = distance;
+    public PathResponse(List<StationResponse> stations, int distance) {
         this.stations = stations;
+        this.distance = distance;
+    }
+
+    public static PathResponse of(List<Station> stationList, int distance) {
+        List<StationResponse> stationResponses = stationList.stream().map(StationResponse::of).collect(Collectors.toList());
+        return new PathResponse(stationResponses, distance);
     }
 
     public int getDistance() {
