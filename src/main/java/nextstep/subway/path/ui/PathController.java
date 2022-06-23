0bf1e6e9253de807ec3345287c65771604b45907
millennion-jin <Jadestern2@>
2022-06-23 17:44:47 +0900

    feat(PathService) : Path 관련 컨트롤러 구현

---
+++ src/main/java/nextstep/subway/path/ui/PathController.java
@@ -0,0 +1,31 @@
+package nextstep.subway.path.ui;
+
+import java.util.NoSuchElementException;
+import nextstep.subway.path.application.PathService;
+import nextstep.subway.path.dto.PathResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/paths")
+public class PathController {
+    private final PathService pathService;
+
+    public PathController(final PathService pathService) {
+        this.pathService = pathService;
+    }
+
+    @GetMapping
+    public ResponseEntity<PathResponse> findPath(@RequestParam Long source, @RequestParam Long target) {
+        return ResponseEntity.ok(pathService.findShortestPath(source, target));
+    }
+
+    @ExceptionHandler({IllegalArgumentException.class, NoSuchElementException.class})
+    public ResponseEntity handleIllegalArgsException() {
+        return ResponseEntity.badRequest().build();
+    }
+}
