89d1b2812a58ea107ce66dc50588ee38955a955d
yong2ss <korea92co@gmail.com>
2022-06-24 23:07:48 +0900

    test(PathServiceTest): 요금관련 추가 assert확인

--- src/test/java/nextstep/subway/path/application/PathServiceTest.java
+++ src/test/java/nextstep/subway/path/application/PathServiceTest.java
@@ -2,6 +2,8 @@ package nextstep.subway.path.application;
 
 import nextstep.subway.line.domain.Line;
 import nextstep.subway.line.domain.LineRepository;
+import nextstep.subway.line.domain.Section;
+import nextstep.subway.line.domain.SectionRepository;
 import nextstep.subway.path.dto.PathResponse;
 import nextstep.subway.path.exception.PathException;
 import nextstep.subway.station.application.StationService;
@@ -42,6 +44,9 @@ class PathServiceTest {
     @Mock
     StationRepository stationRepository;
 
+    @Mock
+    SectionRepository sectionRepository;
+
     /**
      *  청담역 -- 7호선(20) -- 뚝섬유원지역 --7호선(5)-- 건대입구역
      *                                                  |
@@ -53,7 +58,7 @@ class PathServiceTest {
     
     @BeforeEach
     void init() {
-        pathService = new PathService(new StationService(stationRepository), lineRepository);
+        pathService = new PathService(new StationService(stationRepository), lineRepository, sectionRepository);
 
         청담역 = new Station(1L, "청담역");
         뚝섬유원지역 = new Station(2L, "뚝섬유원지역");
@@ -61,10 +66,10 @@ class PathServiceTest {
         구의역 = new Station(4L, "구의역");
         신사역 = new Station(5L, "신사역");
 
-        칠호선 = new Line("칠호선", "yellow", 청담역, 뚝섬유원지역, 20);
+        칠호선 = new Line("칠호선", "yellow", 청담역, 뚝섬유원지역, 20, 200);
         칠호선.addLineStation(뚝섬유원지역, 건대입구역, 5);
         
-        이호선 = new Line("이호선", "green", 건대입구역, 구의역, 10);
+        이호선 = new Line("이호선", "green", 건대입구역, 구의역, 10, 0);
 
         when(lineRepository.findAll()).thenReturn(Arrays.asList(칠호선, 이호선));
     }
@@ -74,9 +79,12 @@ class PathServiceTest {
     void getShortestPath() {
         when(stationRepository.findById(1L)).thenReturn(Optional.of(청담역));
         when(stationRepository.findById(4L)).thenReturn(Optional.of(구의역));
+        when(sectionRepository.findAll()).thenReturn(Arrays.asList(new Section(칠호선, 청담역, 뚝섬유원지역, 20),
+                                                                    new Section(칠호선, 뚝섬유원지역, 건대입구역, 5),
+                                                                    new Section(이호선, 건대입구역, 구의역, 10)));
 
         //given
-        PathResponse response = pathService.getShortestPath(청담역.getId(), 구의역.getId());
+        PathResponse response = pathService.getShortestPath(청담역.getId(), 구의역.getId(), 15);
 
         //then
         assertAll(
@@ -85,7 +93,8 @@ class PathServiceTest {
                         StationResponse.of(청담역),
                         StationResponse.of(뚝섬유원지역),
                         StationResponse.of(건대입구역),
-                        StationResponse.of(구의역))
+                        StationResponse.of(구의역)),
+                () -> assertThat(response.getFare()).isEqualTo(1440)
                 );
     }
 
@@ -94,7 +103,7 @@ class PathServiceTest {
         when(stationRepository.findById(1L)).thenReturn(Optional.of(청담역));
 
         //then
-        assertThatThrownBy(() -> pathService.getShortestPath(청담역.getId(), 청담역.getId()))
+        assertThatThrownBy(() -> pathService.getShortestPath(청담역.getId(), 청담역.getId(), 10))
                 .isInstanceOf(PathException.class)
                 .hasMessageContaining(PathException.SAME_SOURCE_TARGET_STATION_MSG);
     }
@@ -105,7 +114,7 @@ class PathServiceTest {
         when(stationRepository.findById(5L)).thenReturn(Optional.of(신사역));
 
         //then
-        assertThatThrownBy(() -> pathService.getShortestPath(청담역.getId(), 신사역.getId()))
+        assertThatThrownBy(() -> pathService.getShortestPath(청담역.getId(), 신사역.getId(), 10))
                 .isInstanceOf(PathException.class)
                 .hasMessageContaining(PathException.NO_CONTAIN_STATION_MSG);
     }
