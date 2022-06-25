7cb91f3fe5bad43d2af219b9a80ee308e65ff3d5
kbh0581 <kbh0581@gmail.com>
2022-06-25 13:58:01 +0900

    test: 토큰 발급 인수 테스트 작성

--- src/test/java/nextstep/subway/auth/acceptance/AuthAcceptanceTest.java
+++ src/test/java/nextstep/subway/auth/acceptance/AuthAcceptanceTest.java
@@ -1,24 +1,78 @@
 package nextstep.subway.auth.acceptance;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
 import nextstep.subway.AcceptanceTest;
+import nextstep.subway.auth.dto.TokenRequest;
+import nextstep.subway.auth.dto.TokenResponse;
+import nextstep.subway.member.MemberAcceptanceTest;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 
 public class AuthAcceptanceTest extends AcceptanceTest {
+
+    //Given 회원 등록되어 있음
+    @BeforeEach
+    public void setUp() {
+        super.setUp();
+        회원이_등록되어_있음("email@email.com", "password", 20);
+
+    }
+
+    @DisplayName("로그인 기능")
+    @Test
+    void login() {
+        //given 회원이 등록되어 있음
+        TokenResponse 회원_토큰 = 토큰_요청(new TokenRequest("email@email.com", "password")).as(TokenResponse.class);
+
+    }
+
 
     @DisplayName("Bearer Auth")
     @Test
     void myInfoWithBearerAuth() {
+        //when
+        final ExtractableResponse<Response> response = 토큰_요청(new TokenRequest("email@email.com", "password"));
+
+        //then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+        assertThat(response.as(TokenResponse.class).getAccessToken()).isNotEmpty();
     }
 
     @DisplayName("Bearer Auth 로그인 실패")
     @Test
     void myInfoWithBadBearerAuth() {
+        final ExtractableResponse<Response> 잘못된_토큰_요청 = 토큰_요청(new TokenRequest("email@email.com", "1"));
+
+        assertThat(잘못된_토큰_요청.statusCode()).isEqualTo(HttpStatus.UNAUTHORIZED.value());
     }
 
     @DisplayName("Bearer Auth 유효하지 않은 토큰")
     @Test
     void myInfoWithWrongBearerAuth() {
     }
+
+
+
+    private ExtractableResponse<Response> 토큰_요청(TokenRequest tokenRequest) {
+        return RestAssured.given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(tokenRequest)
+                .when().post("/login/token")
+                .then().log().all()
+                .extract();
+    }
+
+
+    private ExtractableResponse<Response> 회원이_등록되어_있음(String email, String password, int age) {
+        return MemberAcceptanceTest.회원_생성을_요청(email, password, age);
+    }
+
 
 }
