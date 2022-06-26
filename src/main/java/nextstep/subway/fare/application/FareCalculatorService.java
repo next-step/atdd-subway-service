8be0fbc660a777170186691d58cfdf041adff1f3
14km <k.connor614@gmail.com>
2022-06-26 15:42:31 +0900

    refactor: 키로별 계산기내에 주석을 통하여 설명 추가

--- src/main/java/nextstep/subway/fare/application/FareCalculatorService.java
+++ src/main/java/nextstep/subway/fare/application/FareCalculatorService.java
@@ -5,8 +5,11 @@ public class FareCalculatorService {
     private static final int DEFAULT_FARE = 1_250;
     private static final int MAXIMUM_EXTRA_FARE_DISTANCE = 50;
 
-    private static final int FIVE_KILOMETRE = 5;
-    private static final int EIGHT_KILOMETRE = 8;
+    // 10~50km 이내 : 5km 까지 마다 100원 추가
+    private static final int EXTRA_FARE_FIVE_KILOMETRE = 5;
+
+    // 50km 초과 : 8km 까지 마다 100원 추가
+    private static final int EXTRA_FARE_EIGHT_KILOMETRE = 8;
 
     public static int calculate(int distance) {
         if (DEFAULT_MAXIMUM_DISTANCE >= distance) {
@@ -14,20 +17,19 @@ public class FareCalculatorService {
         }
 
         if (MAXIMUM_EXTRA_FARE_DISTANCE >= distance) {
-            return DEFAULT_FARE + calculateOverFareAsFiveKm(distance - DEFAULT_MAXIMUM_DISTANCE);
+            return DEFAULT_FARE + calculateExtraFareAsFiveKm(distance - DEFAULT_MAXIMUM_DISTANCE);
         }
 
-        int calculationFareAsFiveKm = calculateOverFareAsFiveKm(MAXIMUM_EXTRA_FARE_DISTANCE - DEFAULT_MAXIMUM_DISTANCE);
-        int maxCalculateFare = calculateOverFareAsEightKm(distance - MAXIMUM_EXTRA_FARE_DISTANCE) + calculationFareAsFiveKm;
+        int calculationFareAsFiveKm = calculateExtraFareAsFiveKm(MAXIMUM_EXTRA_FARE_DISTANCE - DEFAULT_MAXIMUM_DISTANCE);
+        int maxCalculateFare = calculateExtraFareAsEightKm(distance - MAXIMUM_EXTRA_FARE_DISTANCE) + calculationFareAsFiveKm;
         return DEFAULT_FARE + maxCalculateFare;
     }
 
-    private static int calculateOverFareAsFiveKm(int distance) {
-        return (int) ((Math.ceil((distance - 1) / FIVE_KILOMETRE) + 1) * 100);
+    private static int calculateExtraFareAsFiveKm(int distance) {
+        return (int) ((Math.ceil((distance - 1) / EXTRA_FARE_FIVE_KILOMETRE) + 1) * 100);
     }
 
-    private static int calculateOverFareAsEightKm(int distance) {
-        return (int) ((Math.ceil((distance - 1) / EIGHT_KILOMETRE) + 1) * 100);
+    private static int calculateExtraFareAsEightKm(int distance) {
+        return (int) ((Math.ceil((distance - 1) / EXTRA_FARE_EIGHT_KILOMETRE) + 1) * 100);
     }
-
 }
