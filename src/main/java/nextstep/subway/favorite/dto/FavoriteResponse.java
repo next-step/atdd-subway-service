cb3ce28cc36446a8459e51647496c574d8a1ae64
Philip <pch4623@gmail.com>
2022-06-26 12:08:44 +0900

    feat: DTO 리스트로 변환 메서드 추가

--- src/main/java/nextstep/subway/favorite/dto/FavoriteResponse.java
+++ src/main/java/nextstep/subway/favorite/dto/FavoriteResponse.java
@@ -3,6 +3,9 @@ package nextstep.subway.favorite.dto;
 import nextstep.subway.favorite.domain.Favorite;
 import nextstep.subway.station.dto.StationResponse;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class FavoriteResponse {
     private Long id;
     private StationResponse source;
@@ -18,6 +21,12 @@ public class FavoriteResponse {
         StationResponse sourceResponse = StationResponse.of(favorite.getSource());
         StationResponse targetResponse = StationResponse.of(favorite.getTarget());
         return new FavoriteResponse(favorite.getId(), sourceResponse, targetResponse);
+    }
+
+    public static List<FavoriteResponse> from(List<Favorite> favorites) {
+        return favorites.stream()
+                .map(FavoriteResponse::from)
+                .collect(Collectors.toList());
     }
 
     public Long getId() {
