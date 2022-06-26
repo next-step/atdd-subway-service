a06289279b667212aa6c9a2760c204f24d6859a6
vvsungho <vvsungho@gmail.com>
2022-06-26 17:51:02 +0900

    test: 최단 경로 조회 예외 테스트 케이스 추가

--- src/test/java/nextstep/subway/path/unit/PathTest.java
+++ src/test/java/nextstep/subway/path/unit/PathTest.java
@@ -1,11 +1,13 @@
 package nextstep.subway.path.unit;
 
 import nextstep.subway.UnitTest;
+import nextstep.subway.exception.BadRequestException;
 import nextstep.subway.line.domain.Distance;
 import nextstep.subway.line.domain.Line;
 import nextstep.subway.line.domain.Section;
 import nextstep.subway.path.domain.PathFinder;
 import nextstep.subway.path.dto.ShortestPathResponse;
+import nextstep.subway.path.dto.StationResponse;
 import nextstep.subway.station.domain.Station;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -15,8 +17,10 @@ import org.springframework.boot.web.server.LocalServerPort;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -31,6 +35,8 @@ public class PathTest extends UnitTest {
     private Station 양재역;
     private Station 교대역;
     private Station 남부터미널역;
+
+    private List<Line> allLines = Arrays.asList(신분당선, 이호선, 삼호선);
 
     @BeforeEach
     public void setUp() {
@@ -56,15 +62,33 @@ public class PathTest extends UnitTest {
     @Test
     @DisplayName("최단 경로 조회 단위 테스트")
     void 최단경로조회_테스트() {
-        // given
-        List<Line> allLines = Arrays.asList(신분당선, 이호선, 삼호선);
-
         // when
         ShortestPathResponse response = PathFinder.findShortestPath(allLines, 강남역, 남부터미널역);
+        List<String> answerPaths = Arrays.asList("강남역", "교대역", "남부터미널역");
 
         // then
         assertAll(
-                () -> assertThat(response.getDistance()).isEqualTo(13)
+                () -> assertThat(response.getDistance()).isEqualTo(13),
+                () -> assertThat(
+                        response.getStations()
+                                .getStations()
+                                .stream()
+                                .map(StationResponse::getName)
+                                .collect(Collectors.toList())
+                                .containsAll(answerPaths)
+                ).isTrue()
         );
+    }
+
+    /**
+     * 출발역, 도착역 동일할 시 최단 거리 조회 예외 테스트
+     */
+    @Test
+    @DisplayName("출발역, 도착역 동일 예외 테스트")
+    void 최단경로조회_출발역_도착역_동일_예외() {
+        assertThatThrownBy(
+                () -> PathFinder.findShortestPath(allLines, 강남역, 강남역)
+        ).isInstanceOf(BadRequestException.class)
+                .hasMessageContaining("[ERROR]");
     }
 }
