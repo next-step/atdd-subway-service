42b19c1f154f61cc79aa4f183bf88079443abed2
14km <k.connor614@gmail.com>
2022-06-26 15:38:56 +0900

    refactor: MAXIMUM_EXTRA_FARE_DISTANCE 명칭 수정

--- src/main/java/nextstep/subway/fare/application/FareCalculatorService.java
+++ src/main/java/nextstep/subway/fare/application/FareCalculatorService.java
@@ -3,7 +3,7 @@ package nextstep.subway.fare.application;
 public class FareCalculatorService {
     private static final int DEFAULT_MAXIMUM_DISTANCE = 10;
     private static final int DEFAULT_FARE = 1_250;
-    private static final int MAX_DISTANCE = 50;
+    private static final int MAXIMUM_EXTRA_FARE_DISTANCE = 50;
 
     private static final int FIVE_KILOMETRE = 5;
     private static final int EIGHT_KILOMETRE = 8;
@@ -13,12 +13,12 @@ public class FareCalculatorService {
             return DEFAULT_FARE;
         }
 
-        if (MAX_DISTANCE >= distance) {
+        if (MAXIMUM_EXTRA_FARE_DISTANCE >= distance) {
             return DEFAULT_FARE + calculateOverFareAsFiveKm(distance - DEFAULT_MAXIMUM_DISTANCE);
         }
 
-        int calculationFareAsFiveKm = calculateOverFareAsFiveKm(MAX_DISTANCE - DEFAULT_MAXIMUM_DISTANCE);
-        int maxCalculateFare = calculateOverFareAsEightKm(distance - MAX_DISTANCE) + calculationFareAsFiveKm;
+        int calculationFareAsFiveKm = calculateOverFareAsFiveKm(MAXIMUM_EXTRA_FARE_DISTANCE - DEFAULT_MAXIMUM_DISTANCE);
+        int maxCalculateFare = calculateOverFareAsEightKm(distance - MAXIMUM_EXTRA_FARE_DISTANCE) + calculationFareAsFiveKm;
         return DEFAULT_FARE + maxCalculateFare;
     }
 
