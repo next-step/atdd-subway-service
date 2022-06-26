e4a9f0f6ec264c847cfa9f302052bcbc670da3dd
yong2ss <korea92co@gmail.com>
2022-06-26 20:36:45 +0900

    refac(AgeFarePolicy.java): 나이별 할인 적용 Predicate Interface 적용

--- src/main/java/nextstep/subway/path/domain/AgeFarePolicy.java
+++ src/main/java/nextstep/subway/path/domain/AgeFarePolicy.java
@@ -1,33 +1,29 @@
 package nextstep.subway.path.domain;
 
+import java.util.Arrays;
 import java.util.function.Function;
+import java.util.function.Predicate;
 
 public enum AgeFarePolicy {
-    ALL(value -> value),
-    TEENAGER(value -> (int)(Math.ceil((value - 350) * 4 / 5.0))),
-    CHILDREN(value -> (int)(Math.ceil((value - 350) / 2.0))),
-    FREE(value -> 0);
+    ALL(value -> value, age -> age >= 19 && age < 65),
+    TEENAGER(value -> (int)(Math.ceil((value - 350) * 4 / 5.0)), age -> age >= 13 && age < 19),
+    CHILDREN(value -> (int)(Math.ceil((value - 350) / 2.0)), age -> age >= 6 && age < 13),
+    FREE(value -> 0, age -> age < 6 || age >= 65);
 
     private Function<Integer, Integer> operator;
+    private Predicate<Integer> agePredicate;
 
-    AgeFarePolicy(Function<Integer, Integer> operator) {
+    AgeFarePolicy(Function<Integer, Integer> operator, Predicate<Integer> agePredicate) {
         this.operator = operator;
+        this.agePredicate = agePredicate;
     }
 
     public static int calculate(int fare, int age) {
-        if(age < 6 || age >= 65) {
-            return FREE.operator.apply(fare);
-        }
-
-        if(age >= 6 && age < 13) {
-            return CHILDREN.operator.apply(fare);
-        }
-
-        if(age >= 13 && age < 19) {
-            return TEENAGER.operator.apply(fare);
-        }
-
-        return ALL.operator.apply(fare);
+        return Arrays.stream(AgeFarePolicy.values())
+                .filter(ageFarePolicy -> ageFarePolicy.agePredicate.test(age))
+                .findFirst()
+                .map(ageFarePolicy -> ageFarePolicy.operator.apply(fare))
+                .orElseThrow(IllegalArgumentException::new);
     }
 
     public Function<Integer, Integer> getOperator() {
