9a93fe0c62aa2dbd91ace44f495ac20e697a0ec8
Woody <yuncheol92@naver.com>
2022-06-26 23:27:37 +0900

    refactor : GlobalControllerAdvice AuthorizationException 핸들러 추가

--- src/main/java/nextstep/subway/common/ui/GlobalControllerAdvice.java
+++ src/main/java/nextstep/subway/common/ui/GlobalControllerAdvice.java
@@ -1,5 +1,6 @@
 package nextstep.subway.common.ui;
 
+import nextstep.subway.auth.application.AuthorizationException;
 import nextstep.subway.common.dto.ErrorResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -16,7 +17,6 @@ public class GlobalControllerAdvice {
 
     @ExceptionHandler(DataIntegrityViolationException.class)
     public ResponseEntity<ErrorResponse> handleDataIntegrityViolationException(DataIntegrityViolationException exception) {
-
         LOGGER.info(exception.getMessage());
         ErrorResponse errorResponse = ErrorResponse.of("데이터 정합성에 문제가 발생했습니다.", HttpStatus.INTERNAL_SERVER_ERROR.value());
 
@@ -25,17 +25,19 @@ public class GlobalControllerAdvice {
 
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<ErrorResponse> handleIllegalArgsException(IllegalArgumentException exception) {
-
         ErrorResponse errorResponse = ErrorResponse.of(exception.getMessage(), HttpStatus.BAD_REQUEST.value());
-
         return ResponseEntity.badRequest().body(errorResponse);
     }
 
     @ExceptionHandler(IllegalStateException.class)
     public ResponseEntity<ErrorResponse> handleIllegalStateException(IllegalStateException exception) {
-
         ErrorResponse errorResponse = ErrorResponse.of(exception.getMessage(), HttpStatus.BAD_REQUEST.value());
-
         return ResponseEntity.badRequest().body(errorResponse);
+    }
+
+    @ExceptionHandler(AuthorizationException.class)
+    public ResponseEntity<ErrorResponse> handleAuthorizationException(AuthorizationException exception) {
+        ErrorResponse errorResponse = ErrorResponse.of(exception.getMessage(), HttpStatus.UNAUTHORIZED.value());
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(errorResponse);
     }
 }
