755500c7135ea607b002730e551d2e2ccd60f91c
seungin yang <rhfpdk92@naver.com>
2022-06-26 21:07:47 +0900

    test: 로그인사용자별 요금할인 테스트 추가

--- src/test/java/nextstep/subway/path/PathAcceptanceFactory.java
+++ src/test/java/nextstep/subway/path/PathAcceptanceFactory.java
@@ -29,4 +29,19 @@ public class PathAcceptanceFactory {
     public static void 최단_경로_조회_조회_실패(ExtractableResponse<Response> response) {
         assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
     }
+
+    public static ExtractableResponse<Response> 회원의_최단경로_조회_요청(Long sourceId, Long targetId, String accessToken) {
+        return RestAssured
+                .given().log().all()
+                .auth().oauth2(accessToken)
+                .accept(MediaType.APPLICATION_JSON_VALUE)
+                .when().get("/paths?source={sourceId}&target={targetId}", sourceId, targetId)
+                .then().log().all()
+                .extract();
+    }
+
+    public static void 이용요금_조회(ExtractableResponse<Response> response, int fare) {
+        PathResponse 거리응답결과 = response.as(PathResponse.class);
+        assertThat(거리응답결과.getFare()).isEqualTo(fare);
+    }
 }
