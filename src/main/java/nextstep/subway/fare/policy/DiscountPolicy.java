804ecb3c01c4fbd3a784ed3dc4c85aab75ab8a6a
su-hub9 <su9581@gmail.com>
2022-06-28 00:07:50 +0900

    refactor(policy): interface 클래스 내 default 메소드 제거

--- src/main/java/nextstep/subway/fare/policy/DiscountPolicy.java
+++ src/main/java/nextstep/subway/fare/policy/DiscountPolicy.java
@@ -2,8 +2,4 @@ package nextstep.subway.fare.policy;
 
 public interface DiscountPolicy {
     int discount(int fare);
-
-    default int atLeastBasicFare(int calculateFare, int basicFare) {
-        return Math.max(calculateFare, basicFare);
-    }
 }
