805e3b91af2d4f6905165730dde8dbbbc4a66e63
las139 <qqwer153@gmail.com>
2022-06-28 04:13:05 +0900

    refactor: DistanceFarePolicy 하드코딩 상수로 변경

--- src/main/java/nextstep/subway/path/domain/DistanceFarePolicy.java
+++ src/main/java/nextstep/subway/path/domain/DistanceFarePolicy.java
@@ -9,6 +9,8 @@ public class DistanceFarePolicy {
     private static final Fare EXTRA_FARE = Fare.of(100);
     private static final Distance BASIC_DISTANCE = Distance.of(10);
     private static final Distance EXTRA_FARE_DISTANCE = Distance.of(50);
+    private static final Distance BASIC_DISTANCE_PER_FARE = Distance.of(5);
+    private static final Distance EXTRA_FARE_DISTANCE_PER_FARE = Distance.of(8);
 
     public Fare calculate(Distance distance) {
         if (!distance.isLonger(BASIC_DISTANCE)) {
@@ -22,12 +24,12 @@ public class DistanceFarePolicy {
 
     private Fare getExtraFareEvery5KM(Distance distance) {
         Distance extraDistance = distance.minus(BASIC_DISTANCE);
-        return Fare.of((int) ((Math.ceil((extraDistance.getDistance() - 1) / 5) + 1) * EXTRA_FARE.getFare()));
+        return Fare.of((int) ((Math.ceil((extraDistance.getDistance() - 1) / BASIC_DISTANCE_PER_FARE.getDistance()) + 1) * EXTRA_FARE.getFare()));
     }
 
     private Fare getExtraFareEvery8KM(Distance distance) {
         Fare extraFare5KM = getExtraFareEvery5KM(EXTRA_FARE_DISTANCE);
         Distance extraDistance = distance.minus(EXTRA_FARE_DISTANCE);
-        return Fare.of(extraFare5KM.getFare() + (int) ((Math.ceil((extraDistance.getDistance() - 1) / 8) + 1) * EXTRA_FARE.getFare()));
+        return Fare.of(extraFare5KM.getFare() + (int) ((Math.ceil((extraDistance.getDistance() - 1) / EXTRA_FARE_DISTANCE_PER_FARE.getDistance()) + 1) * EXTRA_FARE.getFare()));
     }
 }
