42c8658be4abc3869d8436ba2aa49b981283d049
millennion-jin <Jadestern2@>
2022-06-28 01:22:18 +0900

    test(FavoriteTest) : Favorite 객체 관련 테스트 작성

---
+++ src/test/java/nextstep/subway/favorite/FavoriteTest.java
@@ -0,0 +1,44 @@
+package nextstep.subway.favorite;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import nextstep.subway.favorite.domain.Favorite;
+import nextstep.subway.member.domain.Member;
+import nextstep.subway.station.domain.Station;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class FavoriteTest {
+
+    private static final Station 강남역 = new Station("강남역");
+    private static final Station 광교중앙역 = new Station("광교중앙역");
+    private static final Member 작성자 = new Member("aabbd@gmail.com", "1234", 33);
+
+    @DisplayName("즐겨찾기 객체 정상 생성")
+    @Test
+    void constructor() {
+        Favorite favorite = new Favorite(강남역, 광교중앙역, 작성자);
+        assertThat(favorite).isNotNull();
+    }
+
+    @DisplayName("즐겨찾기 객체 생성시 null 매개변수 입력")
+    @Test
+    void constructorWithNull() {
+        assertThatThrownBy(() -> new Favorite(강남역, null, 작성자)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("즐겨찾기 객체 생성시 출발역과 도착역 동일")
+    @Test
+    void constructorSameStation() {
+        assertThatThrownBy(() -> new Favorite(강남역, 강남역, 작성자)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void isSameRoute() {
+        Favorite favorite = new Favorite(강남역, 광교중앙역, 작성자);
+        Favorite reverse = new Favorite(광교중앙역, 강남역, 작성자);
+        assertThat(favorite.isSameRoute(favorite)).isTrue();
+        assertThat(favorite.isSameRoute(reverse)).isFalse();
+    }
+}
