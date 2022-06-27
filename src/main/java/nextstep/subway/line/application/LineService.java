c9b13aa1c48c395a972f392707f00d18109745de
choi-ys <project.log.062@gmail.com>
2022-06-27 22:55:37 +0900

    refactor: 신규 노선 엔티티 및 노선 생성 응답 객체 생성 부를 요청/응답 객체로 위임

--- src/main/java/nextstep/subway/line/application/LineService.java
+++ src/main/java/nextstep/subway/line/application/LineService.java
@@ -35,12 +35,8 @@ public class LineService {
     public LineResponse saveLine(LineRequest request) {
         Station upStation = stationService.findById(request.getUpStationId());
         Station downStation = stationService.findById(request.getDownStationId());
-        Line persistLine = lineRepository
-            .save(new Line(request.getName(), request.getColor(), upStation, downStation, request.getDistance()));
-        List<StationResponse> stations = getStations(persistLine).stream()
-            .map(it -> StationResponse.of(it))
-            .collect(Collectors.toList());
-        return LineResponse.of(persistLine, stations);
+        Line persistLine = lineRepository.save(request.toEntity(upStation, downStation));
+        return LineResponse.of(persistLine);
     }
 
     /**
