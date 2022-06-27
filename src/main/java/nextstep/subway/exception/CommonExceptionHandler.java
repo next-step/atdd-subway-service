6d2d98ac70deb8c2aaced754cb1829a525c7defd
las139 <qqwer153@gmail.com>
2022-06-28 04:08:13 +0900

    feat: CommonExceptionHandler Exception 추가

--- src/main/java/nextstep/subway/exception/CommonExceptionHandler.java
+++ src/main/java/nextstep/subway/exception/CommonExceptionHandler.java
@@ -17,12 +17,24 @@ public class CommonExceptionHandler {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(NoSearchStationException.class)
     public ResponseEntity handleNoSearchStationException(NoSearchStationException ex) {
-        return ResponseEntity.notFound().build();
+        return ResponseEntity.badRequest().body(ex.getMessage());
     }
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(NoSearchLineException.class)
     public ResponseEntity handleNoSearchLineException(NoSearchLineException ex) {
-        return ResponseEntity.notFound().build();
+        return ResponseEntity.badRequest().body(ex.getMessage());
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(NoSearchFavoriteException.class)
+    public ResponseEntity handleNoSearchFavoriteException(NoSearchFavoriteException ex) {
+        return ResponseEntity.badRequest().body(ex.getMessage());
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(NoSearchMemberException.class)
+    public ResponseEntity handleNoSearchMemberException(NoSearchMemberException ex) {
+        return ResponseEntity.badRequest().body(ex.getMessage());
     }
 }
