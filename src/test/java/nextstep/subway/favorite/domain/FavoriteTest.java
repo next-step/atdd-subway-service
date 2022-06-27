53d69c7d8b009dfbd08b863ac65686782d97e316
Philip <pch4623@gmail.com>
2022-06-27 23:43:00 +0900

    refactor: Favorite 도메인 변경에 따른 테스트 코드 수정

--- src/test/java/nextstep/subway/favorite/domain/FavoriteTest.java
+++ src/test/java/nextstep/subway/favorite/domain/FavoriteTest.java
@@ -1,6 +1,5 @@
 package nextstep.subway.favorite.domain;
 
-import nextstep.subway.member.domain.Member;
 import nextstep.subway.station.domain.Station;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -11,22 +10,22 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 public class FavoriteTest {
+    private Long 사용자1_ID;
     private Station 강남역;
     private Station 정자역;
-    private Member 콜펌;
 
     @BeforeEach
     void setUp() {
         강남역 = Station.of(1L, "강남역");
         정자역 = Station.of(2L, "정자역");
 
-        콜펌 = new Member("test@gamil.com", "password", 30);
+        사용자1_ID = 1L;
     }
 
     @Test
     @DisplayName("출발역과 도착역이 같으면 즐겨찾기 추가에 실패한다.")
     void validateStations() {
-        assertThatThrownBy(() -> Favorite.of(콜펌, 정자역, 정자역))
+        assertThatThrownBy(() -> Favorite.of(사용자1_ID, 정자역, 정자역))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("같은 역을 즐겨찾기에 추가할 수 없습니다.");
     }
@@ -34,10 +33,10 @@ public class FavoriteTest {
     @Test
     @DisplayName("즐겨찾기를 등록했던 사용자가 아니면 등록에 실패한다.")
     void validateMember() {
-        Favorite 즐겨찾기 = Favorite.of(콜펌, 강남역, 정자역);
-        Member 필필 = new Member("test2@gamil.com", "password2", 30);
+        Favorite 즐겨찾기 = Favorite.of(사용자1_ID, 강남역, 정자역);
+        Long 사용자2_ID = 2L;
 
-        assertThatThrownBy(() -> 즐겨찾기.validateMember(필필))
+        assertThatThrownBy(() -> 즐겨찾기.validateMember(사용자2_ID))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("즐겨찾기를 등록한 사용자가 아닙니다.");
     }
@@ -45,12 +44,12 @@ public class FavoriteTest {
     @Test
     @DisplayName("즐겨찾기를 생성한다.")
     void createFavorite() {
-        Favorite 즐겨찾기 = Favorite.of(콜펌, 강남역, 정자역);
+        Favorite 즐겨찾기 = Favorite.of(사용자1_ID, 강남역, 정자역);
 
         assertAll(
             () ->  assertThat(즐겨찾기.getSource().getName()).isEqualTo("강남역"),
             () ->  assertThat(즐겨찾기.getTarget().getName()).isEqualTo("정자역"),
-            () ->  assertThat(즐겨찾기.getMember()).isEqualTo(콜펌)
+            () ->  assertThat(즐겨찾기.getMemberId()).isEqualTo(사용자1_ID)
         );
     }
 }
