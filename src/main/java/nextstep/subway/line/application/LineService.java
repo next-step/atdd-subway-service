0b02964e2466854272c51be094c4bba6425c4b40
choi-ys <project.log.062@gmail.com>
2022-06-27 23:02:44 +0900

    refactor: 노선 관련 기능을 관련 도메인 객체로 분산함에 따라 더 이상 참조가 없는 코드 제거

--- src/main/java/nextstep/subway/line/application/LineService.java
+++ src/main/java/nextstep/subway/line/application/LineService.java
@@ -1,9 +1,6 @@
 package nextstep.subway.line.application;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 import nextstep.subway.line.domain.Line;
 import nextstep.subway.line.domain.LineRepository;
@@ -13,12 +10,11 @@ import nextstep.subway.line.dto.LineResponse;
 import nextstep.subway.line.dto.SectionRequest;
 import nextstep.subway.station.application.StationService;
 import nextstep.subway.station.domain.Station;
-import nextstep.subway.station.dto.StationResponse;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
-@Transactional
+@Transactional(readOnly = true)
 public class LineService {
 
     private LineRepository lineRepository;
@@ -29,6 +25,7 @@ public class LineService {
         this.stationService = stationService;
     }
 
+    @Transactional
     public LineResponse saveLine(LineRequest request) {
         Station upStation = stationService.findById(request.getUpStationId());
         Station downStation = stationService.findById(request.getDownStationId());
@@ -52,18 +49,18 @@ public class LineService {
         return LineResponse.of(persistLine);
     }
 
+    @Transactional
     public void updateLine(Long id, LineRequest lineUpdateRequest) {
         Line persistLine = lineRepository.findById(id).orElseThrow(RuntimeException::new);
         persistLine.update(lineUpdateRequest.toLine());
     }
 
-    /**
-     * 특정 노선 삭제
-     */
+    @Transactional
     public void deleteLineById(Long id) {
         lineRepository.deleteById(id);
     }
 
+    @Transactional
     public void addLineStation(Long lineId, SectionRequest request) {
         Line line = findLineById(lineId);
         Station upStation = stationService.findStationById(request.getUpStationId());
@@ -73,59 +70,10 @@ public class LineService {
         line.addSection(targetSection);
     }
 
-    public void renewal_removeLineStation(Long lineId, Long stationId) {
+    @Transactional
+    public void removeLineStation(Long lineId, Long stationId) {
         Line line = findLineById(lineId);
         Station station = stationService.findStationById(stationId);
         line.removeSection(station);
-    }
-
-    /**
-     * 노선의 정렬된 역 목록을 조회한다.
-     * <p>
-     * - 상행종점역 구간을 기준으로 기준 구간의 하행역이 다른 구간의 상행역이 아닐때까지 전체 구간을 순회하며 정렬된 역 목록을 구성한다.
-     */
-    private List<Station> getStations(Line line) {
-        if (line.getSections().isEmpty()) {
-            return Arrays.asList();
-        }
-
-        List<Station> stations = new ArrayList<>();
-        Station downStation = findUpStation(line);
-        stations.add(downStation);
-
-        while (downStation != null) {
-            Station finalDownStation = downStation;
-            Optional<Section> nextLineStation = line.getSections().stream()
-                .filter(it -> it.getUpStation() == finalDownStation)
-                .findFirst();
-            if (!nextLineStation.isPresent()) {
-                break;
-            }
-            downStation = nextLineStation.get().getDownStation();
-            stations.add(downStation);
-        }
-
-        return stations;
-    }
-
-    /**
-     * 노선의 상행종점역을 조회한다.
-     * <p>
-     * - 전체 구간을 순회하며 상행역이 다른 구간의 하행역이 아닌 구간의 상행역을 반환한다.
-     */
-    private Station findUpStation(Line line) {
-        Station downStation = line.getSections().get(0).getUpStation(); // 양재
-        while (downStation != null) {
-            Station finalDownStation = downStation;
-            Optional<Section> nextLineStation = line.getSections().stream()
-                .filter(it -> it.getDownStation() == finalDownStation)
-                .findFirst();
-            if (!nextLineStation.isPresent()) {
-                break;
-            }
-            downStation = nextLineStation.get().getUpStation(); // 강남 -> 신논현 -> 논현
-        }
-
-        return downStation; // 논현
     }
 }
--- src/main/java/nextstep/subway/line/ui/LineController.java
+++ src/main/java/nextstep/subway/line/ui/LineController.java
@@ -56,7 +56,7 @@ public class LineController {
 
     @DeleteMapping("/{lineId}/sections")
     public ResponseEntity removeLineStation(@PathVariable Long lineId, @RequestParam Long stationId) {
-        lineService.renewal_removeLineStation(lineId, stationId);
+        lineService.removeLineStation(lineId, stationId);
         return ResponseEntity.ok().build();
     }
 
