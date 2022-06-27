b3f1a6713c2adc7e487e3ced139554b7683aaa88
Woody <yuncheol92@naver.com>
2022-06-28 02:35:37 +0900

    refactor : Favorite, Station OneToOne 관계설정 불필요한 로직 제거

--- src/main/java/nextstep/subway/favorite/domain/FavoriteRepository.java
+++ src/main/java/nextstep/subway/favorite/domain/FavoriteRepository.java
@@ -1,9 +1,11 @@
 package nextstep.subway.favorite.domain;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface FavoriteRepository extends JpaRepository<Favorite, Long> {
+    @EntityGraph(attributePaths = {"sourceStation", "targetStation"}, type = EntityGraph.EntityGraphType.LOAD)
     List<Favorite> findByMemberId(Long memberId);
 }
