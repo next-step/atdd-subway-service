ade6e57ff10e1d92617a1a0d8dbb692a0e8a3efd
wenodev <logicalstark1105@gmail.com>
2022-06-29 00:57:27 +0900

    style: 공백을 추가한다

--- src/main/java/nextstep/subway/common/exception/BadRequestException.java
+++ src/main/java/nextstep/subway/common/exception/BadRequestException.java
@@ -3,7 +3,7 @@ package nextstep.subway.common.exception;
 import nextstep.subway.common.ErrorMessage;
 import org.springframework.http.HttpStatus;
 
-public class BadRequestException extends SubwayException{
+public class BadRequestException extends SubwayException {
     public BadRequestException(ErrorMessage message) {
         super(HttpStatus.BAD_REQUEST, message);
     }
