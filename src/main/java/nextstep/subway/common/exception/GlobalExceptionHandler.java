9ecf855e37f49b58f51a9798d8c5ca83e3ec65b3
wenodev <logicalstark1105@gmail.com>
2022-06-29 01:19:11 +0900

    fix: Exception.class에 대한 error처리를 제거한다

--- src/main/java/nextstep/subway/common/exception/GlobalExceptionHandler.java
+++ src/main/java/nextstep/subway/common/exception/GlobalExceptionHandler.java
@@ -1,7 +1,6 @@
 package nextstep.subway.common.exception;
 
 import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -16,10 +15,5 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(DataIntegrityViolationException.class)
     public ResponseEntity handleIllegalArgsException(DataIntegrityViolationException e) {
         return ResponseEntity.badRequest().build();
-    }
-
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity exception(Exception e) {
-        return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
