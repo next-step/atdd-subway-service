34261d70522f502b1f8283d5089b4653b3e02ff3
wenodev <logicalstark1105@gmail.com>
2022-06-29 00:54:51 +0900

    test: map 대신 TokenRequest를 사용한다

--- src/test/java/nextstep/subway/auth/acceptance/AuthAcceptanceRequest.java
+++ src/test/java/nextstep/subway/auth/acceptance/AuthAcceptanceRequest.java
@@ -3,20 +3,16 @@ package nextstep.subway.auth.acceptance;
 import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
+import nextstep.subway.auth.dto.TokenRequest;
 import org.springframework.http.MediaType;
-
-import java.util.HashMap;
-import java.util.Map;
 
 public class AuthAcceptanceRequest {
     public static ExtractableResponse<Response> 로그인_요청(String email, String password) {
-        Map<String, String> params = new HashMap<>();
-        params.put("email", email);
-        params.put("password", password);
+        TokenRequest request = new TokenRequest(email, password);
 
         return RestAssured.given().log().all()
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .body(params)
+                .body(request)
                 .when().post("/login/token")
                 .then().log().all()
                 .extract();
