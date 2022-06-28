9cc713d5081b4dee7d12bfc1dbcdccb9d90a346a
choi-ys <project.log.062@gmail.com>
2022-06-28 20:30:29 +0900

    feat: 최단 경로 조회 기능을 제공하는 도메인 구현
    기능 구현
    - 주어진 노선에 포함된 역과 구간을 이용하여 그래프 구성
    - 구성된 그래프 내에서 출발지로부터 도착지까지의 최단 거리 경로 조회
    
    예외 처리
    - 출발지와 도착지가 같은 경우 에외 처리
    - 출발지 혹은 도착지가 전체 노선에 포함된 역이 아닌 경우 예외 처리
    - 출발지와 도착지 사이에 간선이 없는 경우 예외 처리

---
+++ src/main/java/nextstep/subway/line/domain/PathFinder.java
@@ -0,0 +1,85 @@
+package nextstep.subway.line.domain;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+import nextstep.subway.line.dto.ShortestPathResponse;
+import nextstep.subway.station.domain.Station;
+import org.jgrapht.GraphPath;
+import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
+import org.jgrapht.graph.DefaultWeightedEdge;
+import org.jgrapht.graph.WeightedMultigraph;
+
+public class PathFinder {
+    public static final String SOURCE_AND_TARGET_IS_EQUAL_ERROR = "출발지와 도착지는 같을 수 없습니다.";
+    public static final String SOURCE_OR_TARGET_IS_NOT_CONTAINS_ALL_LINE_STATION_ERROR = "대상 노선에서 해당역을 찾을 수 없습니다.";
+    static WeightedMultigraph<Station, DefaultWeightedEdge> graph;
+    static {
+        graph = new WeightedMultigraph(DefaultWeightedEdge.class);
+    }
+
+    public static ShortestPathResponse findShortestPath(List<Line> allLines, Station source, Station target) {
+        validate(allLines, source, target);
+        addSectionsToGraph(allLines);
+
+        DijkstraShortestPath dijkstraShortestPath = new DijkstraShortestPath(graph);
+        GraphPath path = dijkstraShortestPath.getPath(source, target);
+        checkResultIsNull(path);
+        return ShortestPathResponse.of(path);
+    }
+
+    private static void validate(List<Line> allLines, Station source, Station target){
+        validateStationEquals(source, target);
+        validateStationContains(allLines, source, target);
+    }
+
+    private static void validateStationEquals(Station source, Station target) {
+        if(source.equals(target)){
+            throw new IllegalArgumentException(SOURCE_AND_TARGET_IS_EQUAL_ERROR);
+        }
+    }
+
+    private static void validateStationContains(List<Line> allLines, Station source, Station target){
+        List<Station> stations = mergeAllLinesStations(allLines);
+        if(!stations.contains(source) || !stations.contains(target)){
+            throw new IllegalArgumentException(SOURCE_OR_TARGET_IS_NOT_CONTAINS_ALL_LINE_STATION_ERROR);
+        }
+    }
+
+    private static void addSectionsToGraph(List<Line> allLines) {
+        addVertexByStation(mergeAllLinesStations(allLines));
+        setEdgeWeightBySection(mergeAllLinesSections(allLines));
+    }
+
+    private static void addVertexByStation(List<Station> allLinesStations) {
+        allLinesStations.forEach(it -> graph.addVertex(it));
+    }
+
+    private static void setEdgeWeightBySection(List<Section> allLinesSections) {
+        allLinesSections.forEach(it ->
+            graph.setEdgeWeight(
+                graph.addEdge(it.getUpStation(), it.getDownStation()),
+                it.getDistance()
+            ));
+    }
+
+    private static List<Station> mergeAllLinesStations(List<Line> allLines) {
+        return allLines.stream()
+            .map(it -> it.getAllStations())
+            .flatMap(Collection::stream)
+            .collect(Collectors.toList());
+    }
+
+    private static List<Section> mergeAllLinesSections(List<Line> allLines) {
+        return allLines.stream()
+            .map(it -> it.getSections())
+            .flatMap(Collection::stream)
+            .collect(Collectors.toList());
+    }
+
+    private static void checkResultIsNull(GraphPath path) {
+        if(path == null){
+            throw new IllegalArgumentException("출발지와 도착지가 연결 되어있는지 확인하세요.");
+        }
+    }
+}
---
+++ src/main/java/nextstep/subway/line/dto/ShortestPathResponse.java
@@ -0,0 +1,31 @@
+package nextstep.subway.line.dto;
+
+import java.util.List;
+import nextstep.subway.station.domain.Station;
+import org.jgrapht.GraphPath;
+
+public class ShortestPathResponse {
+    private int distance;
+    private List<Station> stations;
+
+    public ShortestPathResponse() {
+
+    }
+
+    public ShortestPathResponse(int distance, List<Station> stations) {
+        this.distance = distance;
+        this.stations = stations;
+    }
+
+    public static ShortestPathResponse of(GraphPath path) {
+        return new ShortestPathResponse((int) path.getWeight(), path.getVertexList());
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+
+    public List<Station> getStations() {
+        return stations;
+    }
+}
