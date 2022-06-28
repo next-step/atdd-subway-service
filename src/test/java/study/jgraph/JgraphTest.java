1b32193dca9f8f0f7cbc71f8247089e8a071470e
choi-ys <project.log.062@gmail.com>
2022-06-28 16:49:32 +0900

    test: jgrapht에 대한 학습 테스트 작성

--- src/test/java/study/jgraph/JgraphTest.java
+++ src/test/java/study/jgraph/JgraphTest.java
@@ -1,5 +1,8 @@
 package study.jgraph;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
 import org.jgrapht.GraphPath;
 import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
 import org.jgrapht.alg.shortestpath.KShortestPaths;
@@ -7,11 +10,18 @@ import org.jgrapht.graph.DefaultWeightedEdge;
 import org.jgrapht.graph.WeightedMultigraph;
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
-
-import static org.assertj.core.api.Assertions.assertThat;
-
 public class JgraphTest {
+
+    /**
+     * 'v1' - (100) - `v3`
+     *  |             |
+     *   (2)       (2)
+     *     |      |
+     *        v2
+     *
+     * path : v3 -> v1 = 102
+     * path : v3 -> v2 -> v1 = 4
+     */
     @Test
     public void getDijkstraShortestPath() {
         String source = "v3";
@@ -27,29 +37,41 @@ public class JgraphTest {
         DijkstraShortestPath dijkstraShortestPath = new DijkstraShortestPath(graph);
         List<String> shortestPath = dijkstraShortestPath.getPath(source, target).getVertexList();
 
-        assertThat(shortestPath.size()).isEqualTo(3);
+        assertThat(shortestPath)
+            .hasSize(3)
+            .containsExactly("v3", "v2", "v1");
     }
 
+    /**
+     * `v1` - (2) -  v2
+     * |
+     * (100)
+     * |
+     * v3 - (10) - 'v4'
+     *
+     * path : v1 -> v3, v3 -> v4 = 110
+     */
     @Test
     public void getKShortestPaths() {
-        String source = "v3";
-        String target = "v1";
+        String source = "v1";
+        String target = "v4";
 
         WeightedMultigraph<String, DefaultWeightedEdge> graph = new WeightedMultigraph(DefaultWeightedEdge.class);
         graph.addVertex("v1");
         graph.addVertex("v2");
         graph.addVertex("v3");
+        graph.addVertex("v4");
         graph.setEdgeWeight(graph.addEdge("v1", "v2"), 2);
-        graph.setEdgeWeight(graph.addEdge("v2", "v3"), 2);
         graph.setEdgeWeight(graph.addEdge("v1", "v3"), 100);
+        graph.setEdgeWeight(graph.addEdge("v3", "v4"), 10);
 
         List<GraphPath> paths = new KShortestPaths(graph, 100).getPaths(source, target);
-
-        assertThat(paths).hasSize(2);
-        paths.stream()
-                .forEach(it -> {
-                    assertThat(it.getVertexList()).startsWith(source);
-                    assertThat(it.getVertexList()).endsWith(target);
-                });
+        System.out.println(paths);
+        assertThat(paths)
+            .hasSize(2)
+            .allSatisfy(it -> {
+                assertThat(it.getVertexList()).startsWith(source);
+                assertThat(it.getVertexList()).endsWith(target);
+            });
     }
 }
