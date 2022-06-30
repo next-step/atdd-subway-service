02eccde7701cf8e6afff75eb16fcb803f9ffaef8
DongGyuJang <cdgu1129@knou.ac.kr>
2022-06-30 23:19:54 +0900

    feat: 즐겨찾기 조회 및 삭제 추가

--- src/main/java/nextstep/subway/favorite/application/FavoriteService.java
+++ src/main/java/nextstep/subway/favorite/application/FavoriteService.java
@@ -11,8 +11,13 @@ import nextstep.subway.station.application.StationService;
 import nextstep.subway.station.domain.Station;
 import nextstep.subway.station.domain.StationRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
+@Transactional
 public class FavoriteService {
 
     private final FavoriteRepository favoriteRepository;
@@ -28,6 +33,7 @@ public class FavoriteService {
         this.memberService = memberService;
     }
 
+    @Transactional
     public FavoriteResponse saveFavorite(long memberId, FavoriteRequest favoriteRequest) {
         Member member = memberService.findMemberById(memberId);
         Station source = stationService.findStationById(favoriteRequest.getSource());
@@ -36,5 +42,28 @@ public class FavoriteService {
         final Favorite save = favoriteRepository.save(new Favorite(member, source, target));
 
         return FavoriteResponse.of(save);
+    }
+
+    @Transactional(readOnly = true)
+    public FavoriteResponse findFavorite(long favoriteId) {
+        return favoriteRepository.findById(favoriteId)
+                .map(FavoriteResponse::of)
+                .orElseThrow(() -> new IllegalArgumentException("즐겨찾기가 존재하지 않습니다."));
+    }
+
+    @Transactional(readOnly = true)
+    public List<FavoriteResponse> findFavorites(long memberId) {
+        return favoriteRepository.findAllByMemberId(memberId)
+                .stream()
+                .map(FavoriteResponse::of)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void deleteFavorite(long memberId, long favoriteId) {
+        final Favorite favorite = favoriteRepository.findByMemberIdAndId(memberId, favoriteId)
+                .orElseThrow(() -> new IllegalArgumentException("즐겨찾기가 존재하지 않습니다."));
+
+        favoriteRepository.delete(favorite);
     }
 }
