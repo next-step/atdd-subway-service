4c59e17ad265664d08034e7f06e2b766320d5cb6
jiyeonghwang <gyp0288@naver.com>
2022-06-30 17:58:03 +0900

    feat: 즐겨찾기 기능 등록 구현

---
+++ src/main/java/nextstep/subway/favorite/ui/FavoriteController.java
@@ -0,0 +1,30 @@
+package nextstep.subway.favorite.ui;
+
+import nextstep.subway.auth.domain.AuthenticationPrincipal;
+import nextstep.subway.auth.domain.LoginMember;
+import nextstep.subway.favorite.application.FavoriteService;
+import nextstep.subway.favorite.dto.FavoriteRequest;
+import nextstep.subway.favorite.dto.FavoriteResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/favorites")
+public class FavoriteController {
+    private FavoriteService favoriteService;
+
+    public FavoriteController(FavoriteService favoriteService) {
+        this.favoriteService = favoriteService;
+    }
+
+    @PostMapping
+    public ResponseEntity saveFavorite(@AuthenticationPrincipal LoginMember loginMember, @RequestBody FavoriteRequest request) {
+        FavoriteResponse favorite = favoriteService.saveFavorite(loginMember, request);
+        return ResponseEntity.created(URI.create("/favorite/" + favorite.getId())).body(favorite);
+    }
+}
