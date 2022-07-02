d259133e8abf7760509cd3c92fa53241cf376052
Philip <pch4623@gmail.com>
2022-07-02 18:48:37 +0900

    feat: 추가요금 일급 컬렉션 추가

---
+++ src/main/java/nextstep/subway/line/domain/AdditionalFare.java
@@ -0,0 +1,49 @@
+package nextstep.subway.line.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import java.util.Objects;
+
+@Embeddable
+public class AdditionalFare {
+    private static final int INITIAL_FARE = 0;
+
+    @Column(nullable = false)
+    private int fare;
+
+    protected AdditionalFare() {
+    }
+
+    private AdditionalFare(int fare) {
+        this.fare = fare;
+    }
+
+    public static AdditionalFare from(int fare) {
+        if (fare < INITIAL_FARE) {
+            String message = String.format("추가 요금은 %d 보다 작을 수 없습니다.", INITIAL_FARE);
+            throw new IllegalArgumentException(message);
+        }
+        return new AdditionalFare(fare);
+    }
+
+    public static AdditionalFare init() {
+        return new AdditionalFare(INITIAL_FARE);
+    }
+
+    public int getFare() {
+        return fare;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AdditionalFare that = (AdditionalFare) o;
+        return fare == that.fare;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(fare);
+    }
+}
