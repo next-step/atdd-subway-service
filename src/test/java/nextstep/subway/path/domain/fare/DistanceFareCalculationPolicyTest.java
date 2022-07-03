5e1f8392f22ec4357ba4a51415f492ba6c5d48a7
Philip <pch4623@gmail.com>
2022-07-04 00:19:45 +0900

    test: 나이별 거리별 요금 계산 테스트 수정

--- src/test/java/nextstep/subway/path/domain/fare/DistanceFareCalculationPolicyTest.java
+++ src/test/java/nextstep/subway/path/domain/fare/DistanceFareCalculationPolicyTest.java
@@ -11,7 +11,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 class DistanceFareCalculationPolicyTest {
 
     @ParameterizedTest
-    @CsvSource(value = {"10:1250", "15:1350", "25:1550", "40:1850", "50:2050", "66:2250", "90:2550"}, delimiter = ':')
+    @CsvSource(value = {"10:1250", "15:1350", "25:1550", "39:1850", "40:1850", "50:2050", "65:2250", "66:2250", "90:2550"}, delimiter = ':')
     @DisplayName("거리별 어른 요금을 계산한다.")
     void calculateFareOfAdultByDistance(int 거리, int 예상요금) {
         int 성인_연령 = 20;
@@ -20,7 +20,7 @@ class DistanceFareCalculationPolicyTest {
     }
 
     @ParameterizedTest
-    @CsvSource(value = {"10:720", "15:800", "25:960", "40:1200", "50:1360", "66:1520", "90:1760"}, delimiter = ':')
+    @CsvSource(value = {"10:720", "15:800", "25:960", "39:1200", "40:1200", "50:1360", "65:1520", "66:1520", "90:1760"}, delimiter = ':')
     @DisplayName("거리별 청소년 요금을 계산한다.")
     void calculateFareOfAdolescentByDistance(int 거리, int 예상요금) {
         int 청소년_연령 = 15;
@@ -29,7 +29,7 @@ class DistanceFareCalculationPolicyTest {
     }
 
     @ParameterizedTest
-    @CsvSource(value = {"10:450", "15:500", "25:600", "40:750", "50:850", "66:950", "90:1100"}, delimiter = ':')
+    @CsvSource(value = {"10:450", "15:500", "25:600", "39:750", "40:750", "50:850", "65:950", "66:950", "90:1100"}, delimiter = ':')
     @DisplayName("거리별 어린이 요금을 계산한다.")
     void calculateFareOfChildByDistance(int 거리, int 예상요금) {
         int 어린이_연령 = 10;
