38f15156b29d1e4ed87085c9f167b37262c8853c
bestsilver <dkvkxm99@gmail.com>
2022-07-03 21:02:45 +0900

    test: 경로 조회 시 거리 기준 요금 정보 포함 인수테스트 작성

--- src/test/java/nextstep/subway/path/acceptance/PathAcceptanceTest.java
+++ src/test/java/nextstep/subway/path/acceptance/PathAcceptanceTest.java
@@ -27,18 +27,23 @@ public class PathAcceptanceTest extends AcceptanceTest {
     private LineResponse 신분당선;
     private LineResponse 이호선;
     private LineResponse 삼호선;
+    private LineResponse 오호선;
     private StationResponse 강남역;
     private StationResponse 양재역;
     private StationResponse 교대역;
     private StationResponse 남부터미널역;
+    private StationResponse 미사역;
+    private StationResponse 군자역;
 
     /**
-     * 교대역    --- *2호선* (7) ---   강남역
+     * 교대역    --- *2호선* (20) ---   강남역
      * |                              |
      * *3호선*                      *신분당선*
-     * (3)                           (10)
+     * (5)                           (10)
      * |                              |
-     * 남부터미널역  --- *3호선* (2)---   양재
+     * 남부터미널역  --- *3호선* (30)---   양재
+     *
+     * 미사역 ----- *5호선* (55) ------- 군자역
      */
     @BeforeEach
     public void setUp() {
@@ -48,12 +53,15 @@ public class PathAcceptanceTest extends AcceptanceTest {
         양재역 = StationAcceptanceTest.지하철역_등록되어_있음("양재역").as(StationResponse.class);
         교대역 = StationAcceptanceTest.지하철역_등록되어_있음("교대역").as(StationResponse.class);
         남부터미널역 = StationAcceptanceTest.지하철역_등록되어_있음("남부터미널역").as(StationResponse.class);
+        미사역 = StationAcceptanceTest.지하철역_등록되어_있음("미사역").as(StationResponse.class);
+        군자역 = StationAcceptanceTest.지하철역_등록되어_있음("군자역").as(StationResponse.class);
 
         신분당선 = LineAcceptanceTest.지하철_노선_등록되어_있음(new LineRequest("신분당선", "bg-red-600", 강남역.getId(), 양재역.getId(), 10)).as(LineResponse.class);
-        이호선 = LineAcceptanceTest.지하철_노선_등록되어_있음(new LineRequest("이호선", "bg-red-600", 교대역.getId(), 강남역.getId(), 7)).as(LineResponse.class);
-        삼호선 = LineAcceptanceTest.지하철_노선_등록되어_있음(new LineRequest("삼호선", "bg-red-600", 교대역.getId(), 양재역.getId(), 5)).as(LineResponse.class);
+        이호선 = LineAcceptanceTest.지하철_노선_등록되어_있음(new LineRequest("이호선", "bg-red-600", 교대역.getId(), 강남역.getId(), 20)).as(LineResponse.class);
+        삼호선 = LineAcceptanceTest.지하철_노선_등록되어_있음(new LineRequest("삼호선", "bg-red-600", 교대역.getId(), 양재역.getId(), 35)).as(LineResponse.class);
+        오호선 = LineAcceptanceTest.지하철_노선_등록되어_있음(new LineRequest("오호선", "bg-red-600", 미사역.getId(), 군자역.getId(), 55)).as(LineResponse.class);
 
-        LineSectionAcceptanceTest.지하철_노선에_지하철역_등록_요청(삼호선, 교대역, 남부터미널역, 3);
+        LineSectionAcceptanceTest.지하철_노선에_지하철역_등록_요청(삼호선, 교대역, 남부터미널역, 5);
     }
 
     @Test
@@ -62,7 +70,8 @@ public class PathAcceptanceTest extends AcceptanceTest {
         ExtractableResponse<Response> findPathResponse = 최단_경로_조회_요청(교대역.getId(), 양재역.getId());
 
         //then
-        최단_경로_거리_비교(findPathResponse, 5);
+        최단_경로_거리_비교(findPathResponse, 30);
+        경로_별_요금_비교(findPathResponse, 1850);
     }
 
     @Test
@@ -87,6 +96,29 @@ public class PathAcceptanceTest extends AcceptanceTest {
 
     }
 
+    @Test
+    void 거리_기준_요금_정보_조회_기본운임() {
+        ExtractableResponse<Response> findPathResponse = 최단_경로_조회_요청(강남역.getId(), 양재역.getId());
+
+        경로_별_요금_비교(findPathResponse, 1250);
+    }
+
+    @Test
+    void 거리_기준_요금_정보_조회_10km_초과() {
+        //교대 - 양재 distance 30
+        ExtractableResponse<Response> findPathResponse = 최단_경로_조회_요청(교대역.getId(), 양재역.getId());
+
+        경로_별_요금_비교(findPathResponse, 1850);
+    }
+
+    @Test
+    void 거리_기준_요금_정보_조회_50km_초과() {
+        ExtractableResponse<Response> findPathResponse = 최단_경로_조회_요청(미사역.getId(), 군자역.getId());
+
+        경로_별_요금_비교(findPathResponse, 1950);
+
+    }
+
     private static ExtractableResponse<Response> getPath(String path, Map<String, ?> queryParams) {
         return RestAssured.given().log().all()
                 .queryParams(queryParams)
@@ -107,6 +139,12 @@ public class PathAcceptanceTest extends AcceptanceTest {
         PathResponse pathResponse = response.as(PathResponse.class);
 
         assertThat(pathResponse.getDistance()).isEqualTo(expectedDistance);
+    }
+
+    private void 경로_별_요금_비교(ExtractableResponse<Response> response, int expectedFare) {
+        PathResponse pathResponse = response.as(PathResponse.class);
+
+        assertThat(pathResponse.getFare()).isEqualTo(expectedFare);
     }
 
     private void 최단_경로_조회_404_실패(ExtractableResponse<Response> response) {
