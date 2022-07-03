3742aaa181a5ed40d3040ea780a95c7a4d7f69fd
Philip <pch4623@gmail.com>
2022-07-04 00:20:12 +0900

    feat: 노선별 추가요금 필드 추가

--- src/main/java/nextstep/subway/line/dto/LineRequest.java
+++ src/main/java/nextstep/subway/line/dto/LineRequest.java
@@ -8,16 +8,22 @@ public class LineRequest {
     private Long upStationId;
     private Long downStationId;
     private int distance;
+    private int additionalFare;
 
     public LineRequest() {
     }
 
-    public LineRequest(String name, String color, Long upStationId, Long downStationId, int distance) {
+    public LineRequest(String name, String color, Long upStationId, Long downStationId, int distance, int additionalFare) {
         this.name = name;
         this.color = color;
         this.upStationId = upStationId;
         this.downStationId = downStationId;
         this.distance = distance;
+        this.additionalFare = additionalFare;
+    }
+
+    public LineRequest(String name, String color, Long upStationId, Long downStationId, int distance) {
+        this(name, color, upStationId, downStationId, distance, 0);
     }
 
     public String getName() {
@@ -38,6 +44,10 @@ public class LineRequest {
 
     public int getDistance() {
         return distance;
+    }
+
+    public int getAdditionalFare() {
+        return additionalFare;
     }
 
     public Line toLine() {
