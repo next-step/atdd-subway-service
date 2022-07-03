06c28bcc3ad65167875c9e69341311c9e9a0cf86
Philip <pch4623@gmail.com>
2022-07-03 16:57:49 +0900

    refactor: 기본요금 인터페이스에서 공유변수로 사용

--- src/main/java/nextstep/subway/path/domain/fare/BaseFareCalculationPolicy.java
+++ src/main/java/nextstep/subway/path/domain/fare/BaseFareCalculationPolicy.java
@@ -3,8 +3,6 @@ package nextstep.subway.path.domain.fare;
 import nextstep.subway.path.domain.fare.discount.DiscountPolicy;
 
 public class BaseFareCalculationPolicy implements FareCalculationPolicy {
-    private static final int DEFAULT_FARE = 1_250;
-
     private final DiscountPolicy discountPolicy;
 
     public BaseFareCalculationPolicy(DiscountPolicy discountPolicy) {
--- src/main/java/nextstep/subway/path/domain/fare/FareCalculationPolicy.java
+++ src/main/java/nextstep/subway/path/domain/fare/FareCalculationPolicy.java
@@ -1,5 +1,7 @@
 package nextstep.subway.path.domain.fare;
 
 public interface FareCalculationPolicy {
+    int DEFAULT_FARE = 1_250;
+
     int calculateFare();
 }
