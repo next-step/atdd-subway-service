48873b8751b01e9f00718405a8399da049979c41
wenodev <logicalstark1105@gmail.com>
2022-07-07 14:35:45 +0900

    fix: 연령별 할인 금액 추가

--- src/main/java/nextstep/subway/auth/domain/LoginMember.java
+++ src/main/java/nextstep/subway/auth/domain/LoginMember.java
@@ -1,7 +1,8 @@
 package nextstep.subway.auth.domain;
 
 public class LoginMember {
-    private static final Long GUEST_ID = -1L;
+    public static final Long GUEST_ID = -1L;
+    public static final String GUEST_EMAIL = "guest@nextstep.com";
     private static final Integer GUEST_AGE = 0;
 
     private Long id;
@@ -10,6 +11,7 @@ public class LoginMember {
 
     public LoginMember() {
         this.id = GUEST_ID;
+        this.email = GUEST_EMAIL;
         this.age = GUEST_AGE;
     }
 
@@ -29,5 +31,9 @@ public class LoginMember {
 
     public Integer getAge() {
         return age;
+    }
+
+    public boolean isGuest() {
+        return GUEST_EMAIL.equals(this.email);
     }
 }
---
+++ src/test/java/nextstep/subway/fare/AgeFarePolicyTest.java
@@ -0,0 +1,44 @@
+package nextstep.subway.fare;
+
+import nextstep.subway.auth.domain.LoginMember;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class AgeFarePolicyTest {
+    private AgeFarePolicy ageFarePolicy = new AgeFarePolicy();
+
+    @DisplayName("비회원은 할인하지 않는다")
+    @Test
+    void calculate_guest() {
+        Fare fare = new Fare(1000);
+        LoginMember loginMember = new LoginMember();
+
+        Fare result = ageFarePolicy.calculate(fare, loginMember);
+
+        assertThat(result).isEqualTo(new Fare(1000));
+    }
+
+    @DisplayName("어린이는 50프로 할인한다")
+    @Test
+    void calculate_children() {
+        Fare fare = new Fare(1350);
+        LoginMember loginMember = new LoginMember(1L, "children", 7);
+
+        Fare result = ageFarePolicy.calculate(fare, loginMember);
+
+        assertThat(result).isEqualTo(new Fare(500));
+    }
+
+    @DisplayName("청소년은 50프로 할인한다")
+    @Test
+    void calculate_teenager() {
+        Fare fare = new Fare(1350);
+        LoginMember loginMember = new LoginMember(1L, "teenager", 15);
+
+        Fare result = ageFarePolicy.calculate(fare, loginMember);
+
+        assertThat(result).isEqualTo(new Fare(800));
+    }
+}
