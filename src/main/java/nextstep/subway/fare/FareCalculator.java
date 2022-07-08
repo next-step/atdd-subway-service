902f8572cae0ad6fe76e46ba05dab23bdae18cba
wenodev <logicalstark1105@gmail.com>
2022-07-08 21:56:48 +0900

    refactor: 불필요한 코드 제거

--- src/main/java/nextstep/subway/fare/FareCalculator.java
+++ src/main/java/nextstep/subway/fare/FareCalculator.java
@@ -7,20 +7,16 @@ import org.springframework.stereotype.Component;
 @Component
 public class FareCalculator {
     private final DistanceFarePolicy distanceFarePolicy;
-    private final LineFarePolicy lineFarePolicy;
     private final AgeFarePolicy ageFarePolicy;
 
-    public FareCalculator(DistanceFarePolicy distanceFarePolicy, LineFarePolicy lineFarePolicy, AgeFarePolicy ageFarePolicy) {
+    public FareCalculator(DistanceFarePolicy distanceFarePolicy, AgeFarePolicy ageFarePolicy) {
         this.distanceFarePolicy = distanceFarePolicy;
-        this.lineFarePolicy = lineFarePolicy;
         this.ageFarePolicy = ageFarePolicy;
     }
 
     public void calculate(Path path, LoginMember loginMember) {
         Fare distanceFare = distanceFarePolicy.calculate(new Distance(path.getDistance()));
-        Fare lineFare = lineFarePolicy.calculate(path.getLines());
-        Fare addedFare = distanceFare.add(lineFare);
-        Fare resultFare = ageFarePolicy.calculate(addedFare, loginMember);
+        Fare resultFare = ageFarePolicy.calculate(distanceFare, loginMember);
         path.updateFare(resultFare);
     }
 }
