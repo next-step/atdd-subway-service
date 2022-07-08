874496e9cbd1f81b66df5c0ebf0f5f7b3f14cfc1
jiyeonghwang <gyp0288@naver.com>
2022-07-08 20:29:20 +0900

    feat: 비회원인 경우 경로 조회 테스트 추가

--- src/test/java/nextstep/subway/path/acceptance/PathAcceptanceTest.java
+++ src/test/java/nextstep/subway/path/acceptance/PathAcceptanceTest.java
@@ -33,7 +33,6 @@ public class PathAcceptanceTest extends AcceptanceTest {
     private StationResponse 양재역;
     private StationResponse 교대역;
     private StationResponse 남부터미널역;
-    private String accessToken;
 
     /**
      * 교대역    --- *2호선* ---   강남역
@@ -57,17 +56,13 @@ public class PathAcceptanceTest extends AcceptanceTest {
         삼호선 = 지하철_노선_등록되어_있음(new LineRequest("삼호선", "bg-red-600", 교대역.getId(), 양재역.getId(), 5, 0)).as(LineResponse.class);
 
         지하철_노선에_지하철역_등록_요청(삼호선, 교대역, 남부터미널역, 3);
-
-        회원_생성을_요청(EMAIL, PASSWORD, 25);
-        ExtractableResponse<Response> loginResponse = 로그인(new TokenRequest(EMAIL, PASSWORD));
-        accessToken = loginResponse.as(TokenResponse.class).getAccessToken();
     }
 
     @DisplayName("최단 경로 조회 기능")
     @Test
     void getShortestPathTest() {
         // when
-        ExtractableResponse<Response> response = 최단_경로_조회(강남역.getId(), 양재역.getId(), accessToken);
+        ExtractableResponse<Response> response = 최단_경로_조회(강남역.getId(), 양재역.getId());
 
         // then
         최단_경로_길이_비교(response, 10);
@@ -77,7 +72,7 @@ public class PathAcceptanceTest extends AcceptanceTest {
     @Test
     void 예외_출발역과_도착역이_같음() {
         // when
-        ExtractableResponse<Response> response = 최단_경로_조회(강남역.getId(), 강남역.getId(), accessToken);
+        ExtractableResponse<Response> response = 최단_경로_조회(강남역.getId(), 강남역.getId());
 
         // then
         최단_경로_조회_실패(response);
@@ -87,7 +82,7 @@ public class PathAcceptanceTest extends AcceptanceTest {
     @Test
     void 예외_출발역과_도착역_미연결() {
         // when
-        ExtractableResponse<Response> response = 최단_경로_조회(강남역.getId(), 광교역.getId(), accessToken);
+        ExtractableResponse<Response> response = 최단_경로_조회(강남역.getId(), 광교역.getId());
 
         // then
         최단_경로_조회_실패(response);
@@ -97,10 +92,20 @@ public class PathAcceptanceTest extends AcceptanceTest {
     @Test
     void 예외_존재하지_않는_출발역_또는_도착역() {
         // when
-        ExtractableResponse<Response> response = 최단_경로_조회(Long.MAX_VALUE-1, Long.MAX_VALUE-2, accessToken);
+        ExtractableResponse<Response> response = 최단_경로_조회(Long.MAX_VALUE-1, Long.MAX_VALUE-2);
 
         // then
         최단_경로_조회_실패(response);
+    }
+
+    @DisplayName("비회원인 경우 성인 요금 조회")
+    @Test
+    void 비회원_성인_요금_조회() {
+        // given
+        ExtractableResponse<Response> response = 최단_경로_조회(강남역.getId(), 양재역.getId());
+
+        // then
+        assertThat(response.jsonPath().getInt("fare")).isEqualTo(1250);
     }
 
     @DisplayName("성인 요금 조회")
@@ -113,7 +118,7 @@ public class PathAcceptanceTest extends AcceptanceTest {
         ExtractableResponse<Response> response = 최단_경로_조회(강남역.getId(), 양재역.getId(), accessToken);
 
         // then
-        assertThat(response.jsonPath().getInt("fare")).isEqualTo(1250);;
+        assertThat(response.jsonPath().getInt("fare")).isEqualTo(1250);
     }
 
     @DisplayName("청소년 요금 조회")
@@ -126,7 +131,7 @@ public class PathAcceptanceTest extends AcceptanceTest {
         ExtractableResponse<Response> response = 최단_경로_조회(강남역.getId(), 양재역.getId(), accessToken);
 
         // then
-        assertThat(response.jsonPath().getInt("fare")).isEqualTo(720);;
+        assertThat(response.jsonPath().getInt("fare")).isEqualTo(720);
     }
 
     @DisplayName("어린이 요금 조회")
@@ -139,7 +144,18 @@ public class PathAcceptanceTest extends AcceptanceTest {
         ExtractableResponse<Response> response = 최단_경로_조회(강남역.getId(), 양재역.getId(), accessToken);
 
         // then
-        assertThat(response.jsonPath().getInt("fare")).isEqualTo(450);;
+        assertThat(response.jsonPath().getInt("fare")).isEqualTo(450);
+    }
+
+    public ExtractableResponse<Response> 최단_경로_조회(Long source, Long target) {
+        return RestAssured
+                .given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .param("source", source)
+                .param("target", target)
+                .when().get("/paths")
+                .then().log().all()
+                .extract();
     }
 
     public ExtractableResponse<Response> 최단_경로_조회(Long source, Long target, String accessToken) {
