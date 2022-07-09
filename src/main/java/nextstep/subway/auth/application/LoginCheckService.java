c00ca4f4a5649d6bc7f023c4ae787d18ac9ba8d5
millennion-jin <Jadestern2@>
2022-07-10 02:04:02 +0900

    refactor(LoginCheckService) : 컨벤션 맞춤

--- src/main/java/nextstep/subway/auth/application/LoginCheckService.java
+++ src/main/java/nextstep/subway/auth/application/LoginCheckService.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class LoginCheckService {
     public void needLogin(LoginMember loginMember) {
-        if(loginMember.isGuest()) {
+        if (loginMember.isGuest()) {
             throw new AuthorizationException();
         }
     }
