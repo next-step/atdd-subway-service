5e1533e4e13f7460a838864b3bca9c439ca2b400
earth-h <earth.h.183@gmail.com>
2022-11-20 22:10:21 +0900

    test(service): 노선 수정 서비스 리팩토링 전 Mocking 테스트 생성
    - 기존 로직의 경우, 노선색상이 비어있거나 노선명이 비어있을 때 대응이 없어 해당 부분 테스트 주석처리함

--- src/test/java/nextstep/subway/line/application/LineServiceTest.java
+++ src/test/java/nextstep/subway/line/application/LineServiceTest.java
@@ -1,5 +1,6 @@
 package nextstep.subway.line.application;
 
+import static nextstep.subway.line.domain.LineTestFixture.createLine;
 import static nextstep.subway.station.domain.StationTestFixture.createStation;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -9,15 +10,17 @@ import static org.mockito.Mockito.when;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 import nextstep.subway.common.constant.ErrorCode;
+import nextstep.subway.line.domain.Color;
 import nextstep.subway.line.domain.Line;
 import nextstep.subway.line.domain.LineRepository;
+import nextstep.subway.line.domain.Name;
 import nextstep.subway.line.dto.LineRequest;
 import nextstep.subway.line.dto.LineResponse;
 import nextstep.subway.station.application.StationService;
 import nextstep.subway.station.domain.Station;
 import nextstep.subway.station.dto.StationResponse;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -38,28 +41,38 @@ public class LineServiceTest {
     @InjectMocks
     private LineService lineService;
 
+    private Station 강남역;
+    private Station 역삼역;
+    private Line 이호선;
+    private LineRequest lineRequest;
+
+    @BeforeEach
+    void setUp() {
+        강남역 = createStation("강남역");
+        역삼역 = createStation("역삼역");
+        이호선 = createLine("2호선", "bg-green", 강남역, 역삼역, 10);
+    }
+
     @DisplayName("노선을 생성한다.")
     @Test
     void saveLine() {
         // given
-        Station upStation = createStation("강남역");
-        Station downStation = createStation("역삼역");
-        LineRequest lineRequest = new LineRequest("2호선", "bg-green", 1L, 2L, 10);
-        Line line = lineRequest.toLine(upStation, downStation);
+        LineRequest lineRequest = new LineRequest(이호선.getName().value(), 이호선.getColor().value(), 1L, 2L, 10);
+        Line line = lineRequest.toLine();
         when(stationService.findById(1L))
-                .thenReturn(upStation);
+                .thenReturn(강남역);
         when(stationService.findById(2L))
-                .thenReturn(downStation);
+                .thenReturn(역삼역);
         when(lineRepository.save(line))
-                .thenReturn(line);
+                .thenReturn(이호선);
 
         // when
         LineResponse response = lineService.saveLine(lineRequest);
 
         // then
         assertAll(
-                () -> assertThat(response.getColor()).isEqualTo("bg-green"),
-                () -> assertThat(response.getName()).isEqualTo("2호선"),
+                () -> assertThat(response.getColor()).isEqualTo(이호선.getColor().value()),
+                () -> assertThat(response.getName()).isEqualTo(이호선.getName().value()),
                 () -> assertThat(response.getStations()).hasSize(2)
         );
     }
@@ -68,21 +81,18 @@ public class LineServiceTest {
     @Test
     void findLineById() {
         // given
-        Station upStation = createStation("강남역");
-        Station downStation = createStation("역삼역");
-        Line line = new Line("2호선", "bg-green", upStation, downStation, 10);
-        when(lineRepository.findById(1L)).thenReturn(Optional.of(line));
+        when(lineRepository.findById(1L)).thenReturn(Optional.of(이호선));
 
         // when
         LineResponse response = lineService.findLineResponseById(1L);
 
         // then
         assertAll(
-                () -> assertThat(response.getName()).isEqualTo("2호선"),
-                () -> assertThat(response.getColor()).isEqualTo("bg-green"),
+                () -> assertThat(response.getName()).isEqualTo(이호선.getName().value()),
+                () -> assertThat(response.getColor()).isEqualTo(이호선.getColor().value()),
                 () -> assertThat(response.getStations()).hasSize(2),
                 () -> assertThat(response.getStations().stream()
-                        .map(StationResponse::getName)).containsExactly("강남역", "역삼역")
+                        .map(StationResponse::getName)).containsExactly(강남역.getName().value(), 역삼역.getName().value())
         );
     }
 
@@ -90,9 +100,6 @@ public class LineServiceTest {
     @Test
     void findLineByIdThrowErrorIfNotExists() {
         // given
-        Station upStation = createStation("강남역");
-        Station downStation = createStation("역삼역");
-        Line line = new Line("2호선", "bg-green", upStation, downStation, 10);
         when(lineRepository.findById(1L)).thenReturn(Optional.empty());
 
         // when & then
@@ -105,13 +112,10 @@ public class LineServiceTest {
     @Test
     void findLines() {
         // given
-        Station upStation = createStation("강남역");
-        Station downStation = createStation("역삼역");
-        Line line = new Line("2호선", "bg-green", upStation, downStation, 10);
         Station upStation2 = createStation("양재역");
         Station downStation2 = createStation("매봉역");
         Line line2 = new Line("3호선", "bg-orange", upStation2, downStation2, 25);
-        when(lineRepository.findAll()).thenReturn(Arrays.asList(line, line2));
+        when(lineRepository.findAll()).thenReturn(Arrays.asList(이호선, line2));
 
         // when
         List<LineResponse> lines = lineService.findLines();
@@ -119,8 +123,58 @@ public class LineServiceTest {
         // then
         assertAll(
                 () -> assertThat(lines).hasSize(2),
-                () -> assertThat(lines.stream().map(LineResponse::getColor)).contains("bg-green", "bg-orange"),
-                () -> assertThat(lines.stream().map(LineResponse::getName)).contains("2호선", "3호선")
+                () -> assertThat(lines.stream().map(LineResponse::getColor)).contains(이호선.getColor().value(), "bg-orange"),
+                () -> assertThat(lines.stream().map(LineResponse::getName)).contains(이호선.getName().value(), "3호선")
         );
     }
+
+    @DisplayName("노선명과 노선색상을 수정한다.")
+    @Test
+    void updateLineColorAndLineName() {
+        // given
+        when(lineRepository.findById(1L)).thenReturn(Optional.of(이호선));
+
+        // when
+        lineService.updateLine(1L, new LineRequest("3호선", "bg-orange", null, null, 0));
+
+        // then
+        assertAll(
+                () -> assertThat(이호선.getColor()).isEqualTo(Color.from("bg-orange")),
+                () -> assertThat(이호선.getName()).isEqualTo(Name.from("3호선"))
+        );
+    }
+
+//    @DisplayName("노선명과 노선색상 수정 시, 노선명이 비어있으면 노선색상만 수정한다.")
+//    @Test
+//    void updateLineColor() {
+//        // given
+//        Name actualName = 이호선.getName();
+//        when(lineRepository.findById(1L)).thenReturn(Optional.of(이호선));
+//
+//        // when
+//        lineService.updateLine(1L, new LineRequest(null, "bg-orange", null, null, 0));
+//
+//        // then
+//        assertAll(
+//                () -> assertThat(이호선.getColor()).isEqualTo(Color.from("bg-orange")),
+//                () -> assertThat(이호선.getName()).isEqualTo(actualName)
+//        );
+//    }
+//
+//    @DisplayName("노선명과 노선색상 수정 시, 노선색상이 비어있으면 노선명만 수정한다.")
+//    @Test
+//    void updateLineName() {
+//        // given
+//        Color actualColor = 이호선.getColor();
+//        when(lineRepository.findById(1L)).thenReturn(Optional.of(이호선));
+//
+//        // when
+//        lineService.updateLine(1L, new LineRequest("3호선", null, null, null, 0));
+//
+//        // then
+//        assertAll(
+//                () -> assertThat(이호선.getColor()).isEqualTo(actualColor),
+//                () -> assertThat(이호선.getName()).isEqualTo(Name.from("3호선"))
+//        );
+//    }
 }
