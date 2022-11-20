d4b81666dff8d32f840da705066e19c219070d00
jisu1211 <dbdnzl1211@gmail.com>
2022-11-21 03:59:10 +0900

    refactor: Set 컬렉션으로 변경

--- src/main/java/nextstep/subway/line/domain/SectionConnectManager.java
+++ src/main/java/nextstep/subway/line/domain/SectionConnectManager.java
@@ -2,15 +2,16 @@ package nextstep.subway.line.domain;
 
 import java.util.List;
 import java.util.Objects;
+import java.util.Set;
 
-import static java.util.stream.Collectors.toList;
+import static java.util.stream.Collectors.toSet;
 
 public class SectionConnectManager {
     public static void connectAll(Line line, Section request, List<Section> matchedSections) {
-        List<Section> requests = matchedSections.stream()
+        Set<Section> requests = matchedSections.stream()
                 .map(section -> SectionConnectManager.connect(section, request))
                 .filter(Objects::nonNull)
-                .collect(toList());
+                .collect(toSet());
 
         addNewSections(line, requests);
     }
@@ -21,7 +22,7 @@ public class SectionConnectManager {
         return request;
     }
 
-    private static void addNewSections(Line line, List<Section> requests) {
+    private static void addNewSections(Line line, Set<Section> requests) {
         requests.forEach(request -> line.addSection(request));
     }
 }
