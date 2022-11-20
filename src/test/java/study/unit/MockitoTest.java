af3f598da03346f8f85f1890f2a5723270fc1f01
earth-h <earth.h.183@gmail.com>
2022-11-20 18:37:10 +0900

    fix(test): 테스트 내 new Line()으로 인해 NPE 에러 나는 부분 수정
    Line
    - Line() 생성자는 로직에서 실제로 사용하지 않으므로, entity 생성을 위해 protected로 변경
    
    MockitoExtensionTest & MockitoTest & SpringExtensionTest
    - Line 생성 부분 createLine() 사용하도록 변경

--- src/test/java/study/unit/MockitoTest.java
+++ src/test/java/study/unit/MockitoTest.java
@@ -1,20 +1,19 @@
 package study.unit;
 
-import com.google.common.collect.Lists;
-import nextstep.subway.line.application.LineService;
-import nextstep.subway.line.domain.Line;
-import nextstep.subway.line.domain.LineRepository;
-import nextstep.subway.line.dto.LineResponse;
-import nextstep.subway.station.application.StationService;
-import nextstep.subway.station.domain.StationRepository;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-
-import java.util.List;
-
+import static nextstep.subway.line.domain.LineTestFixture.createLine;
+import static nextstep.subway.station.domain.StationTestFixture.createStation;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
+
+import com.google.common.collect.Lists;
+import java.util.List;
+import nextstep.subway.line.application.LineService;
+import nextstep.subway.line.domain.LineRepository;
+import nextstep.subway.line.dto.LineResponse;
+import nextstep.subway.station.application.StationService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 @DisplayName("단위 테스트 - mockito를 활용한 가짜 협력 객체 사용")
 public class MockitoTest {
@@ -24,7 +23,8 @@ public class MockitoTest {
         LineRepository lineRepository = mock(LineRepository.class);
         StationService stationService = mock(StationService.class);
 
-        when(lineRepository.findAll()).thenReturn(Lists.newArrayList(new Line()));
+        when(lineRepository.findAll())
+                .thenReturn(Lists.newArrayList(createLine("신분당선", "bg-red", createStation("강남역"), createStation("양재역"), 10)));
         LineService lineService = new LineService(lineRepository, stationService);
 
         // when
