9053a3943bc475457b9fc7fec946a0841edff6f9
earth-h <earth.h.183@gmail.com>
2022-11-21 01:22:10 +0900

    refactor(handler, controller): controller에 존재하던 핸들러 한 곳에 모음, controller 내 메소드 반환값 리팩토링

--- src/main/java/nextstep/subway/common/handler/CustomExceptionHandler.java
+++ src/main/java/nextstep/subway/common/handler/CustomExceptionHandler.java
@@ -1,11 +1,17 @@
 package nextstep.subway.common.handler;
 
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class CustomExceptionHandler {
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<Void> handleIllegalArgsException(DataIntegrityViolationException e) {
+        return ResponseEntity.badRequest().build();
+    }
 
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<Void> handleIllegalArgsException() {
