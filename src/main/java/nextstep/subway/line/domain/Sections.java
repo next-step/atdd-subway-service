e406d8a56322eb66abd557981eb83b8da45c5afe
earth-h <earth.h.183@gmail.com>
2022-11-20 16:12:08 +0900

    feat(domain): Sections 내 지하철역들 정렬하여 조회하는 메소드 추가
    Sections
    - 상행부터 하행까지 정렬하여 지하철역 조회하는 메소드 추가
      - findInOrderStations() 내에서 먼저 <상행역,하행역>Map(stations) 생성
      - findLineUpStation() 내에서 주어진 map 내에서 key에만 존재하고 value에는 존재하지 않는 상행역 찾음
      - 찾은 상행역에서부터 sortStations() 메소드를 수행하여 정렬한 리스트 생성
    
    SectionsTest
    - Sections 내 지하철역들 정렬하여 조회하는 메소드 테스트 로직 추가

--- src/main/java/nextstep/subway/line/domain/Sections.java
+++ src/main/java/nextstep/subway/line/domain/Sections.java
@@ -3,10 +3,12 @@ package nextstep.subway.line.domain;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 import javax.persistence.CascadeType;
 import javax.persistence.Embeddable;
 import javax.persistence.OneToMany;
+import nextstep.subway.common.constant.ErrorCode;
 import nextstep.subway.station.domain.Station;
 
 @Embeddable
@@ -37,6 +39,31 @@ public class Sections {
                 .flatMap(Collection::stream)
                 .distinct()
                 .collect(Collectors.toList());
+    }
+
+    public List<Station> findInOrderStations() {
+        Map<Station, Station> stations = sections.stream()
+                .collect(Collectors.toMap(Section::getUpStation, Section::getDownStation));
+        return sortStations(findLineUpStation(stations), stations);
+    }
+
+    private Station findLineUpStation(Map<Station, Station> stations) {
+        return stations.keySet()
+                .stream()
+                .filter(upStation -> !stations.containsValue(upStation))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException(ErrorCode.상행종착역은_비어있을_수_없음.getErrorMessage()));
+    }
+
+    private List<Station> sortStations(Station lineUpStation, Map<Station, Station> stations) {
+        List<Station> sortStations = new ArrayList<>();
+        sortStations.add(lineUpStation);
+        Station currentStation = lineUpStation;
+        while(stations.get(currentStation) != null) {
+            currentStation = stations.get(currentStation);
+            sortStations.add(currentStation);
+        }
+        return sortStations;
     }
 
     public List<Section> getSections() { // TODO 추후 리팩토링 작업에 의해 제거 예정
--- src/test/java/nextstep/subway/line/domain/SectionsTest.java
+++ src/test/java/nextstep/subway/line/domain/SectionsTest.java
@@ -7,6 +7,8 @@ import static nextstep.subway.station.domain.StationTestFixture.createStation;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import nextstep.subway.station.domain.Station;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -29,5 +31,22 @@ public class SectionsTest {
         assertThat(sections.findStations()).hasSize(2);
     }
 
+    @DisplayName("구역을 정렬된 형태로 조회하면 상행역부터 하행역까지 정렬되어 조회된다.")
+    @Test
+    void findInOrderStations() {
+        //given
+        Station 양재역 = createStation("양재역");
+        Station 양재시민의숲역 = createStation("양재시민의숲역");
+        Station 청계산입구역 = createStation("청계산입구역");
+        Line line = createLine("신분당선", "bg-red", createStation("강남역"), 양재역, 10);
+        Section section1 = createSection(line, 양재역, 양재시민의숲역, 15);
+        Section section2 = createSection(line, 양재시민의숲역, 청계산입구역, 10);
 
+        //when
+        Sections sections = createSections(Arrays.asList(section1, section2));
+        List<Station> stations = sections.findInOrderStations();
+
+        //then
+        assertThat(stations).containsExactly(양재역, 양재시민의숲역, 청계산입구역);
+    }
 }
