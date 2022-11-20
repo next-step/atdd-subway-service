31d50375110b1bf43fba80691a7bcb8972e89003
sjk5766 <wjdrnr6662@gmail.com>
2022-11-21 02:58:31 +0900

    test(domain): Line의 getStations 함수에 대한 테스트 코드 작성

---
+++ src/test/java/nextstep/subway/line/domain/LineTest.java
@@ -0,0 +1,26 @@
+package nextstep.subway.line.domain;
+
+import nextstep.subway.station.domain.Station;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class LineTest {
+    @DisplayName("노선에 포함된 지하철 역 정보를 가져올 수 있다")
+    @Test
+    void stations() {
+        // given
+        Station upStation = new Station("판교역");
+        Station downStation = new Station("강남역");
+        Line line = new Line("신분당선", "bg-red-600", upStation, downStation, 10);
+
+        // when
+        List<Station> results = line.getStations();
+
+        // then
+        assertThat(results).containsOnly(upStation, downStation);
+    }
+}
