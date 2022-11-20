c34b809adefc8d15ea0214106da258637f72769a
earth-h <earth.h.183@gmail.com>
2022-11-20 20:57:53 +0900

    feat(domain): String name을 감쌀 Name 클래스 생성
    Name
    - 지하철명 또는 노선명으로 사용할 Name 클래스 생성
      - Name은 비어있으면 안되므로 해당 validation 추가
    
    NameTest
    - Name이 비어있으면 에러가 발생하는 테스트 생성
    - Name이 정상적으로 생성되었을 때 이름이 제대로 조회되는지 테스트

---
+++ src/main/java/nextstep/subway/line/domain/Name.java
@@ -0,0 +1,53 @@
+package nextstep.subway.line.domain;
+
+import java.util.Objects;
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import nextstep.subway.common.constant.ErrorCode;
+import nextstep.subway.utils.StringUtils;
+
+@Embeddable
+public class Name {
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    protected Name() {
+    }
+
+    private Name(String name) {
+        validateNameNullOrEmpty(name);
+        this.name = name;
+    }
+
+    public static Name from(String name) {
+        return new Name(name);
+    }
+
+    private void validateNameNullOrEmpty(String name) {
+        if(StringUtils.isNullOrEmpty(name)) {
+            throw new IllegalArgumentException(ErrorCode.노선_또는_지하철명은_비어있을_수_없음.getErrorMessage());
+        }
+    }
+
+    public String value() {
+        return this.name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Name name1 = (Name) o;
+        return Objects.equals(name, name1.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+}
---
+++ src/main/java/nextstep/subway/utils/StringUtils.java
@@ -0,0 +1,8 @@
+package nextstep.subway.utils;
+
+public class StringUtils {
+
+    public static boolean isNullOrEmpty(String text) {
+        return text == null || text.trim().isEmpty();
+    }
+}
