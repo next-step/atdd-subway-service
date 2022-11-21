d7e3ff59c04c8b1d4a0b6eb59b6f32a44a40b944
sjk5766 <wjdrnr6662@gmail.com>
2022-11-21 11:02:00 +0900

    test(domain): 노선 추가 기능의 domain 테스트 코드 작성

---
+++ src/test/java/nextstep/subway/section/SectionTest.java
@@ -0,0 +1,53 @@
+package nextstep.subway.section;
+
+import nextstep.subway.line.domain.Section;
+import nextstep.subway.line.domain.Sections;
+import nextstep.subway.station.domain.Station;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class SectionTest {
+    @DisplayName("상행선을 바꿀 수 있다.")
+    @Test
+    void updateUpStation() {
+        // given
+        Station upStation = new Station("판교역");
+        Station downStation = new Station("강남역");
+        Section section = new Section(upStation, downStation, 10);
+
+        Station newStation = new Station("양재역");
+        Section newSection = new Section(upStation, newStation, 10);
+
+        // when
+        section.update(newSection);
+
+        // then
+        assertThat(section.getUpStation()).isEqualTo(newStation);
+    }
+
+    @DisplayName("하행선을 바꿀 수 있다.")
+    @Test
+    void updateDownStation() {
+        // given
+        Station upStation = new Station("판교역");
+        Station downStation = new Station("강남역");
+        Section section = new Section(upStation, downStation, 10);
+
+        Station newStation = new Station("양재역");
+        Section newSection = new Section(newStation, downStation, 10);
+
+        // when
+        section.update(newSection);
+
+        // then
+        assertThat(section.getDownStation()).isEqualTo(newStation);
+    }
+}
--- src/test/java/nextstep/subway/sections/SectionsTest.java
+++ src/test/java/nextstep/subway/sections/SectionsTest.java
@@ -6,23 +6,25 @@ import nextstep.subway.station.domain.Station;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class SectionsTest {
     @DisplayName("정렬된 지하철 역 정보를 반환할 수 있다")
     @Test
-    void sortStationsTest() {
+    void sortStations() {
         // given
-        Station station1 = new Station("판교역");
-        Station station2 = new Station("양재역");
-        Station station3 = new Station("강남역");
+        Station upStation = new Station("판교역");
+        Station downStation = new Station("양재역");
+        Station newStation = new Station("강남역");
 
-        Section section1 = new Section(station3, station2, 10);
-        Section section2 = new Section(station2, station1, 5);
+        Section section1 = new Section(newStation, downStation, 10);
+        Section section2 = new Section(downStation, upStation, 5);
 
         Sections sections = new Sections(Arrays.asList(section1, section2));
 
@@ -34,5 +36,39 @@ public class SectionsTest {
 
         // then
         assertThat(stations).containsExactly("강남역", "양재역", "판교역");
+    }
+
+    @DisplayName("추가하려는 구간의 지하철 역이 모두 존재할 경우 예외가 발생한다")
+    @Test
+    void addBothExistStationException() {
+        // given
+        Station upStation = new Station("판교역");
+        Station downStation = new Station("양재역");
+
+        Section section = new Section(upStation, downStation, 10);
+
+        Sections sections = new Sections(Arrays.asList(section));
+        sections.add(section);
+
+        // then
+        assertThatThrownBy(() -> sections.add(section))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("이미 등록된 구간 입니다.");
+    }
+
+    @DisplayName("추가하려는 구간의 지하철 역이 모두 존재하지 않으면 예외가 발생한다")
+    @Test
+    void addBothNotExistStationException() {
+        // given
+        Station upStation = new Station("판교역");
+        Station downStation = new Station("양재역");
+        Section section = new Section(upStation, downStation, 10);
+
+        Sections sections = new Sections(new ArrayList<>());
+
+        // then
+        assertThatThrownBy(() -> sections.add(section))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("등록할 수 없는 구간 입니다.");
     }
 }
