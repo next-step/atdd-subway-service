7a5a06c7ea8e1abd181bccbe8d0c3f83e5e79971
손윤 <sonyoon7@joonggonara.co.kr>
2022-11-23 18:26:53 +0900

    docs (TODO) Domain으로 옮길 로직을 찾기

--- src/main/java/nextstep/subway/line/application/LineService.java
+++ src/main/java/nextstep/subway/line/application/LineService.java
@@ -18,6 +18,17 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+/**
+ * TODO
+ *   1. Domain으로 옮길 로직을 찾기
+ *      스프링 빈을 사용하는 객체와 의존하는 로직을 제외하고는 도메인으로 옮길 예정  <br>
+ *   2. Domain의 단위 테스트를 작성하기 <br>
+ *      서비스 레이어에서 옮겨 올 로직의 기능을 테스트  <br>
+ *      SectionsTest나 LineTest 클래스가 생성될 수 있음  <br>
+ *   3. 로직을 옮기기
+ *      기존 로직을 지우지 말고 새로운 로직을 만들어 수행 <br>
+ *      정상 동작 확인 후 기존 로직 제거 <br>
+ */
 @Service
 @Transactional
 public class LineService {
@@ -41,6 +52,7 @@ public class LineService {
 
     public List<LineResponse> findLines() {
         List<Line> persistLines = lineRepository.findAll();
+        // 아래에 중복코드 있음
         return persistLines.stream()
                 .map(line -> {
                     List<StationResponse> stations = getStations(line).stream()
@@ -56,8 +68,10 @@ public class LineService {
     }
 
 
+
     public LineResponse findLineResponseById(Long id) {
         Line persistLine = findLineById(id);
+        //계속 중복되고 있음
         List<StationResponse> stations = getStations(persistLine).stream()
                 .map(it -> StationResponse.of(it))
                 .collect(Collectors.toList());
@@ -78,23 +92,29 @@ public class LineService {
         Station upStation = stationService.findStationById(request.getUpStationId());
         Station downStation = stationService.findStationById(request.getDownStationId());
         List<Station> stations = getStations(line);
+
+        //TODO 도메인으로 옮길것
         boolean isUpStationExisted = stations.stream().anyMatch(it -> it == upStation);
         boolean isDownStationExisted = stations.stream().anyMatch(it -> it == downStation);
 
+        //TODO 도메인으로 옮길것
         if (isUpStationExisted && isDownStationExisted) {
             throw new RuntimeException("이미 등록된 구간 입니다.");
         }
 
+        //TODO 도메인으로 옮길것
         if (!stations.isEmpty() && stations.stream().noneMatch(it -> it == upStation) &&
                 stations.stream().noneMatch(it -> it == downStation)) {
             throw new RuntimeException("등록할 수 없는 구간 입니다.");
         }
 
+        //TODO 도메인으로 옮길것
         if (stations.isEmpty()) {
             line.getSections().add(new Section(line, upStation, downStation, request.getDistance()));
             return;
         }
 
+        //TODO 도메인으로 옮길것
         if (isUpStationExisted) {
             line.getSections().stream()
                     .filter(it -> it.getUpStation() == upStation)
@@ -102,6 +122,8 @@ public class LineService {
                     .ifPresent(it -> it.updateUpStation(downStation, request.getDistance()));
 
             line.getSections().add(new Section(line, upStation, downStation, request.getDistance()));
+
+        //TODO 도메인으로 옮길것
         } else if (isDownStationExisted) {
             line.getSections().stream()
                     .filter(it -> it.getDownStation() == downStation)
@@ -117,10 +139,13 @@ public class LineService {
     public void removeLineStation(Long lineId, Long stationId) {
         Line line = findLineById(lineId);
         Station station = stationService.findStationById(stationId);
+
+        //TODO 도메인으로 옮길것
         if (line.getSections().size() <= 1) {
             throw new RuntimeException();
         }
 
+        //TODO 도메인으로 옮길것
         Optional<Section> upLineStation = line.getSections().stream()
                 .filter(it -> it.getUpStation() == station)
                 .findFirst();
@@ -128,6 +153,7 @@ public class LineService {
                 .filter(it -> it.getDownStation() == station)
                 .findFirst();
 
+        //TODO 도메인으로 옮길것
         if (upLineStation.isPresent() && downLineStation.isPresent()) {
             Station newUpStation = downLineStation.get().getUpStation();
             Station newDownStation = upLineStation.get().getDownStation();
@@ -140,6 +166,7 @@ public class LineService {
     }
 
 
+    //TODO 도메인으로 옮길것
     public List<Station> getStations(Line line) {
         if (line.getSections().isEmpty()) {
             return Arrays.asList();
@@ -164,6 +191,7 @@ public class LineService {
         return stations;
     }
 
+    //TODO 도메인으로 옮길것
     private Station findUpStation(Line line) {
         Station downStation = line.getSections().get(0).getUpStation();
         while (downStation != null) {
