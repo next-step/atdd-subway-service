899293a3c6165d20932e022ac2fd92355ea93dc7
giraffelim <taeyang7704@naver.com>
2022-11-24 23:29:09 +0900

    test(Line): 노선의 지하철 역 제거 테스트 작성

--- src/test/java/nextstep/subway/line/domain/LineTest.java
+++ src/test/java/nextstep/subway/line/domain/LineTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.Test;
 
 class LineTest {
     private Line 역2개_2호선;
+    private Line 역3개_2호선;
     private Station 서초역;
     private Station 강남역;
     private Station 교대역;
@@ -19,6 +20,8 @@ class LineTest {
         교대역 = new Station("교대역");
         강남역 = new Station("강남역");
         역2개_2호선 = Line.builder().upStation(서초역).downStation(강남역).distance(10).build();
+        역3개_2호선 = Line.builder().upStation(서초역).downStation(강남역).distance(10).build();
+        역3개_2호선.addSection(new Section(서초역, 교대역, 5));
     }
 
     @DisplayName("노선에 할당된 역을 조회할 수 있다.")
@@ -27,13 +30,21 @@ class LineTest {
         assertThat(역2개_2호선.findAssignedStations()).containsExactly(서초역, 강남역);
     }
 
+    @DisplayName("구간을 추가하고 노선 조회 시 추가한 역이 조회된다.")
     @Test
-    @DisplayName("구간을 추가하고 노선 조회 시 추가한 역 조회를 성공한다.")
     void addSection() {
         Section newSection = new Section(서초역, 교대역, 5);
 
         역2개_2호선.addSection(newSection);
 
         assertThat(역2개_2호선.findAssignedStations()).containsExactly(서초역, 교대역, 강남역);
+    }
+
+    @DisplayName("지하철 역을 제거하고 노선 조회 시 제거한 역이 조회되지 않는다.")
+    @Test
+    void deleteStation() {
+        역3개_2호선.deleteStation(교대역);
+
+        assertThat(역3개_2호선.findAssignedStations()).containsExactly(서초역, 강남역);
     }
 }
