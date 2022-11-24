d17b73b2066bede939b9da6abcf83a6c4a66537c
giraffelim <taeyang7704@naver.com>
2022-11-24 23:25:26 +0900

    test(Sections): 지하철 역 제거 테스트 작성

--- src/test/java/nextstep/subway/line/domain/SectionsTest.java
+++ src/test/java/nextstep/subway/line/domain/SectionsTest.java
@@ -95,4 +95,61 @@ class SectionsTest {
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("상행역과 하행역 모두 노선에 포함되어 있지 않습니다.");
     }
+
+    @DisplayName("구간의 가운데 역을 제거한 뒤 지하철 역 조회 시 제거된 역이 조회되지 않는다.")
+    @Test
+    void deleteMiddleStation() {
+        Station 교대역 = new Station("교대역");
+        Section newSection = new Section(서초역, 교대역, 5);
+        sections.add(newSection);
+        assertThat(sections.assignedOrderedStation()).containsExactly(서초역, 교대역, 강남역);
+
+        sections.delete(교대역);
+
+        assertThat(sections.assignedOrderedStation()).containsExactly(서초역, 강남역);
+    }
+
+    @DisplayName("노선의 상행 종점을 제거한 뒤 지하철 역 조회 시 제거한 역이 조회되지 않는다.")
+    @Test
+    void deleteStationHeadOfLine() {
+        Station 교대역 = new Station("교대역");
+        Section newSection = new Section(서초역, 교대역, 5);
+        sections.add(newSection);
+        assertThat(sections.assignedOrderedStation()).containsExactly(서초역, 교대역, 강남역);
+
+        sections.delete(서초역);
+
+        assertThat(sections.assignedOrderedStation()).containsExactly(교대역, 강남역);
+    }
+
+    @DisplayName("노선의 하행 종점을 제거한 뒤 지하철 역 조회 시 제거한 역이 조회되지 않는다.")
+    @Test
+    void deleteStationEndOfLine() {
+        Station 교대역 = new Station("교대역");
+        Section newSection = new Section(서초역, 교대역, 5);
+        sections.add(newSection);
+        assertThat(sections.assignedOrderedStation()).containsExactly(서초역, 교대역, 강남역);
+
+        sections.delete(강남역);
+
+        assertThat(sections.assignedOrderedStation()).containsExactly(서초역, 교대역);
+    }
+
+    @DisplayName("하나의 구간만 존재하는 노선의 역을 제거 할 경우 예외가 발생한다.")
+    @Test
+    void deleteStationOneSectionException() {
+        assertThatThrownBy(() -> sections.delete(서초역))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("노선의 마지막 구간은 삭제할 수 없습니다.");
+    }
+
+    @DisplayName("노선에 포함되지 않은 역을 제거하려는 경우 예외가 발생한다.")
+    @Test
+    void deleteStationNotIncludeLine() {
+        Station 성수역 = new Station("성수역");
+
+        assertThatThrownBy(() -> sections.delete(성수역))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("노선에 포함되지 않은 지하철 역은 삭제할 수 없습니다.");
+    }
 }
