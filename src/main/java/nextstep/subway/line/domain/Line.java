a939bc2e78be77dcbc417ec6e6809483d74871f6
giraffelim <taeyang7704@naver.com>
2022-11-24 23:29:21 +0900

    feat(Line): 노선의 지하철 역 제거 기능 추가

--- src/main/java/nextstep/subway/line/domain/Line.java
+++ src/main/java/nextstep/subway/line/domain/Line.java
@@ -69,6 +69,10 @@ public class Line extends BaseEntity {
         this.sections.add(newSection);
     }
 
+    public void deleteStation(Station station) {
+        this.sections.delete(station);
+    }
+
     public static class Builder {
         private String name;
         private int distance;
