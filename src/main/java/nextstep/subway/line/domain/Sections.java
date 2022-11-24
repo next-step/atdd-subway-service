77d843f8f1319895b5c42abbda6e61a3ca908486
giraffelim <taeyang7704@naver.com>
2022-11-24 23:25:58 +0900

    feat(Sections): 지하철 역 제거 기능 추가

--- src/main/java/nextstep/subway/line/domain/Sections.java
+++ src/main/java/nextstep/subway/line/domain/Sections.java
@@ -16,6 +16,9 @@ import nextstep.subway.station.domain.Station;
 public class Sections {
     private static final String DUPLICATE_UP_DOWN_STATIONS = "상행역과 하행역이 이미 모두 노선에 등록되어 있습니다.";
     private static final String NOT_INCLUDE_UP_DOWN_STATIONS = "상행역과 하행역 모두 노선에 포함되어 있지 않습니다.";
+    private static final int ONE_SECTION = 1;
+    private static final String CAN_NOT_DELETE_LAST_SECTION = "노선의 마지막 구간은 삭제할 수 없습니다.";
+    private static final String CAN_NOT_DELETE_NOT_INCLUDED_STATION = "노선에 포함되지 않은 지하철 역은 삭제할 수 없습니다.";
     @OneToMany(mappedBy = "line", cascade = {CascadeType.PERSIST, CascadeType.MERGE}, orphanRemoval = true)
     private List<Section> sections;
 
@@ -86,5 +89,48 @@ public class Sections {
         List<Station> assignedStations = this.assignedStations();
         return newSection.stations().stream()
                 .noneMatch(assignedStations::contains);
+    }
+
+    public void delete(Station station) {
+        validateNotIncludeStation(station);
+        validateIsLastSection();
+        Optional<Section> prevSection = findPrevSection(station);
+        Optional<Section> nextSection = findNextSection(station);
+        if (prevSection.isPresent() && nextSection.isPresent()) {
+            deleteMiddle(prevSection.get(), nextSection.get());
+            return;
+        }
+        nextSection.ifPresent(sections::remove);
+        prevSection.ifPresent(sections::remove);
+    }
+
+    private void validateIsLastSection() {
+        if (sections.size() == ONE_SECTION) {
+            throw new IllegalArgumentException(CAN_NOT_DELETE_LAST_SECTION);
+        }
+    }
+
+    private void validateNotIncludeStation(Station station) {
+        boolean isNotInclude = this.assignedStations().stream().noneMatch(station::equals);
+        if (isNotInclude) {
+            throw new IllegalArgumentException(CAN_NOT_DELETE_NOT_INCLUDED_STATION);
+        }
+    }
+
+    private Optional<Section> findPrevSection(Station station) {
+        return this.sections.stream()
+                .filter(section -> section.isDownStation(station))
+                .findFirst();
+    }
+
+    private Optional<Section> findNextSection(Station station) {
+        return this.sections.stream()
+                .filter(section -> section.isUpStation(station))
+                .findFirst();
+    }
+
+    private void deleteMiddle(Section prevSection, Section nextSection) {
+        prevSection.merge(nextSection);
+        this.sections.remove(nextSection);
     }
 }
