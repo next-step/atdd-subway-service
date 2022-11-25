d59ae5cd42b53999c3c44d3a24c1b3fafc5d6ae4
anwjrrp33 <anwjrrp33@gmail.com>
2022-11-25 23:52:58 +0900

    test: 이름 equals 테스트 코드 작성

--- src/test/java/nextstep/subway/line/domain/NameTest.java
+++ src/test/java/nextstep/subway/line/domain/NameTest.java
@@ -1,8 +1,10 @@
 package nextstep.subway.line.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 
@@ -14,5 +16,14 @@ public class NameTest {
     void validateNullOrEmptyException(String name) {
         assertThatThrownBy(() -> Name.from(name))
             .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("이름이 동일하면 동일한 객체다.")
+    @Test
+    void equals() {
+        Name name1 = Name.from("마곡역");
+        Name name2 = Name.from("마곡역");
+
+        assertThat(name1).isEqualTo(name2);
     }
 }
