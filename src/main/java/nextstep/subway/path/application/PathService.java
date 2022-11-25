5e911b75a600fd713108c34fdfde8cf476606f22
sjk5766 <wjdrnr6662@gmail.com>
2022-11-25 16:49:34 +0900

    feat(service): PathService 추가

--- src/main/java/nextstep/subway/path/application/PathService.java
+++ src/main/java/nextstep/subway/path/application/PathService.java
@@ -1,18 +1,34 @@
 package nextstep.subway.path.application;
 
+import nextstep.subway.line.domain.LineRepository;
+import nextstep.subway.path.domain.PathFinder;
 import nextstep.subway.path.dto.PathResponse;
-import nextstep.subway.station.dto.StationResponse;
+import nextstep.subway.station.domain.Station;
+import nextstep.subway.station.domain.StationRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Service
 @Transactional(readOnly = true)
 public class PathService {
+    private final LineRepository lineRepository;
+    private final StationRepository stationRepository;
+
+    public PathService(LineRepository lineRepository, StationRepository stationRepository) {
+        this.lineRepository = lineRepository;
+        this.stationRepository = stationRepository;
+    }
+
     public PathResponse findShortestPath(Long source, Long target) {
-        List<StationResponse> stations = new ArrayList<>();
-        return new PathResponse(stations,10);
+        Station sourceStation = findStationById(source);
+        Station targetStation = findStationById(target);
+
+        PathFinder pathFinder = new PathFinder(lineRepository.findAll());
+        return pathFinder.shortestPath(sourceStation, targetStation);
+    }
+
+    private Station findStationById(Long id) {
+        return stationRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 역입니다."));
     }
 }
