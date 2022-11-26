16f0f541ba19cfab10bbc001f9fa6eb7c18a7dd5
chunhodong <yhms4432@naver.com>
2022-11-26 21:34:18 +0900

    test: SectionsTest에서 개수검증 테스트추가

--- src/main/java/nextstep/subway/line/domain/Sections.java
+++ src/main/java/nextstep/subway/line/domain/Sections.java
@@ -1,16 +1,24 @@
 package nextstep.subway.line.domain;
 
 import javax.persistence.CascadeType;
+import javax.persistence.Embeddable;
 import javax.persistence.OneToMany;
 import java.util.ArrayList;
 import java.util.List;
 
+@Embeddable
 public class Sections {
-
-
     @OneToMany(mappedBy = "line", cascade = {CascadeType.PERSIST, CascadeType.MERGE}, orphanRemoval = true)
     private List<Section> sections = new ArrayList<>();
 
-    protected Sections() {
+    public Sections() {
+    }
+
+    public int size(){
+        return sections.size();
+    }
+
+    public void addSection(Section section){
+        sections.add(section);
     }
 }
