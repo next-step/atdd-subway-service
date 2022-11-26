5af479019a37950df96c9fe0e744f06fd7fd43c2
ilmare-cbk <cbk104325@gmail.com>
2022-11-26 11:13:12 +0900

    refactor(LineService.java): findLineResponseById 메소드 리팩토링
    - Line 의 getStations 메소드를 사용해서 응답하도록 수정

--- src/main/java/nextstep/subway/line/application/LineService.java
+++ src/main/java/nextstep/subway/line/application/LineService.java
@@ -50,10 +50,7 @@ public class LineService {
 
     public LineResponse findLineResponseById(Long id) {
         Line persistLine = findLineById(id);
-        List<StationResponse> stations = getStations(persistLine).stream()
-                .map(it -> StationResponse.of(it))
-                .collect(Collectors.toList());
-        return LineResponse.of(persistLine, stations);
+        return LineResponse.from(persistLine);
     }
 
     public void updateLine(Long id, LineRequest lineUpdateRequest) {
