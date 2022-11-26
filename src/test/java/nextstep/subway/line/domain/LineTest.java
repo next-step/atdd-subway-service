3b38c9c7b19ff61689dee639f2820141107bc0bb
jimbaemon <jimbae@kakao.com>
2022-11-26 21:28:39 +0900

    test: 구간 추가를 위한 단위테스트 추가.

--- src/test/java/nextstep/subway/line/domain/LineTest.java
+++ src/test/java/nextstep/subway/line/domain/LineTest.java
@@ -14,6 +14,7 @@ class LineTest {
     private final Station 강남역 = new Station("강남역");
     private final Station 역삼역 = new Station("역삼역");
     private final Station 선릉역 = new Station("선릉역");
+    private final Station 블루보틀역 = new Station("블루보틀역");
 
     @Test
     void 정류장_조회() {
@@ -54,6 +55,43 @@ class LineTest {
 
         //then
         assertThat(이호선.getStations()).isEqualTo(Arrays.asList(강남역, 선릉역));
+    }
+
+    @Test
+    void 구간_추가() {
+        //given
+        Line 이호선 = new Line("이호선", "green");
+
+        //when
+        line.addSection(new Section(강남역, 역삼역));
+        line.addSection(new Section(역삼역, 선릉역));
+
+        //then
+        assertThat(이호선.getStations()).isEqualTo(Arrays.asList(강남역, 역삼역, 선릉역));
+    }
+
+    @Test
+    void 동일_구간이_이미_존재하면_오류발생() {
+        //given
+        Line 이호선 = new Line("이호선", "green");
+        line.addSection(new Section(강남역, 역삼역));
+
+        //when & then
+        assertThatThrownBy(() -> line.addSection(new Section(강남역, 역삼역)))
+            .isInstanceOf(RuntimeException.class)
+            .hasMessage("이미 등록된 구간 입니다.");
+    }
+
+    @Test
+    void 구간내_상행선과_하행선중_동일한_항목이_없다면_등록_불가능() {
+        //given
+        Line 이호선 = new Line("이호선", "green");
+        line.addSection(new Section(강남역, 역삼역));
+
+        //when & then
+        assertThatThrownBy(() -> line.addSection(new Section(선릉역, 블루보틀역)))
+            .isInstanceOf(RuntimeException.class)
+            .hasMessage("등록할 수 없는 구간 입니다.");
     }
 
 
