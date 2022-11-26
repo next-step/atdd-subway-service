3d827504a55d44edbb41a07e81c5ead25f8cccff
anwjrrp33 <anwjrrp33@gmail.com>
2022-11-27 00:06:36 +0900

    feat: 정적 팩토리 메서드

--- src/main/java/nextstep/subway/line/dto/LineResponse.java
+++ src/main/java/nextstep/subway/line/dto/LineResponse.java
@@ -1,5 +1,6 @@
 package nextstep.subway.line.dto;
 
+import java.util.stream.Collectors;
 import nextstep.subway.line.domain.Line;
 import nextstep.subway.station.dto.StationResponse;
 
@@ -17,7 +18,7 @@ public class LineResponse {
     public LineResponse() {
     }
 
-    public LineResponse(Long id, String name, String color, List<StationResponse> stations, LocalDateTime createdDate, LocalDateTime modifiedDate) {
+    private LineResponse(Long id, String name, String color, List<StationResponse> stations, LocalDateTime createdDate, LocalDateTime modifiedDate) {
         this.id = id;
         this.name = name;
         this.color = color;
@@ -26,8 +27,19 @@ public class LineResponse {
         this.modifiedDate = modifiedDate;
     }
 
+    public static LineResponse from(Line line) {
+        return new LineResponse(line.getId(),
+            line.getName().value(),
+            line.getColor().value(),
+            line.getStations().stream()
+                .map(StationResponse::of)
+                .collect(Collectors.toList()),
+            line.getCreatedDate(),
+            line.getModifiedDate());
+    }
+
     public static LineResponse of(Line line, List<StationResponse> stations) {
-        return new LineResponse(line.getId(), line.getName().value(), line.getColor(), stations, line.getCreatedDate(), line.getModifiedDate());
+        return new LineResponse(line.getId(), line.getName().value(), line.getColor().value(), stations, line.getCreatedDate(), line.getModifiedDate());
     }
 
     public Long getId() {
