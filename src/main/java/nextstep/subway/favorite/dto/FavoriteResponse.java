f02c6a6877515a9b0d9cc9ac6c3335bb6f200773
earth-h <earth.h.183@gmail.com>
2022-11-26 22:07:17 +0900

    fix(dto): FavoriteResponse의 id String이 아닌 Long으로 수정

--- src/main/java/nextstep/subway/favorite/dto/FavoriteResponse.java
+++ src/main/java/nextstep/subway/favorite/dto/FavoriteResponse.java
@@ -1,23 +1,28 @@
 package nextstep.subway.favorite.dto;
 
+import nextstep.subway.favorite.domain.Favorite;
 import nextstep.subway.station.dto.StationResponse;
 
 public class FavoriteResponse {
 
-    String id;
+    Long id;
     StationResponse source;
     StationResponse target;
 
     private FavoriteResponse() {
     }
 
-    public FavoriteResponse(String id, StationResponse source, StationResponse target) {
+    public FavoriteResponse(Long id, StationResponse source, StationResponse target) {
         this.id = id;
         this.source = source;
         this.target = target;
     }
 
-    public String getId() {
+    public static FavoriteResponse from(Favorite favorite) {
+        return new FavoriteResponse(favorite.getId(), StationResponse.from(favorite.getSourceStation()), StationResponse.from(favorite.getTargetStation()));
+    }
+
+    public Long getId() {
         return id;
     }
 
