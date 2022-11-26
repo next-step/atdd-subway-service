d91e16c1727af728e59bc6e791f57e47c86f8814
crongcm <ucm5459@kakao.com>
2022-11-27 01:32:56 +0900

    feat: Exception 공통처리

---
+++ src/main/java/nextstep/subway/common/ExceptionAdvice.java
@@ -0,0 +1,19 @@
+package nextstep.subway.common;
+
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ExceptionAdvice {
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<Void> handleIllegalArgsException() {
+        return ResponseEntity.badRequest().build();
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<Void> handleIllegalArgumentException() {
+        return ResponseEntity.badRequest().build();
+    }
+}
