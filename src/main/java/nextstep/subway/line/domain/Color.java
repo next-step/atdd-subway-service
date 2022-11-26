a934b7a53aa88a1a94f7f472893bab650940712d
anwjrrp33 <anwjrrp33@gmail.com>
2022-11-26 12:48:57 +0900

    test: 색상 테스트 코드 추가

---
+++ src/main/java/nextstep/subway/line/domain/Color.java
@@ -0,0 +1,32 @@
+package nextstep.subway.line.domain;
+
+import java.util.Objects;
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+@Embeddable
+public class Color {
+
+    private static final String ROUTE_COLOR_ERROR = "노선 색상을 지정해주세요.";
+
+    @Column(unique = true)
+    private String color;
+
+    protected Color() {
+    }
+
+    private Color(String color) {
+        this.color = color;
+    }
+
+    public static Color from(String color) {
+        if (Objects.isNull(color) || color.trim().isEmpty()) {
+            throw new IllegalArgumentException(ROUTE_COLOR_ERROR);
+        }
+        return new Color(color);
+    }
+
+    public String value() {
+        return color;
+    }
+}
