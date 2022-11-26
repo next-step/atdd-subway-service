ed7a9454dec8e0254d16a92cc723cc1c348d47fd
earth-h <earth.h.183@gmail.com>
2022-11-26 18:50:01 +0900

    refactor(test): accessToken이 잘못되었을 때 정보 조회 안됨 테스트 추가 및 기존 테스트 리팩토링

--- src/test/java/nextstep/subway/auth/acceptance/AuthAcceptanceTest.java
+++ src/test/java/nextstep/subway/auth/acceptance/AuthAcceptanceTest.java
@@ -1,6 +1,7 @@
 package nextstep.subway.auth.acceptance;
 
 import static nextstep.subway.auth.acceptance.AuthRestAssured.로그인_요청;
+import static nextstep.subway.member.acceptance.MemberRestAssured.내_정보_조회_요청;
 import static nextstep.subway.member.acceptance.MemberRestAssured.회원_생성되어_있음;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -43,48 +44,66 @@ public class AuthAcceptanceTest extends AcceptanceTest {
     /**
      * Feature: 로그인 기능
      *
+     *   Background
+     *     Given 정상회원 생성되어 있음
+     *     And 비정상회원 생성되어 있지 않음
+     *
      *   Scenario: 로그인을 시도한다.
-     *     Given 정상회원 등록되어 있음
-     *     And 비정상회원 등록되어 있지 않음
-     *     When 정상회원 로그인 요청
-     *     Then 정상회원 로그인 됨
      *     When 비정상회원 로그인 요청
      *     Then 비정상회원 로그인 안됨
+     *     When 정상회원 이메일 틀리게 로그인 요청
+     *     Then 정상회원 로그인 안됨
+     *     When 정상회원 비밀번호 틀리게 로그인 요청
+     *     Then 정상회원 로그인 안됨
+     *     When 정상회원 이메일, 비밀번호 올바르게 로그인 요청
+     *     Then 정상회원 로그인 됨
      */
     @DisplayName("로그인 기능")
     @TestFactory
     Collection<DynamicTest> loginSenario() {
         return Arrays.asList(
-                DynamicTest.dynamicTest("존재하는 회원이 로그인을 시도하면 로그인이 된다.", () -> {
-                    // when
-                    ExtractableResponse<Response> response = 로그인_요청(정상회원_로그인_요청);
-
-                    // then
-                    로그인됨(response);
-                }),
                 DynamicTest.dynamicTest("존재하지 않는 회원이 로그인을 시도하면 로그인이 안된다.", () -> {
                     // when
                     ExtractableResponse<Response> response = 로그인_요청(비정상회원_로그인_요청);
 
                     // then
                     로그인_안됨(response);
+                }),
+                DynamicTest.dynamicTest("존재하는 회원이 이메일 틀리게 로그인을 시도하면 로그인이 안된다.", () -> {
+                    // when
+                    ExtractableResponse<Response> response = 로그인_요청(new TokenRequest(WRONG_EMAIL, PASSWORD));
+
+                    // then
+                    로그인_안됨(response);
+                }),
+                DynamicTest.dynamicTest("존재하는 회원이 비밀번호 틀리게 로그인을 시도하면 로그인이 안된다.", () -> {
+                    // when
+                    ExtractableResponse<Response> response = 로그인_요청(new TokenRequest(EMAIL, WRONG_PASSWORD));
+
+                    // then
+                    로그인_안됨(response);
+                }),
+                DynamicTest.dynamicTest("존재하는 회원이 올바른 이메일과 비밀번호로 로그인을 시도하면 로그인이 된다.", () -> {
+                    // when
+                    ExtractableResponse<Response> response = 로그인_요청(정상회원_로그인_요청);
+
+                    // then
+                    로그인됨(response);
                 })
         );
     }
 
-    @DisplayName("Bearer Auth")
-    @Test
-    void myInfoWithBearerAuth() {
-    }
-
-    @DisplayName("Bearer Auth 로그인 실패")
-    @Test
-    void myInfoWithBadBearerAuth() {
-    }
-
-    @DisplayName("Bearer Auth 유효하지 않은 토큰")
+    @DisplayName("Bearer Auth가 유효하지 않은 토큰이면 내 정보 조회할 수 없다.")
     @Test
     void myInfoWithWrongBearerAuth() {
+        // given
+        String wrongAccessToken = "wrongAccessToken";
+
+        // when
+        ExtractableResponse<Response> response = 내_정보_조회_요청(wrongAccessToken);
+
+        // then
+        내_정보_조회_안됨(response);
     }
 
     private static void 로그인됨(ExtractableResponse<Response> response) {
@@ -93,5 +112,9 @@ public class AuthAcceptanceTest extends AcceptanceTest {
 
     private static void 로그인_안됨(ExtractableResponse<Response> response) {
         assertThat(response.statusCode()).isEqualTo(HttpStatus.UNAUTHORIZED.value());
+    }
+
+    private static void 내_정보_조회_안됨(ExtractableResponse<Response> response) {
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
     }
 }
