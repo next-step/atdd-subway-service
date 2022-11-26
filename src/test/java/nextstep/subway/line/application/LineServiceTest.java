61613db0a5111e5312a5674d29466a742e08bb7c
ilmare-cbk <cbk104325@gmail.com>
2022-11-26 12:01:56 +0900

    refactor(LineServiceTest.java): 테스트마다 반복 사용되는 필드를 @BeforeEach를 통해 사용하도록 수정

--- src/test/java/nextstep/subway/line/application/LineServiceTest.java
+++ src/test/java/nextstep/subway/line/application/LineServiceTest.java
@@ -6,6 +6,7 @@ import nextstep.subway.line.dto.LineRequest;
 import nextstep.subway.line.dto.LineResponse;
 import nextstep.subway.station.application.StationService;
 import nextstep.subway.station.domain.Station;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -35,17 +36,30 @@ class LineServiceTest {
     @InjectMocks
     private LineService lineService;
 
+    private Station 강남역;
+    private Station 광교역;
+    private Station 죽전역;
+    private Station 수원역;
+    private Line 신분당선;
+    private Line 분당선;
+
+    @BeforeEach
+    void setUp() {
+        강남역 = new Station("강남역");
+        광교역 = new Station("광교역");
+        죽전역 = new Station("죽전역");
+        수원역 = new Station("수원역");
+        신분당선 = new Line("신분당선", "red", 강남역, 광교역, 10);
+        분당선 = new Line("분당선", "yellow", 죽전역, 수원역, 10);
+    }
+
     @DisplayName("지하철 노선 생성 후 요청한 노선이름, 색상, 지하철역 갯수를 확인한다.")
     @Test
     void saveLine() {
         // given
-        Station 강남역 = new Station("강남역");
-        Station 광교역 = new Station("광교역");
         when(stationService.findById(any())).thenReturn(강남역);
         when(stationService.findById(any())).thenReturn(광교역);
-
-        Line line = new Line("신분당선", "red", 강남역, 광교역, 10);
-        when(lineRepository.save(any())).thenReturn(line);
+        when(lineRepository.save(any())).thenReturn(신분당선);
 
         // when
         LineRequest request = new LineRequest("신분당선", "red", 1L, 2L, 10);
@@ -62,12 +76,7 @@ class LineServiceTest {
     @DisplayName("지하철 노선 생성 후 지하철 노선 목록 조회 결과 확인")
     @Test
     void findLines() {
-        when(lineRepository.findAll()).thenReturn(
-                Arrays.asList(
-                        new Line("신분당선", "red", new Station("강남역"), new Station("광교역"), 10),
-                        new Line("분당선", "yellow", new Station("죽전역"), new Station("수원역"), 10)
-                )
-        );
+        when(lineRepository.findAll()).thenReturn(Arrays.asList(신분당선, 분당선));
 
         List<LineResponse> lines = lineService.findLines();
 
@@ -81,10 +90,7 @@ class LineServiceTest {
     @DisplayName("조회한 지하철 노선의 이름, 색상, 지하철 역 갯수를 확인한다.")
     @Test
     void findLineResponseById() {
-        when(lineRepository.findById(any()))
-                .thenReturn(
-                        Optional.of(new Line("신분당선", "red", new Station("강남역"), new Station("광교역"), 10))
-                );
+        when(lineRepository.findById(any())).thenReturn(Optional.of(신분당선));
 
         LineResponse response = lineService.findLineResponseById(any());
 
@@ -98,14 +104,13 @@ class LineServiceTest {
     @DisplayName("지하철 노선의 이름과 색상 변경 시 정상적으로 변경됨을 확인한다.")
     @Test
     void updateLine() {
-        Line line = new Line("신분당선", "red", new Station("강남역"), new Station("광교역"), 10);
-        when(lineRepository.findById(any())).thenReturn(Optional.of(line));
+        when(lineRepository.findById(any())).thenReturn(Optional.of(신분당선));
 
         lineService.updateLine(1L, new LineRequest("분당선", "yellow", 1L, 2L, 10));
 
         assertAll(
-                () -> assertThat(line.getName()).isEqualTo("분당선"),
-                () -> assertThat(line.getColor()).isEqualTo("yellow")
+                () -> assertThat(신분당선.getName()).isEqualTo("분당선"),
+                () -> assertThat(신분당선.getColor()).isEqualTo("yellow")
         );
     }
 }
