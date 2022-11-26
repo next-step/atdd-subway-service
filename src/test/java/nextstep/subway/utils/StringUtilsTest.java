704a2b448112a85dd2960f879bd2b458f0feeaea
earth-h <earth.h.183@gmail.com>
2022-11-26 22:47:28 +0900

    feat(utils): String to Long 메소드 생성
    StringUtils
    - Long.parseLong() 사용 시, exception을 IllegalArgumentException으로 감싸서 Error 내용을 구분하고자 메소드 생성

--- src/test/java/nextstep/subway/utils/StringUtilsTest.java
+++ src/test/java/nextstep/subway/utils/StringUtilsTest.java
@@ -1,8 +1,10 @@
 package nextstep.subway.utils;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
+import nextstep.subway.common.constant.ErrorCode;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -18,5 +20,14 @@ public class StringUtilsTest {
                 () -> assertThat(StringUtils.isNullOrEmpty("")).isTrue(),
                 () -> assertThat(StringUtils.isNullOrEmpty("abc")).isFalse()
         );
+    }
+
+    @DisplayName("문자열이 정수가 아니면 Long 형태로 변환할 수 없다.")
+    @Test
+    void ifStringIsNotNumberThenThrowException() {
+        // when & then
+        assertThatThrownBy(() -> StringUtils.stringToLong("텍"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ErrorCode.숫자가_될_수_있는_문자열.getErrorMessage());
     }
 }
