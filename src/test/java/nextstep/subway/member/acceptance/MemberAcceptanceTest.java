13bd65f3394a8b27987278663b4e3e63973d0ba0
earth-h <earth.h.183@gmail.com>
2022-11-26 18:40:10 +0900

    test(acceptance): 내 정보 조회/수정/삭제 관련 인수 테스트 작성
    MemberAcceptanceTest
    - manageMyInfo 인수 테스트 작성
      - 정상적인 token을 가진 경우와 아닌 경우를 나누어 조회/수정/삭제 테스트
    
    MemberRestAssured
    - RestAssured 메소드 호출 시, authorization 헤더에 accessToken 바로 넣어 테스트할 수 있도록 구성

--- src/test/java/nextstep/subway/member/acceptance/MemberAcceptanceTest.java
+++ src/test/java/nextstep/subway/member/acceptance/MemberAcceptanceTest.java
@@ -1,21 +1,33 @@
 package nextstep.subway.member.acceptance;
 
+import static nextstep.subway.auth.acceptance.AuthRestAssured.로그인_되어_있음;
+import static nextstep.subway.member.acceptance.MemberRestAssured.내_정보_삭제_요청;
+import static nextstep.subway.member.acceptance.MemberRestAssured.내_정보_수정_요청;
+import static nextstep.subway.member.acceptance.MemberRestAssured.내_정보_조회_요청;
 import static nextstep.subway.member.acceptance.MemberRestAssured.회원_삭제_요청;
+import static nextstep.subway.member.acceptance.MemberRestAssured.회원_생성되어_있음;
 import static nextstep.subway.member.acceptance.MemberRestAssured.회원_생성을_요청;
 import static nextstep.subway.member.acceptance.MemberRestAssured.회원_정보_수정_요청;
 import static nextstep.subway.member.acceptance.MemberRestAssured.회원_정보_조회_요청;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
+import java.util.Arrays;
+import java.util.Collection;
 import nextstep.subway.AcceptanceTest;
+import nextstep.subway.auth.dto.TokenRequest;
 import nextstep.subway.member.dto.MemberRequest;
 import nextstep.subway.member.dto.MemberResponse;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DynamicTest;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestFactory;
 import org.springframework.http.HttpStatus;
 
+@DisplayName("회원 관련 기능")
 public class MemberAcceptanceTest extends AcceptanceTest {
     private static final String EMAIL = "email@email.com";
     private static final String PASSWORD = "password";
@@ -54,10 +66,80 @@ public class MemberAcceptanceTest extends AcceptanceTest {
         회원_삭제됨(deleteResponse);
     }
 
-    @DisplayName("나의 정보를 관리한다.")
-    @Test
-    void manageMyInfo() {
+    /**
+     * Feature: 내 정보 관련 기능
+     *
+     *   Background
+     *     Given 정상회원 생성되어 있음
+     *     And 비정상회원 생성되어 있지 않음
+     *
+     *   Scenario: 내 정보를 관리
+     *     When 정상회원이 자신의 정보 조회 요청
+     *     Then 자신의 정보 조회됨
+     *     When 정상회원이 자신의 정보 수정 요청
+     *     Then 자신의 정보 수정됨
+     *     When 정상회원이 자신의 정보 삭제 요청
+     *     Then 자신의 정보 삭제됨
+     *     When 유효하지 않은 토큰을 가진 비정상회원이 자신의 정보 조회 요청
+     *     Then 자신의 정보 조회 실패됨
+     *     When 유효하지 않은 토큰을 가진 비정상회원이 자신의 정보 수정 요청
+     *     Then 자신의 정보 수정 실패됨
+     *     When 유효하지 않은 토큰을 가진 비정상회원이 자신의 정보 삭제 요청
+     *     Then 자신의 정보 삭제 실패됨
+     */
+    @DisplayName("내 정보 관리")
+    @TestFactory
+    Collection<DynamicTest> manageMyInfo() {
+        // Background
+        회원_생성되어_있음(정상회원_등록_요청);
+        ExtractableResponse<Response> loginResponse = 로그인_되어_있음(new TokenRequest(정상회원_등록_요청.getEmail(), 정상회원_등록_요청.getPassword()));
+        String accessToken = loginResponse.jsonPath().getString("accessToken");
+        String wrongAccessToken = "wrongAccessToken";
 
+        return Arrays.asList(
+                DynamicTest.dynamicTest("정상회원은 자신의 정보를 조회할 수 있다.", () -> {
+                    // when
+                    ExtractableResponse<Response> response = 내_정보_조회_요청(accessToken);
+
+                    // then
+                    내_정보_조회됨(response);
+                }),
+                DynamicTest.dynamicTest("정상회원은 자신의 정보를 수정할 수 있다.", () -> {
+                    // when
+                    ExtractableResponse<Response> response = 내_정보_수정_요청(accessToken, new MemberRequest(NEW_EMAIL, NEW_PASSWORD, NEW_AGE));
+
+                    // then
+                    내_정보_수정됨(response);
+                }),
+                DynamicTest.dynamicTest("정상회원은 자신의 정보를 삭제할 수 있다.", () -> {
+                    // when
+                    ExtractableResponse<Response> response = 내_정보_삭제_요청(accessToken);
+
+                    // then
+                    내_정보_삭제됨(response);
+                }),
+                DynamicTest.dynamicTest("유효하지 않은 토큰을 가진 비정상회원은 자신의 정보를 조회할 수 없다.", () -> {
+                    // when
+                    ExtractableResponse<Response> response = 내_정보_조회_요청(wrongAccessToken);
+
+                    // then
+                    내_정보_조회_안됨(response);
+                }),
+                DynamicTest.dynamicTest("유효하지 않은 토큰을 가진 비정상회원은 자신의 정보를 수정할 수 없다.", () -> {
+                    // when
+                    ExtractableResponse<Response> response = 내_정보_수정_요청(wrongAccessToken, new MemberRequest(NEW_EMAIL, NEW_PASSWORD, NEW_AGE));
+
+                    // then
+                    내_정보_수정_안됨(response);
+                }),
+                DynamicTest.dynamicTest("유효하지 않은 토큰을 가진 비정상회원은 자신의 정보를 삭제할 수 없다.", () -> {
+                    // when
+                    ExtractableResponse<Response> response = 내_정보_삭제_요청(wrongAccessToken);
+
+                    // then
+                    내_정보_삭제_안됨(response);
+                })
+        );
     }
 
     private static void 회원_생성됨(ExtractableResponse<Response> response) {
@@ -66,9 +148,11 @@ public class MemberAcceptanceTest extends AcceptanceTest {
 
     private static void 회원_정보_조회됨(ExtractableResponse<Response> response, String email, int age) {
         MemberResponse memberResponse = response.as(MemberResponse.class);
-        assertThat(memberResponse.getId()).isNotNull();
-        assertThat(memberResponse.getEmail()).isEqualTo(email);
-        assertThat(memberResponse.getAge()).isEqualTo(age);
+        assertAll(
+                () -> assertThat(memberResponse.getId()).isNotNull(),
+                () -> assertThat(memberResponse.getEmail()).isEqualTo(email),
+                () -> assertThat(memberResponse.getAge()).isEqualTo(age)
+        );
     }
 
     private static void 회원_정보_수정됨(ExtractableResponse<Response> response) {
@@ -77,5 +161,29 @@ public class MemberAcceptanceTest extends AcceptanceTest {
 
     private static void 회원_삭제됨(ExtractableResponse<Response> response) {
         assertThat(response.statusCode()).isEqualTo(HttpStatus.NO_CONTENT.value());
+    }
+
+    private static void 내_정보_조회됨(ExtractableResponse<Response> response) {
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+    }
+
+    private static void 내_정보_수정됨(ExtractableResponse<Response> response) {
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+    }
+
+    private static void 내_정보_삭제됨(ExtractableResponse<Response> response) {
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.NO_CONTENT.value());
+    }
+
+    private static void 내_정보_조회_안됨(ExtractableResponse<Response> response) {
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+    }
+
+    private static void 내_정보_수정_안됨(ExtractableResponse<Response> response) {
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+    }
+
+    private static void 내_정보_삭제_안됨(ExtractableResponse<Response> response) {
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
     }
 }
