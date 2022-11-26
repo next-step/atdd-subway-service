d00e49f81ba12e2ad958821d5393ef81874f764b
giraffelim <taeyang7704@naver.com>
2022-11-27 00:03:57 +0900

    feat(PathFinder): 출발역과 도착역이 연결되어 있는지 검증하는 기능 추가

--- src/main/java/nextstep/subway/path/domain/PathFinder.java
+++ src/main/java/nextstep/subway/path/domain/PathFinder.java
@@ -11,6 +11,7 @@ import org.jgrapht.graph.WeightedMultigraph;
 
 public class PathFinder {
     private static final String CAN_NOT_FIND_PATH_WHEN_SOURCE_TARGET_IS_SAME = "출발역과 도착역이 동일한 경우 경로를 조회할 수 없습니다.";
+    private static final String CAN_NOT_FIND_WHEN_SOURCE_TARGET_IS_NOT_CONNECT = "출발역과 도착역이 연결되어 있지 않은 경우 경로를 조회할 수 없습니다.";
     private final WeightedMultigraph<Station, DefaultWeightedEdge> graph;
 
     public PathFinder(List<Line> lines) {
@@ -44,6 +45,7 @@ public class PathFinder {
         validateSourceAndTargetIsSame(source, target);
         DijkstraShortestPath<Station, DefaultWeightedEdge> dijkstraShortestPath = new DijkstraShortestPath<>(graph);
         GraphPath<Station, DefaultWeightedEdge> stationShortedPath = dijkstraShortestPath.getPath(source, target);
+        validateSourceAndTargetIsConnect(stationShortedPath);
         List<Station> stations = stationShortedPath.getVertexList();
         double totalDistance = stationShortedPath.getWeight();
 
@@ -53,6 +55,12 @@ public class PathFinder {
     private void validateSourceAndTargetIsSame(Station source, Station target) {
         if (source.equals(target)) {
             throw new IllegalArgumentException(CAN_NOT_FIND_PATH_WHEN_SOURCE_TARGET_IS_SAME);
+        }
+    }
+
+    private void validateSourceAndTargetIsConnect(GraphPath<Station, DefaultWeightedEdge> stationShortedPath) {
+        if (stationShortedPath == null) {
+            throw new IllegalArgumentException(CAN_NOT_FIND_WHEN_SOURCE_TARGET_IS_NOT_CONNECT);
         }
     }
 }
