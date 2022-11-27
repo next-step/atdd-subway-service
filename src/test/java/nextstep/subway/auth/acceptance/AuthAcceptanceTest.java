c80550b721e1d13263580bc6c45488bd3dfee885
ilmare-cbk <cbk104325@gmail.com>
2022-11-27 23:36:31 +0900

    test(AuthAcceptanceTest.java): 등록된 회원 정보로 로그인 성공 테스트 작성

--- src/test/java/nextstep/subway/auth/acceptance/AuthAcceptanceTest.java
+++ src/test/java/nextstep/subway/auth/acceptance/AuthAcceptanceTest.java
@@ -1,14 +1,45 @@
 package nextstep.subway.auth.acceptance;
 
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
 import nextstep.subway.AcceptanceTest;
+import nextstep.subway.auth.dto.TokenRequest;
+import nextstep.subway.auth.dto.TokenResponse;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 
-public class AuthAcceptanceTest extends AcceptanceTest {
+import static nextstep.subway.member.MemberAcceptanceTest.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+/**
+ * Feature: 로그인 기능
+ *
+ *   Scenario: 로그인을 시도한다.
+ *     Given 회원 등록되어 있음
+ *     When 로그인 요청
+ *     Then 로그인 됨
+ */
+class AuthAcceptanceTest extends AcceptanceTest {
+
+    @Override
+    @BeforeEach
+    public void setUp() {
+        super.setUp();
+
+        회원_생성을_요청(EMAIL, PASSWORD, AGE);
+    }
 
     @DisplayName("Bearer Auth")
     @Test
     void myInfoWithBearerAuth() {
+        ExtractableResponse<Response> response = 로그인_요청(new TokenRequest(EMAIL, PASSWORD));
+
+        로그인_됨(response);
     }
 
     @DisplayName("Bearer Auth 로그인 실패")
@@ -21,4 +52,20 @@ public class AuthAcceptanceTest extends AcceptanceTest {
     void myInfoWithWrongBearerAuth() {
     }
 
+    private ExtractableResponse<Response> 로그인_요청(TokenRequest tokenRequest) {
+        return RestAssured
+                .given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(tokenRequest)
+                .when().post("/login/token")
+                .then().log().all()
+                .extract();
+    }
+
+    private void 로그인_됨(ExtractableResponse<Response> response) {
+        assertAll(
+                () -> assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value()),
+                () -> assertThat(response.as(TokenResponse.class).getAccessToken()).isNotBlank()
+        );
+    }
 }
