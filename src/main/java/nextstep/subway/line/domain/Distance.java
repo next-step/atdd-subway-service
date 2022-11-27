902e9be80e7aee0326e376f9d7a4cc3f02317997
crongcm <ucm5459@kakao.com>
2022-11-27 19:21:11 +0900

    refactor: 거리 빼기 유효성 검사 분기처리

--- src/main/java/nextstep/subway/line/domain/Distance.java
+++ src/main/java/nextstep/subway/line/domain/Distance.java
@@ -8,7 +8,7 @@ import javax.persistence.Embeddable;
 public class Distance {
     private static final String ERROR_MESSAGE_NOT_NULL_DISTANCE = "거리는 필수입니다.";
     private static final String ERROR_MESSAGE_GRATER_THAN_ZERO_DISTANCE = "거리는 0보다 커야합니다.";
-    public static final String ERROR_MESSAGE_VALID_DISTANCE = "기존 노선의 거리보다 작거나 같을 수 없습니다.";
+    public static final String ERROR_MESSAGE_VALID_SECTION_DISTANCE = "기존 노선의 거리보다 작거나 같을 수 없습니다.";
     private static final int INVALID_DISTANCE_CRITERION = 0;
 
     @Column(nullable = false)
@@ -41,10 +41,14 @@ public class Distance {
     }
 
     public Distance subtract(Distance distance) {
-        try {
-            return Distance.from(this.distance - distance.distance);
-        } catch (IllegalArgumentException e) {
-            throw new IllegalArgumentException(ERROR_MESSAGE_VALID_DISTANCE);
+        int subtractDistance = this.distance - distance.distance;
+        validSectionDistance(subtractDistance);
+        return Distance.from(subtractDistance);
+    }
+
+    private static void validSectionDistance(int subtract) {
+        if (subtract <= INVALID_DISTANCE_CRITERION) {
+            throw new IllegalArgumentException(ERROR_MESSAGE_VALID_SECTION_DISTANCE);
         }
     }
 
