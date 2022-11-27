747d8ef204ea351078aa75de2634c946f22e824f
giraffelim <taeyang7704@naver.com>
2022-11-28 00:08:19 +0900

    feat(FavoriteController, FavoriteService, FavoriteRepository): 즐겨찾기 목록 조회 기능 추가

--- src/main/java/nextstep/subway/favorite/application/FavoriteService.java
+++ src/main/java/nextstep/subway/favorite/application/FavoriteService.java
@@ -1,5 +1,7 @@
 package nextstep.subway.favorite.application;
 
+import java.util.List;
+import java.util.stream.Collectors;
 import javax.persistence.EntityNotFoundException;
 import nextstep.subway.favorite.domain.Favorite;
 import nextstep.subway.favorite.domain.FavoriteRepository;
@@ -30,13 +32,20 @@ public class FavoriteService {
     }
 
     @Transactional
-    public FavoriteResponse saveFavorite(Long id, FavoriteRequest favoriteRequest) {
-        Member member = findMember(id);
+    public FavoriteResponse saveFavorite(Long memberId, FavoriteRequest favoriteRequest) {
+        Member member = findMember(memberId);
         Station source = findStation(favoriteRequest.getSource());
         Station target = findStation(favoriteRequest.getTarget());
         Favorite favorite = new Favorite(member, source, target);
         Favorite savedFavorite = favoriteRepository.save(favorite);
         return FavoriteResponse.from(savedFavorite);
+    }
+
+    public List<FavoriteResponse> findFavorites(Long memberId) {
+        List<Favorite> favorites = favoriteRepository.findAllByMemberId(memberId);
+        return favorites.stream()
+                .map(FavoriteResponse::from)
+                .collect(Collectors.toList());
     }
 
     private Member findMember(Long id) {
--- src/main/java/nextstep/subway/favorite/ui/FavoriteController.java
+++ src/main/java/nextstep/subway/favorite/ui/FavoriteController.java
@@ -1,6 +1,7 @@
 package nextstep.subway.favorite.ui;
 
 import java.net.URI;
+import java.util.List;
 import nextstep.subway.auth.domain.AuthenticationPrincipal;
 import nextstep.subway.auth.domain.LoginMember;
 import nextstep.subway.favorite.application.FavoriteService;
@@ -8,6 +9,7 @@ import nextstep.subway.favorite.dto.FavoriteRequest;
 import nextstep.subway.favorite.dto.FavoriteResponse;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,5 +31,11 @@ public class FavoriteController {
     ) {
         FavoriteResponse response = favoriteService.saveFavorite(loginMember.getId(), favoriteRequest);
         return ResponseEntity.created(URI.create("/favorites/" + response.getId())).body(response);
+    }
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<FavoriteResponse>> findFavorites(@AuthenticationPrincipal LoginMember loginMember) {
+        List<FavoriteResponse> response = favoriteService.findFavorites(loginMember.getId());
+        return ResponseEntity.ok().body(response);
     }
 }
