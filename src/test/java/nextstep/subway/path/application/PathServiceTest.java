293c492a956c2d29974f61353bcbc64db5ae6711
ilmare-cbk <cbk104325@gmail.com>
2022-11-27 19:14:28 +0900

    test(PathServiceTest.java): findShortestPath 테스트 작성
    - Mockito를 활용한 stub 행위 정의

---
+++ src/test/java/nextstep/subway/path/application/PathServiceTest.java
@@ -0,0 +1,83 @@
+package nextstep.subway.path.application;
+
+import nextstep.subway.line.domain.Line;
+import nextstep.subway.line.domain.LineRepository;
+import nextstep.subway.line.domain.Section;
+import nextstep.subway.path.dto.PathResponse;
+import nextstep.subway.station.domain.Station;
+import nextstep.subway.station.domain.StationRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Arrays;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.mockito.Mockito.when;
+
+
+@DisplayName("지하철 최단 경로 조회 서비스 테스트")
+@ExtendWith(MockitoExtension.class)
+class PathServiceTest {
+
+    @Mock
+    private LineRepository lineRepository;
+
+    @Mock
+    private StationRepository stationRepository;
+
+    private Line 신분당선;
+    private Line 분당선;
+    private Line 삼호선;
+    private Station 정자역;
+    private Station 양재역;
+    private Station 수서역;
+    private Station 서현역;
+
+    /**
+     * 양재역 ------*3호선(5)*------ 수서역
+     * ㅣ                          ㅣ
+     * ㅣ                          ㅣ
+     * *신분당선(10)*             *분당선(5)*
+     * ㅣ                          ㅣ
+     * ㅣ                          ㅣ
+     * 정쟈역 ------*분당선(5)*------ 서현역
+     */
+    @BeforeEach
+    void setUp() {
+        정자역 = new Station("정자역");
+        양재역 = new Station("양재역");
+        수서역 = new Station("수서역");
+        서현역 = new Station("서현역");
+
+        신분당선 = new Line("신분당선", "red", 양재역, 정자역, 10);
+        분당선 = new Line("분당선", "yellow", 수서역, 정자역, 10);
+        삼호선 = new Line("삼호선", "orange", 양재역, 수서역, 5);
+
+        분당선.addSection(new Section(분당선, 서현역, 정자역, 5));
+    }
+
+    @InjectMocks
+    private PathService pathService;
+
+    @DisplayName("출발역(양재역)과 도착역(서현역) 사이의 최단 경로 찾기")
+    @Test
+    void findShortestPath() {
+        when(stationRepository.findById(1L)).thenReturn(Optional.of(양재역));
+        when(stationRepository.findById(2L)).thenReturn(Optional.of(서현역));
+        when(lineRepository.findAll()).thenReturn(Arrays.asList(신분당선, 분당선, 삼호선));
+
+        PathResponse response = pathService.findShortestPath(1L, 2L);
+
+        assertAll(
+                () -> assertThat(response.getStations()).hasSize(3),
+                () -> assertThat(response.getDistance()).isEqualTo(10)
+        );
+    }
+}
