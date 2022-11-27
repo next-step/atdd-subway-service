79f8385a51fb4da79ff8b87d7bd2ebe885845d3a
ilmare-cbk <cbk104325@gmail.com>
2022-11-27 23:48:26 +0900

    test(AuthAcceptanceTest.java): 잘못된 비밀번호로 로그인 시 실패 테스트 작성

--- src/test/java/nextstep/subway/auth/acceptance/AuthAcceptanceTest.java
+++ src/test/java/nextstep/subway/auth/acceptance/AuthAcceptanceTest.java
@@ -6,6 +6,7 @@ import io.restassured.response.Response;
 import nextstep.subway.AcceptanceTest;
 import nextstep.subway.auth.dto.TokenRequest;
 import nextstep.subway.auth.dto.TokenResponse;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -17,12 +18,22 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 /**
- * Feature: 로그인 기능
+ *   Feature: 로그인 기능
  *
- *   Scenario: 로그인을 시도한다.
- *     Given 회원 등록되어 있음
- *     When 로그인 요청
- *     Then 로그인 됨
+ *     Background
+ *       Given 회원 등록되어 있음
+ *
+ *     Scenario: 로그인을 시도한다.
+ *       When 로그인 요청
+ *       Then 로그인 됨
+ *
+ *     Scenario: 잘못된 비밀번호로 로그인을 시도한다.
+ *       When 로그인 요청
+ *       Then 로그인 실패
+ *
+ *     Scenario: 등록하지 않은 회원정보로 로그인을 시도한다.
+ *       When 로그인 요청
+ *       Then 로그인 실패
  */
 class AuthAcceptanceTest extends AcceptanceTest {
 
@@ -34,6 +45,11 @@ class AuthAcceptanceTest extends AcceptanceTest {
         회원_생성을_요청(EMAIL, PASSWORD, AGE);
     }
 
+    /**
+     * Scenario: 로그인을 시도한다.
+     * When 로그인 요청
+     * Then 로그인 됨
+     */
     @DisplayName("Bearer Auth")
     @Test
     void myInfoWithBearerAuth() {
@@ -42,9 +58,17 @@ class AuthAcceptanceTest extends AcceptanceTest {
         로그인_됨(response);
     }
 
-    @DisplayName("Bearer Auth 로그인 실패")
+    /**
+     * Scenario: 잘못된 비밀번호로 로그인을 시도한다.
+     * When 로그인 요청
+     * Then 로그인 실패
+     */
+    @DisplayName("Bearer Auth 로그인 실패(잘못된 비밀번호로 로그인 시도)")
     @Test
     void myInfoWithBadBearerAuth() {
+        ExtractableResponse<Response> response = 로그인_요청(new TokenRequest(EMAIL, "wrong password"));
+
+        로그인_안됨(response);
     }
 
     @DisplayName("Bearer Auth 유효하지 않은 토큰")
@@ -67,5 +91,9 @@ class AuthAcceptanceTest extends AcceptanceTest {
                 () -> assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value()),
                 () -> assertThat(response.as(TokenResponse.class).getAccessToken()).isNotBlank()
         );
+    }
+
+    private void 로그인_안됨(ExtractableResponse<Response> response) {
+        Assertions.assertThat(response.statusCode()).isEqualTo(HttpStatus.UNAUTHORIZED.value());
     }
 }
