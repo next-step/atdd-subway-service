958a492a91af906ac3b1ab4fbfc5a92797f35ccc
chunhodong <yhms4432@naver.com>
2022-11-27 20:22:56 +0900

    refactor: Line클래스에 안쓰는 isEmptySections메소드 삭제

--- src/test/java/nextstep/subway/line/domain/LineTest.java
+++ src/test/java/nextstep/subway/line/domain/LineTest.java
@@ -14,46 +14,6 @@ import static org.junit.jupiter.api.Assertions.assertAll;
 
 @DisplayName("Line도메인의 ")
 public class LineTest {
-
-    @Nested
-    @DisplayName("isEmptySections메소드는 ")
-    class DescribeIsEmptySection {
-
-        @Nested
-        @DisplayName("추가된 노선이 없으면 true를 반환")
-        class ContextWithNoSection {
-
-            private final Line line = new Line();
-
-            @Test
-            void returnsTrue() {
-                assertThat(line.isEmptySections()).isTrue();
-            }
-        }
-
-        @Nested
-        @DisplayName("추가된 노선이 하나이상이면 false를 반환")
-        class ContextWithSection {
-
-            private Line line = new Line();
-            private Random random = new Random();
-
-            @BeforeEach
-            void before() {
-                List<Section> sections = IntStream
-                        .rangeClosed(1, random.nextInt(100) + 1)
-                        .mapToObj(value -> new Section())
-                        .collect(Collectors.toList());
-                line.getSections().addAll(sections);
-            }
-
-            @RepeatedTest(100)
-            void returnFalse() {
-                assertThat(line.isEmptySections()).isFalse();
-            }
-        }
-    }
-
     @Nested
     @DisplayName("removeStation메소드는 ")
     class DescribeRemoveStation {
