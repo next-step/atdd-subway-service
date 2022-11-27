4a90ec264a0d41f2212a0d911cf26b86d7a82dab
crongcm <ucm5459@kakao.com>
2022-11-27 18:10:36 +0900

    style: 에러 메세지 변경

--- src/main/java/nextstep/subway/station/domain/Station.java
+++ src/main/java/nextstep/subway/station/domain/Station.java
@@ -8,7 +8,7 @@ import org.apache.commons.lang3.StringUtils;
 
 @Entity
 public class Station extends BaseEntity {
-    private static final String ERROR_MESSAGE_IS_BLANK_NAME = "노선명은 필수 입니다.";
+    private static final String ERROR_MESSAGE_IS_BLANK_NAME = "지하철명은 필수 입니다.";
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
