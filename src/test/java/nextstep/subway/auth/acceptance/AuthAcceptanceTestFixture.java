2227cee88c55d808a52ee8be8046a35bd9900a10
giraffelim <taeyang7704@naver.com>
2022-11-27 21:24:40 +0900

    test(AuthAcceptanceTestFixture): 로그인을 요청하고, 응답된 accessToken을 반환해주는 로그인 됨 추가

--- src/test/java/nextstep/subway/auth/acceptance/AuthAcceptanceTestFixture.java
+++ src/test/java/nextstep/subway/auth/acceptance/AuthAcceptanceTestFixture.java
@@ -6,6 +6,7 @@ import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import nextstep.subway.auth.dto.TokenRequest;
+import nextstep.subway.auth.dto.TokenResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
@@ -19,6 +20,11 @@ public class AuthAcceptanceTestFixture {
                 .when().post("/login/token")
                 .then().log().all()
                 .extract();
+    }
+
+    public static String 로그인_됨(String email, String password) {
+        TokenResponse response = 로그인_요청(email, password).as(TokenResponse.class);
+        return response.getAccessToken();
     }
 
     public static void 로그인_성공(ExtractableResponse<Response> response) {
