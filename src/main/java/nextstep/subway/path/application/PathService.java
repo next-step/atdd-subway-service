52371fa031a4cbb34a6a89cb2c876a9010a0abc6
ilmare-cbk <cbk104325@gmail.com>
2022-11-27 17:59:34 +0900

    feat(path package): PathController, PathService 생성

---
+++ src/main/java/nextstep/subway/path/application/PathService.java
@@ -0,0 +1,14 @@
+package nextstep.subway.path.application;
+
+import nextstep.subway.path.dto.PathResponse;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+public class PathService {
+
+    public PathResponse findShortestPath(Long source, Long target) {
+        return null;
+    }
+}
