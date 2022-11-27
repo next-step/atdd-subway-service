55f86fb701e5e41f39e9fcad352aba58a1e19719
crongcm <ucm5459@kakao.com>
2022-11-27 18:22:29 +0900

    test: 테스트 생성자 도메인에서 제거

--- src/test/java/study/unit/MockitoExtensionTest.java
+++ src/test/java/study/unit/MockitoExtensionTest.java
@@ -6,10 +6,13 @@ import static org.mockito.Mockito.when;
 import com.google.common.collect.Lists;
 import java.util.List;
 import nextstep.subway.line.application.LineService;
+import nextstep.subway.line.domain.Distance;
 import nextstep.subway.line.domain.Line;
 import nextstep.subway.line.domain.LineRepository;
+import nextstep.subway.line.domain.Section;
 import nextstep.subway.line.dto.LineResponse;
 import nextstep.subway.station.application.StationService;
+import nextstep.subway.station.domain.Station;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -27,7 +30,8 @@ class MockitoExtensionTest {
     @Test
     void findAllLines() {
         // given
-        when(lineRepository.findAll()).thenReturn(Lists.newArrayList(Line.mock()));
+        when(lineRepository.findAll()).thenReturn(Lists.newArrayList(Line.of("신분당선", "bg-red-600",
+                Section.of(Station.from("신논현역"), Station.from("강남역"), Distance.from(3)))));
         LineService lineService = new LineService(lineRepository, stationService);
 
         // when
