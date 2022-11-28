aeae1d34a3b0db7f6bf1ec1ddd9255c55b189393
paki1019 <paki1019@gmail.com>
2022-11-29 00:12:09 +0900

    refactor: 미사용 getter 제거

--- src/main/java/nextstep/subway/line/domain/Section.java
+++ src/main/java/nextstep/subway/line/domain/Section.java
@@ -111,20 +111,12 @@ public class Section {
         return id;
     }
 
-    public Line getLine() {
-        return line;
-    }
-
     public Station getUpStation() {
         return upStation;
     }
 
     public Station getDownStation() {
         return downStation;
-    }
-
-    public Distance getDistance() {
-        return distance;
     }
 
     @Override
