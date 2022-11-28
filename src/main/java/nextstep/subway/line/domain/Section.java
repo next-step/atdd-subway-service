89895b282501e7009110b70114edf948e7af151d
seogineer <dgseo8981@gmail.com>
2022-11-28 12:07:05 +0900

    refactor: 거리 유효성검사 메소드 추출

--- src/main/java/nextstep/subway/line/domain/Section.java
+++ src/main/java/nextstep/subway/line/domain/Section.java
@@ -60,18 +60,20 @@ public class Section {
     }
 
     public void updateUpStation(Station station, int newDistance) {
-        if (this.distance <= newDistance) {
-            throw new RuntimeException("역과 역 사이의 거리보다 좁은 거리를 입력해주세요");
-        }
+        validateDistance(newDistance);
         this.upStation = station;
         this.distance -= newDistance;
     }
 
     public void updateDownStation(Station station, int newDistance) {
+        validateDistance(newDistance);
+        this.downStation = station;
+        this.distance -= newDistance;
+    }
+
+    private void validateDistance(int newDistance){
         if (this.distance <= newDistance) {
             throw new RuntimeException("역과 역 사이의 거리보다 좁은 거리를 입력해주세요");
         }
-        this.downStation = station;
-        this.distance -= newDistance;
     }
 }
