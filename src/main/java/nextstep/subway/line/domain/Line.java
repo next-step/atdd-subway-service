339697172f7bdbbcdc686eed8750132f3c35e223
dongha-byun <byunsw4@naver.com>
2022-11-28 16:21:36 +0900

    Refactor: LineService refactoring

--- src/main/java/nextstep/subway/line/domain/Line.java
+++ src/main/java/nextstep/subway/line/domain/Line.java
@@ -1,6 +1,9 @@
 package nextstep.subway.line.domain;
 
+import java.util.Arrays;
+import java.util.Optional;
 import nextstep.subway.BaseEntity;
+import nextstep.subway.line.dto.LineRequest;
 import nextstep.subway.station.domain.Station;
 
 import javax.persistence.*;
@@ -9,6 +12,7 @@ import java.util.List;
 
 @Entity
 public class Line extends BaseEntity {
+    private static final int MINIMUM_SECTION_COUNT = 1;
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -33,9 +37,102 @@ public class Line extends BaseEntity {
         sections.add(new Section(this, upStation, downStation, distance));
     }
 
-    public void update(Line line) {
-        this.name = line.getName();
-        this.color = line.getColor();
+    public static Line to(LineRequest request, Station upStation, Station downStation){
+        return new Line(request.getName(), request.getColor(), upStation, downStation, request.getDistance());
+    }
+
+    public void update(LineRequest request){
+        this.name = request.getName();
+        this.color = request.getColor();
+    }
+
+    public void addSection(Station upStation, Station downStation, int distance){
+        boolean isUpStationExisted = isExistsUpStation(upStation);
+        boolean isDownStationExisted = isExistsDownStation(downStation);
+
+        validateStation(isUpStationExisted, isDownStationExisted);
+
+        if (isUpStationExisted) {
+            updateUpSection(upStation, downStation, distance);
+        }
+        if (isDownStationExisted) {
+            updateDownSection(upStation, downStation, distance);
+        }
+
+        sections.add(new Section(this, upStation, downStation, distance));
+    }
+    private boolean isExistsUpStation(Station upStation){
+        return getStations().stream().anyMatch(it -> it == upStation);
+    }
+
+    private boolean isExistsDownStation(Station downStation){
+        return getStations().stream().anyMatch(it -> it == downStation);
+    }
+    private void validateStation(boolean isUpStationExisted, boolean isDownStationExisted){
+        if (!isUpStationExisted && !isDownStationExisted) {
+            throw new RuntimeException("등록할 수 없는 구간 입니다.");
+        }
+
+        if (isUpStationExisted && isDownStationExisted) {
+            throw new RuntimeException("이미 등록된 구간 입니다.");
+        }
+    }
+
+    private void updateUpSection(Station upStation, Station downStation, int distance){
+        sections.stream()
+                .filter(it -> it.getUpStation() == upStation)
+                .findFirst()
+                .ifPresent(it -> it.updateUpStation(downStation, distance));
+    }
+
+    private void updateDownSection(Station upStation, Station downStation, int distance){
+        sections.stream()
+                .filter(it -> it.getDownStation() == downStation)
+                .findFirst()
+                .ifPresent(it -> it.updateDownStation(upStation, distance));
+    }
+
+    public void removeSection(Station station){
+        validateLastSection();
+
+        Optional<Section> upLineStation = getUpLineStation(station);
+        Optional<Section> downLineStation = getDownLineStation(station);
+
+        if (upLineStation.isPresent() && downLineStation.isPresent()) {
+            createNewSection(upLineStation.get(), downLineStation.get());
+        }
+
+        upLineStation.ifPresent(it -> sections.remove(it));
+        downLineStation.ifPresent(it -> sections.remove(it));
+    }
+
+    private void validateLastSection() {
+        if (isLastSection()) {
+            throw new RuntimeException();
+        }
+    }
+
+    private boolean isLastSection(){
+        return this.getSections().size() <= MINIMUM_SECTION_COUNT;
+    }
+
+    private Optional<Section> getUpLineStation(Station station){
+        return sections.stream()
+                .filter(it -> it.getUpStation() == station)
+                .findFirst();
+    }
+
+    private Optional<Section> getDownLineStation(Station station){
+        return sections.stream()
+                .filter(it -> it.getDownStation() == station)
+                .findFirst();
+    }
+
+    private void createNewSection(Section upLineStation, Section downLineStation) {
+        Station newUpStation = downLineStation.getUpStation();
+        Station newDownStation = upLineStation.getDownStation();
+        int newDistance = upLineStation.getDistance() + downLineStation.getDistance();
+        sections.add(new Section(this, newUpStation, newDownStation, newDistance));
     }
 
     public Long getId() {
@@ -52,5 +149,46 @@ public class Line extends BaseEntity {
 
     public List<Section> getSections() {
         return sections;
+    }
+
+    public List<Station> getStations() {
+        if (hasNotSection()) {
+            return Arrays.asList();
+        }
+
+        return new ArrayList<>(getOrderStations(findUpStation()));
+    }
+
+    private boolean hasNotSection(){
+        return this.getSections().isEmpty();
+    }
+
+    private List<Station> getOrderStations(Station downStation) {
+        List<Station> stations = new ArrayList<>();
+        do{
+            stations.add(downStation);
+            Station finalDownStation = downStation;
+            downStation = findNextDownStation(finalDownStation);
+        } while (downStation != null);
+        return stations;
+    }
+
+    private Station findNextDownStation(Station station){
+        Optional<Section> nextLineStation = getUpLineStation(station);
+        return nextLineStation.map(Section::getDownStation).orElse(null);
+    }
+
+    private Station findUpStation() {
+        Station downStation = sections.get(0).getUpStation();
+        while (downStation != null) {
+            Station finalDownStation = downStation;
+            Optional<Section> nextLineStation = getDownLineStation(finalDownStation);
+            if (!nextLineStation.isPresent()) {
+                break;
+            }
+            downStation = nextLineStation.get().getUpStation();
+        }
+
+        return downStation;
     }
 }
---
+++ src/test/java/nextstep/subway/line/domain/LineTest.java
@@ -0,0 +1,156 @@
+package nextstep.subway.line.domain;
+
+import static org.assertj.core.api.Assertions.*;
+
+import nextstep.subway.station.domain.Station;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class LineTest {
+
+    Station station1;
+    Station station2;
+    Line line;
+
+    @BeforeEach
+    void beforeEach(){
+        station1 = new Station("강남역");
+        station2 = new Station("잠실역");
+        line = new Line("2호선", "green", station1, station2, 20);
+    }
+
+    @Test
+    @DisplayName("중간 구간 추가 성공 테스트")
+    void addSection(){
+        // given - beforeEach
+
+        // when
+        Station newStation = new Station("종합운동장역");
+        line.addSection(station1, newStation, 10);
+
+        // then
+        assertThat(line.getStations()).containsExactly(
+                station1, newStation, station2
+        );
+    }
+
+    @Test
+    @DisplayName("상행 종점 추가 성공 테스트")
+    void addSection_upStation(){
+        // given - beforeEach
+
+        // when
+        Station newStation = new Station("사당역");
+        line.addSection(newStation, station1, 30);
+
+        // then
+        assertThat(line.getStations()).containsExactly(
+                newStation, station1, station2
+        );
+    }
+
+    @Test
+    @DisplayName("하행 종점 추가 성공 테스트")
+    void addSection_downStation(){
+        // given - beforeEach
+
+        // when
+        Station newStation = new Station("성수역");
+        line.addSection(station2, newStation, 30);
+
+        // then
+        assertThat(line.getStations()).containsExactly(
+                station1, station2, newStation
+        );
+    }
+
+    @Test
+    @DisplayName("구간 추가 실패 - 이미 존재하는 역들로는 구간을 추가할 수 없다.")
+    void addSection_allStationExistsException(){
+        // given - beforeEach
+
+        // when
+
+        // then
+        assertThatThrownBy(
+                () -> line.addSection(station1, station2, 30)
+        ).isInstanceOf(RuntimeException.class);
+    }
+
+    @Test
+    @DisplayName("구간 추가 실패 - 존재하지 않는 역들로는 구간을 추가할 수 없다.")
+    void addSection_stationNotExistsException(){
+        // given - beforeEach
+
+        // when
+        Station newStation1 = new Station("시청역");
+        Station newStation2 = new Station("왕십리역");
+
+        // then
+        assertThatThrownBy(
+                () -> line.addSection(newStation1, newStation2, 30)
+        ).isInstanceOf(RuntimeException.class);
+    }
+
+    @Test
+    @DisplayName("중간 구간 제거 성공 테스트")
+    void removeSection(){
+        // given - beforeEach
+        Station newStation = new Station("왕십리역");
+        line.addSection(station2, newStation, 30);
+
+        // when
+        line.removeSection(station2);
+
+        // then
+        assertThat(line.getStations()).containsExactly(
+                station1, newStation
+        );
+    }
+
+    @Test
+    @DisplayName("상행 종점 제거 성공 테스트")
+    void removeSection_upStation(){
+        // given - beforeEach
+        Station newStation = new Station("왕십리역");
+        line.addSection(station2, newStation, 30);
+
+        // when
+        line.removeSection(station1);
+
+        // then
+        assertThat(line.getStations()).containsExactly(
+                station2, newStation
+        );
+    }
+
+    @Test
+    @DisplayName("하행 종점 제거 성공 테스트")
+    void removeSection_downStation(){
+        // given - beforeEach
+        Station newStation = new Station("왕십리역");
+        line.addSection(station2, newStation, 30);
+
+        // when
+        line.removeSection(newStation);
+
+        // then
+        assertThat(line.getStations()).containsExactly(
+                station1, station2
+        );
+    }
+
+    @Test
+    @DisplayName("구간 제거 실패 테스트 - 마지막 구간만 남은 경우 해당 구간을 제거할 수 없다.")
+    void removeSectionException_lastSection(){
+        // given - beforeEach
+
+        // when
+
+        // then
+        assertThatThrownBy(
+                () -> line.removeSection(station2)
+        ).isInstanceOf(RuntimeException.class);
+    }
+}
