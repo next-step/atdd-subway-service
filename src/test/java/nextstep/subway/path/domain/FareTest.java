4c0e8dfe1bb93a5e5f3cdfd3304dfdd5ef4c6779
sjk5766 <wjdrnr6662@gmail.com>
2022-11-29 06:48:29 +0900

    test(domain): Fare(요금) domain 테스트 코드 추가

---
+++ src/test/java/nextstep/subway/path/domain/FareTest.java
@@ -0,0 +1,49 @@
+package nextstep.subway.path.domain;
+
+import nextstep.subway.line.domain.ExtraFare;
+import nextstep.subway.line.domain.Line;
+import nextstep.subway.line.domain.Lines;
+import nextstep.subway.station.domain.Station;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class FareTest {
+    private static final int BASIC_PRICE = 1250;
+
+    @DisplayName("추가 요금이 없을 때의 기본 지하철 요금은 1250원이다.")
+    @Test
+    void basicPrice() {
+        // given
+        Station station1 = new Station("강남역");
+        Station station2 = new Station("판교역");
+        Line line = new Line("신분당선", "bg-red-600", new ExtraFare(0));
+        Lines lines = new Lines(Arrays.asList(line));
+
+        // when
+        Fare result = new Fare(lines);
+
+        // then
+        assertThat(result.get()).isEqualTo(BASIC_PRICE);
+    }
+
+    @DisplayName("추가 요금이 있을경우, 지하철 요금은 기본요금과 추가요금의 합이다.")
+    @Test
+    void extraFarePrice() {
+        // given
+        int extraFare = 1_000;
+        Station station1 = new Station("강남역");
+        Station station2 = new Station("판교역");
+        Line line = new Line("신분당선", "bg-red-600", new ExtraFare(extraFare));
+        Lines lines = new Lines(Arrays.asList(line));
+
+        // when
+        Fare result = new Fare(lines);
+
+        // then
+        assertThat(result.get()).isEqualTo(BASIC_PRICE + extraFare);
+    }
+}
