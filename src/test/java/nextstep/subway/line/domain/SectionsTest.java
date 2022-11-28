eadbd0e659ed83da6846730b41b67df5a5a792b4
jylim <jylim@12cm.co.kr>
2022-11-28 16:15:59 +0900

    feat(SectionsTest): SectionsTest 구현

---
+++ src/test/java/nextstep/subway/line/domain/SectionsTest.java
@@ -0,0 +1,107 @@
+package nextstep.subway.line.domain;
+
+import nextstep.subway.station.domain.Station;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class SectionsTest {
+    private Line 신분당선;
+    private Station 강남역;
+    private Station 정자역;
+    private Section 강남역_정자역_구간;
+    private Sections sections;
+
+    @BeforeEach
+    void setUp() {
+        신분당선 = new Line("신분당선", "red");
+        강남역 = new Station("강남역");
+        정자역 = new Station("정자역");
+        강남역_정자역_구간 = new Section(신분당선, 강남역, 정자역, 10);
+        sections = Sections.initSections();
+        sections.add(강남역_정자역_구간);
+    }
+
+    @DisplayName("중복된 구간을 등록하면 예외가 발생한다.")
+    @Test
+    void sameSectionException() {
+        Assertions.assertThatThrownBy(() -> sections.add(강남역_정자역_구간))
+                .isInstanceOf(RuntimeException.class)
+                .hasMessageStartingWith("이미 등록된 구간 입니다.");
+    }
+
+    @DisplayName("노선에 포함되지 않는 역이 있는 구간을 포함하면 에러가 발생한다")
+    @Test
+    void notContainedSectionException() {
+        Station 서현역 = new Station("서현역");
+        Station 왕십리역 = new Station("왕십리역");
+        Section 서현역_왕십리역_구간 = new Section(신분당선, 서현역, 왕십리역, 15);
+
+        Assertions.assertThatThrownBy(() -> sections.add(서현역_왕십리역_구간))
+                .isInstanceOf(RuntimeException.class)
+                .hasMessageStartingWith("등록할 수 없는 구간 입니다.");
+    }
+
+    @DisplayName("상행역을 등록할 때 기존 구간 길이보다 크거나 같으면 예외가 발생한다. ")
+    @ParameterizedTest
+    @ValueSource(ints = {10, 20})
+    void upStationsSizeException(int input) {
+        Station 역삼역 = new Station("역삼역");
+        Section 강남역_역삼역_구간 = new Section(신분당선, 강남역, 역삼역, input);
+
+        Assertions.assertThatThrownBy(() -> sections.add(강남역_역삼역_구간))
+                .isInstanceOf(RuntimeException.class)
+                .hasMessageStartingWith("역과 역 사이의 거리보다 좁은 거리를 입력해주세요");
+    }
+
+    @DisplayName("하행역을 등록할 때 기존 구간 길이보다 크거나 같으면 예외가 발생한다. ")
+    @ParameterizedTest
+    @ValueSource(ints = {10, 20})
+    void downStationSizeException(int input) {
+        Station 역삼역 = new Station("역삼역");
+        Section 역삼역_정자역_구간 = new Section(신분당선, 역삼역, 정자역, input);
+
+        Assertions.assertThatThrownBy(() -> sections.add(역삼역_정자역_구간))
+                .isInstanceOf(RuntimeException.class)
+                .hasMessageStartingWith("역과 역 사이의 거리보다 좁은 거리를 입력해주세요");
+    }
+
+    @DisplayName("기존 지하철 구간에 새로운 종점이 아닌 지하철 구간을 추가한다.")
+    @Test
+    void addNotLastSection() {
+        Station 역삼역 = new Station("역삼역");
+        sections.add(new Section(신분당선, 강남역, 역삼역, 3));
+
+        Assertions.assertThat(sections.getStations())
+                .containsExactlyInAnyOrder(강남역, 역삼역, 정자역);
+    }
+
+    @DisplayName("기존 지하철 구간에 상행 종점 지하철 구간을 추가한다.")
+    @Test
+    void addLastUpStationSection() {
+        Station 역삼역 = new Station("역삼역");
+        sections.add(new Section(신분당선, 역삼역, 강남역, 5));
+
+        Assertions.assertThat(sections.getStations())
+                .containsExactlyInAnyOrder(역삼역, 강남역, 정자역);
+    }
+
+    @DisplayName("기존 지하철 구간에 하행 종점 지하철 구간을 추가한다.")
+    @Test
+    void addLastDownStationSection() {
+        Station 역삼역 = new Station("역삼역");
+        sections.add(new Section(신분당선, 정자역, 역삼역, 5));
+
+        Assertions.assertThat(sections.getStations())
+                .containsExactlyInAnyOrder(강남역, 정자역, 역삼역);
+    }
+
+
+
+
+
+}
