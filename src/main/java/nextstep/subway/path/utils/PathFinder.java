f3c624f10ed0b92a6d272e6f3bb717ba0633e051
seogineer <dgseo8981@gmail.com>
2022-11-29 15:20:14 +0900

    refactor: 메소드 추출

--- src/main/java/nextstep/subway/path/utils/PathFinder.java
+++ src/main/java/nextstep/subway/path/utils/PathFinder.java
@@ -14,34 +14,65 @@ import org.springframework.stereotype.Component;
 @Component
 public class PathFinder {
     public Path findPath(List<Line> lines, Long source, Long target) {
+        DijkstraShortestPath dijkstraShortestPath = getDijkstraShortestPath(lines);
+        List<StationResponse> stationResponses
+                = getStationResponses(lines, getShortestPath(dijkstraShortestPath, source, target));
+        return new Path(stationResponses, getPathWeight(dijkstraShortestPath, source, target));
+    }
+
+    private List<String> getShortestPath(DijkstraShortestPath dijkstraShortestPath, Long source, Long target) {
+        return dijkstraShortestPath.getPath(String.valueOf(source), String.valueOf(target)).getVertexList();
+    }
+
+    private int getPathWeight(DijkstraShortestPath dijkstraShortestPath, Long source, Long target){
+        return (int) dijkstraShortestPath.getPathWeight(String.valueOf(source), String.valueOf(target));
+    }
+
+    private List<StationResponse> getStationResponses(List<Line> lines, List<String> shortestPath){
+        List<Station> stations = getStations(lines);
+        return shortestPath.stream()
+                .map(stationId -> findStation(stations, stationId))
+                .map(StationResponse::of)
+                .collect(Collectors.toList());
+    }
+
+    private List<Station> getStations(List<Line> lines){
+        return lines.stream()
+                .flatMap(line -> line.getStations().stream())
+                .distinct()
+                .collect(Collectors.toList());
+    }
+
+    private Station findStation(List<Station> stations, String stationId){
+        return stations.stream()
+                .filter(station -> station.isEqualToStationId(stationId))
+                .findFirst()
+                .orElseThrow(() -> new RuntimeException("station id와 일치하는 역을 찾을 수 없습니다."));
+    }
+
+    private DijkstraShortestPath getDijkstraShortestPath (List<Line> lines) {
+        return new DijkstraShortestPath(getLineGraph(lines));
+    }
+
+    private WeightedMultigraph<String, DefaultWeightedEdge> getLineGraph(List<Line> lines){
         WeightedMultigraph<String, DefaultWeightedEdge> graph = new WeightedMultigraph<>(DefaultWeightedEdge.class);
+        addVertex(lines, graph);
+        setEdgeWeight(lines, graph);
+        return graph;
+    }
 
+    private void addVertex(List<Line> lines, WeightedMultigraph<String, DefaultWeightedEdge> graph){
         lines.stream()
-            .flatMap(line -> line.getStations().stream())
-            .forEach(station -> graph.addVertex(String.valueOf(station.getId())));
+                .flatMap(line -> line.getStations().stream())
+                .forEach(station -> graph.addVertex(String.valueOf(station.getId())));
+    }
 
+    private void setEdgeWeight(List<Line> lines, WeightedMultigraph<String, DefaultWeightedEdge> graph){
         lines.stream()
                 .flatMap(line -> line.getSections().getSections().stream())
                 .forEach(section -> graph.setEdgeWeight(
                         graph.addEdge(String.valueOf(section.getUpStation().getId()),
                                       String.valueOf(section.getDownStation().getId())),
                         section.getDistance().getDistance()));
-
-        DijkstraShortestPath dijkstraShortestPath = new DijkstraShortestPath(graph);
-        List<String> shortestPath = dijkstraShortestPath.getPath(String.valueOf(source), String.valueOf(target)).getVertexList();
-
-        List<Station> stations = lines.stream()
-                .flatMap(line -> line.getStations().stream())
-                .distinct()
-                .collect(Collectors.toList());
-
-        List<StationResponse> stationResponses = shortestPath.stream()
-                .map(stationId -> stations.stream().filter(station -> String.valueOf(station.getId()).equals(stationId)).findFirst())
-                .map(station -> StationResponse.of(station.get()))
-                .collect(Collectors.toList());
-
-        int pathWeight = (int) dijkstraShortestPath.getPathWeight(String.valueOf(source), String.valueOf(target));
-
-        return new Path(stationResponses, pathWeight);
     }
 }
