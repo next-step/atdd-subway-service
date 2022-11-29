e008ec8743f27c73ed27090f9f77c86a050915c1
ilmare-cbk <cbk104325@gmail.com>
2022-11-29 20:48:51 +0900

    feat(FavoriteService.java, Favorite.java): 즐겨찾기 생성 메소드 구현 및 즐겨찾기 엔티티 구현

--- src/main/java/nextstep/subway/favorite/application/FavoriteService.java
+++ src/main/java/nextstep/subway/favorite/application/FavoriteService.java
@@ -1,14 +1,51 @@
 package nextstep.subway.favorite.application;
 
+import nextstep.subway.exception.EntityNotFoundException;
 import nextstep.subway.favorite.domain.Favorite;
+import nextstep.subway.favorite.domain.FavoriteRepository;
 import nextstep.subway.favorite.dto.FavoriteRequest;
+import nextstep.subway.member.domain.Member;
+import nextstep.subway.member.domain.MemberRepository;
+import nextstep.subway.message.ExceptionMessage;
+import nextstep.subway.station.domain.Station;
+import nextstep.subway.station.domain.StationRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional(readOnly = true)
 public class FavoriteService {
+
+    private final FavoriteRepository favoriteRepository;
+    private final StationRepository stationRepository;
+    private final MemberRepository memberRepository;
+
+    public FavoriteService(FavoriteRepository favoriteRepository,
+                           StationRepository stationRepository,
+                           MemberRepository memberRepository) {
+        this.favoriteRepository = favoriteRepository;
+        this.stationRepository = stationRepository;
+        this.memberRepository = memberRepository;
+    }
+
     public Favorite create(Long memberId, FavoriteRequest request) {
-        return null;
+        Station sourceStation = findStationById(request.getSource());
+        Station targetStation = findStationById(request.getTarget());
+        Member member = findMemberById(memberId);
+
+        Favorite favorite = Favorite.of(member, sourceStation, targetStation);
+        favoriteRepository.save(favorite);
+
+        return favorite;
+    }
+
+    private Station findStationById(Long request) {
+        return stationRepository.findById(request)
+                .orElseThrow(() -> new EntityNotFoundException(ExceptionMessage.STATION_NOT_EXIST));
+    }
+
+    private Member findMemberById(Long memberId) {
+        return memberRepository.findById(memberId)
+                .orElseThrow(() -> new EntityNotFoundException(ExceptionMessage.MEMBER_NOT_EXIST));
     }
 }
