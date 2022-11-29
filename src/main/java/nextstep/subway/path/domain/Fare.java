2af86158cd54d8d8d0202c1175b7d477db63287c
giraffelim <taeyang7704@naver.com>
2022-11-30 01:18:52 +0900

    feat(Fare): 요금(Fare) 추가

---
+++ src/main/java/nextstep/subway/path/domain/Fare.java
@@ -0,0 +1,42 @@
+package nextstep.subway.path.domain;
+
+import java.util.Objects;
+
+public class Fare {
+    private static final int ZERO = 0;
+    private static final String CAN_NOT_NEGATIVE = "요금은 음수일 수 없습니다.";
+
+    private final int value;
+
+    public Fare(int value) {
+        validate(value);
+        this.value = value;
+    }
+
+    private void validate(int value) {
+        if (value < ZERO) {
+            throw new IllegalArgumentException(CAN_NOT_NEGATIVE);
+        }
+    }
+
+    public Fare add(Fare fare) {
+        return new Fare(this.value + fare.value);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Fare fare = (Fare) o;
+        return value == fare.value;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
+}
---
+++ src/test/java/nextstep/subway/path/domain/FareTest.java
@@ -0,0 +1,26 @@
+package nextstep.subway.path.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class FareTest {
+    @DisplayName("요금이 음수일 경우 예외가 발생한다.")
+    @Test
+    void negative() {
+        assertThatThrownBy(() -> new Fare(-1))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("요금은 음수일 수 없습니다.");
+    }
+
+    @DisplayName("요금을 더할 수 있다.")
+    @Test
+    void add() {
+        Fare fare = new Fare(1000);
+        Fare extraFare = new Fare(500);
+
+        assertThat(fare.add(extraFare)).isEqualTo(new Fare(1500));
+    }
+}
