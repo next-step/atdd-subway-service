6f668e70e35ae3ab3d4d9789483c742bead65bb6
sjk5766 <wjdrnr6662@gmail.com>
2022-11-29 11:18:59 +0900

    test(domain): FareDistance(거리별 요금) 테스트 추가

---
+++ src/test/java/nextstep/subway/path/domain/FareDistanceTest.java
@@ -0,0 +1,42 @@
+package nextstep.subway.path.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class FareDistanceTest {
+    @DisplayName("기본 지하철 요금은 1250원이다.")
+    @Test
+    void basicFare() {
+        // when
+        int result = FareDistance.calculate(0);
+
+        // then
+        assertThat(result).isEqualTo(1_250);
+    }
+
+    @DisplayName("10km 초과 ~ 50km 까지는 5km 마다 100원이 추가된다")
+    @ParameterizedTest
+    @CsvSource(value = {"11:1350", "15:1350", "16:1450", "20:1450", "21:1550"}, delimiter = ':')
+    void middleFare(int distance, int fare) {
+        // when
+        int result = FareDistance.calculate(distance);
+
+        // then
+        assertThat(result).isEqualTo(fare);
+    }
+
+    @DisplayName("50km 초과 ~ 178km 까지는 8km 마다 100원이 추가된다")
+    @ParameterizedTest
+    @CsvSource(value = {"51:2150", "58:2150", "59:2250", "66:2250", "67:2350"}, delimiter = ':')
+    void longFare(int distance, int fare) {
+        // when
+        int result = FareDistance.calculate(distance);
+
+        // then
+        assertThat(result).isEqualTo(fare);
+    }
+}
