611bac9d3af39e0e5dc34d9be48961dd769c9c36
anwjrrp33 <anwjrrp33@gmail.com>
2022-11-29 11:01:13 +0900

    refactor: 커스텀 예외 메세지 수정

--- src/main/java/nextstep/subway/exception/NotFoundException.java
+++ src/main/java/nextstep/subway/exception/NotFoundException.java
@@ -2,7 +2,7 @@ package nextstep.subway.exception;
 
 public class NotFoundException extends RuntimeException {
 
-    private static final String NOT_FOUND_ERROR = "존재하지 않습니다.";
+    private static final String NOT_FOUND_ERROR = "데이터가 존재하지 않습니다.";
 
     public NotFoundException() {
         super(NOT_FOUND_ERROR);
