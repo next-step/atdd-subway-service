e0cd92fb78066c219cd7c78e6d626eb7790c1ee2
anwjrrp33 <anwjrrp33@gmail.com>
2022-11-30 21:17:10 +0900

    refactor: Auth 코드 리팩토링

--- src/main/java/nextstep/subway/auth/domain/LoginMember.java
+++ src/main/java/nextstep/subway/auth/domain/LoginMember.java
@@ -5,7 +5,7 @@ public class LoginMember {
     private String email;
     private Integer age;
 
-    public LoginMember() {
+    private LoginMember() {
     }
 
     public LoginMember(Long id, String email, Integer age) {
