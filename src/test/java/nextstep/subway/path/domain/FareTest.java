b39f5d954a6fcc5306f7166422e0cd2fdeb8398a
giraffelim <taeyang7704@naver.com>
2022-11-30 12:59:50 +0900

    test(FareTest): 요금을 감소시키는 기능에 대한 테스트 작성

--- src/test/java/nextstep/subway/path/domain/FareTest.java
+++ src/test/java/nextstep/subway/path/domain/FareTest.java
@@ -23,4 +23,24 @@ class FareTest {
 
         assertThat(fare.add(extraFare)).isEqualTo(new Fare(1500));
     }
+
+    @DisplayName("요금을 뺄 수 있다.")
+    @Test
+    void subtract() {
+        Fare fare = new Fare(1500);
+        Fare extraFare = new Fare(500);
+
+        assertThat(fare.subtract(extraFare)).isEqualTo(new Fare(1000));
+    }
+
+    @DisplayName("요금을 뺀 결과가 음수일 경우 예외가 발생한다.")
+    @Test
+    void subtractNegative() {
+        Fare fare = new Fare(1500);
+        Fare extraFare = new Fare(2000);
+
+        assertThatThrownBy(() -> fare.subtract(extraFare))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("요금은 음수일 수 없습니다.");
+    }
 }
