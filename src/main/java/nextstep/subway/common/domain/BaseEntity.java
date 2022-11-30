b9f3dde9cd03dd664dfe5596adf7886aef261317
bky4564 <bky4564@hunet.co.kr>
2022-11-30 10:48:07 +0900

    chore: 패키지 정리

--- src/main/java/nextstep/subway/common/domain/BaseEntity.java
+++ src/main/java/nextstep/subway/common/domain/BaseEntity.java
@@ -1,12 +1,13 @@
-package nextstep.subway;
+package nextstep.subway.common.domain;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
 
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
-import javax.persistence.EntityListeners;
-import javax.persistence.MappedSuperclass;
-import java.time.LocalDateTime;
 
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
