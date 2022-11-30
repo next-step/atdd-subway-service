1cee541bfdff383333566bbc003644edba76927f
chunhodong <yhms4432@naver.com>
2022-11-30 14:58:46 +0900

    test: PathFinderTest테스트추가
    - 노선없는경우 예외
    - 존재하지 않는 역을 조회
    - 연결되지 않는 역을 조회

--- src/main/java/nextstep/subway/path/domain/PathFinder.java
+++ src/main/java/nextstep/subway/path/domain/PathFinder.java
@@ -3,30 +3,62 @@ package nextstep.subway.path.domain;
 import nextstep.subway.line.domain.Line;
 import nextstep.subway.station.domain.Station;
 import nextstep.subway.station.dto.StationResponse;
+import org.jgrapht.GraphPath;
+import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
+import org.jgrapht.graph.DefaultWeightedEdge;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class PathFinder {
-    private final List<Line> lines = new ArrayList();
+    private static final String NONE_EQUAL_STATION = "출발역과 도착역 다른 경우만 조회할 수 있습니다";
+    private static final String NONE_EXISTS_STATION = "출발역과 도착역이 모두 존재해야합니다";
+    private static final String NONE_LINK_PATH = "출발역과 도착역이 연결되있어야 합니다";
+
+    private static final String NULL_LINES = "노선이 존재해야합니다";
+
+    private SubwayGraph subwayGraph;
 
     public PathFinder(List<Line> lines) {
-        this.lines.addAll(lines);
-    }
-
-    public PathFinder(Line line) {
-        lines.add(line);
-    }
-
-    public PathFinder() {
+        validateLines(lines);
+        this.subwayGraph = new SubwayGraph(DefaultWeightedEdge.class, lines);
 
     }
 
-    public List<StationResponse> getShortestPath(){
+    private void validateLines(List<Line> lines) {
+        if (Objects.isNull(lines)) {
+            throw new NullPointerException(NULL_LINES);
+        }
+    }
+
+    public List<StationResponse> getShortestPath(Station sourceStation, Station targetStation) {
+        validateEqualStation(sourceStation, targetStation);
+        validateExistsStation(sourceStation, targetStation);
+        validateLinkStation(sourceStation, targetStation);
+
+        //subwayGraph.containsVertex(startStation);
 
         return null;
     }
 
+    private void validateLinkStation(Station sourceStation, Station targetStation) {
+        GraphPath<Station, DefaultWeightedEdge> shortestPath = new DijkstraShortestPath<>(subwayGraph).getPath(sourceStation, targetStation);
 
+        if(Objects.isNull(shortestPath)){
+            throw new IllegalArgumentException(NONE_LINK_PATH);
+        }
+    }
+
+    private void validateEqualStation(Station sourceStation, Station targetStation) {
+        if (Objects.equals(sourceStation, targetStation)) {
+            throw new IllegalArgumentException(NONE_EQUAL_STATION);
+        }
+    }
+
+    private void validateExistsStation(Station sourceStation, Station targetStation) {
+        if (!subwayGraph.containsVertex(sourceStation) || !subwayGraph.containsVertex(targetStation)) {
+            throw new IllegalArgumentException(NONE_EXISTS_STATION);
+        }
+    }
 
 }
--- src/test/java/nextstep/subway/path/domain/PathFinderTest.java
+++ src/test/java/nextstep/subway/path/domain/PathFinderTest.java
@@ -1,8 +1,8 @@
 package nextstep.subway.path.domain;
 
 import nextstep.subway.line.domain.Line;
+import nextstep.subway.line.domain.Section;
 import nextstep.subway.station.domain.Station;
-import nextstep.subway.station.dto.StationResponse;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -12,15 +12,21 @@ import java.lang.reflect.Field;
 import java.util.Arrays;
 import java.util.Objects;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 public class PathFinderTest {
 
     private Line 신분당선;
     private Line 이호선;
     private Line 삼호선;
+    private Line 칠호선;
     private Station 강남역;
     private Station 양재역;
     private Station 교대역;
     private Station 남부터미널역;
+    private Station 학동역;
+    private Station 강남구청역;
+
 
     /**
      * 교대역    --- *2호선* ---   강남역
@@ -31,21 +37,57 @@ public class PathFinderTest {
      */
     @BeforeEach
     public void setUp() {
-        강남역 = createStation("강남역",1l);
-        양재역 = createStation("양재역",2l);
-        교대역 = createStation("교대역",3l);
-        남부터미널역 = createStation("남부터미널역",4l);
+        강남역 = createStation("강남역", 1l);
+        양재역 = createStation("양재역", 2l);
+        교대역 = createStation("교대역", 3l);
+        남부터미널역 = createStation("남부터미널역", 4l);
         신분당선 = createLine("신분당선", "bg-red-600", 강남역, 양재역, 10);
         이호선 = createLine("이호선", "bg-red-600", 교대역, 강남역, 10);
         삼호선 = createLine("삼호선", "bg-red-600", 교대역, 양재역, 5);
+        삼호선.addSection(new Section(삼호선, 교대역, 남부터미널역, 3));
+        학동역 = createStation("강남역", 10l);
+        강남구청역 = createStation("강남역", 11l);
+        칠호선 = createLine("칠호선", "bg-green-600", 학동역, 강남구청역, 10);
+
     }
 
+    @DisplayName("존재하지 않는 노선을 입력하면 예외발생 ")
+    @Test
+    void returnsExceptionWithNullLines() {
+        assertThatThrownBy(() -> new PathFinder(null))
+                .isInstanceOf(NullPointerException.class)
+                .hasMessageContaining("노선이 존재해야합니다");
+    }
 
     @DisplayName("최단경로를 조회할때 출발역과 도착역이 같으면 예외발생")
     @Test
-    void getLinesWithException() {
+    void returnsExceptionWithSameStations() {
+        PathFinder pathFinder = new PathFinder(Arrays.asList(신분당선, 이호선, 삼호선));
+
+        assertThatThrownBy(() -> pathFinder.getShortestPath(강남역, 강남역))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("출발역과 도착역 다른 경우만 조회할 수 있습니다");
     }
 
+    @DisplayName("존재하지 않는 역을 조회하면 같으면 예외발생")
+    @Test
+    void returnsExceptionWithNoneExistsStartStation() {
+        PathFinder pathFinder = new PathFinder(Arrays.asList(신분당선, 이호선, 삼호선));
+
+        assertThatThrownBy(() -> pathFinder.getShortestPath(createStation("마포역", 3l), 강남역))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("출발역과 도착역이 모두 존재해야합니다");
+    }
+
+    @DisplayName("출발역과 도착역이 연결되지않으면 예외발생")
+    @Test
+    void returnsExceptionWithNoneLinkStation() {
+        PathFinder pathFinder = new PathFinder(Arrays.asList(신분당선, 이호선, 삼호선, 칠호선));
+
+        assertThatThrownBy(() -> pathFinder.getShortestPath(학동역, 강남역))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("출발역과 도착역이 연결되있어야 합니다");
+    }
 
 
     public Station createStation(String name, long id) {
@@ -56,7 +98,7 @@ public class PathFinderTest {
         return station;
     }
 
-    public Line createLine(String name, String color, Station upStation, Station downStation, int distance){
+    public Line createLine(String name, String color, Station upStation, Station downStation, int distance) {
         return new Line(name, color, upStation, downStation, distance);
     }
 }
