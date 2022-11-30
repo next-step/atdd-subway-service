508c62c7c9ff21c70ed4072a63eb754cb25cba46
giraffelim <taeyang7704@naver.com>
2022-11-30 21:45:46 +0900

    feat(LoginMember): 로그인하지 않은 사용자인 GUEST 추가

--- src/main/java/nextstep/subway/auth/ui/AuthenticationPrincipalArgumentResolver.java
+++ src/main/java/nextstep/subway/auth/ui/AuthenticationPrincipalArgumentResolver.java
@@ -2,6 +2,7 @@ package nextstep.subway.auth.ui;
 
 import nextstep.subway.auth.application.AuthService;
 import nextstep.subway.auth.domain.AuthenticationPrincipal;
+import nextstep.subway.auth.domain.LoginMember;
 import nextstep.subway.auth.infrastructure.AuthorizationExtractor;
 import org.springframework.core.MethodParameter;
 import org.springframework.web.bind.support.WebDataBinderFactory;
@@ -26,6 +27,9 @@ public class AuthenticationPrincipalArgumentResolver implements HandlerMethodArg
     @Override
     public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) {
         String credentials = AuthorizationExtractor.extract(webRequest.getNativeRequest(HttpServletRequest.class));
+        if (credentials == null) {
+            return LoginMember.GUEST;
+        }
         return authService.findMemberByToken(credentials);
     }
 }
