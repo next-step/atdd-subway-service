eedc7cee64f837a4dc99c12f564767e7d00709bd
sixthou <justyuk@naver.com>
2022-11-30 20:33:59 +0900

    test : 경로조회 테스트 작성

--- src/test/java/nextstep/subway/path/applicaiton/PathServiceTest.java
+++ src/test/java/nextstep/subway/path/applicaiton/PathServiceTest.java
@@ -1,19 +1,83 @@
 package nextstep.subway.path.applicaiton;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.mockito.BDDMockito.given;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import nextstep.subway.line.domain.Section;
+import nextstep.subway.line.domain.SectionRepository;
+import nextstep.subway.station.domain.Station;
+import nextstep.subway.station.domain.StationRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+@ExtendWith(MockitoExtension.class)
 class PathServiceTest {
+
+    @Mock
+    StationRepository stationRepository;
+
+    @Mock
+    SectionRepository sectionRepository;
+
+    @InjectMocks
+    PathService pathService;
+
+    private Station 강남역;
+    private Station 양재역;
+    private Station 교대역;
+    private Station 남부터미널역;
+
+    private Section 강남_교대;
+    private Section 교대_강남;
+    private Section 교대_남부;
+    private Section 남부_양재;
+    /**
+     * 교대역    --- *2호선* ---   강남역
+     * |                        |
+     * *3호선*                   *신분당선*
+     * |                        |
+     * 남부터미널역  --- *3호선* ---   양재역
+     */
+
+    @BeforeEach
+    public void setUp() {
+
+        교대역 = new Station("교대역");
+        강남역 = new Station("강남역");
+        양재역 = new Station("양재역");
+        남부터미널역 = new Station("남부터미널역");
+
+        강남_교대 = new Section(null, 강남역, 양재역, 10);
+        교대_강남 = new Section(null, 교대역, 강남역, 10);
+        교대_남부 = new Section(null, 교대역, 남부터미널역, 2);
+        남부_양재 = new Section(null, 남부터미널역, 양재역, 3);
+    }
 
     @DisplayName("출발역과 도착역으로 최단거리를 조회할 수 있다.")
     @Test
     void shortest_path() {
-        //given
+        given(stationRepository.findById(1L)).willReturn(Optional.of(교대역));
+        given(stationRepository.findById(2L)).willReturn(Optional.of(양재역));
+        given(stationRepository.findAll()).willReturn(Arrays.asList(교대역, 강남역, 양재역, 남부터미널역));
+        given(sectionRepository.findAll()).willReturn(Arrays.asList(강남_교대, 교대_강남, 교대_남부, 남부_양재));
 
         //when
+        List<Station> stations = pathService.findPaths(1L, 2L);
 
         //then
-
+        assertAll(
+                () -> assertThat(stations.size()).isEqualTo(3),
+                () -> assertThat(stations).containsExactly(교대역, 남부터미널역, 양재역)
+        );
     }
 
     @DisplayName("출발역과 도착역이 같은 경우 IllegalArgumentException 이 발생한다.")
