73653d2a3ee2f36b7d2276e1793c40d3b72305fd
giraffelim <taeyang7704@naver.com>
2022-11-30 23:30:42 +0900

    refactor(AgeDiscountFareTest): 연령대별 할인된 요금 계산 결과 변경

--- src/test/java/nextstep/subway/path/domain/AgeDiscountFareTest.java
+++ src/test/java/nextstep/subway/path/domain/AgeDiscountFareTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 class AgeDiscountFareTest {
-    @DisplayName("연령대별 할인 요금을 계산 할 수 있다.")
+    @DisplayName("연령대별 할인된 요금을 계산 할 수 있다.")
     @ParameterizedTest
     @CsvSource(value = {"3:2450:0", "10:2450:1050", "18:2450:1680", "26:2450:2450"}, delimiter = ':')
     void calculate(int age, int fare, int expectedFare) {
