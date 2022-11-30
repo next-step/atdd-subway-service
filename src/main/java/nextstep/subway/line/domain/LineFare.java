0f1f48583ae3d7def16f29f30a9d25e78da50256
ilmare-cbk <cbk104325@gmail.com>
2022-12-01 01:06:56 +0900

    feat(LineFare.java): 요금 비교를 위한 compareTo 메소드 오버라이딩
    - equals, compareTo 테스트 추가

--- src/main/java/nextstep/subway/line/domain/LineFare.java
+++ src/main/java/nextstep/subway/line/domain/LineFare.java
@@ -4,9 +4,10 @@ import nextstep.subway.exception.NegativeOverFareException;
 import nextstep.subway.message.ExceptionMessage;
 
 import javax.persistence.Embeddable;
+import java.util.Objects;
 
 @Embeddable
-public class LineFare {
+public class LineFare implements Comparable<LineFare> {
     private static final int MIN_FARE = 0;
 
     private int fare;
@@ -31,5 +32,23 @@ public class LineFare {
         if (fare < MIN_FARE) {
             throw new NegativeOverFareException(ExceptionMessage.INVALID_OVER_FARE);
         }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        LineFare lineFare = (LineFare) o;
+        return fare == lineFare.fare;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(fare);
+    }
+
+    @Override
+    public int compareTo(LineFare o) {
+        return Integer.compare(this.fare, o.fare);
     }
 }
--- src/test/java/nextstep/subway/line/domain/LineFareTest.java
+++ src/test/java/nextstep/subway/line/domain/LineFareTest.java
@@ -4,8 +4,12 @@ import nextstep.subway.exception.NegativeOverFareException;
 import nextstep.subway.message.ExceptionMessage;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 class LineFareTest {
 
@@ -24,6 +28,38 @@ class LineFareTest {
     void create(int input) {
         LineFare lineFare = LineFare.from(input);
 
-        Assertions.assertThat(lineFare).isNotNull();
+        assertThat(lineFare).isNotNull();
+    }
+
+    @DisplayName("추가요금이 같은 LineFare 객체는 동등하다.")
+    @Test
+    void equals() {
+        LineFare fare1 = LineFare.from(10);
+        LineFare fare2 = LineFare.from(10);
+
+        assertThat(fare1).isEqualTo(fare2);
+    }
+
+    @DisplayName("추가요금이 다른 LineFare 객체는 동등하지 않다.")
+    @Test
+    void notEquals() {
+        LineFare fare1 = LineFare.from(10);
+        LineFare fare2 = LineFare.from(20);
+
+        assertThat(fare1).isNotEqualTo(fare2);
+    }
+
+    @DisplayName("LineFare 객체를 추가요금을 가지고 비교한다.")
+    @Test
+    void compare() {
+        LineFare fare1 = LineFare.from(20);
+        LineFare fare2 = LineFare.from(10);
+        LineFare fare3 = LineFare.from(10);
+
+        assertAll(
+                () -> assertThat(fare1.compareTo(fare2) > 0).isTrue(),
+                () -> assertThat(fare2.compareTo(fare1) < 0).isTrue(),
+                () -> assertThat(fare2.compareTo(fare3) == 0).isTrue()
+        );
     }
 }
