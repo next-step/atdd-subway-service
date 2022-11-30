f7033fef77167b7e08c64de4afa8c309e1f21665
sjk5766 <wjdrnr6662@gmail.com>
2022-11-30 10:06:19 +0900

    refactor: FareDistance 계산값을 Enum 요소로 추가해서 변경에 영향이 적도록 리팩토링

--- src/main/java/nextstep/subway/path/domain/FareDistance.java
+++ src/main/java/nextstep/subway/path/domain/FareDistance.java
@@ -1,39 +1,35 @@
 package nextstep.subway.path.domain;
 
 import java.util.Arrays;
+import java.util.function.Function;
 
 public enum FareDistance {
-    BASIC(0, 10, 0),
-    MIDDLE(11, 50, 5),
-    LONG(51, 178, 8);
+    BASIC(0, 10, distance -> Constant.BASIC_PRICE),
+    MIDDLE(11, 50, distance ->
+            BASIC.expression.apply(distance) + calculateFare(distance - BASIC.end, 5)),
+    LONG(51, 178, distance ->
+            MIDDLE.expression.apply(MIDDLE.end) + calculateFare(distance - MIDDLE.end, 8));
 
     private static final int ZERO = 0;
-    private static final int BASIC_PRICE = 1_250;
     private static final int INCREMENT_FARE = 100;
+
+    private static class Constant {
+        private static final int BASIC_PRICE = 1_250;
+    }
 
     private final int start;
     private final int end;
-    private final int condition;
+    private final Function<Integer, Integer> expression;
 
-    FareDistance(int start, int end, int condition) {
+    FareDistance(int start, int end, Function<Integer, Integer> expression) {
         this.start = start;
         this.end = end;
-        this.condition = condition;
+        this.expression = expression;
     }
 
     public static int calculate(int distance) {
         FareDistance fareDistance = findByDistance(distance);
-        if (fareDistance.equals(BASIC)) {
-            return BASIC_PRICE;
-        }
-
-        if (fareDistance.equals(MIDDLE)) {
-            return BASIC_PRICE + fareDistance.calculateFare(distance - MIDDLE.start + 1, MIDDLE.condition);
-        }
-
-        return BASIC_PRICE
-                + fareDistance.calculateFare(MIDDLE.end - MIDDLE.start + 1, MIDDLE.condition)
-                + fareDistance.calculateFare(distance - LONG.start + 1, LONG.condition);
+        return fareDistance.expression.apply(distance);
     }
 
     private static FareDistance findByDistance(int distance) {
@@ -47,7 +43,7 @@ public enum FareDistance {
         return start <= distance && distance <= end;
     }
 
-    private int calculateFare(int distance, int condition) {
+    private static int calculateFare(int distance, int condition) {
         if (distance <= ZERO) {
             return ZERO;
         }
