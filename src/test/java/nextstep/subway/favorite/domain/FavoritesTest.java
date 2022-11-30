98f0cb60715e08b268cbbbaf958ec75c393fabed
crongcm <ucm5459@kakao.com>
2022-12-01 02:06:09 +0900

    test: FavoritesTest 작성

---
+++ src/test/java/nextstep/subway/favorite/domain/FavoritesTest.java
@@ -0,0 +1,29 @@
+package nextstep.subway.favorite.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.util.Collections;
+import nextstep.subway.member.domain.Member;
+import nextstep.subway.station.domain.Station;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class FavoritesTest {
+    @Test
+    @DisplayName("즐겨찾기 목록 생성")
+    void createFavorites() {
+        Favorites actual = Favorites.from(Collections.singletonList(
+                Favorite.of(
+                        Member.of("email@email.com", "password", 20),
+                        Station.from("강남역"),
+                        Station.from("잠실역")
+                )));
+
+        assertAll(
+                () -> assertNotNull(actual),
+                () -> assertThat(actual).isInstanceOf(Favorites.class)
+        );
+    }
+}
