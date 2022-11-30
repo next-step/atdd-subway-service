88d83c167f5bfc271fe0848ac20fdb4fdd94e48f
dongha-byun <byunsw4@naver.com>
2022-12-01 03:01:18 +0900

    Refactor: 경로 조회 리펙터링

--- src/main/java/nextstep/subway/line/domain/PathGraph.java
+++ src/main/java/nextstep/subway/line/domain/PathGraph.java
@@ -1,6 +1,12 @@
 package nextstep.subway.line.domain;
 
 import java.util.List;
+import java.util.stream.Collectors;
+import nextstep.subway.line.dto.PathResponse;
+import nextstep.subway.station.application.StationService;
+import nextstep.subway.station.domain.Station;
+import nextstep.subway.station.domain.StationRepository;
+import nextstep.subway.station.dto.StationResponse;
 import org.jgrapht.GraphPath;
 import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
 import org.jgrapht.graph.DefaultWeightedEdge;
@@ -10,31 +16,66 @@ import org.springframework.stereotype.Component;
 @Component
 public class PathGraph {
 
-    public static GraphPath findPath(Long source, Long target, List<Line> lines){
+    private final StationRepository stationRepository;
+
+    public PathGraph(StationRepository stationRepository) {
+        this.stationRepository = stationRepository;
+    }
+
+    public PathResponse findPath(Long source, Long target, List<Line> lines){
         WeightedMultigraph<String, DefaultWeightedEdge> graph = new WeightedMultigraph(DefaultWeightedEdge.class);
+        DijkstraShortestPath stationGraph = getStationGraph(lines, graph);
+        GraphPath path = stationGraph.getPath(source.toString(), target.toString());
+
+        return new PathResponse(createStations(path), (int)path.getWeight());
+    }
+
+    private DijkstraShortestPath getStationGraph(List<Line> lines, WeightedMultigraph<String, DefaultWeightedEdge> graph) {
         setVertex(graph, lines);
         setEdgeWeight(graph, lines);
-        DijkstraShortestPath stationGraph = new DijkstraShortestPath(graph);
-        return stationGraph.getPath(source.toString(), target.toString());
+        return new DijkstraShortestPath(graph);
     }
-    private static void setVertex(WeightedMultigraph<String, DefaultWeightedEdge> graph, List<Line> lines) {
+
+    private void setVertex(WeightedMultigraph<String, DefaultWeightedEdge> graph, List<Line> lines) {
         lines.stream().forEach(
-                line -> line.getStations().stream().forEach(
-                        station -> graph.addVertex(station.getId().toString())
-                )
+                line -> addVertex(graph, line)
         );
     }
-    private static void setEdgeWeight(WeightedMultigraph<String, DefaultWeightedEdge> graph, List<Line> lines) {
+
+    private void addVertex(WeightedMultigraph<String, DefaultWeightedEdge> graph, Line line) {
+        line.getStations().stream().forEach(
+                station -> graph.addVertex(station.getId().toString())
+        );
+    }
+
+    private void setEdgeWeight(WeightedMultigraph<String, DefaultWeightedEdge> graph, List<Line> lines) {
         lines.stream().forEach(
-                line -> line.getSections()
-                        .stream()
-                        .forEach(
-                                section ->
-                                        graph.setEdgeWeight(
-                                                graph.addEdge(section.getUpStation().getId().toString(), section.getDownStation().getId().toString()),
-                                                section.getDistance()
-                                        )
+                line -> addEdgeWeight(graph, line)
+        );
+    }
+
+    private void addEdgeWeight(WeightedMultigraph<String, DefaultWeightedEdge> graph, Line line) {
+        line.getSections().stream().forEach(
+                section -> graph.setEdgeWeight(
+                                graph.addEdge(
+                                        section.getUpStation().getId().toString(),
+                                        section.getDownStation().getId().toString()
+                                ), section.getDistance()
                         )
         );
+    }
+
+    private List<StationResponse> createStations(GraphPath path) {
+        List<String> vertexList = path.getVertexList();
+        return vertexList.stream()
+                .map(vertex -> StationResponse.of(findStation(vertex)))
+                .collect(Collectors.toList());
+    }
+
+    private Station findStation(String vertex) {
+        return stationRepository.findById(Long.valueOf(vertex))
+                .orElseThrow(
+                        () -> new IllegalArgumentException("역 조회 실패")
+                );
     }
 }
--- src/test/java/nextstep/subway/line/domain/PathGraphTest.java
+++ src/test/java/nextstep/subway/line/domain/PathGraphTest.java
@@ -3,30 +3,42 @@ package nextstep.subway.line.domain;
 import static org.assertj.core.api.Assertions.*;
 
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import nextstep.subway.line.dto.PathResponse;
 import nextstep.subway.station.domain.Station;
-import org.jgrapht.GraphPath;
+import nextstep.subway.station.domain.StationRepository;
+import nextstep.subway.station.dto.StationResponse;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class PathGraphTest {
+
+    @Autowired
+    PathGraph pathGraph;
+
+    @Autowired
+    StationRepository stationRepository;
+
     @Test
     void path(){
         // given
-        Station station1 = new Station(1L, "암사역");
-        Station station2 = new Station(2L, "천호역");
-        Station station3 = new Station(3L, "몽촌토성역");
+        Station station1 = stationRepository.save(new Station("암사역"));
+        Station station2 = stationRepository.save(new Station("천호역"));
+        Station station3 = stationRepository.save(new Station("몽촌토성역"));
         Line line = new Line("8호선", "bg-color-555", station1, station2, 10);
         line.addSection(station2, station3, 10);
 
         // when
-        GraphPath path = PathGraph.findPath(station1.getId(), station3.getId(), Arrays.asList(line));
+        PathResponse pathResponse = pathGraph.findPath(station1.getId(), station3.getId(), Arrays.asList(line));
+        List<String> list = pathResponse.getStations().stream()
+                .map(StationResponse::getName)
+                .collect(Collectors.toList());
 
         // then
-        assertThat(path.getVertexList()).containsExactly(
-                station1.getId().toString(), station2.getId().toString(), station3.getId().toString()
-        );
-        assertThat((int)path.getWeight()).isEqualTo(20);
-
+        assertThat(list).containsExactly("암사역","천호역","몽촌토성역");
+        assertThat(pathResponse.getDistance()).isEqualTo(20);
     }
 }
