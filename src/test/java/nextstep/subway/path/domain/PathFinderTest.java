89424e317c48105d555df2d8d8a114e4fd3f2377
hybrid.simpler@gmail.com <hybrid.simpler@gmail.com>
2022-11-30 23:41:10 +0900

    test: 존재하지 않는 출발역이나 도착역을 조회 할 경우 예외 발생 테스트 추가

--- src/test/java/nextstep/subway/path/domain/PathFinderTest.java
+++ src/test/java/nextstep/subway/path/domain/PathFinderTest.java
@@ -8,24 +8,30 @@ import java.util.Arrays;
 import nextstep.subway.line.domain.Line;
 import nextstep.subway.station.domain.Station;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class PathFinderTest {
     private Line 신분당선;
     private Line 이호선;
     private Line 삼호선;
+    private Line 구호선;
     private Station 강남역;
     private Station 양재역;
     private Station 교대역;
     private Station 남부터미널역;
+    private Station 동작역;
+    private Station 석촌역;
 
     /**
-     * 교대역       --- *2호선(10)* ---   강남역
-     * |                                    |
+     * 교대역      ----- *2호선(10)* -----   강남역
+     * |                                   |
+     * |                                   |
      * *3호선(3)*                       *신분당선(10)*
-     * |                                    |
-     * 남부터미널역  --- *3호선(2)* ---     양재
+     * |                                   |
+     * |                                   |
+     * 남부터미널역 ----- *3호선(2)* -----     양재
+     *
+     * 동작역     ----- *9호선(13)* -----    석촌역
      */
     @BeforeEach
     public void setUp() {
@@ -33,9 +39,12 @@ public class PathFinderTest {
         양재역 = new Station("양재역");
         교대역 = new Station("교대역");
         남부터미널역 = new Station("남부터미널역");
+        동작역 = new Station("동작역");
+        석촌역 = new Station("석촌역");
         신분당선 = new Line("신분당선", "bg-red-600", 강남역, 양재역, 10);
         이호선 = new Line("이호선", "bg-red-600", 교대역, 강남역, 10);
         삼호선 = new Line("삼호선", "bg-red-600", 교대역, 양재역, 5);
+        구호선 = new Line("삼호선", "bg-red-600", 동작역, 석촌역, 13);
     }
 
     @Test
@@ -61,7 +70,15 @@ public class PathFinderTest {
     void 출발역과_도착역이_연결이_되어_있지_않는_경우_예외_발생() {
         PathFinder pathFinder = PathFinder.from(Arrays.asList(삼호선, 이호선));
 
-        assertThatThrownBy(() -> pathFinder.findShortestPath(교대역, 남부터미널역))
+        assertThatThrownBy(() -> pathFinder.findShortestPath(교대역, 동작역))
+                .isInstanceOf(RuntimeException.class);
+    }
+
+    @Test
+    void 존재하지_않는_출발역이나_도착역을_조회_할_경우_예외_발생() {
+        PathFinder pathFinder = PathFinder.from(Arrays.asList(삼호선, 이호선));
+
+        assertThatThrownBy(() -> pathFinder.findShortestPath(양재역, 남부터미널역))
                 .isInstanceOf(RuntimeException.class);
     }
 
