d15f946b186a2c214340dcc624ffb0da9bd91dcd
sixthou <justyuk@naver.com>
2022-11-30 16:51:26 +0900

    refactor : PathTestFixture 일부 수정 및 래핑 메서드 생성

--- src/test/java/nextstep/subway/path/acceptance/PathTestFixture.java
+++ src/test/java/nextstep/subway/path/acceptance/PathTestFixture.java
@@ -5,16 +5,21 @@ import static org.assertj.core.api.Assertions.assertThat;
 import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
+import nextstep.subway.station.dto.StationResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
 public class PathTestFixture {
 
+    public static ExtractableResponse<Response> 출발역에서_도착역_경로_조회됨(StationResponse source, StationResponse target) {
+        return 출발역에서_도착역_경로_조회_요청(source.getId(), target.getId());
+    }
+
     public static ExtractableResponse<Response> 출발역에서_도착역_경로_조회_요청(Long sourceId, Long targetId) {
         return RestAssured
                 .given().log().all()
                 .accept(MediaType.APPLICATION_JSON_VALUE)
-                .when().get("/lines//paths?source={}&targetId={}", sourceId, targetId)
+                .when().get("/paths?source={sourceId}&target={targetId}", sourceId, targetId)
                 .then().log().all()
                 .extract();
     }
