da988bd89d963eff7b7127e0e6ca16d858176996
sawhong <sawhong@gmarket.com>
2022-11-30 23:10:25 +0900

    test: distance관련 단위테스트 작성

--- src/main/java/nextstep/subway/exception/type/ValidExceptionType.java
+++ src/main/java/nextstep/subway/exception/type/ValidExceptionType.java
@@ -3,7 +3,8 @@ package nextstep.subway.exception.type;
 public enum ValidExceptionType {
     STATION_BOTH_NOT_EXIST("등록할 수 없는 구간 입니다"),
     ALREADY_EXIST_LINE_STATION("이미 등록된 구간 입니다."),
-
+    NOT_ZERO_VALID_DISTANCE("거리는 0보다 커야합니다"),
+    NOT_SHORT_VALID_DISTANCE("역과 역 사이의 거리보다 좁은 거리를 입력해주세요"),
     SECTIONS_MIN_SIZE_ONE("구간의 사이즈는 1보다 커야합니다");
 
     private final String message;
--- src/main/java/nextstep/subway/line/domain/Distance.java
+++ src/main/java/nextstep/subway/line/domain/Distance.java
@@ -1,25 +1,39 @@
 package nextstep.subway.line.domain;
 
+import nextstep.subway.exception.NotValidDataException;
+import nextstep.subway.exception.type.ValidExceptionType;
+
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
+
+import static nextstep.subway.exception.type.ValidExceptionType.NOT_SHORT_VALID_DISTANCE;
 
 @Embeddable
 public class Distance {
 
+    private static final int SIZE_ZERO = 0;
     @Column(nullable = false)
     private int distance;
 
     protected Distance() {}
 
     private Distance(int distance) {
+        validCheckMinDistanceSize(distance);
         this.distance = distance;
     }
 
     public void validCheckOverDistance(int distance) {
         if (this.distance <= distance) {
-            throw new RuntimeException("역과 역 사이의 거리보다 좁은 거리를 입력해주세요");
+            throw new NotValidDataException(NOT_SHORT_VALID_DISTANCE.getMessage());
         }
     }
+
+    public void validCheckMinDistanceSize(int distance) {
+        if (distance <= SIZE_ZERO) {
+            throw new NotValidDataException(ValidExceptionType.NOT_ZERO_VALID_DISTANCE.getMessage());
+        }
+    }
+
 
     public Distance minus(int distance) {
         return Distance.from(this.distance - distance);
