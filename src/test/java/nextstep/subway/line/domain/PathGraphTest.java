02fb979e7e15ba5aa872433379ff81687fa2c643
dongha-byun <byunsw4@naver.com>
2022-12-01 01:39:36 +0900

    Test: PathGrapthTest 수정

--- src/test/java/nextstep/subway/line/domain/PathGraphTest.java
+++ src/test/java/nextstep/subway/line/domain/PathGraphTest.java
@@ -2,54 +2,31 @@ package nextstep.subway.line.domain;
 
 import static org.assertj.core.api.Assertions.*;
 
-import nextstep.subway.line.application.LineService;
-import nextstep.subway.line.dto.LineRequest;
-import nextstep.subway.line.dto.LineResponse;
-import nextstep.subway.station.application.StationService;
-import nextstep.subway.station.dto.StationRequest;
-import nextstep.subway.station.dto.StationResponse;
+import java.util.Arrays;
+import nextstep.subway.station.domain.Station;
 import org.jgrapht.GraphPath;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.transaction.annotation.Transactional;
 
-@Transactional
 @SpringBootTest
 public class PathGraphTest {
-
-    @Autowired
-    LineService lineService;
-
-    @Autowired
-    StationService stationService;
-
-    @Autowired
-    LineRepository lineRepository;
-
-    StationResponse station1;
-    StationResponse station2;
-    LineResponse line;
-    @BeforeEach
-    void beforeEach(){
-        station1 = stationService.saveStation(new StationRequest("암사역"));
-        station2 = stationService.saveStation(new StationRequest("천호역"));
-        line = lineService.saveLine(new LineRequest("8호선", "bg-color-333", station1.getId(), station2.getId(), 10));
-    }
-
     @Test
     void path(){
         // given
+        Station station1 = new Station(1L, "암사역");
+        Station station2 = new Station(2L, "천호역");
+        Station station3 = new Station(3L, "몽촌토성역");
+        Line line = new Line("8호선", "bg-color-555", station1, station2, 10);
+        line.addSection(station2, station3, 10);
 
         // when
-        GraphPath path = PathGraph.findPath(station1.getId(), station2.getId(), lineRepository.findAll());
+        GraphPath path = PathGraph.findPath(station1.getId(), station3.getId(), Arrays.asList(line));
 
         // then
         assertThat(path.getVertexList()).containsExactly(
-                station1.getId().toString(), station2.getId().toString()
+                station1.getId().toString(), station2.getId().toString(), station3.getId().toString()
         );
-        assertThat((int)path.getWeight()).isEqualTo(10);
+        assertThat((int)path.getWeight()).isEqualTo(20);
 
     }
 }
