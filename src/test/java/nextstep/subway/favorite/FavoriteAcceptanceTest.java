605316584a2dae47875a5d9f0b31d638c0c7c69e
seogineer <dgseo8981@gmail.com>
2022-12-01 15:59:26 +0900

    test: 즐겨찾기 인수 테스트 추가

--- src/test/java/nextstep/subway/favorite/FavoriteAcceptanceTest.java
+++ src/test/java/nextstep/subway/favorite/FavoriteAcceptanceTest.java
@@ -1,8 +1,71 @@
 package nextstep.subway.favorite;
 
+import static nextstep.subway.auth.acceptance.AuthAcceptanceTest.로그인_요청;
+import static nextstep.subway.member.MemberAcceptanceTest.회원_생성을_요청;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
 import nextstep.subway.AcceptanceTest;
+import nextstep.subway.favorite.dto.FavoriteRequest;
+import nextstep.subway.line.acceptance.LineAcceptanceTest;
+import nextstep.subway.line.dto.LineRequest;
+import nextstep.subway.line.dto.LineResponse;
+import nextstep.subway.member.dto.MemberResponse;
+import nextstep.subway.station.StationAcceptanceTest;
+import nextstep.subway.station.dto.StationResponse;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 
 @DisplayName("즐겨찾기 관련 기능")
 class FavoriteAcceptanceTest extends AcceptanceTest {
+    private StationResponse 강남역;
+    private StationResponse 광교역;
+    private LineRequest lineRequest;
+    private LineResponse 신분당선;
+    private MemberResponse 사용자;
+    private final String EMAIL = "email@email.com";
+    private final String PASSWORD = "password";
+    private final int AGE = 20;
+
+    @BeforeEach
+    public void setUp() {
+        super.setUp();
+
+        // given
+        강남역 = StationAcceptanceTest.지하철역_등록되어_있음("강남역").as(StationResponse.class);
+        광교역 = StationAcceptanceTest.지하철역_등록되어_있음("광교역").as(StationResponse.class);
+
+        lineRequest = new LineRequest("신분당선", "bg-red-600", 강남역.getId(), 광교역.getId(), 10);
+        신분당선 = LineAcceptanceTest.지하철_노선_등록되어_있음(lineRequest).as(LineResponse.class);
+
+        회원_생성을_요청(EMAIL, PASSWORD, AGE);
+        사용자 = 로그인_요청(EMAIL, PASSWORD).as(MemberResponse.class);
+    }
+
+    @DisplayName("즐겨찾기를 생성한다.")
+    @Test
+    void createFavorite() {
+        ExtractableResponse<Response> response = 즐겨찾기_생성_요청(new FavoriteRequest(강남역.getId(), 광교역.getId()));
+
+        즐겨찾기_생성됨(response);
+    }
+
+    public static ExtractableResponse<Response> 즐겨찾기_생성_요청(FavoriteRequest favoriteRequest) {
+        return RestAssured
+                .given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(favoriteRequest)
+                .when().post("/favorites")
+                .then().log().all()
+                .extract();
+    }
+
+    public static void 즐겨찾기_생성됨(ExtractableResponse<Response> response){
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+    }
 }
