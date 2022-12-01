456d2d041115ed6dc9fde58f7c1106cc15062636
sixthou <justyuk@naver.com>
2022-12-01 10:34:28 +0900

    refactor : Transactional readOnly 추가

--- src/main/java/nextstep/subway/path/applicaiton/PathService.java
+++ src/main/java/nextstep/subway/path/applicaiton/PathService.java
@@ -15,7 +15,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
-@Transactional
+@Transactional(readOnly = true)
 public class PathService {
 
     private final StationRepository stationRepository;
