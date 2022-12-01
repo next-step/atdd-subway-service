2205c8d918308063e222b4fabca9ff2b4d117935
CHEESE <81912261+ychxexn@users.noreply.github.com>
2022-12-01 22:58:28 +0900

    refactor: 역 조회 StationService 호출로 변경

--- src/main/java/nextstep/subway/path/application/PathService.java
+++ src/main/java/nextstep/subway/path/application/PathService.java
@@ -1,10 +1,10 @@
 package nextstep.subway.path.application;
 
-import nextstep.subway.exception.EntityNotFoundException;
 import nextstep.subway.line.domain.SectionRepository;
 import nextstep.subway.path.domain.PathFinder;
 import nextstep.subway.path.dto.PathRequest;
 import nextstep.subway.path.dto.PathResponse;
+import nextstep.subway.station.application.StationService;
 import nextstep.subway.station.domain.Station;
 import nextstep.subway.station.domain.StationRepository;
 import org.springframework.stereotype.Service;
@@ -12,25 +12,19 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class PathService {
+    private final StationService stationService;
     private final SectionRepository sectionRepository;
-    private final StationRepository stationRepository;
 
-    public PathService(SectionRepository sectionRepository, StationRepository stationRepository) {
+    public PathService(StationService stationService, SectionRepository sectionRepository) {
+        this.stationService = stationService;
         this.sectionRepository = sectionRepository;
-        this.stationRepository = stationRepository;
     }
 
     @Transactional(readOnly = true)
     public PathResponse getShortestPath(PathRequest pathRequest) {
-        Station source = findStationById(pathRequest.getSource());
-        Station target = findStationById(pathRequest.getTarget());
+        Station source = stationService.findStationById(pathRequest.getSource());
+        Station target = stationService.findStationById(pathRequest.getTarget());
 
         return PathResponse.of(new PathFinder(sectionRepository.findAll()).getShortestPath(source, target));
-    }
-
-    @Transactional(readOnly = true)
-    public Station findStationById(Long stationId) {
-        return stationRepository.findById(stationId)
-                .orElseThrow(() -> new EntityNotFoundException("역이 존재하지 않습니다.", stationId));
     }
 }
