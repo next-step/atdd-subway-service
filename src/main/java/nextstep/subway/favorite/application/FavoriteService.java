e75ebb867a0e7f9ce0e399e1523409f4230d59b3
chunhodong <yhms4432@naver.com>
2022-12-01 17:04:34 +0900

    feat: FavoriteServiceTest에 테스트추가
    - 즐겨찾기 목록조회
    - 즐겨찾기 삭제시 없는경우 예외
    - 즐겨찾기 삭제시 주인과 다른경우 예외

--- src/main/java/nextstep/subway/favorite/application/FavoriteService.java
+++ src/main/java/nextstep/subway/favorite/application/FavoriteService.java
@@ -1,5 +1,7 @@
 package nextstep.subway.favorite.application;
 
+import nextstep.subway.favorite.domain.Favorite;
+import nextstep.subway.favorite.domain.FavoriteRepository;
 import nextstep.subway.favorite.dto.FavoriteResponse;
 import nextstep.subway.favorite.exception.FavoriteException;
 import nextstep.subway.member.domain.Member;
@@ -8,6 +10,10 @@ import nextstep.subway.station.domain.Station;
 import nextstep.subway.station.domain.StationRepository;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import static nextstep.subway.favorite.exception.FavoriteExceptionCode.*;
 
 @Service
@@ -15,17 +21,39 @@ public class FavoriteService {
 
     private final MemberRepository memberRepository;
     private final StationRepository stationRepository;
+    private final FavoriteRepository favoriteRepository;
 
-    public FavoriteService(MemberRepository memberRepository, StationRepository stationRepository) {
+
+    public FavoriteService(MemberRepository memberRepository, StationRepository stationRepository, FavoriteRepository favoriteRepository) {
         this.memberRepository = memberRepository;
         this.stationRepository = stationRepository;
+        this.favoriteRepository = favoriteRepository;
     }
 
     public FavoriteResponse createFavorite(Long memberId, Long srcStationId, Long targetStationId) {
         Member member = memberRepository.findById(memberId).orElseThrow(() -> new FavoriteException(NONE_EXISTS_MEMBER));
-        Station srcStation = stationRepository.findById(srcStationId).orElseThrow(() -> new FavoriteException(NONE_EXISTS_SOURCE_STATION));
+        Station sourceStation = stationRepository.findById(srcStationId).orElseThrow(() -> new FavoriteException(NONE_EXISTS_SOURCE_STATION));
         Station targetStation = stationRepository.findById(targetStationId).orElseThrow(() -> new FavoriteException(NONE_EXISTS_TARGET_STATION));
-        return FavoriteResponse.of(member, srcStation, targetStation);
+        favoriteRepository.findByMemberAndSourceStationAndTargetStation(member, sourceStation, targetStation)
+                .ifPresent(favorite -> {
+                    throw new FavoriteException(ALREADY_REGISTER);
+                });
+        return FavoriteResponse.of(member, sourceStation, targetStation);
+    }
 
+    public List<FavoriteResponse> findAllFavoriteByMember(Long memberId) {
+        Member member = memberRepository.findById(memberId).orElseThrow(() -> new FavoriteException(NONE_EXISTS_MEMBER));
+        return favoriteRepository.findAllByMember(member)
+                .stream()
+                .map(favorite -> FavoriteResponse.of(favorite))
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void deleteFavorite(Long favoriteId,Long memberId) {
+        Member member = memberRepository.findById(memberId).orElseThrow(() -> new FavoriteException(NONE_EXISTS_MEMBER));
+        Favorite favorite = favoriteRepository.findById(favoriteId).orElseThrow(() -> new FavoriteException(NONE_EXISTS_FAVORITE));
+        favorite.validateOwner(member);
+        favoriteRepository.delete(favorite);
     }
 }
