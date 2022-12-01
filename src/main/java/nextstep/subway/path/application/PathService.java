74c329060bad5d099e7d5d339ac7fefa89f0e662
CHEESE <81912261+ychxexn@users.noreply.github.com>
2022-12-01 22:59:36 +0900

    refactor: 클래스 단위 트랜잭션 설정 추가

--- src/main/java/nextstep/subway/path/application/PathService.java
+++ src/main/java/nextstep/subway/path/application/PathService.java
@@ -6,11 +6,11 @@ import nextstep.subway.path.dto.PathRequest;
 import nextstep.subway.path.dto.PathResponse;
 import nextstep.subway.station.application.StationService;
 import nextstep.subway.station.domain.Station;
-import nextstep.subway.station.domain.StationRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional(readOnly = true)
 public class PathService {
     private final StationService stationService;
     private final SectionRepository sectionRepository;
