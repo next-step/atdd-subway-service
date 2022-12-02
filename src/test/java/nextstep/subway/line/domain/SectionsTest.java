f0c28c6f1cadf1ef14275ba40658bbe3777f0d81
jimbaemon <jimbae@kakao.com>
2022-12-03 00:02:16 +0900

    test: static method 보다 테스트를 위쪽으로 배치, BDD타입으로 정리.

--- src/test/java/nextstep/subway/line/domain/SectionsTest.java
+++ src/test/java/nextstep/subway/line/domain/SectionsTest.java
@@ -6,9 +6,11 @@ import static nextstep.subway.line.domain.StationFixture.선릉역;
 import static nextstep.subway.line.domain.StationFixture.스타벅스역;
 import static nextstep.subway.line.domain.StationFixture.역삼역;
 
+import java.util.List;
 import nextstep.subway.line.exception.InvalidRemoveException;
 import nextstep.subway.line.exception.NoRelateStationException;
 import nextstep.subway.line.exception.SectionAlreadyExistException;
+import nextstep.subway.station.domain.Station;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -16,18 +18,29 @@ class SectionsTest {
 
     @Test
     void 정류장_순서별_조회() {
+        //given
         Sections sections = Sections
             .of(new Section(null, 강남역, 역삼역, 4), new Section(null, 역삼역, 블루보틀역, 3));
-        Assertions.assertThat(sections.getStations())
+
+        //when
+        List<Station> stations = sections.getStations();
+
+        //then
+        Assertions.assertThat(stations)
             .hasSize(3)
             .containsExactly(강남역, 역삼역, 블루보틀역);
     }
 
     @Test
     void 구간_추가() {
+        //given
         Sections sections = Sections
             .of(new Section(null, 강남역, 역삼역, 4), new Section(null, 역삼역, 블루보틀역, 3));
+
+        //when
         sections.add(null, 블루보틀역, 선릉역, new Distance(3));
+
+        //then
         Assertions.assertThat(sections.getStations())
             .hasSize(4)
             .containsExactly(강남역, 역삼역, 블루보틀역, 선릉역);
@@ -35,8 +48,11 @@ class SectionsTest {
 
     @Test
     void 구간_추가중_해당_구간이_이미_있을경우_에러발생() {
+        //given
         Sections sections = Sections
             .of(new Section(null, 강남역, 역삼역, 4), new Section(null, 역삼역, 블루보틀역, 3));
+
+        //when & then
         Assertions.assertThatThrownBy(() -> sections.add(null, 역삼역, 블루보틀역, new Distance(3)))
             .isInstanceOf(SectionAlreadyExistException.class)
             .hasMessage("이미 등록된 구간 입니다.");
@@ -44,8 +60,11 @@ class SectionsTest {
 
     @Test
     void 구간_추가중_구간에_연결되는_역이_없을경우_에러발생생() {
+        //given
         Sections sections = Sections
             .of(new Section(null, 강남역, 역삼역, 4), new Section(null, 역삼역, 블루보틀역, 3));
+
+        //when & then
         Assertions.assertThatThrownBy(() -> sections.add(null, 선릉역, 스타벅스역, new Distance(3)))
             .isInstanceOf(NoRelateStationException.class)
             .hasMessage("등록할 수 없는 구간 입니다.");
@@ -53,9 +72,14 @@ class SectionsTest {
 
     @Test
     void 구간_제거() {
+        //given
         Sections sections = Sections
             .of(new Section(null, 강남역, 역삼역, 4), new Section(null, 역삼역, 블루보틀역, 3));
+
+        //when
         sections.remove(null, 블루보틀역);
+
+        //then
         Assertions.assertThat(sections.getStations())
             .hasSize(2)
             .containsExactly(강남역, 역삼역);
@@ -63,7 +87,10 @@ class SectionsTest {
 
     @Test
     void 구간_제거할_항목이_없을경우_에러_발생() {
+        //given
         Sections sections = Sections.of(new Section(null, 강남역, 역삼역, 4));
+
+        //when & then
         Assertions.assertThatThrownBy(() -> sections.remove(null, 강남역))
             .isInstanceOf(InvalidRemoveException.class)
             .hasMessage("해당 정류장 제거시 구간이 모두 사라집니다.");
