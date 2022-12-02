e43e8c2ac9131f5f992ef1b2c777092bef891ec5
hybrid.simpler@gmail.com <hybrid.simpler@gmail.com>
2022-12-02 22:06:08 +0900

    fix: exception message에 대한 npe 처리

--- src/main/java/nextstep/subway/common/exception/RestControllerAdvice.java
+++ src/main/java/nextstep/subway/common/exception/RestControllerAdvice.java
@@ -1,16 +1,19 @@
 package nextstep.subway.common.exception;
 
 import java.util.HashMap;
+import nextstep.subway.auth.application.AuthorizationException;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @org.springframework.web.bind.annotation.RestControllerAdvice
 public class RestControllerAdvice {
 
-    @ExceptionHandler({IllegalStateException.class, IllegalArgumentException.class, RuntimeException.class})
+    @ExceptionHandler({IllegalStateException.class, IllegalArgumentException.class, RuntimeException.class,
+            AuthorizationException.class})
     public ResponseEntity<HashMap> IllegalStateException(Exception e) {
         HashMap<Object, Object> errorMap = new HashMap<>();
-        errorMap.put("errorMessage", e.getMessage());
+        errorMap.put("errorMessage", StringUtils.defaultString(e.getMessage()));
         return ResponseEntity.badRequest().body(errorMap);
     }
 
