50e81e227671c04088d0fc927ae63ed92c208440
sixthou <justyuk@naver.com>
2022-12-02 14:40:43 +0900

    test : 인수테스트 시나리오 작성

--- src/test/java/nextstep/subway/auth/acceptance/AuthAcceptanceTest.java
+++ src/test/java/nextstep/subway/auth/acceptance/AuthAcceptanceTest.java
@@ -9,16 +9,27 @@ public class AuthAcceptanceTest extends AcceptanceTest {
     @DisplayName("Bearer Auth")
     @Test
     void myInfoWithBearerAuth() {
+        //  Given 회원 등록되어 있음
+        //  When 로그인 요청
+        //  Then 로그인 됨
     }
 
     @DisplayName("Bearer Auth 로그인 실패")
     @Test
     void myInfoWithBadBearerAuth() {
+        //  Given 회원 등록되어 있음
+        //  When 로그인 요청
+        //  Then 로그인 실패
     }
 
     @DisplayName("Bearer Auth 유효하지 않은 토큰")
     @Test
     void myInfoWithWrongBearerAuth() {
+        //  Given 회원 등록되어 있음
+        // Given 회원 등록되어 있음
+        //  given 로그인 요청
+        //  When /members/me에 유효하지 않는 요청
+        // then 요청 실패함.
     }
 
 }
