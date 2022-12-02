d8dc3921c09868031ffed58cd9e83d03c7d29cb8
seogineer <dgseo8981@gmail.com>
2022-12-02 22:32:22 +0900

    test: Favorite 도메인 테스트 추가 및 FetchType.LAZY 추가

---
+++ src/test/java/nextstep/subway/favorite/FavoriteTest.java
@@ -0,0 +1,44 @@
+package nextstep.subway.favorite;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+import nextstep.subway.favorite.domain.Favorite;
+import nextstep.subway.member.domain.Member;
+import nextstep.subway.station.domain.Station;
+import org.junit.jupiter.api.Test;
+
+class FavoriteTest {
+    private final Station 강남역 = new Station("강남역");
+    private final Station 광교역 = new Station("광교역");
+    private final String EMAIL = "email@email.com";
+    private final String PASSWORD = "password";
+    private final Integer AGE = 30;
+    private final Member 사용자 = new Member(EMAIL, PASSWORD, AGE);
+
+    @Test
+    void 생성() {
+        Favorite favorite = new Favorite(강남역, 광교역, 사용자);
+        assertAll(
+                () -> assertThat(favorite.getSource()).isEqualTo(강남역),
+                () -> assertThat(favorite.getTarget()).isEqualTo(광교역),
+                () -> assertThat(favorite.getMember()).isEqualTo(사용자)
+        );
+    }
+
+    @Test
+    void 유효성_검증() {
+        assertAll(
+                () -> assertThatThrownBy(
+                        () -> new Favorite(null, 광교역, 사용자)
+                ).isInstanceOf(IllegalArgumentException.class),
+                () -> assertThatThrownBy(
+                        () -> new Favorite(강남역, null, 사용자)
+                ).isInstanceOf(IllegalArgumentException.class),
+                () -> assertThatThrownBy(
+                        () -> new Favorite(강남역, 광교역, null)
+                ).isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+}
