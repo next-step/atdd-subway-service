c9b5b6724010e6ccdcd421245a44e2333d574684
sixthou <justyuk@naver.com>
2022-12-02 17:36:34 +0900

    test : 나의 정보 관리 인수테스트 작성

--- src/test/java/nextstep/subway/member/MemberAcceptanceTest.java
+++ src/test/java/nextstep/subway/member/MemberAcceptanceTest.java
@@ -1,5 +1,9 @@
 package nextstep.subway.member;
 
+import static nextstep.subway.auth.acceptance.AuthTestFixture.로그인을_요청;
+import static nextstep.subway.member.MemberTestFixture.나의_정보_삭제_요청;
+import static nextstep.subway.member.MemberTestFixture.나의_정보_수정_요청;
+import static nextstep.subway.member.MemberTestFixture.나의_정보_조회_요청;
 import static nextstep.subway.member.MemberTestFixture.회원_삭제_요청;
 import static nextstep.subway.member.MemberTestFixture.회원_삭제됨;
 import static nextstep.subway.member.MemberTestFixture.회원_생성됨;
@@ -12,6 +16,8 @@ import static nextstep.subway.member.MemberTestFixture.회원_정보_조회됨;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import nextstep.subway.AcceptanceTest;
+import nextstep.subway.auth.dto.TokenResponse;
+import nextstep.subway.member.dto.MemberRequest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -50,7 +56,25 @@ public class MemberAcceptanceTest extends AcceptanceTest {
     @DisplayName("나의 정보를 관리한다.")
     @Test
     void manageMyInfo() {
+        // given
+        회원_생성을_요청(EMAIL, PASSWORD, AGE);
+        TokenResponse token = 로그인을_요청(EMAIL, PASSWORD).as(TokenResponse.class);
+
+        // when
+        ExtractableResponse<Response> response = 나의_정보_조회_요청(token);
+        // then
+        회원_정보_조회됨(response, EMAIL, AGE);
+
+        // when
+        ExtractableResponse<Response> updateResponse
+                = 나의_정보_수정_요청(token, new MemberRequest(NEW_EMAIL, NEW_PASSWORD, NEW_AGE));
+        // then
+        회원_정보_수정됨(updateResponse);
+
+        // when
+        ExtractableResponse<Response> deleteResponse = 나의_정보_삭제_요청(token);
+        // then
+        회원_삭제됨(deleteResponse);
 
     }
-
 }
