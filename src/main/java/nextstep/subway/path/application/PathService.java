20b006952696c292839476f277f658d852a7a0b4
jylim <jylim@12cm.co.kr>
2022-12-02 14:50:41 +0900

    fix(PathService): StationsService 의존성 삭제 -> mock 테스트 시 nullPointError해결

--- src/main/java/nextstep/subway/path/application/PathService.java
+++ src/main/java/nextstep/subway/path/application/PathService.java
@@ -1,11 +1,11 @@
 package nextstep.subway.path.application;
 
+import nextstep.subway.exception.StationNotFoundException;
 import nextstep.subway.line.domain.LineRepository;
 import nextstep.subway.line.domain.Section;
 import nextstep.subway.path.domain.DijkstraPathFinder;
 import nextstep.subway.path.domain.PathFinder;
 import nextstep.subway.path.dto.PathResponse;
-import nextstep.subway.station.application.StationService;
 import nextstep.subway.station.domain.Station;
 import nextstep.subway.station.domain.StationRepository;
 import org.springframework.stereotype.Service;
@@ -14,24 +14,24 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import static nextstep.subway.utils.Message.NOT_EXISTS_STATION;
+
 @Service
 @Transactional(readOnly = true)
 public class PathService {
 
     private final LineRepository lineRepository;
     private final StationRepository stationRepository;
-    private final StationService stationService;
 
-    public PathService(LineRepository lineRepository, StationRepository stationRepository, StationService stationService) {
+    public PathService(LineRepository lineRepository, StationRepository stationRepository) {
         this.lineRepository = lineRepository;
         this.stationRepository = stationRepository;
-        this.stationService = stationService;
     }
 
 
     public PathResponse findShortestPath(Long source, Long target) {
-        Station sourceStation = stationService.findStationById(source);
-        Station targetStation = stationService.findStationById(target);
+        Station sourceStation = findStationById(source);
+        Station targetStation = findStationById(target);
         List<Section> sections = findAllSections();
 
         PathFinder pathFinder = DijkstraPathFinder.from(sections);
@@ -46,5 +46,10 @@ public class PathService {
                 .stream()
                 .flatMap(l -> l.getSections().stream())
                 .collect(Collectors.toList());
+    }
+
+    private Station findStationById(Long stationId) {
+        return stationRepository.findById(stationId)
+                .orElseThrow(() -> new StationNotFoundException(NOT_EXISTS_STATION));
     }
 }
