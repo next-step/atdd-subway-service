14b950077153ecb300276ef3f76f3578c68ca45c
sixthou <justyuk@naver.com>
2022-12-02 21:22:57 +0900

    refactor : 접근제어자 수정

--- src/test/java/nextstep/subway/member/MemberAcceptanceTest.java
+++ src/test/java/nextstep/subway/member/MemberAcceptanceTest.java
@@ -22,12 +22,12 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class MemberAcceptanceTest extends AcceptanceTest {
-    public static final String EMAIL = "email@email.com";
-    public static final String PASSWORD = "password";
-    public static final String NEW_EMAIL = "newemail@email.com";
-    public static final String NEW_PASSWORD = "newpassword";
-    public static final int AGE = 20;
-    public static final int NEW_AGE = 21;
+    private String EMAIL = "email@email.com";
+    private String PASSWORD = "password";
+    private String NEW_EMAIL = "newemail@email.com";
+    private String NEW_PASSWORD = "newpassword";
+    private int AGE = 20;
+    private int NEW_AGE = 21;
 
     @DisplayName("회원 정보를 관리한다.")
     @Test
