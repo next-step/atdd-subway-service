7cd6883863a1905b25742e6acf44471389103631
hybrid.simpler@gmail.com <hybrid.simpler@gmail.com>
2022-12-04 01:25:14 +0900

    feat: 추가요금 계산 기능 구현

--- src/main/java/nextstep/subway/auth/fare/domain/SubwayFareCalculator.java
+++ src/main/java/nextstep/subway/auth/fare/domain/SubwayFareCalculator.java
@@ -5,15 +5,40 @@ import nextstep.subway.common.exception.ErrorEnum;
 public class SubwayFareCalculator {
     public static final int ZERO_DISTANCE = 0;
     public static final int BASIC_FARE = 1_250;
+    public static final int UNIT_5_KM = 5;
+    public static final int UNIT_8_KM = 8;
+    public static final int SURCHARGE_FARE = 100;
     public static final int MAX_DISTANCE_OF_BASIC_FARE = 10;
+    public static final int FIRST_DISTANCE_SECTION = 10;
+    public static final int SECOND_DISTANCE_SECTION = 50;
 
     public static int calculate(int distance) {
-        if (distance <= ZERO_DISTANCE) {
-            throw new IllegalArgumentException(ErrorEnum.DISTANCE_GREATER_ZERO.message());
-        }
+        checkInvalidDistance(distance);
+        int fare = BASIC_FARE;
+
         if (distance <= MAX_DISTANCE_OF_BASIC_FARE) {
             return BASIC_FARE;
         }
-        return 0;
+
+        if (distance >= FIRST_DISTANCE_SECTION && distance <= SECOND_DISTANCE_SECTION) {
+            fare += calculateOverFare(distance, UNIT_5_KM);
+        }
+
+        if (distance > SECOND_DISTANCE_SECTION) {
+             fare += calculateOverFare(distance, UNIT_8_KM);
+        }
+
+        return fare;
+    }
+
+    private static void checkInvalidDistance(int distance) {
+        if (distance <= ZERO_DISTANCE) {
+            throw new IllegalArgumentException(ErrorEnum.DISTANCE_GREATER_ZERO.message());
+        }
+    }
+
+    private static int calculateOverFare(int distance, int unitKm) {
+        int overDistance = distance - MAX_DISTANCE_OF_BASIC_FARE;
+        return (int) ((Math.ceil((overDistance - 1) / unitKm) + 1) * SURCHARGE_FARE);
     }
 }
--- src/test/java/nextstep/subway/fare/domain/SubwayFareCalculatorTest.java
+++ src/test/java/nextstep/subway/fare/domain/SubwayFareCalculatorTest.java
@@ -7,8 +7,15 @@ import nextstep.subway.auth.fare.domain.SubwayFareCalculator;
 import nextstep.subway.common.exception.ErrorEnum;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
+/**
+ * 기본운임(10㎞ 이내) : 기본운임 1,250원
+ * 이용 거리초과 시 추가운임 부과
+ *   10km초과 ∼ 50km까지(5km마다 100원)
+ *   50km초과 시 (8km마다 100원)
+ */
 public class SubwayFareCalculatorTest {
 
     @ParameterizedTest()
@@ -19,10 +26,28 @@ public class SubwayFareCalculatorTest {
         assertThat(fare).isEqualTo(SubwayFareCalculator.BASIC_FARE);
     }
 
-    @Test
-    void 거리_1km이하_유효하지않는_경우_예외_발생() {
-        assertThatThrownBy(() -> SubwayFareCalculator.calculate(0))
+    @ParameterizedTest
+    @ValueSource(ints = {0, -1})
+    void 거리_0km이하_유효하지않는_경우_예외_발생(int distance) {
+        assertThatThrownBy(() -> SubwayFareCalculator.calculate(distance))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageStartingWith(ErrorEnum.DISTANCE_GREATER_ZERO.message());
     }
+
+    @ParameterizedTest()
+    @CsvSource(value = {"11:1350", "50:2050"}, delimiter = ':')
+    void 거리_10km_이상부터_50km이하_입력시_추가_운임요금_부과(int distance, int expected) {
+        int fare = SubwayFareCalculator.calculate(distance);
+
+        assertThat(fare).isEqualTo(expected);
+    }
+
+    @ParameterizedTest()
+    @CsvSource(value = {"51:1850", "80:2150"}, delimiter = ':')
+    void 거리_50km_초과_입력시_추가_운임요금_부과(int distance, int expected) {
+        int fare = SubwayFareCalculator.calculate(distance);
+
+        assertThat(fare).isEqualTo(expected);
+    }
+
 }
