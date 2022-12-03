6cc43c341086095e51efa1018b03d2fc4853fa14
choiyongseok90 <badaelephant>
2022-12-03 14:05:40 +0900

    feat: 경로 응답객체 및 도메인 객체 생성

---
+++ src/main/java/nextstep/subway/path/domain/Path.java
@@ -0,0 +1,31 @@
+package nextstep.subway.path.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+import nextstep.subway.line.domain.Distance;
+import nextstep.subway.station.domain.Station;
+
+public class Path {
+
+    private List<Station> stations;
+    private Distance distance;
+
+    private Path(List<Station> stations, int distance) {
+        System.out.println("distance");
+        System.out.println(distance);
+        this.stations = stations;
+        this.distance = Distance.from(distance);
+    }
+
+    public static Path of(List<Station> stations, int distance) {
+        return new Path(stations, distance);
+    }
+
+    public List<Station> getStations() {
+        return stations;
+    }
+
+    public Distance getDistance() {
+        return distance;
+    }
+}
