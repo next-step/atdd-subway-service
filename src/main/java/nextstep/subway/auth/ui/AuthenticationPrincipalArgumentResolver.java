22cecc0a343fdfc0142fbf0b300f5bb9193c109e
sangjaeoh <sangjaeoh91@gmail.com>
2022-12-04 00:47:27 +0900

    fix: resolveArgument

--- src/main/java/nextstep/subway/auth/ui/AuthenticationPrincipalArgumentResolver.java
+++ src/main/java/nextstep/subway/auth/ui/AuthenticationPrincipalArgumentResolver.java
@@ -29,7 +29,7 @@ public class AuthenticationPrincipalArgumentResolver implements HandlerMethodArg
     public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest,
             WebDataBinderFactory binderFactory) {
         String credentials = AuthorizationExtractor.extract(webRequest.getNativeRequest(HttpServletRequest.class));
-        if (Objects.isNull(credentials) && !isRequired(parameter)) {
+        if (Objects.isNull(credentials) || !isRequired(parameter)) {
             return new LoginMember();
         }
         return authService.findMemberByToken(credentials);
