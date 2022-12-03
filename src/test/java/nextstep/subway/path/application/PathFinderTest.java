a629bd07c89911e31b5ab2c9cee2d016dd28de56
namhyun.sung <namhyun.sung@yanolja.com>
2022-12-04 00:46:12 +0900

    test(PathFinder): 최단경로 조회

---
+++ src/test/java/nextstep/subway/path/application/PathFinderTest.java
@@ -0,0 +1,60 @@
+package nextstep.subway.path.application;
+
+import nextstep.subway.line.domain.Line;
+import nextstep.subway.line.domain.Section;
+import nextstep.subway.path.domain.Path;
+import nextstep.subway.station.domain.Station;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class PathFinderTest {
+
+    private PathFinder pathFinder;
+
+    private Line 신분당선;
+    private Line 이호선;
+    private Line 삼호선;
+    private Station 강남역;
+    private Station 양재역;
+    private Station 교대역;
+    private Station 남부터미널역;
+    private Station 부산역;
+
+    /**
+     * 교대역    --- *2호선* ---   강남역
+     * |                        |
+     * *3호선*                   *신분당선*
+     * |                        |
+     * 남부터미널역  --- *3호선* ---   양재
+     */
+
+    @BeforeEach
+    void setUp() {
+        강남역 = new Station("강남역");
+        양재역 = new Station("양재역");
+        교대역 = new Station("교대역");
+        남부터미널역 = new Station("남부터미널역");
+        부산역 = new Station("부산역");
+
+        신분당선 = new Line("신분당선", "bg-red-600", 강남역, 양재역, 10);
+        이호선 = new Line("이호선", "bg-red-600", 교대역, 강남역, 10);
+        삼호선 = new Line("삼호선", "bg-red-600", 교대역, 양재역, 5);
+        삼호선.addLineStation(new Section(삼호선, 교대역, 남부터미널역, 3));
+
+        pathFinder = new PathFinder(Arrays.asList(신분당선, 이호선, 삼호선));
+    }
+
+    @DisplayName("최단 경로를 조회한다.")
+    @Test
+    void find_shortest_path() {
+        Path path = pathFinder.findShortestPath(교대역, 양재역);
+
+        assertThat(path.getStations()).containsExactly(교대역, 남부터미널역, 양재역);
+        assertThat(path.getDistance()).isEqualTo(5);
+    }
+}
