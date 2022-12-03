3d03e28ff6c5808913fdc5835130320d612bc02e
sawhong <sawhong@gmarket.com>
2022-12-03 20:07:39 +0900

    test: pathFinder 도메인 관련 단위테스트 작성

--- src/main/java/nextstep/subway/path/domain/MockShortestPathStrategy.java
+++ src/main/java/nextstep/subway/path/domain/MockShortestPathStrategy.java
@@ -5,6 +5,10 @@ import nextstep.subway.station.domain.Station;
 import java.util.Arrays;
 
 public class MockShortestPathStrategy implements PathStrategy {
+
+    public MockShortestPathStrategy() {
+    }
+
     @Override
     public PathFinder getShortPath(Station source, Station target) {
         return new PathFinder(Arrays.asList(source, target), 10);
---
+++ src/test/java/nextstep/subway/path/domain/PathFinderTest.java
@@ -0,0 +1,35 @@
+package nextstep.subway.path.domain;
+
+import nextstep.subway.line.domain.Distance;
+import nextstep.subway.line.domain.Line;
+import nextstep.subway.station.domain.Station;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class PathFinderTest {
+
+
+    private Station 신도림역 = Station.from("신도림역");
+    private Station 가디역 = Station.from("가디역");
+    private Station 대림역 = Station.from("대림역");
+
+    private Line 일호선 = Line.of("1호선", "blue", 가디역, 신도림역, Distance.from(30));
+    private Line 이호선 = Line.of("2호선", "green", 신도림역, 대림역, Distance.from(10));
+    private Line 칠호선 = Line.of("7호선", "deep-green", 가디역, 대림역, Distance.from(10));
+
+
+    @Test
+    @DisplayName("최단거리 전략을 이용하여 역들과 거리를 구한다")
+    void strategyGetStationsAndWeight() {
+        PathFinder result = PathFinder.of(new MockShortestPathStrategy(), 신도림역, 대림역);
+
+        assertThat(result.getWeight()).isEqualTo(10.0);
+        assertThat(result.getStations()).containsAll(Arrays.asList(신도림역, 대림역));
+    }
+}
