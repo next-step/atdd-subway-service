81ed5ec677f3618c9fa16fcd84f8506432e6bc67
sixthou <justyuk@naver.com>
2022-12-04 15:04:09 +0900

    feat : Favorite 엔티티 생성

---
+++ src/main/java/nextstep/subway/favorite/domain/Favorite.java
@@ -0,0 +1,35 @@
+package nextstep.subway.favorite.domain;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import nextstep.subway.member.domain.Member;
+import nextstep.subway.station.domain.Station;
+
+@Entity
+public class Favorite {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne(cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "source_station_id")
+    private Station sourceStation;
+
+    @ManyToOne(cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "target_station_id")
+    private Station targetStation;
+
+    protected Favorite() {
+    }
+
+}
