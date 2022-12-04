755ebf8fa741cbb5290dc280bc4126ff42f6ecbb
sixthou <justyuk@naver.com>
2022-12-04 16:22:07 +0900

    feat : Favorite 생성자 및 getter 메소드 추가

--- src/main/java/nextstep/subway/favorite/domain/Favorite.java
+++ src/main/java/nextstep/subway/favorite/domain/Favorite.java
@@ -32,4 +32,21 @@ public class Favorite {
     protected Favorite() {
     }
 
+    public Favorite(Member member, Station sourceStation, Station targetStation) {
+        this.member = member;
+        this.sourceStation = sourceStation;
+        this.targetStation = targetStation;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Station getSourceStation() {
+        return sourceStation;
+    }
+
+    public Station getTargetStation() {
+        return targetStation;
+    }
 }
