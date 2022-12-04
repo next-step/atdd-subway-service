3c34a08183948f01167f1c72e2a18b0a0b34cfe5
Minjae Kim <minjae.kim@mytrum.com>
2022-12-04 09:23:56 +0900

    refactor: 지하철역 목록 관련 테스트 생성 및 일급 컬렉션 생성

---
+++ src/main/java/nextstep/subway/station/domain/StationLineUp.java
@@ -0,0 +1,24 @@
+package nextstep.subway.station.domain;
+
+import java.util.List;
+
+public class StationLineUp {
+    private final List<Station> stationList;
+
+    public StationLineUp(List<Station> stationList) {
+        this.stationList = stationList;
+    }
+
+    public boolean stationExisted(Station station) {
+        return this.stationList.stream().anyMatch(it -> it == station);
+    }
+
+
+    public boolean isEmpty() {
+        return this.stationList.isEmpty();
+    }
+
+    public boolean unKnownStation(Station station) {
+        return this.stationList.stream().noneMatch(it -> it.equals(station));
+    }
+}
