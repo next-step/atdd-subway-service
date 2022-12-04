24da48df68915619fe106b4cdb2e129919129986
cwJohnPark <pierrotcw@gmail.com>
2022-12-04 19:14:11 +0900

    fix: Add Fare field to Line

--- src/main/java/nextstep/subway/line/domain/Line.java
+++ src/main/java/nextstep/subway/line/domain/Line.java
@@ -2,6 +2,7 @@ package nextstep.subway.line.domain;
 
 import nextstep.subway.BaseEntity;
 import nextstep.subway.path.domain.PathFinder;
+import nextstep.subway.path.fare.Fare;
 import nextstep.subway.station.domain.Station;
 
 import javax.persistence.Column;
@@ -25,6 +26,9 @@ public class Line extends BaseEntity {
     @Embedded
     private Sections sections = new Sections();
 
+    @Embedded
+    private Fare fare = Fare.ZERO;
+
     public Line() {
     }
 
@@ -34,9 +38,14 @@ public class Line extends BaseEntity {
     }
 
     public Line(String name, String color, Station upStation, Station downStation, int distance) {
+        this(name, color, upStation, downStation, distance, Fare.ZERO);
+    }
+
+    public Line(String name, String color, Station upStation, Station downStation, int distance, Fare fare) {
         this.name = name;
         this.color = color;
         sections.addSection(new Section(this, upStation, downStation, distance));
+        this.fare = fare;
     }
 
     public void update(Line line) {
@@ -54,6 +63,10 @@ public class Line extends BaseEntity {
 
     public String getColor() {
         return color;
+    }
+
+    public Fare getFare() {
+        return fare;
     }
 
     public List<Station> getStations() {
