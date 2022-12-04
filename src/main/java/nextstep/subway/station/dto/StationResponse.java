4fe8449d4887931f3bf8c51fb04c44cd04ebd5aa
Hyeongwon-up <leehwdatabase@gmail.com>
2022-12-04 15:40:48 +0900

    refactor: line service 리팩터링

--- src/main/java/nextstep/subway/station/dto/StationResponse.java
+++ src/main/java/nextstep/subway/station/dto/StationResponse.java
@@ -1,18 +1,16 @@
 package nextstep.subway.station.dto;
 
-import nextstep.subway.station.domain.Station;
-
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import nextstep.subway.station.domain.Station;
 
 public class StationResponse {
     private Long id;
     private String name;
     private LocalDateTime createdDate;
     private LocalDateTime modifiedDate;
-
-    public static StationResponse of(Station station) {
-        return new StationResponse(station.getId(), station.getName(), station.getCreatedDate(), station.getModifiedDate());
-    }
 
     public StationResponse() {
     }
@@ -22,6 +20,17 @@ public class StationResponse {
         this.name = name;
         this.createdDate = createdDate;
         this.modifiedDate = modifiedDate;
+    }
+
+    public static StationResponse of(Station station) {
+        return new StationResponse(station.getId(), station.getName(), station.getCreatedDate(),
+                station.getModifiedDate());
+    }
+
+    public static List<StationResponse> listOf(List<Station> stations) {
+        return stations.stream()
+                .map(StationResponse::of)
+                .collect(Collectors.toList());
     }
 
     public Long getId() {
