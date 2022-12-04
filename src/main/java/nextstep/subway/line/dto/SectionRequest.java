0c01c9feab9cdf526716e7731f9c739ba90ff1e8
cylee9409 <cylee9409@gmail.com>
2022-12-04 19:14:48 +0900

    [feat] 거리 클래스로 포장 및 기존 거리 값 수정

--- src/main/java/nextstep/subway/line/dto/SectionRequest.java
+++ src/main/java/nextstep/subway/line/dto/SectionRequest.java
@@ -1,9 +1,11 @@
 package nextstep.subway.line.dto;
 
+import nextstep.subway.line.domain.Distance;
+
 public class SectionRequest {
     private Long upStationId;
     private Long downStationId;
-    private int distance;
+    private Distance distance;
 
     public SectionRequest() {
     }
@@ -11,7 +13,7 @@ public class SectionRequest {
     public SectionRequest(Long upStationId, Long downStationId, int distance) {
         this.upStationId = upStationId;
         this.downStationId = downStationId;
-        this.distance = distance;
+        this.distance = new Distance(distance);
     }
 
     public Long getUpStationId() {
@@ -22,7 +24,7 @@ public class SectionRequest {
         return downStationId;
     }
 
-    public int getDistance() {
+    public Distance getDistance() {
         return distance;
     }
 }
