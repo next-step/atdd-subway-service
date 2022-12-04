f225f0e9328d466da02e6d80e196031eaad612af
Gyeom <koreatech93@naver.com>
2022-12-04 15:17:26 +0900

    refactor(fareCalculator): 상수 사용

--- src/main/java/nextstep/subway/path/domain/FareCalculator.java
+++ src/main/java/nextstep/subway/path/domain/FareCalculator.java
@@ -8,8 +8,15 @@ import java.util.List;
 public class FareCalculator {
 
     public static final int BASIC_FARE = 1250;
+    public static final int ADDITIONAL_FARE_UNIT = 100;
     public static final int ADDITIONAL_FARE_DISTANCE_LEVEL1 = 10;
     public static final int ADDITIONAL_FARE_DISTANCE_LEVEL2 = 50;
+    public static final int DEDUCTION_AMOUNT = 350;
+    public static final double CHILD_AGE_MIN = 6;
+    public static final double TEEN_AGE_MIN = 13;
+    public static final double TEEN_AGE_MAX = 18;
+    public static final double DISCOUNT_RATE_CHILD = 0.5;
+    public static final double DISCOUNT_RATE_TEEN = 0.2;
     public static final int DISTANCE_UNIT_LEVEL1 = 5;
     public static final int DISTANCE_UNIT_LEVEL2 = 8;
 
@@ -39,7 +46,7 @@ public class FareCalculator {
     }
 
     private static int calculateOverFare(double distance, int distanceUnit) {
-        return (int) ((Math.floor((distance - 1) / distanceUnit) + 1) * 100);
+        return (int) ((Math.floor((distance - 1) / distanceUnit) + 1) * ADDITIONAL_FARE_UNIT);
     }
 
     public static int applyDiscountFare(final int fare, final int age) {
@@ -54,18 +61,18 @@ public class FareCalculator {
     }
 
     private static boolean isChild(int age) {
-        return age >= 6 && age < 13;
+        return age >= CHILD_AGE_MIN && age < TEEN_AGE_MIN;
     }
 
     private static boolean isTeen(int age) {
-        return age >= 13 && age < 19;
+        return age >= TEEN_AGE_MIN && age <= TEEN_AGE_MAX;
     }
 
     private static int applyChildDiscountFare(int fare) {
-        return (int) (fare - ((fare - 350) * 0.5));
+        return (int) (fare - ((fare - DEDUCTION_AMOUNT) * DISCOUNT_RATE_CHILD));
     }
 
     private static int applyTeenDiscountFare(int fare) {
-        return (int) (fare - ((fare - 350) * 0.2));
+        return (int) (fare - ((fare - DEDUCTION_AMOUNT) * DISCOUNT_RATE_TEEN));
     }
 }
