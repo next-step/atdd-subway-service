629184d42169f1a7d7eb6e1d3ee28dd656f08934
sixthou <justyuk@naver.com>
2022-12-04 19:49:57 +0900

    feat : 정적팩토리 메서드 추가

--- src/main/java/nextstep/subway/favorite/domain/Favorite.java
+++ src/main/java/nextstep/subway/favorite/domain/Favorite.java
@@ -1,5 +1,6 @@
 package nextstep.subway.favorite.domain;
 
+import java.util.Objects;
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -32,10 +33,15 @@ public class Favorite {
     protected Favorite() {
     }
 
-    public Favorite(Member member, Station sourceStation, Station targetStation) {
+    private Favorite(Member member, Station sourceStation, Station targetStation) {
+        validFavorite(sourceStation, targetStation);
         this.member = member;
         this.sourceStation = sourceStation;
         this.targetStation = targetStation;
+    }
+
+    public static Favorite of(Member member, Station sourceStation, Station targetStation) {
+        return new Favorite(member, sourceStation, targetStation);
     }
 
     public Long getId() {
@@ -50,9 +56,45 @@ public class Favorite {
         return targetStation;
     }
 
-    public void isOwner(Member member) {
-        if (member != this.member) {
-            throw new IllegalArgumentException("본인의 즐겨찾기만 삭제할 수 있습니다.");
+    public boolean isSameOwner(Member member) {
+        return member != this.member;
+    }
+
+    private static void validFavorite(Station sourceStation, Station targetStation) {
+        if (sourceStation == targetStation) {
+            throw new IllegalArgumentException("출발지와 목적지가 동일할 수 없습니다.");
         }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        Favorite favorite = (Favorite) o;
+
+        if (!Objects.equals(id, favorite.id)) {
+            return false;
+        }
+        if (!Objects.equals(member, favorite.member)) {
+            return false;
+        }
+        if (!Objects.equals(sourceStation, favorite.sourceStation)) {
+            return false;
+        }
+        return Objects.equals(targetStation, favorite.targetStation);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (member != null ? member.hashCode() : 0);
+        result = 31 * result + (sourceStation != null ? sourceStation.hashCode() : 0);
+        result = 31 * result + (targetStation != null ? targetStation.hashCode() : 0);
+        return result;
     }
 }
