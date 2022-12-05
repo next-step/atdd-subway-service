aec0969c76d5e23d7ce6f8ba3f2adee0a2e0d6e4
chunhodong <yhms4432@naver.com>
2022-12-05 19:15:37 +0900

    feat: DistanceFare클래스에서 distance값과 DistancePolicy로 요금구하도록수정

--- src/main/java/nextstep/subway/fare/domain/DistanceFare.java
+++ src/main/java/nextstep/subway/fare/domain/DistanceFare.java
@@ -1,39 +1,15 @@
 package nextstep.subway.fare.domain;
 
-import nextstep.subway.fare.exception.AgeFareException;
+public class DistanceFare {
+    private int distance;
+    private DistancePolicy policy;
 
-import java.util.Arrays;
-import java.util.function.IntFunction;
-
-import static nextstep.subway.fare.exception.AgeFareExceptionCode.NONE_EXISTS_AGE;
-
-public enum DistanceFare {
-
-    BASE(0, 10),
-    MIDDLE(10, 50),
-    LONG(50, Integer.MAX_VALUE);
-
-    private int minDistance;
-    private int maxDistance;
-    private IntFunction<Integer> policyFuction;
-
-    DistanceFare(int minDistance, int maxDistance, IntFunction<Integer> policyFuction) {
-        this.minDistance = minDistance;
-        this.maxDistance = maxDistance;
-        this.policyFuction = policyFuction;
+    public DistanceFare(int distance,DistancePolicy policy){
+        this.distance = distance;
+        this.policy = policy;
     }
 
-    public static DistanceFare valueOfRange(int age) {
-        return Arrays.stream(DistanceFare.values())
-                .filter(it -> it.isRange(age))
-                .findAny()
-                .orElseThrow(() -> new AgeFareException(NONE_EXISTS_AGE));
+    public int getFare(int fare){
+        return fare + policy.getFare(distance);
     }
-
-    private boolean isRange(int distance) {
-        return this.minDistance < distance && distance <= this.maxDistance;
-    }
-
-    //거리입력하면, 얼마나오는지
-
 }
