60421d8ec30364c12b1148a47319440dc4a5d26a
seogineer <dgseo8981@gmail.com>
2022-12-05 21:14:05 +0900

    test: @DataJpaTest 제거

--- src/test/java/nextstep/subway/path/PathFinderTest.java
+++ src/test/java/nextstep/subway/path/PathFinderTest.java
@@ -6,46 +6,30 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import java.util.Arrays;
 import nextstep.subway.line.domain.Distance;
 import nextstep.subway.line.domain.Line;
-import nextstep.subway.line.domain.LineRepository;
 import nextstep.subway.path.domain.Path;
 import nextstep.subway.path.domain.PathFinder;
 import nextstep.subway.station.domain.Station;
-import nextstep.subway.station.domain.StationRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
-@DataJpaTest
 public class PathFinderTest {
-    @Autowired
-    LineRepository lineRepository;
-    @Autowired
-    StationRepository stationRepository;
     private final Line 신분당선 = Line.of("신분당선", "red");
     private final Line 수인분당선 = Line.of("수인분당선", "yellow");
     private final Line 이호선 = Line.of("이호선", "green");
     private final Line 일호선 = Line.of("일호선", "blue");
-    private final Station 강남역 = new Station("강남역");
-    private final Station 판교역 = new Station("판교역");
-    private final Station 광교역 = new Station("광교역");
-    private final Station 선릉역 = new Station("선릉역");
-    private final Station 수원역 = new Station("수원역");
-    private final Station 소요산역 = new Station("소요산역");
-    private final Station 인천역 = new Station("인천역");
+    private final Station 강남역 = new Station(1L, "강남역");
+    private final Station 판교역 = new Station(2L, "판교역");
+    private final Station 광교역 = new Station(3L, "광교역");
+    private final Station 선릉역 = new Station(4L, "선릉역");
+    private final Station 수원역 = new Station(5L, "수원역");
+    private final Station 소요산역 = new Station(6L, "소요산역");
+    private final Station 인천역 = new Station(7L, "인천역");
     private final Distance TEN = Distance.from(10);
     private final Distance FIVE = Distance.from(5);
     private PathFinder pathFinder;
 
     @BeforeEach
     void setUp() {
-        stationRepository.save(강남역);
-        stationRepository.save(판교역);
-        stationRepository.save(광교역);
-        stationRepository.save(선릉역);
-        stationRepository.save(수원역);
-        stationRepository.save(소요산역);
-        stationRepository.save(인천역);
         신분당선.addSection(강남역, 광교역, FIVE);
         수인분당선.addSection(선릉역, 수원역, TEN);
         이호선.addSection(강남역, 선릉역, FIVE);
@@ -66,6 +50,7 @@ public class PathFinderTest {
         Path path = pathFinder.findPath(강남역.getId(), 광교역.getId());
         assertThat(path.getAdditionalFareByLine()).isEqualTo(500);
     }
+
 
     @Test
     void 추가_요금_조회_2() {
