6451c4deb228d58fe4f3538da5af13261ac2b555
jhkang <serdozz@naver.com>
2022-12-06 01:39:24 +0900

    refactor: service findLines 리팩터링

--- src/main/java/nextstep/subway/line/dto/LineResponse.java
+++ src/main/java/nextstep/subway/line/dto/LineResponse.java
@@ -1,6 +1,8 @@
 package nextstep.subway.line.dto;
 
+import java.util.stream.Collectors;
 import nextstep.subway.line.domain.Line;
+import nextstep.subway.station.domain.Station;
 import nextstep.subway.station.dto.StationResponse;
 
 import java.time.LocalDateTime;
@@ -14,20 +16,17 @@ public class LineResponse {
     private LocalDateTime createdDate;
     private LocalDateTime modifiedDate;
 
-    public LineResponse() {
-    }
-
-    public LineResponse(Long id, String name, String color, List<StationResponse> stations, LocalDateTime createdDate, LocalDateTime modifiedDate) {
+    public LineResponse(Long id, String name, String color, List<Station> stations, LocalDateTime createdDate, LocalDateTime modifiedDate) {
         this.id = id;
         this.name = name;
         this.color = color;
-        this.stations = stations;
+        this.stations = stations.stream().map(StationResponse::of).collect(Collectors.toList());
         this.createdDate = createdDate;
         this.modifiedDate = modifiedDate;
     }
 
-    public static LineResponse of(Line line, List<StationResponse> stations) {
-        return new LineResponse(line.getId(), line.getName(), line.getColor(), stations, line.getCreatedDate(), line.getModifiedDate());
+    public static LineResponse of(Line line) {
+        return new LineResponse(line.getId(), line.getName(), line.getColor(), line.getStations(), line.getCreatedDate(), line.getModifiedDate());
     }
 
     public Long getId() {
--- src/test/java/nextstep/subway/line/service/LineServiceTest.java
+++ src/test/java/nextstep/subway/line/service/LineServiceTest.java
@@ -1,22 +1,39 @@
 package nextstep.subway.line.service;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.Mockito.when;
+
+import java.util.Arrays;
+import java.util.List;
 import nextstep.subway.line.application.LineService;
 import nextstep.subway.line.domain.Line;
+import nextstep.subway.line.domain.LineRepository;
+import nextstep.subway.line.dto.LineResponse;
+import nextstep.subway.station.application.StationService;
 import nextstep.subway.station.domain.Station;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 @DisplayName("지하철 노선 서비스 단위테스트")
-@SpringBootTest
+@ExtendWith(MockitoExtension.class)
 public class LineServiceTest {
     private Station 강남역;
     private Station 광교역;
     private Line 신분당선;
 
-    @Autowired
+    @InjectMocks
     private LineService lineService;
+
+    @Mock
+    private LineRepository lineRepository;
+
+    @Mock
+    private StationService stationService;
 
     @BeforeEach
     public void setUp() {
@@ -25,5 +42,12 @@ public class LineServiceTest {
         광교역 = new Station("광교역");
         신분당선 = new Line("2호선","bg-green-600", 강남역, 광교역, 10);
     }
-
+    
+    @Test
+    @DisplayName("기존 findLines 로직 검증")
+    void getStationsTest() {
+        when(lineRepository.findAll()).thenReturn(Arrays.asList(신분당선));
+        List<LineResponse> lineResponses = lineService.findLines();
+        assertThat(lineResponses.get(0).getName()).isEqualTo(신분당선.getName());
+    }
 }
