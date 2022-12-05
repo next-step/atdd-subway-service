857958ee7ff896cd453160045f4234d7c94abe3a
chosundeveloper <67059660+chosundeveloper@users.noreply.github.com>
2022-12-05 17:41:16 +0900

    test(FavoriteServiceTest.java) : 출발역과 도착역이 같을 수 없다.

--- src/test/java/nextstep/subway/favorite/application/FavoriteServiceTest.java
+++ src/test/java/nextstep/subway/favorite/application/FavoriteServiceTest.java
@@ -7,13 +7,16 @@ import nextstep.subway.member.domain.Member;
 import nextstep.subway.member.domain.MemberRepository;
 import nextstep.subway.station.domain.Station;
 import nextstep.subway.station.domain.StationRepository;
+import nextstep.subway.utils.DatabaseCleanup;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static nextstep.subway.favorite.domain.Favorite.TARGET_SOURCE_SAME_EXCEPTION_MESSAGE;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 @DisplayName("즐겨찾기 서비스")
@@ -32,12 +35,16 @@ class FavoriteServiceTest {
     @Autowired
     private FavoriteRepository favoriteRepository;
 
+    @Autowired
+    private DatabaseCleanup databaseCleanup;
+
     private Member member;
     private Station stationA;
     private Station stationB;
 
     @BeforeEach
     void setUp() {
+        databaseCleanup.execute();
         member = memberRepository.save(new Member("email", "password", 20));
         stationA = stationRepository.save(new Station("A"));
         stationB = stationRepository.save(new Station("B"));
@@ -57,5 +64,13 @@ class FavoriteServiceTest {
                 () -> assertThat(response.getTarget().getCreatedDate()).isNotNull(),
                 () -> assertThat(response.getTarget().getName()).isEqualTo("B")
         );
+    }
+
+    @DisplayName("출발역과 도착역이 같을 수 없다.")
+    @Test
+    void create_fail_sameStation() {
+        assertThatThrownBy(() -> favoriteService.create(member.getId(), new FavoriteCreatedRequest(stationA.getId(), stationA.getId())))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(TARGET_SOURCE_SAME_EXCEPTION_MESSAGE);
     }
 }
