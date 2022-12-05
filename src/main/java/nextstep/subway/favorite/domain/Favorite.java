62b7464f164ab77346355c4ef3d51920f5711a64
chosundeveloper <67059660+chosundeveloper@users.noreply.github.com>
2022-12-05 16:34:07 +0900

    test(FavoriteTest.java) : 출발역과 도착역이 같을 수 없다.

--- src/main/java/nextstep/subway/favorite/domain/Favorite.java
+++ src/main/java/nextstep/subway/favorite/domain/Favorite.java
@@ -9,6 +9,7 @@ import javax.persistence.*;
 @Entity
 public class Favorite extends BaseEntity {
 
+    public static final String TARGET_SOURCE_SAME_EXCEPTION_MESSAGE = "출발역과 도착역이 같을 수 없다.";
     @Id
     @GeneratedValue
     private Long id;
@@ -21,8 +22,15 @@ public class Favorite extends BaseEntity {
     private Station source;
 
     public Favorite(Member member, Station target, Station source) {
+        validateStation(target, source);
         this.member = member;
         this.target = target;
         this.source = source;
+    }
+
+    private void validateStation(Station target, Station source) {
+        if (target.equals(source)) {
+            throw new IllegalArgumentException(TARGET_SOURCE_SAME_EXCEPTION_MESSAGE);
+        }
     }
 }
