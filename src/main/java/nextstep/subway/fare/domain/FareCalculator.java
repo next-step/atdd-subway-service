f6c2f806d026f08048ff62255fb5b678cd04c071
chunhodong <yhms4432@naver.com>
2022-12-05 19:44:30 +0900

    feat: 요금계산하는 FareCalculator클래스추가

--- src/main/java/nextstep/subway/fare/domain/FareCalculator.java
+++ src/main/java/nextstep/subway/fare/domain/FareCalculator.java
@@ -1,10 +1,22 @@
 package nextstep.subway.fare.domain;
 
+import nextstep.subway.line.domain.Line;
+
 public class FareCalculator {
     private static final int BASE_FARE = 1250;
-    private AgeFare ageFare;
+    private Line line;
     private DistanceFare distanceFare;
+    private AgePolicy agePolicy;
 
 
+    public FareCalculator(Line line, DistanceFare distanceFare, AgePolicy agePolicy) {
+        this.line = line;
+        this.distanceFare = distanceFare;
+        this.agePolicy = agePolicy;
+    }
 
+    public int getCalculcate() {
+        int fare = distanceFare.getFare(BASE_FARE + line.getFare());
+        return agePolicy.getFare(fare);
+    }
 }
