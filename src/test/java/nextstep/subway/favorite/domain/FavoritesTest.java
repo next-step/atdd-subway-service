6cba27664535c05b828a66046c9b9f0940616e4a
chosundeveloper <67059660+chosundeveloper@users.noreply.github.com>
2022-12-05 19:15:11 +0900

    test(FavoriteServiceTest.java) : 가지고 있지 않은 즐겨찾기를 삭제할 수 없다.

--- src/test/java/nextstep/subway/favorite/domain/FavoritesTest.java
+++ src/test/java/nextstep/subway/favorite/domain/FavoritesTest.java
@@ -5,7 +5,10 @@ import nextstep.subway.station.domain.Station;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.NoSuchElementException;
+
 import static nextstep.subway.favorite.domain.Favorites.FAVORITE_DUPLICATE_EXCEPTION_MESSAGE;
+import static nextstep.subway.favorite.domain.Favorites.HAS_NOT_FAVORITE_EXCEPTION_MESSAGE;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class FavoritesTest {
@@ -25,5 +28,21 @@ class FavoritesTest {
         assertThatThrownBy(() -> favorites.add(favorite))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(FAVORITE_DUPLICATE_EXCEPTION_MESSAGE);
+    }
+
+    @DisplayName("가지고 있지 않은 즐겨찾기를 삭제할 수 없다.")
+    @Test
+    void delete_fail() {
+        Member member = new Member("email", "password", 20);
+        Station stationA = new Station(1L, "A");
+        Station stationB = new Station(2L, "B");
+
+        Favorite favorite = new Favorite(member, stationA, stationB);
+
+        Favorites favorites = new Favorites();
+
+        assertThatThrownBy(() -> favorites.validateDeleteFavorite(favorite))
+                .isInstanceOf(NoSuchElementException.class)
+                .hasMessageContaining(HAS_NOT_FAVORITE_EXCEPTION_MESSAGE);
     }
 }
