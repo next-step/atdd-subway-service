525cab548cd99704b2468fdb75fdb3a7004e22d1
chunhodong <yhms4432@naver.com>
2022-12-05 19:45:19 +0900

    refactor: findLineByStations에서 중복제거

--- src/main/java/nextstep/subway/line/domain/Lines.java
+++ src/main/java/nextstep/subway/line/domain/Lines.java
@@ -2,11 +2,12 @@ package nextstep.subway.line.domain;
 
 import nextstep.subway.station.domain.Station;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Lines {
-    private List<Line> lines;
+    private final List<Line> lines;
 
     private Lines(List<Line> lines) {
         this.lines = lines;
@@ -21,15 +22,14 @@ public class Lines {
                 .stream()
                 .flatMap(line -> line.getSections().stream())
                 .filter(section -> stations.contains(section.getUpStation()) && stations.contains(section.getDownStation()))
-                .map(section -> section.getLine())
+                .map(Section::getLine)
+                .distinct()
                 .collect(Collectors.toList());
     }
 
     public Line findMaxFareLineByStations(List<Station> stations) {
         return findLinesByStations(stations)
-                .stream()
-                .sorted((o1, o2) -> o1.getFare() - o2.getFare())
-                .findFirst()
+                .stream().max(Comparator.comparingInt(Line::getFare))
                 .orElse(new Line());
     }
 }
