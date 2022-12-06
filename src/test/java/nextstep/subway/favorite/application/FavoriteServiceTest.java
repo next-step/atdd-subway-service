185df45886070fd6a0c8412730cbc99f067f1274
jylim <jylim@12cm.co.kr>
2022-12-06 09:19:51 +0900

    test(FavoriteServiceTest): 즐겨찾기 생성 단위 테스트 작성

--- src/test/java/nextstep/subway/favorite/application/FavoriteServiceTest.java
+++ src/test/java/nextstep/subway/favorite/application/FavoriteServiceTest.java
@@ -1,7 +1,12 @@
 package nextstep.subway.favorite.application;
 
+import nextstep.subway.favorite.domain.Favorite;
+import nextstep.subway.favorite.domain.FavoriteRepository;
+import nextstep.subway.favorite.dto.FavoriteRequest;
+import nextstep.subway.member.application.MemberService;
 import nextstep.subway.member.domain.Member;
 import nextstep.subway.member.domain.MemberRepository;
+import nextstep.subway.station.application.StationService;
 import nextstep.subway.station.domain.Station;
 import nextstep.subway.station.domain.StationRepository;
 import org.assertj.core.api.Assertions;
@@ -26,12 +31,16 @@ import static org.mockito.Mockito.when;
 @ExtendWith(MockitoExtension.class)
 class FavoriteServiceTest {
     @Mock
-    private StationRepository stationRepository;
+    private StationService stationService;
 
     @Mock
-    private MemberRepository memberRepository;
+    private MemberService memberService;
 
+    @Mock
+    private FavoriteRepository favoriteRepository;
 
+    @InjectMocks
+    private FavoriteService favoriteService;
 
     private Member member;
     private Station sourceStation;
@@ -39,9 +48,22 @@ class FavoriteServiceTest {
 
     @BeforeEach
     void setUp() {
-        member = new Member("abc@gmail.com", "password", 10);
+        member = new Member("yalmung@gmail.com", "password", 10);
         sourceStation = new Station("강남역");
         targetStation = new Station("광교역");
+    }
+
+    @DisplayName("즐겨찾기를 생성한다.")
+    @Test
+    void create() {
+        when(stationService.findStationById(1L)).thenReturn(sourceStation);
+        when(stationService.findStationById(2L)).thenReturn(targetStation);
+        when(memberService.findMemberById(any())).thenReturn(member);
+        when(favoriteRepository.save(any())).thenReturn(Favorite.of(member, sourceStation, targetStation));
+
+        Favorite favorite = favoriteService.create(1L, new FavoriteRequest(1L, 2L));
+
+        Assertions.assertThat(favorite).isNotNull();
     }
 
 
