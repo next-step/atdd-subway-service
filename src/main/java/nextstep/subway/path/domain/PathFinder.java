d77be194e25af408ca8f470e9514542e9c19fd37
choiyongseok90 <badaelephant>
2022-12-07 04:12:11 +0900

    feat: 경로조회 인터페이스에 로그인 멤버 경로조회 메서드 추가

--- src/main/java/nextstep/subway/path/domain/PathFinder.java
+++ src/main/java/nextstep/subway/path/domain/PathFinder.java
@@ -1,9 +1,8 @@
 package nextstep.subway.path.domain;
 
 import nextstep.subway.station.domain.Station;
-import org.jgrapht.GraphPath;
 
 public interface PathFinder {
-
     Path findPath(Station source, Station target);
+    Path findPathByLoginMember(Station source, Station target, Integer age);
 }
