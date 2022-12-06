e5a2fad59c9a3ff052355793bcdabf8b9bd74f53
choiyongseok90 <badaelephant>
2022-12-07 04:16:22 +0900

    feat: 경로 도메인 추가

--- src/main/java/nextstep/subway/path/domain/Path.java
+++ src/main/java/nextstep/subway/path/domain/Path.java
@@ -1,9 +1,9 @@
 package nextstep.subway.path.domain;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import nextstep.subway.line.domain.Distance;
+import nextstep.subway.line.domain.Section;
 import nextstep.subway.station.domain.Station;
 import org.jgrapht.GraphPath;
 
@@ -12,23 +12,42 @@ public class Path {
     private List<Station> stations;
     private Distance distance;
 
-    private Path(List<Station> stations, int distance) {
+    private Fare fare;
+
+    private Path(List<Station> stations, Distance distance, Fare fare) {
         this.stations = stations;
-        this.distance = Distance.from(distance);
+        this.distance = distance;
+        this.fare = fare;
     }
 
     public static Path of(GraphPath graphPath) {
-        Double totalDistance = graphPath.getWeight();
-        return new Path(graphPath.getVertexList(), totalDistance.intValue());
+        Distance distance = Distance.from((int)graphPath.getWeight());
+        int maxSurCharge = getMaxSurCharge(graphPath.getEdgeList());
+        return new Path(graphPath.getVertexList(), distance, Fare.of(distance, maxSurCharge));
     }
 
-
+    public static Path ofAge(GraphPath graphPath, Integer age) {
+        Distance distance = Distance.from((int)graphPath.getWeight());
+        int maxSurCharge = getMaxSurCharge(graphPath.getEdgeList());
+        Discount discount = Discount.getDiscountByAge(age.intValue());
+        return new Path(graphPath.getVertexList(), distance, Fare.ofDiscount(distance, maxSurCharge, discount));
+    }
 
     public List<Station> getStations() {
         return Collections.unmodifiableList(stations);
     }
-
     public Distance getDistance() {
         return distance;
+    }
+
+    public Fare getFare() {
+        return fare;
+    }
+
+    private static int getMaxSurCharge(List<SectionEdge> sections){
+
+        return sections.stream().mapToInt(
+                SectionEdge::getLineSurCharge
+        ).max().orElse(0);
     }
 }
