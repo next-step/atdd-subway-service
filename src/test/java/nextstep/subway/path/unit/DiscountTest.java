ef7c2bfe01a9d169d71b2d4470c68a299d8970f6
choiyongseok90 <badaelephant>
2022-12-07 04:17:36 +0900

    test: 할인 테스트 코드 추가

---
+++ src/test/java/nextstep/subway/path/unit/DiscountTest.java
@@ -0,0 +1,35 @@
+package nextstep.subway.path.unit;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import nextstep.subway.path.domain.Discount;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class DiscountTest {
+
+    @DisplayName("아동 할인률(50%)을 조회할 수 있다.")
+    @Test
+    void getDiscountByAge_child_test() {
+        // given
+        int 아동_나이 = 10;
+        // when
+        Discount discount = Discount.getDiscountByAge(아동_나이);
+        // then
+        assertEquals(Discount.CHILD_DISCOUNT, discount);
+        assertEquals(0.5, discount.getRate());
+    }
+
+    @DisplayName("청소년 할인률(20%)을 조회할 수 있다.")
+    @Test
+    void getDiscountByAge_teenager_test() {
+        // given
+        int 청소년_나이 = 17;
+        // when
+        Discount discount = Discount.getDiscountByAge(청소년_나이);
+        // then
+        assertEquals(Discount.TEENAGER_DISCOUNT, discount);
+        assertEquals(0.2, discount.getRate());
+    }
+
+}
