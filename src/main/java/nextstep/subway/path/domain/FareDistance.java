38c15e7a51edbef3128f121978dcb617e3c65256
sixthou <justyuk@naver.com>
2022-12-06 14:00:59 +0900

    feat : FareDistance Enum 생성

---
+++ src/main/java/nextstep/subway/path/domain/FareDistance.java
@@ -0,0 +1,41 @@
+package nextstep.subway.path.domain;
+
+import static nextstep.subway.path.domain.Fare.ADDITIONAL_FARE;
+import static nextstep.subway.path.domain.Fare.MIDDLE_SECTOR_CHARGE_DISTANCE;
+import static nextstep.subway.path.domain.Fare.MIDDLE_DISTANCE;
+import static nextstep.subway.path.domain.Fare.HIGH_SECTOR_CHARGE_DISTANCE;
+import static nextstep.subway.path.domain.Fare.HIGH_DISTANCE;
+
+import java.util.Arrays;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public enum FareDistance {
+    MIDDLE(distance -> distance > MIDDLE_DISTANCE,
+            distance -> calSectorFare(Math.min(distance - MIDDLE_DISTANCE, HIGH_DISTANCE - MIDDLE_DISTANCE),
+                    MIDDLE_SECTOR_CHARGE_DISTANCE)),
+    HIGH(distance -> distance > HIGH_DISTANCE,
+            distance -> calSectorFare(distance - HIGH_DISTANCE, HIGH_SECTOR_CHARGE_DISTANCE));
+
+    private final Predicate<Integer> range;
+    private final Function<Integer, Integer> formula;
+
+    FareDistance(Predicate<Integer> range, Function<Integer, Integer> formula) {
+        this.range = range;
+        this.formula = formula;
+    }
+
+    public static int calAdditionalFare(int distance) {
+        return Arrays.stream(FareDistance.values())
+                .filter(i -> i.range.test(distance))
+                .map(i -> i.formula.apply(distance))
+                .reduce(0, Integer::sum);
+    }
+
+
+    private static int calSectorFare(int distance, int chargingDistance) {
+        return (int) ((Math.ceil((distance - 1) / chargingDistance) + 1) * ADDITIONAL_FARE);
+    }
+
+
+}
