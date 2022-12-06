bdf8980b28ef2c0881063af60df95b1abaa4e3d7
sixthou <justyuk@naver.com>
2022-12-06 20:58:40 +0900

    feat : lines 생성

---
+++ src/main/java/nextstep/subway/line/domain/Lines.java
@@ -0,0 +1,23 @@
+package nextstep.subway.line.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Lines {
+    private List<Line> lines = new ArrayList<>();
+
+    public Lines(List<Line> lines) {
+        this.lines.addAll(lines);
+    }
+
+    public static Lines From(List<Line> lines) {
+        return new Lines(lines);
+    }
+
+    public int getMaxAdditionalFare() {
+        return lines.stream()
+                .mapToInt(Line::getAdditionalFare)
+                .max()
+                .orElse(0);
+    }
+}
