07af8743481db1c136721e5fa6b606519adbf398
CHEESE <81912261+ychxexn@users.noreply.github.com>
2022-12-06 21:37:11 +0900

    refactor: DiscountCalculator 상수명 변경

--- src/main/java/nextstep/subway/path/domain/DiscountCalculator.java
+++ src/main/java/nextstep/subway/path/domain/DiscountCalculator.java
@@ -3,9 +3,9 @@ package nextstep.subway.path.domain;
 import nextstep.subway.auth.domain.LoginMember;
 
 public class DiscountCalculator {
-    private static final Integer DISCOUNT_DEFAULT = 350;
-    private static final double DISCOUNT_CHILD = 0.5;
-    private static final double DISCOUNT_TEEN = 0.8;
+    private static final Integer DEFAULT_DISCOUNT_FARE = 350;
+    private static final double DISCOUNT_RATE_FOR_CHILD = 0.5;
+    private static final double DISCOUNT_RATE_FOR_TEEN = 0.8;
 
     private DiscountCalculator() {
 
@@ -15,12 +15,12 @@ public class DiscountCalculator {
         if (member == null || member.isAdult()) {
             return fare;
         }
-        fare -= DISCOUNT_DEFAULT;
+        fare -= DEFAULT_DISCOUNT_FARE;
         if (member.isTeen()) {
-            return (int) Math.ceil(fare * DISCOUNT_TEEN);
+            return (int) Math.ceil(fare * DISCOUNT_RATE_FOR_TEEN);
         }
         if (member.isChild()) {
-            return (int) Math.ceil(fare * DISCOUNT_CHILD);
+            return (int) Math.ceil(fare * DISCOUNT_RATE_FOR_CHILD);
         }
         return fare;
     }
