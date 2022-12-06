90320a6db98a75334def38f1d07f9ca5c876b69e
namhyun.sung <namhyun.sung@yanolja.com>
2022-12-07 00:48:28 +0900

    test(AuthAcceptanceTest): 로그인 요청 및 성공

--- src/test/java/nextstep/subway/auth/acceptance/AuthAcceptanceTest.java
+++ src/test/java/nextstep/subway/auth/acceptance/AuthAcceptanceTest.java
@@ -1,14 +1,36 @@
 package nextstep.subway.auth.acceptance;
 
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
 import nextstep.subway.AcceptanceTest;
+import nextstep.subway.auth.dto.TokenRequest;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+import static nextstep.subway.member.MemberAcceptanceTest.회원_생성을_요청;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class AuthAcceptanceTest extends AcceptanceTest {
+
+    private String email = "test@nextstep.com";
+    private String password = "password";
+    private Integer age = 5;
+    @BeforeEach
+    public void setUp() {
+        super.setUp();
+        회원_생성을_요청(email, password, age);
+    }
 
     @DisplayName("Bearer Auth")
     @Test
     void myInfoWithBearerAuth() {
+        ExtractableResponse<Response> response = 로그인_요청(email, password);
+
+        로그인_됨(response);
     }
 
     @DisplayName("Bearer Auth 로그인 실패")
@@ -21,4 +43,18 @@ public class AuthAcceptanceTest extends AcceptanceTest {
     void myInfoWithWrongBearerAuth() {
     }
 
+    private static ExtractableResponse<Response> 로그인_요청(String email, String password) {
+        TokenRequest request = new TokenRequest(email, password);
+        return RestAssured
+            .given().log().all()
+            .contentType(MediaType.APPLICATION_JSON_VALUE)
+            .body(request)
+            .when().post("/login/token")
+            .then().log().all()
+            .extract();
+    }
+
+    private void 로그인_됨(ExtractableResponse<Response> response) {
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+    }
 }
