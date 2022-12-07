7211cca7b2022ab4293d331da662f1f7026c5163
SanghoonSon <mue0329@naver.com>
2022-12-07 15:16:00 +0900

    refactor: step1 피드백 반영

--- src/main/java/nextstep/subway/line/application/LineService.java
+++ src/main/java/nextstep/subway/line/application/LineService.java
@@ -40,11 +40,6 @@ public class LineService {
         return LineResponse.list(lineRepository.findAllWithSections());
     }
 
-    public Line findLineById(Long id) {
-        return lineRepository.findByIdWithSections(id)
-                .orElseThrow(RuntimeException::new);
-    }
-
     public LineResponse findLineResponseById(Long id) {
         return LineResponse.of(findLineById(id));
     }
@@ -73,5 +68,10 @@ public class LineService {
         Line line = findLineById(lineId);
         Station station = stationService.findStationById(stationId);
         line.removeStation(station);
+    }
+
+    private Line findLineById(Long id) {
+        return lineRepository.findByIdWithSections(id)
+                .orElseThrow(RuntimeException::new);
     }
 }
