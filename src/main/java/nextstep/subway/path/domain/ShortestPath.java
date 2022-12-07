88bb257c87f8c8cfa6b0d0775af87bb7c1f3e4d5
chunhodong <yhms4432@naver.com>
2022-12-07 11:35:03 +0900

    test: ShortestPathTest에서 역조회테스트 추가

--- src/main/java/nextstep/subway/path/domain/ShortestPath.java
+++ src/main/java/nextstep/subway/path/domain/ShortestPath.java
@@ -5,8 +5,11 @@ import org.jgrapht.GraphPath;
 import org.jgrapht.graph.DefaultWeightedEdge;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ShortestPath {
+
+
     private static final String NONE_EXISTS_PATH = "경로가 존재해야 합니다";
     private GraphPath<Station, DefaultWeightedEdge> shortestPath;
 
@@ -19,17 +22,17 @@ public class ShortestPath {
         if(shortestPath == null){
             throw new NullPointerException(NONE_EXISTS_PATH);
         }
-
     }
 
-    public List<Station> getPath(){
-
-        return null;
+    public List<Station> getStations(){
+        return shortestPath
+                .getVertexList()
+                .stream()
+                .collect(Collectors.toList());
     }
 
     public int getDistance(){
-
-        return 0;
+        return (int) shortestPath.getWeight();
     }
 
 }
--- src/test/java/nextstep/subway/path/domain/ShortestPathTest.java
+++ src/test/java/nextstep/subway/path/domain/ShortestPathTest.java
@@ -1,11 +1,47 @@
 package nextstep.subway.path.domain;
 
+import nextstep.subway.line.domain.Line;
+import nextstep.subway.line.domain.Section;
+import nextstep.subway.station.domain.Station;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
+import static nextstep.subway.Fixture.createLine;
+import static nextstep.subway.Fixture.createStation;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class ShortestPathTest {
+
+    private Line 신분당선;
+    private Line 이호선;
+    private Line 삼호선;
+    private Station 강남역;
+    private Station 광산역;
+    private Station 양재역;
+    private Station 교대역;
+    private Station 남부터미널역;
+    private Station 학동역;
+    private Station 강남구청역;
+
+    @BeforeEach
+    public void setUp() {
+        강남역 = createStation("강남역", 1l);
+        양재역 = createStation("양재역", 2l);
+        교대역 = createStation("교대역", 3l);
+        남부터미널역 = createStation("남부터미널역", 4l);
+        광산역 = createStation("광산역", 7l);
+        신분당선 = createLine("신분당선", "bg-red-600", 강남역, 양재역, 10);
+        이호선 = createLine("이호선", "bg-red-600", 교대역, 강남역, 10);
+        삼호선 = createLine("삼호선", "bg-red-600", 교대역, 양재역, 5);
+        삼호선.addSection(new Section(삼호선, 교대역, 남부터미널역, 3));
+        삼호선.addSection(new Section(삼호선, 광산역, 교대역, 5));
+        학동역 = createStation("강남역", 10l);
+        강남구청역 = createStation("강남역", 11l);
+    }
 
     @DisplayName("경로값이 없으면 예외발생")
     @Test
@@ -15,5 +51,13 @@ public class ShortestPathTest {
                 .hasMessageContaining("경로가 존재해야 합니다");
     }
 
-
+    @DisplayName("최단경로에 해당하는 역들을 조회")
+    @Test
+    void returnsStations() {
+        PathFinder pathFinder = new PathFinder(Arrays.asList(신분당선, 이호선, 삼호선));
+        ShortestPath shortestPath = pathFinder.getShortestPath(광산역, 양재역,13);
+        assertThat(shortestPath.getStations())
+                .containsExactly(광산역, 교대역, 남부터미널역, 양재역);
+    }
 }
+
