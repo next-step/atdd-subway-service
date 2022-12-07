44f87f6ee9ab979764c7e41c9b86676ea0dbba8b
namhyun.sung <namhyun.sung@yanolja.com>
2022-12-08 03:32:46 +0900

    feat(FavoriteController): 즐겨찾기 조회 엔드포인트 구현

--- src/main/java/nextstep/subway/favorite/ui/FavoriteController.java
+++ src/main/java/nextstep/subway/favorite/ui/FavoriteController.java
@@ -5,12 +5,15 @@ import nextstep.subway.auth.domain.LoginMember;
 import nextstep.subway.favorite.application.FavoriteService;
 import nextstep.subway.favorite.domain.Favorite;
 import nextstep.subway.favorite.dto.FavoriteRequest;
+import nextstep.subway.favorite.dto.FavoriteResponse;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.net.URI;
+import java.util.List;
 
 @RestController
 public class FavoriteController {
@@ -23,8 +26,13 @@ public class FavoriteController {
 
     @PostMapping("/favorites")
     public ResponseEntity createFavorite(@AuthenticationPrincipal LoginMember loginMember,
-        @RequestBody FavoriteRequest favoriteRequest) {
+                                         @RequestBody FavoriteRequest favoriteRequest) {
         Favorite favorite = favoriteService.save(loginMember.getId(), favoriteRequest.getSource(), favoriteRequest.getTarget());
         return ResponseEntity.created(URI.create("/favorites/" + favorite.getId())).build();
+    }
+
+    @GetMapping("/favorites")
+    public ResponseEntity<List<FavoriteResponse>> findFavorites(@AuthenticationPrincipal LoginMember loginMember) {
+        return ResponseEntity.ok(favoriteService.findAllByMemberId(loginMember.getId()));
     }
 }
