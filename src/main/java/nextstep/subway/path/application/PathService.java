308083b610b993be4003ffeef2d0302552e92bfa
hahoho87 <bbaing4564@gmail.com>
2022-12-08 01:58:30 +0900

    feat: 최단거리 조회 controller 생성

---
+++ src/main/java/nextstep/subway/path/application/PathService.java
@@ -0,0 +1,13 @@
+package nextstep.subway.path.application;
+
+import org.springframework.stereotype.Service;
+
+import nextstep.subway.path.dto.PathRequest;
+import nextstep.subway.path.dto.PathResponse;
+
+@Service
+public class PathService {
+	public PathResponse findShortestPath(PathRequest request) {
+		return null;
+	}
+}
