90bd78f01bb0724b4bff367a2037e17f391ca5a3
jimbaemon <jimbae@kakao.com>
2022-12-09 02:30:01 +0900

    feat: 요금 도메인 생성

---
+++ src/main/java/nextstep/subway/fare/domain/Fare.java
@@ -0,0 +1,47 @@
+package nextstep.subway.fare.domain;
+
+import java.util.Objects;
+
+public class Fare {
+
+    public static final Fare DEFAULT_FARE = new Fare(1250);
+
+    private final int fare;
+
+    public Fare(int fare) {
+        this.fare = fare;
+    }
+
+    public Fare plus(Fare other) {
+        return new Fare(this.fare + other.fare);
+    }
+
+    public Fare minus(Fare other) {
+        return new Fare(this.fare - other.fare);
+    }
+
+    public Fare discount(int percent) {
+        return this.minus(new Fare(disCountPrice(percent)));
+    }
+
+    private int disCountPrice(int percent) {
+        return fare * percent / 100;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Fare fare1 = (Fare) o;
+        return fare == fare1.fare;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(fare);
+    }
+}
