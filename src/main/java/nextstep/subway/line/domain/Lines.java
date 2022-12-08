c6312ece06f3d1caaf9f5fb2220c5897f097a198
jylim <jylim@12cm.co.kr>
2022-12-08 21:28:00 +0900

    test(SectionsTest): getLines 테스트

--- src/main/java/nextstep/subway/line/domain/Lines.java
+++ src/main/java/nextstep/subway/line/domain/Lines.java
@@ -1,5 +1,6 @@
 package nextstep.subway.line.domain;
 
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
@@ -12,6 +13,11 @@ public class Lines {
 
     public static Lines of(List<Line> lines) {
         return new Lines(lines);
+    }
+
+
+    public List<Line> getLines() {
+        return Collections.unmodifiableList(lines);
     }
 
     public LineFare findMaxLineFare() {
