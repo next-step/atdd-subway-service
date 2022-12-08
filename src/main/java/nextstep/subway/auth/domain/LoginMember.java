f355611d5cd5194a3f0d9daf3c2d870eae4efad9
Minjae Kim <minjae.kim@mytrum.com>
2022-12-08 14:58:16 +0900

    feat: 로그인 멤버의 나이를 체크하여 할인 정책을 적용하는 기능 구현

--- src/main/java/nextstep/subway/auth/domain/LoginMember.java
+++ src/main/java/nextstep/subway/auth/domain/LoginMember.java
@@ -1,14 +1,23 @@
 package nextstep.subway.auth.domain;
 
+import nextstep.subway.member.domain.Age;
+
 public class LoginMember {
     private Long id;
     private String email;
-    private Integer age;
+    private Age age;
+    private DiscountPolicy discountPolicy;
 
     public LoginMember() {
     }
 
     public LoginMember(Long id, String email, Integer age) {
+        this.id = id;
+        this.email = email;
+        this.age = Age.from(age);
+    }
+
+    public LoginMember(Long id, String email, Age age) {
         this.id = id;
         this.email = email;
         this.age = age;
@@ -22,7 +31,11 @@ public class LoginMember {
         return email;
     }
 
-    public Integer getAge() {
+    public Age getAge() {
         return age;
+    }
+
+    public void addDiscountPolicy(DiscountPolicy discountPolicyByAge) {
+        this.discountPolicy = discountPolicyByAge;
     }
 }
--- src/main/java/nextstep/subway/auth/ui/AuthenticationPrincipalArgumentResolver.java
+++ src/main/java/nextstep/subway/auth/ui/AuthenticationPrincipalArgumentResolver.java
@@ -2,6 +2,8 @@ package nextstep.subway.auth.ui;
 
 import nextstep.subway.auth.application.AuthService;
 import nextstep.subway.auth.domain.AuthenticationPrincipal;
+import nextstep.subway.auth.domain.DiscountPolicy;
+import nextstep.subway.auth.domain.LoginMember;
 import nextstep.subway.auth.infrastructure.AuthorizationExtractor;
 import org.springframework.core.MethodParameter;
 import org.springframework.web.bind.support.WebDataBinderFactory;
@@ -24,8 +26,11 @@ public class AuthenticationPrincipalArgumentResolver implements HandlerMethodArg
     }
 
     @Override
-    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) {
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer,
+            NativeWebRequest webRequest, WebDataBinderFactory binderFactory) {
         String credentials = AuthorizationExtractor.extract(webRequest.getNativeRequest(HttpServletRequest.class));
+        LoginMember loginMember = authService.findMemberByToken(credentials);
+        loginMember.addDiscountPolicy(DiscountPolicy.from(loginMember.getAge()));
         return authService.findMemberByToken(credentials);
     }
 }
