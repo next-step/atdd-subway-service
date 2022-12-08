3f3d6b28df5128637503a8333e80915f4fa7c672
chosundeveloper <67059660+chosundeveloper@users.noreply.github.com>
2022-12-08 21:49:47 +0900

    test(FareTest.java) : 10km 초과  50km 까지(5km 마다 100원)

--- src/main/java/nextstep/subway/path/domain/Fare.java
+++ src/main/java/nextstep/subway/path/domain/Fare.java
@@ -2,6 +2,8 @@ package nextstep.subway.path.domain;
 
 public class Fare {
 
+    public static final int DEFAULT_FARE = 1250;
+    public static final int DEFAULT_DISTANCE = 10;
     private final int distance;
 
     protected Fare(int distance) {
@@ -9,9 +11,20 @@ public class Fare {
     }
 
     public int calculate() {
-        if (distance <= 10) {
-            return 1250;
+        if (distance <= DEFAULT_DISTANCE) {
+            return DEFAULT_FARE;
         }
-        return 0;
+        if (10 < this.distance && this.distance <= 50) {
+            return DEFAULT_FARE + calculateOverFareByFiveKm(this.distance - DEFAULT_DISTANCE);
+        }
+        return DEFAULT_FARE + calculateOverFareByEightKm(this.distance - DEFAULT_DISTANCE);
+    }
+
+    private int calculateOverFareByFiveKm(int distance) {
+        return (int) ((Math.ceil((distance - 1) / 5) + 1) * 100);
+    }
+
+    private int calculateOverFareByEightKm(int distance) {
+        return (int) ((Math.ceil((distance - 1) / 5) + 1) * 100);
     }
 }
