d4d9b4d88b4680f0e8f6e6713030072c09b8209b
Minjae Kim <minjae.kim@mytrum.com>
2022-12-08 14:56:26 +0900

    feat: 나이 클래스 구현

---
+++ src/main/java/nextstep/subway/member/domain/Age.java
@@ -0,0 +1,49 @@
+package nextstep.subway.member.domain;
+
+import javax.persistence.Embeddable;
+
+@Embeddable
+public class Age {
+
+    public static final int CHILD_START_AGE = 6;
+    public static final int CHILD_END_AGE = 12;
+    public static final int TEENAGER_START_AGE = 13;
+    public static final int TEENAGER_END_AGE = 18;
+
+    private int age;
+
+    private Age(int age) {
+        this.age = age;
+    }
+
+    protected Age() {
+    }
+
+    public static Age from(int age) {
+        return new Age(age);
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    protected void setAge(int age) {
+        this.age = age;
+    }
+
+    public boolean isChild() {
+        return isEqualsAfter(CHILD_START_AGE) && isEqualsBefore(CHILD_END_AGE);
+    }
+
+    public boolean isTeenager() {
+        return isEqualsAfter(TEENAGER_START_AGE) && isEqualsBefore(TEENAGER_END_AGE);
+    }
+
+    private boolean isEqualsBefore(int age) {
+        return this.age <= age;
+    }
+
+    private boolean isEqualsAfter(int age) {
+        return this.age >= age;
+    }
+}
