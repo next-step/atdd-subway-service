2e60cb218a768c4b9a1b7053b436d2299ea75698
Minjae Kim <minjae.kim@mytrum.com>
2022-12-08 14:54:31 +0900

    test: 요금 기능 생성

---
+++ src/main/java/nextstep/subway/auth/domain/Money.java
@@ -0,0 +1,49 @@
+package nextstep.subway.auth.domain;
+
+import java.util.Objects;
+
+public class Money {
+
+    private final double amount;
+
+    public Money(double amount) {
+        this.amount = amount;
+    }
+
+    public double getAmount() {
+        return this.amount;
+    }
+
+    public Money plus(Money targetMoney) {
+        return new Money(this.amount + targetMoney.amount);
+    }
+
+    public Money minus(Money targetMoney) {
+        if (this.amount < targetMoney.amount) {
+            throw new IllegalArgumentException(
+                    "기존 요금은 차감하고자 하는 요금보다 커야합니다. 기존요금 " + this.amount + " 차감요금:" + targetMoney.amount);
+        }
+        return new Money(this.amount - targetMoney.amount);
+    }
+
+    public Money divideByDecimalPoint(double rate) {
+        return new Money(this.amount - (this.amount * rate));
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Money money = (Money) o;
+        return Double.compare(money.amount, amount) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(amount);
+    }
+}
