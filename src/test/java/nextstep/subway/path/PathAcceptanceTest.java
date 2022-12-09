1b146702f5983f251796d20445f9e2ac06131c6f
namhyun.sung <namhyun.sung@yanolja.com>
2022-12-10 01:24:23 +0900

    fix(LineAcceptanceTest, PathAcceptanceTest): fare 변수 적용

--- src/test/java/nextstep/subway/path/PathAcceptanceTest.java
+++ src/test/java/nextstep/subway/path/PathAcceptanceTest.java
@@ -69,9 +69,9 @@ public class PathAcceptanceTest extends AcceptanceTest {
         남부터미널역 = StationAcceptanceTest.지하철역_등록되어_있음("남부터미널역").as(StationResponse.class);
         부산역 = StationAcceptanceTest.지하철역_등록되어_있음("부산역").as(StationResponse.class);
 
-        신분당선 = 지하철_노선_등록되어_있음(new LineRequest("신분당선", "bg-red-600", 강남역.getId(), 양재역.getId(), 10)).as(LineResponse.class);
-        이호선 = 지하철_노선_등록되어_있음(new LineRequest("이호선", "bg-red-600", 교대역.getId(), 강남역.getId(), 10)).as(LineResponse.class);
-        삼호선 = 지하철_노선_등록되어_있음(new LineRequest("삼호선", "bg-red-600", 교대역.getId(), 양재역.getId(), 11)).as(LineResponse.class);
+        신분당선 = 지하철_노선_등록되어_있음(new LineRequest("신분당선", "bg-red-600", 강남역.getId(), 양재역.getId(), 10, 100)).as(LineResponse.class);
+        이호선 = 지하철_노선_등록되어_있음(new LineRequest("이호선", "bg-red-600", 교대역.getId(), 강남역.getId(), 10, 200)).as(LineResponse.class);
+        삼호선 = 지하철_노선_등록되어_있음(new LineRequest("삼호선", "bg-red-600", 교대역.getId(), 양재역.getId(), 11, 300)).as(LineResponse.class);
 
         지하철_노선에_지하철역_등록_요청(삼호선, 교대역, 남부터미널역, 3);
     }
@@ -84,7 +84,7 @@ public class PathAcceptanceTest extends AcceptanceTest {
         최단_경로_조회_응답됨(response);
         경로_순서에_맞게_노출됨(response, Arrays.asList(교대역, 남부터미널역, 양재역));
         최단_경로_거리_확인됨(response, 11);
-        총_요금_확인됨(response, 1350);
+        총_요금_확인됨(response, 1650);
     }
 
     private void find_shortest_path_same_station() {
