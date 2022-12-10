5c7d71f562048d6db1677c04c6df41f6c14a8a69
namhyun.sung <namhyun.sung@yanolja.com>
2022-12-10 12:15:09 +0900

    fix(AgeFarePolicy): 순서와 무관하게 동작되도록 변경

--- src/main/java/nextstep/subway/path/domain/AgeFarePolicy.java
+++ src/main/java/nextstep/subway/path/domain/AgeFarePolicy.java
@@ -3,28 +3,35 @@ package nextstep.subway.path.domain;
 import java.util.Arrays;
 
 public enum AgeFarePolicy {
-    OLD(65, 100),
-    ADULT(19, 0),
-    TEENAGER(13, 20),
-    CHILDREN(6, 50),
-    TODDLER(0, 100);
+    OLD(65, Integer.MAX_VALUE, 100),
+    ADULT(19, 64,0),
+    TEENAGER(13, 18, 20),
+    CHILDREN(6, 12, 50),
+    TODDLER(0, 5,100);
 
     private static final int DEFAULT_MINUS_FARE = 350;
 
-    private final int age;
+    private final int minimumAge;
+    private final int maximumAge;
     private final double discount;
 
-    AgeFarePolicy(int age, int discount) {
-        this.age = age;
+    AgeFarePolicy(int minimumAge, int maximumAge, double discount) {
+        this.minimumAge = minimumAge;
+        this.maximumAge = maximumAge;
         this.discount = discount;
     }
 
     public static AgeFarePolicy of(int age) {
         return Arrays.stream(values())
-            .filter(it -> it.age <= age)
+            .filter(it -> it.checkAgeRange(age))
             .findFirst()
             .orElseThrow(IllegalArgumentException::new);
     }
+
+    private boolean checkAgeRange(int age) {
+        return this.minimumAge <= age && this.maximumAge >= age;
+    }
+
 
     public int discount(int fare) {
         if (discount == 0) {
