862103a08ea0ba256721f74f6675df9dcf26fbbf
diqksrk <diqksrk123@naver.com>
2022-12-11 17:22:02 +0900

    test : 최단 경로 조회 인수 테스트 작성

---
+++ src/test/java/nextstep/subway/line/acceptance/PathAcceptanceTest.java
@@ -0,0 +1,134 @@
+package nextstep.subway.line.acceptance;
+
+import io.restassured.RestAssured;
+import nextstep.subway.AcceptanceTest;
+import nextstep.subway.line.domain.Section;
+import nextstep.subway.line.domain.SectionRepository;
+import nextstep.subway.line.dto.LineRequest;
+import nextstep.subway.line.dto.LineResponse;
+import nextstep.subway.station.StationAcceptanceTest;
+import nextstep.subway.station.domain.Station;
+import nextstep.subway.station.domain.StationRepository;
+import nextstep.subway.station.dto.StationResponse;
+import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
+import org.jgrapht.graph.DefaultWeightedEdge;
+import org.jgrapht.graph.WeightedMultigraph;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static nextstep.subway.line.acceptance.LineAcceptanceTest.지하철_노선_생성_요청;
+import static nextstep.subway.line.acceptance.LineSectionAcceptanceTest.지하철_노선에_지하철역_등록_요청;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@DisplayName("지하철 구간 경로 관련 기능")
+public class PathAcceptanceTest extends AcceptanceTest {
+    private LineResponse 신분당선;
+    private LineResponse 이호선;
+    private LineResponse 삼호선;
+    private StationResponse 강남역;
+    private StationResponse 양재역;
+    private StationResponse 교대역;
+    private StationResponse 남부터미널역;
+
+    @Autowired
+    private StationRepository stationRepository;
+
+    @Autowired
+    private SectionRepository sectionRepository;
+
+    /**
+     * 교대역    --- *2호선* ---   강남역
+     * |                        |
+     * *3호선*                   *신분당선*
+     * |                        |
+     * 남부터미널역  --- *3호선* ---   양재
+     */
+    @BeforeEach
+    public void setUp() {
+        super.setUp();
+
+        강남역 = StationAcceptanceTest.지하철역_등록되어_있음("강남역").as(StationResponse.class);
+        양재역 = StationAcceptanceTest.지하철역_등록되어_있음("양재역").as(StationResponse.class);
+        교대역 = StationAcceptanceTest.지하철역_등록되어_있음("교대역").as(StationResponse.class);
+        남부터미널역 = StationAcceptanceTest.지하철역_등록되어_있음("남부터미널역").as(StationResponse.class);
+
+        신분당선 = 지하철_노선_등록되어_있음("신분당선", "bg-red-600", 강남역, 양재역, 10);
+        이호선 = 지하철_노선_등록되어_있음("이호선", "bg-red-600", 교대역, 강남역, 10);
+        삼호선 = 지하철_노선_등록되어_있음("삼호선", "bg-red-600", 교대역, 양재역, 5);
+
+        지하철_노선에_지하철역_등록_요청(삼호선, 교대역, 남부터미널역, 3);
+    }
+
+    private LineResponse 지하철_노선_등록되어_있음(String name, String color, StationResponse upStation, StationResponse downStation, int distance) {
+        LineRequest params = new LineRequest(name, color, upStation.getId(), downStation.getId(), distance);
+        return 지하철_노선_생성_요청(params).body()
+                .jsonPath()
+                .getObject(".", LineResponse.class);
+    }
+
+    @Test
+    @DisplayName("다익스트라 알고리즘 예시")
+    public void getDijkstraShortestPath() {
+        WeightedMultigraph<String, DefaultWeightedEdge> graph
+                = new WeightedMultigraph(DefaultWeightedEdge.class);
+        graph.addVertex("v1");
+        graph.addVertex("v2");
+        graph.addVertex("v3");
+        graph.setEdgeWeight(graph.addEdge("v1", "v2"), 2);
+        graph.setEdgeWeight(graph.addEdge("v2", "v3"), 2);
+        graph.setEdgeWeight(graph.addEdge("v1", "v3"), 100);
+
+        DijkstraShortestPath dijkstraShortestPath
+                = new DijkstraShortestPath(graph);
+        List<String> shortestPath
+                = dijkstraShortestPath.getPath("v3", "v1").getVertexList();
+
+        assertThat(shortestPath.size()).isEqualTo(3);
+    }
+
+    @Test
+    @DisplayName("테스트 - 최적경로 조회 테스트(추후 삭제)")
+    void getShortestLinePathTest() {
+        WeightedMultigraph<String, DefaultWeightedEdge> graph
+                = new WeightedMultigraph(DefaultWeightedEdge.class);
+        List<Station> stationAll = stationRepository.findAll();
+        List<Section> sectionAll = sectionRepository.findAll();
+
+        for (Station station : stationAll) {
+            graph.addVertex(String.valueOf(station.getName()));
+        }
+
+        for (Section section : sectionAll) {
+            graph.setEdgeWeight(graph.addEdge(section.getUpStation().getName(), section.getDownStation().getName()), section.getDistance());
+        }
+
+        DijkstraShortestPath dijkstraShortestPath
+                = new DijkstraShortestPath(graph);
+        List<String> shortestPath
+                = dijkstraShortestPath.getPath("교대역", "양재역").getVertexList();
+
+        assertThat(shortestPath.size()).isEqualTo(3);
+        assertThat(shortestPath.containsAll(Arrays.asList("교대역", "남부터미널역", "양재역"))).isTrue();
+    }
+
+    /**
+     * Given 출발역과 도착역을 받으면
+     * When 다익스트라 알고리즘으로 최적의 경우를 구한후
+     * Then 최적의 경로를 돌려준다.
+     */
+    @Test
+    @DisplayName("최적경로 조회")
+    void getShortestPath() {
+        // 요청 Request를 세팅하고
+
+        // Assert를 이용해 요청을 보내고
+
+        // 응답값을 받는다
+    }
+}
