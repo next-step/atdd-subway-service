ca0100a87615185cf26bfb1f78fb9b3782206ed7
tlaqk229 <tlaqk229@gmail.com>
2022-12-13 03:26:13 +0900

    refactor(LineService): 서비스단의 구간제거 로직 도메인단으로 이동

--- src/main/java/nextstep/subway/line/domain/Line.java
+++ src/main/java/nextstep/subway/line/domain/Line.java
@@ -1,5 +1,6 @@
 package nextstep.subway.line.domain;
 
+import java.util.Optional;
 import nextstep.subway.BaseEntity;
 import nextstep.subway.station.domain.Station;
 
@@ -72,6 +73,31 @@ public class Line extends BaseEntity {
             downStation = nextSection.getStationByPosition(stationPosition);
         }
         return downStation;
+    }
+
+    public void checkLineStationRemovable() {
+        if (sections.size() <= 1) {
+            throw new RuntimeException();
+        }
+    }
+
+    public void removeLineStation(Station station) {
+        Optional<Section> upLineStation = sections.stream()
+                .filter(it -> it.getUpStation() == station)
+                .findFirst();
+        Optional<Section> downLineStation = sections.stream()
+                .filter(it -> it.getDownStation() == station)
+                .findFirst();
+
+        if (upLineStation.isPresent() && downLineStation.isPresent()) {
+            Station newUpStation = downLineStation.get().getUpStation();
+            Station newDownStation = upLineStation.get().getDownStation();
+            int newDistance = upLineStation.get().getDistance() + downLineStation.get().getDistance();
+            sections.add(new Section(this, newUpStation, newDownStation, newDistance));
+        }
+
+        upLineStation.ifPresent(it -> sections.remove(it));
+        downLineStation.ifPresent(it -> sections.remove(it));
     }
 
     public Long getId() {
