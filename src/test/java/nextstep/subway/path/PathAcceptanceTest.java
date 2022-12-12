e7490583c16a77ec37ecc92ca1ccf87200108e2f
deokmoon <deok2moon@gmail.com>
2022-12-13 02:55:06 +0900

    test: 요구사항 인수테스트 모두 작성

--- src/test/java/nextstep/subway/path/PathAcceptanceTest.java
+++ src/test/java/nextstep/subway/path/PathAcceptanceTest.java
@@ -43,10 +43,13 @@ public class PathAcceptanceTest extends AcceptanceTest {
     private LineResponse 신분당선;
     private LineResponse 이호선;
     private LineResponse 삼호선;
+    private LineResponse 사호선;
     private StationResponse 강남역;
     private StationResponse 양재역;
     private StationResponse 교대역;
     private StationResponse 남부터미널역;
+    private StationResponse 사당역;
+    private StationResponse 이수역;
 
     /**
      * 교대역    --- *2호선* ---   강남역
@@ -54,6 +57,8 @@ public class PathAcceptanceTest extends AcceptanceTest {
      * *3호선*                   *신분당선*
      * |                        |
      * 남부터미널역  --- *3호선* ---   양재
+     * <p>
+     * 사당역----4호선(5)------이수역
      */
     @BeforeEach
     public void setUp() {
@@ -63,10 +68,13 @@ public class PathAcceptanceTest extends AcceptanceTest {
         양재역 = StationAcceptanceTest.지하철역_등록되어_있음("양재역").as(StationResponse.class);
         교대역 = StationAcceptanceTest.지하철역_등록되어_있음("교대역").as(StationResponse.class);
         남부터미널역 = StationAcceptanceTest.지하철역_등록되어_있음("남부터미널역").as(StationResponse.class);
+        사당역 = StationAcceptanceTest.지하철역_등록되어_있음("사당역").as(StationResponse.class);
+        이수역 = StationAcceptanceTest.지하철역_등록되어_있음("이수역").as(StationResponse.class);
 
         신분당선 = LineRestAssured.지하철_노선_등록되어_있음(new LineRequest("신분당선", "bg-red-600", 강남역.getId(), 양재역.getId(), 10)).as(LineResponse.class);
         이호선 = LineRestAssured.지하철_노선_등록되어_있음(new LineRequest("이호선", "bg-red-600", 교대역.getId(), 강남역.getId(), 10)).as(LineResponse.class);
         삼호선 = LineRestAssured.지하철_노선_등록되어_있음(new LineRequest("삼호선", "bg-red-600", 교대역.getId(), 양재역.getId(), 5)).as(LineResponse.class);
+        사호선 = LineRestAssured.지하철_노선_등록되어_있음(new LineRequest("사호선", "bg-blue-600", 사당역.getId(), 이수역.getId(), 5)).as(LineResponse.class);
 
         LineSectionRestAssured.지하철_노선에_지하철역_등록되어_있음(삼호선, 교대역, 남부터미널역, 3);
     }
@@ -78,36 +86,42 @@ public class PathAcceptanceTest extends AcceptanceTest {
         지하철_최단_경로_조회_조회됨(response,10);
     }
 
-    @DisplayName("예외발생 - 출발역과 도착역으로 최단 경로 조회")
+    @DisplayName("예외발생 - 출발역과 도착역이 같은데 조회하는 경우")
     @Test
-    void makeExceptionWhenSourceStationAndTargetStationIsSame() {
-
+    void makeExceptionWhenSourceStationAndTargetStationIsEqual() {
+        ExtractableResponse<Response> response = 지하철_최단_경로_조회_요청(교대역.getId(), 교대역.getId());
+        지하철_최단_경로_조회_실패됨(response);
     }
 
     @DisplayName("예외발생 - 연결되어 있지 않은 출발역과 도착역으로 최단 경로 조회")
     @Test
     void makeExceptionWhenSourceStationAndTargetStationIsNotBelongToSameLine() {
-
+        ExtractableResponse<Response> response = 지하철_최단_경로_조회_요청(교대역.getId(), 사당역.getId());
+        지하철_최단_경로_조회_실패됨(response);
     }
 
     @DisplayName("예외발생 - 존재하지 않은 출발역으로 최단 경로 조회")
     @Test
     void makeExceptionWhenSourceStationIsNotExist() {
-
+        ExtractableResponse<Response> response = 지하철_최단_경로_조회_요청(교대역.getId(), 10L);
+        지하철_최단_경로_조회_실패됨(response);
     }
 
     @DisplayName("예외발생 - 존재하지 않은 도착역으로 최단 경로 조회")
     @Test
     void makeExceptionWhenTargetStationIsNotExist() {
-
+        ExtractableResponse<Response> response = 지하철_최단_경로_조회_요청(10L, 교대역.getId());
+        지하철_최단_경로_조회_실패됨(response);
     }
 
-
-    @Test
     public static void 지하철_최단_경로_조회_조회됨(ExtractableResponse<Response> response, int expectDistance) {
         assertAll(() -> {
-            assertThat(response.statusCode()).isEqualTo(HttpStatus.OK);
-            assertThat(response.as(PathResponse.class)).isEqualTo(expectDistance);
+            assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+            assertThat(response.as(PathResponse.class).getDistance()).isEqualTo(expectDistance);
         });
+    }
+
+    public static void 지하철_최단_경로_조회_실패됨(ExtractableResponse<Response> response) {
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR.value());
     }
 }
--- src/test/java/nextstep/subway/path/PathRestAssured.java
+++ src/test/java/nextstep/subway/path/PathRestAssured.java
@@ -5,13 +5,19 @@ import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import org.springframework.http.MediaType;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class PathRestAssured {
     public static ExtractableResponse<Response> 지하철_최단_경로_조회_요청(Long sourceStationId, Long targetStationId) {
-        return RestAssured
-                .given().log().all()
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .when().get("/lines/{lineId}/paths?source={sourceId}&target={targetId}", sourceStationId, targetStationId)
-                .then().log().all().
-                        extract();
+        Map<String, Long> params = new HashMap<>();
+        params.put("source", sourceStationId);
+        params.put("target", targetStationId);
+        return RestAssured.given().log().all()
+                .queryParams(params)
+                .accept(MediaType.APPLICATION_JSON_VALUE)
+                .when().get("/paths")
+                .then().log().all()
+                .extract();
     }
 }
