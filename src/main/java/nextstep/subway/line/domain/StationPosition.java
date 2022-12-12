b656749a26927e2bbe93e9e1eb5a44d4a46b6efc
tlaqk229 <tlaqk229@gmail.com>
2022-12-13 02:46:01 +0900

    feat(StationPosition): 구간 내 지하철역 위치 Enum 클래스 추가

---
+++ src/main/java/nextstep/subway/line/domain/StationPosition.java
@@ -0,0 +1,18 @@
+package nextstep.subway.line.domain;
+
+public enum StationPosition {
+    UP_STATION,
+    DOWN_STATION;
+
+    public boolean isUpStation() {
+        return this == UP_STATION;
+    }
+
+    public boolean isDownStation() {
+        return this == DOWN_STATION;
+    }
+
+    public boolean isOpposite(StationPosition stationPosition) {
+        return stationPosition != null & this != stationPosition;
+    }
+}
