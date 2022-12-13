7fb3ad3c4dfa39a3defd894ed65f1c36290639e1
tlaqk229 <tlaqk229@gmail.com>
2022-12-13 15:52:09 +0900

    feat(Distance): 구간 길이 원시값 포장 클래스 추가

---
+++ src/main/java/nextstep/subway/line/domain/Distance.java
@@ -0,0 +1,53 @@
+package nextstep.subway.line.domain;
+
+import java.util.Objects;
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+@Embeddable
+public class Distance implements Comparable<Distance> {
+
+    @Column(nullable = false)
+    private int distance;
+
+    public Distance() {
+    }
+
+    public Distance(int distance) {
+        this.distance = distance;
+    }
+
+    public Distance add(Distance distance) {
+        return new Distance(this.distance + distance.distance);
+    }
+
+    public void subtract(Distance distanceToSubtract) {
+        distance -= distanceToSubtract.distance;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Distance distance1 = (Distance) o;
+        return distance == distance1.distance;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(distance);
+    }
+
+    @Override
+    public int compareTo(Distance o) {
+        return Integer.compare(this.distance, o.distance);
+    }
+}
