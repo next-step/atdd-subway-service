4a1b352fcbb43a0b3bc9e4f0bdc3af8f6d0bb74f
tlaqk229 <tlaqk229@gmail.com>
2022-12-13 13:41:02 +0900

    refactor(LineTest): 테스트 setUp 추가

--- src/test/java/nextstep/subway/line/domain/LineTest.java
+++ src/test/java/nextstep/subway/line/domain/LineTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 import nextstep.subway.station.domain.Station;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -11,16 +12,23 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 @DataJpaTest
 public class LineTest {
 
+    private Station 강남역;
+    private Station 양재역;
+    private Line 신분당선;
+
+    @BeforeEach
+    public void setUp() {
+        강남역 = new Station("강남역");
+        양재역 = new Station("양재역");
+        신분당선 = new Line("신분당선", "red", 강남역, 양재역, 10);
+    }
+
     @DisplayName("지정위치에 따라 다음역 찾기")
     @Test
     void 다음역_찾기() {
-        Station 강남역 = new Station("강남역");
-        Station 양재역 = new Station("양재역");
-        Line line = new Line("신분당선", "red", 강남역, 양재역, 10);
-
         assertAll(
-            () -> assertThat(line.nextStationOf(강남역, StationPosition.DOWN_STATION)).isEqualTo(양재역),
-            () -> assertThat(line.nextStationOf(양재역, StationPosition.UP_STATION)).isEqualTo(강남역)
+            () -> assertThat(신분당선.nextStationOf(강남역, StationPosition.DOWN_STATION)).isEqualTo(양재역),
+            () -> assertThat(신분당선.nextStationOf(양재역, StationPosition.UP_STATION)).isEqualTo(강남역)
         );
     }
 
@@ -28,15 +36,12 @@ public class LineTest {
     @Test
     void 상행종점_찾기() {
         Station 신논현역 = new Station("신논현역");
-        Station 강남역 = new Station("강남역");
-        Station 양재역 = new Station("양재역");
         Station 정자역 = new Station("정자역");
-        Line line = new Line("신분당선", "red", 양재역, 정자역, 10);
-        Section section1 = new Section(line, 강남역, 양재역, 10);
-        Section section2 = new Section(line, 신논현역, 강남역, 10);
-        line.getSections().add(section1);
-        line.getSections().add(section2);
+        Section 구간1 = new Section(신분당선, 신논현역, 강남역, 10);
+        Section 구간2 = new Section(신분당선, 양재역, 정자역, 10);
+        신분당선.getSections().add(구간1);
+        신분당선.getSections().add(구간2);
 
-        assertThat(line.findFinalUpStation()).isEqualTo(신논현역);
+        assertThat(신분당선.findFinalUpStation()).isEqualTo(신논현역);
     }
 }
