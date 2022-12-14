9c974c0a35c4967d677f38d3a42aaffbcdb2bde3
mansik <90mansin@gmail.com>
2022-12-15 01:08:22 +0900

    feat: 출발역과 도착역이 같은 경우 예외 검증 개발

--- src/main/java/nextstep/subway/path/domain/PathFinder.java
+++ src/main/java/nextstep/subway/path/domain/PathFinder.java
@@ -1,8 +1,8 @@
 package nextstep.subway.path.domain;
 
+import nextstep.subway.enums.ErrorMessage;
 import nextstep.subway.line.domain.Line;
 import nextstep.subway.line.domain.Section;
-import nextstep.subway.path.domain.Path;
 import nextstep.subway.station.domain.Station;
 import org.jgrapht.GraphPath;
 import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
@@ -12,7 +12,6 @@ import org.jgrapht.graph.WeightedMultigraph;
 import java.util.List;
 
 public class PathFinder {
-
     private final WeightedMultigraph<Station, DefaultWeightedEdge> graph = new WeightedMultigraph<>(DefaultWeightedEdge.class);
     private final DijkstraShortestPath<Station, DefaultWeightedEdge> dijkstraShortestPath = new DijkstraShortestPath<>(graph);
 
@@ -23,21 +22,27 @@ public class PathFinder {
         });
     }
 
+    private void addVertx(Line line) {
+        line.getStations().forEach(graph::addVertex);
+    }
+
+    public Path findShortPath(Station startStation, Station endStation) {
+        isValidDuplicatedStation(startStation, endStation);
+        GraphPath<Station, DefaultWeightedEdge> path = dijkstraShortestPath.getPath(startStation, endStation);
+        return new Path(path.getVertexList(), (int) path.getWeight());
+    }
+
+    private static void isValidDuplicatedStation(Station startStation, Station endStation) {
+        if (startStation.equals(endStation)) {
+            throw new IllegalArgumentException(ErrorMessage.DUPLICATED_STATION_PATH.getMessage());
+        }
+    }
+
     private void setEdgeWeight(List<Section> sections) {
         sections.forEach(section ->
                 graph.setEdgeWeight(
                         graph.addEdge(section.getUpStation(), section.getDownStation()),
                         section.getDistance()
                 ));
-    }
-
-    private void addVertx(Line line) {
-        line.getStations().forEach(graph::addVertex);
-    }
-
-    public Path findShortPath(Station startStation, Station endStation) {
-
-        GraphPath<Station, DefaultWeightedEdge> path = dijkstraShortestPath.getPath(startStation, endStation);
-        return new Path(path.getVertexList(), (int) path.getWeight());
     }
 }
