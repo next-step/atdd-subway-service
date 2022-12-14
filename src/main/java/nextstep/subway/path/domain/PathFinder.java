434de6fef4ede207c535b16a4d12f9a7df60d1e3
mansik <90mansin@gmail.com>
2022-12-15 01:52:48 +0900

    feat: 존재하지 않는 출발역이나 도착역을 조회 할 경우 예외 기능 개발

--- src/main/java/nextstep/subway/path/domain/PathFinder.java
+++ src/main/java/nextstep/subway/path/domain/PathFinder.java
@@ -12,8 +12,8 @@ import org.jgrapht.graph.WeightedMultigraph;
 import java.util.List;
 
 public class PathFinder {
-    private final WeightedMultigraph<Station, DefaultWeightedEdge> graph = new WeightedMultigraph<>(DefaultWeightedEdge.class);
-    private final DijkstraShortestPath<Station, DefaultWeightedEdge> dijkstraShortestPath = new DijkstraShortestPath<>(graph);
+    private static final WeightedMultigraph<Station, DefaultWeightedEdge> graph = new WeightedMultigraph<>(DefaultWeightedEdge.class);
+    private static final DijkstraShortestPath<Station, DefaultWeightedEdge> dijkstraShortestPath = new DijkstraShortestPath<>(graph);
 
     public PathFinder(List<Line> lines) {
         lines.forEach(line -> {
@@ -28,8 +28,15 @@ public class PathFinder {
 
     public Path findShortPath(Station startStation, Station endStation) {
         isValidDuplicatedStation(startStation, endStation);
+        isValidExistStation(startStation, endStation);
         GraphPath<Station, DefaultWeightedEdge> path = dijkstraShortestPath.getPath(startStation, endStation);
         return new Path(path.getVertexList(), (int) path.getWeight());
+    }
+
+    private static void isValidExistStation(Station startStation, Station endStation) {
+        if (!graph.containsVertex(startStation) || !graph.containsVertex(endStation)) {
+            throw new IllegalArgumentException(ErrorMessage.NOT_EXISTS_STATION_PATH.getMessage());
+        }
     }
 
     private static void isValidDuplicatedStation(Station startStation, Station endStation) {
