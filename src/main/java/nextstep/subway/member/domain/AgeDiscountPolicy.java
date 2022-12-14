55b4a5d3afd35978f494160e2fc19a9e295eb606
bky4564 <bky4564@hunet.co.kr>
2022-12-14 13:42:42 +0900

    refactor: 나이 정책 리팩토링
    - 기준 나이 상수화
    - IntPredicate 메서드 추출

--- src/main/java/nextstep/subway/member/domain/AgeDiscountPolicy.java
+++ src/main/java/nextstep/subway/member/domain/AgeDiscountPolicy.java
@@ -9,17 +9,21 @@ import nextstep.subway.common.domain.Fare;
 
 public enum AgeDiscountPolicy {
 
-	TODDLER(age -> age < 6, totalFare -> Fare.zero()),
-	KIDS(age -> age >= 6 && age < 13, totalFare -> discount(totalFare, Rate.KIDS_DISCOUNT_RATE)),
-	TEENAGER(age -> age >= 13 && age < 19, totalFare -> discount(totalFare, Rate.TEENAGER_DISCOUNT_RATE)),
-	ADULT(age -> age >= 19, totalFare -> totalFare);
+	TODDLER(AgeDiscountPolicy::isToddler, totalFare -> Fare.zero()),
+	KIDS(AgeDiscountPolicy::isKids, totalFare -> discount(totalFare, Rate.KIDS_DISCOUNT_RATE)),
+	TEENAGER(AgeDiscountPolicy::isTeenager, totalFare -> discount(totalFare, Rate.TEENAGER_DISCOUNT_RATE)),
+	ADULT(AgeDiscountPolicy::isAdult, totalFare -> totalFare);
 
 	private static final Fare FIX_DISCOUNT_FARE = Fare.from(350);
+	private static final int MINIMUM_KIDS_AGE = 6;
+	private static final int MINIMUM_TEENAGER_AGE = 13;
+	private static final int MINIMUM_ADULT_AGE = 19;
 
 	private static class Rate {
 		private static final double KIDS_DISCOUNT_RATE = 0.5;
 		private static final double TEENAGER_DISCOUNT_RATE = 0.2;
 	}
+
 	private final IntPredicate matcher;
 	private final UnaryOperator<Fare> expression;
 
@@ -39,6 +43,22 @@ public enum AgeDiscountPolicy {
 	private static Fare discount(Fare totalFare, double discountRate) {
 		return totalFare.subtract(FIX_DISCOUNT_FARE)
 			.percentOf(1 - discountRate);
+	}
+
+	private static boolean isAdult(int age) {
+		return MINIMUM_ADULT_AGE <= age;
+	}
+
+	private static boolean isTeenager(int age) {
+		return MINIMUM_TEENAGER_AGE <= age && age < MINIMUM_ADULT_AGE;
+	}
+
+	private static boolean isKids(int age) {
+		return MINIMUM_KIDS_AGE <= age && age < MINIMUM_TEENAGER_AGE;
+	}
+
+	private static boolean isToddler(int age) {
+		return age < MINIMUM_KIDS_AGE;
 	}
 
 	private static AgeDiscountPolicy valueOf(Age age) {
