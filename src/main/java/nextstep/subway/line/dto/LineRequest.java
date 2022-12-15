ace18f829aec04c785ce1a26ddc3bb9c88651f44
cylee9409 <cylee9409@gmail.com>
2022-12-15 11:13:12 +0900

    [feat] LineRequest 에 surCharge 추가

--- src/main/java/nextstep/subway/line/dto/LineRequest.java
+++ src/main/java/nextstep/subway/line/dto/LineRequest.java
@@ -2,6 +2,7 @@ package nextstep.subway.line.dto;
 
 import nextstep.subway.line.domain.Distance;
 import nextstep.subway.line.domain.Line;
+import nextstep.subway.line.domain.SurCharge;
 import nextstep.subway.station.domain.Station;
 
 public class LineRequest {
@@ -10,6 +11,7 @@ public class LineRequest {
     private Long upStationId;
     private Long downStationId;
     private int distance;
+    private int surCharge;
 
     public LineRequest() {
     }
@@ -22,8 +24,17 @@ public class LineRequest {
         this.distance = distance;
     }
 
+    public LineRequest(String name, String color, Long upStationId, Long downStationId, int distance, int surCharge) {
+        this.name = name;
+        this.color = color;
+        this.upStationId = upStationId;
+        this.downStationId = downStationId;
+        this.distance = distance;
+        this.surCharge = surCharge;
+    }
+
     public Line toLine(Station upStation, Station downStation) {
-        return new Line(name, color, upStation, downStation, new Distance(distance));
+        return new Line(name, color, upStation, downStation, new Distance(distance), new SurCharge(surCharge));
     }
 
     public String getName() {
@@ -44,6 +55,10 @@ public class LineRequest {
 
     public int getDistance() {
         return distance;
+    }
+
+    public int getSurCharge() {
+        return surCharge;
     }
 
     public Line toLine() {
