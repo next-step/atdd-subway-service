e59702cd8b4fa22096731945174914128f8c48cf
iamsojung <sojungkid@gmail.com>
2022-12-21 00:24:49 +0900

    refactor: PathFinder 주석제거

--- src/main/java/nextstep/subway/path/domain/PathFinder.java
+++ src/main/java/nextstep/subway/path/domain/PathFinder.java
@@ -73,34 +73,4 @@ public class PathFinder {
             throw new IllegalArgumentException("출발역에서 도착역까지의 경로를 찾을 수 없습니다.");
         }
     }
-
-//
-//
-//    private void addStationVertex(List<Station> stations) {
-//        for (Station station : stations) {
-//            graph.addVertex(station);
-//        }
-//    }
-//
-//    private void addEdgeSection(List<Section> sections) {
-//        for (Section section : sections) {
-//            graph.setEdgeWeight(graph.addEdge(section.getUpStation(), section.getDownStation()), section.getDistance().value());
-//        }
-//    }
-//
-//    public Path getShortestPath(Station sourceStation, Station targetStation) {
-//        validCheck(sourceStation, targetStation);
-//        GraphPath<Station, DefaultWeightedEdge> graphPath = dijkstraShortestPath.getPath(sourceStation, targetStation);
-//        return new Path(graphPath.getVertexList(), (int) graphPath.getWeight());
-//    }
-//
-//    private void validCheck(Station sourceStation, Station targetStation) {
-//        if (sourceStation.equals(targetStation)) {
-//            throw new IllegalArgumentException("출발역과 도착역이 같은 경우 최단 경로를 조회할 수 없습니다.");
-//        }
-//        if (!graph.containsVertex(sourceStation) || !graph.containsVertex(targetStation)) {
-//            throw new IllegalArgumentException("출발역과 도착역은 서로 연결이 되어있어야 최단 경로를 조회할 수 있습니다.");
-//        }
-//    }
-
-    }
+}
