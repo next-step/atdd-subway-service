0355784c5cb813943f2b7da274afa4f17af09a2c
tlaqk229 <tlaqk229@gmail.com>
2022-12-20 21:21:03 +0900

    feat(favorite): Favorite 연관관계 매핑

--- src/main/java/nextstep/subway/favorite/domain/Favorite.java
+++ src/main/java/nextstep/subway/favorite/domain/Favorite.java
@@ -1,12 +1,14 @@
 package nextstep.subway.favorite.domain;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import nextstep.subway.station.domain.Station;
-import org.springframework.stereotype.Component;
 
 @Entity
 public class Favorite {
@@ -18,11 +20,13 @@ public class Favorite {
     @Column(nullable = false)
     private Long memberId;
 
-    @Column(name = "source_id", nullable = false)
-    private Long sourceStationId;
+    @ManyToOne(cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "source_station_id", nullable = false)
+    private Station sourceStation;
 
-    @Column(name = "target_id", nullable = false)
-    private Long targetStationId;
+    @ManyToOne(cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "target_station_id", nullable = false)
+    private Station targetStation;
 
     public Favorite() {
     }
