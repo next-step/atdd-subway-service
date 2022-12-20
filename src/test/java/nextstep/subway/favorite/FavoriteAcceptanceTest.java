c5f9b53413cc6efb2296033bed38f8c5937612b5
tlaqk229 <tlaqk229@gmail.com>
2022-12-20 21:22:13 +0900

    test(favorite): 기능별 테스트 추가

--- src/test/java/nextstep/subway/favorite/FavoriteAcceptanceTest.java
+++ src/test/java/nextstep/subway/favorite/FavoriteAcceptanceTest.java
@@ -9,6 +9,7 @@ import java.util.Arrays;
 import java.util.List;
 import nextstep.subway.favorite.dto.FavoriteCreateRequest;
 import nextstep.subway.favorite.dto.FavoriteResponse;
+import nextstep.subway.station.dto.StationResponse;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -42,9 +43,11 @@ public class FavoriteAcceptanceTest extends FavoriteAcceptanceTestFixture {
         // Then 즐겨찾기 생성됨
         즐겨찾기_생성됨(response);
         FavoriteResponse 생성된_즐겨찾기 = 즐겨찾기_정보(response);
+        StationResponse 생성된_즐겨찾기_출발역 = 생성된_즐겨찾기.getSource();
+        StationResponse 생성된_즐겨찾기_도착역 = 생성된_즐겨찾기.getTarget();
         assertAll(
-                () -> assertThat(생성된_즐겨찾기.getSourceId()).isEqualTo(favoriteCreateRequest.getSource()),
-                () -> assertThat(생성된_즐겨찾기.getTargetId()).isEqualTo(favoriteCreateRequest.getTarget())
+                () -> assertThat(생성된_즐겨찾기_출발역.getId()).isEqualTo(favoriteCreateRequest.getSource()),
+                () -> assertThat(생성된_즐겨찾기_도착역.getId()).isEqualTo(favoriteCreateRequest.getTarget())
         );
 
         // When 즐겨찾기 목록 조회 요청
@@ -57,6 +60,50 @@ public class FavoriteAcceptanceTest extends FavoriteAcceptanceTestFixture {
 
         // When 즐겨찾기 삭제 요청
         response = 즐겨찾기_삭제_요청(myAccessToken, 생성된_즐겨찾기.getId());
+        // Then 즐겨찾기 삭제됨
+        즐겨찾기_삭제됨(response);
+    }
+
+    @DisplayName("즐겨찾기를 생성한다")
+    @Test
+    void createFavorite() {
+        // When 즐겨찾기 생성을 요청
+        FavoriteCreateRequest favoriteCreateRequest = new FavoriteCreateRequest(강남역.getId(), 정자역.getId());
+        ExtractableResponse<Response> response = 즐겨찾기_생성_요청(myAccessToken, favoriteCreateRequest);
+        // Then 즐겨찾기 생성됨
+        즐겨찾기_생성됨(response);
+        FavoriteResponse 생성된_즐겨찾기 = 즐겨찾기_정보(response);
+        StationResponse 생성된_즐겨찾기_출발역 = 생성된_즐겨찾기.getSource();
+        StationResponse 생성된_즐겨찾기_도착역 = 생성된_즐겨찾기.getTarget();
+        assertAll(
+                () -> assertThat(생성된_즐겨찾기_출발역.getId()).isEqualTo(favoriteCreateRequest.getSource()),
+                () -> assertThat(생성된_즐겨찾기_도착역.getId()).isEqualTo(favoriteCreateRequest.getTarget())
+        );
+    }
+
+    @DisplayName("즐겨찾기를 조회한다")
+    @Test
+    void findFavorites() {
+        // Given
+        FavoriteResponse 생성된_즐겨찾기 = 즐겨찾기_등록되어_있음(myAccessToken, new FavoriteCreateRequest(강남역.getId(), 정자역.getId()));
+        FavoriteResponse 생성된_즐겨찾기2 = 즐겨찾기_등록되어_있음(myAccessToken, new FavoriteCreateRequest(광교역.getId(), 양재역.getId()));
+
+        // When 즐겨찾기 목록 조회 요청
+        ExtractableResponse<Response> response = 즐겨찾기_정보_조회_요청(myAccessToken);
+        // Then 즐겨찾기 목록 조회됨
+        즐겨찾기_정보_조회됨(response);
+        List<FavoriteResponse> 조회된_즐겨찾기_목록 = 즐겨찾기_목록(response);
+        assertThat(조회된_즐겨찾기_목록).containsAll(Arrays.asList(생성된_즐겨찾기, 생성된_즐겨찾기2));
+    }
+
+    @DisplayName("즐겨찾기를 삭제한다")
+    @Test
+    void deleteFavorite() {
+        // Given
+        FavoriteResponse 생성된_즐겨찾기 = 즐겨찾기_등록되어_있음(myAccessToken, new FavoriteCreateRequest(강남역.getId(), 정자역.getId()));
+
+        // When 즐겨찾기 삭제 요청
+        ExtractableResponse<Response> response = 즐겨찾기_삭제_요청(myAccessToken, 생성된_즐겨찾기.getId());
         // Then 즐겨찾기 삭제됨
         즐겨찾기_삭제됨(response);
     }
