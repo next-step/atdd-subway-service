32e4203c6ecc506fbe15b016b61ac784fc1de049
지영진 <wldudwls12@gmail.com>
2022-12-23 01:20:12 +0900

    test: LineSectionAcceptanceTest 인수 테스트 통합 추가

--- src/test/java/nextstep/subway/line/acceptance/LineSectionAcceptanceTest.java
+++ src/test/java/nextstep/subway/line/acceptance/LineSectionAcceptanceTest.java
@@ -20,6 +20,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 @DisplayName("지하철 구간 관련 기능")
 public class LineSectionAcceptanceTest extends AcceptanceTest {
@@ -40,6 +41,35 @@ public class LineSectionAcceptanceTest extends AcceptanceTest {
 
         LineRequest lineRequest = new LineRequest("신분당선", "bg-red-600", 강남역.getId(), 광교역.getId(), 10);
         신분당선 = LineAcceptanceTest.지하철_노선_등록되어_있음(lineRequest).as(LineResponse.class);
+    }
+
+    /**
+     * Feature: 지하철 구간 관련 기능
+     * <p>
+     * Background
+     * Given 지하철역 등록되어 있음
+     * And 지하철 노선 등록되어 있음
+     * And 지하철 노선에 지하철역 등록되어 있음
+     * <p>
+     * Scenario: 지하철 구간을 관리
+     * When 지하철 구간 등록 요청
+     * Then 지하철 구간 등록됨
+     * When 지하철 노선에 등록된 역 목록 조회 요청
+     * Then 등록한 지하철 구간이 반영된 역 목록이 조회됨
+     * When 지하철 구간 삭제 요청
+     * Then 지하철 구간 삭제됨
+     * When 지하철 노선에 등록된 역 목록 조회 요청
+     * Then 삭제한 지하철 구간이 반영된 역 목록이 조회됨
+     */
+    @DisplayName("지하철 구간 관리 시나리오 테스트")
+    @Test
+    void createAndDeleteLineSection() {
+        assertAll(
+                () -> 지하철_노선에_지하철역_등록됨(지하철_노선에_지하철역_등록_요청(신분당선, 강남역, 양재역, 3))
+                , () -> 지하철_노선에_지하철역_순서_정렬됨(LineAcceptanceTest.지하철_노선_조회_요청(신분당선), Arrays.asList(강남역, 양재역, 광교역))
+                , () -> 지하철_노선에_지하철역_제외됨(지하철_노선에_지하철역_제외_요청(신분당선, 강남역))
+                , () -> 지하철_노선에_지하철역_순서_정렬됨(LineAcceptanceTest.지하철_노선_조회_요청(신분당선), Arrays.asList(양재역, 광교역))
+        );
     }
 
     @DisplayName("지하철 구간을 등록한다.")
