18ca31838e36953ca7ba739f33bc8fdd76bf1052
deokmoon <deok2moon@gmail.com>
2022-12-25 01:55:35 +0900

    feat: 거리비례제 요금 적용 테스트코드 및 프로덕트 코드 작성

--- src/main/java/nextstep/subway/path/domain/Fare.java
+++ src/main/java/nextstep/subway/path/domain/Fare.java
@@ -2,7 +2,13 @@ package nextstep.subway.path.domain;
 
 public class Fare {
 
-    private static final long INIT_FARE = 1_250;
+    private static final long ZERO_FARE = 0L;
+    private static final long INIT_FARE = 1_250L;
+    private static final int FIRST_FARE_SECTION_DELIMITER = 10;
+    private static final int SECOND_FARE_SECTION_DELIMITER = 50;
+    private static final int ADD_FARE = 100;
+    private static final int FIRST_FARE_SECTION_PER_DISTANCE = 5;
+    private static final int SECOND_FARE_SECTION_PER_DISTANCE = 8;
 
     private long fare;
 
@@ -16,5 +22,35 @@ public class Fare {
 
     public long currentFare() {
         return this.fare;
+    }
+
+    public void calculateFareByDistanceProportional(int distance) {
+        long tempFare = ZERO_FARE;
+        if (isBelongToFirstFareSection(distance)) {
+            tempFare += calculateOverFareWhenFirstFareSection(distance - FIRST_FARE_SECTION_DELIMITER);
+        }
+        if (isBelongToSecondFareSection(distance)) {
+            tempFare += calculateOverFareWhenSecondFareSection(distance - SECOND_FARE_SECTION_DELIMITER);
+        }
+        this.fare += tempFare;
+    }
+
+    private boolean isBelongToSecondFareSection(int distance) {
+        return distance > SECOND_FARE_SECTION_DELIMITER;
+    }
+
+    private boolean isBelongToFirstFareSection(int distance) {
+        return distance > FIRST_FARE_SECTION_DELIMITER;
+    }
+
+    private long calculateOverFareWhenFirstFareSection(int distance) {
+        if (isBelongToSecondFareSection(distance + FIRST_FARE_SECTION_DELIMITER)) {
+            distance = SECOND_FARE_SECTION_DELIMITER - FIRST_FARE_SECTION_DELIMITER;
+        }
+        return (int) ((Math.ceil((distance - 1) / FIRST_FARE_SECTION_PER_DISTANCE) + 1) * ADD_FARE);
+    }
+
+    private long calculateOverFareWhenSecondFareSection(int distance) {
+        return (int) ((Math.ceil((distance - 1) / SECOND_FARE_SECTION_PER_DISTANCE) + 1) * ADD_FARE);
     }
 }
--- src/test/java/nextstep/subway/path/domain/FareTest.java
+++ src/test/java/nextstep/subway/path/domain/FareTest.java
@@ -2,12 +2,11 @@ package nextstep.subway.path.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-/**
- * 1. 기본 생성(기본 운임)은 1,250원
- */
 class FareTest {
 
     @DisplayName("요금 객체의 Default_운임(기본요금)은 1250원이다.")
@@ -16,4 +15,15 @@ class FareTest {
         Fare fare = Fare.from();
         assertThat(fare.currentFare()).isEqualTo(1250);
     }
+
+    @DisplayName("거리비례제를 적용하여 요금을 계산한다.")
+    @ParameterizedTest
+    @CsvSource(value = {"11:1350", "59:2250"}, delimiter = ':')
+    void calculateFareByDistanceProportional(int distance, long expectFare) {
+        Fare fare = Fare.from();
+        fare.calculateFareByDistanceProportional(distance);
+        assertThat(fare.currentFare()).isEqualTo(expectFare);
+    }
+
+
 }
